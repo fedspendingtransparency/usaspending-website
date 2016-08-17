6270cec42f58dce95bfcc7b36fe88718f58c0ce3
Emily Gullo <gullo_emily@bah.com>
2016-08-17 12:51:40 -0400

    package update + text change
    - adding url for bugs to package json
    - updating text case in h1 as per request

--- src/js/components/Welcome.jsx
+++ src/js/components/Welcome.jsx
@@ -4,7 +4,7 @@ export default class Welcome extends React.Component {
     render() {
         return (
             <div className="usa-da-page-title">
-                <h1>Welcome to USASpending.gov</h1>
+                <h1>Welcome to USAspending.gov</h1>
             </div>
         );
     }
