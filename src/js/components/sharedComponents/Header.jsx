172d3f10fb4849240a1b44b10b3a308fcd06e94f
Mike Bray <mab6bf@virginia.edu>
2016-10-20 11:25:16 -0400

    Update Header.jsx

--- src/js/components/sharedComponents/Header.jsx
+++ src/js/components/sharedComponents/Header.jsx
@@ -12,12 +12,12 @@ export default class Header extends React.Component {
                 <div className="container">
                     <div className="row">
                         <div className="top-alert-icon">
-                            <i className="usa-da-icon"><Icons.ExclamationTriangle/>
-                            </i>
+                            <i className="usa-da-icon"><Icons.ExclamationTriangle/></i>
                         </div>
                         <div className="top-alert-text">
                             <p>This site is not intended to be an official resource for federal spending data. To submit official federal spending data, please visit
-                                <a href="http://www.USAspending.gov" target="_blank">USAspending.gov</a>.</p>
+                                <a href="http://www.USAspending.gov" target="_blank">USAspending.gov</a>.
+                            </p>
                         </div>
                     </div>
                 </div>
