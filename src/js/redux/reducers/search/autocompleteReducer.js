decfb00d9984c7783bf2d182f433ff97bb8ccf59
Michael Bray <bray_michael@bah.com>
2016-12-20 17:39:52 -0500

    Changing return of Autocomplete Reducer to be a new array

--- src/js/redux/reducers/search/autocompleteReducer.js
+++ src/js/redux/reducers/search/autocompleteReducer.js
@@ -2,12 +2,14 @@
  * Created by michaelbray on 12/12/16.
  */
 
+import _ from 'lodash';
+
 const initialState = [];
 
 const autocompleteReducer = (state = initialState, action) => {
     switch (action.type) {
         case 'SET_AUTOCOMPLETE_LOCATIONS':
-            return action.locations;
+            return _.concat([], action.locations);
         default:
             return state;
     }
