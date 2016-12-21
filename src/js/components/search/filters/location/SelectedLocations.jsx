19c67a8059abe9afb0af0b600484dd16e6037f00
Emily Gullo <gullo_emily@bah.com>
2016-12-21 10:56:47 -0500

    - Updating ShownLocation, passing down label from parent component SelectedLocations.

--- src/js/components/search/filters/location/SelectedLocations.jsx
+++ src/js/components/search/filters/location/SelectedLocations.jsx
@@ -4,6 +4,7 @@
  **/
 
 import React from 'react';
+import _ from 'lodash';
 import ShownLocation from './ShownLocation';
 
 const propTypes = {
@@ -12,10 +13,33 @@ const propTypes = {
 };
 
 export default class SelectedLocations extends React.Component {
+    constructor(props) {
+        super(props);
+        this.formatLocation = this.formatLocation.bind(this);
+    }
+
+    formatLocation(loc) {
+        const location = loc;
+        let displayValue = '';
+
+        if (location.place_type !== null) {
+            displayValue = `${_.startCase(_.toLower(location.place_type))} | `;
+        }
+
+        displayValue += `${location.place}`;
+
+        if (location.parent !== null) {
+            displayValue += `, ${location.parent}`;
+        }
+
+        return displayValue;
+    }
+
     render() {
         const shownLocations = this.props.selectedLocations.map((location, key) => (
             <ShownLocation
                 location={location}
+                label={this.formatLocation(location)}
                 key={key.matched_ids.join(',').concat(`_${key.place}`).concat(`_${key.place_type}`)}
                 removeLocation={this.props.removeLocation.bind(null, location)} />
         ));
--- src/js/components/search/filters/location/ShownLocation.jsx
+++ src/js/components/search/filters/location/ShownLocation.jsx
@@ -4,46 +4,21 @@
  **/
 
 import React from 'react';
-import _ from 'lodash';
 
 const propTypes = {
-    location: React.PropTypes.object,
-    removeLocation: React.PropTypes.func
+    removeLocation: React.PropTypes.func,
+    label: React.PropTypes.string
 };
 
 export default class ShownLocation extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            locationDisplayValue: this.formatLocation()
-        };
-    }
-
-    formatLocation() {
-        const location = this.props.location;
-        let displayValue = '';
-
-        if (location.place_type !== null) {
-            displayValue = `${_.startCase(_.toLower(location.place_type))} | `;
-        }
-
-        displayValue += `${location.place}`;
-
-        if (location.parent !== null) {
-            displayValue += `, ${location.parent}`;
-        }
-
-        return displayValue;
-    }
 
     render() {
         return (
             <button
                 className="shown-location-button"
-                value={this.state.locationDisplayValue}
+                value={this.props.label}
                 onClick={this.props.removeLocation}>
-                {this.state.locationDisplayValue} <span className="close">x</span>
+                {this.props.label} <span className="close">x</span>
             </button>
         );
     }
