c97d609fcce81b646901a64b6ce779200dfbffef
Kevin Li <li_kevin@bah.com>
2017-01-17 15:17:57 -0500

    Refactored Location filter redux store to be an OrderedMap instead of a Set

--- src/js/redux/reducers/search/filters/locationFilterFunctions.js
+++ src/js/redux/reducers/search/filters/locationFilterFunctions.js
@@ -2,18 +2,21 @@
  * Created by michaelbray on 12/12/16.
  */
 
-import { Set } from 'immutable';
+import _ from 'lodash';
 
 /* eslint-disable import/prefer-default-export */
 // We only have one export but want to maintain consistency with other query modules
 export const updateSelectedLocations = (state, value) => {
-    let updatedSet = new Set(state);
+    let updatedSet = state;
+    // generate an identifier string based on matched IDs and place name
+    const locationIdentifier =
+        `${_.sortBy(value.matched_ids).join(',')}_${value.place}_${value.place_type}`;
 
-    if (updatedSet.includes(value)) {
-        updatedSet = updatedSet.delete(value);
+    if (updatedSet.has(locationIdentifier)) {
+        updatedSet = updatedSet.delete(locationIdentifier);
     }
     else {
-        updatedSet = updatedSet.add(value);
+        updatedSet = updatedSet.set(locationIdentifier, value);
     }
 
     return updatedSet;
