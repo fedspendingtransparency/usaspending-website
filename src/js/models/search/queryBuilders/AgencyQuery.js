afeb62a72b4884850459745ac00364edbff3923b
Michael Bray <bray_michael@bah.com>
2017-02-16 14:26:41 -0500

    Modifying AgencyQuery to exclude empty objects from the filters object

--- src/js/models/search/queryBuilders/AgencyQuery.js
+++ src/js/models/search/queryBuilders/AgencyQuery.js
@@ -3,59 +3,69 @@
 * Created by Emily Gullo
 **/
 
-import _ from 'lodash';
-
 /* eslint-disable import/prefer-default-export */
 // We only have one export but want to maintain consistency with other query modules
 
 export const buildAgencyQuery = (funding, awarding) => {
-    let toptierFundingSet = [];
-    let subtierFundingSet = [];
-    let toptierAwardingSet = [];
-    let subtierAwardingSet = [];
+    const toptierFundingSet = [];
+    const subtierFundingSet = [];
+    const toptierAwardingSet = [];
+    const subtierAwardingSet = [];
+
+    const filter = {
+        combine_method: 'OR',
+        filters: []
+    };
 
     funding.forEach((agencyArray) => {
         if (agencyArray.agencyType === 'toptier') {
-            toptierFundingSet = _.concat(toptierFundingSet, agencyArray.toptier_agency.name);
+            toptierFundingSet.push(agencyArray.toptier_agency.name);
         }
         else {
-            subtierFundingSet = _.concat(subtierFundingSet, agencyArray.subtier_agency.name);
+            subtierFundingSet.push(agencyArray.subtier_agency.name);
         }
     });
 
     awarding.forEach((agencyArray) => {
         if (agencyArray.agencyType === 'toptier') {
-            toptierAwardingSet = _.concat(toptierAwardingSet, agencyArray.toptier_agency.name);
+            toptierAwardingSet.push(agencyArray.toptier_agency.name);
         }
         else {
-            subtierAwardingSet = _.concat(subtierAwardingSet, agencyArray.subtier_agency.name);
+            subtierAwardingSet.push(agencyArray.subtier_agency.name);
         }
     });
 
-    const filter = {
-        combine_method: 'OR',
-        filters: [
-            {
-                field: 'funding_agency__toptier_agency__name',
-                operation: "in",
-                value: toptierFundingSet
-            },
-            {
-                field: 'funding_agency__subtier_agency__name',
-                operation: "in",
-                value: subtierFundingSet
-            },
-            {
-                field: 'awarding_agency__toptier_agency__name',
-                operation: "in",
-                value: toptierAwardingSet
-            },
-            {
-                field: 'awarding_agency__subtier_agency__name',
-                operation: "in",
-                value: subtierAwardingSet
-            }]
-    };
+    if (toptierFundingSet.length > 0) {
+        filter.filters.push({
+            field: 'funding_agency__toptier_agency__name',
+            operation: "in",
+            value: toptierFundingSet
+        });
+    }
+
+    if (subtierFundingSet.length > 0) {
+        filter.filters.push({
+            field: 'funding_agency__subtier_agency__name',
+            operation: "in",
+            value: subtierFundingSet
+        });
+    }
+
+    if (toptierAwardingSet.length > 0) {
+        filter.filters.push({
+            field: 'awarding_agency__toptier_agency__name',
+            operation: "in",
+            value: toptierAwardingSet
+        });
+    }
+
+    if (subtierAwardingSet.length > 0) {
+        filter.filters.push({
+            field: 'awarding_agency__subtier_agency__name',
+            operation: "in",
+            value: subtierAwardingSet
+        });
+    }
 
     return filter;
 };
