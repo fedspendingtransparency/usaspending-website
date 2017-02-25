795f9af9be15f7f89285e8373a2dcd953b897a66
Kevin Li <li_kevin@bah.com>
2017-02-25 17:56:14 -0500

    Fix narrative

--- src/js/components/award/AwardAmounts.jsx
+++ src/js/components/award/AwardAmounts.jsx
@@ -5,8 +5,9 @@
 
 import React from 'react';
 import accounting from 'accounting';
-import _ from 'lodash';
 import AmountsChartContainer from 'containers/award/AmountsChartContainer';
+
+import * as MoneyFormatter from 'helpers/moneyFormatter';
 
 const propTypes = {
     selectedAward: React.PropTypes.object
@@ -14,44 +15,77 @@ const propTypes = {
 
 export default class AwardAmounts extends React.Component {
 
-    render() {
+    formatFriendlyString(value) {
+        // format the ceiling and current values to be friendly strings
+        const units = MoneyFormatter.calculateUnitForSingleValue(value);
+        // only reformat at a million or higher
+        if (units.unit < MoneyFormatter.unitValues.MILLION) {
+            units.unit = 1;
+            units.unitLabel = '';
+            units.longLabel = '';
+        }
+        const formattedValue = value / units.unit;
+        let precision = 1;
+        if (formattedValue % 1 === 0) {
+            // whole number
+            precision = 0;
+        }
+
+        const formattedCurrency =
+            MoneyFormatter.formatMoneyWithPrecision(formattedValue, precision);
+
+        // don't add an extra space when there's no units string to display
+        let longLabel = '';
+        if (units.unit > 1) {
+            longLabel = ` ${units.longLabel}`;
+        }
+
+        return `${formattedCurrency}${longLabel}`;
+    }
+
+    generateNarrative() {
         const recipient = this.props.selectedAward.recipient_name.toLowerCase();
+
+        const ceiling =
+            accounting.unformat(this.props.selectedAward.potential_total_value_of_award);
+        const current = accounting.unformat(this.props.selectedAward.total_obligation);
+
+        // calculate the percentage spent
+        let percentage = 'N/A';
+        if (ceiling && ceiling !== 0) {
+            percentage = Math.floor((current / ceiling) * 1000) / 10;
+        }
+
+        return {
+            recipient,
+            percentage,
+            ceiling: this.formatFriendlyString(ceiling),
+            current: this.formatFriendlyString(current)
+        };
+    }
+
+    render() {
         // Math
-        const potential = accounting.unformat(this.props.selectedAward.total_obligation);
-        const current =
-        accounting.unformat(this.props.selectedAward.potential_total_value_of_award);
-        const percentage = _.divide(current, potential);
-        let sub = null;
-        let truncatedPercent = "";
+        const current = accounting.unformat(this.props.selectedAward.total_obligation);
+        const potential =
+            accounting.unformat(this.props.selectedAward.potential_total_value_of_award);
 
-        if (this.props.selectedAward.subaward_amount) {
-            sub = accounting.unformat(this.props.selectedAward.subaward_amount);
-        }
-        if (percentage === 1) {
-            truncatedPercent = 100;
-        }
-        else {
-            truncatedPercent = _.ceil(percentage, 2);
-        }
-
-        // TODO: When subaward amount is available on endpoint,add it here if it exists, as per mockup
-        // TODO: round dollar amounts in description to billions/ millions
-        const subAward = "";
+        const narrative = this.generateNarrative();
         return (
             <div className="amounts-wrapper">
                 <div className="award-amounts">
                     <h4>Award Amounts</h4>
                     <div className="border" />
                     <div className="text-details">
-                        <p>This contract was awarded to <b className="recipient-name">{recipient}</b> with
-                            a ceiling of <b>{this.props.selectedAward.total_obligation}</b>.
-                            Of this amount, <b>{truncatedPercent}% ({this.props.selectedAward.potential_total_value_of_award})</b> has
-                            been obligated. { subAward }</p>
+                        <p>This contract was awarded to&nbsp;
+                            <b className="recipient-name">{narrative.recipient}</b> with a ceiling
+                            of <b>{narrative.ceiling}</b>.
+                            Of this amount, <b>{narrative.percentage}%</b> ({narrative.current}) has
+                            been obligated.</p>
                     </div>
                     <AmountsChartContainer
                         potential={potential}
-                        current={current}
-                        sub={sub} />
+                        current={current} />
                 </div>
             </div>
         );
