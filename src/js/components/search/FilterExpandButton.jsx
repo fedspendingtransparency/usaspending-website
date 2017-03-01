91406699137b875b5c6bc0df53c9b2b20ea61abd
Marco Mendoza <marco@poplicus.com>
2017-03-01 10:33:53 -0500

    Fix the logic operator which was disabling the working filters

--- src/js/components/search/FilterExpandButton.jsx
+++ src/js/components/search/FilterExpandButton.jsx
@@ -26,11 +26,11 @@ export default class FilterExpandButton extends React.Component {
         }
 
         let disabledStatus = false;
-        if (this.props.name !== 'Search' ||
-            this.props.name !== 'Budget Categories' ||
-            this.props.name !== 'Award ID' ||
-            this.props.name !== 'Award Amount' ||
-            this.props.name !== 'Other Award Items') {
+        if (this.props.name === 'Search' ||
+            this.props.name === 'Budget Categories' ||
+            this.props.name === 'Award ID' ||
+            this.props.name === 'Award Amount' ||
+            this.props.name === 'Other Award Items') {
                 disabledStatus = true;
         }
 
