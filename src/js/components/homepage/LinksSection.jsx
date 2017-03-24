c8159d5c06a6b49496a642128a4a50ce583b8a47
Destin Frasier <frasier_destin@bah.com>
2017-03-24 13:19:45 -0400

    Fixes to the TreeMap and bottom section of the homepage.
    * Updated the colors of the TreeMap.
    * Made the bottom section fully responsive.

--- src/js/components/homepage/LinksSection.jsx
+++ src/js/components/homepage/LinksSection.jsx
@@ -11,32 +11,34 @@ export default class LinksSection extends React.Component {
 
     render() {
         return (
-            <div className="links-section-wrap">
-                <h4
-                    className="links-section-primary-title">
-                    Check out these sections for more views into our data.
-                </h4>
-                <div className="links-section-box-wrap">
-                    <LinksSectionBox
-                        icon="recipient"
-                        text="Get insights into specific recipients."
-                        linkText="Find a recipient"
-                        linkUrl="http://google.com" />
-                    <LinksSectionBox
-                        icon="federalAccount"
-                        text="See how spending is disbursed."
-                        linkText="Find a Federal account"
-                        linkUrl="http://google.com" />
-                    <LinksSectionBox
-                        icon="api"
-                        text="Plug directly into our data."
-                        linkText="View the documentation"
-                        linkUrl="http://google.com" />
-                    <LinksSectionBox
-                        icon="resources"
-                        text="Learn more about the data."
-                        linkText="Visit our 'About' page"
-                        linkUrl="http://google.com" />
+            <div className="links-section-outter-wrap">
+                <div className="links-section-wrap">
+                    <h4
+                        className="links-section-primary-title">
+                        Check out these sections for more views into our data.
+                    </h4>
+                    <div className="links-section-box-wrap">
+                        <LinksSectionBox
+                            icon="recipient"
+                            text="Get insights into specific recipients."
+                            linkText="Find a recipient"
+                            linkUrl="http://google.com" />
+                        <LinksSectionBox
+                            icon="federalAccount"
+                            text="See how spending is disbursed."
+                            linkText="Find a Federal account"
+                            linkUrl="http://google.com" />
+                        <LinksSectionBox
+                            icon="api"
+                            text="Plug directly into our data."
+                            linkText="View the documentation"
+                            linkUrl="http://google.com" />
+                        <LinksSectionBox
+                            icon="resources"
+                            text="Learn more about the data."
+                            linkText="Visit our 'About' page"
+                            linkUrl="http://google.com" />
+                    </div>
                 </div>
             </div>
         );
