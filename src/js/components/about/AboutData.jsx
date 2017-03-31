f0a4f6b46eec7f46a01f0d5281034c8c93accecc
Rickey An <an_richard@bah.com>
2017-03-31 15:37:36 -0400

    Update About the data section

--- src/js/components/about/AboutData.jsx
+++ src/js/components/about/AboutData.jsx
@@ -11,7 +11,7 @@ export default class AboutData extends React.Component {
     render() {
         return (
             <div className="aboutdata-wrap">
-                <p>TEST Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet risus sed urna cursus mollis. Donec sagittis nunc pretium dui congue, id gravida purus lobortis. Nunc sed varius massa. Nam blandit cursus metus a maximus. Vivamus pretium augue sed est aliquam mollis. Mauris efficitur arcu vitae venenatis sodales. Nulla et massa nibh. Duis ut eleifend augue. Suspendisse elit lacus, bibendum non felis eu, accumsan consectetur augue. Phasellus id aliquet nulla. Vivamus tristique, mauris et suscipit luctus, mi sapien imperdiet risus, quis condimentum libero neque vel risus.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet risus sed urna cursus mollis. Donec sagittis nunc pretium dui congue, id gravida purus lobortis. Nunc sed varius massa. Nam blandit cursus metus a maximus. Vivamus pretium augue sed est aliquam mollis. Mauris efficitur arcu vitae venenatis sodales. Nulla et massa nibh. Duis ut eleifend augue. Suspendisse elit lacus, bibendum non felis eu, accumsan consectetur augue. Phasellus id aliquet nulla. Vivamus tristique, mauris et suscipit luctus, mi sapien imperdiet risus, quis condimentum libero neque vel risus.</p>
             </div>
         );
     }
