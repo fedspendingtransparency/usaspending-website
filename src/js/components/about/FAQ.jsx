e9f5a5e79062812ae232f837a36c1e0a0f6b1b45
dfrasier <frasier_destin@bah.com>
2017-04-10 11:50:01 -0400

    Small changes to the about us page.

--- src/js/components/about/FAQ.jsx
+++ src/js/components/about/FAQ.jsx
@@ -13,10 +13,12 @@ export default class FAQ extends React.Component {
         return (
             <div className="faq-wrap">
                 <div className="faq-inner-wrap">
-                    <div className="img-placeholder"></div>
+                    <div className="img-placeholder" />
                     <h3>Frequently Asked Questions</h3>
                     <hr className="results-divider" />
-                    <p>Frequently Asked Questions. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet risus sed urna cursus mollis. Donec sagittis nunc pretium dui congue, id gravida purus lobortis. Nunc sed varius massa. Nam blandit cursus metus a maximus. Vivamus pretium augue sed est aliquam mollis. Mauris efficitur arcu vitae venenatis sodales.</p>
+                    <p>Frequently Asked Questions. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet risus sed urna cursus mollis. 
+                    Donec sagittis nunc pretium dui congue, id gravida purus lobortis. Nunc sed varius massa. Nam blandit cursus metus a maximus. 
+                    Vivamus pretium augue sed est aliquam mollis. Mauris efficitur arcu vitae venenatis sodales.</p>
                     <a href="#">
                         <button
                             className="usa-button-primary"
