ec2f7027075adeac7d507d97f5427bba24a227ef
Elizabeth Dabbs <dabbs_elizabeth@bah.com>
2017-04-19 18:07:19 -0400

    added tests for quarters and filtered obligated

--- tests/containers/account/mockAccount.js
+++ tests/containers/account/mockAccount.js
@@ -32,6 +32,80 @@ export const mockReduxAccount = {
     }
 };
 
+export const mockReduxAccountFiltered = {
+    id: 2507,
+    title: 'Title 17 Innovative Technology Loan Guarantee Program, Energy Programs, Energy',
+    agency_identifier: '089',
+    main_account_code: '0208',
+    description: 'Not available',
+    totals: {
+        outlay: {
+            2016: '-5505246.42'
+        },
+        budgetAuthority: {
+            2016: '201404661.47'
+        },
+        unobligated: {
+            2016: '198707976.61'
+        },
+        obligatedFiltered: {
+            2016: '2696684.86'
+        }
+    }
+};
+
+export const mockReduxAccountQuarters = {
+    id: 2507,
+    title: 'Title 17 Innovative Technology Loan Guarantee Program, Energy Programs, Energy',
+    agency_identifier: '089',
+    main_account_code: '0208',
+    description: 'Not available',
+    totals: {
+        outlay: {
+            "2016 Q1": '-5505246.42',
+            "2016 Q2": '-4413237.11'
+        },
+        budgetAuthority: {
+            "2016 Q1": '201404661.47',
+            "2016 Q2": '101905442.35'
+        },
+        obligated: {
+            "2016 Q1": '2696684.86',
+            "2016 Q2": '3851752.00'
+        },
+        unobligated: {
+            "2016 Q1": '198707976.61',
+            "2016 Q2": '5851779752.00'
+        }
+    }
+};
+
+export const mockReduxAccountQuartersFiltered = {
+    id: 2507,
+    title: 'Title 17 Innovative Technology Loan Guarantee Program, Energy Programs, Energy',
+    agency_identifier: '089',
+    main_account_code: '0208',
+    description: 'Not available',
+    totals: {
+        outlay: {
+            "2016 Q1": '-5505246.42',
+            "2016 Q2": '-4413237.11'
+        },
+        budgetAuthority: {
+            "2016 Q1": '201404661.47',
+            "2016 Q2": '101905442.35'
+        },
+        unobligated: {
+            "2016 Q1": '198707976.61',
+            "2016 Q2": '5851779752.00'
+        },
+        obligatedFiltered: {
+            "2016 Q1": '2696684.86',
+            "2016 Q2": '3851752.00'
+        }
+    }
+};
+
 export const mockBalances = {
     unobligated: {
         page_metadata: {
@@ -80,6 +154,235 @@ export const mockBalances = {
         results: [{
             item: "2016",
             aggregate: "-5505246.42"
+        }],
+        total_metadata: {
+            count: 1
+        }
+    }
+};
+
+export const mockQuarters = {
+    unobligated: {
+        page_metadata: {
+            count: 1,
+            page_number: 1
+        },
+        results: [{
+            item: "2016",
+            aggregate: "198707976.61",
+            submission__reporting_fiscal_year: "2016",
+            submission__reporting_fiscal_quarter: "1"
+        },
+        {
+            item: "2016",
+            aggregate: "5851779752.00",
+            submission__reporting_fiscal_year: "2016",
+            submission__reporting_fiscal_quarter: "2"
+        }],
+        total_metadata: {
+            count: 1
+        }
+    },
+    obligated: {
+        page_metadata: {
+            count: 1,
+            page_number: 1
+        },
+        results: [{
+            item: "2016",
+            aggregate: "2696684.86",
+            submission__reporting_fiscal_year: "2016",
+            submission__reporting_fiscal_quarter: "1"
+        },
+        {
+            item: "2016",
+            aggregate: "3851752.00",
+            submission__reporting_fiscal_year: "2016",
+            submission__reporting_fiscal_quarter: "2"
+        }],
+        total_metadata: {
+            count: 1
+        }
+    },
+    budgetAuthority: {
+        page_metadata: {
+            count: 1,
+            page_number: 1
+        },
+        results: [{
+            item: "2016",
+            aggregate: "201404661.47",
+            submission__reporting_fiscal_year: "2016",
+            submission__reporting_fiscal_quarter: "1"
+        },
+        {
+            item: "2016",
+            aggregate: "101905442.35",
+            submission__reporting_fiscal_year: "2016",
+            submission__reporting_fiscal_quarter: "2"
+        }],
+        total_metadata: {
+            count: 1
+        }
+    },
+    outlay: {
+        page_metadata: {
+            count: 1,
+            page_number: 1
+        },
+        results: [{
+            item: "2016",
+            aggregate: "-5505246.42",
+            submission__reporting_fiscal_year: "2016",
+            submission__reporting_fiscal_quarter: "1"
+        },
+        {
+            item: "2016",
+            aggregate: "-4413237.11",
+            submission__reporting_fiscal_year: "2016",
+            submission__reporting_fiscal_quarter: "2"
+        }],
+        total_metadata: {
+            count: 1
+        }
+    }
+};
+
+export const mockFilteredObligated = {
+    unobligated: {
+        page_metadata: {
+            count: 1,
+            page_number: 1
+        },
+        results: [{
+            item: "2016",
+            aggregate: "198707976.61"
+        }],
+        total_metadata: {
+            count: 1
+        }
+    },
+    obligatedFiltered: {
+        page_metadata: {
+            count: 1,
+            page_number: 1
+        },
+        results: [{
+            item: "2016",
+            aggregate: "2696684.86"
+        }],
+        total_metadata: {
+            count: 1
+        }
+    },
+    budgetAuthority: {
+        page_metadata: {
+            count: 1,
+            page_number: 1
+        },
+        results: [{
+            item: "2016",
+            aggregate: "201404661.47"
+        }],
+        total_metadata: {
+            count: 1
+        }
+    },
+    outlay: {
+        page_metadata: {
+            count: 1,
+            page_number: 1
+        },
+        results: [{
+            item: "2016",
+            aggregate: "-5505246.42"
+        }],
+        total_metadata: {
+            count: 1
+        }
+    }
+};
+
+export const mockFilteredObligatedQuarters = {
+    unobligated: {
+        page_metadata: {
+            count: 1,
+            page_number: 1
+        },
+        results: [{
+            item: "2016",
+            aggregate: "198707976.61",
+            submission__reporting_fiscal_year: "2016",
+            submission__reporting_fiscal_quarter: "1"
+        },
+        {
+            item: "2016",
+            aggregate: "5851779752.00",
+            submission__reporting_fiscal_year: "2016",
+            submission__reporting_fiscal_quarter: "2"
+        }],
+        total_metadata: {
+            count: 1
+        }
+    },
+    obligatedFiltered: {
+        page_metadata: {
+            count: 1,
+            page_number: 1
+        },
+        results: [{
+            item: "2016",
+            aggregate: "2696684.86",
+            submission__reporting_fiscal_year: "2016",
+            submission__reporting_fiscal_quarter: "1"
+        },
+        {
+            item: "2016",
+            aggregate: "3851752.00",
+            submission__reporting_fiscal_year: "2016",
+            submission__reporting_fiscal_quarter: "2"
+        }],
+        total_metadata: {
+            count: 1
+        }
+    },
+    budgetAuthority: {
+        page_metadata: {
+            count: 1,
+            page_number: 1
+        },
+        results: [{
+            item: "2016",
+            aggregate: "201404661.47",
+            submission__reporting_fiscal_year: "2016",
+            submission__reporting_fiscal_quarter: "1"
+        },
+        {
+            item: "2016",
+            aggregate: "101905442.35",
+            submission__reporting_fiscal_year: "2016",
+            submission__reporting_fiscal_quarter: "2"
+        }],
+        total_metadata: {
+            count: 1
+        }
+    },
+    outlay: {
+        page_metadata: {
+            count: 1,
+            page_number: 1
+        },
+        results: [{
+            item: "2016",
+            aggregate: "-5505246.42",
+            submission__reporting_fiscal_year: "2016",
+            submission__reporting_fiscal_quarter: "1"
+        },
+        {
+            item: "2016",
+            aggregate: "-4413237.11",
+            submission__reporting_fiscal_year: "2016",
+            submission__reporting_fiscal_quarter: "2"
         }],
         total_metadata: {
             count: 1
--- tests/containers/account/visualizations/AccountTimeVisualizationContainer-test.jsx
+++ tests/containers/account/visualizations/AccountTimeVisualizationContainer-test.jsx
@@ -12,7 +12,9 @@ import { AccountTimeVisualizationSectionContainer } from
     'containers/account/visualizations/AccountTimeVisualizationContainer';
 
 import * as AccountHelper from 'helpers/accountHelper';
-import { mockBalances, mockReduxAccount } from '../mockAccount';
+import { mockBalances, mockReduxAccount, mockQuarters, mockFilteredObligated, mockFilteredObligatedQuarters,
+    mockReduxAccountFiltered, mockReduxAccountQuarters, mockReduxAccountQuartersFiltered}
+    from '../mockAccount';
 import { defaultFilters } from '../defaultFilters';
 
 // force Jest to use native Node promises
@@ -143,6 +145,197 @@ describe('AccountTimeVisualizationSectionContainer', () => {
                         obligated: parseFloat(mockReduxAccount.totals.obligated['2016']),
                         outlay: parseFloat(mockReduxAccount.totals.outlay['2016']),
                         unobligated: parseFloat(mockReduxAccount.totals.unobligated['2016'])
+                    }]
+                ]
+            };
+
+            expect(container.state()).toEqual(expectedState);
+        });
+    });
+
+    describe('parseBalancesQuarters', () => {
+        it('should parse the API responses and update the container state with series data', () => {
+            const container = shallow(<AccountTimeVisualizationSectionContainer
+                reduxFilters={defaultFilters}
+                account={mockReduxAccount} />);
+
+            container.instance().setState({
+                visualizationPeriod: "quarter"
+            });
+
+            container.instance().balanceRequests = [
+                {
+                    type: 'outlay'
+                },
+                {
+                    type: 'budgetAuthority'
+                },
+                {
+                    type: 'obligated'
+                },
+                {
+                    type: 'unobligated'
+                }
+            ];
+
+            container.instance().parseBalances([
+                {
+                    data: mockQuarters.outlay
+                },
+                {
+                    data: mockQuarters.budgetAuthority
+                },
+                {
+                    data: mockQuarters.obligated
+                },
+                {
+                    data: mockQuarters.unobligated
+                }
+            ]);
+
+            const expectedState = {
+                allY:
+                    [-5505246.42, -4413237.11, 201404661.47, 101905442.35, 2696684.86, 3851752, 198707976.61, 5851779752],
+                groups: ["2016 Q1", "2016 Q2"],
+                loading: false,
+                visualizationPeriod: "quarter",
+                xSeries: [["2016 Q1"], ["2016 Q2"]],
+                ySeries: [
+                    [{
+                        budgetAuthority: parseFloat(mockReduxAccountQuarters.totals.budgetAuthority['2016 Q1']),
+                        obligated: parseFloat(mockReduxAccountQuarters.totals.obligated['2016 Q1']),
+                        outlay: parseFloat(mockReduxAccountQuarters.totals.outlay['2016 Q1']),
+                        unobligated: parseFloat(mockReduxAccountQuarters.totals.unobligated['2016 Q1'])
+                    }],
+                    [{
+                        budgetAuthority: parseFloat(mockReduxAccountQuarters.totals.budgetAuthority['2016 Q2']),
+                        obligated: parseFloat(mockReduxAccountQuarters.totals.obligated['2016 Q2']),
+                        outlay: parseFloat(mockReduxAccountQuarters.totals.outlay['2016 Q2']),
+                        unobligated: parseFloat(mockReduxAccountQuarters.totals.unobligated['2016 Q2'])
+                    }]
+                ]
+            };
+
+            expect(container.state()).toEqual(expectedState);
+        });
+    });
+
+    describe('parseBalancesFiltered', () => {
+        it('should parse the API responses and update the container state with series data', () => {
+            const container = shallow(<AccountTimeVisualizationSectionContainer
+                reduxFilters={defaultFilters}
+                account={mockReduxAccount} />);
+
+            container.instance().balanceRequests = [
+                {
+                    type: 'outlay'
+                },
+                {
+                    type: 'budgetAuthority'
+                },
+                {
+                    type: 'obligatedFiltered'
+                },
+                {
+                    type: 'unobligated'
+                }
+            ];
+
+            container.instance().parseBalances([
+                {
+                    data: mockFilteredObligated.outlay
+                },
+                {
+                    data: mockFilteredObligated.budgetAuthority
+                },
+                {
+                    data: mockFilteredObligated.obligatedFiltered
+                },
+                {
+                    data: mockFilteredObligated.unobligated
+                }
+            ]);
+
+            const expectedState = {
+                allY:
+                    [-5505246.42, 201404661.47, 2696684.86, 198707976.61],
+                groups: ["2016"],
+                loading: false,
+                visualizationPeriod: "year",
+                xSeries: [["2016"]],
+                ySeries: [
+                    [{
+                        budgetAuthority: parseFloat(mockReduxAccountFiltered.totals.budgetAuthority['2016']),
+                        obligatedFiltered: parseFloat(mockReduxAccountFiltered.totals.obligatedFiltered['2016']),
+                        outlay: parseFloat(mockReduxAccountFiltered.totals.outlay['2016']),
+                        unobligated: parseFloat(mockReduxAccountFiltered.totals.unobligated['2016'])
+                    }]
+                ]
+            };
+
+            expect(container.state()).toEqual(expectedState);
+        });
+    });
+
+    describe('parseBalancesQuartersFiltered', () => {
+        it('should parse the API responses and update the container state with series data', () => {
+            const container = shallow(<AccountTimeVisualizationSectionContainer
+                reduxFilters={defaultFilters}
+                account={mockReduxAccount} />);
+
+            container.instance().setState({
+                visualizationPeriod: "quarter"
+            });
+
+            container.instance().balanceRequests = [
+                {
+                    type: 'outlay'
+                },
+                {
+                    type: 'budgetAuthority'
+                },
+                {
+                    type: 'obligatedFiltered'
+                },
+                {
+                    type: 'unobligated'
+                }
+            ];
+
+            container.instance().parseBalances([
+                {
+                    data: mockFilteredObligatedQuarters.outlay
+                },
+                {
+                    data: mockFilteredObligatedQuarters.budgetAuthority
+                },
+                {
+                    data: mockFilteredObligatedQuarters.obligatedFiltered
+                },
+                {
+                    data: mockFilteredObligatedQuarters.unobligated
+                }
+            ]);
+
+            const expectedState = {
+                allY:
+                    [-5505246.42, -4413237.11, 201404661.47, 101905442.35, 2696684.86, 3851752, 198707976.61, 5851779752],
+                groups: ["2016 Q1", "2016 Q2"],
+                loading: false,
+                visualizationPeriod: "quarter",
+                xSeries: [["2016 Q1"], ["2016 Q2"]],
+                ySeries: [
+                    [{
+                        budgetAuthority: parseFloat(mockReduxAccountQuartersFiltered.totals.budgetAuthority['2016 Q1']),
+                        obligatedFiltered: parseFloat(mockReduxAccountQuartersFiltered.totals.obligatedFiltered['2016 Q1']),
+                        outlay: parseFloat(mockReduxAccountQuartersFiltered.totals.outlay['2016 Q1']),
+                        unobligated: parseFloat(mockReduxAccountQuartersFiltered.totals.unobligated['2016 Q1'])
+                    }],
+                    [{
+                        budgetAuthority: parseFloat(mockReduxAccountQuartersFiltered.totals.budgetAuthority['2016 Q2']),
+                        obligatedFiltered: parseFloat(mockReduxAccountQuartersFiltered.totals.obligatedFiltered['2016 Q2']),
+                        outlay: parseFloat(mockReduxAccountQuartersFiltered.totals.outlay['2016 Q2']),
+                        unobligated: parseFloat(mockReduxAccountQuartersFiltered.totals.unobligated['2016 Q2'])
                     }]
                 ]
             };
