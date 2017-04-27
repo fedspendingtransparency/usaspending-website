70517512c87dfce46f99591584f2d80074a89048
Rickey An <an_richard@bah.com>
2017-04-27 10:05:40 -0400

    Fixed lint errors

--- src/js/components/article/Breadcrumb.jsx
+++ src/js/components/article/Breadcrumb.jsx
@@ -14,41 +14,41 @@ export default class Breadcrumb extends React.Component {
     render() {
         return (
             <div className="breadcrumbs">
-                    <ul>
-                        <li>
-                            <a href="#" role="button" title="Home" aria-label="Home">
-                                Home
-                            </a>
-                        </li>
-                        <div className="breadcrumb-arrow">
-                            <Icons.AngleRight />
-                        </div>
-                        <li>
-                            <a
-                                href="#/about"
-                                role="button"
-                                title="About Us"
-                                aria-label="About Us">
-                                About Us
-                            </a>
-                        </li>
-                        <svg
-                            className="usa-da-icon-angle-right"
-                            viewBox="0 0 512 512"
-                            aria-label="Arrow Pointing Right Icon"
-                            height="12">
-                            <title>
-                                Arrow Pointing Right Icon
-                            </title>
-                            <g>
-                                <path d="M143.5 434.8L304 257 143.8 77.3 143.4 6l225.2 250.5L144 506" />
-                            </g>
-                        </svg>
-                        <li>
-                            {this.props.title}
-                        </li>
-                    </ul>
-                </div>
+                <ul>
+                    <li>
+                        <a href="#" role="button" title="Home" aria-label="Home">
+                            Home
+                        </a>
+                    </li>
+                    <div className="breadcrumb-arrow">
+                        <Icons.AngleRight />
+                    </div>
+                    <li>
+                        <a
+                            href="#/about"
+                            role="button"
+                            title="About Us"
+                            aria-label="About Us">
+                            About Us
+                        </a>
+                    </li>
+                    <svg
+                        className="usa-da-icon-angle-right"
+                        viewBox="0 0 512 512"
+                        aria-label="Arrow Pointing Right Icon"
+                        height="12">
+                        <title>
+                            Arrow Pointing Right Icon
+                        </title>
+                        <g>
+                            <path d="M143.5 434.8L304 257 143.8 77.3 143.4 6l225.2 250.5L144 506" />
+                        </g>
+                    </svg>
+                    <li>
+                        {this.props.title}
+                    </li>
+                </ul>
+            </div>
         );
     }
 }
