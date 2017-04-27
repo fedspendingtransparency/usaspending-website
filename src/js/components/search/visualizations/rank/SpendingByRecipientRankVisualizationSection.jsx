e688f9a036c0db242d8290cb6932395f5bf3af26
Michael Bray <bray_michael@bah.com>
2017-04-27 17:56:55 -0400

    Spending By Awarding Agency and Spending By Recipient Visualizations

--- src/js/components/search/visualizations/rank/SpendingByRecipientRankVisualizationSection.jsx
+++ src/js/components/search/visualizations/rank/SpendingByRecipientRankVisualizationSection.jsx
@@ -1,7 +1,7 @@
 /**
-  * RankVisualizationSection.jsx
-  * Created by Kevin Li 12/13/16
-  **/
+ * SpendingByRecipientRankVisualizationSection.jsx
+ * Created by michaelbray on 4/27/17.
+ */
 
 import React from 'react';
 import _ from 'lodash';
@@ -13,17 +13,16 @@ import RankVisualization from './RankVisualization';
 import RankVisualizationScopeButton from './RankVisualizationScopeButton';
 
 const propTypes = {
-    agencyScope: React.PropTypes.string,
+    scope: React.PropTypes.string,
     changeScope: React.PropTypes.func,
     nextPage: React.PropTypes.func,
     previousPage: React.PropTypes.func,
     loading: React.PropTypes.bool,
     hasNextPage: React.PropTypes.bool,
-    hasPreviousPage: React.PropTypes.bool,
-    agencyType: React.PropTypes.string
+    hasPreviousPage: React.PropTypes.bool
 };
 
-export default class RankVisualizationSection extends React.Component {
+export default class SpendingByRecipientRankVisualizationSection extends React.Component {
     constructor(props) {
         super(props);
 
@@ -87,9 +86,9 @@ export default class RankVisualizationSection extends React.Component {
                 <div className="visualization-top">
                     <div className="visualization-description">
                         <div className="content">
-                            View a list of the top {this.props.agencyType} Agencies from highest to
-                            lowest. Filter your results more (at left) and watch this graph
-                            update automatically. View your results in a bar graph or a tree map.
+                            View a list of the top Recipients from highest to lowest. Filter
+                            your results more (at left) and watch this graph update automatically.
+                            View your results in a bar graph or a tree map.
                         </div>
                     </div>
                     <div className="visualization-period">
@@ -97,23 +96,23 @@ export default class RankVisualizationSection extends React.Component {
                             <ul>
                                 <li>
                                     <RankVisualizationScopeButton
-                                        value="toptier"
-                                        label="Agencies"
-                                        active={this.props.agencyScope === 'toptier'}
+                                        value="parentCompany"
+                                        label="Parent Company (Parent DUNS)"
+                                        active={this.props.scope === 'parentCompany'}
                                         changeScope={this.props.changeScope} />
                                 </li>
                                 <li>
                                     <RankVisualizationScopeButton
-                                        value="subtier"
-                                        label="Sub-Agencies"
-                                        active={this.props.agencyScope === 'subtier'}
+                                        value="subsidiary"
+                                        label="Subsidiary (DUNS)"
+                                        active={this.props.scope === 'subsidiary'}
                                         changeScope={this.props.changeScope} />
                                 </li>
                                 <li className="coming-soon">
                                     <RankVisualizationScopeButton
-                                        value="office"
-                                        label="Offices"
-                                        active={this.props.agencyScope === 'office'}
+                                        value="recipientType"
+                                        label="Recipient Type"
+                                        active={this.props.scope === 'recipientType'}
                                         changeScope={this.props.changeScope}
                                         disabled />
                                     <ComingSoonLabel />
@@ -165,4 +164,4 @@ export default class RankVisualizationSection extends React.Component {
     }
 }
 
-RankVisualizationSection.propTypes = propTypes;
+SpendingByRecipientRankVisualizationSection.propTypes = propTypes;
