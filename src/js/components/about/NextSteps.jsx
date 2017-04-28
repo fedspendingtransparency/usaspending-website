d7015d7f7f07cbe7c0adeecc4b7c89dbc14871c3
dfrasier <frasier_destin@bah.com>
2017-04-28 13:32:46 -0400

    Updates to the bottom section of the about page. Also added the “BETA” tag to the logo. Removed the "Congressional Budget Office" link from footer. Added a link to the api from the homepage blocks at the bottom of the page. Added a new outlined disabled button style.

--- src/js/components/about/NextSteps.jsx
+++ src/js/components/about/NextSteps.jsx
@@ -1,64 +1,53 @@
 /**
- * BudgetProcess.jsx
+ * NextSteps.jsx
  * Created by Destin Frasier 04/27/2017
  **/
 
 import React from 'react';
-import * as AboutIcons from 'components/sharedComponents/icons/about/AboutIcons';
+
+import NextStepsBox from './NextStepsBox';
 
 export default class NextSteps extends React.Component {
 
     render() {
         return (
             <div className="wrapper">
-                <div className="block-wrap">
-                    <div className="icon-wrap">
-                        <div className="faq-icon">
-                            <AboutIcons.QAIcon className="usa-da-qa-icon" />
-                        </div>
-                    </div>
-                    <h6>Frequently Asked Questions</h6>
-                    <p>Get answers to commonly asked questions</p>
-                    <a href="#">
-                        <button className="usa-button-outline" title="View FAQ" aria-label="View FAQ">View FAQ</button>
-                    </a>
-                </div>
-                <div className="block-wrap">
-                    <div className="icon-wrap">
-                        <div className="contact-icon">
-                            <AboutIcons.ContactIcon className="usa-da-contact-icon" />
-                        </div>
-                    </div>
-                    <h6>Contact Us</h6>
-                    <p>We want to hear your questions and comments</p>
-                    <a href="#">
-                        <button className="usa-button-outline" title="Email Us" aria-label="Email Us">Email Us</button>
-                    </a>
-                </div>
-                <div className="block-wrap">
-                    <div className="icon-wrap">
-                        <div className="download-icon">
-                            <AboutIcons.DownloadIcon className="usa-da-download-icon" />
-                        </div>
-                    </div>
-                    <h6>Agency Raw Submission Files</h6>
-                    <p>Access the data files submitted directly by agencies</p>
-                    <a href="#">
-                        <button className="usa-button-outline" title="Download Files" aria-label="Download Files">Download Files</button>
-                    </a>
-                </div>
-                <div className="block-wrap">
-                    <div className="icon-wrap">
-                        <div className="next-icon">
-                            <AboutIcons.WhatsNextIcon className="usa-da-whats-next-icon" />
-                        </div>
-                    </div>
-                    <h6>What’s Next</h6>
-                    <p>We&#8216;re adding new pages, features, and fixes every two weeks</p>
-                    <a href="#">
-                        <button className="usa-button-outline" title="Read More" aria-label="Read More">Read More</button>
-                    </a>
-                </div>
+                <NextStepsBox
+                    icon="faq"
+                    sectionTitle="Frequently Asked Questions"
+                    iconClass="faq-icon"
+                    sectionText="Get answers to commonly asked questions"
+                    linkText="View FAQ"
+                    ariaLabel="View FAQ"
+                    title="View FAQ"
+                    linkUrl="" />
+                <NextStepsBox
+                    icon="contact"
+                    sectionTitle="Contact Us"
+                    iconClass="contact-icon"
+                    sectionText="We want to hear your questions and comments"
+                    linkText="Email Us"
+                    ariaLabel="Email Us"
+                    title="Email US"
+                    linkUrl="mailto:datapmo@fiscal.treasury.gov" />
+                <NextStepsBox
+                    icon="download"
+                    sectionTitle="Agency Submission Files"
+                    iconClass="download-icon"
+                    sectionText="Access the data files submitted directly by agencies"
+                    linkText="Download Files"
+                    ariaLabel="Download Files"
+                    title="Download Files"
+                    linkUrl="" />
+                <NextStepsBox
+                    icon="next"
+                    sectionTitle="What’s New"
+                    iconClass="next-icon"
+                    sectionText="We're adding new pages, features, and fixes every two weeks"
+                    linkText="Read More"
+                    ariaLabel="Read More"
+                    title="Read More"
+                    linkUrl="" />
             </div>
         );
     }
