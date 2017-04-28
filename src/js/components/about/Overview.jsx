158208f7fc9212f9d6e51031cbb6319bb462dc55
Marco Mendoza <marco@poplicus.com>
2017-04-28 13:09:19 -0400

    Replace teaser text with “more coming soon”

--- src/js/components/about/Overview.jsx
+++ src/js/components/about/Overview.jsx
@@ -16,31 +16,15 @@ export default class Overview extends React.Component {
                 </div>
                 <div className="overview-inner-wrap-left">
                     <h5>Source of Data</h5>
-                    <p>
-                        The U.S Treasury Department brings together data from hundreds of federal
-                        agencies and systems to make federal spending data readily available to the
-                        public. Read more about how the data arrives in our system.
-                    </p>
-                    <button
-                        className="usa-button-disabled"
-                        title="View the Policy"
-                        aria-label="View the Policy">
-                        Coming Soon
-                    </button>
+                    <span className="overview-coming-soon-text">
+                        More coming soon
+                    </span>
                 </div>
                 <div className="overview-inner-wrap-right">
                     <h5>Relevant Legislature</h5>
-                    <p>
-                        Two major pieces of legislation (FFATA and the DATA Act) have paved the way
-                        for more accessible, reliable, and transparent federal spending data. Read
-                        more about these landmark legislations and their impact.
-                    </p>
-                    <button
-                        className="usa-button-disabled"
-                        title="View the Relevant Legislature"
-                        aria-label="View the Relevant Legislature">
-                        Coming Soon
-                    </button>
+                    <span className="overview-coming-soon-text">
+                        More coming soon
+                    </span>
                 </div>
             </div>
         );
