abb1ac75a927e2207753cc06f2c12fb9eec06b22
Rickey An <an_richard@bah.com>
2017-04-28 10:47:24 -0400

    Update about data section in about us page

--- src/js/components/about/AboutData.jsx
+++ src/js/components/about/AboutData.jsx
@@ -10,13 +10,27 @@ export default class AboutData extends React.Component {
 
     render() {
         return (
-            <div className="aboutdata-wrap">
-                <div className="img-placeholder" />
-                <div className="aboutdata-inner-wrap">
-                    <h3>About the Data</h3>
-                    <hr className="results-divider" />
-                    <p>USAspending.gov hosts an abundance of data that can help you understand the nuances of government spending. This data can answer the who, what, where, why, and how of the complex government spending landscape. Read on for more background on the data you&#8217;ll find on this site.</p>
-                    <a href="#/aboutdata" role="button" title="About the Data" aria-label="About the Data">Not enough for you? Click here for more discussion of the data</a>
+            <div className="aboutdata-outer-wrap">
+                <div className="aboutdata-wrap">
+                    <div className="image-wrap">
+                        <img src="img/aboutdata-graphic@2x.png" alt="About the Data" />
+                    </div>
+                    <div className="aboutdata-inner-wrap">
+                        <h3>About the Data</h3>
+                        <hr className="results-divider" />
+                        <p>USAspending.gov hosts an abundance of data that can help you understand the
+                            nuances of government spending. This data can answer the who, what, where, 
+                            why, and how of the complex government spending landscape. Read on for more
+                            background on the data you&#8217;ll find on this site.
+                        </p>
+                        <a 
+                            href="#/aboutdata" 
+                            role="button" 
+                            title="About the Data" 
+                            aria-label="About the Data">
+                            Not enough for you? Click here for more discussion of the data
+                        </a>
+                    </div>
                 </div>
             </div>
         );
