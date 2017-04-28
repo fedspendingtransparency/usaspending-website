99178cc879efde12e4ce485d48f433741ba4527e
Elizabeth Salita <dabbs_elizabeth@bah.com>
2017-04-28 10:32:14 -0400

    Added reason for modification column

--- src/js/dataMapping/contracts/transactionTable.js
+++ src/js/dataMapping/contracts/transactionTable.js
@@ -5,15 +5,17 @@
 
 const tableSearchFields = {
     columnWidths: {
-        modification: 230,
-        action_date: 200,
-        amount: 220,
-        description: 380
+        modification: 200,
+        action_date: 150,
+        amount: 200,
+        action_type_description: 300,
+        description: 180
     },
     defaultSortDirection: {
         modification: 'asc',
         action_date: 'desc',
         amount: 'desc',
+        reason: 'desc',
         description: 'asc'
     },
     table: {
@@ -21,17 +23,20 @@ const tableSearchFields = {
             'modification',
             'action_date',
             'amount',
+            'action_type_description',
             'description'
         ],
         _mapping: {
             modification: 'modification_number',
             action_date: 'action_date',
             amount: 'federal_action_obligation',
+            action_type_description: 'action_type_description',
             description: 'description'
         },
         modification: 'Modification Number',
         action_date: 'Action Date',
         amount: 'Amount',
+        action_type_description: 'Reason for Modification',
         description: 'Description'
     }
 };
