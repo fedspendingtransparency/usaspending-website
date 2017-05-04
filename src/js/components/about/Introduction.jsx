dd09d72ca1ce3de13eb9dac03a185446dde44490
Rickey An <an_richard@bah.com>
2017-05-04 16:22:25 -0400

    Update about us page

--- src/js/components/about/Introduction.jsx
+++ src/js/components/about/Introduction.jsx
@@ -13,17 +13,17 @@ export default class Introduction extends React.Component {
                 <div className="intro-inner-wrap">
                     <h3>
                         Beta.USAspending.gov is the new official source of accessible, searchable
-                        and reliable spending data for the U.S. Government. 
+                        and reliable spending data for the U.S. Government.
                     </h3>
                     <p>
                         Treasury released this new version of the USAspending.gov site in accordance
                         with the Digital Accountability and Transparency Act (DATA Act) requirements.
-                        The "Beta" site will run concurrently with the previous version of the
-                        USAspending.gov website over the summer to minimize disruptions to users&#39;
-                        data access and provide more time to add user-centered enhancements.  The
-                        new Beta.USAspending.gov site tracks agency expenditures and for the first
-                        time, links relevant agency expenditure data with awards distributed by the
-                        government. 
+                        The &#8220;Beta&#8221; site will run concurrently with the previous version
+                        of the USAspending.gov website over the summer to minimize disruptions to
+                        users&#39; data access and provide more time to add user-centered
+                        enhancements. The new Beta.USAspending.gov site tracks agency expenditures
+                        and for the first time, links relevant agency expenditure data with awards
+                        distributed by the government.
                     </p>
                 </div>
             </div>
