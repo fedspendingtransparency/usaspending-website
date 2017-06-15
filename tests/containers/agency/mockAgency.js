f8caf2e2fd143772f2d0bb1c6f808387211da47b
Kevin Li <li_kevin@bah.com>
2017-06-15 10:33:27 -0400

    Adding tests

--- tests/containers/agency/mockAgency.js
+++ tests/containers/agency/mockAgency.js
@@ -2,7 +2,7 @@ import AgencyOverviewModel from 'models/agency/AgencyOverviewModel';
 
 export const mockApi = {
     results: {
-        agency_name: "Department of Justice",
+        agency_name: "Department of Test",
         active_fy: "2017"
     }
 };
@@ -12,7 +12,8 @@ export const mockRedux = {
         id: '123',
         overview: new AgencyOverviewModel({
             id: '123',
-            name: 'Department of Test'
+            name: 'Department of Test',
+            activeFY: '2017'
         })
     },
     params: {
