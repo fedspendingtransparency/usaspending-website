918c3e22683806da9ebe2ce22a35b6a662ddd347
Kevin Li <li_kevin@bah.com>
2017-06-15 16:48:29 -0400

    Handle invalid agencies

--- src/js/components/agency/AgencyError.jsx
+++ src/js/components/agency/AgencyError.jsx
@@ -5,12 +5,14 @@
 
 import React from 'react';
 
+import Error from 'components/sharedComponents/Error';
+
 export default class AgencyError extends React.Component {
     render() {
         return (
-            <div>
-                Error
-            </div>
+            <Error
+                title="Invalid Agency"
+                message="The agency ID provided is invalid. Please check the ID and try again." />
         );
     }
 }
