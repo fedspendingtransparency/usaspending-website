cd372769285f1793894c35339c1aabf3e697e636
Emily Gullo <gullo_emily@bah.com>
2017-06-27 15:47:42 -0400

    Adding logic for show more/less

--- src/js/components/search/filters/contractFilters/ContractFilter.jsx
+++ src/js/components/search/filters/contractFilters/ContractFilter.jsx
@@ -4,6 +4,7 @@
  */
 
 import React from 'react';
+import * as Icons from 'components/sharedComponents/icons/Icons';
 import * as ContractFieldDefinitions from 'dataMapping/search/contractFields';
 
 import PrimaryCheckboxType from 'components/sharedComponents/checkbox/PrimaryCheckboxType';
@@ -15,37 +16,103 @@ const propTypes = {
     contractFilterState: React.PropTypes.string
 };
 
+const defaultShown = 10;
+
+const defaultState = {
+    shown: defaultShown,
+    shownType: 'more'
+};
+
 export default class ContractFilter extends React.Component {
 
     constructor(props) {
         super(props);
 
+        this.state = defaultState;
+
         // Bind functions
         this.toggleValue = this.toggleValue.bind(this);
+        this.generateContractFilters = this.generateContractFilters.bind(this);
+        this.toggleShownAmount = this.toggleShownAmount.bind(this);
+    }
+
+    toggleShownAmount() {
+        const contractFilters = ContractFieldDefinitions[this.props.contractFilterOptions];
+
+        let updatedState = defaultState;
+
+        if (this.state.shownType === 'more') {
+            updatedState = {
+                shown: Object.keys(contractFilters).length,
+                shownType: 'fewer'
+            };
+        }
+
+        this.setState(updatedState);
     }
 
     toggleValue(value) {
         this.props.toggleFilter(value);
     }
 
+    generateContractFilters(filters) {
+        const contractFilters = [];
+
+        if (contractFilters.length < this.state.shown) {
+            Object.keys(filters).forEach((key) => {
+                if (contractFilters.length <= this.state.shown
+                    && (filters.name !== null && filters.name !== '')) {
+                    const label = ContractFieldDefinitions[this.props.contractFilterOptions][key];
+                    contractFilters.push(
+                        <PrimaryCheckboxType
+                            {...this.props}
+                            key={key}
+                            id={`${this.props.contractFilterOptions}-${key}`}
+                            name={label}
+                            value={key}
+                            types={ContractFieldDefinitions[this.props.contractFilterOptions]}
+                            code={key}
+                            filterType={this.props.contractFilterType}
+                            selectedCheckboxes={this.props[this.props.contractFilterState]}
+                            toggleCheckboxType={this.toggleValue}
+                            enableAnalytics />);
+                }
+            });
+        }
+
+        return contractFilters;
+    }
+
+    generateToggleButton() {
+        const contractFilters = ContractFieldDefinitions[this.props.contractFilterOptions];
+        let toggleButton = null;
+
+        if (contractFilters && Object.keys(contractFilters).length > 10) {
+            const remaining = Object.keys(contractFilters).length - this.state.shown;
+            let shownStatement = `${remaining} ${this.state.shownType}`;
+            let arrow = (<Icons.AngleDown alt={`See ${shownStatement}`} />);
+
+            if (remaining === 0) {
+                shownStatement = this.state.shownType;
+                arrow = (<Icons.AngleUp alt={`See ${shownStatement}`} />);
+            }
+
+            toggleButton = (<button
+                className="see-more contract-filter-toggle-button"
+                onClick={this.toggleShownAmount}
+                title={`See ${shownStatement}`}>
+                See {shownStatement}
+                &nbsp; {arrow}
+            </button>);
+        }
+
+        return toggleButton;
+    }
+
     render() {
-        const contractFilterItems = [];
-        Object.keys(ContractFieldDefinitions[this.props.contractFilterOptions]).forEach((key) => {
-            const label = ContractFieldDefinitions[this.props.contractFilterOptions][key];
-            contractFilterItems.push(
-                <PrimaryCheckboxType
-                    {...this.props}
-                    key={key}
-                    id={`${this.props.contractFilterOptions}-${key}`}
-                    name={label}
-                    value={key}
-                    types={ContractFieldDefinitions[this.props.contractFilterOptions]}
-                    code={key}
-                    filterType={this.props.contractFilterType}
-                    selectedCheckboxes={this.props[this.props.contractFilterState]}
-                    toggleCheckboxType={this.toggleValue}
-                    enableAnalytics />);
-        });
+        const contractFilterItems =
+        this.generateContractFilters(ContractFieldDefinitions[this.props.contractFilterOptions]);
+        const toggleButton = this.generateToggleButton();
 
         return (
             <div
@@ -58,6 +125,7 @@ export default class ContractFilter extends React.Component {
                     <ul className="contract-types checkbox-types">
                         {contractFilterItems}
                     </ul>
+                    {toggleButton}
                 </div>
             </div>
         );
