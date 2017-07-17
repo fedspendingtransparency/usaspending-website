7d37b5af089c150bd5ea7799ed392351a0c08fc6
Michael Bray <bray_michael@bah.com>
2017-07-16 20:44:01 -0400

    Fixing Recipient visualization tests

--- tests/containers/search/visualizations/mockVisualizations.js
+++ tests/containers/search/visualizations/mockVisualizations.js
@@ -1,3 +1,28 @@
+export const geo = {
+    results: [
+        {
+            item: 'AK',
+            aggregate: '123.12'
+        },
+        {
+            item: 'AL',
+            aggregate: '345.56'
+        }
+    ],
+    total_metadata: {
+        count: 2
+    },
+    page_metadata: {
+        page_number: 1,
+        count: 2,
+        num_pages: 1
+    }
+};
+
+export const awardingAgency = {
+
+};
+
 export const spendingByCategory = {
     page_metadata: {
         page: 1,
@@ -18,25 +43,12 @@ export const spendingByCategory = {
     ]
 };
 
-export const geo = {
-    results: [
-        {
-            item: 'AK',
-            aggregate: '123.12'
-        },
-        {
-            item: 'AL',
-            aggregate: '345.56'
-        }
-    ],
-    total_metadata: {
-        count: 2
-    },
-    page_metadata: {
-        page_number: 1,
-        count: 2,
-        num_pages: 1
-    }
+export const cfda = {
+
+};
+
+export const fundingAgency = {
+
 };
 
 export const industryCode = {
@@ -59,4 +71,26 @@ export const industryCode = {
             contract_data__product_or_service_code: '2345'
         }
     ]
-}
+};
+
+export const recipient = {
+    page_metadata: {
+        page: 1,
+        has_next_page: false,
+        has_previous_page: false,
+        next: null,
+        previous: null
+    },
+    results: [
+        {
+            recipient__legal_entity_id: '1',
+            recipient__recipient_name: 'Multiple Recipients',
+            aggregate: '149620471458.92'
+        },
+        {
+            recipient__legal_entity_id: '113704139',
+            recipient__recipient_name: 'Michigan',
+            aggregate: '6684225478.00'
+        }
+    ]
+};
--- tests/containers/search/visualizations/rank/mocks/recipientHelper.js
+++ tests/containers/search/visualizations/rank/mocks/recipientHelper.js
@@ -1,3 +1,15 @@
-/**
- * Created by michaelbray on 7/16/17.
- */
+import { recipient } from '../../mockVisualizations';
+
+// Fetch Transactions Total for Geo
+export const performTransactionsTotalSearch = () => (
+    {
+        promise: new Promise((resolve) => {
+            process.nextTick(() => {
+                resolve({
+                    data: recipient
+                });
+            });
+        }),
+        cancel: jest.fn()
+    }
+);
