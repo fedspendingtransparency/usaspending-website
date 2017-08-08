f6e20805de42f191ac5974ea8da25963caea7952
Michael Bray <bray_michael@bah.com>
2017-08-08 16:59:59 -0400

    Moving keys into their own file

---
+++ src/js/dataMapping/search/awardsOperationKeys.js
@@ -0,0 +1,43 @@
+/**
+ * awardsOperationKeys.js
+ * Created by michaelbray on 8/8/17.
+ */
+
+export const rootKeys = {
+    keyword: 'keyword',
+    timePeriod: 'time_period',
+    awardType: 'award_type_codes',
+    agencies: 'agencies',
+    recipients: 'legal_entities',
+    recipientLocationScope: 'recipient_scope',
+    recipientLocation: 'recipient_location',
+    recipientType: 'recipient_type_names',
+    placeOfPerformanceScope: 'place_of_performance_scope',
+    placeOfPerformance: 'place_of_performance_location',
+    awardAmount: 'award_amount',
+    awardID: 'award_ids',
+    cfda: 'program_numbers',
+    naics: 'naics_codes',
+    psc: 'psc_codes',
+    contractPricing: 'contract_pricing_type_codes',
+    setAsideType: 'set_aside_type_codes',
+    extentCompeted: 'extent_competed_type_codes'
+};
+
+export const timePeriodKeys = {
+    type: 'type',
+    value: 'value',
+    startDate: 'start_date',
+    endDate: 'end_date'
+};
+
+export const agencyKeys = {
+    type: 'type',
+    tier: 'tier',
+    name: 'name'
+};
+
+export const awardAmountKeys = {
+    min: 'lower_bound',
+    max: 'upper_bound'
+};
--- src/js/models/search/SearchAwardsOperation.js
+++ src/js/models/search/SearchAwardsOperation.js
@@ -3,44 +3,8 @@
  * Created by michaelbray on 8/7/17.
  */
 
-const rootKeys = {
-    keyword: 'keyword',
-    timePeriod: 'time_period',
-    awardType: 'award_type_codes',
-    agencies: 'agencies',
-    recipients: 'legal_entities',
-    recipientLocationScope: 'recipient_scope',
-    recipientLocation: 'recipient_location',
-    recipientType: 'recipient_type_names',
-    placeOfPerformanceScope: 'place_of_performance_scope',
-    placeOfPerformance: 'place_of_performance_location',
-    awardAmount: 'award_amount',
-    awardID: 'award_ids',
-    cfda: 'program_numbers',
-    naics: 'naics_codes',
-    psc: 'psc_codes',
-    contractPricing: 'contract_pricing_type_codes',
-    setAsideType: 'set_aside_type_codes',
-    extentCompeted: 'extent_competed_type_codes'
-};
-
-const timePeriodKeys = {
-    type: 'type',
-    value: 'value',
-    startDate: 'start_date',
-    endDate: 'end_date'
-};
-
-const agencyKeys = {
-    type: 'type',
-    tier: 'tier',
-    name: 'name'
-};
-
-const awardAmountKeys = {
-    min: 'lower_bound',
-    max: 'upper_bound'
-};
+import { rootKeys, timePeriodKeys, agencyKeys, awardAmountKeys }
+    from 'dataMapping/search/awardsOperationKeys';
 
 class SearchAwardsOperation {
     constructor() {
