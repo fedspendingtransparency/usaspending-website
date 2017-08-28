aa7c7a29d219e807f92800b8f1d3380e937dcc62
Michael Bray <bray_michael@bah.com>
2017-08-28 11:10:52 -0400

    Additional helper comment

--- src/js/helpers/monthHelper.js
+++ src/js/helpers/monthHelper.js
@@ -8,6 +8,7 @@ import moment from "moment";
 // Months are 1-indexed strings, starting with the first month in the fiscal year, which is October
 // We parse the month, using 1 by default if the month is not a number
 // To account for the Fiscal Year beginning in October, we subtract three months
+// As moment months are 0-indexed, we need to actually subtract four months
 const monthConversion = (m) =>
     moment().month(isNaN(parseInt(m, 10)) ? 1 : parseInt(m, 10)).subtract(4, "months");
 
