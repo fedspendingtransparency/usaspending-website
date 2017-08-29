ad551723ebe17a173d9ece8c0f374970991e9747
Elizabeth Salita <dabbs_elizabeth@bah.com>
2017-08-29 10:03:51 -0400

    Added a loading message when Detail Container is inFlight

--- src/js/containers/explorer/detail/DetailContentContainer.jsx
+++ src/js/containers/explorer/detail/DetailContentContainer.jsx
@@ -19,6 +19,7 @@ import * as ExplorerHelper from 'helpers/explorerHelper';
 
 import DetailContent from 'components/explorer/detail/DetailContent';
 import ExplorerSidebar from 'components/explorer/detail/sidebar/ExplorerSidebar';
+import DetailLoading from 'components/explorer/detail/DetailLoading';
 
 const propTypes = {
     explorer: PropTypes.object,
@@ -362,6 +363,27 @@ export class DetailContentContainer extends React.Component {
     }
 
     render() {
+        let content = (
+            <DetailContent
+                isRoot={this.props.explorer.active.within === 'root'}
+                isLoading={this.state.inFlight}
+                root={this.props.explorer.root}
+                fy={this.props.explorer.fy}
+                active={this.props.explorer.active}
+                trail={this.props.explorer.trail.toJS()}
+                total={this.props.explorer.active.total}
+                data={this.state.data}
+                lastUpdate={this.state.lastUpdate}
+                transitionSteps={this.state.transitionSteps}
+                transition={this.state.transition}
+                goDeeper={this.goDeeper}
+                changeSubdivisionType={this.changeSubdivisionType}
+                showTooltip={this.props.showTooltip}
+                hideTooltip={this.props.hideTooltip} />
+        );
+        if (this.state.inFlight) {
+            content = (<DetailLoading />);
+        }
         return (
             <div className="explorer-detail">
                 <ExplorerSidebar
@@ -369,22 +391,7 @@ export class DetailContentContainer extends React.Component {
                     trail={this.props.explorer.trail}
                     setExplorerYear={this.props.setExplorerYear}
                     rewindToFilter={this.rewindToFilter} />
-                <DetailContent
-                    isRoot={this.props.explorer.active.within === 'root'}
-                    isLoading={this.state.inFlight}
-                    root={this.props.explorer.root}
-                    fy={this.props.explorer.fy}
-                    active={this.props.explorer.active}
-                    trail={this.props.explorer.trail.toJS()}
-                    total={this.props.explorer.active.total}
-                    data={this.state.data}
-                    lastUpdate={this.state.lastUpdate}
-                    transitionSteps={this.state.transitionSteps}
-                    transition={this.state.transition}
-                    goDeeper={this.goDeeper}
-                    changeSubdivisionType={this.changeSubdivisionType}
-                    showTooltip={this.props.showTooltip}
-                    hideTooltip={this.props.hideTooltip} />
+                {content}
             </div>
         );
     }
