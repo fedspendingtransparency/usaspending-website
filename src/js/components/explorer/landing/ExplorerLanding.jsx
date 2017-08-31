b9f0c055348606562bca38d36f3fa07c152d3b1c
Elizabeth Salita <dabbs_elizabeth@bah.com>
2017-08-31 18:16:04 -0400

    Added glossary links to landing page

--- src/js/components/explorer/landing/ExplorerLanding.jsx
+++ src/js/components/explorer/landing/ExplorerLanding.jsx
@@ -27,17 +27,20 @@ const ExplorerLanding = () => (
                     icon="budget_function"
                     title="Budget Function"
                     description="See spending divided by a high level categorization based on purpose."
-                    url="#/explorer/budget_function" />
+                    url="#/explorer/budget_function"
+                    term="budget-function" />
                 <ExplorerLandingOption
                     icon="agency"
                     title="Agency"
                     description="See spending divided by all U.S. government agencies."
-                    url="#/explorer/agency" />
+                    url="#/explorer/agency"
+                    term="agency" />
                 <ExplorerLandingOption
                     icon="object_class"
                     title="Object Class"
                     description="See spending divided by a broad categorization based on what was purchased."
-                    url="#/explorer/object_class" />
+                    url="#/explorer/object_class"
+                    term="object-class" />
             </div>
         </div>
     </ExplorerWrapperPage>
--- src/js/components/explorer/landing/ExplorerLandingOption.jsx
+++ src/js/components/explorer/landing/ExplorerLandingOption.jsx
@@ -7,6 +7,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { icons } from 'dataMapping/explorer/dropdownScopes';
+import * as Icons from 'components/sharedComponents/icons/Icons';
 
 const propTypes = {
     icon: PropTypes.string,
@@ -23,7 +24,11 @@ const ExplorerLandingOption = (props) => {
             <div className="icon">
                 {icon}
             </div>
-            <h3>{props.title}</h3>
+            <h3>{props.title}
+                <a href={`#/explorer/?glossary=${props.term}`}>
+                    <Icons.Glossary />
+                </a>
+            </h3>
             <div className="description">
                 {props.description}
             </div>
