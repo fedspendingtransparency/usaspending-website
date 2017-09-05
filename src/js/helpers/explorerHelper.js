716a1075cb196c1e21ec51438cf17a91078055d7
Elizabeth Salita <dabbs_elizabeth@bah.com>
2017-09-05 09:18:10 -0400

    Removed mock API

--- src/js/helpers/explorerHelper.js
+++ src/js/helpers/explorerHelper.js
@@ -7,22 +7,9 @@ import Axios, { CancelToken } from 'axios';
 
 import kGlobalConstants from 'GlobalConstants';
 
-// import MockAPI from './mockExplorerApi';
-
 export const fetchBreakdown = (params) => {
     const source = CancelToken.source();
     return {
-        // promise: new Promise((resolve) => {
-        //    setTimeout(() => {
-        //        console.log(params);
-        //        const data = MockAPI.respondToRequest(params.type, params.filters);
-        //        console.log(data);
-        //
-        //        resolve({
-        //            data
-        //        });
-        //    }, 1500);
-        // }),
         promise: Axios.request({
             url: 'v2/spending/',
             baseURL: kGlobalConstants.API,
