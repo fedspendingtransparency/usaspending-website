d1fcecb7c949491d9ef16fd097e324fd0dab3a82
Elizabeth Salita <dabbs_elizabeth@bah.com>
2017-09-06 15:20:29 -0400

    Updated Obligated Container test

--- tests/containers/agency/visualizations/ObligatedContainer-test.jsx
+++ tests/containers/agency/visualizations/ObligatedContainer-test.jsx
@@ -8,6 +8,13 @@ import { mount } from 'enzyme';
 import sinon from 'sinon';
 
 import { ObligatedContainer } from 'containers/agency/visualizations/ObligatedContainer';
+import * as AgencyHelper from 'helpers/agencyHelper';
+
+import { mockObligatedAmounts } from './mocks/mockObligatedAmounts';
+
+// force Jest to use native Node promises
+// see: https://facebook.github.io/jest/docs/troubleshooting.html#unresolved-promises
+global.Promise = require.requireActual('promise');
 
 // spy on specific functions inside the component
 const loadDataSpy = sinon.spy(ObligatedContainer.prototype, 'loadData');
@@ -16,8 +23,6 @@ const inboundProps = {
     id: '246',
     activeFY: '2017'
 };
-
-jest.mock('helpers/agencyHelper', () => require('../../../containers/agency/agencyHelper'));
 
 // mock the child component by replacing it with a function that returns a null element
 jest.mock('components/agency/visualizations/obligated/ObligatedVisualization', () =>
@@ -31,20 +36,57 @@ jest.mock('containers/glossary/GlossaryButtonWrapperContainer', () =>
 jest.mock('containers/glossary/GlossaryContainer', () =>
     jest.fn(() => null));
 
+const mockAgencyHelper = (functionName, event, expectedResponse) => {
+    jest.useFakeTimers();
+    // override the specified function
+    AgencyHelper[functionName] = jest.fn(() => {
+        // Axios normally returns a promise, replicate this, but return the expected result
+        const networkCall = new Promise((resolve, reject) => {
+            process.nextTick(() => {
+                if (event === 'resolve') {
+                    resolve({
+                        data: expectedResponse
+                    });
+                }
+                else {
+                    reject({
+                        data: expectedResponse
+                    });
+                }
+            });
+        });
+
+        return {
+            promise: networkCall,
+            cancel: jest.fn()
+        };
+    });
+};
+
+const unmockAgencyHelper = () => {
+    jest.useRealTimers();
+    jest.unmock('helpers/agencyHelper');
+};
+
 describe('ObligatedContainer', () => {
-    it('should make an API call for the selected agency obligated amounts on mount', async () => {
-        const container = mount(<ObligatedContainer
+    it('should make an API call for the selected agency obligated amounts on mount', () => {
+        mockAgencyHelper('fetchAgencyObligatedAmounts', 'resolve', mockObligatedAmounts);
+
+        mount(<ObligatedContainer
             {...inboundProps} />);
 
-        await container.instance().searchRequest.promise;
+        jest.runAllTicks();
 
         expect(loadDataSpy.callCount).toEqual(1);
-
         loadDataSpy.reset();
+
+        unmockAgencyHelper();
     });
 
     it('should make a new API call for obligated amounts when the inbound agency ID prop' +
         ' changes', () => {
+        mockAgencyHelper('fetchAgencyObligatedAmounts', 'resolve', mockObligatedAmounts);
+
         const container = mount(<ObligatedContainer
             {...inboundProps} />);
 
@@ -56,8 +98,26 @@ describe('ObligatedContainer', () => {
             id: '555'
         });
 
+        jest.runAllTicks();
+
         expect(loadDataMock).toHaveBeenCalledWith('555', inboundProps.activeFY);
         loadDataSpy.reset();
-        loadDataSpy.reset();
+
+        unmockAgencyHelper();
+    });
+
+    it('should correctly set the state values', async () => {
+        mockAgencyHelper('fetchAgencyObligatedAmounts', 'resolve', mockObligatedAmounts);
+
+        const container = mount(<ObligatedContainer
+            {...inboundProps} />);
+
+        await container.instance().searchRequest.promise;
+
+        expect(container.instance().state.obligatedAmount).toEqual(17839104086.11);
+        expect(container.instance().state.budgetAuthority).toEqual(46564455029.68);
+        expect(container.instance().state.outlay).toEqual(19368986358.21);
+
+        unmockAgencyHelper();
     });
 });
