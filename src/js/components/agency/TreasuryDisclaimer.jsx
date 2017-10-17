445cf732d07d67401853bc39db73f9c2cffcbbcf
Kevin Li <li_kevin@bah.com>
2017-10-17 09:38:09 -0400

    Shortened text

--- src/js/components/agency/TreasuryDisclaimer.jsx
+++ src/js/components/agency/TreasuryDisclaimer.jsx
@@ -7,7 +7,7 @@ import React from 'react';
 
 const TreasuryDisclaimer = () => (
     <div className="treasury-disclaimer">
-        <span className="disclaimer">Note:</span> Budgetary resources and obligations displayed on this page may not agree with the Department of the Treasury&apos;s Statement of Budgetary Resources (SBR) in the Agency Financial Report (AFR) due to certain presentation adjustments that reflect the Department&apos;s programmatic mission and component budgetary account activity.  For example, amounts available for interest payments on the Public Debt and most tax refunds appear as resources and obligations of Treasury, but due to their nature as either means of financing budget deficits or as offsets to the receipts side of the budget, respectively, they are not presented in Treasury&apos;s SBR.
+        <span className="disclaimer">Note:</span> The numbers on this page might differ from the Statement of Budgetary Resources (SBR) in the Agency Financial Report (AFR) due to certain presentation adjustments that reflect the Department&apos;s programmatic mission and component budgetary account activity.
     </div>
 );
 
