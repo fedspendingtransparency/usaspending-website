c8d7d278dd7a5a2e721b9a4fce08174bf1b6bdb5
Kevin Li <li_kevin@bah.com>
2017-10-17 09:45:26 -0400

    Remove space

--- src/js/components/agency/TreasuryDisclaimer.jsx
+++ src/js/components/agency/TreasuryDisclaimer.jsx
@@ -7,7 +7,7 @@ import React from 'react';
 
 const TreasuryDisclaimer = () => (
     <div className="treasury-disclaimer">
-        <span className="disclaimer">Note:</span> The numbers on this page might differ from the Statement of Budgetary Resources (SBR) in the Agency Financial Report (AFR) due to certain presentation adjustments that reflect the Department&apos;s programmatic mission and component budgetary account activity. 
+        <span className="disclaimer">Note:</span> The numbers on this page might differ from the Statement of Budgetary Resources (SBR) in the Agency Financial Report (AFR) due to certain presentation adjustments that reflect the Department&apos;s programmatic mission and component budgetary account activity.
     </div>
 );
 
