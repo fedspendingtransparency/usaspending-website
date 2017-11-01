2ff12d34e219a4a591b64885e0169e52b7c55ab1
Michael Bray <bray_michael@bah.com>
2017-11-01 08:52:59 -0400

    Surfacing PSC description in the PSC autocomplete in the autocomplete results, selection, and top filter bar

--- src/js/components/search/filters/psc/SelectedPSC.jsx
+++ src/js/components/search/filters/psc/SelectedPSC.jsx
@@ -5,7 +5,6 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import _ from 'lodash';
 
 import ShownValue from 'components/search/filters/otherFilters/ShownValue';
 
@@ -17,11 +16,14 @@ const propTypes = {
 export default class SelectedPSC extends React.Component {
     render() {
         const shownPSC = [];
+
         this.props.selectedPSC.entrySeq().forEach((entry) => {
-            const psc = entry[1].product_or_service_code;
+            const pscCode = entry[1].product_or_service_code;
+            const pscDescription = entry[1].psc_description;
+
             const value = (<ShownValue
-                label={_.toString(psc)}
-                key={psc}
+                label={`${pscCode} - ${pscDescription}`}
+                key={pscCode}
                 removeValue={this.props.removePSC.bind(null, entry[1])} />);
             shownPSC.push(value);
         });
