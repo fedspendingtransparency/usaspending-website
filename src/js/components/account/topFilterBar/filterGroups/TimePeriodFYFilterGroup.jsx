baac962251e9e82cd778378d96a7127cac9c5323
Kevin Li <li_kevin@bah.com>
2017-12-26 11:41:02 -0500

    All federal account top filter groups use legacy top filter bar

--- src/js/components/account/topFilterBar/filterGroups/TimePeriodFYFilterGroup.jsx
+++ src/js/components/account/topFilterBar/filterGroups/TimePeriodFYFilterGroup.jsx
@@ -8,7 +8,7 @@ import PropTypes from 'prop-types';
 
 import * as FiscalYearHelper from 'helpers/fiscalYearHelper';
 
-import BaseTopFilterGroup from 'components/search/topFilterBar/filterGroups/BaseTopFilterGroup';
+import LegacyBaseTopFilterGroup from './LegacyBaseTopFilterGroup';
 
 const propTypes = {
     filter: PropTypes.object,
@@ -86,7 +86,7 @@ export default class TimePeriodFYFilterGroup extends React.Component {
     render() {
         const tags = this.generateTags();
 
-        return (<BaseTopFilterGroup
+        return (<LegacyBaseTopFilterGroup
             tags={tags}
             filter={this.props.filter}
             clearFilterGroup={this.clearGroup} />);
