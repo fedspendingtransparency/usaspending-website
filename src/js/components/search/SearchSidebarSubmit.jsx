fd9501922d839e975e0052d979a51d1aee43d3ec
Kevin Li <li_kevin@bah.com>
2017-12-28 15:29:42 -0500

    Cleaning up linter errors

--- src/js/components/search/SearchSidebarSubmit.jsx
+++ src/js/components/search/SearchSidebarSubmit.jsx
@@ -4,26 +4,34 @@
  */
 
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const SearchSidebarSubmit = (props) => {
-    return (
-        <div className="sidebar-submit">
-            <button
-                className="submit-button"
-                title="Add or update a filter to submit."
-                aria-label="Add or update a filter to submit."
-                disabled={!props.requestsComplete || !props.filtersChanged}
-                onClick={props.applyStagedFilters}>
-                Submit Search
-            </button>
-            <button
-                className="reset-button"
-                disabled={!props.requestsComplete}
-                onClick={props.resetStagedFilters}>
-                Reset filters
-            </button>
-        </div>
-    );
+const propTypes = {
+    requestsComplete: PropTypes.bool,
+    filtersChanged: PropTypes.bool,
+    applyStagedFilters: PropTypes.func,
+    resetStagedFilters: PropTypes.func
 };
+
+const SearchSidebarSubmit = (props) => (
+    <div className="sidebar-submit">
+        <button
+            className="submit-button"
+            title="Add or update a filter to submit."
+            aria-label="Add or update a filter to submit."
+            disabled={!props.requestsComplete || !props.filtersChanged}
+            onClick={props.applyStagedFilters}>
+            Submit Search
+        </button>
+        <button
+            className="reset-button"
+            disabled={!props.requestsComplete}
+            onClick={props.resetStagedFilters}>
+            Reset filters
+        </button>
+    </div>
+);
+
+SearchSidebarSubmit.propTypes = propTypes;
 
 export default SearchSidebarSubmit;
