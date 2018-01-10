4f7172925016b5c7d3563911dc9c690f69f7e748
Minahm Kim <mdkim105@gmail.com>
2018-01-10 11:10:50 -0500

    adding padding to keep data from being blocked

--- src/js/components/sharedComponents/IBTable/components/Table.jsx
+++ src/js/components/sharedComponents/IBTable/components/Table.jsx
@@ -160,7 +160,7 @@ export default class Table extends React.Component {
             // the internal height is less than the external height, this means that some of the
             // internal height height is being blocked by a horizontal scrollbar (which takes up
             // vertical space)
-            scrollbarHeight = containerExternalHeight - containerInternalHeight;
+            scrollbarHeight = containerExternalHeight - containerInternalHeight + 40;
         }
 
         return scrollbarHeight;
@@ -168,9 +168,10 @@ export default class Table extends React.Component {
 
     render() {
         const visibleWidth = Math.min(this.props.bodyWidth, this.props.contentWidth);
-        const visibleHeight = Math.min(this.props.bodyHeight,
-            this.props.rowCount * this.props.rowHeight);
-
+        // On single row increate the row height so that windows horizontal scroll bar
+        // does not block the data
+        const visibleHeight = this.props.rowCount != 1 ? Math.min(this.props.bodyHeight,
+            this.props.rowCount * this.props.rowHeight) : this.props.rowHeight + 2;
         const style = {
             minWidth: visibleWidth,
             maxWidth: visibleWidth,
