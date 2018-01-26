64a724f4e96fb5c338b5d722275ed308c6064436
Kevin Li <li_kevin@bah.com>
2018-01-26 16:34:26 -0500

    Rebuilding site navigation

--- src/js/components/sharedComponents/header/NavBar.jsx
+++ src/js/components/sharedComponents/header/NavBar.jsx
@@ -69,77 +69,79 @@ export default class NavBar extends React.Component {
 
         return (
             <nav
-                className="nav-container"
+                className="site-navigation"
                 aria-label="Site navigation">
-                <div className="logo">
-                    <div className={`usa-logo ${betaClass}`} id="logo">
-                        <a href="#/" title="USAspending.gov Home" aria-label="USAspending.gov Home">
-                            <span className="logo-sr">USAspending.gov</span>
-                        </a>
-                    </div>
-                </div>
-                <div className="mobile-menu">
-                    <div className="mobile-button-wrapper">
-                        <button
-                            className="usa-menu-btn"
-                            onClick={this.toggleMobileNav}>
-                            <span className="nav-lines" />
-                        </button>
-                    </div>
-                </div>
-                <div className="mobile-nav-animations">
-                    <CSSTransitionGroup
-                        transitionName="mobile-nav-slide"
-                        transitionLeaveTimeout={195}
-                        transitionEnterTimeout={225}
-                        transitionLeave>
-                        {mobileNav}
-                    </CSSTransitionGroup>
-                </div>
-                <div className="primary-menu">
-                    <ul
-                        className="nav-menu"
-                        role="menu">
-                        <li
-                            className="menu-item"
-                            role="menuitem">
-                            <a
-                                className="usa-nav-link"
-                                href="#/explorer"
-                                title="Spending Explorer: Navigate the levels of government spending from top to bottom">
-                                <span>Spending Explorer</span>
+                <div className="site-navigation__wrapper">
+                    <div className="logo">
+                        <div className={`usa-logo ${betaClass}`} id="logo">
+                            <a href="#/" title="USAspending.gov Home" aria-label="USAspending.gov Home">
+                                <span className="logo-sr">USAspending.gov</span>
                             </a>
-                        </li>
-                        <li
-                            className="menu-item"
-                            role="menuitem">
-                            <Dropdown
-                                title="Award Search: Search through awards and discover trends and connections"
-                                label="Award Search"
-                                items={searchOptions} />
-                        </li>
-                        <li
-                            className="menu-item"
-                            role="menuitem">
-                            <Dropdown
-                                title="Profiles: Learn more about organizations and accounts"
-                                label="Profiles"
-                                items={profileOptions} />
-                        </li>
-                        <li
-                            className="menu-item"
-                            role="menuitem">
-                            <Dropdown
-                                title="Bulk Download"
-                                label="Bulk Download"
-                                items={downloadOptions} />
-                        </li>
-                        <li
-                            className="menu-item"
-                            role="menuitem">
-                            <GlossaryButtonWrapperContainer child={NavBarGlossaryLink} />
-                        </li>
-                    </ul>
+                        </div>
+                    </div>
+                    <div className="mobile-menu">
+                        <div className="mobile-button-wrapper">
+                            <button
+                                className="usa-menu-btn"
+                                onClick={this.toggleMobileNav}>
+                                <span className="nav-lines" />
+                            </button>
+                        </div>
+                    </div>
+                    <div className="mobile-nav-animations">
+                        <CSSTransitionGroup
+                            transitionName="mobile-nav-slide"
+                            transitionLeaveTimeout={195}
+                            transitionEnterTimeout={225}
+                            transitionLeave>
+                            {mobileNav}
+                        </CSSTransitionGroup>
+                    </div>
+                    <div className="primary-menu">
+                        <ul
+                            className="nav-menu"
+                            role="menu">
+                            <li
+                                className="menu-item"
+                                role="menuitem">
+                                <a
+                                    className="usa-nav-link"
+                                    href="#/explorer"
+                                    title="Spending Explorer: Navigate the levels of government spending from top to bottom">
+                                    <span>Spending Explorer</span>
+                                </a>
+                            </li>
+                            <li
+                                className="menu-item"
+                                role="menuitem">
+                                <Dropdown
+                                    title="Award Search: Search through awards and discover trends and connections"
+                                    label="Award Search"
+                                    items={searchOptions} />
+                            </li>
+                            <li
+                                className="menu-item"
+                                role="menuitem">
+                                <Dropdown
+                                    title="Profiles: Learn more about organizations and accounts"
+                                    label="Profiles"
+                                    items={profileOptions} />
+                            </li>
+                            <li
+                                className="menu-item"
+                                role="menuitem">
+                                <Dropdown
+                                    title="Bulk Download"
+                                    label="Bulk Download"
+                                    items={downloadOptions} />
+                            </li>
+                            <li
+                                className="menu-item"
+                                role="menuitem">
+                                <GlossaryButtonWrapperContainer child={NavBarGlossaryLink} />
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </nav>
         );
