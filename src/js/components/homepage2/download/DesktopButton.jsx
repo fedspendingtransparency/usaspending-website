289a10cd40142c110e32991470526c65d06370fd
Kevin Li <li_kevin@bah.com>
2018-01-29 09:41:11 -0500

    Keyboard events take priority over mouse events when navigating the download section

--- src/js/components/homepage2/download/DesktopButton.jsx
+++ src/js/components/homepage2/download/DesktopButton.jsx
@@ -20,72 +20,39 @@ export default class DesktopButton extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {
-            isHovering: false,
-            isFocused: false
-        };
-
         this.onHover = this.onHover.bind(this);
         this.onFocus = this.onFocus.bind(this);
-        this.endHover = this.endHover.bind(this);
-        this.endFocus = this.endFocus.bind(this);
         this.pressedEsc = this.pressedEsc.bind(this);
     }
 
     onHover() {
         this.props.changeActiveItem(this.props.index);
-        this.setState({
-            isHovering: true
-        });
     }
 
     onFocus() {
-        this.setState({
-            isFocused: true
-        });
         this.onHover();
     }
 
-    endHover() {
-        this.setState({
-            isHovering: false
-        });
-    }
-
-    endFocus() {
-        this.setState({
-            isFocused: false
-        });
-        this.endHover();
-    }
-
     pressedEsc(e) {
-        if (!this.state.isHovering || !this.props.active) {
+        if (!this.props.active) {
             // not selected
-            return;
-        }
-        else if (!this.isFocused) {
-            // only respond while focused
             return;
         }
 
         if (e.key === 'Escape') {
-            this.endHover();
             this.props.forceClear();
         }
     }
 
     render() {
-        const iconActive = this.props.active || this.state.isHovering ? '-active' : '';
-        const buttonActive = this.props.active || this.state.isHovering ? 'desktop-download__active' : '';
+        const iconActive = this.props.active ? '-active' : '';
+        const buttonActive = this.props.active ? 'desktop-download__active' : '';
         return (
             <button
                 className={`homepage-download__button desktop-download ${buttonActive}`}
                 disabled={!this.props.enabled}
                 onMouseOver={this.onHover}
-                onMouseLeave={this.endHover}
-                onFocus={this.onFocus}
-                onBlur={this.endFocus}
+                onFocus={this.onHover}
                 onKeyDown={this.pressedEsc}>
                 <div className={`homepage-download__icon homepage-download__icon_type_${this.props.code}${iconActive}`} />
                 <div className="homepage-download__button-label">
