89558bcdf6544741013984a61c4c836020e3e64b
Lizzie Salita <salita_elizabeth@bah.com>
2018-02-01 15:51:31 -0500

    Update landing page headers to thinner style

--- src/js/components/accountLanding/header/AccountLandingHeader.jsx
+++ src/js/components/accountLanding/header/AccountLandingHeader.jsx
@@ -8,11 +8,17 @@ import React from 'react';
 export default class AccountLandingHeader extends React.Component {
     render() {
         return (
-            <div className="page-title-bar">
-                <div className="page-title-bar-wrap">
-                    <h1 className="page-title">
-                        Federal Account Profiles
-                    </h1>
+            <div className="search-header-wrapper">
+                <div className="search-header-container">
+                    <div
+                        className="search-header"
+                        aria-labelledby="main-focus">
+                        <div className="search-title">
+                            <h1 tabIndex={-1} id="main-focus">
+                                Federal Account Profiles
+                            </h1>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
--- src/js/components/agencyLanding/header/AgencyLandingHeader.jsx
+++ src/js/components/agencyLanding/header/AgencyLandingHeader.jsx
@@ -8,11 +8,17 @@ import React from 'react';
 export default class AgencyLandingHeader extends React.Component {
     render() {
         return (
-            <div className="page-title-bar">
-                <div className="page-title-bar-wrap">
-                    <h1 className="page-title">
-                        Agency Profiles
-                    </h1>
+            <div className="search-header-wrapper">
+                <div className="search-header-container">
+                    <div
+                        className="search-header"
+                        aria-labelledby="main-focus">
+                        <div className="search-title">
+                            <h1 tabIndex={-1} id="main-focus">
+                                Agency Profiles
+                            </h1>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
