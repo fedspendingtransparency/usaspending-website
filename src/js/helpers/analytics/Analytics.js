8e4b0c97e2831ed51c16e723484014fad455ca2a
Kevin Li <li_kevin@bah.com>
2018-02-02 16:53:36 -0500

    Update analytics obj

--- src/js/helpers/analytics/Analytics.js
+++ src/js/helpers/analytics/Analytics.js
@@ -1,26 +1,39 @@
+/**
+ * Analytics.js
+ * Created by Kevin Li 2/2/18
+ */
+
 
 const Analytics = {
-    _lib(...args) {
-        if (window.gas && typeof window.gas === 'function') {
+    _prefix: 'USAspending - ',
+    _execute(...args) {
+        if (this.isDAP) {
             return window.gas(...args);
         }
-        else if (window.ga && typeof window.gas === 'function') {
+        else if (this.GA) {
             return window.ga(...args);
         }
+        // fall back if no library is loaded (most likely due to adblocking)
         return null;
+    },
+    get isDAP() {
+        return Boolean(window.gas && typeof window.gas === 'function');
+    },
+    get isGA() {
+        return Boolean(!this.isDAP && window.ga && typeof window.ga === 'function');
     },
     event(args) {
         if (!args.category || !args.action) {
             return;
         }
-        this._lib(
+        this._execute(
             'send',
             'event',
-            args.category,
+            `${this._prefix}${args.category}`,
             args.action,
             args.label || undefined,
             args.value || undefined,
-            args.noninteraction || undefined
+            args.nonInteraction || undefined
         );
     },
     pageview(args) {
@@ -29,7 +42,7 @@ const Analytics = {
         if (typeof args === 'object') {
             ({ path, title } = args);
         }
-        this._lib(
+        this._execute(
             'send',
             'pageview',
             path,
@@ -37,5 +50,8 @@ const Analytics = {
         );
     }
 };
+
+// no hack approaches allowed
+Object.freeze(Analytics);
 
 export default Analytics;
