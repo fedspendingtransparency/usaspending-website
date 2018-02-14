60944f169d4636735423628e42447bd7f0e1d4b1
Lizzie Salita <salita_elizabeth@bah.com>
2018-02-14 11:28:34 -0500

    Added handle url tests

--- tests/containers/keyword/keywordHelper.js
+++ tests/containers/keyword/keywordHelper.js
@@ -38,3 +38,14 @@ export const performTabCountSearch = () => (
         cancel: jest.fn()
     }
 );
+
+export const slugFromString = (string) =>
+    string.toString().toLowerCase().trim()
+        .replace(/\s+/g, "-") // Replace spaces with -
+        // .replace(/[^\w-]+/g, "") // Remove non word or hyphen characters
+        .replace(/--+/g, "-") // Replace multiple - with single -
+        .replace(/^-+/, "") // Don't use hyphens at the beginning or end of the slug
+        .replace(/-+$/, "");
+
+export const stringFromSlug = (slug) =>
+    slug.replace(/[-._~]+|([a-z])([A-Z])/g, ' ');
