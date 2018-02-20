3a855c05c6587c35ae6d7dab5f34eff9e961d3a3
Lizzie Salita <salita_elizabeth@bah.com>
2018-02-20 14:54:06 -0500

    Updated info banner text; converted styling to BEM

--- src/js/components/sharedComponents/header/InfoBanner.jsx
+++ src/js/components/sharedComponents/header/InfoBanner.jsx
@@ -8,32 +8,49 @@ const propTypes = {
 
 export default class InfoBanner extends React.Component {
     render() {
+        // TODO - Lizzie: add FAQ url
         return (
-            <div className="warning-banner-wrap info">
-                <div className="warning-banner">
-                    <div className="top-alert-icon">
-                        <i className="usa-da-icon"><InfoCircle alt="Information" /></i>
+            <div className="warning-banner">
+                <div className="warning-banner__content">
+                    <div className="warning-banner__alert-icon">
+                        <i className="usa-da-icon">
+                            <InfoCircle alt="Information" />
+                        </i>
                     </div>
-                    <div className="top-alert-text">
-                        <p>
-                            <span className="good-news">We&apos;ve updated USAspending.gov!</span>&nbsp;
-                            You can still access an archive of the old site&nbsp;
-                            <a
-                                href="https://legacy.usaspending.gov"
-                                target="_blank"
-                                rel="noopener noreferrer">
-                                here
-                            </a>.
-                        </p>
+                    <div className="warning-banner__alert-text">
+                        Welcome to the new&nbsp;
+                        <a href="#/">
+                            USAspending.gov
+                        </a>!
+                        We offer agency financial data along with awards data. Questions? Check out our&nbsp;
+                        <a
+                            href=""
+                            target="_blank"
+                            rel="noopener noreferrer">
+                            FAQ
+                        </a>.
+                        Want to suggest a feature? Join the conversation on&nbsp;
+                        <a href="https://usaspending-help.zendesk.com/hc/en-us/community/topics"
+                           target="_blank"
+                           rel="noopener noreferrer">
+                            our community page
+                        </a>.
+                        Visit the old site at&nbsp;
+                        <a
+                            href="https://legacy.usaspending.gov"
+                            target="_blank"
+                            rel="noopener noreferrer">
+                            legacy.usaspending.gov
+                        </a>.
                     </div>
+                    <button
+                        className="warning-banner__close-button"
+                        title="Dismiss message"
+                        aria-label="Dismiss message"
+                        onClick={this.props.closeBanner}>
+                        <Close alt="Dismiss message" />
+                    </button>
                 </div>
-                <button
-                    className="close"
-                    title="Dismiss message"
-                    aria-label="Dismiss message"
-                    onClick={this.props.closeBanner}>
-                    <Close alt="Dismiss message" />
-                </button>
             </div>
         );
     }
--- src/js/components/sharedComponents/header/WarningBanner.jsx
+++ src/js/components/sharedComponents/header/WarningBanner.jsx
@@ -9,32 +9,32 @@ const propTypes = {
 export default class WarningBanner extends React.Component {
     render() {
         return (
-            <div className="warning-banner-wrap">
-                <div className="warning-banner">
-                    <div className="top-alert-icon">
-                        <i className="usa-da-icon"><Icons.ExclamationTriangle /></i>
+            <div className="warning-banner">
+                <div className="warning-banner__content">
+                    <div className="warning-banner__alert-icon">
+                        <i className="usa-da-icon">
+                            <Icons.ExclamationTriangle />
+                        </i>
                     </div>
-                    <div className="top-alert-text">
-                        <p>
-                            This site is in beta. To view the production site, visit&nbsp;
-                            <a
-                                href="https://www.usaspending.gov"
-                                target="_blank"
-                                rel="noopener noreferrer">
-                                USAspending.gov
-                            </a>.
-                            We&#39;re updating this site on a rolling basis and we&#39;d love your feedback!
-                            Share your thoughts&nbsp;
-                            <a
-                                href="https://usaspending-help.zendesk.com/hc/en-us/community/topics"
-                                target="_blank"
-                                rel="noopener noreferrer">
-                                here
-                            </a>.
-                        </p>
+                    <div className="warning-banner__alert-text">
+                        This site is in beta. To view the production site, visit&nbsp;
+                        <a
+                            href="https://www.usaspending.gov"
+                            target="_blank"
+                            rel="noopener noreferrer">
+                            USAspending.gov
+                        </a>.
+                        We&#39;re updating this site on a rolling basis and we&#39;d love your feedback!
+                        Share your thoughts&nbsp;
+                        <a
+                            href="https://usaspending-help.zendesk.com/hc/en-us/community/topics"
+                            target="_blank"
+                            rel="noopener noreferrer">
+                            here
+                        </a>.
                     </div>
                     <button
-                        className="close"
+                        className="warning-banner__close-button"
                         title="Dismiss message"
                         aria-label="Dismiss message"
                         onClick={this.props.closeBanner}>
