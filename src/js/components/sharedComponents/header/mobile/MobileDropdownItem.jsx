f69e9c4cc69e439529d458a9f28f8ff00e154011
Lizzie Salita <salita_elizabeth@bah.com>
2018-02-26 13:10:59 -0500

    Handle the mobile Database Snapshots link; sass fixes

--- src/js/components/sharedComponents/header/mobile/MobileDropdownItem.jsx
+++ src/js/components/sharedComponents/header/mobile/MobileDropdownItem.jsx
@@ -6,6 +6,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import * as redirectHelper from 'helpers/redirectHelper';
+
 import DropdownComingSoon from '../DropdownComingSoon';
 
 const propTypes = {
@@ -13,39 +15,67 @@ const propTypes = {
     comingSoon: PropTypes.bool,
     url: PropTypes.string,
     title: PropTypes.string,
-    hideMobileNav: PropTypes.func
+    hideMobileNav: PropTypes.func,
+    externalLink: PropTypes.bool
 };
 
-const MobileDropdownItem = (props) => {
-    let activeClass = '';
-    if (props.active) {
-        activeClass = 'mobile-dropdown__link_active';
+export default class MobileDropdownItem extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.redirect = this.redirect.bind(this);
     }
 
-    let comingSoonClass = '';
-    let comingSoonDecorator = null;
-    if (props.comingSoon) {
-        comingSoonClass = 'mobile-dropdown__item_coming-soon';
-        comingSoonDecorator = (
-            <div className="mobile-dropdown__coming-soon">
-                <DropdownComingSoon />
-            </div>
+    redirect() {
+        redirectHelper.showRedirectModal(this.props.url);
+        this.props.hideMobileNav();
+    }
+
+    render() {
+        let activeClass = '';
+        if (this.props.active) {
+            activeClass = 'mobile-dropdown__link_active';
+        }
+
+        let comingSoonClass = '';
+        let comingSoonDecorator = null;
+        if (this.props.comingSoon) {
+            comingSoonClass = 'mobile-dropdown__item_coming-soon';
+            comingSoonDecorator = (
+                <div className="mobile-dropdown__coming-soon">
+                    <DropdownComingSoon />
+                </div>
+            );
+        }
+
+        let link = (
+            <li className={`mobile-dropdown__item ${comingSoonClass}`}>
+                <a
+                    href={this.props.url}
+                    className={`mobile-dropdown__link ${activeClass}`}
+                    onClick={this.props.hideMobileNav}>
+                    {this.props.title}
+                </a>
+                {comingSoonDecorator}
+            </li>
         );
-    }
 
-    return (
-        <li className={`mobile-dropdown__item ${comingSoonClass}`}>
-            <a
-                href={props.url}
-                className={`mobile-dropdown__link ${activeClass}`}
-                onClick={props.hideMobileNav}>
-                {props.title}
-            </a>
-            {comingSoonDecorator}
-        </li>
-    );
-};
+        if (this.props.externalLink) {
+            // Trigger the redirect modal
+            link = (
+                <li className={`mobile-dropdown__item ${comingSoonClass}`}>
+                    <button
+                        className={`mobile-dropdown__link ${activeClass}`}
+                        onClick={this.redirect}>
+                        {this.props.title}
+                    </button>
+                    {comingSoonDecorator}
+                </li>
+            );
+        }
+
+        return link;
+    }
+}
 
 MobileDropdownItem.propTypes = propTypes;
-
-export default MobileDropdownItem;
