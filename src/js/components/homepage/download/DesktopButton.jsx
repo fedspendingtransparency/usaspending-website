fb71cd63174309482c6e4d6c169f7555b40a7043
Michael Bray <bray_michael@bah.com>
2018-02-27 11:35:44 -0500

    Updates from design review

--- src/js/components/homepage/download/DesktopButton.jsx
+++ src/js/components/homepage/download/DesktopButton.jsx
@@ -5,11 +5,13 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
+import * as Icons from 'components/sharedComponents/icons/Icons';
 
 const propTypes = {
     code: PropTypes.string.isRequired,
     label: PropTypes.string.isRequired,
     index: PropTypes.number.isRequired,
+    enabled: PropTypes.bool,
     active: PropTypes.bool,
     changeActiveItem: PropTypes.func,
     forceClear: PropTypes.func
@@ -46,15 +48,28 @@ export default class DesktopButton extends React.Component {
     render() {
         const iconActive = this.props.active ? '-active' : '';
         const buttonActive = this.props.active ? 'desktop-download__active' : '';
+
+        const disabled = !this.props.enabled;
+        let comingSoon = null;
+        if (disabled) {
+            comingSoon = (
+                <div className="homepage-download__coming-soon">
+                    <Icons.InfoCircle />&nbsp;Coming Soon
+                </div>
+            );
+        }
+
         return (
             <button
                 className={`homepage-download__button desktop-download ${buttonActive}`}
+                disabled={disabled}
                 onMouseOver={this.onHover}
                 onFocus={this.onHover}
                 onKeyDown={this.pressedEsc}>
                 <div className={`homepage-download__icon homepage-download__icon_type_${this.props.code}${iconActive}`} />
                 <div className="homepage-download__button-label">
                     {this.props.label}
+                    {comingSoon}
                 </div>
             </button>
         );
