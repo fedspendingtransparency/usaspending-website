72551ef24efcb6335705f5b6b1780d334c0ed2f0
Michael Bray <bray_michael@bah.com>
2018-02-28 18:02:22 -0500

    Adds an information icon to the homepage, fixes the Header bar CSS for Firefox

--- src/js/components/homepage/hero/Hero.jsx
+++ src/js/components/homepage/hero/Hero.jsx
@@ -5,27 +5,68 @@
 
 import React from 'react';
 
+import * as Icons from 'components/sharedComponents/icons/Icons';
 import HeroButton from './HeroButton';
+import HeroTooltip from './HeroTooltip';
 
-const Hero = () => (
-    <section
-        className="homepage-hero"
-        aria-label="Introduction">
-        <div className="homepage-hero__wrapper">
-            <div className="homepage-hero__content">
-                <h1
-                    className="homepage-hero__headline"
-                    tabIndex={-1}>
-                    In 2017, the government spent <strong className="homepage-hero__headline homepage-hero__headline_weight_bold">$3.98 trillion.</strong>
-                </h1>
-                <hr className="homepage-hero__divider" />
-                <div className="homepage-hero__description">
-                    <strong className="homepage-hero__description homepage-hero__description_weight_bold">Curious to see how this money was spent?</strong> We hope so &mdash; we&apos;ve opened the conversation around federal spending and provide the tools to help you navigate the budget from top to bottom.
+export default class Hero extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            showInfoTooltip: false
+        };
+
+        this.showTooltip = this.showTooltip.bind(this);
+        this.closeTooltip = this.closeTooltip.bind(this);
+    }
+
+    showTooltip() {
+        this.setState({
+            showInfoTooltip: true
+        });
+    }
+
+    closeTooltip() {
+        this.setState({
+            showInfoTooltip: false
+        });
+    }
+
+    render() {
+        let tooltip = null;
+        if (this.state.showInfoTooltip) {
+            tooltip = (
+                <HeroTooltip
+                    closeTooltip={this.closeTooltip} />
+            );
+        }
+
+        return (
+            <section className="homepage-hero" aria-label="Introduction">
+                <div
+                    id="homepage-hero__wrapper"
+                    className="homepage-hero__wrapper">
+                    <div className="homepage-hero__content">
+                        <h1 className="homepage-hero__headline" tabIndex={-1}>
+                            In 2017, the government spent <strong className="homepage-hero__headline homepage-hero__headline_weight_bold">$3.98 trillion.</strong>
+                            <span
+                                id="homepage-hero__info_icon"
+                                className="homepage-hero__info_icon"
+                                onFocus={this.showTooltip}
+                                onMouseEnter={this.showTooltip}>
+                                <Icons.InfoCircle />
+                            </span>
+                        </h1>
+                        {tooltip}
+                        <hr className="homepage-hero__divider" />
+                        <div className="homepage-hero__description">
+                            <strong className="homepage-hero__description homepage-hero__description_weight_bold">Curious to see how this money was spent?</strong> We hope so &mdash; we&apos;ve opened the conversation around federal spending and provide the tools to help you navigate the budget from top to bottom.
+                        </div>
+                    </div>
+                    <HeroButton />
                 </div>
-            </div>
-            <HeroButton />
-        </div>
-    </section>
-);
-
-export default Hero;
+            </section>
+        );
+    }
+};
---
+++ src/js/components/homepage/hero/HeroTooltip.jsx
@@ -0,0 +1,72 @@
+/**
+ * HeroTooltip.jsx
+ * Created by michaelbray on 2/28/18.
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import * as Icons from 'components/sharedComponents/icons/Icons';
+
+const propTypes = {
+    closeTooltip: PropTypes.func
+};
+
+const tooltipWidth = 160;
+const margin = 15;
+
+export default class HeroTooltip extends React.Component {
+    getPosition() {
+        const container = document.getElementById('homepage-hero__wrapper');
+        const conatinerOffsetY = container.getBoundingClientRect().top;
+
+        const icon = document.getElementById('homepage-hero__info_icon');
+        const iconTop = icon.getBoundingClientRect().top - conatinerOffsetY;
+        let iconLeft = icon.getBoundingClientRect().left - 12;
+
+        const windowWidth = window.innerWidth;
+        if ((iconLeft + tooltipWidth) > windowWidth) {
+            iconLeft = windowWidth - tooltipWidth - margin;
+        }
+
+        return { iconTop, iconLeft };
+    }
+
+    render() {
+        const position = this.getPosition();
+
+        return (
+            <div
+                className="homepage-hero-tooltip"
+                style={{
+                    top: position.iconTop,
+                    left: position.iconLeft
+                }}>
+                <div className="homepage-hero-tooltip__info_icon">
+                    <Icons.InfoCircle />
+                </div>
+                <button
+                    className="homepage-hero-tooltip__close_icon"
+                    onClick={this.props.closeTooltip}>
+                    <Icons.Close />
+                </button>
+                <div className="homepage-hero-tooltip__text_holder">
+                    <div className="homepage-hero-tooltip__tooltip_title">
+                        Data Source
+                    </div>
+                    <div className="homepage-hero-tooltip__tooltip_text">
+                        Fiscal Year 2017 net outlays as reported on the&nbsp;
+                        <a
+                            href="https://www.fiscal.treasury.gov/fsreports/rpt/mthTreasStmt/current.htm"
+                            target="_blank"
+                            rel="noopener noreferrer">
+                            Monthly Treasury Statement
+                        </a>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+};
+
+HeroTooltip.propTypes = propTypes;
