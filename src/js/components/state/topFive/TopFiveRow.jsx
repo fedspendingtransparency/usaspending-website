a77d4fd075c2003d4a1ac3fe5a156f75b1463f68
dtrinh50 <trinh_david@bah.com>
2018-05-25 15:43:51 -0400

    adding in text changes

--- src/js/components/state/topFive/TopFiveRow.jsx
+++ src/js/components/state/topFive/TopFiveRow.jsx
@@ -16,19 +16,30 @@ const TopFiveRow = (props) => {
 
     const percent = isNaN(percentValue) ? '--' : `${Math.round(percentValue * 100) / 100}%`;
 
+    const originalName = props.data.name;
+    let name = "";
+    if (props.data.name.length > 30) {
+        name = `${props.data.name.substring(0, 28)}...`;
+    } else {
+        name = props.data.name;
+    }
+
     return (
         <tr
             className="category-table__table-row">
             <td
-                className="category-table__table-cell">
-                {props.data.name}
+                className="category-table__table-cell"
+                title={originalName}>
+                {name}
             </td>
             <td
-                className="category-table__table-cell category-table__table-cell_centered">
+                className="category-table__table-cell category-table__table-cell_centered"
+                title={props.data.amount}>
                 {props.data.amount}
             </td>
             <td
-                className="category-table__table-cell category-table__table-cell_centered">
+                className="category-table__table-cell category-table__table-cell_centered"
+                title={percent}>
                 {percent}
             </td>
         </tr>
