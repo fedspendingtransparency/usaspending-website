e25a5a0aed25ab76faf3ef03ef5dd4afcdfbf74a
dtrinh50 <trinh_david@bah.com>
2018-06-18 13:19:52 -0400

    minor edit

--- src/js/containers/bulkDownload/accounts/AccountDataContainer.jsx
+++ src/js/containers/bulkDownload/accounts/AccountDataContainer.jsx
@@ -59,7 +59,6 @@ export class AccountDataContainer extends React.Component {
 
         this.agencyListRequest.promise
             .then((res) => {
-                console.log(res.data.agencies);
                 const cfoAgencies = res.data.agencies.cfo_agencies;
                 const otherAgencies = res.data.agencies.other_agencies;
                 this.setState({
