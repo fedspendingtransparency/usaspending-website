6982af4fd2ead994b5cfc7d5338bf18149785da2
dtrinh50 <trinh_david@bah.com>
2018-07-20 16:36:29 -0400

    Additional language change

--- src/js/components/bulkDownload/accounts/filters/FiscalYearFilter.jsx
+++ src/js/components/bulkDownload/accounts/filters/FiscalYearFilter.jsx
@@ -64,7 +64,7 @@ export default class FiscalYearFilter extends React.Component {
                             pickedQuarter={this.pickedQuarter}
                             pickedYear={this.pickedYear} />
                     </div>
-                    <p className="download-filter__content-note"><span className="download-filter__content-note_bold">Note:</span> The data included in the Custom Account Download was first collected in the second quarter of fiscal year 2017. Financial data will not be available prior to that timeframe.</p>
+                    <p className="download-filter__content-note"><span className="download-filter__content-note_bold">Note:</span> The data included in the Custom Account Download was first collected in the second quarter of fiscal year 2017, per the Digital Accountability and Transparency Act of 2014 (DATA Act). Financial data will not be available prior to that timeframe.</p>
                     <p className="download-filter__content-note">The Account Balances and Account Breakdown by Program Activity & Object Class files contain cumulative financial balances at the account and agency levels, as of the end of the quarter selected. The Account Breakdown by Award file contains every transaction reported at the account and agency levels, for the fiscal year through the end of the quarter selected.</p>
                 </div>
             </div>
