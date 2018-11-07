2ab414578e51d8724c51c5801471983410fbeed3
dtrinh50 <trinh_david@bah.com>
2018-11-07 11:26:48 -0500

    Style updates

--- src/js/components/awardv2/contract/AwardRecipient.jsx
+++ src/js/components/awardv2/contract/AwardRecipient.jsx
@@ -16,15 +16,15 @@ export default class AwardRecipient extends React.Component {
         const award = this.props.selectedAward;
         return (
 
-            <div className="award__col award-overview">
-                <div className="award-overview__awarding">
-                    <span className="award-overview__awarding-title">Awarding Agency</span>
-                    <span className="award-overview__awarding-detail">{`${award.awardingAgency.toptierName} ${award.awardingAgency.toptierAbbr}`}</span> <br />
-                    <span><span className="award-overview__awarding-icon"><Icons.ArrowDown /></span><a className="award-overview__awarding-info" href="#">View additional information</a></span>
+            <div className="award__col award-recipient">
+                <div className="award-recipient__awarding">
+                    <span className="award-recipient__awarding-title">Awarding Agency</span>
+                    <span className="award-recipient__awarding-detail">{`${award.awardingAgency.toptierName} ${award.awardingAgency.toptierAbbr}`}</span> <br />
+                    <span><span className="award-recipient__awarding-icon"><Icons.ArrowDown /></span><a className="award-recipient__awarding-info" href="#">View additional information</a></span>
                 </div>
-                <div className="award-overview__recipient">
-                    <span className="award-overview__recipient-title">Recipient</span> <br />
-                    <span className="award-overview__recipient-detail"><a href={`/#/recipient/${award.recipient.internalId}`}>{award.recipient.name}</a></span>
+                <div className="award-recipient__recipient">
+                    <span className="award-recipient__recipient-title">Recipient</span> <br />
+                    <span className="award-recipient__recipient-detail"><a href={`/#/recipient/${award.recipient.internalId}`}>{award.recipient.name}</a></span>
                 </div>
             </div>
         );
