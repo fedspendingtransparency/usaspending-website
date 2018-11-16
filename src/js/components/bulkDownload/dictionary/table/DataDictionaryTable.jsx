92ef89207c4a885e1009ff1c12e6beeff9cb5f9b
dtrinh50 <trinh_david@bah.com>
2018-11-16 12:05:27 -0500

    Remove unused code that caused console error

--- src/js/components/bulkDownload/dictionary/table/DataDictionaryTable.jsx
+++ src/js/components/bulkDownload/dictionary/table/DataDictionaryTable.jsx
@@ -21,12 +21,6 @@ const propTypes = {
 };
 
 export default class DataDictionaryTable extends React.Component {
-    // constructor(props) {
-    //     super(props);
-
-    //     this.scrollRightTop = throttle(this.scrollRightTop.bind(this), 16);
-    //     this.scrollRightBottom = throttle(this.scrollRightBottom.bind(this), 16);
-    // }
     scrollRightTop(e) {
         const topBar = document.getElementById("topBar");
         const bottomBar = document.getElementById("bottomBar");
