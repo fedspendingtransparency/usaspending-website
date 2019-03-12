0546bb677fd9a38e1e90c363018375430c1b7bbe
Lizzie Salita <ebdabbs@users.noreply.github.com>
2019-03-12 14:46:24 -0400

    Feature idv tooltips (#1022)
    * Starting shared component for tooltip, added in some info circle
    
    * Added related awards tooltip
    
    * Added dates section tooltip
    
    * award amounts tooltip; updated styling
    
    * Description tooltip; added left side option
    
    * increased width of tooltip, ensure pointer doesn't obscure the icon

--- src/js/components/awardv2/visualizations/overview/RelatedAwards.jsx
+++ src/js/components/awardv2/visualizations/overview/RelatedAwards.jsx
@@ -6,6 +6,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import InfoTooltip from '../../idv/InfoTooltip';
+
 const propTypes = {
     overview: PropTypes.object
 };
@@ -23,9 +25,28 @@ export default class RelatedAwards extends React.Component {
             );
         }
         return (
-            <div className="related-awards">
+            <div
+                className="related-awards">
                 <div className="award-overview__title related-awards__title">
                     Related Awards
+                    <InfoTooltip>
+                        <div className="info-tooltip__title">
+                            Related Awards
+                        </div>
+                        <div className="info-tooltip__text">
+                            <p>
+                                Related Awards refers to two possible types of awards related to this IDV:
+                            </p>
+                            <ul>
+                                <li>
+                                    <strong>Parent Award</strong> – The parent award is an IDV award that this contract was made under.  Click on the link to view more information on this award&apos;s parent.
+                                </li>
+                                <li>
+                                    <strong>Parent Award Orders Under this IDV</strong> – This is a count of how many awards were made under this IDV.  Click on the link to see more information about all of those orders.
+                                </li>
+                            </ul>
+                        </div>
+                    </InfoTooltip>
                 </div>
                 <div className="related-awards__parent">
                     <div className="related-awards__label">
