d2e60381f5bf1f7b290432e80f3f21e1b6920e76
Lizzie Salita <salita_elizabeth@bah.com>
2019-03-12 17:50:53 -0400

    added award history tooltip

--- src/js/components/awardv2/idv/AwardHistory.jsx
+++ src/js/components/awardv2/idv/AwardHistory.jsx
@@ -8,6 +8,7 @@ import PropTypes from 'prop-types';
 
 import { AwardLoop } from 'components/sharedComponents/icons/Icons';
 import TablesSection from './TablesSection';
+import InfoTooltip from './InfoTooltip';
 
 const propTypes = {
     overview: PropTypes.object
@@ -32,12 +33,27 @@ export default class AwardHistory extends React.Component {
 
     render() {
         return (
-            <div className="award-history">
-                <div className="award-history__title">
-                    <span className="award-history__icon">
+            <div className="award-viz award-history">
+                <div className="award-viz__heading">
+                    <div className="award-viz__icon">
                         <AwardLoop alt="Award History" />
-                    </span>&nbsp;
-                    Award History for this IDV
+                    </div>
+                    <h3 className="award-viz__title">
+                        Award History for this IDV
+                    </h3>
+                    <InfoTooltip left>
+                        <div className="info-tooltip__title">
+                            Award History
+                        </div>
+                        <div className="info-tooltip__text">
+                            <p>
+                                <strong>Transaction History</strong> – This table contains historical changes made to this award, shown as individual modification records. This information is reported by the Awarding Agency&apos;s contracting office.
+                            </p>
+                            <p>
+                                <strong>Federal Account Funding</strong> – The data documenting the funding, or the actual transactions made my an agency to obligate money, of an award can be found in this table. This data comes from the Awarding Agency&apos;s financial accounting offices.
+                            </p>
+                        </div>
+                    </InfoTooltip>
                 </div>
                 <hr />
                 <TablesSection
