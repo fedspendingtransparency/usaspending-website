7c3ae449c6d1be3b75d0dd9acc036a614f3a4a64
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2019-04-10 10:04:21 -0400

    [react16] disabling new enyzme 3 feature for shallow()

--- __tests__/setup.js
+++ __tests__/setup.js
@@ -2,4 +2,5 @@
 import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
-configure({ adapter: new Adapter() });
+// In Enzyme 3, shallow now by default triggers life cycle methods :thumbs-up: but this ruins all our tests :thumbs-down:
+configure({ adapter: new Adapter(), disableLifecycleMethods: true });
