ca909938c35a1c48628e7260874672e76b22034a
Jonathan Hill <hill_jonathan@bah.com>
2019-05-01 09:10:32 -0400

    update tests

--- tests/models/awardsV2/FederalAccountSummary-test.js
+++ tests/models/awardsV2/FederalAccountSummary-test.js
@@ -28,40 +28,40 @@ describe('FederalAccountSummary Model', () => {
     describe('FederalAccountSummary Model federalAccountName', () => {
         // Test three scenarios
         // 1. name is < 34 chars
-        it('should have a field percent', () => {
+        it('should format federalAccountName < 36 characters properly', () => {
             account.account_title = 'Jimmy Timmy';
             federalAccountSummary.populate(account, 100);
             expect(federalAccountSummary.federalAccountName).toEqual('JIMMY TIMMY');
         });
         // 2. name is 34 chars
-        it('should have a field percent', () => {
-            account.account_title = 'JimmyTimmyJimmyTimmyJimmyTimmy1';
+        it('should format federalAccountName = 36 characters properly', () => {
+            account.account_title = 'JimmyTimmyJimmyTimmyJimmyTimmy111111';
             federalAccountSummary.populate(account, 100);
-            expect(federalAccountSummary.federalAccountName).toEqual('JIMMYTIMMYJIMMYTIMMYJIMMYTIMMY1');
+            expect(federalAccountSummary.federalAccountName).toEqual('JIMMYTIMMYJIMMYTIMMYJIMMYTIMMY111111');
         });
         // 3. name is > 34 chars
-        it('should have a field percent', () => {
+        it('should format federalAccountName > 36 characters properly', () => {
             account.account_title = 'JimmyTimmyJimmyTimmyJimmyTimmy1111JimmyTimmyJimmyTimmy';
             federalAccountSummary.populate(account, 100);
-            expect(federalAccountSummary.federalAccountName).toEqual('JIMMYTIMMYJIMMYTIMMYJIMMYTIMMY1...');
+            expect(federalAccountSummary.federalAccountName).toEqual('JIMMYTIMMYJIMMYTIMMYJIMMYTIMMY1111JI...');
         });
     });
     describe('FederalAccountSummary Model Percents', () => {
         // Test all three scenarios
         // 1. Normal Percent 50%
-        it('should have a field percent', () => {
+        it('should format a normal percent properly', () => {
             account.total_transaction_obligated_amount = 4000000;
             federalAccountSummary.populate(account, 5000000);
             expect(federalAccountSummary.percent).toEqual('80%');
         });
         // 2. Decimal Percent 0.04%
-        it('should have a field percent', () => {
+        it('should format a percent < 1 properly', () => {
             account.total_transaction_obligated_amount = 2000;
             federalAccountSummary.populate(account, 5000000);
             expect(federalAccountSummary.percent).toEqual('0.04%');
         });
         // 3. Decimal Percent `Less than 0.01%`
-        it('should have a field percent', () => {
+        it('should format a percent < 0.01 properly', () => {
             account.total_transaction_obligated_amount = 100;
             federalAccountSummary.populate(account, 5000000);
             expect(federalAccountSummary.percent).toEqual('Less than 0.01%');
