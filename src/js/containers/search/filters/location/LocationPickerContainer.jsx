94e4ca0a06d00436d014c72fbaa41eefcdcd4873
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2019-05-10 15:54:04 -0400

    [dev-2642] if state is not selected, autopopulate

--- src/js/containers/search/filters/location/LocationPickerContainer.jsx
+++ src/js/containers/search/filters/location/LocationPickerContainer.jsx
@@ -259,6 +259,9 @@ export default class LocationPickerContainer extends React.Component {
     }
 
     selectEntity(level, value) {
+        if (level === 'city' && this.state.state.code !== value.code) {
+            this.setState({ state: this.state.availableStates.find((state) => state.code === value.code) });
+        }
         this.setState({
             [level]: value
         });
@@ -404,8 +407,8 @@ export default class LocationPickerContainer extends React.Component {
     parseCities(results) {
         // Convert the array of strings to an array of objects for consistency
         const availableCities = results.map((city) => ({
-            name: city,
-            code: city
+            name: city.city_name,
+            code: city.state_code
         }));
         this.setState({
             availableCities
