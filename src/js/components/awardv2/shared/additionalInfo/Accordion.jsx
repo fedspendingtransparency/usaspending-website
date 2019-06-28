424317fa2e1fbc88eda63f1ff36b9db8bb5882bc
Lizzie Salita <ebdabbs@users.noreply.github.com>
2019-06-28 10:10:51 -0400

    IDV Additional Information updates (#1145)
    * Created a separate data mapping for IDVs
    
    * improved handling boolean values
    
    * Added tooltips to period of performance
    
    * Use the existing CSS tooltip
    
    * Updated the mapping for parent IDV agency name
    
    * Removed unused code
    
    * Fixed expand/collapse all
    
    * added a keydown event helper
    
    * switched to fontAwesome for info icons

--- src/js/components/awardv2/shared/additionalInfo/Accordion.jsx
+++ src/js/components/awardv2/shared/additionalInfo/Accordion.jsx
@@ -6,6 +6,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { createOnKeyDownHandler } from 'helpers/keyboardEventsHelper';
 
 const propTypes = {
     accordionName: PropTypes.string,
@@ -38,13 +39,14 @@ export default class Accordion extends React.Component {
     }
 
     render() {
+        const onKeyDownHandler = createOnKeyDownHandler(this.handleClick);
         return (
             <div className={this.state.open ? 'accordion accordion_open' : 'accordion'}>
                 <div
                     className="accordion__bar"
                     tabIndex={0}
                     role="button"
-                    onKeyPress={this.handleClick}
+                    onKeyDown={onKeyDownHandler}
                     onClick={this.handleClick}>
                     <span>
                         <FontAwesomeIcon size="lg" icon={this.props.accordionIcon} />
