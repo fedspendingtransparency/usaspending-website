8be0c30fd36a85dd4b3b44e5038243f5da88a0cd
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2019-06-28 15:25:49 -0400

    [dev-2788] using existing mock data

--- tests/models/awardsV2/BaseParentAwardDetails-test.js
+++ tests/models/awardsV2/BaseParentAwardDetails-test.js
@@ -4,21 +4,13 @@
  */
 
 import parentAwardDetails from "../../../src/js/models/v2/awardsV2/BaseParentAwardDetails";
-
-const mockData = {
-    award_id: 'test_award_id',
-    idv_type_description: 'test_idv_type_description',
-    type_of_idc_description: 'test_',
-    agency_id: 'test_agency_id',
-    agency_name: 'test_agency_name',
-    multiple_or_single_aw_desc: 'test_multiple_or_single_aw_desc',
-    piid: 'test_piid'
-};
+import { mockIdv } from "./mockAwardApi";
 
 describe('parentAwardDetails', () => {
+    const mockData = mockIdv.parent_award;
     it('successfully populates the object with the expected shape and value', () => {
         const result = Object.create(parentAwardDetails);
-        result.populateCore(mockData);
+        result.populateCore(mockIdv.parent_award);
         expect(result.awardId).toEqual(mockData.award_id);
         expect(result.idvType).toEqual(mockData.idv_type_description);
         expect(result.idcType).toEqual(mockData.type_of_idc_description);
