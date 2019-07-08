2af16bac46aebafe3b0b8530b90c1cfcde307a65
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2019-07-08 17:52:54 -0400

    [dev-3004] globalConstants file for the public

--- src/js/GlobalConstants.js
+++ src/js/GlobalConstants.js
@@ -1,16 +1,45 @@
 /**
  * GlobalConstants.js
- * Created by Kevin Li 6/20/17
+ * Created by Maxwell Kendall 7/8/19
  */
 
 // use the correct GlobalConstants file based on the current environment
 
-let isDev = true;
-if (process.env.NODE_ENV && process.env.NODE_ENV === 'production') {
-    isDev = false;
-}
+// Production ENV values
+const prod = {
+    // PROD
+    API: 'https://api.usaspending.gov/api/',
+    LOCAL_ROOT: "",
+    GITHUB: "",
+    GA_TRACKING_ID: "UA-92617810-1",
+    LOCAL: false,
+    DEV: true,
+    IN_BETA: false,
+    KEYWORD_AVAILABLE: true,
+    PERF_LOG: false,
+    MAPBOX_TOKEN:
+    "pk.eyJ1IjoidXNhc3BlbmRpbmciLCJhIjoiY2l6ZnZjcmh0MDBtbDMybWt6NDR4cjR6ZSJ9.zsCqjJgrMDOA-i1RcCvGvg"
+};
 
-const kGlobalConstants = isDev ?
-    require('../../GlobalConstants_dev').default : require('../../GlobalConstants_prod').default;
+// Development ENV values
+const dev = {
+    // LOCAL
+    API: "http://localhost:8000/api/",
+    LOCAL_ROOT: "",
+    GITHUB: "",
+    GA_TRACKING_ID: "UA-92617810-1",
+    LOCAL: false,
+    DEV: true,
+    IN_BETA: false,
+    KEYWORD_AVAILABLE: true,
+    PERF_LOG: false,
+    MAPBOX_TOKEN:
+    "pk.eyJ1IjoidXNhc3BlbmRpbmciLCJhIjoiY2l6ZnZjcmh0MDBtbDMybWt6NDR4cjR6ZSJ9.zsCqjJgrMDOA-i1RcCvGvg"
+};
 
-export default kGlobalConstants;
+const globalConstants = {
+    development: dev,
+    production: prod
+};
+
+export default globalConstants[process.env.NODE_ENV];
