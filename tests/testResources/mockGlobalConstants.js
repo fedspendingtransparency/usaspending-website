8e8d59519d87f62cd7ebf16c31fb34c3c4ed467e
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2019-07-19 12:44:32 -0400

    [dev-3004] adding mock for globalConstants

---
+++ tests/testResources/mockGlobalConstants.js
@@ -0,0 +1,16 @@
+export default {
+    // LOCAL
+    API: "http://localhost:8000/api/",
+    LOCAL_ROOT: "",
+    GITHUB: "",
+    GA_TRACKING_ID: "UA-92617810-1",
+    LOCAL: false,
+    DEV: true,
+    IN_BETA: false,
+    KEYWORD_AVAILABLE: true,
+    PERF_LOG: false,
+    OVERRIDE_FISCAL_YEAR: false,
+    FISCAL_YEAR: 2017,
+    MAPBOX_TOKEN:
+    "pk.eyJ1IjoidXNhc3BlbmRpbmciLCJhIjoiY2l6ZnZjcmh0MDBtbDMybWt6NDR4cjR6ZSJ9.zsCqjJgrMDOA-i1RcCvGvg"
+};
