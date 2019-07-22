35a0bddb6a62d6e1f16fe550d0a1729baee6aa08
Lizzie Salita <salita_elizabeth@bah.com>
2019-07-22 17:05:39 -0400

    simplified by combining container and component

--- src/js/containers/search/filters/programSource/ProgramSourceAutocompleteContainer.jsx
+++ src/js/containers/search/filters/programSource/ProgramSourceAutocompleteContainer.jsx
@@ -6,10 +6,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { isCancel } from 'axios';
-import { pickBy } from 'lodash';
+import { pickBy, debounce } from 'lodash';
 
 import * as ProgramSourceHelper from 'helpers/programSourceHelper';
-import SourceSelectFilter from 'components/search/filters/programSource/SourceSelectFilter';
+import Autocomplete from 'components/sharedComponents/autocomplete/Autocomplete';
 
 const propTypes = {
     component: PropTypes.object,
@@ -23,19 +23,24 @@ export default class ProgramSourceAutocompleteContainer extends React.Component 
         super(props);
 
         this.state = {
-            inFlight: false,
-            error: false,
-            results: []
+            noResults: false,
+            searchString: '',
+            autocompleteOptions: []
         };
 
         this.autocompleteRequest = null;
         this.fetchAutocompleteResults = this.fetchAutocompleteResults.bind(this);
+        this.queryAutocompleteDebounced = debounce(this.fetchAutocompleteResults, 300);
+        this.handleTextInput = this.handleTextInput.bind(this);
+        this.clearAutocompleteSuggestions = this.clearAutocompleteSuggestions.bind(this);
+        this.selectSourceComponent = this.selectSourceComponent.bind(this);
     }
 
     componentWillUnmount() {
         if (this.autocompleteRequest) {
             this.autocompleteRequest.cancel();
         }
+        this.queryAutocompleteDebounced.cancel();
     }
 
     fetchAutocompleteResults(input) {
@@ -45,8 +50,7 @@ export default class ProgramSourceAutocompleteContainer extends React.Component 
         }
 
         this.setState({
-            inFlight: true,
-            error: false
+            noResults: false
         });
 
         // Create the filter object from current selections and the input value
@@ -72,8 +76,7 @@ export default class ProgramSourceAutocompleteContainer extends React.Component 
                 this.autocompleteRequest = null;
                 if (!isCancel(err)) {
                     this.setState({
-                        inFlight: false,
-                        error: true
+                        noResults: true
                     });
                     console.log(err);
                 }
@@ -86,29 +89,65 @@ export default class ProgramSourceAutocompleteContainer extends React.Component 
             parsedResults = results.map((agency) => {
                 const abbreviation = agency.agency_abbreviation ? ` (${agency.agency_abbreviation})` : '';
                 return ({
-                    code: agency.aid,
-                    name: `${agency.agency_name}${abbreviation}`
+                    title: agency.aid,
+                    subtitle: `${agency.agency_name}${abbreviation}`,
+                    data: { code: agency.aid }
                 });
             });
         }
         else {
-            parsedResults = results.map((option) => ({ code: option }));
+            parsedResults = results.map((option) => ({
+                title: option,
+                subtitle: '',
+                data: { code: option }
+            }));
         }
         this.setState({
-            inFlight: false,
-            results: parsedResults
+            autocompleteOptions: parsedResults,
+            noResults: parsedResults.length === 0
         });
     }
 
+    selectSourceComponent(selectedSource) {
+        this.props.updateComponent(this.props.component.code, selectedSource.code);
+    }
+
+    clearAutocompleteSuggestions() {
+        this.setState({
+            autocompleteOptions: []
+        });
+    }
+
+    handleTextInput(event) {
+        event.persist();
+        this.queryAutocompleteDebounced(event.target.value);
+    }
+
     render() {
+        const requiredIndicator = this.props.component.required ? (<span className="program-source-select-filter__label-required">Required</span>) : '';
         return (
-            <SourceSelectFilter
-                {...this.props.component}
-                options={this.state.results}
-                selectedSources={this.props.selectedSources}
-                updateComponent={this.props.updateComponent}
-                fetchAutocompleteResults={this.fetchAutocompleteResults}
-                dirtyFilters={this.props.dirtyFilters} />
+            <div className="program-source-select-filter">
+                <label className="program-source-select-filter__label">
+                    {`${this.props.component.label} (${this.props.component.code.toUpperCase()})`}
+                    {requiredIndicator}
+                </label>
+                <Autocomplete
+                    values={this.state.autocompleteOptions}
+                    handleTextInput={this.handleTextInput}
+                    onSelect={this.selectSourceComponent}
+                    retainValue
+                    dirtyFilters={this.props.dirtyFilters}
+                    minCharsToSearch={1}
+                    placeholder={`Enter ${this.props.component.code.toUpperCase()} value (${this.props.component.characterLimit} characters)`}
+                    errorHeader={`Unknown ${this.props.component.code.toUpperCase()}`}
+                    errorMessage={`We were unable to find that ${this.props.component.label}`}
+                    ref={(input) => {
+                        this.programSourceList = input;
+                    }}
+                    clearAutocompleteSuggestions={this.clearAutocompleteSuggestions}
+                    noResults={this.state.noResults}
+                    characterLimit={this.props.component.characterLimit} />
+            </div>
         );
     }
 }
