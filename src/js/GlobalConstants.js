15f5a174aed364fa671f2660a0dc904f0e72830b
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2019-08-09 16:02:56 -0400

    [dev-3004] updating globalConstants file

--- src/js/GlobalConstants.js
+++ src/js/GlobalConstants.js
@@ -4,18 +4,17 @@
  */
 
 const globalConstants = {
-    API: process.env.API_URL,
+    API: process.env.APIL,
     LOCAL_ROOT: "",
     GITHUB: "",
-    GA_TRACKING_ID: "UA-92617810-1",
+    GA_TRACKING_ID: process.env.GA_TRACKING_ID,
     LOCAL: false,
     DEV: (process.env.IS_DEV === 'true'),
     KEYWORD_AVAILABLE: true,
     PERF_LOG: false,
     OVERRIDE_FISCAL_YEAR: false,
     FISCAL_YEAR: 2017,
-    MAPBOX_TOKEN:
-    "pk.eyJ1IjoidXNhc3BlbmRpbmciLCJhIjoiY2l6ZnZjcmh0MDBtbDMybWt6NDR4cjR6ZSJ9.zsCqjJgrMDOA-i1RcCvGvg"
+    MAPBOX_TOKEN: process.env.MAPBOX_TOKEN
 };
 
 export default globalConstants;
