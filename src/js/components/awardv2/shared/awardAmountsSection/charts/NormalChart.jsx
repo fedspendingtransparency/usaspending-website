a27a3e4b9814146d2ba49bdc02bd207e6c9d33c7
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2019-08-20 12:12:56 -0400

    [dev-1564] fixing import path

--- src/js/components/awardv2/shared/awardAmountsSection/charts/NormalChart.jsx
+++ src/js/components/awardv2/shared/awardAmountsSection/charts/NormalChart.jsx
@@ -5,8 +5,8 @@
 
 import React from 'react';
 import { generatePercentage } from 'helpers/aggregatedAmountsHelper';
-import TooltipWrapper from "../../../sharedComponents/TooltipWrapper";
-import { AWARD_V2_AGGREGATED_AMOUNTS_PROPS, TOOLTIP_PROPS } from '../../../../propTypes/index';
+import TooltipWrapper from "../../../../sharedComponents/TooltipWrapper";
+import { AWARD_V2_AGGREGATED_AMOUNTS_PROPS, TOOLTIP_PROPS } from '../../../../../propTypes/index';
 
 const propTypes = {
     awardAmounts: AWARD_V2_AGGREGATED_AMOUNTS_PROPS,
