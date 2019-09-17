922a66dfc22785d01d65ce6083c616672f983b00
Jonathan Hill <hill_jonathan@bah.com>
2019-09-17 09:44:35 -0400

    remove old code

--- src/js/helpers/awardAmountHelper.js
+++ src/js/helpers/awardAmountHelper.js
@@ -11,9 +11,6 @@ export const formatAwardAmountRange = (range, options = 2) => {
     const minLabel = formatMoneyWithPrecision(range[0], options);
     const maxLabel = formatMoneyWithPrecision(range[1], options);
     let label = `${minLabel} - ${maxLabel}`;
-    // if (range[0] === 0 && range[1] === 0) {
-    //     label = `$0 - $0`;
-    // }
     if (!range[0] && (range[0] !== 0)) {
         label = `Under ${maxLabel}`;
     }
