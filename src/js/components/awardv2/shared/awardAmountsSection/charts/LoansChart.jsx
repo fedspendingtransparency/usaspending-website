41668f85188e619e8896aa36332aa3b325da906e
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2019-09-17 17:08:37 -0400

    [DEV-2992] using hook

--- src/js/components/awardv2/shared/awardAmountsSection/charts/LoansChart.jsx
+++ src/js/components/awardv2/shared/awardAmountsSection/charts/LoansChart.jsx
@@ -7,8 +7,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { generatePercentage } from 'helpers/aggregatedAmountsHelper';
+
+import { useTooltips } from "./AwardAmountsChart";
 import TooltipWrapper from "../../../../sharedComponents/TooltipWrapper";
 import { AWARD_V2_AGGREGATED_AMOUNTS_PROPS, TOOLTIP_PROPS } from '../../../../../propTypes/index';
+import { SubsidyTooltip, FaceValueTooltip } from "../Tooltips";
 
 const propTypes = {
     awardType: PropTypes.string,
@@ -17,81 +20,110 @@ const propTypes = {
     faceValueTooltipProps: TOOLTIP_PROPS
 };
 
-export default class LoansChart extends React.Component {
-    render() {
-        // Rename properties to improve readability of the calculations
-        const subsidy = this.props.awardAmounts._subsidy;
-        const faceValue = this.props.awardAmounts._faceValue;
-        const isSubsidyZero = (subsidy === 0);
+const tooltipPropsBySpendingCategory = {
+    subsidy: {
+        offsetAdjustments: { top: 0 },
+        tooltipComponent: <SubsidyTooltip />
+    },
+    faceValue: {
+        offsetAdjustments: { top: -7 },
+        tooltipComponent: <FaceValueTooltip />
+    }
+};
 
-        const subsidyBarAndLabelStyles = {
-            width: generatePercentage(subsidy / faceValue),
-            backgroundColor: '#F5A623'
-        };
+const LoansChart = ({ awardAmounts }) => {
+    const [
+        activeTooltip,
+        closeTooltip,
+        showSubsidyTooltip,
+        showFaceValueTooltip
+    ] = useTooltips(["subsidy", "faceValue"]);
 
-        const faceValueColor = "#FFF";
+    const buildTooltipProps = (spendingCategory, isVisible, showTooltip) => ({
+        ...tooltipPropsBySpendingCategory[spendingCategory],
+        wide: true,
+        controlledProps: {
+            isControlled: true,
+            isVisible,
+            showTooltip,
+            closeTooltip
+        }
+    });
 
-        const { subsidyTooltipProps, faceValueTooltipProps } = this.props;
+    // Rename properties to improve readability of the calculations
+    const subsidy = awardAmounts._subsidy;
+    const faceValue = awardAmounts._faceValue;
+    const isSubsidyZero = (subsidy === 0);
 
-        return (
-            <div className="award-amounts-viz">
-                {!isSubsidyZero &&
+    const subsidyBarAndLabelStyles = {
+        width: generatePercentage(subsidy / faceValue),
+        backgroundColor: '#F5A623'
+    };
+
+    const faceValueColor = "#FFF";
+
+    const propsForSubsidyTooltip = buildTooltipProps('subsidy', (activeTooltip === 'subsidy'), showSubsidyTooltip);
+    const propsForFaceValueTooltip = buildTooltipProps('faceValue', (activeTooltip === 'faceValue'), showFaceValueTooltip);
+
+    return (
+        <div className="award-amounts-viz">
+            {!isSubsidyZero &&
+                <div
+                    className="award-amounts-viz__desc-top--loans"
+                    role="button"
+                    tabIndex="0"
+                    onBlur={closeTooltip}
+                    onFocus={showSubsidyTooltip}
+                    onKeyPress={showSubsidyTooltip}
+                    onMouseEnter={showSubsidyTooltip}
+                    onMouseLeave={closeTooltip}
+                    onClick={showSubsidyTooltip}>
+                    <strong>{awardAmounts.subsidyAbbreviated}</strong><br />Original Subsidy Cost
+                    <div className="award-amounts-viz__label" style={subsidyBarAndLabelStyles}>
+                        <div className="award-amounts-viz__line-up--loans" />
+                    </div>
+                </div>}
+            {isSubsidyZero &&
+                <TooltipWrapper {...propsForSubsidyTooltip} styles={{ width: '160px' }}>
+                    <div className="award-amounts-viz__desc-top--loans" role="button" tabIndex="0">
+                        <strong>{awardAmounts.subsidyAbbreviated}</strong>
+                        <span>Original Subsidy Cost</span>
+                    </div>
+                </TooltipWrapper>
+            }
+            <div className="award-amounts-viz__bar-wrapper">
+                <TooltipWrapper {...propsForFaceValueTooltip} styles={{ backgroundColor: faceValueColor }}>
+                    <div className="award-amounts-viz__bar" style={{ backgroundColor: faceValueColor, width: '100%' }}>
+                        {!isSubsidyZero &&
+                            <TooltipWrapper {...propsForSubsidyTooltip} styles={{ width: subsidyBarAndLabelStyles.width }}>
+                                <div className="award-amounts-viz__obligated--grants" style={{ width: generatePercentage(1), backgroundColor: subsidyBarAndLabelStyles.backgroundColor }} />
+                            </TooltipWrapper>}
+                        {isSubsidyZero && <div className="award-amounts-viz__obligated--grants" style={subsidyBarAndLabelStyles} />}
+                    </div>
+                </TooltipWrapper>
+            </div>
+            <div className="award-amounts-viz__label">
+                <div className="award-amounts-viz__line" style={{ backgroundColor: faceValueColor }} />
+                <div className="award-amounts-viz__desc">
                     <div
-                        className="award-amounts-viz__desc-top--loans"
+                        className="award-amounts-viz__desc-text"
                         role="button"
                         tabIndex="0"
-                        onBlur={subsidyTooltipProps.controlledProps.closeTooltip}
-                        onFocus={subsidyTooltipProps.controlledProps.showTooltip}
-                        onKeyPress={subsidyTooltipProps.controlledProps.showTooltip}
-                        onMouseEnter={subsidyTooltipProps.controlledProps.showTooltip}
-                        onMouseLeave={subsidyTooltipProps.controlledProps.closeTooltip}
-                        onClick={subsidyTooltipProps.controlledProps.showTooltip}>
-                        <strong>{this.props.awardAmounts.subsidyAbbreviated}</strong><br />Original Subsidy Cost
-                        <div className="award-amounts-viz__label" style={subsidyBarAndLabelStyles}>
-                            <div className="award-amounts-viz__line-up--loans" />
-                        </div>
-                    </div>}
-                {isSubsidyZero &&
-                    <TooltipWrapper {...subsidyTooltipProps} styles={{ width: '160px' }}>
-                        <div className="award-amounts-viz__desc-top--loans" role="button" tabIndex="0">
-                            <strong>{this.props.awardAmounts.subsidyAbbreviated}</strong>
-                            <span>Original Subsidy Cost</span>
-                        </div>
-                    </TooltipWrapper>
-                }
-                <div className="award-amounts-viz__bar-wrapper">
-                    <TooltipWrapper {...faceValueTooltipProps} style={{ backgroundColor: faceValueColor }}>
-                        <div className="award-amounts-viz__bar" style={{ backgroundColor: faceValueColor, width: '100%' }}>
-                            {!isSubsidyZero &&
-                                <TooltipWrapper {...subsidyTooltipProps} styles={{ width: subsidyBarAndLabelStyles.width }}>
-                                    <div className="award-amounts-viz__obligated--grants" style={{ width: generatePercentage(1), backgroundColor: subsidyBarAndLabelStyles.backgroundColor }} />
-                                </TooltipWrapper>}
-                            {isSubsidyZero && <div className="award-amounts-viz__obligated--grants" style={subsidyBarAndLabelStyles} />}
-                        </div>
-                    </TooltipWrapper>
-                </div>
-                <div className="award-amounts-viz__label">
-                    <div className="award-amounts-viz__line" style={{ backgroundColor: faceValueColor }} />
-                    <div className="award-amounts-viz__desc">
-                        <div
-                            className="award-amounts-viz__desc-text"
-                            role="button"
-                            tabIndex="0"
-                            onBlur={faceValueTooltipProps.controlledProps.closeTooltip}
-                            onFocus={faceValueTooltipProps.controlledProps.showTooltip}
-                            onKeyPress={faceValueTooltipProps.controlledProps.showTooltip}
-                            onMouseEnter={faceValueTooltipProps.controlledProps.showTooltip}
-                            onMouseLeave={faceValueTooltipProps.controlledProps.closeTooltip}
-                            onClick={faceValueTooltipProps.controlledProps.showTooltip}>
-                            <strong>{this.props.awardAmounts.faceValueAbbreviated}</strong><br />Face Value of Direct Loan
-                        </div>
-                        <div className="award-amounts-viz__legend-line award-amounts-viz__legend-line_potential" style={{ backgroundColor: faceValueColor }} />
+                        onBlur={closeTooltip}
+                        onFocus={showFaceValueTooltip}
+                        onKeyPress={showFaceValueTooltip}
+                        onMouseEnter={showFaceValueTooltip}
+                        onMouseLeave={closeTooltip}
+                        onClick={showFaceValueTooltip}>
+                        <strong>{awardAmounts.faceValueAbbreviated}</strong><br />Face Value of Direct Loan
                     </div>
+                    <div className="award-amounts-viz__legend-line award-amounts-viz__legend-line_potential" style={{ backgroundColor: faceValueColor }} />
                 </div>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
 
+export default LoansChart;
 // LoansChart.defaultProps = defaultProps;
 LoansChart.propTypes = propTypes;
