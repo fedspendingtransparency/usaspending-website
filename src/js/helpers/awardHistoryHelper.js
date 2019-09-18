45c11c1bb850e9ff31b06387eb452fecbba9ab57
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2019-09-18 16:50:52 -0400

    [DEV-3099] adding count endpoint

---
+++ src/js/helpers/awardHistoryHelper.js
@@ -0,0 +1,26 @@
+/**
+  * idvHelper.js
+  * Created by Max Kendall 09/18/19
+  **/
+
+/* eslint-disable import/prefer-default-export */
+import Axios, { CancelToken } from 'axios';
+import kGlobalConstants from 'GlobalConstants';
+
+/**
+ * @param type: oneOf(['subaward', 'transaction', 'federal_account'])
+ */
+export const getAwardHistoryCounts = (type, awardId) => {
+    const source = CancelToken.source();
+    return {
+        promise: Axios.request({
+            url: `v2/awards/count/${type}/${awardId}`,
+            baseURL: kGlobalConstants.API,
+            method: 'post',
+            cancelToken: source.token
+        }),
+        cancel() {
+            source.cancel();
+        }
+    };
+};
