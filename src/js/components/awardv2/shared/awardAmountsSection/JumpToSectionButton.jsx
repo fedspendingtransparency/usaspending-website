52b5fbd87492086a65e94d7f273c344898643e65
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2019-09-19 22:05:26 -0400

    [DEV-3099] fixing lint errors

--- src/js/components/awardv2/shared/awardAmountsSection/JumpToSectionButton.jsx
+++ src/js/components/awardv2/shared/awardAmountsSection/JumpToSectionButton.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
@@ -8,20 +8,18 @@ const propTypes = {
     icon: PropTypes.string
 };
 
-const JumpToSectionButton = ({ onClick, linkText, icon }) => {
-    return (
-        <button
-            onClick={onClick}
-            className="award-viz__button">
-            <div className="award-viz__link-icon">
-                <FontAwesomeIcon icon={icon} />
-            </div>
-            <div className="award-viz__link-text">
-                {linkText}
-            </div>
-        </button>
-    );
-};
+const JumpToSectionButton = ({ onClick, linkText, icon }) => (
+    <button
+        onClick={onClick}
+        className="award-viz__button">
+        <div className="award-viz__link-icon">
+            <FontAwesomeIcon icon={icon} />
+        </div>
+        <div className="award-viz__link-text">
+            {linkText}
+        </div>
+    </button>
+);
 
 JumpToSectionButton.propTypes = propTypes;
 
