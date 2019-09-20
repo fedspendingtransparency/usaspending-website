bf5f38e77905b359b4c6d5e072d73adf71d28a9a
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2019-09-19 20:57:24 -0400

    [dev-3100] adding test for getSubAwardedPercent

--- tests/models/awardsV2/CoreAward-test.js
+++ tests/models/awardsV2/CoreAward-test.js
@@ -8,7 +8,8 @@ import CoreAward from 'models/v2/awardsV2/CoreAward';
 const awardData = {
     subawardTotal: 12004.75,
     baseExercisedOptions: 2342342.32,
-    totalObligation: 12345678.00
+    totalObligation: 12345678.00,
+    baseAndAllOptions: 20100.00
 };
 
 const award = Object.create(CoreAward);
@@ -17,5 +18,8 @@ award.populateCore(awardData);
 describe('Core Award getter functions', () => {
     it('should format the subaward total', () => {
         expect(award.subawardTotal).toEqual('$12,005');
+    });
+    it('should derive the subawardedPercet', () => {
+        expect(award.subAwardedPercent).toEqual('60%');
     });
 });
