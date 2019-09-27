cbcf47ab87124a43528e6d550bd1a684c620b245
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2019-09-27 15:18:26 -0400

    [DEV-3635] fixing lint error

--- src/js/components/awardv2/shared/AwardPageWrapper.jsx
+++ src/js/components/awardv2/shared/AwardPageWrapper.jsx
@@ -12,34 +12,32 @@ const AwardPageWrapper = ({
     identifier,
     idLabel,
     children
-}) => {
-    return (
-        <div className={`award award-${awardType}`}>
-            <div className="award__heading">
-                <div className="award__info">
-                    <h2 className="award__heading-text">{startCase(awardTypeDescription)}</h2>
-                    <div className="award__heading-icon">
-                        <a href={glossaryLink}>
-                            <Glossary />
-                        </a>
-                    </div>
-                    <div className="award__heading-id">
-                        <h3>{idLabel}</h3>
-                        <p>{identifier}</p>
-                    </div>
+}) => (
+    <div className={`award award-${awardType}`}>
+        <div className="award__heading">
+            <div className="award__info">
+                <h2 className="award__heading-text">{startCase(awardTypeDescription)}</h2>
+                <div className="award__heading-icon">
+                    <a href={glossaryLink}>
+                        <Glossary />
+                    </a>
                 </div>
-                <div className="award__last-modified">
-                Last Modified On:{" "}
-                    <span className="award__last-modified award__last-modified_date">
-                        {lastModifiedDateLong}
-                    </span>
+                <div className="award__heading-id">
+                    <h3>{idLabel}</h3>
+                    <p>{identifier}</p>
                 </div>
             </div>
-            <hr />
-            {children}
+            <div className="award__last-modified">
+            Last Modified On:{" "}
+                <span className="award__last-modified award__last-modified_date">
+                    {lastModifiedDateLong}
+                </span>
+            </div>
         </div>
-    );
-};
+        <hr />
+        {children}
+    </div>
+);
 
 AwardPageWrapper.defaultProps = { idLabel: "PIID" };
 AwardPageWrapper.propTypes = AWARD_PAGE_WRAPPER_PROPS;
