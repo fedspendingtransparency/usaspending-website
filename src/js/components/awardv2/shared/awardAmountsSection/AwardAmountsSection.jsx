031b42bb406c6e376111f3e8ffc900abff68cdb4
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2019-10-01 16:48:40 -0400

    [dev-2992] making import path absolute

--- src/js/components/awardv2/shared/awardAmountsSection/AwardAmountsSection.jsx
+++ src/js/components/awardv2/shared/awardAmountsSection/AwardAmountsSection.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { determineSpendingScenario, getAscendingSpendingCategoriesByAwardType } from 'helpers/awardAmountHelper';
 import AwardSection from '../AwardSection';
 import AwardSectionHeader from '../AwardSectionHeader';
 import AwardAmountsChart from './charts/AwardAmountsChart';
 import AwardAmountsTable from './AwardAmountsTable';
 import { AWARD_OVERVIEW_AWARD_AMOUNTS_SECTION_PROPS, AWARD_TYPE_PROPS } from '../../../../propTypes';
-import { determineSpendingScenario, getAscendingSpendingCategoriesByAwardType } from '../../../../helpers/awardAmountHelper';
 import JumpToSectionButton from './JumpToSectionButton';
 import { ContractAwardAmountsInfo, GrantAwardAmountsInfo, LoanAwardAmountsInfo } from '../InfoTooltipContent';
 
