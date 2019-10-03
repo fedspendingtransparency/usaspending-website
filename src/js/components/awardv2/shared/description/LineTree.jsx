60c9bc68464da335702c0ba708d733f33d6f899a
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2019-10-03 15:44:27 -0400

    [DEV-1663] polishing styles

--- src/js/components/awardv2/shared/description/LineTree.jsx
+++ src/js/components/awardv2/shared/description/LineTree.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { get } from 'lodash';
 
 const propTypes = {
     type: PropTypes.oneOf(["naics", "psc"]),
     data: PropTypes.shape({})
 };
-
-const parentMargin = 14;
 
 const LineTree = ({
     type,
@@ -20,8 +17,8 @@ const LineTree = ({
             const second = data[tierType2];
             if (first.code === 'SERVICES') return -1;
             if (second.code === 'SERVICES') return 1;
-            if (first.code > second.code) return -1;
-            if (second.code > first.code) return 1;
+            if (first.code < second.code) return -1;
+            if (second.code < first.code) return 1;
             return 0;
         })
         .reduce((acc, tierType) => ({ ...acc, [tierType]: data[tierType] }), {});
@@ -38,19 +35,21 @@ const LineTree = ({
     };
 
     return (
-        <div style={{ margin: `${parentMargin}px 0px` }} className={`line-tree-${type}`}>
+        <div className={`line-tree-${type}`}>
             {getTierData(0) && (
-                <div className="line-tree__section">
-                    <span>{`${getTierData(0).code}: `}</span>
-                    <span>{`${getTierData(0).description}`}</span>
+                <div className="tier--1">
+                    <span>{`${getTierData(0).code}: ${getTierData(0).description}`}</span>
                     {getTierData(1) && (
-                        <div className={`${numberOfSections >= 2 ? 'line-tree__section--last' : 'line-tree__section'}`}>
-                            <span>{`${getTierData(1).code}: `}</span>
-                            <span>{`${getTierData(1).description}`}</span>
+                        <div className={`tier--2 ${numberOfSections <= 2 ? 'tier--last' : ''}`}>
+                            <span>{`${getTierData(1).code}: ${getTierData(1).description}`}</span>
                             {getTierData(2) && (
-                                <div className={`${numberOfSections >= 3 ? 'line-tree__section--last' : 'line-tree__section'}`}>
-                                    <span>{`${getTierData(2).code}: `}</span>
-                                    <span>{`${getTierData(2).description}`}</span>
+                                <div className={`tier--3 ${numberOfSections <= 3 ? 'tier--last' : ''}`}>
+                                    <span>{`${getTierData(2).code}: ${getTierData(2).description}`}</span>
+                                    {getTierData(3) && (
+                                        <div className={`tier--4 ${numberOfSections <= 4 ? 'tier--last' : ''}`}>
+                                            <span>{`${getTierData(3).code}: ${getTierData(3).description}`}</span>
+                                        </div>
+                                    )}
                                 </div>
                             )}
                         </div>
