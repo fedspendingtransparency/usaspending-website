1e7f7bedd9d6a55dd01e67a7353d0538a2fd3219
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2019-10-03 12:57:46 -0400

    [DEV-1663] refactor to use css for height

--- src/js/components/awardv2/shared/description/LineTree.jsx
+++ src/js/components/awardv2/shared/description/LineTree.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useRef, useEffect, useCallback } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { uniqueId, isEqual } from 'lodash';
+import { get } from 'lodash';
 
 const propTypes = {
     type: PropTypes.oneOf(["naics", "psc"]),
@@ -13,84 +13,51 @@ const LineTree = ({
     type,
     data
 }) => {
-    const initialState = Object.keys(data)
-        .reduce((acc, tierType) => ({ ...acc, [tierType]: '0px' }), {});
-    const [lineHeightByTierType, setLineHeightsByTierType] = useState(initialState);
-    const parentRef = useRef('');
-    const pscServicesRef = useRef('');
-    const topTierRef = useRef('');
-    const baseTierRef = useRef('');
-    const subTierRef = useRef('');
-    const midTierRef = useRef('');
+    const parsedData = Object.keys(data)
+        .filter((tierType) => Object.keys(data[tierType]).includes('code') && Object.keys(data[tierType]).includes('description'))
+        .sort((tierType1, tierType2) => {
+            const first = data[tierType1];
+            const second = data[tierType2];
+            if (first.code === 'SERVICES') return -1;
+            if (second.code === 'SERVICES') return 1;
+            if (first.code > second.code) return -1;
+            if (second.code > first.code) return 1;
+            return 0;
+        })
+        .reduce((acc, tierType) => ({ ...acc, [tierType]: data[tierType] }), {});
 
-    const refsByTierType = {
-        additionalDataForPsc: pscServicesRef,
-        midtier_code: midTierRef,
-        toptier_code: topTierRef,
-        subtier_code: subTierRef,
-        basetier_code: baseTierRef
+    const tiersInHierarchialOrder = Object.keys(parsedData);
+    const numberOfSections = tiersInHierarchialOrder.length;
+
+    const getTierData = (index) => {
+        if (tiersInHierarchialOrder.length - 1 >= index) {
+            return parsedData[tiersInHierarchialOrder[index]];
+        }
+        // no more available data
+        return false;
     };
 
-    const getLineHeightForTierType = useCallback((tierType) => {
-        const refForTier = refsByTierType[tierType];
-        if (refForTier) {
-            const coordinateToBottomOfListItem = refForTier.current.getBoundingClientRect().bottom;
-            const coordinateToBottomOfParent = parentRef.current.getBoundingClientRect().bottom;
-            const appropriateHeight = coordinateToBottomOfParent - coordinateToBottomOfListItem;
-            return `${appropriateHeight}px`;
-        }
-        return '0px';
-    }, [refsByTierType]);
-
-    useEffect(() => {
-        // For each tier type, calculate the height
-        if (isEqual(initialState, lineHeightByTierType)) {
-            const newState = Object.keys(data)
-                .filter((tierType) => Object.keys(data[tierType]).length > 0)
-                .reduce((acc, tierType) => ({
-                    ...acc,
-                    [tierType]: getLineHeightForTierType(tierType)
-                }), {});
-            setLineHeightsByTierType(newState);
-        }
-    }, [data, getLineHeightForTierType, lineHeightByTierType, initialState]);
-
-    const handleSort = (a, b, source = data) => {
-        const first = source[a].code;
-        const second = source[b].code;
-        if (first === 'SERVICES') return -1;
-        if (second === 'SERVICES') return 1;
-        if (first.length < second.length) {
-            // place first at a lower index than second
-            return -1;
-        }
-        if (second.length < first.length) {
-            // place second at a lower index than first
-            return 1;
-        }
-        // neither first nor second sorted at a lower index
-        return 0;
-    };
     return (
-        <ul ref={parentRef} style={{ margin: `${parentMargin}px 0px` }} className={`line-tree-${type}`}>
-            {Object.keys(data)
-                .filter((tierType) => Object.keys(data[tierType]).includes('code') && Object.keys(data[tierType]).includes('description'))
-                .sort(handleSort)
-                .map((tierType, index, arr) => {
-                    const showLine = (arr.length - 1) > index;
-                    const ref = refsByTierType[tierType];
-                    return (
-                        <li key={`${tierType}-${uniqueId(index)}`} ref={ref} className={`line-tree__${tierType}`}>
-                            <span>{`${data[tierType].code}: `}</span>
-                            <span>{`${data[tierType].description}`}</span>
-                            {showLine && (
-                                <div style={{ height: lineHeightByTierType[tierType] }} className="line-tree__line" />
+        <div style={{ margin: `${parentMargin}px 0px` }} className={`line-tree-${type}`}>
+            {getTierData(0) && (
+                <div className="line-tree__section">
+                    <span>{`${getTierData(0).code}: `}</span>
+                    <span>{`${getTierData(0).description}`}</span>
+                    {getTierData(1) && (
+                        <div className={`${numberOfSections >= 2 ? 'line-tree__section--last' : 'line-tree__section'}`}>
+                            <span>{`${getTierData(1).code}: `}</span>
+                            <span>{`${getTierData(1).description}`}</span>
+                            {getTierData(2) && (
+                                <div className={`${numberOfSections >= 3 ? 'line-tree__section--last' : 'line-tree__section'}`}>
+                                    <span>{`${getTierData(2).code}: `}</span>
+                                    <span>{`${getTierData(2).description}`}</span>
+                                </div>
                             )}
-                        </li>
-                    );
-                })
-            }
-        </ul>
+                        </div>
+                    )}
+                </div>
+            )}
+        </div>
     );
 };
 
