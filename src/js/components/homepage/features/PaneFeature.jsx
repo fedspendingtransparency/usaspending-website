51e5c68114a8e4bd06cb9178cd3cf5b096069231
Jonathan Hill <hill_jonathan@bah.com>
2019-10-11 10:30:21 -0400

    dev-3382 update homepage

--- src/js/components/homepage/features/PaneFeature.jsx
+++ src/js/components/homepage/features/PaneFeature.jsx
@@ -4,6 +4,7 @@
  */
 
 import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const PaneFeature = () => (
     <div className="feature-pane">
@@ -11,28 +12,55 @@ const PaneFeature = () => (
             <h2 className="feature-pane__title">Featured Content</h2>
             <div className="feature-pane__content-wrapper">
                 <div className="feature-pane__content">
-                    <h3 className="feature-pane__content-title">New! - Congressional Budget Justifications</h3>
-                    <p className="feature-pane_content-text">See the rich, detailed documents each agency submits to Congress to justify its annual budget request.</p>
+                    <div className="feature-pane__content-title">
+                        New! - Congressional Budget Justifications
+                    </div>
+                    <p className="feature-pane_content-text">
+                        See the rich, detailed documents each agency submits to Congress
+                        to justify its annual budget request.
+                    </p>
                     <div className="feature-pane__button-wrapper">
-                        <a href="/#/agency" className="feature-pane__button">Explore Budget Justifications</a>
+                        <a href="/#/agency" className="feature-pane__button">
+                            Explore Budget Justifications
+                        </a>
                     </div>
                 </div>
                 <div className="feature-pane__content-divider" />
                 <hr className="feature-pane__mobile-content-divider" />
-                <div className="feature-pane__content">
-                    <h3 className="feature-pane__content-title">New! – Your Guide to America’s Finances</h3>
-                    <p className="feature-pane_content-text">Your Guide presents straightforward information about the federal government’s spending and revenue, as well as the deficit and debt in 2018.</p>
+                <div className="feature-pane__content" id="feature-pane__content-middle">
+                    <div id="feature-pane__content-header__middle">
+                        Updated for FY 2019
+                    </div>
+                    <div className="feature-pane__content-title" id="feature-pane__content-title__middle">
+                        Your Guide to America’s Finances
+                    </div>
+                    <p className="feature-pane_content-text">
+                        Your Guide provides a snapshot of Fiscal Year 2019 revenue, spending,
+                        deficit, and debt, along with data for download.
+                        Click below to visit our partner site.
+                    </p>
                     <div className="feature-pane__button-wrapper">
-                        <a href="https://datalab.usaspending.gov/americas-finance-guide/" target="_blank" rel="noopener noreferrer" className="feature-pane__button">Explore the Data</a>
+                        <a
+                            href="https://datalab.usaspending.gov/americas-finance-guide/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="feature-pane__button">
+                            Explore the Guide <span id="middle-pane-feature-icon"><FontAwesomeIcon icon="external-link-alt" /></span>
+                        </a>
                     </div>
                 </div>
                 <hr className="feature-pane__mobile-content-divider" />
                 <div className="feature-pane__content-divider" />
                 <div className="feature-pane__content">
-                    <h3 className="feature-pane__content-title">New! - Recipient Profiles</h3>
-                    <p className="feature-pane_content-text" >Find insights into each recipient with the tools and summaries found in our new Recipient Profile pages.</p>
+                    <div className="feature-pane__content-title">New! - Recipient Profiles</div>
+                    <p className="feature-pane_content-text" >
+                        Find insights into each recipient with the tools and
+                        summaries found in our new Recipient Profile pages.
+                    </p>
                     <div className="feature-pane__button-wrapper">
-                        <a href="/#/recipient" className="feature-pane__button">Explore Recipient Profiles</a>
+                        <a href="/#/recipient" className="feature-pane__button">
+                            Explore Recipient Profiles
+                        </a>
                     </div>
                 </div>
             </div>
