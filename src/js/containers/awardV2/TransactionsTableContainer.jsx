4b113134c52ea488f227c3f61ca0384ad4d6ab6a
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2019-12-11 15:33:17 -0500

    [DEV-3687] TransactionsTableContainer.jsx

--- src/js/containers/awardV2/TransactionsTableContainer.jsx
+++ src/js/containers/awardV2/TransactionsTableContainer.jsx
@@ -17,7 +17,7 @@ import BaseAssistanceTransaction from 'models/v2/awards/transactions/BaseAssista
 import BaseContractTransaction from 'models/v2/awards/transactions/BaseContractTransaction';
 import BaseLoanTransaction from 'models/v2/awards/transactions/BaseLoanTransaction';
 
-import TransactionsTable from 'components/award/table/TransactionsTable';
+import TransactionsTable from '../../components/awardv2/table/TransactionsTable';
 
 const propTypes = {
     award: PropTypes.object,
