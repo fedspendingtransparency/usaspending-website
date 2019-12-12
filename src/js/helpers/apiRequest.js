141789ad5255fb2706656fd57348937a941b20fb
Jonathan Hill <hill_jonathan@bah.com>
2019-12-12 05:08:59 -0500

    * dev-3982
    * add apiRequest
    
    * migrate accountHelper to apiRequest

---
+++ src/js/helpers/apiRequest.js
@@ -0,0 +1,63 @@
+/**
+ * apiRequest.js
+ * Created by Jonathan Hill 12/11/19
+ **/
+
+import Axios, { CancelToken } from 'axios';
+import kGlobalConstants from 'GlobalConstants';
+
+// eslint-disable-next-line import/prefer-default-export
+export const apiRequest = (axiosParams) => {
+    const defaultHeaders = { 'X-Requested-With': 'USASpendingFrontend' };
+    const cancelToken = CancelToken.source();
+    const defaultParams = {
+        baseURL: kGlobalConstants.API,
+        cancelToken: cancelToken.token
+    };
+
+    const headers = (additionalHeaders) => {
+        if (additionalHeaders) return { ...defaultHeaders, ...additionalHeaders };
+        return defaultHeaders;
+    };
+
+    const params = (options) => {
+        const parameters = { ...defaultParams };
+        Object.assign(parameters, options);
+        console.log( ' Parameters : ', parameters);
+        parameters.headers = headers(options.headers);
+        return parameters;
+    };
+
+    const cancel = () => cancelToken.cancel();
+
+    const before = () => {};
+
+    const after = () => {};
+
+    const request = async (parameters) => {
+        const newRequest = Axios.request(parameters);
+        try {
+            before();
+            const response = await newRequest;
+            after();
+            return response;
+        }
+        catch (e) {
+            cancel();
+            throw new Error(e);
+        }
+    };
+
+    const execute = async () => request(params(axiosParams));
+
+    const promise = Axios.request(params(axiosParams));
+
+    return {
+        promise,
+        execute,
+        cancel,
+        before,
+        after
+    };
+};
+
