4d125e450e075a4c7e60c4d1e6534eb5a0a3fd22
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2019-12-12 14:57:46 -0500

    [DEV-3157] logging <subawardId> (<prime award Id>)

--- src/js/components/search/table/ResultsTable.jsx
+++ src/js/components/search/table/ResultsTable.jsx
@@ -81,10 +81,11 @@ export default class ResultsTable extends React.Component {
             props.column = 'award';
         }
         else if ((column.columnName === 'Sub-Award ID') && this.props.subaward) {
+            const row = this.props.results[rowIndex];
             cellClass = ResultsTableLinkCell;
             props.column = 'award';
-            props.id = this.props.results[rowIndex].prime_award_generated_internal_id;
-            props.onClick = this.props.subAwardIdClick.bind(null, this.props.results[rowIndex]['Sub-Award ID']);
+            props.id = row.prime_award_generated_internal_id;
+            props.onClick = this.props.subAwardIdClick.bind(null, `${row['Sub-Award ID']} (${props.id})`);
         }
         else if (column.columnName === 'Recipient Name' && this.props.results[rowIndex].recipient_id) {
             cellClass = ResultsTableLinkCell;
