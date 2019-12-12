6cf2db5415729085b07548c7c59ca531f9e66380
Jonathan Hill <hill_jonathan@bah.com>
2019-12-12 05:27:01 -0500

    * dev-3982
    * remove archive helper
    
    * migrate award history helper to api request

--- src/js/helpers/awardHistoryHelper.js
+++ src/js/helpers/awardHistoryHelper.js
@@ -3,59 +3,26 @@
   * Created by Max Kendall 09/18/19
   **/
 
-/* eslint-disable import/prefer-default-export */
-import Axios, { CancelToken } from 'axios';
-import kGlobalConstants from 'GlobalConstants';
+import { apiRequest } from './apiRequest';
 
 // Get federal accounts
-export const fetchFederalAccountFunding = (params) => {
-    const source = CancelToken.source();
-    return {
-        promise: Axios.request({
-            url: 'v2/awards/funding/',
-            baseURL: kGlobalConstants.API,
-            method: 'post',
-            data: params,
-            cancelToken: source.token
-        }),
-        cancel() {
-            source.cancel();
-        }
-    };
-};
+export const fetchFederalAccountFunding = (params) => apiRequest({
+    url: 'v2/awards/funding/',
+    method: 'post',
+    data: params
+});
 
-export const getAwardHistoryFederalAccountsIdv = (awardId) => {
-    const source = CancelToken.source();
-    return {
-        promise: Axios.request({
-            url: `v2/idvs/count/federal_account/${awardId}/`,
-            baseURL: kGlobalConstants.API,
-            method: 'get',
-            cancelToken: source.token
-        }),
-        cancel() {
-            source.cancel();
-        }
-    };
-};
-
+export const getAwardHistoryFederalAccountsIdv = (awardId) => apiRequest({
+    url: `v2/idvs/count/federal_account/${awardId}/`,
+});
 
 /**
  * @param type: oneOf(['subaward', 'transaction', 'federal_account'])
  */
 export const getAwardHistoryCounts = (type, awardId, isIdv = false) => {
-    const source = CancelToken.source();
     const parsedAwardId = encodeURI(awardId);
     if (type === 'federal_account' && isIdv) return getAwardHistoryFederalAccountsIdv(parsedAwardId);
-    return {
-        promise: Axios.request({
-            url: `v2/awards/count/${type}/${parsedAwardId}/`,
-            baseURL: kGlobalConstants.API,
-            method: 'get',
-            cancelToken: source.token
-        }),
-        cancel() {
-            source.cancel();
-        }
-    };
+    return apiRequest({
+        url: `v2/awards/count/${type}/${parsedAwardId}/`,
+    });
 };
