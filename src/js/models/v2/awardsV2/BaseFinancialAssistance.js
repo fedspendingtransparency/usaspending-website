0501370fe4ebfb4c3442fd02a1e76f67349577dd
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2019-12-19 16:20:24 -0500

    [DEV-3994] encoding generatedId in CoreAward model

--- src/js/models/v2/awardsV2/BaseFinancialAssistance.js
+++ src/js/models/v2/awardsV2/BaseFinancialAssistance.js
@@ -37,7 +37,7 @@ BaseFinancialAssistance.populate = function populate(data) {
     // reformat some fields that are required by the CoreAward
     const coreData = {
         id: data.id,
-        generatedId: data.generated_unique_award_id,
+        generatedId: encodeURI(`${data.generated_unique_award_id}`),
         type: data.type,
         typeDescription: data.type_description,
         description: data.description,
