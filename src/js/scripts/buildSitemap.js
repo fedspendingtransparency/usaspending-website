744616a980cfc5d312bf43da8fce6adad333809d
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2020-01-03 17:58:16 -0500

    [dev-4036] builds site map

--- src/js/scripts/buildSitemap.js
+++ src/js/scripts/buildSitemap.js
@@ -13,24 +13,86 @@ const Routes = require('../containers/router/RouterRoutes.js').routes;
 // 2e. x # of awards?
 
 const xmlStart = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"><url>`;
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
 
-const testRoutes = [
-    'google.com/test'
+const urls = [
+    {
+        name: 'state',
+        url: 'https://api.usaspending.gov/api/v2/recipient/state/',
+        method: 'get',
+        accessor: 'fips',
+        clientRoute: 'https://www.usaspending.gov/#/state'
+    },
+    {
+        name: 'agency',
+        url: 'https://api.usaspending.gov/api/v2/references/toptier_agencies/',
+        method: 'get',
+        accessor: 'agency_id',
+        clientRoute: 'https://www.usaspending.gov/#/agency'
+    },
+    {
+        name: 'federal_account',
+        url: 'https://api.usaspending.gov/api/v2/federal_accounts/',
+        method: 'post',
+        accessor: 'account_number',
+        requestObject: {
+            sort: { field: "budgetary_resources", direction: "desc" },
+            page: 1,
+            limit: 50,
+            filters: { fy: "2019" }
+        },
+        clientRoute: 'https://www.usaspending.gov/#/federal_account'
+    }
 ];
 
-const xmlEnd = `</url></urlset>`;
+const xmlEnd = `</urlset>`;
 
-const buildRouteString = () => {
-    return testRoutes.reduce((acc, route) => {
-        return `${acc}<url><loc>${route}</loc></url>`;
-    }, '');
+const getBigStringOfXML = (xml, data, previousUrl) => {
+    if (!data) return '';
+    console.log("previousUrl", previousUrl.name);
+    const { accessor, clientRoute } = previousUrl;
+    return data.reduce((str, apiPayloadItem) => {
+        return `${str}<url><loc>${clientRoute}/${apiPayloadItem[accessor]}</loc></url>`;
+    }, xml);
 };
 
-const writeXMLFile = () => {
-    fs.writeFile(path.resolve(__dirname, '../../../sitemap.xml'), `${xmlStart}${buildRouteString()}${xmlEnd}`, () => {
-        console.log("Sitemap successfully created!");
+const writeXMLFile = (xmlRoutes) => {
+    fs.writeFile(
+        path.resolve(__dirname, '../../../sitemap.xml'),
+        `${xmlStart}${xmlRoutes}${xmlEnd}`,
+        () => {
+            console.log("Sitemap successfully created!");
+        });
+};
+
+const buildRouteString = () => {
+    let xml = '';
+    const result = urls.reduce((previousPromise, url, i, arr) => {
+        return previousPromise
+            .then((resp) => {
+                if (resp !== 'first') {
+                    // use previous item in array as the source of truth for traversing the previousPromises' response
+                    const previousUrl = arr[i - 1];
+                    const results = resp.data.results || resp.data;
+                    xml = getBigStringOfXML(xml, results, previousUrl);
+                    if 
+                }
+                return axios({
+                    method: url.method,
+                    data: url.requestObject || null,
+                    url: url.url
+                });
+        })
+        .catch((e) => console.log("e", e));
+    }, Promise.resolve('first'));
+
+    result.then((resp) => {
+        // Once the final promise resolves, we can add it to our xml string and then write the file.
+        const previousUrl = urls[urls.length -1];
+        const { results } = resp.data;
+        xml = getBigStringOfXML(xml, results, previousUrl);
+        writeXMLFile(xml);
     });
 };
 
-writeXMLFile();
+buildRouteString();
