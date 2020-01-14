97fcbffa1c1fd21cb08c56d5fd7f3b28b487c24e
Maxwell Kendall <MaxwellKendall@users.noreply.github.com>
2020-01-14 09:14:11 -0500

    [DEV-4036] Create Sitemaps on build (#1458)
    * [DEV-4036] uploading robots.txt & sitemap.xml
    
    * [DEV-4036] moving robots.txt and sitemap.xml
    
    * [DEV-4036] fixing router file to play nice w/ script
    
    * [DEV-4036] updating imports of Router module
    whose export statement was updated to play nice w/ script
    
    * [DEV-4036] moving buildSitemap script
    
    * [DEV-4036] updating location of robots.txt and
    sitemap.xml in webpack config
    
    * [DEV-4036] calling buildSitemap in prod config
    
    * [dev-4036] builds site map
    
    * [dev-4036] removing bug
    
    * [DEV-4036] adding awards to sitemap
    
    * [DEV-4036] impl indexed sitemap & static routes
    
    * [DEV-4036] moving robots.txt
    
    * [DEV-4036] using site map index file
    
    * [DEV-4036] really big indexed sitemaps
    
    * [DEV-4036] moving sitemaps & robots.txt,
    also generating robots.txt on build by env
    
    * [DEV-4036] updating webpack copy from location
    
    * [dev-4036] filtering unacceptable sitemap entries
    
    * [DEV-4036] adding new tags
    
    * [dev-4036] adding sitemaps to .gitignore
    
    * [dev-4036] moving stuff around, adding to gitignore
    
    * [DEV-4036] updating import path
    
    * [dev-4036] always using prod as param
    
    * [dev-4036] troubleshooting
    
    * [DEV-4036] sandbox test w/ relative path
    
    * [dev-4036] sandbox test
    
    * [dev-4036] moving away from glob copyWebpackPlugin
    bc it doesnt work for some reason? added comments etc...
    
    * [dev-4036] only index site maps actually created
    
    * [dev-4036] fixing exports/imports of RouterRoutes.js
    
    * [dev-4036] fixing import
    
    * [DEV-4036] changing var name
    
    * [DEV-4036] adding ./ to .gitignore
    
    * [DEV-4036] adding a test for every sitemap reliant on an api
    
    * [DEV-4036] removing . from gitignore
    
    * [DEV-4036] removing lodash
    
    * [DEV-4036] using glob
    
    * [DEV-4036] updating JSDocs sytnax
    
    * [DEV-4036] updating comments for jon
    
    * [DEV-4036] adds return to JSDOC comment
    
    * [DEV-4036] removing param specifying env [prod,dev]

---
+++ scripts/buildSitemap.js
@@ -0,0 +1,169 @@
+const fs = require('fs');
+const axios = require('axios');
+const path = require('path');
+
+const pages = require('./pages');
+
+const siteUrl = 'https://www.usaspending.gov';
+const xmlStart = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
+
+const sitemapsWritten = [];
+const xmlEnd = `</urlset>`;
+const forbiddenChars = ['&', "'", '"', '<', '>'];
+
+/**
+ * @param {string} xml string of xml, previous xml entries from current sitemap
+ * @param {Array.object} pageData data w/ the url param needed to the page
+ * @param {object} pageInfo object with 4 properties of interest
+    * * @param {string} accessor how to access the url param on the pageData obj
+    * * @param {string} clientRoute client side route for page
+    * * @param {string} priority how important the page is on scale of 0.1 - 0.9
+    * * @param {string} updatedFrequency how frequently google should crawl this page
+* @returns {string} looks like this <url><loc>https://www.usaspending.gov/#/award/CONT_IDV_GS35F0045K_4730</loc><changefreq>weekly</changefreq><priority>1</priority></url>
+ */
+const createSitemapEntry = (xml, pageData, pageInfo) => {
+    if (!pageData) return '';
+    const { accessor, clientRoute, priority, updatedFrequency } = pageInfo;
+    return pageData
+        .map((page) => ({ value: page[accessor], name: page.name }))
+        .filter((page) => {
+            if (typeof page.value === 'string') {
+                return page.value
+                    .split('')
+                    // id inside url can't contain one of these characters cause they have to be entity-escaped
+                    .some((letter) => forbiddenChars.indexOf(letter) === -1);
+            }
+            return true;
+        })
+        .reduce((str, page) => {
+            if (page.name === 'award') {
+                return `${str}<url><loc>${clientRoute}/${encodeURI(page.value)}</loc><changefreq>${updatedFrequency}</changefreq><priority>${priority}</priority></url>`;
+            }
+            return `${str}<url><loc>${clientRoute}/${page.value}</loc><changefreq>${updatedFrequency}</changefreq><priority>${priority}</priority></url>`;
+        }, xml);
+};
+
+const createRobots = () => {
+    fs.writeFile(
+        path.resolve(__dirname, `../robots.txt`),
+        `User-agent: * \nAllow: /\n\nSitemap: ${siteUrl}/sitemap.xml`,
+        () => console.log("robots.txt successfully created!")
+    );
+};
+
+const createSitemap = (xmlRoutes, siteMapName = 'sitemap') => {
+    fs.writeFile(
+        path.resolve(__dirname, `../${siteMapName}.xml`),
+        `${xmlStart}${xmlRoutes}${xmlEnd}`,
+        () => console.log(`Sitemap ${siteMapName}.xml successfully created!`)
+    );
+
+    sitemapsWritten.push(siteMapName);
+};
+
+const handleApiResponse = (resp) => {
+    // flatten massive arrays returned from Promise.all();
+    if (Array.isArray(resp)) {
+        return resp.reduce((acc, result) => {
+            return [
+                ...acc,
+                ...result.data.results
+            ];
+        }, []);
+    }
+    // return array in results/data namespace (res.data is for the states api response :shrug:)
+    return resp.data.results || resp.data;
+};
+
+const buildIndividualSitemaps = () => {
+    // 1. get routes from router
+    let staticRoutesXml = '';
+    pages
+        .find((page) => page.name === 'static-routes').routes
+        .filter((route) => route.addToSitemap)
+        .map((route) => ({
+            ...route,
+            clientRoute: `https://www.usaspending.gov/#${route.path}`,
+            accessor: ''
+        }))
+        .forEach((route) => {
+            staticRoutesXml = `${staticRoutesXml}<url><loc>${route.clientRoute}</loc></url>`;
+        });
+
+    createSitemap(staticRoutesXml, 'static');
+
+    // 2. get routes from api (w/ ids)
+    const asyncPages = pages
+        .filter((page) => page.isAsync || Array.isArray(page))
+        .reduce((previousPromise, page, i, arr) => previousPromise
+            .then((resp) => {
+                if (resp !== 'first') {
+                    let xml = '';
+                    // use previous item in array as the source of truth for traversing the previousPromises' response
+                    const responseContext = Array.isArray(arr[i - 1]) ? arr[i - 1][0] : arr[i - 1];
+                    const results = handleApiResponse(resp);
+                    xml = createSitemapEntry(xml, results, responseContext);
+                    createSitemap(xml, responseContext.name);
+                }
+                if (Array.isArray(page)) {
+                    const nestedPages = page.map((nestedPage) => axios({
+                        method: nestedPage.method,
+                        data: nestedPage.requestObject || null,
+                        url: nestedPage.url,
+                        headers: { 'X-Requested-With': 'USASpendingFrontend' }
+                    }));
+                    return Promise.all([...nestedPages]);
+                }
+                return axios({
+                    method: page.method,
+                    data: page.requestObject || null,
+                    url: page.url,
+                    headers: { 'X-Requested-With': 'USASpendingFrontend' }
+                });
+            })
+            .catch((e) => console.log("error", e))
+            , Promise.resolve('first'));
+
+    return asyncPages
+        .then((resp) => {
+            // Once the final promise resolves, we can add it to our xml string and then write the file.
+            const results = handleApiResponse(resp);
+            let xml = '';
+
+            const responseIndex = pages.length - 1;
+
+            const responseContext = Array.isArray(pages[responseIndex])
+                ? pages[responseIndex][0]
+                : pages[responseIndex];
+
+            xml = createSitemapEntry(xml, results, responseContext);
+            createSitemap(xml, responseContext.name);
+            return Promise.resolve(sitemapsWritten);
+        })
+        .catch((e) => {
+            console.log(`error on sitemap ${e}`);
+            return Promise.resolve(sitemapsWritten);
+        });
+};
+
+const buildIndexedSitemap = (individualSiteMaps) => {
+    const env = process.argv[2];
+    const xml = individualSiteMaps
+        .reduce((acc, pageName) => {
+            return (
+                `${acc}<sitemap><loc>${siteUrl[env]}/${pageName}.xml</loc></sitemap>`
+            );
+        }, '');
+
+    createSitemap(xml);
+};
+
+buildIndividualSitemaps()
+    .then((individalSiteMaps) => {
+        buildIndexedSitemap(individalSiteMaps);
+        createRobots();
+    })
+    .catch((e) => {
+        console.log(`error build site maps: ${e}`);
+    });
