e0fb2365f797d61450a9d10ccd1d14d91fb0424a
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2020-01-21 14:06:33 -0500

    [DEV-3194] refactoring FY picker to use new datamapping

--- src/js/dataMapping/recipients/fiscalYearDropdown.js
+++ src/js/dataMapping/recipients/fiscalYearDropdown.js
@@ -8,23 +8,35 @@ import { currentFiscalYear, earliestFiscalYear } from "helpers/fiscalYearHelper"
 
 const earliestFY = moment(`10-01-${earliestFiscalYear}`);
 const currentFY = moment(`10-01-${currentFiscalYear()}`);
+const dropdownLabels = ['Trailing 12 Months', 'All Fiscal Years'];
 
 // eslint-disable-next-line import/prefer-default-export
-export const dropdownOptions = () => {
-    const options = ['Trailing 12 Months', 'All Fiscal Years'];
+export const getDropdownLabelsByApiValue = () => {
     const yearsSinceEarliestFY = currentFY.diff(earliestFY, 'y');
 
     for (let i = 0; i < yearsSinceEarliestFY; i++) {
         const nextFY = earliestFiscalYear + i;
-        options.push(`FY ${nextFY}`);
+        dropdownLabels.push(`FY ${nextFY}`);
     }
 
-    return options;
-};
+    return dropdownLabels
+        .reduce((acc, label) => {
+            if (label === 'Trailing 12 Months') {
+                return {
+                    ...acc,
+                    latest: label
+                };
+            }
+            else if (label === 'All Fiscal Years') {
+                return {
+                    ...acc,
+                    all: label
+                };
+            }
 
-export const urlSelections = dropdownOptions()
-    .map((option) => option
-        .split(" ")
-        .map((word) => word.toLowerCase())
-        .join("_")
-    );
+            return {
+                ...acc,
+                [label.split(" ")[1]]: label
+            };
+        }, {});
+};
