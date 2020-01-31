cb7b495226a5880be0aeed868ae38ce976c62bda
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2020-01-31 15:49:45 -0500

    [DEV-4373] + v2 route & container

---
+++ src/js/containers/agency/AgencyContainerV2.jsx
@@ -0,0 +1,62 @@
+/**
+ * AgencyPage.jsx
+ * Created by Maxwell Kendall 01/31/2020
+ */
+
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import { agencyPageMetaTags } from 'helpers/metaTagHelper';
+
+import { setAgencyOverview, resetAgency } from "../../redux/actions/agency/agencyActions";
+
+import MetaTags from 'components/sharedComponents/metaTags/MetaTags';
+import Header from 'components/sharedComponents/header/Header';
+import StickyHeader from 'components/sharedComponents/stickyHeader/StickyHeader';
+import Footer from 'components/sharedComponents/Footer';
+import { LoadingWrapper } from 'components/sharedComponents/Loading';
+
+require('pages/agency/agencyPage.scss');
+
+const AgencyProfileV2 = ({
+    agencyOverview,
+    agencyId,
+    resetAgency,
+    setAgencyOverview
+}) => {
+    return (
+        <div className="usa-da-agency-page">
+            <MetaTags {...agencyPageMetaTags} />
+            <Header />
+            <StickyHeader>
+                <div className="sticky-header__title">
+                    <h1 tabIndex={-1} id="main-focus">
+                        Agency Profile
+                    </h1>
+                </div>
+            </StickyHeader>
+            <LoadingWrapper>
+                <main id="main-content" className="main-content">
+                    {agencyOverview.name}
+                </main>
+            </LoadingWrapper>
+            <Footer />
+        </div>
+    );
+};
+
+const mapStateToProps = (state) => ({
+    agencyOverview: state.agency.overview,
+    agencyId: state.agency.id
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    resetAgency: () => dispatch(resetAgency()),
+    setAgencyOverview: (agency) => dispatch(setAgencyOverview(agency))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(AgencyProfileV2);
+
+// export default AgencyProfileV2;
+
