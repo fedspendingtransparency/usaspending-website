0d8bb4d57a389c9b413dae0a6d88e2cda93660ee
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2020-02-05 18:03:50 -0500

    [DEV-4386] Sidebar to Functional Component pt 1
    ... this is a big change.
    
    - I've tested a lot. It's responsive and works well
    - Main change, now using reusable hook for determining what was
    the "FloatPoint"

--- src/js/components/sharedComponents/sidebar/Sidebar.jsx
+++ src/js/components/sharedComponents/sidebar/Sidebar.jsx
@@ -3,77 +3,76 @@
  * Created by Kevin Li 6/8/17
  */
 
-import React from 'react';
+import React, { useEffect, useState, createRef } from 'react';
 import PropTypes from 'prop-types';
 import { throttle } from 'lodash';
 
 import FYPicker from 'components/state/RecipientFYPicker';
 import SidebarLink from './SidebarLink';
+import { useDynamicStickyClass } from '../stickyHeader/StickyHeader';
 
 const propTypes = {
     active: PropTypes.string,
     pageName: PropTypes.string,
     sections: PropTypes.array,
     jumpToSection: PropTypes.func,
-    stickyHeaderHeight: PropTypes.number,
     fyPicker: PropTypes.bool,
     selectedFy: PropTypes.string,
     pickedYear: PropTypes.func,
     detectActiveSection: PropTypes.oneOfType([PropTypes.bool, PropTypes.func])
 };
 
-const defaultProps = {
-    detectActiveSection: false
-};
 const defaultSectionOffsets = { stickyVerticalOffset: 0 };
-export default class Sidebar extends React.Component {
-    constructor(props) {
-        super(props);
+const referenceDiv = createRef();
+const div = createRef();
 
-        this.state = {
-            shouldFloat: false,
-            sidebarWidth: 0,
-            floatPoint: 0,
-            sectionPositions: [],
-            window: {
-                height: 0
+const Sidebar = ({
+    active,
+    pageName,
+    sections,
+    jumpToSection,
+    fyPicker,
+    selectedFy,
+    pickedYear,
+    detectActiveSection = false,
+    fixedStickyBreakpoint = null
+}) => {
+    // yPosition, in px, of sections referenced in sidebar
+    const [sectionPositions, setSectionPositions] = useState([]);
+    const [sidebarWidth, setSidebarWidth] = useState("auto");
+    const [isSidebarSticky, , , handleScroll] = useDynamicStickyClass(referenceDiv, fixedStickyBreakpoint);
+
+    useEffect(() => {
+        const updateSidebarWidth = throttle(() => {
+            console.log("refs div, refDiv", div.current.offsetWidth, referenceDiv.current.offsetWidth);
+            if (isSidebarSticky && sidebarWidth !== referenceDiv.current.offsetWidth) {
+                setSidebarWidth(`${referenceDiv.current.offsetWidth}px`);
             }
-        };
+            else if (!isSidebarSticky && sidebarWidth !== div.current.offsetWidth) {
+                // setSidebarWidth(`${div.current.offsetWidth}px`);
+                setSidebarWidth(`auto`);
+            }
+        }, 100);
+        updateSidebarWidth();
+        window.addEventListener('resize', updateSidebarWidth);
 
-        this.lastY = null;
-        this.floatSideBarVertical = throttle(this.floatSideBarVertical.bind(this), 16);
-        this.measureSidebarWidth = this.measureSidebarWidth.bind(this);
-        this.highlightCurrentSection = throttle(this.highlightCurrentSection.bind(this), 100);
-        this.cacheSectionPositions = throttle(this.cacheSectionPositions.bind(this), 100);
-    }
-
-    componentDidMount() {
-        this.measureSidebarWidth();
-        if (this.props.detectActiveSection) {
-            this.cacheSectionPositions();
+        return () => {
+            window.removeEventListener('resize', updateSidebarWidth);
         }
-        window.addEventListener('scroll', this.floatSideBarVertical);
-        window.addEventListener('resize', this.measureSidebarWidth);
-    }
+    }, [sidebarWidth, setSidebarWidth, isSidebarSticky]);
 
-    componentWillUnmount() {
-        window.removeEventListener('scroll', this.floatSideBarVertical);
-        window.removeEventListener('resize', this.measureSidebarWidth);
-    }
-
-    cacheSectionPositions() {
-        // it is expensive to measure the DOM elements on every scroll, so measure them upfront
-        // (and when the window resizes) and cache the values
-        const sectionPositions = this.props.sections
+    const cacheSectionPositions = throttle(() => {
+        // Measure section positions on windowResize and first render
+        const newSectionPositions = sections
             .map((section) => ({ ...defaultSectionOffsets, ...section }))
             .map((section) => {
                 const sectionCode = section.section;
-                const domElement = document.getElementById(`${this.props.pageName}-${sectionCode}`);
+                const domElement = document.getElementById(`${pageName}-${sectionCode}`);
                 if (!domElement) {
                     // couldn't find the element
                     return null;
                 }
-                // Subtracting height of element's w/ fixed positioning
+                // Subtracting summed height of elements w/ fixed positioning
                 const topPos = domElement.offsetTop - section.stickyVerticalOffset;
                 const bottomPos = (domElement.offsetHeight + topPos) - section.stickyVerticalOffset;
 
@@ -84,54 +83,15 @@ export default class Sidebar extends React.Component {
                 };
             });
 
-        const windowHeight = window.innerHeight
-            || document.documentElement.clientHeight || document.body.clientHeight;
+        setSectionPositions(newSectionPositions);
+    }, 100);
 
-        this.setState({
-            sectionPositions,
-            window: {
-                height: windowHeight
-            }
-        });
-    }
-
-    measureSidebarWidth() {
-        // measure the reference div
-        let targetElement = this.referenceDiv;
-        if (!this.state.shouldFloat) {
-            // sidebar isn't floating, so measure that instead
-            targetElement = this.div;
-        }
-
-        const width = targetElement.offsetWidth;
-        // also measure the Y position at which to float the sidebar
-        // Subtract the height of the absolutely-positioned sticky header
-        const floatPoint = targetElement.offsetTop - 30 - this.props.stickyHeaderHeight;
-
-        this.setState({
-            floatPoint,
-            sidebarWidth: width
-        }, () => {
-            if (this.state.shouldFloat) {
-                // when the sidebar floats it calls outside of the grid layout, so
-                // programmatically force it to the correct width
-                this.div.style.width = `${width}px`;
-            }
-            else {
-                this.div.style.width = 'auto';
-            }
-            if (this.props.detectActiveSection) {
-                this.cacheSectionPositions();
-            }
-        });
-    }
-
-    highlightCurrentSection() {
+    const highlightCurrentSection = throttle(() => {
         const windowTop = window.pageYOffset || document.documentElement.scrollTop;
-        const windowBottom = windowTop + this.state.window.height;
+        const windowBottom = windowTop + window.innerHeight;
 
         // determine the section to highlight
-        let activeSection = this.props.active;
+        let nextActiveSection = active;
         let bottomSectionVisible = false;
         const visibleSections = [];
 
@@ -140,10 +100,10 @@ export default class Sidebar extends React.Component {
         const visibleTop = windowTop + edgeMargin;
         const visibleBottom = windowBottom - edgeMargin;
 
-        this.state.sectionPositions.forEach((section, index) => {
-            // check if the section is in view at all
+        sectionPositions.forEach((section, index) => {
+            // 1. check if the section is in view
             if (section.top <= visibleBottom && section.bottom >= visibleTop) {
-                // at least some of the section is in view, determine how much
+                // 2. get % of section in view
                 const height = section.bottom - section.top;
                 const visibleHeight = Math.min(section.bottom, visibleBottom) -
                     Math.max(visibleTop, section.top);
@@ -153,12 +113,12 @@ export default class Sidebar extends React.Component {
                     amount: percentageVisible
                 });
 
-                if (index === this.state.sectionPositions.length - 1) {
+                if (index === sectionPositions.length - 1) {
                     // this is the last section and it is visible
                     bottomSectionVisible = true;
                 }
             }
-            else if (index === this.state.sectionPositions.length - 1) {
+            else if (index === sectionPositions.length - 1) {
                 // this is the last section, so highlight it if we're at the bottom or lower
                 // on the page
                 if (section.top <= visibleTop) {
@@ -174,11 +134,11 @@ export default class Sidebar extends React.Component {
 
         // select the first section we saw
         if (visibleSections.length > 0) {
-            activeSection = visibleSections[0].section;
+            nextActiveSection = visibleSections[0].section;
             if (visibleSections[0].amount < 0.50 && visibleSections.length > 1) {
                 // less than 15% of the first section is visible and we have more than 1 section,
                 // select the next section
-                activeSection = visibleSections[1].section;
+                nextActiveSection = visibleSections[1].section;
             }
         }
 
@@ -191,113 +151,89 @@ export default class Sidebar extends React.Component {
             if (previousSection.amount < 0.5 && bottomSection.amount === 1) {
                 // less than half of the previous section is visible and all of the bottom section
                 // is visible, select the bottom section
-                activeSection = bottomSection.section;
+                nextActiveSection = bottomSection.section;
             }
         }
 
-        if (activeSection === this.state.activeSection) {
+        if (nextActiveSection === active) {
             // no change
             return;
         }
-        this.props.detectActiveSection(activeSection);
-    }
+        detectActiveSection(nextActiveSection);
+    }, 100);
 
-    floatSideBarVertical() {
-        // check where the sidebar is in the viewport
-        if (!this.div || !this.referenceDiv) {
-            // the DOM element is missing!
-            return;
+    useEffect(() => {
+        if (detectActiveSection && sectionPositions.length === 0) {
+            cacheSectionPositions();
         }
 
-        let shouldFloat = false;
-        const yPos = window.scrollY || window.pageYOffset;
-        if (yPos > this.state.floatPoint) {
-            // it needs to float because the sidebar is less than 60px from the top or out of view
-            shouldFloat = true;
-        }
+        const handleScrollAndSetActiveSection = () => {
+            handleScroll();
+            if (detectActiveSection) highlightCurrentSection();
+        };
 
-        if (shouldFloat !== this.state.shouldFloat) {
-            this.setState({
-                shouldFloat
-            }, () => {
-                if (shouldFloat) {
-                    // when the sidebar floats it calls outside of the grid layout, so
-                    // programmatically force it to the correct width
-                    this.div.style.width = `${this.state.sidebarWidth}px`;
-                }
-            });
-        }
+        window.addEventListener('scroll', handleScrollAndSetActiveSection);
+        window.addEventListener('resize', cacheSectionPositions);
 
-        if (this.props.detectActiveSection) {
-            this.highlightCurrentSection();
-        }
-    }
+        return () => {
+            window.removeEventListener('scroll', handleScrollAndSetActiveSection);
+            window.removeEventListener('resize', cacheSectionPositions);
+        };
+    }, [
+        detectActiveSection,
+        cacheSectionPositions,
+        highlightCurrentSection,
+        handleScroll,
+        sectionPositions.length
+    ]);
 
-    render() {
-        const items = this.props.sections.map((section) => {
-            let link = (
-                <SidebarLink
-                    section={section.section}
-                    label={section.label}
-                    active={this.props.active}
-                    onClick={this.props.jumpToSection} />
-            );
-            if (section.url) {
-                const active = this.props.active === section.section ? 'active' : '';
-                link = (
-                    <a
-                        className={`sidebar-link ${active}`}
-                        href={section.url}>
-                        {section.label}
-                    </a>
-                );
-            }
-            return (
-                <li key={section.section}>
-                    {link}
-                </li>
-            );
-        });
-
-        let floatSidebar = '';
-        if (this.state.shouldFloat) {
-            floatSidebar = 'float-sidebar';
-        }
-
-        let fyPicker = null;
-        if (this.props.fyPicker) {
-            fyPicker = (
-                <FYPicker
-                    selectedFy={this.props.selectedFy}
-                    pickedYear={this.props.pickedYear} />
-            );
-        }
-
-        return (
-            <div>
-                <div
-                    className={`${this.props.pageName}-sidebar-reference ${floatSidebar}`}
-                    ref={(div) => {
-                        // this is an empty div that does not float with the page so we can track
-                        // the inline/non-floating Y position of the sidebar
-                        this.referenceDiv = div;
-                    }}>
-                    &nbsp;
-                </div>
-                <div
-                    className={`${this.props.pageName}-sidebar-content ${floatSidebar}`}
-                    ref={(div) => {
-                        this.div = div;
-                    }}>
-                    {fyPicker}
-                    <ul>
-                        { items }
-                    </ul>
-                </div>
-            </div>
+    const buildItems = (section) => {
+        let link = (
+            <SidebarLink
+                section={section.section}
+                label={section.label}
+                active={active}
+                onClick={jumpToSection} />
         );
-    }
-}
+        if (section.url) {
+            const activeClass = active === section.section ? 'active' : '';
+            link = (
+                <a
+                    className={`sidebar-link ${activeClass}`}
+                    href={section.url}>
+                    {section.label}
+                </a>
+            );
+        }
+        return (
+            <li key={section.section}>
+                {link}
+            </li>
+        );
+    };
 
-Sidebar.defaultProps = defaultProps;
+    const floatSidebar = isSidebarSticky
+        ? 'float-sidebar'
+        : '';
+    return (
+        <>
+            <div className={`${pageName}-sidebar-reference ${floatSidebar}`} ref={referenceDiv}>
+                &nbsp;
+            </div>
+            <div ref={div} className={`${pageName}-sidebar-content ${floatSidebar}`} style={{ width: sidebarWidth }}>
+                {fyPicker && (
+                    <FYPicker
+                        selectedFy={selectedFy}
+                        pickedYear={pickedYear} />
+                )}
+                <ul>
+                    {sections.map(buildItems)}
+                </ul>
+            </div>
+        </>
+    );
+};
+
 Sidebar.propTypes = propTypes;
+
+export default Sidebar;
