b4e73755adf721dfd3d2ea4fb9d44ca9564301b3
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2020-02-20 11:26:32 -0500

    [DEV-4211] adding hover styles

--- src/js/components/glossary/definition/GlossaryDefinition.jsx
+++ src/js/components/glossary/definition/GlossaryDefinition.jsx
@@ -131,7 +131,7 @@ export default class GlossaryDefinition extends React.Component {
                     dropdownDirection="left"
                     backgroundColor="#215493"
                     selectedOption="twitter">
-                    <FontAwesomeIcon icon="share-alt" color="white" size="lg" />
+                    <FontAwesomeIcon className="glossary-share-icon" icon="share-alt" color="#e2e2e2" size="lg" />
                 </Picker>
                 <span className={`copy-confirmation ${this.state.showCopiedConfirmation ? '' : 'hide'}` }><FontAwesomeIcon icon="check-circle" color="#3A8250" /> Copied</span>
                 <ItemDefinition
