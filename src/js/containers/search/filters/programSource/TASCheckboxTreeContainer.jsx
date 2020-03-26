be2fc7a2d7922e8b53578ab1ef9c1932ed3bcbfb
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2020-03-26 12:20:42 -0400

    [DEV-4274] Dark release logic

---
+++ src/js/containers/search/filters/programSource/TASCheckboxTreeContainer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import CheckboxTree from 'components/sharedComponents/CheckboxTree';
+
+const propTypes = {};
+
+const TASCheckboxTree = (props) => {
+    return (
+        // <CheckboxTree />
+        "YO"
+    );
+};
+
+export default TASCheckboxTree;
