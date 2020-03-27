ed0786c83cac75d32b1bf9abfbd2b78a97877f15
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2020-03-27 16:55:27 -0400

    [DEV-4274] polishing styles

--- src/js/containers/search/filters/programSource/TASCheckboxTreeContainer.jsx
+++ src/js/containers/search/filters/programSource/TASCheckboxTreeContainer.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { debounce } from 'lodash';
 
 import { cleanTasData } from 'helpers/checkboxTreeHelper';
 import { fetchTas } from 'helpers/searchHelper';
+
 import CheckboxTree from 'components/sharedComponents/CheckboxTree';
+import { EntityDropdownAutocomplete } from 'components/search/filters/location/EntityDropdownAutocomplete';
 
 const propTypes = {};
 
@@ -14,42 +17,87 @@ export default class TASCheckboxTree extends React.Component {
             checked: [],
             expanded: [],
             nodes: [],
-            isLoading: true
+            isLoading: true,
+            searchString: ''
         };
-        this.fetchTas = null;
+        this.request = null;
     }
 
     componentDidMount() {
-        this.fetchTas = fetchTas('', 0);
-        return this.fetchTas.promise
+        this.fetchTas('');
+    }
+
+    onExpand = () => console.log("expand");
+
+    onCheck = () => console.log("check");
+
+    onUncheck = () => console.log("uncheck");
+
+    onSearchChange = debounce(() => {
+        if (!this.state.searchString) return this.onClear();
+        return this.setState({ requestType: 'search' }, this.fetchNAICS);
+    }, 500);
+
+    onClear = () => {
+        if (this.request) this.request.cancel();
+        this.setState({
+            isSearch: false,
+            searchString: '',
+            isLoading: false,
+            requestType: ''
+        });
+    }
+
+    fetchTas = (id = '', depth = 0) => {
+        if (this.request) this.request.cancel();
+        this.request = fetchTas(id, depth);
+        return this.request.promise
             .then(({ data }) => {
                 const nodes = cleanTasData(data.results);
                 this.setState({ nodes, isLoading: false });
             });
     }
 
-    onExpand = () => console.log("expand")
-
-    onCheck = () => console.log("check")
-
-    onUncheck = () => console.log("uncheck")
+    handleTextInputChange = (e) => {
+        const text = e.target.value;
+        if (!text) {
+            return this.onClear();
+        }
+        return this.setState({
+            searchString: text,
+            isSearch: true,
+            isLoading: true
+        }, this.onSearchChange);
+    }
 
     render() {
         const {
             nodes,
             checked,
             expanded,
-            isLoading
+            isLoading,
+            searchString
         } = this.state;
         return (
-            <CheckboxTree
-                isLoading={isLoading}
-                data={nodes}
-                checked={checked}
-                expanded={expanded}
-                onUncheck={this.onUncheck}
-                onCheck={this.onCheck}
-                onExpand={this.onExpand} />
+            <div className="tas-checkbox">
+                <EntityDropdownAutocomplete
+                    enabled
+                    isClearable
+                    placeholder="Type to filter results"
+                    searchString={searchString}
+                    handleTextInputChange={this.handleTextInputChange}
+                    context={{}}
+                    loading={false}
+                    onClear={this.onClear} />
+                <CheckboxTree
+                    isLoading={isLoading}
+                    data={nodes}
+                    checked={checked}
+                    expanded={expanded}
+                    onUncheck={this.onUncheck}
+                    onCheck={this.onCheck}
+                    onExpand={this.onExpand} />
+            </div>
         );
     }
 }
