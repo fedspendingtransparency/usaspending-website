34cc37dd1607dc0091c9714e002ad03f0f296a41
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2020-03-27 16:57:13 -0400

    [DEV-4274] updating mock data

--- tests/containers/search/filters/programSource/mockTas.js
+++ tests/containers/search/filters/programSource/mockTas.js
@@ -2,70 +2,70 @@ export default {
     results: [
         {
             "id": "1",
-            "description": "Treasury Account of Donald Trump",
+            "description": "Treasury Account of XYZ",
             "ancestors": [],
             "count": "100",
             "children": null
         },
         {
             "id": "2",
-            "description": "Treasury Account of Michael Jackson",
+            "description": "Treasury Account of ABC",
             "ancestors": [],
             "count": "92",
             "children": null
         },
         {
             "id": "3",
-            "description": "Treasury Account of Hillary Clinton",
+            "description": "Treasury Account of EDF",
             "ancestors": [],
             "count": "500",
             "children": null
         },
         {
             "id": "4",
-            "description": "Treasury Account of Arnold Schwartzenager",
+            "description": "Treasury Account of HIJK",
             "ancestors": [],
             "count": "200",
             "children": null
         },
         {
             "id": "5",
-            "description": "Treasury Account of Jon Hill",
+            "description": "Treasury Account of LMNOP",
             "ancestors": [],
             "count": "23",
             "children": null
         },
         {
             "id": "6",
-            "description": "Treasury Account of Elton John",
+            "description": "Treasury Account of QRS",
             "ancestors": [],
             "count": "11",
             "children": null
         },
         {
             "id": "7",
-            "description": "Treasury Account of the Pop Group Hanson",
+            "description": "Treasury Account of TUV",
             "ancestors": [],
             "count": "55",
             "children": null
         },
         {
             "id": "8",
-            "description": "Treasury Account of Nickle Back",
+            "description": "Treasury Account of W",
             "ancestors": [],
             "count": "44",
             "children": null
         },
         {
             "id": "9",
-            "description": "Treasury Account of Marco Mendoza",
+            "description": "Treasury Account of 123",
             "ancestors": [],
             "count": "22",
             "children": null
         },
         {
             "id": "10",
-            "description": "Treasury Account of XYZ",
+            "description": "Treasury Account of 456",
             "ancestors": [],
             "count": "3",
             "children": null
