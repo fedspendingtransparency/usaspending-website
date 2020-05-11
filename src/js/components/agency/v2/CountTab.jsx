65c39ec3ab3931bda1038ae9bc4fa9c5f334dc9a
Lizzie Salita <salita_elizabeth@bah.com>
2020-05-11 17:39:18 -0400

    make an API request for each tab

--- src/js/components/agency/v2/CountTab.jsx
+++ src/js/components/agency/v2/CountTab.jsx
@@ -3,8 +3,9 @@
  * Created by Lizzie Salita 5/8/20
  */
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { fetchSpendingCount } from 'helpers/agencyHelper';
 
 const propTypes = {
     fy: PropTypes.string.isRequired,
@@ -19,6 +20,31 @@ const propTypes = {
 const CountTab = (props) => {
     const [count, setCount] = useState(null);
     const [subCount, setSubCount] = useState(null);
+
+    useEffect(() => {
+        const countRequest = fetchSpendingCount(props.agencyId, props.fy, props.id);
+        countRequest.promise
+            .then((res) => {
+                switch (props.id) {
+                    case 'budget_function':
+                        setCount(res.data.budget_function_count);
+                        setSubCount(res.data.budget_sub_function_count);
+                        break;
+                    case 'program_activity':
+                        setCount(res.data.program_activity_count);
+                        break;
+                    case 'object_class':
+                        setCount(res.data.object_class_count);
+                        break;
+                    case 'federal_account':
+                        setCount(res.data.federal_account_count);
+                        setSubCount(res.data.treasury_account_count);
+                        break;
+                    default:
+                        // do nothing
+                }
+            });
+    }, [props.id, props.fy, props.agencyId]);
     return (
         <button
             className={`count-tabs__button${props.active ? ' count-tabs__button_active' : ''}`}
