2257396b56652fe144198045e42e780b1e635fb7
Lizzie Salita <ebdabbs@users.noreply.github.com>
2020-05-15 10:15:26 -0400

    DEV-4938 agency table tabs (#1667)
    * initial account spending section
    
    * added questions above the tabs
    
    * tab styling
    
    * make an API request for each tab
    
    * made the CountTab UI component more reusable
    
    * Reset counts when making a new request; fix IE bug
    
    * Fixed test import statement
    
    * Demo / design review feedback
    
    * fixed console error; code review feedback

---
+++ src/js/components/agency/v2/CountTab.jsx
@@ -0,0 +1,40 @@
+/**
+ * CountTab.jsx
+ * Created by Lizzie Salita 5/8/20
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {
+    type: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    subHeading: PropTypes.string,
+    setActiveTab: PropTypes.func.isRequired,
+    active: PropTypes.bool,
+    count: PropTypes.number,
+    subCount: PropTypes.number
+};
+
+const CountTab = (props) => (
+    <button
+        className={`count-tabs__button${props.active ? ' count-tabs__button_active' : ''}`}
+        onClick={() => props.setActiveTab(props.type)}>
+        <div className="count-button">
+            <div className="count-button__label">
+                {props.label}
+            </div>
+            <div className="count-button__count">
+                {(props.count || props.count === 0) ? `${props.count}` : '--'}
+            </div>
+            {props.subHeading ? (
+                <div className="count-button__sub-heading">
+                    With {(props.subCount || props.subCount === 0) ? `${props.subCount}` : '--'} {props.subHeading}
+                </div>
+            ) : <>&nbsp;</>}
+        </div>
+    </button>
+);
+
+CountTab.propTypes = propTypes;
+export default CountTab;
--- src/js/containers/agency/v2/AgencyContainerV2.jsx
+++ src/js/containers/agency/v2/AgencyContainerV2.jsx
@@ -30,6 +30,8 @@ import Footer from 'containers/Footer';
 import { LoadingWrapper } from 'components/sharedComponents/Loading';
 import { defaultSortFy } from 'components/sharedComponents/pickers/FYPicker';
 
+import AccountSpending from 'components/agency/v2/accountSpending/AccountSpending';
+
 require('pages/agency/v2/index.scss');
 
 // document.querySelector('.site-navigation').offsetHeight + document.querySelector('.site-navigation').offsetTop
@@ -43,7 +45,7 @@ const TooltipComponent = () => (
 );
 
 // eslint-disable-next-line react/prop-types
-const ComingSoonSection = ({ section, icon = "chart-area" }) => (
+const AgencySection = ({ section, icon = "chart-area", children }) => (
     <section id={`agency-v2-${snakeCase(section)}`} className={`body__section ${snakeCase(section)}`}>
         <div className="body__header">
             <div className="body__header-icon">
@@ -56,21 +58,16 @@ const ComingSoonSection = ({ section, icon = "chart-area" }) => (
                 tooltipComponent={<TooltipComponent />} />
         </div>
         <hr />
-        <div className="coming-soon-section">
-            <h4>Coming Soon</h4>
-            <p>This feature is currently under development.</p>
-        </div>
+        {children}
     </section>
 );
 
-const componentByAgencySection = {
-    overview: <ComingSoonSection section="overview" />,
-    account_spending: <ComingSoonSection section="account_spending" />,
-    award_spending: <ComingSoonSection section="award_spending" />,
-    sub_agency_spending: <ComingSoonSection section="sub-agency_spending" />,
-    award_recipients: <ComingSoonSection section="award_recipients" />,
-    top_5_award_dimensions: <ComingSoonSection section="top_5_award_dimensions" />
-};
+const ComingSoon = () => (
+    <div className="coming-soon-section">
+        <h4>Coming Soon</h4>
+        <p>This feature is currently under development.</p>
+    </div>
+);
 
 let timeout;
 
@@ -98,6 +95,15 @@ export const AgencyProfileV2 = ({
         timeout = window.setTimeout(() => {
             setConfirmationText(false);
         }, 1750);
+    };
+
+    const componentByAgencySection = {
+        overview: <ComingSoon />,
+        account_spending: <AccountSpending fy={`${selectedFy}`} agencyId={params.agencyId} />,
+        award_spending: <ComingSoon />,
+        sub_agency_spending: <ComingSoon />,
+        award_recipients: <ComingSoon />,
+        top_5_award_dimensions: <ComingSoon />
     };
 
     const jumpToSection = (section = '') => {
@@ -131,7 +137,7 @@ export const AgencyProfileV2 = ({
         .map((year) => {
             const onClickHandler = () => setSelectedFy(year);
             return {
-                name: year,
+                name: `${year}`,
                 value: year,
                 onClick: onClickHandler
             };
@@ -182,7 +188,7 @@ export const AgencyProfileV2 = ({
                             <Picker
                                 sortFn={defaultSortFy}
                                 icon={<FontAwesomeIcon icon="calendar-alt" />}
-                                selectedOption={selectedFy}
+                                selectedOption={`${selectedFy}`}
                                 options={fyOptions} />
                             <span>Fiscal Year</span>
                         </div>
@@ -228,7 +234,9 @@ export const AgencyProfileV2 = ({
                     </div>
                     <div className="body usda__flex-col">
                         {Object.keys(componentByAgencySection).map((section) => (
-                            componentByAgencySection[section]
+                            <AgencySection key={section} section={section} >
+                                {componentByAgencySection[section]}
+                            </AgencySection>
                         ))}
                     </div>
                 </main>
