fa5ac3243da58c6a35ccab077428362b6430fabf
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2020-05-18 09:59:13 -0400

    [DEV-4180] fixing test

--- tests/containers/agency/v2/AgencyContainerV2-test.jsx
+++ tests/containers/agency/v2/AgencyContainerV2-test.jsx
@@ -8,9 +8,16 @@ import { shallow } from 'enzyme';
 
 import { AgencyProfileV2 } from 'containers/agency/v2/AgencyContainerV2';
 
+const defaultProps = {
+    params: { agencyId: '123' },
+    agencyOverview: {
+        name: 'test'
+    }
+};
+
 describe('AgencyContainer', () => {
     it('should make an API call for the selected agency on mount', () => {
-        const component = shallow(<AgencyProfileV2 params={{ agencyId: '123' }} />);
+        const component = shallow(<AgencyProfileV2 {...defaultProps} />);
         expect(component.exists()).toBe(true);
     });
 });
