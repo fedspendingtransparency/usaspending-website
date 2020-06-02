f7ed2b884e9f9d41f3d1f9a845ebd94701df9a3c
James Lee <65453892+jameslee40@users.noreply.github.com>
2020-06-02 10:52:20 -0400

    Dev-4347 account spending tables (#1706)
    * Initial Table Container
    
    * added pagination and additional column mappings
    
    * Added missing Fontawesome icons
    
    * Store the agency's total obligation in Redux
    
    * added unit tests
    
    * added data model for account spending row, parsing function, and sort capability
    
    * added loading and error states
    
    * added BaseAccountSpendingRow test
    
    * added disabled prop
    
    * added pagination buttons when loading or errored
    
    * updated percentage function in base model
    
    * added styling for table and count tabs
    
    * added word-break
    
    * updated tabs to use color variables
    
    * Refactored CountTab onClick for performance reasons
    
    * added error state to AgencyContainerV2
    
    * Refactored state naming
    
    * Removed useless parenthesis
    
    * Refactored function to useCallback
    
    * Removed value from button
    
    * moved params and setLoading inside callback instead of passing params
    
    * updated data-transparency-ui version
    
    Co-authored-by: Lizzie Salita <salita_elizabeth@bah.com>
    Co-authored-by: James Lee <Lee_James@bah.com>

---
+++ tests/models/account/BaseAccountSpendingRow-test.js
@@ -0,0 +1,50 @@
+import BaseAccountSpendingRow from 'models/v2/account/BaseAccountSpendingRow';
+
+export const mockAccountSpendingRow = {
+    name: "Net Interest",
+    children: [
+        {
+            name: "Interest on Treasury debt securities (gross)",
+            obligated_amount: 269452391857.85,
+            gross_outlay_amount: 269452391857.85,
+            total_obligated_amount: 999999999999.99
+        },
+        {
+            name: "Other interest",
+            obligated_amount: 6298412029.12,
+            gross_outlay_amount: 3415362152.38,
+            total_obligated_amount: 999999999999.99
+        }
+    ],
+    obligated_amount: 275750803886.97,
+    gross_outlay_amount: 272867754010.23,
+    total_obligated_amount: 999999999999.99
+};
+
+
+const accountSpendingRow = Object.create(BaseAccountSpendingRow);
+accountSpendingRow.populate(mockAccountSpendingRow);
+
+describe('BaseAccountSpendingRow', () => {
+    it('should store the raw obligated amount', () => {
+        expect(accountSpendingRow._obligatedAmount).toEqual(275750803886.97);
+    });
+    it('should store the raw gross outlay amount', () => {
+        expect(accountSpendingRow._grossOutlayAmount).toEqual(272867754010.23);
+    });
+    it('should store the raw total obligated amount', () => {
+        expect(accountSpendingRow._totalObligatedAmount).toEqual(999999999999.99);
+    });
+    it('should store the name', () => {
+        expect(accountSpendingRow.name).toEqual('Net Interest');
+    });
+    it('should store the formatted gross outlay amount', () => {
+        expect(accountSpendingRow.grossOutlayAmount).toEqual('$272,867,754,010');
+    });
+    it('should store the formatted obligated amount', () => {
+        expect(accountSpendingRow.obligatedAmount).toEqual('$275,750,803,887');
+    });
+    it('should store the formatted percent of total obligations', () => {
+        expect(accountSpendingRow.percentOfTotalObligations).toEqual('27.58%');
+    });
+});
