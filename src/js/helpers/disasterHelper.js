dc64644c62bb72851e21f8af2a5f92b0bbed784b
Jonathan Hill <hill_jonathan@bah.com>
2020-06-11 14:45:52 -0400

    update api with disaster helper

---
+++ src/js/helpers/disasterHelper.js
@@ -0,0 +1,12 @@
+/**
+ * disasterHelper.js
+ * Created by Jonathan Hill 06/11/20
+ */
+
+import { apiRequest } from 'helpers/apiRequest';
+
+const defcAPI = () => apiRequest({
+    url: '/api/v2/references/def_codes/',
+});
+
+export default defcAPI;
