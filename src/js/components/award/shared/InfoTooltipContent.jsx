91796c39e595dddd81da26fd618dceb9527c78a2
James Lee <jamesleework40@gmail.com>
2020-06-12 13:27:39 -0400

    spacing

--- src/js/components/award/shared/InfoTooltipContent.jsx
+++ src/js/components/award/shared/InfoTooltipContent.jsx
@@ -1290,7 +1290,7 @@ export const recipientOverviewLoanInfo = (
                 What <u>is</u> included from a loan perspective in Total Awarded Amount and the other dollar figures  is the loan subsidy cost, which <u>is</u> considered actual budgetary federal spending. Subsidy cost is the calculated net present value of the loan or loan guarantee to the government, taking into account the size of the loan (face value), interest rate, and the modeled risk of the recipient failing to pay back the loan in part or full; subsidy cost can be positive (indicating that the government is likely to lose money on the loan) or negative (indicating that the government is likely to make money on the loan). Subsidy cost should never be larger in absolute value terms than the face value itself. Administrative costs of running the loan or loan guarantee program itself are excluded from subsidy cost calculations.
             </p>
             <p>
-            As stated above, all subsidy costs associated with loans this recipient has received during the filtered time period are captured within the &quot;Total Awarded Amount&quot; number above and anywhere else on this page dollar amounts are mentioned.            
+                As stated above, all subsidy costs associated with loans this recipient has received during the filtered time period are captured within the &quot;Total Awarded Amount&quot; number above and anywhere else on this page dollar amounts are mentioned.
             </p>
         </div>
     </div>
