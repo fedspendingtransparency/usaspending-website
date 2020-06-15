3c324beb99a2e2f1600539361e7d17ca4ca480a8
Lizzie Salita <salita_elizabeth@bah.com>
2020-06-15 17:28:11 -0400

    Use dynamic DEFC

--- src/js/containers/covid19/spendingOverTime/SpendingOverTimeContainer.jsx
+++ src/js/containers/covid19/spendingOverTime/SpendingOverTimeContainer.jsx
@@ -5,6 +5,7 @@
 
 import React, { useState, useEffect, useCallback } from 'react';
 import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
 import { Table, Pagination } from 'data-transparency-ui';
 import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
 import { awardTypeGroupLabels } from 'dataMapping/search/awardType';
@@ -30,6 +31,29 @@ export const parseRows = (rows, amountColumns) => (
     })
 );
 
+// Generate a column for each award type
+const columns = awardTypes.map((awardType) => (
+    {
+        displayName: `${awardTypeGroupLabels[awardType]}`,
+        title: awardType,
+        right: true // text-align right for dollar values
+    }
+));
+columns.unshift(
+    {
+        displayName: 'All Awards',
+        title: 'total',
+        right: true
+    }
+);
+// Add a column for the time period
+columns.unshift(
+    {
+        displayName: 'Month',
+        title: 'period'
+    }
+);
+
 const SpendingOverTimeContainer = ({ activeTab }) => {
     const [currentPage, changeCurrentPage] = useState(1);
     const [pageSize, changePageSize] = useState(10);
@@ -37,65 +61,43 @@ const SpendingOverTimeContainer = ({ activeTab }) => {
     const [results, setResults] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(false);
-
-    // Generate a column for each award type
-    const columns = awardTypes.map((awardType) => (
-        {
-            displayName: `${awardTypeGroupLabels[awardType]}`,
-            title: awardType,
-            right: true // text-align right for dollar values
-        }
-    ));
-    // TODO - Add a column for all awards
-    columns.unshift(
-        {
-            displayName: 'All Awards',
-            title: 'total',
-            right: true
-        }
-    );
-    // Add a column for the time period
-    columns.unshift(
-        {
-            displayName: 'Month',
-            title: 'period'
-        }
-    );
+    const defCodes = useSelector((state) => state.covid19.defCodes);
 
     const fetchSpendingOverTimeCallback = useCallback(() => {
         setLoading(true);
-        const params = {
-            filter: {
-                // TODO: remove hard-coded values after integration with v2/references/def_codes/ API
-                def_codes: ['L', 'M', 'N', 'O', 'P'],
-                fiscal_year: 2020
-            },
-            limit: pageSize,
-            page: currentPage,
-            group: 'period',
-            spending_type: activeTab
-        };
-        const request = fetchSpendingOverTime(params);
-        request.promise
-            .then((res) => {
-                const amountColumns = columns.slice(1);
-                const rows = parseRows(res.data.results, amountColumns);
-                setResults(rows);
-                setTotalItems(res.data.page_metadata.total);
-                setLoading(false);
-                setError(false);
-            }).catch((err) => {
-                setError(true);
-                setLoading(false);
-                console.error(err);
-            });
+        if (defCodes.length > 0) {
+            const params = {
+                filter: {
+                    def_codes: defCodes.map((defc) => defc.code),
+                    fiscal_year: 2020
+                },
+                limit: pageSize,
+                page: currentPage,
+                group: 'period',
+                spending_type: activeTab
+            };
+            const request = fetchSpendingOverTime(params);
+            request.promise
+                .then((res) => {
+                    const amountColumns = columns.slice(1);
+                    const rows = parseRows(res.data.results, amountColumns);
+                    setResults(rows);
+                    setTotalItems(res.data.page_metadata.total);
+                    setLoading(false);
+                    setError(false);
+                }).catch((err) => {
+                    setError(true);
+                    setLoading(false);
+                    console.error(err);
+                });
+        }
     });
 
     useEffect(() => {
         // Reset to the first page
         changeCurrentPage(1);
         fetchSpendingOverTimeCallback();
-    }, [activeTab, pageSize]);
+    }, [activeTab, pageSize, defCodes]);
 
     useEffect(() => {
         fetchSpendingOverTimeCallback();
