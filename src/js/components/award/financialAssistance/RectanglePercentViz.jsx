2a9074cb401e47797cf19bc78510236fd125d1de
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2020-06-16 15:23:13 -0400

    [DEV-5138] removing log

--- src/js/components/award/financialAssistance/RectanglePercentViz.jsx
+++ src/js/components/award/financialAssistance/RectanglePercentViz.jsx
@@ -251,7 +251,6 @@ const RectanglePercentViz = ({
                 isCaresReleased &&
                 child?.labelSortOrder > 0
             );
-            console.log("child", child.className, child, isBarAbsent);
 
             return (
                 <div className={`award-amounts-viz__desc-container ${position}${classNameForCovid}`}>
