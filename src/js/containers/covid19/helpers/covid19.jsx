48e090b923ec8df1012dea2a8c5cc4f34cbe8973
Jonathan Hill <hill_jonathan@bah.com>
2020-06-25 16:50:20 -0400

    remove code

--- src/js/containers/covid19/helpers/covid19.jsx
+++ src/js/containers/covid19/helpers/covid19.jsx
@@ -7,7 +7,6 @@ import MapContainer from 'containers/covid19/recipient/RecipientContainer';
 
 import React from 'react';
 import BudgetCategories from 'components/covid19/budgetCategories/BudgetCategories';
-// import AwardSpendingOverTime from 'components/covid19/spendingOverTime/AwardSpendingOverTime';
 import AwardQuestion from 'components/covid19/AwardQuestions';
 import SpendingByCFDA from 'components/covid19/assistanceListing/SpendingByCFDA';
 
@@ -82,14 +81,6 @@ export const componentByCovid19Section = () => ({
         showInMenu: true,
         title: 'Award Spending by Agency'
     },
-    // award_spending_over_time: {
-    //     icon: 'chart-area',
-    //     component: <AwardSpendingOverTime />,
-    //     headerText: awardSpendingText,
-    //     headerTextTooltip: awardSpedingTooltip,
-    //     showInMenu: true,
-    //     title: 'Award Spending Over Time'
-    // },
     award_spending_by_assistance_listing: {
         icon: 'plus-circle',
         component: <SpendingByCFDA />,
