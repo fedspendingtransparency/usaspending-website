cf99490a76973a1c6f92b79fe79b825c69647674
Lizzie Salita <salita_elizabeth@bah.com>
2020-07-13 13:21:00 -0400

    Fix CFDA pagination

--- src/js/containers/covid19/assistanceListing/SpendingByCFDAContainer.jsx
+++ src/js/containers/covid19/assistanceListing/SpendingByCFDAContainer.jsx
@@ -228,13 +228,23 @@ const SpendingByCFDAContainer = ({ onRedirectModalClick, activeTab }) => {
     }
 
     return (
-        <div className="table-wrapper">
-            <Table
-                columns={activeTab === 'loans' ? loanColumns : columns}
-                rows={results}
-                updateSort={updateSort}
-                currentSort={{ field: sort, direction: order }} />
-        </div>
+        <>
+            <div className="table-wrapper">
+                <Table
+                    columns={activeTab === 'loans' ? loanColumns : columns}
+                    rows={results}
+                    updateSort={updateSort}
+                    currentSort={{ field: sort, direction: order }} />
+            </div>
+            <Pagination
+                currentPage={currentPage}
+                changePage={changeCurrentPage}
+                changeLimit={changePageSize}
+                limitSelector
+                resultsText
+                pageSize={pageSize}
+                totalItems={totalItems} />
+        </>
     );
 };
 
