00a0d80fd6dc112a1799b091a649e3c6373ef522
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2020-07-21 15:43:32 -0400

    [DEV-5536] moving from h2 to p tag in the banner

--- src/js/components/sharedComponents/header/InfoBanner.jsx
+++ src/js/components/sharedComponents/header/InfoBanner.jsx
@@ -35,7 +35,7 @@ export default class InfoBanner extends React.Component {
                         <FontAwesomeIcon size="lg" icon="info-circle" />
                     </span>
                     <div className="info-banner__alert-text">
-                        <h2 className="info-banner__title-text">New to USAspending: Official COVID-19 Response Data</h2>
+                        <p className="info-banner__title-text">New to USAspending: Official COVID-19 Response Data</p>
                         <p>
                             USAspending now has official spending data from federal agencies related to the Coronavirus Aid, Relief, and Economic Security (CARES) Act and other COVID-19 response spending.
                             <button onClick={this.props.triggerModal}> Learn more</button> about the data and new features or <a href="#/disaster/covid-19">visit the profile page to explore and download the data today!</a>
