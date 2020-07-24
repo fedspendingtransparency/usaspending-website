ba565727aa421a35ca45ae0db9341a72011fa356
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2020-07-24 08:46:41 -0400

    [DEV-5640] pt 2
    Implementing the recipient section.

--- src/js/containers/covid19/recipient/SummaryInsightsContainer.jsx
+++ src/js/containers/covid19/recipient/SummaryInsightsContainer.jsx
@@ -3,9 +3,11 @@
  * Created by Jonathan Hill 07/07/20
  */
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
+import { isEqual } from 'lodash';
+
 import { awardTypeGroups, awardTypeGroupLabels } from 'dataMapping/search/awardType';
 import { fetchAwardAmounts, fetchAwardCount, fetchDisasterSpendingCount } from 'helpers/disasterHelper';
 import { useInFlightList } from 'helpers/covid19Helper';
@@ -39,14 +41,27 @@ const overviewData = [
 const initialInFlightState = overviewData.map((d) => d.type);
 
 const SummaryInsightsContainer = ({ activeFilter }) => {
+    const awardAmountRequest = useRef();
+    const awardCountRequest = useRef();
+    const recipientCountRequest = useRef();
     const [awardOutlays, setAwardOutlays] = useState(null);
     const [awardObligations, setAwardObligations] = useState(null);
     const [numberOfAwards, setNumberOfAwards] = useState(null);
     const [numberOfRecipients, setNumberOfRecipients] = useState(null);
-    const [inFlightList, addToInFlight, removeFromInFlight] = useInFlightList(initialInFlightState);
+    const [inFlightList, , removeFromInFlight, resetInFlight] = useInFlightList(initialInFlightState);
     const defCodes = useSelector((state) => state.covid19.defCodes);
 
     useEffect(() => {
+        // implement fetch/cancel pattern
+        if (recipientCountRequest.current) {
+            recipientCountRequest.current.cancel();
+        }
+        if (awardAmountRequest.current) {
+            awardAmountRequest.current.cancel();
+        }
+        if (awardCountRequest.current) {
+            awardCountRequest.current.cancel();
+        }
         // Reset any existing counts
         setAwardOutlays(null);
         setAwardObligations(null);
@@ -61,16 +76,20 @@ const SummaryInsightsContainer = ({ activeFilter }) => {
         if (activeFilter !== 'all') {
             params.filter.award_type_codes = awardTypeGroups[activeFilter];
         }
-        fetchAwardAmounts(params).promise
+        awardAmountRequest.current = fetchAwardAmounts(params);
+        awardCountRequest.current = fetchAwardCount(params);
+        recipientCountRequest.current = fetchDisasterSpendingCount('recipient', params);
+
+        awardAmountRequest.current.promise
             .then((res) => {
                 setAwardObligations(res.data.obligation);
                 setAwardOutlays(res.data.outlay);
             });
-        fetchAwardCount(params).promise
+        awardCountRequest.current.promise
             .then((res) => {
                 setNumberOfAwards(res.data.count);
             });
-        fetchDisasterSpendingCount('recipient', params).promise
+        recipientCountRequest.current.promise
             .then((res) => {
                 setNumberOfRecipients(res.data.count);
             });
@@ -82,6 +101,38 @@ const SummaryInsightsContainer = ({ activeFilter }) => {
         awardObligations,
         numberOfAwards
     };
+
+    useEffect(() => {
+        if (!awardOutlays && !awardObligations && !numberOfAwards) {
+            if (!isEqual(inFlightList, initialInFlightState)) {
+                resetInFlight();
+            }
+        }
+        else if (inFlightList) {
+            inFlightList.forEach((inFlight) => {
+                if (inFlight === 'awardObligations' && awardObligations) {
+                    removeFromInFlight('awardObligations');
+                }
+                else if (inFlight === 'awardOutlays' && awardOutlays) {
+                    removeFromInFlight('awardOutlays');
+                }
+                else if (inFlight === 'numberOfAwards' && numberOfAwards) {
+                    removeFromInFlight('numberOfAwards');
+                }
+                else if (inFlight === 'numberOfRecipients' && numberOfRecipients) {
+                    removeFromInFlight('numberOfRecipients');
+                }
+            });
+        }
+    }, [
+        awardOutlays,
+        awardObligations,
+        numberOfAwards,
+        numberOfRecipients,
+        inFlightList,
+        removeFromInFlight,
+        resetInFlight
+    ]);
 
     let subtitle = `for ${activeFilter === 'all' ? 'All' : 'all'} ${(awardTypeGroupLabels[activeFilter] || 'Awards')}`;
     if (activeFilter === 'other') {
@@ -95,8 +146,8 @@ const SummaryInsightsContainer = ({ activeFilter }) => {
                     {...data}
                     key={data.label}
                     subtitle={subtitle}
-                    amount={amounts[data.type]} />
-                    // isLoading={categoriesInFlight.includes(data.type)} />
+                    amount={amounts[data.type]}
+                    isLoading={inFlightList.includes(data.type)} />
             ))}
         </div>
     );
