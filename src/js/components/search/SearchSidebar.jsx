7d5afe7571452aac7f5b7de2613d4bc13a7119c0
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2020-07-27 08:36:41 -0400

    [checkbox-cleanup] fixes bug from updated title in advanced search sidebar

--- src/js/components/search/SearchSidebar.jsx
+++ src/js/components/search/SearchSidebar.jsx
@@ -39,6 +39,8 @@ import FilterSidebar from 'components/sharedComponents/filterSidebar/FilterSideb
 import * as SidebarHelper from 'helpers/sidebarHelper';
 
 const staticFilters = {
+    // NOTE: if you update the title here
+    // update the switch statement in helpers/sidebarHelper filterHasSelections!
     options: [
         {
             title: 'Keyword',
