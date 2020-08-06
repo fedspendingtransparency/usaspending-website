7c52d50d3941b7970946a8971b4190d3018b83c8
Afna <afnaa97@gmail.com>
2020-08-06 12:45:41 -0400

    Added fiscal data to resources footer and top navigation

--- src/js/GlobalConstants.js
+++ src/js/GlobalConstants.js
@@ -2,28 +2,24 @@
  * GlobalConstants.js
  * Created by Maxwell Kendall 7/8/19
 */
-
 const globalConstants = {
-    API: process.env.USASPENDING_API,
+    // DEV
+    API: 'https://dev-api.usaspending.gov/api/',
+    // QAT
+    // API: 'https://qat-api.usaspending.gov/api/',
+    // Mock API
+    // API: 'http://localhost:5000/api/',
+    // Production API
+    // API: 'https://api.usaspending.gov/api/',
     LOCAL_ROOT: "",
     GITHUB: "",
     LOCAL: false,
-    DEV: (process.env.ENV === 'dev' || process.env.ENV === 'sandbox'),
+    DEV: true,
     KEYWORD_AVAILABLE: true,
     PERF_LOG: false,
-    OVERRIDE_FISCAL_YEAR: false,
-    FISCAL_YEAR: 2017,
-    MAPBOX_TOKEN: process.env.MAPBOX_TOKEN,
-    QAT: (process.env.ENV === 'qat'),
-    // Phase 1 release
+    MAPBOX_TOKEN: 'pk.eyJ1IjoidXNhc3BlbmRpbmciLCJhIjoiY2l6ZnZjcmh0MDBtbDMybWt6NDR4cjR6ZSJ9.zsCqjJgrMDOA-i1RcCvGvg',
+    QAT: false,
     CARES_ACT_RELEASED: true,
-    // Phase 2 release
-    CARES_ACT_RELEASED_2: (
-        process.env.ENV === 'dev' ||
-        process.env.ENV === 'sandbox' ||
-        process.env.ENV === 'staging' ||
-        process.env.ENV === 'qat'
-    )
+    CARES_ACT_RELEASED_2: true
 };
-
 module.exports = globalConstants;
