345bbfca6d22e83b855758af70bd14678f9ac18f
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2020-08-07 16:44:10 -0400

    [DEV-5831]  updating copy content

--- src/js/containers/covid19/recipient/RecipientContainer.jsx
+++ src/js/containers/covid19/recipient/RecipientContainer.jsx
@@ -35,7 +35,7 @@ const RecipientContainer = () => {
                     </p>
                     <p>
                         Please note that recipient information for the Small Business Administration (SBA) Paycheck Protection Program (PPP) is not available on USAspending but is available on SBA.gov.
-                        <a href="data/data-limitations.pdf" target="_blank" rel="noopener noreferrer"> Please see the &#34;Known Data Limitations&#34; PDF in the banner above for more information.</a>
+                        <a href="data/data-limitations.pdf" target="_blank" rel="noopener noreferrer"> Please see the &#34;Known Data Limitations&#34; PDF for more information.</a>
                     </p>
                     <p className="footnotes">
                         <sup>3</sup> To learn more about eligibility criteria for receiving a financial assistance award or contract, visit <ExternalLink url="https://beta.sam.gov/" />
