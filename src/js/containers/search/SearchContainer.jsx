daaec957b7fcb78a9e75708c690ca2f1c428e1fa
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2020-08-17 14:44:32 -0400

    [DEV-4179] refactor SearchContainer for testablility

--- src/js/containers/search/SearchContainer.jsx
+++ src/js/containers/search/SearchContainer.jsx
@@ -3,7 +3,7 @@
  * Created by Kevin Li 5/30/17
  */
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
 import { isCancel } from 'axios';
@@ -36,6 +36,79 @@ const propTypes = {
     history: PropTypes.object
 };
 
+export const areFiltersBlank = (filters, appliedFilters = initialState) => {
+    // check to see if we are applying any filters
+    // if there are no filters, we shouldn't generate a hash
+    let unfiltered = true;
+
+    // make a copy of the initial and actual Redux filter states
+    const currentState = Object.assign({}, filters);
+    const unfilteredState = Object.assign({}, appliedFilters);
+    if (currentState.timePeriodType === 'fy') {
+        // if the time period is fiscal year, we don't care about the date range values, even
+        // if they're provided because the date range tab isn't selected
+        delete unfilteredState.timePeriodStart;
+        delete unfilteredState.timePeriodEnd;
+        delete currentState.timePeriodStart;
+        delete currentState.timePeriodEnd;
+    }
+    else if (currentState.timePeriodEnd === 'dr') {
+        // if the time period is date range, we don't care about the fiscal year values, even
+        // if they're provided because the fiscal year tab isn't selected
+        delete unfilteredState.timePeriodFY;
+        delete currentState.timePeriodFY;
+    }
+
+    // we need to iterate through each of the filter Redux keys in order to perform equality
+    // comparisons on Immutable children (via the Immutable is() function)
+    const filterKeys = Object.keys(unfilteredState);
+
+    for (let i = 0; i < filterKeys.length; i++) {
+        const key = filterKeys[i];
+        const unfilteredValue = unfilteredState[key];
+        const currentValue = currentState[key];
+        if (!is(unfilteredValue, currentValue)) {
+            // it doesn't match, we can stop looping - filters have been applied
+            unfiltered = false;
+            break;
+        }
+    }
+    return unfiltered;
+};
+
+export const parseRemoteFilters = (data) => {
+    const newFilters = data.filters;
+    const version = data.version;
+
+    if (version !== filterStoreVersion) {
+        // versions don't match, don't populate the filters
+        // TODO: Kevin Li - figure out how we want to deal with Redux structure changes when
+        // a URL hash contains data that no longer applies to the current site
+        console.log("version mismatch");
+        return null;
+    }
+
+    // convert values to Immutable object types as necessary
+    const reduxValues = {};
+    Object.keys(newFilters).forEach((key) => {
+        const value = newFilters[key];
+        if (requiredTypes[key]) {
+            // Redux expects an Immutable-typed object
+            const ObjType = requiredTypes[key];
+            reduxValues[key] = new ObjType(value);
+        }
+        else {
+            reduxValues[key] = value;
+        }
+    });
+
+    // send the selected filters to Google Analytics
+    const events = convertFiltersToAnalyticEvents(reduxValues);
+    sendFieldCombinations(events);
+    sendAnalyticEvents(events);
+    return reduxValues;
+};
+
 const SearchContainer = ({ history }) => {
     const { urlHash } = useParams();
     const dispatch = useDispatch();
@@ -43,124 +116,44 @@ const SearchContainer = ({ history }) => {
     const [downloadAvailable, setDownloadAvailable] = useState(false);
     const [downloadInFlight, setDownloadInFlight] = useState(false);
     const [inFlight, setInFlight] = useState(false);
-    const [stateHash, setStateHash] = useState('');
 
-    const applyFilters = (data) => {
-        const newFilters = data.filters;
-        const version = data.version;
-
-        if (version !== filterStoreVersion) {
-            // versions don't match, don't populate the filters
-            // TODO: Kevin Li - figure out how we want to deal with Redux structure changes when
-            // a URL hash contains data that no longer applies to the current site
-            console.log("version mismatch");
-            return;
-        }
-
-        // convert values to Immutable object types as necessary
-        const reduxValues = {};
-        Object.keys(newFilters).forEach((key) => {
-            const value = newFilters[key];
-            if (requiredTypes[key]) {
-                // Redux expects an Immutable-typed object
-                const ObjType = requiredTypes[key];
-                reduxValues[key] = new ObjType(value);
-            }
-            else {
-                reduxValues[key] = value;
-            }
-        });
-
-        // apply the filters to both the staged and applied stores
-        dispatch(restoreHashedFilters(reduxValues));
-
-        // send the prepopulated filters (received from the hash) to Google Analytics
-        const events = convertFiltersToAnalyticEvents(reduxValues);
-        sendAnalyticEvents(events);
-        sendFieldCombinations(events);
-
-        setInFlight(false);
-    };
-
-    const determineIfUnfiltered = () => {
-        // check to see if we are applying any filters
-        // if there are no filters, we shouldn't generate a hash
-        let unfiltered = true;
-
-        // make a copy of the initial and actual Redux filter states
-        const currentState = Object.assign({}, appliedFilters.filters);
-        const unfilteredState = Object.assign({}, initialState);
-        if (currentState.timePeriodType === 'fy') {
-            // if the time period is fiscal year, we don't care about the date range values, even
-            // if they're provided because the date range tab isn't selected
-            delete unfilteredState.timePeriodStart;
-            delete unfilteredState.timePeriodEnd;
-            delete currentState.timePeriodStart;
-            delete currentState.timePeriodEnd;
-        }
-        else if (currentState.timePeriodEnd === 'dr') {
-            // if the time period is date range, we don't care about the fiscal year values, even
-            // if they're provided because the fiscal year tab isn't selected
-            delete unfilteredState.timePeriodFY;
-            delete currentState.timePeriodFY;
-        }
-
-        // we need to iterate through each of the filter Redux keys in order to perform equality
-        // comparisons on Immutable children (via the Immutable is() function)
-        const filterKeys = Object.keys(unfilteredState);
-
-        for (let i = 0; i < filterKeys.length; i++) {
-            const key = filterKeys[i];
-            const unfilteredValue = unfilteredState[key];
-            const currentValue = currentState[key];
-            if (!is(unfilteredValue, currentValue)) {
-                // it doesn't match, we can stop looping - filters have been applied
-                unfiltered = false;
-                break;
-            }
-        }
-        return unfiltered;
-    };
-
-    const requestFilters = () => {
-        // POST an API request to retrieve the Redux state
-        SearchHelper.restoreUrlHash({
-            hash: urlHash
-        }).promise
-            .then((res) => {
-                dispatch(setAppliedFilterEmptiness(false));
-                applyFilters(res.data.filter);
-            })
-            .catch((err) => {
-                if (!isCancel(err)) {
-                    console.log(err);
-
-                    setInFlight(false);
-                    // nuke the hash since it isn't working
-                    setStateHash('');
-                    dispatch(setAppliedFilterEmptiness(true));
-                    dispatch(setAppliedFilterCompletion(true));
-                    history.push('/search');
-                }
-            });
-    };
-
-    const receiveHash = () => {
+    const fetchFiltersFromHash = useCallback(() => {
         if (!urlHash || urlHash === '') {
             setInFlight(false);
             history.push('/search');
             return;
         }
-
         // this is a hash that has been provided to the search page via the URL. The filters have
         // not yet been applied, so update the container's state and then parse the hash into its
         // original filter set.
-        setStateHash(urlHash);
         setInFlight(true);
-        requestFilters();
-    };
+        // POST an API request to retrieve the corresponding filter selections.
+        SearchHelper.restoreUrlHash({
+            hash: urlHash
+        }).promise
+            .then((res) => {
+                dispatch(setAppliedFilterEmptiness(false));
+                const filtersInImmutableStructure = parseRemoteFilters(res.data.filter);
+                if (filtersInImmutableStructure) {
+                    // apply the filters to both the staged and applied stores
+                    dispatch(restoreHashedFilters(filtersInImmutableStructure));
+                }
+                setInFlight(false);
+            })
+            .catch((err) => {
+                if (!isCancel(err)) {
+                    // eslint-disable-next-line no-console
+                    console.error('Error fetching filters from hash: ', err);
+                    setInFlight(false);
+                    // remove hash since corresponding filter selections aren't retrievable.
+                    dispatch(setAppliedFilterEmptiness(true));
+                    dispatch(setAppliedFilterCompletion(true));
+                    history.push('/search');
+                }
+            });
+    }, [history, dispatch, urlHash]);
 
-    const provideHash = (newHash) => {
+    const setNewHash = useCallback((newHash) => {
         // this is a hash that represents the current filter set. The filters are already applied
         // by way of user interaction. Now update the component state and URL with the hash so the
         // URL can be shared with others. Update the state first (to prevent the hash from being
@@ -169,12 +162,11 @@ const SearchContainer = ({ history }) => {
         // working as expected.
         dispatch(setAppliedFilterEmptiness(false));
         setInFlight(false);
-        setStateHash(newHash);
         history.push(`/search/${newHash}`);
-    };
+    }, [dispatch, history]);
 
-    const generateHash = () => {
-        const unfiltered = determineIfUnfiltered();
+    const generateHash = useCallback(() => {
+        const unfiltered = areFiltersBlank(appliedFilters.filters);
         if (unfiltered) {
             // all the filters were cleared, reset to a blank hash
             dispatch(setAppliedFilterEmptiness(true));
@@ -191,17 +183,17 @@ const SearchContainer = ({ history }) => {
             .then((res) => {
                 // update the URL with the received hash
                 const newHash = res.data.hash;
-                provideHash(newHash);
+                setNewHash(newHash);
             })
             .catch((err) => {
                 if (!isCancel(err)) {
                     console.log(err);
                 }
             });
-    };
+    }, [dispatch, history, setNewHash, appliedFilters.filters]);
 
-    const requestDownloadAvailability = () => {
-        if (determineIfUnfiltered()) {
+    const setDownloadAvailability = useCallback(() => {
+        if (areFiltersBlank(appliedFilters.filters)) {
             // don't make an API call when it's a blank state
             setDownloadAvailable(false);
             setDownloadInFlight(false);
@@ -228,22 +220,20 @@ const SearchContainer = ({ history }) => {
             .catch(() => {
                 setDownloadInFlight(false);
             });
-    };
+    }, [filters, appliedFilters.filters]);
 
     useEffect(() => {
         if (!inFlight) {
-            // the filters changed and it's not because of an inbound/outbound URL hash change
+            // applied filters changed, create a hash.
             generateHash();
         }
-        requestDownloadAvailability();
-    }, [appliedFilters.filters]);
+        setDownloadAvailability();
+    }, [appliedFilters.filters, generateHash, inFlight, setDownloadAvailability]);
 
     useEffect(() => {
-        // When the URL hash changes
-        if (urlHash !== stateHash) {
-            receiveHash(urlHash || '');
-        }
-    }, [urlHash]);
+        // url changed, apply filters.
+        fetchFiltersFromHash(urlHash);
+    }, [urlHash, fetchFiltersFromHash]);
 
     return (
         <SearchPage
--- tests/containers/search/SearchContainer-test.jsx
+++ tests/containers/search/SearchContainer-test.jsx
@@ -2,17 +2,12 @@
  * SearchContainer-test.jsx
  * Created by Kevin Li 6/2/17
  */
-
-import React from 'react';
-import { shallow } from 'enzyme';
 import { Set } from 'immutable';
 
 import { initialState } from 'redux/reducers/search/searchFiltersReducer';
-import { initialState as initialApplied } from 'redux/reducers/search/appliedFiltersReducer';
-import SearchContainer from 'containers/search/SearchContainer';
+import { parseRemoteFilters, areFiltersBlank } from 'containers/search/SearchContainer';
 
-import { mockFilters, mockRedux, mockActions } from './mockSearchHashes';
-import Router from './mockRouter';
+import { mockFilters, mockRedux } from './mockSearchHashes';
 
 // force Jest to use native Node promises
 // see: https://facebook.github.io/jest/docs/troubleshooting.html#unresolved-promises
@@ -45,344 +40,30 @@ jest.mock('react-redux', () => {
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
 
 describe('SearchContainer', () => {
-    it('should try to resolve the current URL hash on mount', () => {
-        const container = shallow(<SearchContainer
-            {...mockActions}
-            {...mockRedux} />);
-
-        const handleInitialUrl = jest.fn();
-        container.instance().handleInitialUrl = handleInitialUrl;
-
-        container.instance().componentDidMount();
-
-        expect(handleInitialUrl).toHaveBeenCalledTimes(1);
-    });
-
-    it('should try to resolve the URL hash when new inbound hashes are passed in the URL', () => {
-        const container = shallow(<SearchContainer
-            {...mockActions}
-            {...mockRedux} />);
-
-        container.setState({
-            hash: '22222'
-        });
-
-        const receiveHash = jest.fn();
-        container.instance().receiveHash = receiveHash;
-
-        container.instance().componentDidUpdate({
-            params: {
-                hash: '11111'
-            }
-        });
-
-        expect(receiveHash).toHaveBeenCalledTimes(1);
-    });
-
-    it('should not try to resolve the URL hash again when the URL changes programmatically', () => {
-        const container = shallow(<SearchContainer
-            params={{ hash: "11111" }}
-            {...mockActions}
-            {...mockRedux} />);
-
-        container.setState({
-            hash: '11111'
-        });
-
-        const receiveHash = jest.fn();
-        container.instance().receiveHash = receiveHash;
-
-        // componentDidUpdate, which we're testing here, does get called simply by updating state as of enzyme v3: https://enzymejs.github.io/enzyme/docs/guides/migration-from-2-to-3.html#lifecycle-methods
-
-        expect(receiveHash).toHaveBeenCalledTimes(0);
-    });
-
-    it('should try to generate a new URL hash when the Redux filters change', () => {
-        const container = shallow(<SearchContainer
-            {...mockActions}
-            {...mockRedux} />);
-
-        container.setState({
-            hash: '',
-            hashState: 'ready'
-        });
-
-        const nextFilters = Object.assign({}, initialState, {
-            timePeriodFY: new Set(['1987'])
-        });
-
-        const prevProps = Object.assign({}, mockRedux, mockActions, {
-            appliedFilters: Object.assign({}, initialApplied, {
-                filters: nextFilters
-            })
-        });
-
-        const generateHash = jest.fn();
-        container.instance().generateHash = generateHash;
-
-        container.instance().componentDidUpdate(prevProps);
-
-        expect(generateHash).toHaveBeenCalledTimes(1);
-    });
-
-    describe('handleInitialUrl', () => {
-        it('should attempt to generate a hash from the existing Redux filters if no URL hash is found', () => {
-            const container = shallow(<SearchContainer
-                {...mockActions}
-                {...mockRedux} />);
-
-            const generateInitialHash = jest.fn();
-            container.instance().generateInitialHash = generateInitialHash;
-
-            container.instance().handleInitialUrl('');
-
-            expect(generateInitialHash).toHaveBeenCalledTimes(1);
-        });
-        it('should attempt to restore the filter set based on the current URL hash', () => {
-            const container = shallow(<SearchContainer
-                {...mockActions}
-                {...mockRedux} />);
-
-            const requestFilters = jest.fn();
-            container.instance().requestFilters = requestFilters;
-
-            container.instance().handleInitialUrl('2222');
-
-            expect(requestFilters).toHaveBeenCalledTimes(1);
-        });
-    });
-
-    describe('generateInitialHash', () => {
-        it('should use a hashless search URL if no filters are applied', () => {
-            const container = shallow(<SearchContainer
-                {...mockActions}
-                {...mockRedux} />);
-
-            const generateHash = jest.fn();
-            container.instance().generateHash = generateHash;
-
-            container.instance().generateInitialHash();
-            expect(Router.history.replace).toHaveBeenLastCalledWith('/search');
-
-            expect(generateHash).toHaveBeenCalledTimes(0);
-        });
-
-        it('should generate a hash if there are filters applied', () => {
-            const filters = Object.assign({}, initialState, {
-                timePeriodFY: new Set(['1987'])
-            });
-
-            const redux = Object.assign({}, mockRedux, {
-                appliedFilters: {
-                    filters
-                }
-            });
-
-            const container = shallow(<SearchContainer
-                {...mockActions}
-                {...redux} />);
-
-            const generateHash = jest.fn();
-            container.instance().generateHash = generateHash;
-
-            container.instance().generateInitialHash();
-
-            expect(generateHash).toHaveBeenCalledTimes(1);
-        });
-    });
-
-    describe('receiveHash', () => {
-        it('should request filters for the given hash', () => {
-            const container = shallow(<SearchContainer
-                {...mockActions}
-                {...mockRedux} />);
-
-            const requestFilters = jest.fn();
-            container.instance().requestFilters = requestFilters;
-
-            container.instance().receiveHash('222');
-
-            expect(requestFilters).toHaveBeenCalledTimes(1);
-        });
-
-        it('should do nothing if no hash is provided', () => {
-            const container = shallow(<SearchContainer
-                {...mockActions}
-                {...mockRedux} />);
-
-            const requestFilters = jest.fn();
-            container.instance().requestFilters = requestFilters;
-
-            container.instance().receiveHash('');
-
-            expect(requestFilters).toHaveBeenCalledTimes(0);
-        });
-    });
-
-    describe('provideHash', () => {
-        it('should update the URL to the given hash', () => {
-            const container = shallow(<SearchContainer
-                {...mockActions}
-                {...mockRedux} />);
-
-            container.instance().provideHash('12345');
-            expect(Router.history.replace).toHaveBeenLastCalledWith('/search/12345');
-
-            expect(container.state().hash).toEqual('12345');
-        });
-    });
-
-    describe('applyFilters', () => {
-        it('should stop if the versions do not match', () => {
-            const container = shallow(<SearchContainer
-                {...mockActions}
-                {...mockRedux} />);
-
+    describe('parseRemoteFilters', () => {
+        it('should return null if the versions do not match', () => {
             const mockResponse = Object.assign({}, mockFilters, {
                 filter: {
                     version: -1000
                 }
             });
 
-            container.setState({
-                hashState: 'inbound'
-            });
-            container.instance().applyFilters(mockResponse.filter);
-
-            expect(container.state().hashState).toEqual('inbound');
+            expect(parseRemoteFilters(mockResponse)).toEqual(null);
         });
-
-        it('should trigger a Redux action to apply the filters', () => {
-            const populateAction = jest.fn();
-
-            const actions = Object.assign({}, mockActions, {
-                restoreHashedFilters: populateAction
-            });
-
-            const container = shallow(<SearchContainer
-                {...actions}
-                {...mockRedux} />);
-
-            container.setState({
-                hashState: 'inbound'
-            });
-            container.instance().applyFilters(mockFilters.filter);
-
-            const expectedFilters = Object.assign({}, initialState, {
-                timePeriodFY: new Set(['1990'])
-            });
-
-            expect(populateAction).toHaveBeenCalledTimes(1);
-            expect(populateAction).toHaveBeenCalledWith(expectedFilters);
-            expect(container.state().hashState).toEqual('ready');
+        it('should return an immutable data structure when versions match', () => {
+            const expectedFilter = new Set(['1990']);
+            expect(parseRemoteFilters(mockFilters.filter).timePeriodFY).toEqual(expectedFilter);
         });
     });
-
-    describe('determineIfUnfiltered', () => {
-        it('should return true if no filters are applied', () => {
-            const container = shallow(<SearchContainer
-                {...mockActions}
-                {...mockRedux} />);
-
-            const unfiltered = container.instance().determineIfUnfiltered(initialState);
-            expect(unfiltered).toBeTruthy();
+    describe('areFiltersBlank', () => {
+        it('should return true when selected filters are effectively blank', () => {
+            expect(areFiltersBlank(initialState)).toBeTruthy();
         });
-        it('should return false if filters have been applied', () => {
-            const container = shallow(<SearchContainer
-                {...mockActions}
-                {...mockRedux} />);
-
-            const modifiedState = Object.assign({}, initialState, {
-                timePeriodFY: new Set(['1987'])
-            });
-
-            const unfiltered = container.instance().determineIfUnfiltered(modifiedState);
-            expect(unfiltered).toBeFalsy();
-        });
-    });
-
-    describe('parseUpdateDate', () => {
-        it('should format the API response correctly and set the state', () => {
-            const container = shallow(<SearchContainer
-                {...mockActions}
-                {...mockRedux} />);
-
-            container.instance().parseUpdateDate('01/01/1984');
-            expect(container.state().lastUpdate).toEqual('January 1, 1984');
-        });
-    });
-
-    describe('requestDownloadAvailability', () => {
-        it('should not make an API requets if the applied filter state equals the initial blank filter state', () => {
-            const blankFilters = Object.assign({}, initialState);
-            const container = shallow(<SearchContainer
-                {...mockActions}
-                {...mockRedux} />);
-
-            const mockParse = jest.fn();
-            container.instance().parseDownloadAvailability = mockParse;
-
-            container.setState({
-                downloadAvailable: true
-            });
-
-            container.instance().requestDownloadAvailability(blankFilters);
-
-            expect(mockParse).toHaveBeenCalledTimes(0);
-            expect(container.state().downloadAvailable).toBeFalsy();
-        });
-        it('should make an API request for how many transaction rows will be returned', async () => {
-            const newFilters = Object.assign({}, initialState, {
-                timePeriodFY: new Set(['1990'])
-            });
-
-            const container = shallow(<SearchContainer
-                {...mockActions}
-                {...mockRedux} />);
-
-            const mockParse = jest.fn();
-            container.instance().parseDownloadAvailability = mockParse;
-
-            container.instance().requestDownloadAvailability(newFilters);
-            await container.instance().downloadRequest.promise;
-
-            expect(mockParse).toHaveBeenCalledWith({
-                transaction_rows_gt_limit: false
-            });
-        });
-    });
-
-    describe('parseDownloadAvailability', () => {
-        it('should set the downloadAvailable state to false if there are more than 500,000 rows', () => {
-            const container = shallow(<SearchContainer
-                {...mockActions}
-                {...mockRedux} />);
-
-            container.setState({
-                downloadAvailable: true
-            });
-
-            container.instance().parseDownloadAvailability({
-                transaction_rows_gt_limit: true
-            });
-
-            expect(container.state().downloadAvailable).toBeFalsy();
-        });
-
-        it('should set the downloadAvailable state to true if there are no more than 500,000 rows', () => {
-            const container = shallow(<SearchContainer
-                {...mockActions}
-                {...mockRedux} />);
-
-            container.setState({
-                downloadAvailable: true
-            });
-
-            container.instance().parseDownloadAvailability({
-                transaction_rows_gt_limit: false
-            });
-
-            expect(container.state().downloadAvailable).toBeTruthy();
+        it('should return false when filters are selected', () => {
+            expect(areFiltersBlank({
+                ...initialState,
+                timePeriodFY: new Set(['2020'])
+            })).toBeFalsy();
         });
     });
 });
