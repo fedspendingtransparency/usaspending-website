7adceb8bd2d67d6017fbb8fe52c07961a6a31af1
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2020-08-20 13:03:21 -0400

    [DEV-4478] scrollRestoration manual if defined!

--- src/js/containers/AppContainer.jsx
+++ src/js/containers/AppContainer.jsx
@@ -37,10 +37,17 @@ storeSingleton.setStore(store);
 
 const ScrollToTop = () => {
     const { pathname } = useLocation();
+    
+    useEffect(() => {
+        if (window?.history?.scrollRestoration) {
+            window.history.scrollRestoration = 'manual';
+        }
+    }, []);
 
     useEffect(() => {
         window.scrollTo(0, 0);
     }, [pathname]);
+
 
     return null;
 };
