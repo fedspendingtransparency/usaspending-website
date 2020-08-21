27d7370863a29669bda498ffee20febdeefc0105
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2020-08-21 09:56:05 -0400

    [DEV-4478] updating polyfill for IE

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ const presets = [
     [
         "@babel/preset-env",
         {
-            useBuiltIns: "usage",
+            useBuiltIns: "entry",
             corejs: {
                 version: 3,
                 proposals: true
@@ -27,6 +27,7 @@ const plugins = [
     "@babel/plugin-proposal-object-rest-spread",
     "@babel/plugin-proposal-class-properties",
     "@babel/plugin-proposal-optional-chaining",
+    "@babel/plugin-syntax-dynamic-import",
     "react-hot-loader/babel"
 ];
 
