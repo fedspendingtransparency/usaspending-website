9e37a4d955fe0c8237d64e0799b975655cf6935d
James Lee <jamesleework40@gmail.com>
2020-09-08 14:18:01 -0400

    update logic and tests

--- src/js/containers/bulkDownload/BulkDownloadPageContainer.jsx
+++ src/js/containers/bulkDownload/BulkDownloadPageContainer.jsx
@@ -102,7 +102,6 @@ export class BulkDownloadPageContainer extends React.Component {
             filters: {
                 prime_award_types: primeAwardTypes,
                 sub_award_types: subAwardTypes,
-                agency: formState.agency.id,
                 sub_agency: formState.subAgency.name,
                 date_type: formState.dateType,
                 date_range: {
@@ -127,15 +126,13 @@ export class BulkDownloadPageContainer extends React.Component {
             }
         }
 
-        if (formState.agencyType) {
-            delete params.filters.agency;
-            const topTierAgencyParams = { type: formState.agencyType === 'awarding_agency' ? 'awarding' : 'funding', tier: "toptier", name: formState.agency.name };
-            params.filters.agencies = [topTierAgencyParams];
-            if (formState.subAgency.name && formState.subAgency.name !== 'Select a Sub-Agency') {
-                const subTierAgencyParams = { type: formState.agencyType === 'awarding_agency' ? 'awarding' : 'funding', tier: "subtier", name: formState.subAgency.name };
-                params.filters.agencies = [topTierAgencyParams, subTierAgencyParams];
-            }
+        const agencyParams = { type: formState.agencyType === 'awarding_agency' ? 'awarding' : 'funding', tier: "toptier", name: formState.agency.name };
+        if (formState.subAgency.name && formState.subAgency.name !== 'Select a Sub-Agency') {
+            agencyParams.name = formState.subAgency.name;
+            agencyParams.tier = 'subtier';
+            agencyParams.toptier_name = formState.agency.name;
         }
+        params.filters.agencies = [agencyParams];
 
         this.requestDownload(params, 'awards');
 
--- tests/containers/bulkDownload/BulkDownloadPageContainer-test.jsx
+++ tests/containers/bulkDownload/BulkDownloadPageContainer-test.jsx
@@ -40,7 +40,9 @@ describe('BulkDownloadPageContainer', () => {
                 filters: {
                     prime_award_types: ["02", "03", "04", "05", "07", "08"],
                     sub_award_types: ['procurement'],
-                    agencies: [{ name: 'Mock Agency', type: 'funding', tier: 'toptier' }, { name: 'Mock Sub-Agency', type: 'funding', tier: 'subtier' }],
+                    agencies: [{
+                        name: 'Mock Sub-Agency', type: 'funding', tier: 'subtier', toptier_name: 'Mock Agency'
+                    }],
                     recipient_locations: [
                         {
                             country: 'USA',
@@ -91,7 +93,9 @@ describe('BulkDownloadPageContainer', () => {
                 columns: [],
                 file_format: 'csv',
                 filters: {
-                    agencies: [{ name: 'Mock Agency', type: 'funding', tier: 'toptier' }, { name: 'Mock Sub-Agency', type: 'funding', tier: 'subtier' }],
+                    agencies: [{
+                        name: 'Mock Sub-Agency', type: 'funding', tier: 'subtier', toptier_name: 'Mock Agency'
+                    }],
                     prime_award_types: ["02", "03", "04", "05", "07", "08"],
                     sub_award_types: ['procurement'],
                     recipient_scope: 'foreign',
@@ -139,7 +143,9 @@ describe('BulkDownloadPageContainer', () => {
                 columns: [],
                 file_format: 'csv',
                 filters: {
-                    agencies: [{ name: 'Mock Agency', type: 'funding', tier: 'toptier' }, { name: 'Mock Sub-Agency', type: 'funding', tier: 'subtier' }],
+                    agencies: [{
+                        name: 'Mock Sub-Agency', type: 'funding', tier: 'subtier', toptier_name: 'Mock Agency'
+                    }],
                     prime_award_types: ["02", "03", "04", "05", "07", "08"],
                     sub_award_types: ['procurement'],
                     date_range: {
@@ -186,7 +192,9 @@ describe('BulkDownloadPageContainer', () => {
                 columns: [],
                 file_format: 'csv',
                 filters: {
-                    agencies: [{ name: 'Mock Agency', type: 'funding', tier: 'toptier' }, { name: 'Mock Sub-Agency', type: 'funding', tier: 'subtier' }],
+                    agencies: [{
+                        name: 'Mock Sub-Agency', type: 'funding', tier: 'subtier', toptier_name: 'Mock Agency'
+                    }],
                     prime_award_types: ["02", "03", "04", "05", "07", "08"],
                     sub_award_types: ['procurement'],
                     recipient_locations: [
@@ -229,7 +237,9 @@ describe('BulkDownloadPageContainer', () => {
                 columns: [],
                 file_format: 'csv',
                 filters: {
-                    agencies: [{ name: 'Mock Agency', type: 'funding', tier: 'toptier' }, { name: 'Mock Sub-Agency', type: 'funding', tier: 'subtier' }],
+                    agencies: [{
+                        name: 'Mock Sub-Agency', type: 'funding', tier: 'subtier', toptier_name: 'Mock Agency'
+                    }],
                     prime_award_types: ["02", "03", "04", "05", "07", "08"],
                     sub_award_types: ['procurement'],
                     place_of_performance_locations: [
@@ -283,7 +293,9 @@ describe('BulkDownloadPageContainer', () => {
                 columns: [],
                 file_format: 'csv',
                 filters: {
-                    agencies: [{ name: 'Mock Agency', type: 'funding', tier: 'toptier' }, { name: 'Mock Sub-Agency', type: 'funding', tier: 'subtier' }],
+                    agencies: [{
+                        name: 'Mock Sub-Agency', type: 'funding', tier: 'subtier', toptier_name: 'Mock Agency'
+                    }],
                     prime_award_types: ["02", "03", "04", "05", "07", "08"],
                     sub_award_types: ['procurement'],
                     place_of_performance_scope: 'foreign',
