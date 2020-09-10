2cb0761fd32d802dcc7f36287971110e1343a30b
Jonathan Hill <hill_jonathan@bah.com>
2020-09-10 12:50:43 -0400

    dev-6079 update dsm and other resources css for mobile

--- src/js/components/covid19/OtherResources.jsx
+++ src/js/components/covid19/OtherResources.jsx
@@ -27,117 +27,117 @@ const OtherResources = ({
                         <li>
                             Pandemic Response Accountability Committee (PRAC) -
                             <a
-                                href=" https://pandemic.oversight.gov/track-the-money "
+                                href="https://pandemic.oversight.gov/track-the-money"
                                 onClick={handleClick}>
-                                {" https://pandemic.oversight.gov/track-the-money "}
+                                https://pandemic.oversight.gov/track-the-money
                                 <FontAwesomeIcon size="sm" icon="external-link-alt" />
                             </a>
                         </li>
                         <li>
                             Federal Reserve Board -
                             <a
-                                href=" https://www.federalreserve.gov/covid-19.htm "
+                                href="https://www.federalreserve.gov/covid-19.htm"
                                 onClick={handleClick}>
-                                {" https://www.federalreserve.gov/covid-19.htm "}
+                                https://www.federalreserve.gov/covid-19.htm
                                 <FontAwesomeIcon size="sm" icon="external-link-alt" />
                             </a>
                         </li>
                         <li>
                             Department of Agriculture -
                             <a
-                                href=" https://www.usda.gov/coronavirus "
+                                href="https://www.usda.gov/coronavirus"
                                 onClick={handleClick}>
-                                {" https://www.usda.gov/coronavirus "}
+                                https://www.usda.gov/coronavirus
                                 <FontAwesomeIcon size="sm" icon="external-link-alt" />
                             </a>
                         </li>
                         <li>
                             Department of Defense -
                             <a
-                                href=" https://www.defense.gov/Explore/Spotlight/Coronavirus/ "
+                                href="https://www.defense.gov/Explore/Spotlight/Coronavirus/"
                                 onClick={handleClick}>
-                                {" https://www.defense.gov/Explore/Spotlight/Coronavirus/ "}
+                                https://www.defense.gov/Explore/Spotlight/Coronavirus/
                                 <FontAwesomeIcon size="sm" icon="external-link-alt" />
                             </a>
                         </li>
                         <li>
                             Department of Education -
                             <a
-                                href=" https://www.ed.gov/coronavirus "
+                                href="https://www.ed.gov/coronavirus"
                                 onClick={handleClick}>
-                                {" https://www.ed.gov/coronavirus "}
+                                https://www.ed.gov/coronavirus
                                 <FontAwesomeIcon size="sm" icon="external-link-alt" />
                             </a>
                         </li>
                         <li>
                             Department of Homeland Security -
                             <a
-                                href=" https://www.dhs.gov/coronavirus/overview-dhs-response "
+                                href="https://www.dhs.gov/coronavirus/overview-dhs-response"
                                 onClick={handleClick}>
-                                {" https://www.dhs.gov/coronavirus/overview-dhs-response "}
+                                https://www.dhs.gov/coronavirus/overview-dhs-response
                                 <FontAwesomeIcon size="sm" icon="external-link-alt" />
                             </a>
                         </li>
                         <li>
                             Department of Labor -
                             <a
-                                href=" https://oui.doleta.gov/unemploy/docs/cares_act_funding_state.html "
+                                href="https://oui.doleta.gov/unemploy/docs/cares_act_funding_state.html"
                                 onClick={handleClick}>
-                                {" https://oui.doleta.gov/unemploy/docs/cares_act_funding_state.html "}
+                                https://oui.doleta.gov/unemploy/docs/cares_act_funding_state.html
                                 <FontAwesomeIcon size="sm" icon="external-link-alt" />
                             </a>
                         </li>
                         <li>
                             Department of Transportation -
                             <a
-                                href=" https://www.transportation.gov/coronavirus "
+                                href="https://www.transportation.gov/coronavirus"
                                 onClick={handleClick}>
-                                {" https://www.transportation.gov/coronavirus "}
+                                https://www.transportation.gov/coronavirus
                                 <FontAwesomeIcon size="sm" icon="external-link-alt" />
                             </a>
                         </li>
                         <li>
                             Department of the Treasury -
                             <a
-                                href=" https://home.treasury.gov/policy-issues/cares "
+                                href="https://home.treasury.gov/policy-issues/cares"
                                 onClick={handleClick}>
-                                {" https://home.treasury.gov/policy-issues/cares "}
+                                https://home.treasury.gov/policy-issues/cares
                                 <FontAwesomeIcon size="sm" icon="external-link-alt" />
                             </a>
                         </li>
                         <li>
                             Department of Veterans Affairs -
                             <a
-                                href=" https://www.publichealth.va.gov/n-coronavirus/ "
+                                href="https://www.publichealth.va.gov/n-coronavirus/"
                                 onClick={handleClick}>
-                                {" https://www.publichealth.va.gov/n-coronavirus/ "}
+                                https://www.publichealth.va.gov/n-coronavirus/
                                 <FontAwesomeIcon size="sm" icon="external-link-alt" />
                             </a>
                         </li>
                         <li>
                             Department of Health &amp; Human Services -
                             <a
-                                href=" https://www.hhs.gov/coronavirus "
+                                href="https://www.hhs.gov/coronavirus"
                                 onClick={handleClick}>
-                                {" https://www.hhs.gov/coronavirus "}
+                                https://www.hhs.gov/coronavirus
                                 <FontAwesomeIcon size="sm" icon="external-link-alt" />
                             </a>
                         </li>
                         <li>
                             Housing &amp; Urban Development -
                             <a
-                                href=" https://www.hud.gov/coronavirus "
+                                href="https://www.hud.gov/coronavirus"
                                 onClick={handleClick}>
-                                {" https://www.hud.gov/coronavirus "}
+                                https://www.hud.gov/coronavirus
                                 <FontAwesomeIcon size="sm" icon="external-link-alt" />
                             </a>
                         </li>
                         <li>
                             Small Business Administration -
                             <a
-                                href=" https://www.sba.gov/page/coronavirus-covid-19-small-business-guidance-loan-resources "
+                                href="https://www.sba.gov/page/coronavirus-covid-19-small-business-guidance-loan-resources"
                                 onClick={handleClick}>
-                                {" https://www.sba.gov/page/coronavirus-covid-19-small-business-guidance-loan-resources "}
+                                https://www.sba.gov/page/coronavirus-covid-19-small-business-guidance-loan-resources
                                 <FontAwesomeIcon size="sm" icon="external-link-alt" />
                             </a>
                         </li>
