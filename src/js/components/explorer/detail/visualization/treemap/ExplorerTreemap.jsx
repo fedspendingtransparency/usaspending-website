182276a45dd81f25de34a970c6a53eea548f7cbf
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2020-09-10 18:28:00 -0400

    [DEV-5590] Invoking the helper
    also sorting and filtering and slicing results such that we show the top  500 and
    ensure that excludes all negative values

--- src/js/components/explorer/detail/visualization/treemap/ExplorerTreemap.jsx
+++ src/js/components/explorer/detail/visualization/treemap/ExplorerTreemap.jsx
@@ -8,9 +8,11 @@ import PropTypes from 'prop-types';
 
 import { hierarchy, treemap, treemapBinary } from 'd3-hierarchy';
 import { scaleLinear } from 'd3-scale';
-import { remove } from 'lodash';
 
+import { appendCellForDataOutsideTree } from 'helpers/explorerHelper';
 import { measureTreemapHeader, measureTreemapValue } from 'helpers/textMeasurement';
+
+
 import LoadingSpinner from 'components/sharedComponents/LoadingSpinner';
 import TreemapCell from 'components/sharedComponents/TreemapCell';
 
@@ -22,7 +24,8 @@ const propTypes = {
     goDeeper: PropTypes.func,
     showTooltip: PropTypes.func,
     hideTooltip: PropTypes.func,
-    goToUnreported: PropTypes.func
+    goToUnreported: PropTypes.func,
+    activeSubdivision: PropTypes.object
 };
 
 const defaultProps = {
@@ -54,19 +57,18 @@ export default class ExplorerTreemap extends React.Component {
     }
 
     buildVirtualChart(props) {
-        const data = props.data.toJS();
-
-        // remove the negative values from the data because they can't be displayed in the treemap
-        remove(data, (v) => v.amount <= 0);
+        const dataForTree = props.data.toJS()
+            .sort((a, b) => b.amount - a.amount)
+            .slice(0, 500);
 
         const total = props.total;
+        // remove the negative values from the data because they can't be displayed in the treemap
+        const data = appendCellForDataOutsideTree(dataForTree, total, props.activeSubdivision)
+            .filter((item) => item.amount >= 0);
 
         // parse the inbound data into D3's treemap hierarchy structure
-        const treemapData = hierarchy({
-            children: data
-        })
-            .sum((d) => d.amount) // tell D3 how to extract the monetary value out of the object
-            .sort((a, b) => b.height - a.height || b.value - a.value); // sort the objects
+        const treemapData = hierarchy({ children: data })
+            .sum((d) => d.amount); // tell D3 how to extract the monetary value out of the object
 
         // set up a function for generating the treemap of the specified size and style
         const tree = treemap()
