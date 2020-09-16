7df3b4192fa97dc26d0108d593d6c6ac9c681be3
James Lee <jamesleework40@gmail.com>
2020-09-16 15:55:59 -0400

    remove checkbox and fix padding

--- src/js/components/bulkDownload/sharedFilters/DefCodeFilter.jsx
+++ src/js/components/bulkDownload/sharedFilters/DefCodeFilter.jsx
@@ -1,35 +1,26 @@
 import React from 'react';
 import PropTypes from "prop-types";
 
-import { CheckCircle } from 'components/sharedComponents/icons/Icons';
 import DEFCheckboxTreeDownload from 'components/bulkDownload/DEFCheckboxTreeDownload';
 
-const DefCodeFilter = ({ type }) => {
-    const icon = (
-        <div className="icon valid">
-            <CheckCircle />
-        </div>
-    );
-
-    return (
-        <div className="download-filter def-code-filter">
-            <h3 className="download-filter__title">
+const DefCodeFilter = ({ type }) => (
+    <div className="download-filter def-code-filter">
+        <h3 className="download-filter__title">
                 Filter by <span className="download-filter__title_em">Disaster Emergency Fund Codes (DEFCs).</span>
-            </h3>
-            <p className="download-filter__subtitle">(Optional)</p>
-            <div className="download-filter__info">
-                <span>Filter your data with codes related to supplemental appropriation bills targeting disasters and emergencies. </span>
-            </div>
-            <div className="download-filter__content">
-                <DEFCheckboxTreeDownload type={type} />
-                {type === "accounts"
-                    ? <p className="download-filter__content-note"><span className="download-filter__content-note_bold">Note:</span> COVID-19 Spending account data is only available starting FY2020 P07.</p>
-                    : <p className="download-filter__content-note"><span className="download-filter__content-note_bold">Note:</span> COVID-19 Spending data is only available for award and sub-award actions with an Action Date on or after 4/1/2020.</p>
-                }
-            </div>
+        </h3>
+        <p className="download-filter__subtitle">(Optional)</p>
+        <div className="download-filter__info">
+            <span>Filter your data with codes related to supplemental appropriation bills targeting disasters and emergencies. </span>
         </div>
-    );
-};
+        <div className="download-filter__content">
+            <DEFCheckboxTreeDownload type={type} />
+            {type === "accounts"
+                ? <p className="download-filter__content-note"><span className="download-filter__content-note_bold">Note:</span> COVID-19 Spending account data is only available starting FY2020 P07.</p>
+                : <p className="download-filter__content-note"><span className="download-filter__content-note_bold">Note:</span> COVID-19 Spending data is only available for award and sub-award actions with an Action Date on or after 4/1/2020.</p>
+            }
+        </div>
+    </div>
+);
 
 DefCodeFilter.propTypes = {
     type: PropTypes.string.isRequired // either "accounts" or "awards"
