9d1fa303ab3f4d735a588082860290cbf9ed414a
Jonathan Hill <hill_jonathan@bah.com>
2020-09-28 13:58:00 -0400

    dev-6043 update to test three websites

--- lighthouserc.js
+++ lighthouserc.js
@@ -7,8 +7,8 @@ module.exports = {
             staticDistDir: './public', // location of static files for lighthouse CI to serve
             isSinglePageApplication: true, // 
             url: [ // the urls to run lighthouse against
-                // 'http://localhost/',
-                // 'http://localhost/search'
+                'http://localhost/',
+                'http://localhost/search',
                 'http://localhost/disaster/covid-19'
             ]
         }
