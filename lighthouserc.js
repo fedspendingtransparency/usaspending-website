303cb8bf4bcb609acfe9db20fae60c73bfa368a1
Jonathan Hill <hill_jonathan@bah.com>
2020-09-28 14:11:28 -0400

    dev-6043 add agency page to lighthouse test

--- lighthouserc.js
+++ lighthouserc.js
@@ -5,11 +5,12 @@ module.exports = {
         },
         collect: {
             staticDistDir: './public', // location of static files for lighthouse CI to serve
-            isSinglePageApplication: true, // 
+            isSinglePageApplication: true,
             url: [ // the urls to run lighthouse against
                 'http://localhost/',
                 'http://localhost/search',
-                'http://localhost/disaster/covid-19'
+                'http://localhost/disaster/covid-19',
+                'http://localhost/agency/456'
             ]
         }
     }
