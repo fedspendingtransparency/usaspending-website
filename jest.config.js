c22e6b3a328916abab1e1c3b5b6cce51891f7f30
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2020-10-08 15:11:34 -0400

    [DEV-6238] mocking redux globally

--- jest.config.js
+++ jest.config.js
@@ -26,7 +26,8 @@ module.exports = {
         "<rootDir>/__mocks__/fileMock.js",
         "^(data-transparency-ui)$": "<rootDir>/node_modules/data-transparency-ui",
         "\\.(css|less|scss)$": "identity-obj-proxy",
-        ".*GlobalConstants$": "<rootDir>/tests/testResources/mockGlobalConstants.js"
+        "react-router-dom": "<rootDir>/tests/testResources/mockComponent.js",
+        ".*GlobalConstants$": "<rootDir>/tests/testResources/mockGlobalConstants.js",
     },
     setupFilesAfterEnv: ["<rootDir>/tests/setup.js"],
     transform: {
@@ -34,5 +35,6 @@ module.exports = {
     },
     transformIgnorePatterns: [
         "node_modules/(?!(data-transparency-ui))"
-    ]
+    ],
+    modulePaths: ["<rootDir>/tests/testResources"]
 };
