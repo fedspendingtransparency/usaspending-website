2971940ed051ca3a1c7948de8405c92500e7942f
brett-varney <brett.varney@kc.frb.org>
2020-11-12 08:52:25 -0600

    indents

--- src/js/components/search/visualizations/time/TimeVisualizationSection.jsx
+++ src/js/components/search/visualizations/time/TimeVisualizationSection.jsx
@@ -13,114 +13,114 @@ import { capitalize } from 'lodash';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const propTypes = {
-  data: PropTypes.object,
-  updateVisualizationPeriod: PropTypes.func,
-  visualizationPeriod: PropTypes.string,
+    data: PropTypes.object,
+    updateVisualizationPeriod: PropTypes.func,
+    visualizationPeriod: PropTypes.string,
 };
 
 export default class TimeVisualizationSection extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      windowWidth: 0,
-      visualizationWidth: 0,
-    };
+        this.state = {
+            windowWidth: 0,
+            visualizationWidth: 0,
+        };
 
-    this.handleWindowResize = throttle(this.handleWindowResize.bind(this), 50);
-  }
-
-  componentDidMount() {
-    this.handleWindowResize();
-    window.addEventListener('resize', this.handleWindowResize);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.handleWindowResize);
-  }
-
-  handleWindowResize() {
-    // determine if the width changed
-    const windowWidth = window.innerWidth;
-    if (this.state.windowWidth !== windowWidth) {
-      // width changed, update the visualization width
-      this.setState({
-        windowWidth,
-        visualizationWidth: this.sectionHr.offsetWidth,
-      });
+        this.handleWindowResize = throttle(this.handleWindowResize.bind(this), 50);
     }
-  }
 
-  downloadLabel() {
-    let periodLabel;
-    if (this.props.data.visualizationPeriod === 'fiscal_year') {
-      periodLabel = 'Year';
-    } else {
-      periodLabel = capitalize(this.props.data.visualizationPeriod);
+    componentDidMount() {
+        this.handleWindowResize();
+        window.addEventListener('resize', this.handleWindowResize);
     }
-    return `Download Data by ${periodLabel}`;
-  }
 
-  render() {
-    return (
-      <section className='results-visualization-time-section' id='results-section-time' aria-label='Spending Over Time'>
-        <h2 className='visualization-title'>Spending Over Time</h2>
-        <hr
-          className='results-divider'
-          ref={hr => {
-            this.sectionHr = hr;
-          }}
-        />
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.handleWindowResize);
+    }
 
-        <div className='visualization-top'>
-          <div className='visualization-description'>
-            <div className='content'>
-              Spot trends in spending over your chosen time period. Break down your results by years, quarters, or months, and hover over the bars for more
-              detailed information.
-            </div>
-          </div>
-          <div className='visualization-period'>
-            <div className='content'>
-              <ul>
-                <li>
-                  <TimeVisualizationPeriodButton
-                    value='fiscal_year'
-                    label='Years'
-                    active={this.props.data.visualizationPeriod === 'fiscal_year'}
-                    changePeriod={this.props.updateVisualizationPeriod}
-                  />
-                </li>
-                <li>
-                  <TimeVisualizationPeriodButton
-                    value='quarter'
-                    label='Quarters'
-                    active={this.props.data.visualizationPeriod === 'quarter'}
-                    changePeriod={this.props.updateVisualizationPeriod}
-                  />
-                </li>
-                <li>
-                  <TimeVisualizationPeriodButton
-                    value='month'
-                    label='Months'
-                    active={this.props.data.visualizationPeriod === 'month'}
-                    changePeriod={this.props.updateVisualizationPeriod}
-                  />
-                </li>
-              </ul>
-            </div>
-            <div className='download'>
-              <button>
-                <FontAwesomeIcon icon='download' size='lg' />
-                <span className='text'>{this.downloadLabel()}</span>
-              </button>
-              <FontAwesomeIcon icon='info-circle' size='lg' className='info' />
-            </div>
-          </div>
-        </div>
-        <TimeVisualization {...this.props.data} width={this.state.visualizationWidth} />
-      </section>
-    );
-  }
+    handleWindowResize() {
+        // determine if the width changed
+        const windowWidth = window.innerWidth;
+        if (this.state.windowWidth !== windowWidth) {
+            // width changed, update the visualization width
+            this.setState({
+                windowWidth,
+                visualizationWidth: this.sectionHr.offsetWidth,
+            });
+        }
+    }
+
+    downloadLabel() {
+        let periodLabel;
+        if (this.props.data.visualizationPeriod === 'fiscal_year') {
+            periodLabel = 'Year';
+        } else {
+            periodLabel = capitalize(this.props.data.visualizationPeriod);
+        }
+        return `Download Data by ${periodLabel}`;
+    }
+
+    render() {
+        return (
+            <section className='results-visualization-time-section' id='results-section-time' aria-label='Spending Over Time'>
+                <h2 className='visualization-title'>Spending Over Time</h2>
+                <hr
+                    className='results-divider'
+                    ref={hr => {
+                        this.sectionHr = hr;
+                    }}
+                />
+
+                <div className='visualization-top'>
+                    <div className='visualization-description'>
+                        <div className='content'>
+                            Spot trends in spending over your chosen time period. Break down your results by years, quarters, or months, and hover over the bars
+                            for more detailed information.
+                        </div>
+                    </div>
+                    <div className='visualization-period'>
+                        <div className='content'>
+                            <ul>
+                                <li>
+                                    <TimeVisualizationPeriodButton
+                                        value='fiscal_year'
+                                        label='Years'
+                                        active={this.props.data.visualizationPeriod === 'fiscal_year'}
+                                        changePeriod={this.props.updateVisualizationPeriod}
+                                    />
+                                </li>
+                                <li>
+                                    <TimeVisualizationPeriodButton
+                                        value='quarter'
+                                        label='Quarters'
+                                        active={this.props.data.visualizationPeriod === 'quarter'}
+                                        changePeriod={this.props.updateVisualizationPeriod}
+                                    />
+                                </li>
+                                <li>
+                                    <TimeVisualizationPeriodButton
+                                        value='month'
+                                        label='Months'
+                                        active={this.props.data.visualizationPeriod === 'month'}
+                                        changePeriod={this.props.updateVisualizationPeriod}
+                                    />
+                                </li>
+                            </ul>
+                        </div>
+                        <div className='download'>
+                            <button>
+                                <FontAwesomeIcon icon='download' size='lg' />
+                                <span className='text'>{this.downloadLabel()}</span>
+                            </button>
+                            <FontAwesomeIcon icon='info-circle' size='lg' className='info' />
+                        </div>
+                    </div>
+                </div>
+                <TimeVisualization {...this.props.data} width={this.state.visualizationWidth} />
+            </section>
+        );
+    }
 }
 
 TimeVisualizationSection.propTypes = propTypes;
