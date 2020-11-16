b3a26b4d68821f0d385f4516917d20fa7f7b6a0c
Brett Varney <brett.varney@kc.frb.org>
2020-11-16 08:44:01 -0600

    another ie idea

--- src/js/components/search/visualizations/time/TimeVisualizationSection.jsx
+++ src/js/components/search/visualizations/time/TimeVisualizationSection.jsx
@@ -108,6 +108,27 @@ export default class TimeVisualizationSection extends React.Component {
         }
     }
 
+
+    function download(text, name, type)
+    {
+        var file = new Blob([text], {type: type});
+        var isIE = /*@cc_on!@*/false || !!document.documentMode;
+        if (isIE)
+        {
+            window.navigator.msSaveOrOpenBlob(file, name);
+        }
+        else
+        {
+            var a = document.createElement('a');
+            a.href = URL.createObjectURL(file);
+            a.download = name;
+            a.click();
+        }
+     }
+
+
+
+
     render() {
         return (
             <section
