ba754c5768c88eda55fc7da8c546d27aaeba7a53
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2020-11-23 17:02:11 -0500

    [DEV-6377] jest config updates:
    - new component to register icons in app and in test
    - adding to modulePaths array

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,61 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { hot } from "react-hot-loader";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import {
-    faAngleDown,
-    faAngleRight,
-    faAngleLeft,
-    faArrowLeft,
-    faArrowCircleLeft,
-    faBan,
-    faBook,
-    faBuilding,
-    faCalendarAlt,
-    faCaretDown,
-    faCaretLeft,
-    faCaretRight,
-    faCaretUp,
-    faChartArea,
-    faChartBar,
-    faChartPie,
-    faCheckSquare,
-    faCheckCircle,
-    faChevronDown,
-    faChevronUp,
-    faChevronRight,
-    faCubes,
-    faDownload,
-    faEllipsisH,
-    faEnvelope,
-    faExclamationCircle,
-    faExternalLinkAlt,
-    faExclamationTriangle,
-    faHandHoldingMedical,
-    faHandsHelping,
-    faInfo,
-    faInfoCircle,
-    faLandmark,
-    faLevelUpAlt,
-    faLink,
-    faMapMarkerAlt,
-    faMinusSquare,
-    faPencilAlt,
-    faPlusCircle,
-    faSearch,
-    faShareAlt,
-    faSitemap,
-    faSpinner,
-    faSquare,
-    faTable,
-    faTag,
-    faThLarge,
-    faTimes,
-    faUserTie,
-    faWindowRestore,
-    faFingerprint
-} from "@fortawesome/free-solid-svg-icons";
 import AppContainer from 'containers/AppContainer';
+import RegisterIcons from './RegisterIcons';
 
 /**
  * babel 7 removed the ECMAScript proposals from babel-polyfill.
@@ -65,59 +12,7 @@ import AppContainer from 'containers/AppContainer';
 require("core-js");
 require('helpers/rafPolyfill');
 
-library.add(
-    faAngleDown,
-    faAngleRight,
-    faAngleLeft,
-    faArrowLeft,
-    faArrowCircleLeft,
-    faBan,
-    faBook,
-    faBuilding,
-    faCalendarAlt,
-    faCaretDown,
-    faCaretLeft,
-    faCaretRight,
-    faCaretUp,
-    faChartArea,
-    faChartBar,
-    faChartPie,
-    faCheckSquare,
-    faCheckCircle,
-    faChevronDown,
-    faChevronUp,
-    faChevronRight,
-    faCubes,
-    faDownload,
-    faEllipsisH,
-    faEnvelope,
-    faExclamationCircle,
-    faExternalLinkAlt,
-    faExclamationTriangle,
-    faFingerprint,
-    faHandHoldingMedical,
-    faHandsHelping,
-    faInfo,
-    faInfoCircle,
-    faLandmark,
-    faLevelUpAlt,
-    faLink,
-    faMapMarkerAlt,
-    faMinusSquare,
-    faPencilAlt,
-    faPlusCircle,
-    faSearch,
-    faShareAlt,
-    faSitemap,
-    faSpinner,
-    faSquare,
-    faTable,
-    faTag,
-    faThLarge,
-    faTimes,
-    faUserTie,
-    faWindowRestore
-);
+RegisterIcons();
 
 const appDiv = document.getElementById('app');
 const App = render(
--- tests/setup.js
+++ tests/setup.js
@@ -1,8 +1,10 @@
 
 import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
+import RegisterIcons from "../src/js/RegisterIcons";
 
 window.URL.createObjectURL = function () {};
+RegisterIcons();
 
 // In Enzyme 3, shallow now by default triggers life cycle methods :thumbs-up: but this ruins all our tests :thumbs-down:
 configure({ adapter: new Adapter(), disableLifecycleMethods: true });
