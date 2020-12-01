ce10cbd1915f8e2ea6a4437d63c72db804655d11
Jonathan Hill <hill_jonathan@bah.com>
2020-12-01 11:15:41 -0500

    show about the data agencies page in qat

--- src/js/containers/router/RouterRoutes.js
+++ src/js/containers/router/RouterRoutes.js
@@ -172,7 +172,7 @@ export const routes = [
         path: '/about-the-data/agencies',
         component: AgenciesContainer,
         exact: true,
-        hide: !kGlobalConstants.DEV
+        hide: !kGlobalConstants.DEV && !kGlobalConstants.QAT // Not DEV and not QAT === Production, so we hide
     },
     {
         path: '/about-the-data/agency/:agencyId',
