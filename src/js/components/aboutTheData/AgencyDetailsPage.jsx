a2159d05ffbe97b27ea3d3909d836e25609473e0
Lizzie Salita <salita_elizabeth@bah.com>
2020-12-04 12:14:11 -0500

    populate agency overview info with mock data

--- src/js/components/aboutTheData/AgencyDetailsPage.jsx
+++ src/js/components/aboutTheData/AgencyDetailsPage.jsx
@@ -4,11 +4,11 @@
 
 import React, { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { LoadingMessage, ErrorMessage } from 'data-transparency-ui';
 
 import { agencyPageMetaTags } from 'helpers/metaTagHelper';
-import { fetchAgencyOverview } from 'helpers/agencyV2Helper';
+import { mockFetchAgencyOverview } from 'helpers/agencyV2Helper';
 
 import MetaTags from 'components/sharedComponents/metaTags/MetaTags';
 import Header from 'containers/shared/HeaderContainer';
@@ -17,6 +17,7 @@ import StickyHeader from 'components/sharedComponents/stickyHeader/StickyHeader'
 import Note from 'components/sharedComponents/Note';
 import AgencyDetailsContainer from 'containers/aboutTheData/AgencyDetailsContainer';
 import { modalTitles, modalClassNames } from 'dataMapping/aboutTheData/modals';
+import BaseAgencyOverview from 'models/v2/agencyV2/BaseAgencyOverview';
 import ExternalLink from 'components/sharedComponents/ExternalLink';
 import AboutTheDataModal from './AboutTheDataModal';
 
@@ -24,14 +25,14 @@ require('pages/aboutTheData/agencyDetailsPage.scss');
 
 const message = 'All numeric figures in this table are calculated based on the set of TAS owned by each agency, as opposed to the set of TAS that the agency directly reported to USAspending.gov. In the vast majority of cases, these are exactly the same (upwards of 95% of TAS—with these TAS representing over 99% of spending—are submitted and owned by the same agency). This display decision is consistent with our practice throughout the website of grouping TAS by the owning agency rather than the reporting agency. While reporting agencies are not identified in this table, they are available in the Custom Account Download in the reporting_agency_name field.';
 
-const propTypes = {
-    agencyCode: PropTypes.string
-};
-
 const AgencyDetailsPage = () => {
     const { agencyCode } = useParams();
-    // TODO - write agency overview data model
-    const [agencyOverview, setAgencyOverview] = useState({ name: '', toptier_code: '', website: '' });
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
+    const initialAgency = Object.create(BaseAgencyOverview);
+    initialAgency.populate();
+    const [agencyOverview, setAgencyOverview] = useState(Object.create(initialAgency));
     const [showModal, setShowModal] = useState('');
     const [modalAgency, setModalAgency] = useState('');
 
@@ -42,14 +43,19 @@ const AgencyDetailsPage = () => {
     const closeModal = () => setShowModal('');
 
     useEffect(() => {
-        // request overview for agency
-        const agencyOverviewRequest = fetchAgencyOverview(agencyCode);
+        // TODO - replace mock API request
+        const agencyOverviewRequest = mockFetchAgencyOverview(agencyCode);
         agencyOverviewRequest.promise
             .then((res) => {
-                // TODO - parse the response using our data model
-                setAgencyOverview(res.data);
+                const agency = Object.create(BaseAgencyOverview);
+                agency.populate(res.data);
+                setAgencyOverview(agency);
+                setLoading(false);
             }).catch((err) => {
                 console.error(err);
+                setErrorMessage(err.message);
+                setLoading(false);
+                setError(true);
             });
     }, [agencyCode]);
 
@@ -65,32 +71,42 @@ const AgencyDetailsPage = () => {
                 </div>
             </StickyHeader>
             <main id="main-content" className="main-content">
-                <div className="heading-container">
-                    <div className="back-link">
-                        <a href="/about-the-data/agencies"><FontAwesomeIcon icon="angle-left" />&nbsp;Back to All Agencies</a>
-                    </div>
-                    <h2 className="header">{agencyOverview.name}</h2>
-                    <div className="agency-info">
-                        <div className="agency-info__group">
-                            <h5>Agency Contact Information</h5>
-                            <div className="more-info-note">Contact this Agency with questions about their submissions</div>
-                            <div className="agency-info__website">
-                                <ExternalLink url={agencyOverview.website} />
+                {loading && <LoadingMessage />}
+                {error && <ErrorMessage description={errorMessage} />}
+                {(!loading && !error) && (
+                    <>
+                        <div className="heading-container">
+                            <div className="back-link">
+                                <a href="/about-the-data/agencies"><FontAwesomeIcon icon="angle-left" />&nbsp;Back to All Agencies</a>
+                            </div>
+                            <h2 className="header">{agencyOverview.name}</h2>
+                            <div className="agency-info">
+                                {agencyOverview.website && (
+                                    <div className="agency-info__group">
+                                        <h5>Agency Contact Information</h5>
+                                        <div className="more-info-note">Contact this Agency with questions about their submissions</div>
+                                        <div className="agency-info__website">
+                                            <ExternalLink url={agencyOverview.website} />
+                                        </div>
+                                    </div>
+                                )}
+                                {agencyOverview.id && (
+                                    <div className="agency-info__group">
+                                        <h5>Agency Profile Page</h5>
+                                        <div className="more-info-note">Learn more about this Agency&#39;s spending</div>
+                                        <div className="agency-info__website">
+                                            <Link to={`/agency/${agencyOverview.id}`}>
+                                                {agencyOverview.name}
+                                            </Link>
+                                        </div>
+                                    </div>
+                                )}
                             </div>
                         </div>
-                        <div className="agency-info__group">
-                            <h5>Agency Profile Page</h5>
-                            <div className="more-info-note">Learn more about this Agency&#39;s spending</div>
-                            <div className="agency-info__website">
-                                <Link to={`/agency/${agencyOverview.toptier_code}`}>
-                                    {agencyOverview.name}
-                                </Link>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <AgencyDetailsContainer agencyName={agencyOverview.name} modalClick={modalClick} />
-                <Note message={message} />
+                        <AgencyDetailsContainer agencyName={agencyOverview.name} modalClick={modalClick} />
+                        <Note message={message} />
+                    </>
+                )}
                 <AboutTheDataModal
                     mounted={!!showModal.length}
                     type={showModal}
@@ -107,5 +123,4 @@ const AgencyDetailsPage = () => {
     );
 };
 
-AgencyDetailsPage.propTypes = propTypes;
 export default AgencyDetailsPage;
