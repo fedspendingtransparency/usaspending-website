21720cefbd4c2d83610d727cfaa82d0e5dc33477
Lizzie Salita <salita_elizabeth@bah.com>
2020-12-10 11:50:12 -0500

    format requests with query-string library

--- src/js/helpers/aboutTheDataHelper.js
+++ src/js/helpers/aboutTheDataHelper.js
@@ -3,24 +3,20 @@
  * Created by Jonathan Hill 11/20/20
  */
 
+import { stringify } from 'query-string';
 import { calculatePercentage, formatMoney } from 'helpers/moneyFormatter';
 import { apiRequest } from './apiRequest';
 
-export const aboutTheDataQueryString = (params) => {
-    if (!Object.keys(params).length) return '';
-    return `?${params.fiscalYear ? `fiscal_year=${params.fiscalYear}` : ''}${params.fiscalPeriod ? `&fiscal_period=${params.fiscalPeriod}` : ''}${params.search ? `&search=${encodeURIComponent(params.search)}` : ''}${params.page ? `&page=${params.page}` : ''}${params.limit ? `&limit=${params.limit}` : ''}${params.order ? `&order=${params.order}` : ''}${params.sort ? `&sort=${params.sort}` : ''}`;
-};
-
 export const fetchPublishDates = (params) => apiRequest({
-    url: `v2/reporting/agencies/${params.agencyCode}/publish_dates/${aboutTheDataQueryString(params)}`
+    url: `v2/reporting/agencies/${params.agencyCode}/publish_dates/?${stringify(params)}`
 });
 
 export const fetchMissingAccountBalances = (params) => apiRequest({
-    url: `v2/reporting/agencies/${params.agencyCode}/discrepancies/${aboutTheDataQueryString(params)}`
+    url: `v2/reporting/agencies/${params.agencyCode}/discrepancies/?${stringify(params)}`
 });
 
 export const fetchReportingDifferences = (params) => apiRequest({
-    url: `/api/v2/reporting/agencies/{agency_code}/differences/${aboutTheDataQueryString(params)}`
+    url: `/api/v2/reporting/agencies/{agency_code}/differences/?${stringify(params)}`
 });
 
 export const dateFormattedMonthDayYear = (date) => {
@@ -235,6 +231,6 @@ export const fetchAgencies = () => ({
 
 export const fetchAgency = (params) => apiRequest({
     isMocked: true,
-    url: `v2/reporting/agencies/${params.agencyCode}/overview/${aboutTheDataQueryString(params)}`
+    url: `v2/reporting/agencies/${params.agencyCode}/overview/?${stringify(params)}`
 });
 
