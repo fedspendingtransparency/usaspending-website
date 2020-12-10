27e6bb850fa4b5fb6a4f5d526fae0cb7e102119c
Lizzie Salita <salita_elizabeth@bah.com>
2020-12-10 11:51:47 -0500

    removed tests

--- tests/helpers/aboutTheDataHelper-test.js
+++ tests/helpers/aboutTheDataHelper-test.js
@@ -26,39 +26,6 @@ const defaultParams = {
 };
 
 describe('About The Data Helper', () => {
-    describe('aboutTheDataQueryString', () => {
-        it('should return empty string when there are no params', () => {
-            expect(aboutTheDataQueryString({})).toBe('');
-        });
-        it('should format a query string with all params', () => {
-            expect(aboutTheDataQueryString(defaultParams).includes('?')).toBeTruthy();
-            expect(aboutTheDataQueryString(defaultParams).includes('fiscal_year=2020')).toBeTruthy();
-            expect(aboutTheDataQueryString(defaultParams).includes('&fiscal_period=8')).toBeTruthy();
-            expect(aboutTheDataQueryString(defaultParams).includes('&search=yolo')).toBeTruthy();
-            expect(aboutTheDataQueryString(defaultParams).includes('&page=2')).toBeTruthy();
-            expect(aboutTheDataQueryString(defaultParams).includes('&limit=10')).toBeTruthy();
-            expect(aboutTheDataQueryString(defaultParams).includes('&order=desc')).toBeTruthy();
-            expect(aboutTheDataQueryString(defaultParams).includes('&sort=publication_date')).toBeTruthy();
-        });
-        it('should not include any params not passed', () => {
-            delete defaultParams.page;
-            expect(aboutTheDataQueryString(defaultParams).includes('?')).toBeTruthy();
-            expect(aboutTheDataQueryString(defaultParams).includes('fiscal_year=2020')).toBeTruthy();
-            expect(aboutTheDataQueryString(defaultParams).includes('&fiscal_period=8')).toBeTruthy();
-            expect(aboutTheDataQueryString(defaultParams).includes('&search=yolo')).toBeTruthy();
-            expect(aboutTheDataQueryString(defaultParams).includes('&page=2')).toBeFalsy();
-            expect(aboutTheDataQueryString(defaultParams).includes('page=2')).toBeFalsy();
-            expect(aboutTheDataQueryString(defaultParams).includes('page')).toBeFalsy();
-            expect(aboutTheDataQueryString(defaultParams).includes('page=')).toBeFalsy();
-            expect(aboutTheDataQueryString(defaultParams).includes('&limit=10')).toBeTruthy();
-            expect(aboutTheDataQueryString(defaultParams).includes('&order=desc')).toBeTruthy();
-            expect(aboutTheDataQueryString(defaultParams).includes('&sort=publication_date')).toBeTruthy();
-        });
-        it('should encode the search param', () => {
-            defaultParams.search = 'beast mode';
-            expect(aboutTheDataQueryString(defaultParams).includes('&search=beast%20mode')).toBeTruthy();
-        });
-    });
     describe('dateFormattedMonthDayYear', () => {
         it('should return null if falsy is passed', () => {
             expect(dateFormattedMonthDayYear('')).toBeNull();
