3a8ccc5be236b791a384ab1efb860c6636cdcf3c
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2020-12-14 10:37:17 -0500

    [DEV-6449] discrepancyCount pointing at correct property on API response

--- src/js/models/v2/aboutTheData/BaseAgencyRow.js
+++ src/js/models/v2/aboutTheData/BaseAgencyRow.js
@@ -12,7 +12,8 @@ const BaseAgencyRow = {
         this._abbreviation = data.abbreviation || '';
         this._code = data.code || '';
         this._budgetAuthority = data.current_total_budget_authority_amount || 0;
-        this._discrepancyCount = data.discrepancy_count || 0;
+        // eslint-disable-next-line camelcase
+        this._discrepancyCount = data.tas_account_discrepancies_totals?.missing_tas_accounts_count || 0;
         this._obligationDifference = data.obligation_difference || 0;
         this._publicationDate = data.recent_publication_date || null;
         this.certified = data.recent_publication_date_certified || false;
