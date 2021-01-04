91fe8c80d454f4d872cc475f827894c195b3a25b
Lizzie Salita <salita_elizabeth@bah.com>
2021-01-04 15:54:41 -0500

    Added PDF link to Data Sources section; removed phased COVID release logic

--- src/js/components/covid19/DataSourcesAndMethodology.jsx
+++ src/js/components/covid19/DataSourcesAndMethodology.jsx
@@ -6,8 +6,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import kGlobalConstants from 'GlobalConstants';
-
 const DataSourcesAndMethodology = () => (
     <div className="heading__container information-body">
         <div className="information-top dsm--border-radius" />
@@ -15,7 +13,8 @@ const DataSourcesAndMethodology = () => (
             <h2 className="dsm__heading">Data Sources &amp; Methodology</h2>
             <div className="dsm__topSection">
                 <p>Government spending funded by COVID-19 supplemental appropriations is tracked through the use of Disaster Emergency Fund Codes (DEFC). This page brings together agency financial and award data submitted monthly, covering activity beginning April 1, 2020. The data in all tables and visualizations are aggregates of all records tagged with an appropriate COVID-19 DEFC.</p>
-                <p>Learn more about our <Link to="/disaster/covid-19/data-sources">data sources and calculation methods</Link>, including details about our COVID-19 download options. {!kGlobalConstants.CARES_ACT_RELEASED_2 && <strong>Check back for more information in the coming days as we continue to update the data on this page.</strong>}</p>
+                <p>Learn more about our <Link to="/disaster/covid-19/data-sources">data sources and calculation methods</Link>, including details about our COVID-19 download options.</p>
+                <p>USAspending is working with federal agencies to address known limitations in COVID-19 spending data. See a <a target="_blank" href="data/data-limitations.pdf" rel="noopener noreferrer">full description</a> of this issue.</p>
             </div>
         </div>
     </div>
