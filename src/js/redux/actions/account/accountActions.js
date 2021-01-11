bd39df48ff6a2b4aeacbfc0474038959595856fc
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2021-01-11 11:12:28 -0500

    [DEV-6609] updating redux.accounts for latest submission period data.
    Really, h/t jhill, we really  just need the periods themselves
    and the consumers of this data can infer what they need:
    
    - date string representing data as of certain date
    - active FY
    - latest period etc...
    
    using helper fns in helpers/account

--- src/js/redux/actions/account/accountActions.js
+++ src/js/redux/actions/account/accountActions.js
@@ -12,17 +12,6 @@ export const resetAccount = () => ({
     type: 'RESET_ACCOUNT'
 });
 
-/**
- * setAccountDataAsOfDate
- * @param {Object} payload: moment representing the latest submission period
- * @returns {Object}: like every redux action creator --> an object w/ a type property and one or more
- * other properties which will constitute the new redux state.
- */
-export const setAccountDataAsOfDate = (payload) => ({
-    type: 'SET_ACCOUNT_DATA_AS_OF',
-    payload
-});
-
 export const setSubmissionPeriods = (submissionPeriods) => ({
     type: 'SET_SUBMISSION_PERIODS',
     submissionPeriods
