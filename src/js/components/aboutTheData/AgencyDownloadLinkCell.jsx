1d28d86e0ab8729bd65c77ec2dc0e95845d725f1
Lizzie Salita <salita_elizabeth@bah.com>
2021-01-12 12:05:17 -0500

    handle a falsy value for the url

--- src/js/components/aboutTheData/AgencyDownloadLinkCell.jsx
+++ src/js/components/aboutTheData/AgencyDownloadLinkCell.jsx
@@ -6,16 +6,18 @@ const propTypes = {
     file: PropTypes.string
 };
 
-const AgencyDownloadLinkCell = ({ file }) => (
-    <a
-        className="agency-table-download"
-        target="_blank"
-        href={file}
-        rel="noopener noreferrer"
-        aria-label="Raw quarterly submission files">
-        <FontAwesomeIcon icon="file-alt" />Download
-    </a>
-);
+const AgencyDownloadLinkCell = ({ file }) => {
+    return file ? (
+        <a
+            className="agency-table-download"
+            target="_blank"
+            href={file}
+            rel="noopener noreferrer"
+            aria-label="Raw quarterly submission files">
+            <FontAwesomeIcon icon="file-alt" />Download
+        </a>
+    ) : '--';
+}
 
 AgencyDownloadLinkCell.propTypes = propTypes;
 export default AgencyDownloadLinkCell;
