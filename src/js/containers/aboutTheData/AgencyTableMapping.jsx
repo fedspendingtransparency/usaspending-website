93c2e77e65e848608efad8f2271e46b7296958b6
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2021-01-20 10:45:56 -0500

    [DEV-6464] centering the table loading container & fixing tests & right-align column

--- src/js/containers/aboutTheData/AgencyTableMapping.jsx
+++ src/js/containers/aboutTheData/AgencyTableMapping.jsx
@@ -88,35 +88,40 @@ export const agenciesTableColumns = {
             displayName: "Number of TAS Missing from Account Balance Data",
             icon: (
                 <Tooltip title="Number of TASs Missing from Account Balance Data" />
-            )
+            ),
+            right: true
         },
         {
             title: "obligation_difference",
             displayName: "Reporting Difference in Obligations",
             icon: (
                 <Tooltip title="Reporting Difference in Obligations" />
-            )
+            ),
+            right: true
         },
         {
             title: "unlinked_contract_award_count",
             displayName: "Number of Unlinked Contract Awards",
             icon: (
                 <Tooltip title="Number of Unlinked Contract Awards" />
-            )
+            ),
+            right: true
         },
         {
             title: "unlinked_assistance_award_count",
-            displayName: "Count of Unlinked Assistance Awards",
+            displayName: "Number of Unlinked Assistance Awards",
             icon: (
                 <Tooltip title="Number of Unlinked Assistance Awards" position="left" />
-            )
+            ),
+            right: true
         },
         {
             title: "assurance_statements",
             displayName: "Agency Comments",
             icon: (
                 <Tooltip title="Agency Comments" position="left" />
-            )
+            ),
+            right: true
         }
     ]
 };
