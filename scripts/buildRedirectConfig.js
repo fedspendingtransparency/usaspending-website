c6b7d74efccc59ca1757552096e5d8263d1a52fa
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2021-02-05 12:16:38 -0500

    [OPS-1636] New Script buildRedirectConfig:
    - uses dataMapping to build redirect .json -- redirectURLByURLRegex
    - adds script to npm scripts
    - webpack now expects this .json to exist and copies it into the build
    artifacts
    - .json is gitignored

---
+++ scripts/buildRedirectConfig.js
@@ -0,0 +1,28 @@
+import fs from 'fs';
+import path from 'path';
+import { stateNameByFipsId } from '../src/js/dataMapping/state/stateNames';
+import { URLifyStateName } from '../src/js/helpers/stateHelper';
+
+const legacyRedirects = {
+    "/^Pages/Default.aspx/": "/#",
+    "/^Pages/AdvancedSearch.aspx/": "/search",
+    "/^DownloadCenter/Pages/DataDownload.aspx/": "/download_center/custom_award_data",
+    "/^DownloadCenter/Pages/dataarchives.aspx/": "/download_center/award_data_archive",
+    "/^covid-19/": "/disaster/covid-19"
+};
+
+const buildPageRedirectByUrlRegex = () => {
+    const stateRedirects = Object.entries(stateNameByFipsId)
+        .reduce((acc, [fipsId, stateName]) => {
+            return {
+                ...acc,
+                [`^/state/${fipsId}`]: `/state/${URLifyStateName(stateName)}`
+            };
+        }, legacyRedirects);
+
+    const file = fs.createWriteStream(path.resolve(__dirname, "../redirect-config.json"));
+    file.write(JSON.stringify(stateRedirects));
+    file.end();
+};
+
+buildPageRedirectByUrlRegex();
