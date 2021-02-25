fac83cb87fcd433c93b87703ae12b1fb0a42c35b
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2021-02-25 15:38:53 -0500

    [DEV-6552] Handling falsy values from API for data that represents dollar amounts:
    CoreReportingRow.js -- preserving raw values in our underscore
    prefixed model properties representing dollar amounts:
    
    1. _budgetAuthority
    2. _gtasObligationTotal
    3. _obligationDifference
    
    (1 & 2) use `formatMoneyWithPrecision` which has test coverage and
    refactoring for this purpose in this PR.
    (3) already has this null check covered [here](https://github.com/fedspendingtransparency/usaspending-website/blob/dev/src/js/helpers/aboutTheDataHelper.js#L155)
    
    These values are later passed to helper functions that parse the falsy
    values for us.
    
    + tests added to confirm the return value for various falsy values
    
    moneyFormatter.js -- formatMoneyWithPrecision refactored to
    allow the caller to specify a return value when invalid value is
    passed. This follows the precedent set by calculatePercentage.
    
    + tests added to confirm the return value for various falsy values

--- src/js/models/v2/aboutTheData/CoreReportingRow.js
+++ src/js/models/v2/aboutTheData/CoreReportingRow.js
@@ -8,25 +8,25 @@ import { format } from 'date-fns';
 
 const CoreReportingRow = {
     populateCore(data) {
-        this._budgetAuthority = data.current_total_budget_authority_amount || 0;
+        this._budgetAuthority = data.current_total_budget_authority_amount;
         this._mostRecentPublicationDate = data.recent_publication_date || null;
         /* eslint-disable camelcase */
-        this._gtasObligationTotal = data.tas_account_discrepancies_totals?.gtas_obligation_total || 0;
+        this._gtasObligationTotal = data.tas_account_discrepancies_totals?.gtas_obligation_total;
         this._discrepancyCount = data.tas_account_discrepancies_totals?.missing_tas_accounts_count || 0;
         /* eslint-enable camelcase */
-        this._obligationDifference = data.obligation_difference || 0;
+        this._obligationDifference = data.obligation_difference;
         this._unlinkedContracts = data.unlinked_contract_award_count || 0;
         this._unlinkedAssistance = data.unlinked_assistance_award_count || 0;
         this.assuranceStatement = data.assurance_statement_url || '';
     },
     get budgetAuthority() {
-        return formatMoneyWithPrecision(this._budgetAuthority, 2);
+        return formatMoneyWithPrecision(this._budgetAuthority, 2, '--');
     },
     get mostRecentPublicationDate() {
         return this._mostRecentPublicationDate ? format(new Date(this._mostRecentPublicationDate), 'MM/dd/yyyy') : '--';
     },
     get obligationDifference() {
-        return formatMoneyWithPrecision(this._obligationDifference, 2);
+        return formatMoneyWithPrecision(this._obligationDifference, 2, '--');
     },
     get discrepancyCount() {
         return formatNumber(this._discrepancyCount);
