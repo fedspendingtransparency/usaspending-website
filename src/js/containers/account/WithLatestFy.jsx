4b76eaa395421a32f8a2fc6aa2d499b866799ef2
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2021-02-25 18:19:33 -0500

    [DEV-4521] new hook: useValideTimeBasedQueryParams
    Q: What does it do?
    A: Takes query params and returns them as state after validating them in
    comparison with the latest available data. It also updates the URL for
    you.
    
    Improvements:
    1. If only one param is invalid, it will only update the invalid param.
    In the case where you provide a valid FY and invalid period, however,
    the period is set to the latest absolute period (Based on actual current
    FY) rather than to the latest period available within whatever valid FY
    was provided. I'm not gonna worry about this ATM.
    
    2. if a period param is invalid because its too small, like a zero or
    negative we are now handling that with an added condtion (> 0)
    in the helper isPeriodVisible
    
    Tests added for custom hook to cases where:
    
    - when params are undefined
    - params are defined but invalid

--- src/js/containers/account/WithLatestFy.jsx
+++ src/js/containers/account/WithLatestFy.jsx
@@ -1,10 +1,17 @@
 import React, { useEffect, useRef, useState } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
 import { isCancel } from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
 import { get } from 'lodash';
 
 import { setSubmissionPeriods } from 'redux/actions/account/accountActions';
 import { getLatestPeriodAsMoment, getLatestPeriod, fetchAllSubmissionDates } from 'helpers/accountHelper';
+import {
+    isPeriodVisible,
+    isPeriodSelectable,
+    getLastPeriodWithinQuarterByPeriod,
+    getPeriodWithTitleById
+} from "helpers/aboutTheDataHelper";
 
 // TODO: Refactor existing consumers of WithLatestFy to use this custom hook
 export const useLatestAccountData = () => {
@@ -63,6 +70,102 @@ export const useLatestAccountData = () => {
         latestPeriod,
         isLoading,
         errorMsg
+    ];
+};
+
+export const useQueryParams = () => Object.fromEntries(new URLSearchParams(useLocation().search));
+
+/*
+    * useValidTimeBasedQueryParams
+    * this function enforces validation logic for fiscal years and fiscal periods in the URL as query params
+    * by using the latest submission periods as a reference.
+*/
+export const useValidTimeBasedQueryParams = (currentUrlFy, currentUrlPeriod = null, requiredParams = ['fy', 'period']) => {
+    const history = useHistory();
+    const existingParams = useQueryParams();
+    const [, submissionPeriods, latestSubmission] = useLatestAccountData();
+    const { year: latestFy, period: latestPeriod } = latestSubmission;
+    const [{ period, fy }, setYearAndPeriod] = useState({ period: '', fy: '' });
+
+    const updateUrl = (newParamsAsObj) => {
+        setYearAndPeriod(newParamsAsObj);
+        history.replace({
+            pathname: ``,
+            search: `?${new URLSearchParams({ ...existingParams, ...newParamsAsObj }).toString()}`
+        });
+    };
+
+    const handleTimeChange = (y, p = null) => {
+        if (y && p) {
+            updateUrl({ fy: `${y}`, period: `${p}` });
+        }
+        else if (y) {
+            updateUrl({ fy: `${y}` });
+        }
+        else if (p) {
+            updateUrl({ period: `${p}` });
+        }
+    };
+
+    useEffect(() => {
+        // Handles defining undefined params
+        const isDataReadyForLatest = (submissionPeriods.size && latestFy && latestPeriod);
+        const periodAndFyRequired = (requiredParams.includes('fy') && requiredParams.includes('period'));
+        if (isDataReadyForLatest && requiredParams.some((p) => !existingParams[p])) {
+            if (periodAndFyRequired && !existingParams.fy && existingParams.period) {
+                handleTimeChange(latestFy);
+            }
+            // only need to update the fy
+            else if (periodAndFyRequired && existingParams.fy && !existingParams.period) {
+                handleTimeChange(null, latestPeriod);
+            }
+            // only need to update the period
+            else if (periodAndFyRequired && !existingParams.fy && !existingParams.period) {
+                handleTimeChange(latestFy, latestPeriod);
+            }
+            else if (isDataReadyForLatest && requiredParams.includes('fy') && !existingParams.fy) {
+                handleTimeChange(latestFy);
+            }
+            else if (isDataReadyForLatest && requiredParams.includes('period') && !existingParams.period) {
+                handleTimeChange(null, latestPeriod);
+            }
+        }
+    }, [history, latestFy, latestPeriod, submissionPeriods.size, currentUrlFy, currentUrlPeriod]);
+
+
+    useEffect(() => {
+        // Handles validating defined params
+        if (submissionPeriods.size && latestFy && latestPeriod && requiredParams.every((p) => existingParams[p])) {
+            const availablePeriodsInFy = submissionPeriods.toJS().filter(({ submission_fiscal_year: y }) => parseInt(currentUrlFy, 10) === y);
+            if (availablePeriodsInFy.length && currentUrlPeriod) {
+                // fy selection is valid but what about the period? 🤔
+                const validPeriod = isPeriodVisible(availablePeriodsInFy, currentUrlPeriod)
+                    ? currentUrlPeriod
+                    : `${latestPeriod}`;
+                const selectablePeriod = isPeriodSelectable(availablePeriodsInFy, validPeriod)
+                    ? validPeriod
+                    : getLastPeriodWithinQuarterByPeriod(validPeriod);
+                handleTimeChange(currentUrlFy, selectablePeriod);
+            }
+            else if (currentUrlPeriod) {
+                // invalid time selection, use the latest available fy/period 👌
+                handleTimeChange(latestFy, latestPeriod);
+            }
+            else if (availablePeriodsInFy.length) {
+                handleTimeChange(currentUrlFy);
+            }
+            else {
+                handleTimeChange(latestFy);
+            }
+        }
+    }, [submissionPeriods, currentUrlFy, currentUrlPeriod, latestPeriod, latestFy]);
+
+    if (requiredParams.length === 1 && requiredParams[0] === 'fy') return [fy, updateUrl];
+
+    return [
+        fy,
+        period && latestPeriod ? getPeriodWithTitleById(period, latestPeriod) : period,
+        updateUrl
     ];
 };
 
