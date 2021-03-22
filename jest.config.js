885342e3ccc9acee80262dd9ad029e5eae42e175
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2021-03-22 12:35:53 -0400

    Circle CI Migration
    - Adding .circleci/config.yml
    - Using junit for jest
    - Using new cli flag maxWorkers for our tests to prevent memory leakage
    while using parallel containers for our tests

--- jest.config.js
+++ jest.config.js
@@ -23,6 +23,10 @@ module.exports = {
         "!node_modules/**",
         "!public/**"
     ],
+    reporters: [
+        "default",
+        "jest-junit"
+    ],
     moduleNameMapper: {
         "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
         "<rootDir>/__mocks__/fileMock.js",
