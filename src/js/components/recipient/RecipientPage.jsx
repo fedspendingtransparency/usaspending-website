1e9417eaed071de05e0c07e6b3e882f7ff33cf69
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2021-03-31 18:14:48 -0400

    [DEV-6889] Recipient Page Implementation

--- src/js/components/recipient/RecipientPage.jsx
+++ src/js/components/recipient/RecipientPage.jsx
@@ -3,24 +3,23 @@
  * Created by Lizzie Salita 8/23/17
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { PageHeader } from 'data-transparency-ui';
 
 import { recipientPageMetaTags } from 'helpers/metaTagHelper';
+import { getStickyBreakPointForSidebar } from "helpers/stickyHeaderHelper";
 
 import Footer from 'containers/Footer';
 import MetaTags from 'components/sharedComponents/metaTags/MetaTags';
 import Header from 'containers/shared/HeaderContainer';
-import StickyHeader from 'components/sharedComponents/stickyHeader/StickyHeader';
-import ShareIcon from 'components/sharedComponents/stickyHeader/ShareIcon';
 import Error from 'components/sharedComponents/Error';
 import { LoadingWrapper } from "components/sharedComponents/Loading";
-import { getBaseUrl } from "helpers/socialShare";
+import { getBaseUrl, handleShareOptionClick } from "helpers/socialShare";
 
 import ChildRecipientModalContainer from 'containers/recipient/modal/ChildRecipientModalContainer';
+import { AlternateNamesRecipientModalContainer } from 'containers/recipient/modal/AlternateNamesRecipientModalContainer';
 import RecipientContent from './RecipientContent';
-import { AlternateNamesRecipientModalContainer } from '../../containers/recipient/modal/AlternateNamesRecipientModalContainer';
-
 
 const propTypes = {
     loading: PropTypes.bool,
@@ -30,78 +29,76 @@ const propTypes = {
     pickedFy: PropTypes.func
 };
 
-export default class RecipientPage extends React.Component {
-    constructor(props) {
-        super(props);
+export const RecipientPage = ({
+    id,
+    recipient,
+    loading,
+    error
+}) => {
+    const [isChildModalVisible, showChildModal] = useState(false);
+    const [isAlternateModalVisible, showAlternateRecipientModal] = useState(false);
 
-        this.state = {
-            showChildRecipientModal: false,
-            showAlternateNamesRecipientModal: false
-        };
+    const showAlternateModal = () => showAlternateRecipientModal(true);
+    const hideAlternateModal = () => showAlternateRecipientModal(false);
+    const showChildRecipientModal = () => showChildModal(true);
+    const hideChildRecipientModal = () => showChildModal(false);
+
+    const slug = `recipient/${id}/${recipient.fy}`;
+    const emailArgs = {
+        subject: `USAspending.gov Recipient Profile: ${recipient.overview.name}`,
+        body: `View the spending activity for this recipient on USAspending.gov: ${getBaseUrl(slug)}`
+    };
+
+    const handleShare = (name) => {
+        handleShareOptionClick(name, slug, emailArgs);
+    };
+
+    let content = (
+        <RecipientContent
+            id={id}
+            showChildRecipientModal={showChildRecipientModal}
+            showAlternateNamesRecipientModal={showAlternateModal}
+            recipient={recipient}
+            loading={loading}
+            error={error} />
+    );
+    if (error) {
+        content = (<Error
+            title="Invalid Recipient"
+            message="The recipient ID provided is invalid. Please check the ID and try again." />);
     }
 
-    showAlternateNamesRecipientModal = () => this.setState({ showAlternateNamesRecipientModal: true });
 
-    hideAlternateNamesRecipientModal = () => this.setState({ showAlternateNamesRecipientModal: false });
-
-    showChildRecipientModal = () => this.setState({ showChildRecipientModal: true });
-
-    hideChildRecipientModal = () => this.setState({ showChildRecipientModal: false });
-
-    render() {
-        const { id, recipient, loading } = this.props;
-        const slug = `recipient/${id}/${recipient.fy}`;
-        let content = (
-            <RecipientContent
-                showChildRecipientModal={this.showChildRecipientModal}
-                showAlternateNamesRecipientModal={this.showAlternateNamesRecipientModal}
-                {...this.props} />
-        );
-        if (this.props.error) {
-            content = (<Error
-                title="Invalid Recipient"
-                message="The recipient ID provided is invalid. Please check the ID and try again." />);
-        }
-
-        return (
-            <div className="usa-da-recipient-page">
-                {recipient.overview.id && !loading && <MetaTags {...recipientPageMetaTags(this.props.recipient.overview)} />}
-                <Header />
-                <StickyHeader>
-                    <div className="sticky-header__title">
-                        <h1 tabIndex={-1} id="main-focus">
-                            Recipient Profile
-                        </h1>
-                    </div>
-                    <div className="sticky-header__toolbar">
-                        <ShareIcon
-                            slug={slug}
-                            email={{
-                                subject: `USAspending.gov Recipient Profile: ${recipient.overview.name}`,
-                                body: `View the spending activity for this recipient on USAspending.gov: ${getBaseUrl(slug)}`
-                            }} />
-                    </div>
-                </StickyHeader>
-                <main
-                    id="main-content"
-                    className="main-content">
-                    <LoadingWrapper isLoading={this.props.loading}>
+    return (
+        <div className="usa-da-recipient-page">
+            {recipient.overview.id && !loading && <MetaTags {...recipientPageMetaTags(recipient.overview)} />}
+            <Header />
+            <PageHeader
+                overLine="Recipient Profile"
+                title={recipient.overview.name}
+                stickyBreakPoint={getStickyBreakPointForSidebar()}
+                shareProps={{
+                    url: getBaseUrl(slug),
+                    onShareOptionClick: handleShare
+                }}>
+                <main id="main-content" className="main-content">
+                    <LoadingWrapper isLoading={loading}>
                         {content}
                         <ChildRecipientModalContainer
-                            mounted={this.state.showChildRecipientModal}
-                            hideModal={this.hideChildRecipientModal}
-                            recipient={this.props.recipient} />
+                            mounted={isChildModalVisible}
+                            hideModal={hideChildRecipientModal}
+                            recipient={recipient} />
                         <AlternateNamesRecipientModalContainer
-                            mounted={this.state.showAlternateNamesRecipientModal}
-                            hideModal={this.hideAlternateNamesRecipientModal}
-                            recipient={this.props.recipient} />
+                            mounted={isAlternateModalVisible}
+                            hideModal={hideAlternateModal}
+                            recipient={recipient} />
                     </LoadingWrapper>
                 </main>
                 <Footer />
-            </div>
-        );
-    }
-}
+            </PageHeader>
+        </div>
+    );
+};
 
 RecipientPage.propTypes = propTypes;
 RecipientPage.defaultProps = {
@@ -111,3 +108,5 @@ RecipientPage.defaultProps = {
     recipient: {},
     pickedFy: () => { }
 };
+
+export default RecipientPage;
