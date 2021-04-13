1b2b5dd91e4cd79d866f490bdf73d4e81e742126
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2021-04-13 17:28:44 -0400

    [DEV-5137] Agency V2 BarChart selectedFY logic:
    (A) if selectedFy is lte than 2021, only show years lte 2021
    (B) if selectedFy gt 2021 only show the preceding 4 years
    
    + Tests

---
+++ tests/components/agency/overview/BarChart-test.jsx
@@ -0,0 +1,29 @@
+/**
+ * BarChart-test.jsx
+ * Created by Max Kendall 4/13/21
+ */
+
+import React from 'react';
+
+import { render, screen } from 'test-utils';
+import BarChart, { mockData } from 'components/agencyV2/overview/BarChart';
+
+test.each([
+    ['2026', ['FY 26', 'FY 25', 'FY 24', 'FY 23', 'FY 22']],
+    ['2025', ['FY 25', 'FY 24', 'FY 23', 'FY 22', 'FY 21']],
+    ['2024', ['FY 24', 'FY 23', 'FY 22', 'FY 21', 'FY 20']],
+    ['2023', ['FY 23', 'FY 22', 'FY 21', 'FY 20', 'FY 19']],
+    ['2022', ['FY 22', 'FY 21', 'FY 20', 'FY 19', 'FY 18']],
+    ['2021', ['FY 21', 'FY 20', 'FY 19', 'FY 18', 'FY 17']],
+    ['2020', ['FY 21', 'FY 20', 'FY 19', 'FY 18', 'FY 17']],
+    ['2019', ['FY 21', 'FY 20', 'FY 19', 'FY 18', 'FY 17']],
+    ['2018', ['FY 21', 'FY 20', 'FY 19', 'FY 18', 'FY 17']],
+    ['2017', ['FY 21', 'FY 20', 'FY 19', 'FY 18', 'FY 17']]
+])('when fy is %s we show these fy: %s', (selectedFy, arrOfDisplayedFy) => {
+    // idea is to only show preceding FY when selected FY is LTE 2021
+    render(<BarChart selectedFy={selectedFy} agencyBudgetByYear={mockData} />);
+    arrOfDisplayedFy
+        .forEach((str) => {
+            expect(screen.getByText(str)).toBeTruthy();
+        });
+});
