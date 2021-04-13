b16e19eb4a4164a6d4a498ffb6bc076ece618e53
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2021-04-13 17:28:44 -0400

    [DEV-5137] Agency V2 BarChart selectedFY logic:
    (A) if selectedFy is lte than 2021, only show years lte 2021
    (B) if selectedFy gt 2021 only show the preceding 4 years
    
    + Tests

--- src/js/components/agencyV2/overview/BarChart.jsx
+++ src/js/components/agencyV2/overview/BarChart.jsx
@@ -4,8 +4,39 @@
  */
 
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const mockData = [
+export const mockData = [
+    {
+        fiscal_year: 2026,
+        agency_budgetary_resources: 376290363576.32,
+        agency_total_obligated: 82847861416.69,
+        total_budgetary_resources: 9688581391799.6
+    },
+    {
+        fiscal_year: 2025,
+        agency_budgetary_resources: 376290363576.32,
+        agency_total_obligated: 82847861416.69,
+        total_budgetary_resources: 9688581391799.6
+    },
+    {
+        fiscal_year: 2024,
+        agency_budgetary_resources: 376290363576.32,
+        agency_total_obligated: 82847861416.69,
+        total_budgetary_resources: 9688581391799.6
+    },
+    {
+        fiscal_year: 2023,
+        agency_budgetary_resources: 376290363576.32,
+        agency_total_obligated: 82847861416.69,
+        total_budgetary_resources: 9688581391799.6
+    },
+    {
+        fiscal_year: 2022,
+        agency_budgetary_resources: 376290363576.32,
+        agency_total_obligated: 82847861416.69,
+        total_budgetary_resources: 9688581391799.6
+    },
     {
         fiscal_year: 2021,
         agency_budgetary_resources: 376290363576.32,
@@ -38,35 +69,50 @@ const mockData = [
     }
 ];
 
+const getLastFourYears = ({ year }, selectedFy) => {
+    if (parseInt(selectedFy, 10) <= 2021 && year <= 2021) return true;
+    if (parseInt(selectedFy, 10) > 2021 && year >= parseInt(selectedFy, 10) - 4) return true;
+    return false;
+};
+
 const BarChart = ({
-    obligationsByFy = mockData,
+    agencyBudgetByYear = mockData,
     selectedFy
 }) => {
     const renderBars = () => {
-        const highestYear = obligationsByFy.reduce((acc, obj) => {
-            if (obj.total_budgetary_resources > acc) return obj.total_budgetary_resources;
-            return acc;
-        }, 0);
-        return obligationsByFy
-            .sort((a, b) => a.fiscal_year - b.fiscal_year)
-            .map(({ fiscal_year: fy, total_budgetary_resources: total }) => {
+        const greatestAgencyBudget = agencyBudgetByYear
+            .filter((o) => getLastFourYears(o, selectedFy))
+            .reduce((acc, obj) => (obj.budget > acc ? obj.budget : acc), 0);
+        return agencyBudgetByYear
+            .filter((o) => getLastFourYears(o, selectedFy))
+            .sort((a, b) => a.year - b.year)
+            .map(({ year: fy, budget }, i) => {
+                const fyStr = String(fy);
                 return (
-                    <li className="bar-chart__bar">
+                    <li className="bar-chart__bar" key={i}>
                         <span
-                            className={`${String(fy) === selectedFy ? 'active-fy ' : ''}`}
+                            className={`${fyStr === selectedFy ? 'active-fy ' : ''}`}
                             style={{
-                                height: `${(total / highestYear) * 100}%`
+                                height: `${(budget / greatestAgencyBudget) * 100}%`
                             }} />
-                        <span>{`FY ${String(fy)[2]}${String(fy)[3]}`}</span>
+                        <span>{`FY ${fyStr[2]}${fyStr[3]}`}</span>
                     </li>
                 );
             });
-    }
+    };
     return (
         <ul className="viz-container bar-chart">
             {renderBars()}
         </ul>
     );
+};
+
+BarChart.propTypes = {
+    selectedFy: PropTypes.string.isRequired,
+    agencyBudgetByYear: PropTypes.arrayOf(PropTypes.shape({
+        fiscal_year: PropTypes.number.isRequired,
+        agency_budgetary_resources: PropTypes.number.isRequired
+    }))
 };
 
 export default BarChart;
