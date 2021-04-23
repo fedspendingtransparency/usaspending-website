2a2f0fd18f463b99949d3f6750b984690eccd359
Elizabeth Salita <salita_elizabeth@bah.com>
2021-04-23 13:15:13 -0400

    cleanup global constants

--- src/js/GlobalConstants.js
+++ src/js/GlobalConstants.js
@@ -3,30 +3,24 @@
  * Created by Maxwell Kendall 7/8/19
 */
 
-// This can be updated to remove "isDti" after DTI migration
-const isDti = process.env.USASPENDING_API.indexOf('.dti.') > -1;
 const filesServerUrlByEnv = {
-    dev: `https://files-nonprod.${isDti ? 'dti.' : ''}usaspending.gov`,
-    sandbox: `https://files-nonprod.${isDti ? 'dti.' : ''}usaspending.gov`,
-    qat: `https://files-nonprod.${isDti ? 'dti.' : ''}usaspending.gov`,
-    staging: `https://files-staging.${isDti ? 'dti.' : ''}usaspending.gov`,
-    prod: `https://files.${isDti ? 'dti.' : ''}usaspending.gov`
+    dev: 'https://files-nonprod.usaspending.gov',
+    sandbox: 'https://files-nonprod.usaspending.gov',
+    qat: 'https://files-nonprod.usaspending.gov',
+    staging: 'https://files-staging.usaspending.gov',
+    prod: 'https://files.usaspending.gov'
 };
 
 const globalConstants = {
     API: process.env.USASPENDING_API,
-    LOCAL_ROOT: "",
-    GITHUB: "",
     LOCAL: false,
     DEV: (process.env.ENV === 'dev' || process.env.ENV === 'sandbox'),
-    KEYWORD_AVAILABLE: true,
     PERF_LOG: false,
-    OVERRIDE_FISCAL_YEAR: false,
-    FISCAL_YEAR: 2017,
     MAPBOX_TOKEN: process.env.MAPBOX_TOKEN,
     QAT: (process.env.ENV === 'qat'),
     STAGING: (process.env.ENV === 'staging'),
     FILES_SERVER_BASE_URL: filesServerUrlByEnv[process.env.ENV],
+    ARP_RELEASED: process.env.ENV !== 'prod'
 };
 
 module.exports = globalConstants;
