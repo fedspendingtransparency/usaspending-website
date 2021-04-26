230592914db71960dc19d97af017887cdef2215a
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2021-04-26 15:09:45 -0400

    [tests-for-agencyv2] useStateWithPrevious hook Used when you want a reference to the previous state as well.
    + tests

---
+++ tests/helpers/index-test.js
@@ -0,0 +1,26 @@
+
+/**
+ * index-test.js
+ * Created by Max Kendall 04/26/2021
+* */
+import { act, renderHook } from '@testing-library/react-hooks';
+import { useStateWithPrevious } from 'helpers';
+
+test('useStateWithPrevious correctly maintains state and previous state', () => {
+    const { result } = renderHook(() => useStateWithPrevious(1));
+    expect(result.current[0]).toEqual(1);
+
+    act(() => {
+        result.current[2](2);
+    });
+
+    expect(result.current[1]).toEqual(2);
+    expect(result.current[0]).toEqual(1);
+
+    act(() => {
+        result.current[2](200);
+    });
+
+    expect(result.current[1]).toEqual(200);
+    expect(result.current[0]).toEqual(2);
+});
