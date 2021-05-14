d7f4ad9657a3011e7197db9d63ef86f1b82380d0
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2021-05-14 10:12:41 -0400

    [DEV-6210] production config updates:
    Removing chunk configurations. It appears the default chunk
    configuration works much better for our app than these.
    
    A "chunk" is a group of modules represented in a minified JS file.
    Previously, we had fewer, bigger chunks; now we more smaller chunks.
    Basically one per page.
    
    Another noteworthy change we've made is there is no "common" chunk.
    Meaning, we no longer have a single chunk made up of modules that are
    re-used by many other modules. Previously, this chunk was 1.21MB and
    it would result in a lot of unused code being loaded for a given chunk
    that depended on it.

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -28,28 +28,6 @@ module.exports = {
         modules: ["node_modules", path.resolve(__dirname, "../src/_scss")]
     },
     optimization: {
-        splitChunks: {
-            cacheGroups: {
-                default: false,
-                vendors: false,
-                // all imported code from node_modules is a single file
-                defaultVendors: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: 'vendors',
-                    chunks: 'all',
-                    priority: 20
-                },
-                // code shared between at least 2 modules, is put into a common chunk file
-                common: {
-                    name: 'common',
-                    minChunks: 2,
-                    chunks: 'all',
-                    priority: 10,
-                    reuseExistingChunk: true,
-                    enforce: true
-                }
-            }
-        },
         moduleIds: 'deterministic'
     },
     module: {
--- webpack/webpack.prod.config.js
+++ webpack/webpack.prod.config.js
@@ -1,8 +1,8 @@
 const merge = require('webpack-merge').merge;
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 const common = require('./webpack.common');
 
@@ -16,26 +16,13 @@ module.exports = merge(common, {
         version: true,
         warnings: true
     },
+    performance: {
+        hints: 'warning'
+    },
     optimization: {
         minimizer: [
-            new TerserPlugin({
-                cache: true,
-                parallel: true
-            })
-        ],
-        runtimeChunk: "single",
-        splitChunks: {
-            chunks: "all",
-            cacheGroups: {
-                styles: {
-                    // all css in one file -- https://github.com/webpack-contrib/mini-css-extract-plugin
-                    name: "styles",
-                    test: /\.css$/,
-                    chunks: "all",
-                    enforce: true
-                }
-            }
-        }
+            new CssMinimizerPlugin()
+        ]
     },
     module: {
         rules: [
@@ -48,7 +35,9 @@ module.exports = merge(common, {
                         loader: "sass-loader",
                         options: {
                             sourceMap: false,
-                            includePaths: ["./src/_scss", "./node_modules"]
+                            sassOptions: {
+                                includePaths: ["./src/_scss", "./node_modules"]
+                            }
                         }
                     }
                 ]
@@ -57,10 +46,8 @@ module.exports = merge(common, {
     },
     plugins: [
         new MiniCssExtractPlugin({
-            filename: "[name].[contenthash].css"
-        }),
-        new webpack.optimize.MinChunkSizePlugin({
-            minChunkSize: 300000
+            filename: "[name].[contenthash].css",
+            chunkFilename: '[id].css'
         }),
         new webpack.DefinePlugin({
             'process.env': {
