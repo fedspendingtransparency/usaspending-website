4cc885d0c85932f6a8e7c46baf5fba3390d60a9f
Jonathan Hill <hill_jonathan@bah.com>
2021-06-10 14:33:16 -0400

    dev-6210 migrate to node 14, remove dead packages

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,16 +1,19 @@
 module.exports = {
     ci: {
         upload: {
-            target: 'temporary-public-storage'
+            // target: 'temporary-public-storage'
+            target: 'lhci',
+            serverBaseURl: 'http://localhost:9001',
+            token: 'e4f41236-8dad-48f2-af21-38124c251441'
         },
         collect: {
             staticDistDir: './public', // location of static files for lighthouse CI to serve
             isSinglePageApplication: true,
             url: [ // the urls to run lighthouse against
-                'http://localhost/',
-                'http://localhost/search',
-                'http://localhost/disaster/covid-19',
-                'http://localhost/agency/456'
+                'http://localhost/'
+                // 'http://localhost/search',
+                // 'http://localhost/disaster/covid-19',
+                // 'http://localhost/agency/456'
             ]
         }
     }
