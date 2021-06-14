95db12024cba3cb826c0e5d671e2624a57a0efb7
Brett Varney <brett.varney@kc.frb.org>
2021-06-14 08:30:46 -0500

    4343 fix not redrawing sector border

--- src/js/components/agencyV2/visualizations/ObligationsByAwardType.jsx
+++ src/js/components/agencyV2/visualizations/ObligationsByAwardType.jsx
@@ -33,93 +33,96 @@ export default function ObligationsByAwardType({ outer, inner, windowWidth }) {
         const rect = chartRef.current.parentElement.getBoundingClientRect();
         if (rect.height !== chartRect[0] || rect.width !== chartRect[1]) {
             setChartRect([rect.height, rect.width]);
+            drawChart(rect.height, rect.width);
         };
     }, [windowWidth]);
 
-    const labelRadius = Math.min(chartRect[0], chartRect[1]) / 2;
-    const outerRadius = labelRadius * 0.7;
-    const outerStrokeWidth = 3;
-    const innerRadius = outerRadius - (outerStrokeWidth * 2);
+    const drawChart = (height, width) => {
+        const labelRadius = Math.min(height, width) / 2;
+        const outerRadius = labelRadius * 0.7;
+        const outerStrokeWidth = 3;
+        const innerRadius = outerRadius - (outerStrokeWidth * 2);
 
-    // clear & append the svg object to the div
-    d3.select('#obl_chart').selectAll('*').remove();
-    const svg = d3.select('#obl_chart')
-        .append('svg')
-        .attr('height', chartRect[0])
-        .attr('width', chartRect[1])
-        .append('g')
-        .attr('transform', `translate(${chartRect[1] / 2}, ${chartRect[0] / 2})`);
+        // clear & append the svg object to the div
+        d3.select('#obl_chart').selectAll('*').remove();
+        const svg = d3.select('#obl_chart')
+            .append('svg')
+            .attr('height', height)
+            .attr('width', width)
+            .append('g')
+            .attr('transform', `translate(${width / 2}, ${height / 2})`);
 
-    const outerData = outer.map((d) => d.value);
-    const outerPie = d3.pie().sortValues(null)(outerData);
-    const innerData = inner.map((d) => d.value);
-    const innerPie = d3.pie().sortValues(null)(innerData);
+        const outerData = outer.map((d) => d.value);
+        const outerPie = d3.pie().sortValues(null)(outerData);
+        const innerData = inner.map((d) => d.value);
+        const innerPie = d3.pie().sortValues(null)(innerData);
 
-    // rotate chart so midpoints are 127deg off vertical
-    const rotationAxis = 127;
-    const rotation = rotationAxis - ((outerPie[0].endAngle / Math.PI) * 90); // rad => deg
-    const chart = svg.append('g').attr('transform', `rotate (${rotation})`);
+        // rotate chart so midpoints are 127deg off vertical
+        const rotationAxis = 127;
+        const rotation = rotationAxis - ((outerPie[0].endAngle / Math.PI) * 90); // rad => deg
+        const chart = svg.append('g').attr('transform', `rotate (${rotation})`);
 
-    // outer ring
-    chart.selectAll()
-        .data(outerPie)
-        .enter()
-        .append('path')
-        .attr('d', d3.arc()
-            .outerRadius(outerRadius)
-            .innerRadius(outerRadius - outerStrokeWidth))
-        .attr('fill', (d, i) => outer[i].color);
+        // outer ring
+        chart.selectAll()
+            .data(outerPie)
+            .enter()
+            .append('path')
+            .attr('d', d3.arc()
+                .outerRadius(outerRadius)
+                .innerRadius(outerRadius - outerStrokeWidth))
+            .attr('fill', (d, i) => outer[i].color);
 
-    // inner ring
-    chart.selectAll()
-        .data(innerPie)
-        .enter()
-        .append('path')
-        .attr('d', d3.arc()
-            .outerRadius(innerRadius)
-            .innerRadius(innerRadius / 2)
-        )
-        .attr('fill', (d, i) => inner[i].color);
+        // inner ring
+        chart.selectAll()
+            .data(innerPie)
+            .enter()
+            .append('path')
+            .attr('d', d3.arc()
+                .outerRadius(innerRadius)
+                .innerRadius(innerRadius / 2)
+            )
+            .attr('fill', (d, i) => inner[i].color);
 
-    // border between categories
-    const borders = [[0, outerRadius], [0, 0], [outerPie[0].endAngle, outerRadius]];
-    chart.selectAll()
-        .data([0]) // one polyline, data in borders
-        .enter()
-        .append('path')
-        .attr('d', d3.lineRadial()(borders))
-        .attr('stroke', 'white')
-        .attr('stroke-width', 3)
-        .attr('fill', 'none');
+        // border between categories
+        const borders = [[0, outerRadius], [0, 0], [outerPie[0].endAngle, outerRadius]];
+        chart.selectAll()
+            .data([0]) // one polyline, data in borders
+            .enter()
+            .append('path')
+            .attr('d', d3.lineRadial()(borders))
+            .attr('stroke', 'white')
+            .attr('stroke-width', 3)
+            .attr('fill', 'none');
 
-    // labels -- commented-out as starting point for DEV-7421
-    // const labelPos = (i, yOffset = 0) => {
-    //         // labels at top left/bottom right or top right/bottom left, depending on relative values
-    //         const labelAngle = outerData[0] < outerData[1] ? -.8 : .8;
-    //         const labelPos = i == 0 ? labelRadius : -labelRadius;
+        // labels -- commented-out as starting point for DEV-7421
+        // const labelPos = (i, yOffset = 0) => {
+        //         // labels at top left/bottom right or top right/bottom left, depending on relative values
+        //         const labelAngle = outerData[0] < outerData[1] ? -.8 : .8;
+        //         const labelPos = i == 0 ? labelRadius : -labelRadius;
 
-    //         return [labelPos * Math.cos(labelAngle), labelPos * Math.sin(labelAngle) + yOffset];
-    //     }
+        //         return [labelPos * Math.cos(labelAngle), labelPos * Math.sin(labelAngle) + yOffset];
+        //     }
 
-    // const outerLabels = outer.map((d) => d.label);
+        // const outerLabels = outer.map((d) => d.label);
 
-    // svg.selectAll()
-    //     .data(outerPie)
-    //     .enter()
-    //     .append('text')
-    //     .attr('transform', (d, i) => 'translate(' + labelPos(i) + ')')
-    //     .attr('class', 'callout-labels')
-    //     .text((d, i) => outerLabels[i][0])
-    //     ;
+        // svg.selectAll()
+        //     .data(outerPie)
+        //     .enter()
+        //     .append('text')
+        //     .attr('transform', (d, i) => 'translate(' + labelPos(i) + ')')
+        //     .attr('class', 'callout-labels')
+        //     .text((d, i) => outerLabels[i][0])
+        //     ;
 
-    // svg.selectAll()
-    //     .data(outerPie)
-    //     .enter()
-    //     .append('text')
-    //     .attr('transform', (d, i) => 'translate(' + labelPos(i, 12) + ')')
-    //     .attr('class', 'callout-labels')
-    //     .text((d, i) => outerLabels[i][1])
-    //     ;
+        // svg.selectAll()
+        //     .data(outerPie)
+        //     .enter()
+        //     .append('text')
+        //     .attr('transform', (d, i) => 'translate(' + labelPos(i, 12) + ')')
+        //     .attr('class', 'callout-labels')
+        //     .text((d, i) => outerLabels[i][1])
+        //     ;
+    }
 
     return <div id="obl_chart" ref={chartRef} />;
 }
