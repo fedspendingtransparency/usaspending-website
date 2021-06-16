6a2b604afa7584d7cf252f58e102840d1036b418
Brett Varney <brett.varney@kc.frb.org>
2021-06-16 10:04:21 -0500

    4343 efficiency adjustments

--- src/js/containers/agencyV2/visualizations/ObligationsByAwardTypeContainer.jsx
+++ src/js/containers/agencyV2/visualizations/ObligationsByAwardTypeContainer.jsx
@@ -10,6 +10,7 @@ import { useSelector } from 'react-redux';
 import ObligationsByAwardType from 'components/agencyV2/visualizations/ObligationsByAwardType';
 import { LoadingMessage, ErrorMessage } from 'data-transparency-ui';
 import { fetchObligationsByAwardType } from 'apis/agencyV2';
+import { isCancel } from 'axios';
 
 // reduce api data into 2 arrays, one for each ring
 let categories;
@@ -21,15 +22,32 @@ const propTypes = {
 };
 
 export default function ObligationsByAwardTypeContainer({ fiscalYear, windowWidth }) {
-    const [isLoading, setLoading] = React.useState(true);
-    const [isError, setError] = React.useState(false);
+    const [categoriesForGraph, setCategoriesForGraph] = React.useState([]);
+    const [detailsForGraph, setDetailsForGraph] = React.useState([]);
+    const [loading, setLoading] = React.useState(true);
+    const [error, setError] = React.useState(false);
+    const obligationsByAwardTypeRequest = React.useRef(null);
     const { toptierCode } = useSelector((state) => state.agencyV2.overview);
 
-    if (toptierCode) {
-        let error = false;
-        fetchObligationsByAwardType(toptierCode, fiscalYear).promise.then((res) => {
-            // reduce api data into 2 arrays, one for each ring
-            categories = [
+    React.useEffect(() => () => {
+        if (obligationsByAwardTypeRequest.current) {
+            obligationsByAwardTypeRequest.current.cancel();
+        }
+    }, []);
+
+    const getObligationsByAwardType = () => {
+        if (obligationsByAwardTypeRequest.current) {
+            obligationsByAwardTypeRequest.current.cancel();
+        }
+        if (error) {
+            setError(false)
+        };
+        if (!loading) {
+            setLoading(true)
+        };
+        obligationsByAwardTypeRequest.current = fetchObligationsByAwardType(toptierCode, fiscalYear);
+        obligationsByAwardTypeRequest.current.promise.then((res) => {
+            const categories = [
                 {
                     label: ['Financial', 'Assistance'], // line break between words
                     value: 0,
@@ -41,7 +59,7 @@ export default function ObligationsByAwardTypeContainer({ fiscalYear, windowWidt
                     color: '#545BA3'
                 }
             ];
-            details = [
+            const details = [
                 {
                     label: ['Grants'],
                     color: '#E66F0E'
@@ -67,7 +85,6 @@ export default function ObligationsByAwardTypeContainer({ fiscalYear, windowWidt
                     color: '#A9ADD1'
                 }
             ];
-
             res.data.results.forEach((d) => {
                 switch (d.category) {
                     case 'grants':
@@ -96,22 +113,37 @@ export default function ObligationsByAwardTypeContainer({ fiscalYear, windowWidt
                         break;
                     default:
                         console.error(`Category name from API not recognized: ${d.category}`);
-                        error = true;
+                        setError(true);
                 }
             });
-
+            setCategoriesForGraph(categories);
+            setDetailsForGraph(details);
             setLoading(false);
-            setError(error);
+            obligationsByAwardTypeRequest.current = null;
+        }).catch((e) => {
+            if (!isCancel(e)) {
+                console.error(e);
+                setError(true);
+                setLoading(false);
+                obligationsByAwardTypeRequest.current = null;
+            }
         });
-    }
+    };
+
+    React.useEffect(() => {
+        if (toptierCode) {
+            getObligationsByAwardType();
+        }
+    }, [fiscalYear, toptierCode]);
+
 
     return (<>
-        { isLoading && <LoadingMessage />}
-        { isError && <ErrorMessage />}
-        { !isLoading && !isError &&
+        { loading && <LoadingMessage />}
+        { error && <ErrorMessage />}
+        { !loading && !error &&
             <ObligationsByAwardType
-                outer={categories}
-                inner={details}
+                outer={categoriesForGraph}
+                inner={detailsForGraph}
                 windowWidth={windowWidth} />
         }
     </>);
