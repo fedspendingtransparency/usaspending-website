9547df21c54d3423a38a4ed71679d2463ec34208
Jonathan Hill <hill_jonathan@bah.com>
2021-06-29 10:20:02 -0400

    dev-6614 add gradients to path and area path for obligations exceeding agency budget

--- tests/components/agency/overview/mockData.js
+++ tests/components/agency/overview/mockData.js
@@ -60,3 +60,49 @@ export const mockTotalBudgetaryResources = [
         total_budgetary_resources: 7157959573120.1
     }
 ];
+
+export const mock2020Data = [
+    {
+        endDate: 1577768400000,
+        obligated: 227170135560.3,
+        period: 3
+    },
+    {
+        endDate: 1585627200000,
+        obligated: 580584099932.03,
+        period: 6
+    },
+    {
+        endDate: 1588219200000,
+        obligated: 1040324359196.91,
+        period: 7
+    }
+];
+
+export const mock2021Data = [
+    {
+        endDate: 1606712400000,
+        obligated: 121466500151.75,
+        period: 2
+    },
+    {
+        endDate: 1609390800000,
+        obligated: 225939783327.81,
+        period: 3
+    },
+    {
+        endDate: 1612069200000,
+        obligated: 441947534122.58,
+        period: 4
+    },
+    {
+        endDate: 1614488400000,
+        obligated: 549265048938.4,
+        period: 5
+    },
+    {
+        endDate: 1617163200000,
+        obligated: 1045875996396.89,
+        period: 6
+    }
+];
