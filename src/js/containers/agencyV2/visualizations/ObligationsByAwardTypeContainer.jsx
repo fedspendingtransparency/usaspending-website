f974305ed47dfa43b031d6b80258eb7f9827daf6
Brett Varney <brett.varney@kc.frb.org>
2021-07-02 09:43:00 -0500

    4343 lint won't allow a space

--- src/js/containers/agencyV2/visualizations/ObligationsByAwardTypeContainer.jsx
+++ src/js/containers/agencyV2/visualizations/ObligationsByAwardTypeContainer.jsx
@@ -145,7 +145,7 @@ export default function ObligationsByAwardTypeContainer({ fiscalYear, windowWidt
         {error && <ErrorMessage />}
         {noData && <NoResultsMessage />}
         {!loading && !error && !noData &&
-            < ObligationsByAwardType outer={categoriesForGraph} inner={detailsForGraph} windowWidth={windowWidth} />
+            <ObligationsByAwardType outer={categoriesForGraph} inner={detailsForGraph} windowWidth={windowWidth} />
         }
     </>);
 }
