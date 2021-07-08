2f0e00434386ff0a9ed4e210ca273be5bf630fb0
Elizabeth Salita <salita_elizabeth@bah.com>
2021-07-08 10:49:19 -0400

    removed redirect modal

--- src/js/components/homepage/CovidVaccineLink.jsx
+++ src/js/components/homepage/CovidVaccineLink.jsx
@@ -1,31 +1,20 @@
 
 
 import React from 'react';
-import { useDispatch } from 'react-redux';
-import { showModal } from 'redux/actions/modal/modalActions';
 
-const CovidVaccineLink = () => {
-    const dispatch = useDispatch();
-    const onExternalLinkClick = (e) => {
-        e.persist();
-        if (e?.target) {
-            dispatch(showModal(e.target.value));
-        }
-    };
-    return (
-        <div className="covid-19-vaccine-link__container">
-            <div className="covid-19-vaccine-link__text">
-                <strong>We Can Do this. </strong>
-                Find COVID-19 vaccines near you.&nbsp;
-                <button
-                    value="https://www.vaccines.gov/ "
-                    role="link"
-                    className="covid-19-vaccine-link__button"
-                    onClick={onExternalLinkClick}>
-                    Visit Vaccines.gov
-                </button>
-            </div>
+const CovidVaccineLink = () => (
+    <div className="covid-19-vaccine-link__container">
+        <div className="covid-19-vaccine-link__text">
+            <strong>We Can Do this. </strong>
+            Find COVID-19 vaccines near you.&nbsp;
+            <a
+                href="https://www.vaccines.gov/ "
+                target="_blank"
+                rel="noopener noreferrer">
+                Visit Vaccines.gov
+            </a>
         </div>
-    ); };
+    </div>
+);
 
 export default CovidVaccineLink;
