af3d20f6b2e6b2aed802370ab338b2565a7ec7ce
Elizabeth Salita <salita_elizabeth@bah.com>
2021-07-08 11:02:19 -0400

    added "We Can Do This" image

--- src/js/components/homepage/CovidVaccineLink.jsx
+++ src/js/components/homepage/CovidVaccineLink.jsx
@@ -4,6 +4,9 @@ import React from 'react';
 
 const CovidVaccineLink = () => (
     <div className="covid-19-vaccine-link__container">
+        <div className="covid-19-vaccine-link__image">
+            <img src="img/wcdt.png" alt="We Can Do This" />
+        </div>
         <div className="covid-19-vaccine-link__text">
             <strong>We Can Do this. </strong>
             Find COVID-19 vaccines near you.&nbsp;
