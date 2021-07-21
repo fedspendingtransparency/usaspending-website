22b449d3b5854b4a22e870e64adecd17d96aebcf
Brett Varney <brett.varney@kc.frb.org>
2021-07-21 13:13:50 -0500

    dev-3563 ah ha, working now

--- tests/components/glossary/definition/GlossaryDefinition-test.jsx
+++ tests/components/glossary/definition/GlossaryDefinition-test.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import GlossaryDefinition from 'components/glossary/definition/GlossaryDefinition';
-import { fromJS } from 'immutable';
-import { shallow } from 'enzyme';
+import { Definition } from 'redux/reducers/glossary/glossaryReducer';
+import { mount } from 'enzyme';
 
 describe('GlossaryDefinition', () => {
-    // jest.setTimeout(20000);
-    it(`should update state for active tab if new entry doesn't have an Official Def`, () => {
+    it(`should update state for active tab if new entry doesn't have a plain or an official def`, () => {
         const entry1 = {
-            term: fromJS({
-                slug: 'slug 1',
-                plain: 'plain 1'
+            term: new Definition({
+                slug: 'slug1',
+                official: 'official only'
             })
         };
         const entry2 = {
-            term: fromJS({
-                slug: 'slug 2',
-                official: 'official 2'
+            term: Definition({
+                slug: 'slug2',
+                plain: 'plain only'
             })
         };
-        const test = shallow(<GlossaryDefinition glossary={entry1} />);
-        expect(test.state().tab).toBe('plain');
-        test.setState(entry2);
+        const test = mount(<GlossaryDefinition glossary={entry1} />);
         expect(test.state().tab).toBe('official');
+        test.setProps({ glossary: entry2 });
+        test.update();
+        expect(test.state().tab).toBe('plain');
     });
 });
