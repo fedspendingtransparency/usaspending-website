f1b91141f6b6a5e4cdbdd8bba8ee777cb26e0f09
Elizabeth Salita <salita_elizabeth@bah.com>
2021-08-27 10:40:57 -0400

    added unit tests

--- tests/containers/covid19/Covid19Container-test.jsx
+++ tests/containers/covid19/Covid19Container-test.jsx
@@ -7,6 +7,7 @@ import React from 'react';
 import { render } from 'test-utils';
 import '@testing-library/jest-dom/extend-expect';
 import * as apis from 'apis/disaster';
+import { useQueryParams } from 'helpers/queryParams';
 import Covid19Container from 'containers/covid19/Covid19Container';
 import { mockDefCodes } from '../../mockData/helpers/disasterHelper';
 import { defaultState } from '../../testResources/defaultReduxFilters';
@@ -15,12 +16,26 @@ import { defaultState } from '../../testResources/defaultReduxFilters';
 jest.mock('components/covid19/Covid19Page', () =>
     jest.fn(() => null));
 
+// Mock the custom hook useQueryParams
+jest.mock('helpers/queryParams', () => ({
+    useQueryParams: jest.fn()
+}));
+
+// Mock history.replace()
+const mockHistoryReplace = jest.fn();
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useHistory: () => ({
+        replace: mockHistoryReplace
+    })
+}));
 
 describe('COVID-19 Container', () => {
     afterEach(() => {
         jest.clearAllMocks();
     });
     it('should fetch overview and totals given we have def code data', () => {
+        useQueryParams.mockImplementation(() => ({ publicLaw: 'all' }));
         const spy = jest.spyOn(apis, 'fetchOverview');
         const spy2 = jest.spyOn(apis, 'fetchAwardAmounts');
         render(
@@ -33,6 +48,7 @@ describe('COVID-19 Container', () => {
     });
 
     it('should not fetch overview and totals given we do not have defcode data', () => {
+        useQueryParams.mockImplementation(() => ({ publicLaw: 'all' }));
         const spy = jest.spyOn(apis, 'fetchOverview');
         const spy2 = jest.spyOn(apis, 'fetchAwardAmounts');
         render((
@@ -40,5 +56,52 @@ describe('COVID-19 Container', () => {
         ));
         expect(spy).toHaveBeenCalledTimes(0);
         expect(spy2).toHaveBeenCalledTimes(0);
+    });
+    it('redirects to all DEFC when the public law query param is invalid', () => {
+        useQueryParams.mockImplementation(() => ({ publicLaw: 'blah' }));
+        render((
+            <Covid19Container />
+        ));
+        expect(mockHistoryReplace).toHaveBeenCalledTimes(1);
+        expect(mockHistoryReplace).toHaveBeenCalledWith({
+            pathname: '',
+            search: '?publicLaw=all'
+        });
+    });
+
+    it('redirects if no public law query param is provided', () => {
+        useQueryParams.mockImplementation(() => ({ publicLaw: '' }));
+        render((
+            <Covid19Container />
+        ));
+        expect(mockHistoryReplace).toHaveBeenCalledTimes(1);
+        expect(mockHistoryReplace).toHaveBeenCalledWith({
+            pathname: '',
+            search: '?publicLaw=all'
+        });
+    });
+
+    describe('when a valid public law query param is provided', () => {
+        it('does not redirect all DEFC', () => {
+            useQueryParams.mockImplementation(() => ({ publicLaw: 'all' }));
+            render((
+                <Covid19Container />
+            ));
+            expect(mockHistoryReplace).not.toHaveBeenCalled();
+        });
+        it('does not redirect American Rescue Plan', () => {
+            useQueryParams.mockImplementation(() => ({ publicLaw: 'american-rescue-plan' }));
+            render((
+                <Covid19Container />
+            ));
+            expect(mockHistoryReplace).not.toHaveBeenCalled();
+        });
+        it('is not case sensitive', () => {
+            useQueryParams.mockImplementation(() => ({ publicLaw: 'AMERican-reSCue-pLAN' }));
+            render((
+                <Covid19Container />
+            ));
+            expect(mockHistoryReplace).not.toHaveBeenCalled();
+        });
     });
 });
