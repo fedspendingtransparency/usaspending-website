4d4037d887d85328fe76c0f73ba3d619e8f2153a
Elizabeth Salita <salita_elizabeth@bah.com>
2021-08-27 13:51:08 -0400

    Moved archive alert to a modal

--- src/js/components/bulkDownload/DatabaseDownload.jsx
+++ src/js/components/bulkDownload/DatabaseDownload.jsx
@@ -5,12 +5,20 @@
  */
 
 import React from 'react';
-import DownloadMessage from './DownloadMessage';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const DatabaseDownload = () => (
     <div className="database-download">
         <h2>Database Download</h2>
-        <DownloadMessage />
+        <div className="database-download-message">
+            <h3 className="database-download-message__heading">
+                <FontAwesomeIcon className="database-download-message__icon" icon="exclamation-triangle" />
+                This page is unavailable while we are making updates to USAspending.
+            </h3>
+            <p className="database-download-message__description">
+                Looking for USAspending data? For award-related data, visit the Custom Award Data download. For federal account-related data, visit the Custom Account Data download.
+            </p>
+        </div>
     </div>
 );
 
