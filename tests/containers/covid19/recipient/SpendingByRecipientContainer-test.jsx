52cff0424237c9b11cc70c2fa7bf0b3010135dfb
Elizabeth Salita <salita_elizabeth@bah.com>
2021-09-03 09:33:26 -0400

    added unit tests for spending by recipient table

---
+++ tests/containers/covid19/recipient/SpendingByRecipientContainer-test.jsx
@@ -0,0 +1,240 @@
+/**
+ * SpendingByRecipientContainer-test.js
+ * Created by Lizzie Salita 7/17/20
+ * */
+
+import React from 'react';
+import { render, waitFor, screen, fireEvent, act, cleanup } from 'test-utils';
+import '@testing-library/jest-dom/extend-expect';
+import { Link } from 'react-router-dom';
+import SpendingByRecipientContainer, { parseRows } from 'containers/covid19/recipient/SpendingByRecipientContainer';
+import * as api from 'apis/disaster';
+import * as redux from 'react-redux';
+
+const mockResults = [
+    {
+        code: "987654321",
+        award_count: 2,
+        description: "RECIPIENT 3",
+        id: ["d2894d22-67fc-f9cb-4005-33fa6a29ef86-C", "d2894d22-67fc-f9cb-4005-33fa6a29ef86-R"],
+        obligation: 2200.0,
+        outlay: 1100.0
+    },
+    {
+        code: "456789123",
+        award_count: 1,
+        description: "RECIPIENT 2",
+        id: ["3c92491a-f2cd-ec7d-294b-7daf91511866-R"],
+        obligation: 20.0,
+        outlay: 0.0
+    }
+];
+
+let spy;
+
+beforeEach(() => {
+    jest.clearAllMocks();
+
+    // Mock the relevant redux state
+    const mockUseSelector = jest.spyOn(redux, 'useSelector').mockReturnValue({
+        defcParams: ['A', 'B', 'C'],
+        recipientTotals: {
+            obligation: 2026286145590.02,
+            outlay: 1604723759880.31,
+            awardCount: 21639525
+        }
+    });
+});
+
+describe('SpendingByRecipientContainer', () => {
+    describe('parseRows', () => {
+        it('should parse returned recipient data', () => {
+            const expected = [
+                [
+                    (
+                        <>
+                            {mockResults[0].description}&nbsp;(
+                            <Link to="/recipient/d2894d22-67fc-f9cb-4005-33fa6a29ef86-C/latest">
+                                as Child
+                            </Link>,&nbsp;
+                            <Link to="/recipient/d2894d22-67fc-f9cb-4005-33fa6a29ef86-R/latest">
+                                as Recipient
+                            </Link>
+                            )
+                        </>
+                    ),
+                    '$2,200',
+                    '$1,100',
+                    '2'
+                ],
+                [
+                    (
+                        <Link to="/recipient/3c92491a-f2cd-ec7d-294b-7daf91511866-R/latest">
+                            RECIPIENT 2
+                        </Link>
+                    ),
+                    '$20',
+                    '$0',
+                    '1'
+                ]
+            ];
+    
+            const parsed = parseRows(mockResults, 'all', '');
+            // using toMatchObject for equality comparison that is more lenient than toEqual https://jestjs.io/docs/en/expect#tomatchobjectobject
+            expect([...parsed]).toMatchObject(expected);
+            
+        });
+        it('should parse returned recipient loans data', () => {
+            const mockLoanResults = [
+                {
+                    code: "987654321",
+                    award_count: 2,
+                    description: "RECIPIENT 3",
+                    face_value_of_loan: 100.0,
+                    id: ["d2894d22-67fc-f9cb-4005-33fa6a29ef86-C", "d2894d22-67fc-f9cb-4005-33fa6a29ef86-R"],
+                    obligation: 2200.0,
+                    outlay: 1100.0
+                },
+                {
+                    code: "456789123",
+                    award_count: 1,
+                    description: "RECIPIENT 2",
+                    face_value_of_loan: 200.0,
+                    id: ["3c92491a-f2cd-ec7d-294b-7daf91511866-R"],
+                    obligation: 20.0,
+                    outlay: 0.0
+                }
+            ];
+
+            const expected = [
+                [
+                    (
+                        <>
+                            {mockLoanResults[0].description}&nbsp;(
+                            <Link to="/recipient/d2894d22-67fc-f9cb-4005-33fa6a29ef86-C/latest">
+                                as Child
+                            </Link>,&nbsp;
+                            <Link to="/recipient/d2894d22-67fc-f9cb-4005-33fa6a29ef86-R/latest">
+                                as Recipient
+                            </Link>
+                            )
+                        </>
+                    ),
+                    '$2,200',
+                    '$1,100',
+                    '$100',
+                    '2'
+                ],
+                [
+                    (
+                        <Link to="/recipient/3c92491a-f2cd-ec7d-294b-7daf91511866-R/latest">
+                            RECIPIENT 2
+                        </Link>
+                    ),
+                    '$20',
+                    '$0',
+                    '$200',
+                    '1'
+                ]
+            ];
+            
+            const parsed = parseRows(mockLoanResults, 'loans', '');
+            expect([...parsed]).toMatchObject(expected);
+        });
+    });
+    it('should make an API call when the sort changes', () => {
+        // spy on the API request helper function
+        spy = jest.spyOn(api, 'fetchDisasterSpending').mockReturnValue({
+            promise: Promise.resolve({
+                data: {
+                    results: mockResults,
+                    page_metadata: {
+                        total: 20
+                    }
+                }
+            }),
+            cancel: jest.fn()
+        });
+        // Initial render
+        act(() => {
+            render(<SpendingByRecipientContainer activeTab="all" prevActiveTab="all" scrollIntoView={jest.fn()} />);
+        });
+        expect(spy).toHaveBeenCalledTimes(1);
+        // Click a sort button
+        fireEvent.click(screen.getByTitle('Sort table by ascending Recipient'));
+        return waitFor(() => {
+            expect(spy).toHaveBeenCalledTimes(2);
+        });
+    });
+    it('should make an API call when a search term is applied', () => {
+        // spy on the API request helper function
+        spy = jest.spyOn(api, 'fetchDisasterSpending').mockReturnValue({
+            promise: Promise.resolve({
+                data: {
+                    results: mockResults,
+                    page_metadata: {
+                        total: 20
+                    }
+                }
+            }),
+            cancel: jest.fn()
+        });
+        // Initial render
+        act(() => {
+            render(<SpendingByRecipientContainer activeTab="all" prevActiveTab="all" scrollIntoView={jest.fn()} />);
+        });
+        expect(spy).toHaveBeenCalledTimes(1);
+        // Enter a search term
+        const input = screen.getByTitle('Search Input');
+        const button = screen.getByTitle('Submit Search Button');
+        fireEvent.change(input, { target: { value: 'hello' } });
+        fireEvent.click(button);
+        return waitFor(() => {
+            expect(spy).toHaveBeenCalledTimes(2);
+        });
+    });
+    it('should make a different API call when the loans tab is active', () => {
+        // spy on the API request helper functions
+        spy = jest.spyOn(api, 'fetchDisasterSpending');
+        const loansSpy = jest.spyOn(api, 'fetchLoanSpending').mockReturnValue({
+            promise: Promise.resolve({
+                data: {
+                    results: mockResults,
+                    page_metadata: {
+                        total: 20
+                    }
+                }
+            }),
+            cancel: jest.fn()
+        });
+        // Initial render
+        act(() => {
+            render(<SpendingByRecipientContainer activeTab="loans" prevActiveTab="loans" scrollIntoView={jest.fn()} />);
+        });
+        expect(spy).not.toHaveBeenCalled();
+        expect(loansSpy).toHaveBeenCalledTimes(1);
+    });
+    it('should make an API call when the active tab changes', () => {
+        // spy on the API request helper functions
+        spy = jest.spyOn(api, 'fetchDisasterSpending').mockReturnValue({
+            promise: Promise.resolve({
+                data: {
+                    results: mockResults,
+                    page_metadata: {
+                        total: 20
+                    }
+                }
+            }),
+            cancel: jest.fn()
+        });
+
+        const { rerender } = render(<SpendingByRecipientContainer activeTab="loans" prevActiveTab="loans" scrollIntoView={jest.fn()} />);
+        waitFor(() => {
+            expect(spy).toHaveBeenCalledTimes(1);
+        });
+        rerender(<SpendingByRecipientContainer activeTab="all" prevActiveTab="loans" scrollIntoView={jest.fn()} />);
+        waitFor(() => {
+            expect(spy).toHaveBeenCalledTimes(2);
+        });
+    });
+});
