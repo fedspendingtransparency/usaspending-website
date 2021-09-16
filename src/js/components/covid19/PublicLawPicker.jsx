d2eaa7c0fb0d1ffed881188a90c43e3e247c517f
Brett Varney <brett.varney@kc.frb.org>
2021-09-16 13:59:23 -0500

    dev7216 readd ui for defcode page filter

---
+++ src/js/components/covid19/PublicLawPicker.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Picker } from 'data-transparency-ui';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import GlossaryLink from '../sharedComponents/GlossaryLink';
+
+const options = [
+    { title: 'All COVID-19 Funding Laws', value: 'all', term: 'disaster-emergency-fund-code-defc' },
+    {
+        title: 'American Rescue Plan', description: 'Emergency Public Law 117-7', value: 'american-rescue-plan', className: 'arp'
+    },
+    { title: 'Learn more about filtering USAspending data by Public Law in our Data Sources & Methodology page.', value: 'dsm' }
+];
+
+const PublicLawPickerOption = ({
+    title, description, term, className
+}) => (
+    <li className={className}>
+        <p>{title}</p>
+        {description && (
+            <p>
+                {description}
+                {term && (
+                    <GlossaryLink term={term} />
+                )}
+            </p>)}
+    </li>
+);
+
+PublicLawPickerOption.propTypes = {
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    term: PropTypes.string,
+    className: PropTypes.string
+};
+
+const PublicLawPicker = ({
+    selectedOption = null,
+    onClick
+}) => {
+    const selected = options.find((obj) => obj.value === selectedOption);
+    return (
+        <div className="public-law-picker__container">
+            <Picker
+                className="public-law-picker"
+                icon={<FontAwesomeIcon icon="scroll" size="sm" />}
+                selectedOption={selected?.title || options[0].title}
+                options={options.map((obj) => ({ name: <PublicLawPickerOption {...obj} />, value: obj.value, onClick }))} />
+            <span>Public Law Filter</span>
+        </div>);
+};
+
+PublicLawPicker.propTypes = {
+    selectedOption: PropTypes.string,
+    onClick: PropTypes.func.isRequired
+};
+
+export default PublicLawPicker;
