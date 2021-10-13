4ff8ed26d6f894275728bfb6a2e66b96f010efba
Brett Varney <brett.varney@kc.frb.org>
2021-10-13 07:58:56 -0500

    7964 wip

--- tests/containers/covid19/DownloadButtonContainer-test.jsx
+++ tests/containers/covid19/DownloadButtonContainer-test.jsx
@@ -7,24 +7,36 @@ import React from 'react';
 import * as redux from 'react-redux';
 import { mount } from 'enzyme';
 import { render } from 'test-utils';
-import { DownloadButtonContainer } from 'containers/covid19/DownloadButtonContainer';
+import DownloadButtonContainer from 'containers/covid19/DownloadButtonContainer';
 import * as downloadHelper from 'helpers/downloadHelper';
 import { mockDefcParams } from '../../mockData/helpers/disasterHelper';
 
 describe('COVID-19 DownloadButtonContainer', () => {
     it('should request data download', async () => {
-        jest.spyOn(redux, 'useSelector').mockReturnValue({
-            defcParams: mockDefcParams,
-            bulkDownload: { download: { pendingDownload: false } }
+        jest.mock('react-redux', () => {
+            const ActualReactRedux = jest.requireActual('react-redux');
+            return {
+                ...ActualReactRedux,
+                useDispatch: jest.fn(() => () => {}),
+                useSelector: jest.fn().mockImplementation(() => ({
+                    defcParams: mockDefcParams,
+                    bulkDownload: { download: { pendingDownload: false } }
+                }))
+            };
         });
+
+        // jest.spyOn(redux, 'useSelector').mockReturnValue({
+        //             defcParams: mockDefcParams,
+        //             bulkDownload: { download: { pendingDownload: false } }
+        //         });
         const spy = jest.spyOn(downloadHelper, 'requestFullDownload');
-        //         const downloadButtonContainer = DownloadButtonContainer();
+        // const downloadButtonContainer = DownloadButtonContainer();
 
         // console.log(JSON.stringify(downloadButtonContainer));
 
-        //         await downloadButtonContainer.downloadData();
-        // const container = mount(<DownloadButtonContainer />);
-        await DownloadButtonContainer().downloadData();
+        // await downloadButtonContainer.downloadData();
+        const container = mount(<DownloadButtonContainer />);
+        // await DownloadButtonContainer().downloadData();
         expect(spy).toHaveBeenCalledTimes(1);
         expect(spy).toHaveBeenNthCalledWith(mockDefcParams, 'disaster');
     });
