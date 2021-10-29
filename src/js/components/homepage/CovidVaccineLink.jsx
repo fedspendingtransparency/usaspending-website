442699ee3e6c6df1a740bd9bb9c3e96f8a625c06
brianpetway <brian.petway@kc.frb.org>
2021-10-29 15:00:46 -0500

    8082 - put back the vaccine banner

---
+++ src/js/components/homepage/CovidVaccineLink.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const CovidVaccineLink = () => (
+  <div className="covid-19-vaccine-link__container">
+    <img src="img/wcdt.png" alt="We Can Do This" />
+    <div className="covid-19-vaccine-link__text">
+      <strong>We can do this. </strong>
+      Find COVID-19 vaccines near you.&nbsp;
+      <a
+        href="https://www.vaccines.gov/ "
+        target="_blank"
+        rel="noopener noreferrer">
+        Visit Vaccines.gov
+      </a>
+    </div>
+  </div>
+);
+
+export default CovidVaccineLink;
