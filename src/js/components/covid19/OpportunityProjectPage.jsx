89c9d76fe02ba68460c12a8ce8dd89e7a5781c46
Brett Varney <brett.varney@kc.frb.org>
2021-11-02 10:17:33 -0500

    7955 update map & wording

--- src/js/components/covid19/OpportunityProjectPage.jsx
+++ src/js/components/covid19/OpportunityProjectPage.jsx
@@ -20,6 +20,13 @@ const OpportunityProjectPage = () => {
         dispatch(showModal(e.target.href));
     };
 
+    const externalLink = (href, linkText) => (
+        <a href={href} onClick={handleClick}>
+            {linkText}&nbsp;
+            <FontAwesomeIcon size="sm" icon="external-link-alt" />
+        </a>
+    );
+
     return (
         <PageWrapper
             pageName="The Opportunity Project"
@@ -28,16 +35,13 @@ const OpportunityProjectPage = () => {
             metaTagProps={topPageMetaTags}>
             <FlexGridContainer className="top-page">
                 <FlexGridRow>
-                    <FlexGridCol
-                        desktop={{ span: 8, offset: 2 }}
-                        className="content">
+                    <FlexGridCol desktop={{ span: 8, offset: 2 }} className="content">
                         <main role="main" className="main-content">
                             <header className="main-heading">Analyzing Equity in Federal COVID-19 Spending: The Opportunity Project</header>
-
                             <p><img src="img/opportunity-project-logo.png" role="presentation" alt="" /></p>
 
-                            <p>The Bureau of the Fiscal Service (Fiscal) at the U.S. Department of the Treasury is proud to have been selected by The Opportunity Project as a Challenge Partner to collaborate, innovate and assist in solving the problem of analyzing equity in covid-19 federal funding.</p>
-                            <p>The Opportunity Project (TOP) is a public & private sector innovation program led by Census Open Innovation Labs at the U.S. Census Bureau with a focus on the nation’s toughest challenges. TOP brings together technologists, government, and community leaders to rapidly prototype digital products— powered by federal open data—that solve real-world problems for people across the country TOP’s 2021 sprint theme is “The World Post Covid: Society, Economy, and Environment.</p>
+                            <p>The Bureau of the Fiscal Service (Fiscal) at the U.S. Department of the Treasury is proud to have been selected by The Opportunity Project as a Challenge Partner to collaborate, innovate and assist in solving the problem of analyzing equity in COVID-19 federal funding.</p>
+                            <p>The Opportunity Project (TOP) is a public & private sector innovation program led by Census Open Innovation Labs at the U.S. Census Bureau with a focus on the nation’s toughest challenges. TOP brings together technologists, government, and community leaders to rapidly prototype digital products— powered by federal open data—that solve real-world problems for people across the country TOP’s 2021 sprint theme is “The World Post COVID-19: Society, Economy, and Environment.</p>
                             <p>The team behind <a href="/about">USAspending.gov</a> at Fiscal brought together tech teams, product advisors, user advocates, local government officials and federal data stewards from all over America to collaborate on the mission of analyzing equity in COVID-19 federal funding for our TOP 2021 sprint.  We want to thank and acknowledge all  of the TOP participants for volunteering their time, contributing to federal open data innovation, serving as a resource, and collaborating across sectors in service of helping communities.</p>
 
                             <ul><div className="list-heading">Tech Teams:</div>
@@ -64,15 +68,14 @@ const OpportunityProjectPage = () => {
                             </ul>
                             <ul><div className="list-heading">Federal Data Stewards:</div></ul>
                             <p>To see a comprehensive list of federal open datasets that were curated in this sprint to help analyze equity in COVID-19 federal funding visit the&nbsp;
-                                <a href="https://opportunity.census.gov/data/covid-spending/" onClick={handleClick}>
-                                    TOP datasets page.&nbsp;
-                                    <span className="other-resources__link__icon">
-                                        <FontAwesomeIcon size="sm" icon="external-link-alt" />
-                                    </span>
-                                </a>
+                                {externalLink("https://opportunity.census.gov/data/covid-spending/", "TOP datasets page.")}
                             </p>
 
-                            <img src="img/opportunity-project-map.png" />
+                            <picture>
+                                <source srcSet="img/opportunity-project-map.webp 950w" type="image/webp" />
+                                <source srcSet="img/opportunity-project-map.png" type="image/png" />
+                                <img src="img/opportunity-project-map.png" />
+                            </picture>
 
                             <p><span className="bold">THE CHALLENGE</span> – Develop innovative tools that combine datasets to help inform community leaders on equitable distribution of federal funding. Teams were encouraged to analyze how federal COVID-19 spending has been shared with communities most vulnerable to impacts of the pandemic.</p>
                             <p><span className="bold">THE PROBLEM</span> – Over the course of the COVID-19 pandemic, the federal government has made $4.5 trillion available for relief efforts. How has this money been spent? A greater understanding of how government spending reaches different communities is a step forward for data transparency and a call-to-action to improve equity.</p>
@@ -80,68 +83,29 @@ const OpportunityProjectPage = () => {
                             <p><span className="bold">THE TARGET END USERS</span> – Local government officials, federal funding decision makers, equity policy experts, community advocacy organizations and data journalists.</p>
                             <p><span className="bold">THE VISION</span> – By bringing together Census, Treasury, and other open data sources, there can be innovative digital tools built to help stakeholders better understand whether the federal government is equitably distributing federal funds during the COVID-19 pandemic.</p>
                             <p><span className="bold">THE RESULTS</span> – The Opportunity Project 2021 sprint concluded in November 2021.  Working with user advocates, data stewards, product advisors and following Census’s TOP product development toolkit, tech teams submitted innovative tools to analyze equity in federal COVID-19 spending.  The results are both inspiring and illuminating.  To see what tech teams built, please visit the website links below.</p>
+                            <p><span className="bold">PLEASE NOTE: Inclusion on this page does not constitute endorsement by the Department of the Treasury, Bureau of the Fiscal Service, or any employees thereof of any product, service, or enterprise.</span></p>
 
                             <p className="bold underline">Bowie State University</p>
-                            <p>Product statement: Bowie State University Opportunity Project uses publicly accessible CDC’s Social Vulnerability Index, CDC’s County vaccination rates, the ARP county covid vaccine spending data, and Co-est 2020 to assess covid vaccination and equity problems for community leaders as end users.</p>
-                            <p>Link:&nbsp;
-                                <a href="https://app.flow.gl/flow/3xfvr3" onClick={handleClick}>
-                                Bowie State University TOP Project on Flow Immersive&nbsp;
-                                    <span className="other-resources__link__icon">
-                                        <FontAwesomeIcon size="sm" icon="external-link-alt" />
-                                    </span>
-                                </a>
-                            </p>
+                            <p>Product statement: Bowie State University Opportunity Project uses publicly accessible CDC’s Social Vulnerability Index, CDC’s County vaccination rates, the ARP county COVID-19 vaccine spending data, and Co-est 2020 to assess COVID-19 vaccination and equity problems for community leaders as end users.</p>
+                            <p>Link: {externalLink("https://app.flow.gl/flow/3xfvr3", "Bowie State University TOP Project on Flow Immersive")}</p>
 
                             <p className="bold underline">Morehouse College</p>
-                            <p>Product statement: The MSI (Minority Serving Institutions) COVID Relief Dashboard for Equity and Transparency uses several federal datasets surrounding school enrollment and COVID relief awards to highlight whether or not COVID funding was equitably distributed to MSIs around the country for federal, state, and local officials and organizations.</p>
-                            <p>Link:&nbsp;
-                                <a href="https://a.flow.gl/flow/kuo62d54" onClick={handleClick}>
-                                MSI COVID Relief Dashboard for Equity and Transparency&nbsp;
-                                    <span className="other-resources__link__icon">
-                                        <FontAwesomeIcon size="sm" icon="external-link-alt" />
-                                    </span>
-                                </a>
-                            </p>
+                            <p>Product statement: The MSI (Minority Serving Institutions) COVID-19 Relief Dashboard for Equity and Transparency uses several federal datasets surrounding school enrollment and COVID-19 relief awards to highlight whether or not COVID-19 funding was equitably distributed to MSIs around the country for federal, state, and local officials and organizations.</p>
+                            <p>Link: {externalLink("https://a.flow.gl/flow/kuo62d54", "MSI COVID-19 Relief Dashboard for Equity and Transparency")}</p>
 
                             <p className="bold underline">The University of Kansas</p>
                             <p>Product statement: The Child Care Planning Assessment Tool will help community leaders better assess and understand the connection between childcare and equitable labor participation including a county level snapshot of child care supply and demand and interactive calculators to begin addressing child care needs in their community.</p>
-                            <p>Link:&nbsp;
-                                <a href="https://top.kucppr.org/" onClick={handleClick}>
-                                Child Care in America | A broken business model (kucppr.org)&nbsp;
-                                    <span className="other-resources__link__icon">
-                                        <FontAwesomeIcon size="sm" icon="external-link-alt" />
-                                    </span>
-                                </a>
-                            </p>
-                            <p>Video:&nbsp;
-                                <a href="https://vimeo.com/638475809" onClick={handleClick}>
-                                Kansas & Flow Immersive TOP Project Video Overview&nbsp;
-                                    <span className="other-resources__link__icon">
-                                        <FontAwesomeIcon size="sm" icon="external-link-alt" />
-                                    </span>
-                                </a>
-                            </p>
+                            <p>Link: {externalLink("https://top.kucppr.org/", "Child Care in America | A broken business model (kucppr.org)")}</p>
+                            <p>Video: {externalLink("https://vimeo.com/638475809", "Kansas & Flow Immersive TOP Project Video Overview")}</p>
 
                             <p className="bold underline">The Mom Project</p>
                             <p>Product statement: The PEI (Predictive Equity Index) model employs machine learning and predictive analytics to understand key drivers of equity in the distribution of PPP loans. This methodology allows for creation of a cohesive dataset, allowing for an in-depth assessment of county-level performance of equity, as well as an understanding of how future actions serve to impact anticipated levels of equity in future funding efforts. Taken together, this product allows for policy makers at the county, state, and national-level to take county-level action maximizing the impact of federal dollars on the communities the investments aim to serve.</p>
-                            <p>Link:&nbsp;
-                                <a href="https://work.themomproject.com/predictiveequity" onClick={handleClick}>
-                                Predictive Equity Index Model, developed by Werklabs, the research division of The Mom Project&nbsp;
-                                    <span className="other-resources__link__icon">
-                                        <FontAwesomeIcon size="sm" icon="external-link-alt" />
-                                    </span>
-                                </a>
-                            </p>
+                            <p>Link: {externalLink("https://work.themomproject.com/predictiveequity", "Predictive Equity Index Model, developed by Werklabs, the research division of The Mom Project")}</p>
 
                             <footer>
-                                <p className="center">Questions, ideas or feedback? E-mail us at <a href="mailto:usaspending@fiscal.treasury.gov">usaspending@fiscal.treasury.gov</a></p>
+                                <p className="center">Questions, ideas, or feedback? E-mail us at <a href="mailto:usaspending@fiscal.treasury.gov">usaspending@fiscal.treasury.gov</a></p>
                                 <p className="center">We want to thank everyone involved in The Opportunity Project.  Learn more at&nbsp;
-                                    <a href="http://www.opportunity.census.gov/" onClick={handleClick}>
-                                opportunity.census.gov&nbsp;
-                                        <span className="other-resources__link__icon">
-                                            <FontAwesomeIcon size="sm" icon="external-link-alt" />
-                                        </span>
-                                    </a>
+                                    {externalLink("http://www.opportunity.census.gov/", "opportunity.census.gov")}
                                 </p>
                             </footer>
                         </main>
