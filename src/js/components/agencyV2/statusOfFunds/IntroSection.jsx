da8fb78da3bf65df95f755085e8e4cc9da05fa8c
brianpetway <brian.petway@kc.frb.org>
2021-11-09 11:41:39 -0600

    8046 - added unit tests

--- src/js/components/agencyV2/statusOfFunds/IntroSection.jsx
+++ src/js/components/agencyV2/statusOfFunds/IntroSection.jsx
@@ -15,18 +15,17 @@ const propTypes = {
 const IntroSection = ({ fy }) => {
     const { name } = useSelector((state) => state.agencyV2.overview);
     const agencyBudget = useSelector((state) => state.agencyV2.budgetaryResources?.[fy]?.agencyBudget) || '--';
-    const subComponents = '';
 
     return (
         <div className="status-of-funds__intro-wrapper">
             <div className="status-of-funds__intro-section-title">
-                <span data-testid="introSentence">
+                <span>
                     How were funds distributed in FY {fy} for the {name}?
                 </span>
             </div>
             <div className="status-of-funds__intro-section-text" >
-                <span >
-                    In FY {fy}, the {name} had {agencyBudget} in available <span className="status-of-funds__glossary-term">budgetary resources</span> <GlossaryLink term="budgetary-resources" /> distributed among its {subComponents} agency sub-components. Agencies spend available budgetary resources by making financial promises called <span className="status-of-funds__glossary-term">obligations</span> <GlossaryLink term="obligation" />. In this section, we show the total budgetary resources broken out by agency sub-component and how much of that funding has been obligated.
+                <span data-testid={'introCopy'}>
+                    In FY {fy}, the {name} had {agencyBudget} in available <span className="status-of-funds__glossary-term">budgetary resources</span> <GlossaryLink term="budgetary-resources" /> distributed among its 12 agency sub-components. Agencies spend available budgetary resources by making financial promises called <span className="status-of-funds__glossary-term">obligations</span> <GlossaryLink term="obligation" />. In this section, we show the total budgetary resources broken out by agency sub-component and how much of that funding has been obligated.
                 </span>
             </div>
             <div className="status-of-funds__intro-section-italic-text">
--- tests/components/agency/StatusOfFunds/IntroSection-test.jsx
+++ tests/components/agency/StatusOfFunds/IntroSection-test.jsx
@@ -4,6 +4,7 @@ import IntroSection
     from "../../../../src/js/components/agencyV2/statusOfFunds/IntroSection";
 
 describe('Agency V2 Status of Funds IntroSection', () => {
+    const fy = '2021';
     const mockStore = {
         agencyV2: {
             overview: {
@@ -16,23 +17,40 @@ describe('Agency V2 Status of Funds IntroSection', () => {
             }
         }
     };
+
+    const introSentence = `How were funds distributed in FY ${fy} for the ${mockStore.agencyV2.overview.name}?`;
+    const copy = `In FY ${fy}, the ${mockStore.agencyV2.overview.name} had ${mockStore.agencyV2.budgetaryResources["2021"].agencyBudget} in available budgetary resources  distributed among its 12 agency sub-components. Agencies spend available budgetary resources by making financial promises called obligations . In this section, we show the total budgetary resources broken out by agency sub-component and how much of that funding has been obligated.`;
+    const directions = 'Select a segment in the chart below to dive deeper into' +
+        ' the data.';
+
     it('displays the fy and agency name in the intro sentence', () => {
         render(
             <IntroSection fy="2021" />,
             { initialState: mockStore }
         );
-        // const introSentenceContainer = screen.getByTestId('introSentence');
-        // expect(introSentenceContainer).toBe('FY 2021');
-        // expect(introSentenceContainer).toContain('Test Agency');
-        // expect(screen.getByTestId('introSentence').props.children.join()).toBe('rong');
+        expect(screen.getByText(introSentence)).toBeTruthy();
     });
     it('displays the fy, agency name, and amount in the copy', () => {
-
+        render(
+            <IntroSection fy="2021" />,
+            { initialState: mockStore }
+        );
+        const copyElement = screen.getByTestId('introCopy');
+        expect(copyElement.textContent).toBe(copy);
     });
     it('contains glossary links for "budgetary resources" and "obligation"', () => {
-
+        render(
+            <IntroSection fy="2021" />,
+            { initialState: mockStore }
+        );
+        const links = screen.getAllByRole('link', { className: 'status-of-funds__glossary-term' });
+        expect(links.length).toBe(2);
     });
     it('contains a sentence with directions about the chart below', () => {
-
+        render(
+            <IntroSection fy="2021" />,
+            { initialState: mockStore }
+        );
+        expect(screen.getByText(directions)).toBeTruthy();
     });
 });
