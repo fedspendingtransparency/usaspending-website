358889843c36820b1c6a3b9b4662529082ad0d3a
brianpetway <brian.petway@kc.frb.org>
2021-11-09 09:44:00 -0600

    8046 - wip; updated copy; still need one var for the copy

--- src/js/components/agencyV2/statusOfFunds/IntroSection.jsx
+++ src/js/components/agencyV2/statusOfFunds/IntroSection.jsx
@@ -15,15 +15,18 @@ const propTypes = {
 const IntroSection = ({ fy }) => {
     const { name } = useSelector((state) => state.agencyV2.overview);
     const agencyBudget = useSelector((state) => state.agencyV2.budgetaryResources?.[fy]?.agencyBudget) || '--';
+    const subComponents = '';
 
     return (
         <div className="status-of-funds__intro-wrapper">
             <div className="status-of-funds__intro-section-title">
-                <span data-testid="introSentence">How were funds distributed in FY {fy} for the {name}?</span>
+                <span data-testid="introSentence">
+                    How were funds distributed in FY {fy} for the {name}?
+                </span>
             </div>
             <div className="status-of-funds__intro-section-text" >
                 <span >
-                    In FY {fy}, the {name} was provided over {agencyBudget} in available <span className="status-of-funds__glossary-term">budgetary resources</span> <GlossaryLink term="budgetary-resources" />. The {name} spends its available budgetary resources by making financial promises called <span className="status-of-funds__glossary-term">obligations</span> <GlossaryLink term="obligation" />. These funds were distributed to its agency sub-components.
+                    In FY {fy}, the {name} had {agencyBudget} in available <span className="status-of-funds__glossary-term">budgetary resources</span> <GlossaryLink term="budgetary-resources" /> distributed among its {subComponents} agency sub-components. Agencies spend available budgetary resources by making financial promises called <span className="status-of-funds__glossary-term">obligations</span> <GlossaryLink term="obligation" />. In this section, we show the total budgetary resources broken out by agency sub-component and how much of that funding has been obligated.
                 </span>
             </div>
             <div className="status-of-funds__intro-section-italic-text">
