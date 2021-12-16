937ffa20b42124050f54873133abcea654f19775
anjenkin <121010+anjenkin@users.noreply.github.com>
2021-12-16 10:13:47 -0500

    fixed unit tests

--- tests/helpers/agencyV2/visualizations/ObligationsByAwardTypeHelper-test.js
+++ tests/helpers/agencyV2/visualizations/ObligationsByAwardTypeHelper-test.js
@@ -7,82 +7,56 @@ import { mapToFullCategoryName, getCategoryNameByAwardType, getActiveCategoryTyp
 
 const mockCategories = [
     {
-        label: ['All Financial', 'Assistance'], // line break between words
-        value: 0,
-        color: 'rgb(192, 86, 0)',
-        fadedColor: 'rgb(192, 86, 0, 25%)'
+        label: ['All Category1'],
     },
     {
-        label: ['All Contracts', ''], // so each cat label array is same length
-        value: 0,
-        color: 'rgb(84, 91, 163)',
-        fadedColor: 'rgb(84, 91, 163, 25%)'
+        label: ['All Category2']
     }
 ];
 
 const mockDetails = [
     {
         label: 'Grants',
-        color: 'rgb(230, 111, 14)',
-        fadedColor: 'rgb(230, 111, 14, 25%)',
-        type: 'financial'
+        type: 'category2'
     },
     {
         label: 'Loans',
-        color: 'rgb(255, 188, 120)',
-        fadedColor: 'rgb(255, 188, 120, 25%)',
-        type: 'financial'
-    },
-    {
-        label: 'Direct Payments',
-        color: 'rgb(250, 148, 65)',
-        fadedColor: 'rgb(250, 148, 65, 25%)',
-        type: 'financial'
-    },
-    {
-        label: 'Other Financial Assistance',
-        color: 'rgb(252, 226, 197)',
-        fadedColor: 'rgb(252, 226, 197, 25%)',
-        type: 'financial'
+        type: 'category2'
     },
     {
         label: 'Contracts',
-        color: 'rgb(127, 132, 186)',
-        fadedColor: 'rgb(127, 132, 186, 25%)',
-        type: 'contracts'
+        type: 'category1'
     },
     {
         label: 'IDVs',
-        color: 'rgb(169, 173, 209)',
-        fadedColor: 'rgb(169, 173, 209, 25%)',
-        type: 'contracts'
+        type: 'category1'
     }
 ];
 
 const mockCategoryMapping = {
-    'All Contracts': ['Contracts', 'IDVs'],
-    'All Financial': ['Grants', 'Loans', 'Direct Payments', 'Other Financial Assistance']
+    'All Category1': ['Contracts', 'IDVs'],
+    'All Category2': ['Grants', 'Loans']
 };
 
 describe('Obligations By Award Type Visualization', () => {
 
-    it('should return a full category name from short category name', () => {
-        expect(mapToFullCategoryName('contracts'))
-            .toEqual('All Contracts');
+    it('should return a full category name from category type', () => {
+        expect(mapToFullCategoryName(mockDetails[2].type))
+            .toEqual('All Category1');
     });
 
     it('should return a full category name by award type', () => {
-        expect(getCategoryNameByAwardType('IDVs', mockCategoryMapping))
-            .toEqual('All Contracts');
+        expect(getCategoryNameByAwardType(mockDetails[3].label, mockCategoryMapping))
+            .toEqual('All Category1');
     });
 
     it('should get the active category type from the active award type', () => {
-        expect(getActiveCategoryType('Loans', mockCategoryMapping))
-            .toEqual('financial');
+        expect(getActiveCategoryType(mockDetails[1].label, mockCategoryMapping))
+            .toEqual('category2');
     });
 
     it('should an id from a full category name', () => {
-        expect(getOuterCategoryId('All Financial', mockCategories))
+        expect(getOuterCategoryId('All Category1', mockCategories))
             .toEqual(0);
     });
 });
