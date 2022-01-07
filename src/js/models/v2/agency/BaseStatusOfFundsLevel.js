1c77c3f413bca8104d4bc96c515aab6cf99226b0
Afna <afnaa97@gmail.com>
2022-01-07 11:51:15 -0500

    dev 8052 unit test fix

--- src/js/models/v2/agency/BaseStatusOfFundsLevel.js
+++ src/js/models/v2/agency/BaseStatusOfFundsLevel.js
@@ -9,8 +9,8 @@ const BaseStatusOfFundsLevel = {
     populate(data) {
         this.id = data.id || null;
         this.name = data.name || '';
-        this._budgetaryResources = formatMoneyWithUnitsShortLabel(data._budgetaryResources, 2);
-        this._obligations = formatMoneyWithUnitsShortLabel(data._obligations, 2);
+        this._budgetaryResources = formatMoneyWithUnitsShortLabel(data._budgetaryResources, 2) || '--';
+        this._obligations = formatMoneyWithUnitsShortLabel(data._obligations, 2) || '--';
     }
 };
 
