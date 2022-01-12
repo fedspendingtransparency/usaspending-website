44cbc5752577e28a24d1e0f62c5b0b1717d50f5a
anjenkin <blackwell_andrea@bah.com>
2022-01-12 11:42:31 -0500

    added fixed tabbing through the donut chart - initial render

--- src/js/components/agencyV2/visualizations/ObligationsByAwardType.jsx
+++ src/js/components/agencyV2/visualizations/ObligationsByAwardType.jsx
@@ -159,7 +159,8 @@ export default function ObligationsByAwardType({
             })
             .on('blur', () => setActiveType(null))
             .attr('aria-label', (d) => `${d.data.label}: ${d3.format("($,.2f")(d.value)}`)
-            .attr('role', 'listitem');
+            .attr('role', 'listitem')
+            .attr('tabIndex', 0);
 
 
         // labels
@@ -217,10 +218,7 @@ export default function ObligationsByAwardType({
         }
     };
 
-    useEffect(() => {
-        renderChart();
-    },
-    []);
+    renderChart();
 
     useEffect(() => {
         const rect = chartRef.current.parentElement.getBoundingClientRect();
