032920d7bb3db8378fe0a9abe0166c6fdf2f651b
Afna <afnaa97@gmail.com>
2022-01-24 08:54:54 -0500

    design feedback, keep tooltip bottom pos always

--- src/js/components/agencyV2/overview/PageTitle.jsx
+++ src/js/components/agencyV2/overview/PageTitle.jsx
@@ -3,12 +3,10 @@
  * Created by Lizzie Salita 12/8/21
  */
 
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 import { TooltipWrapper } from 'data-transparency-ui';
-import { xlargeScreen } from 'dataMapping/shared/mobileBreakpoints';
-import { throttle } from 'lodash';
 import CovidTooltip from './CovidTooltip';
 
 const propTypes = {
@@ -24,21 +22,6 @@ const PageTitle = ({
         covidDefCodes
     } = useSelector((state) => state.agencyV2.overview);
 
-    const [windowWidth, setWindowWidth] = useState(0);
-    const [isXlargeScreen, setIsXlargeScreen] = useState(window.innerWidth < xlargeScreen);
-
-    useEffect(() => {
-        const handleResize = throttle(() => {
-            const newWidth = window.innerWidth;
-            if (windowWidth !== newWidth) {
-                setWindowWidth(newWidth);
-                setIsXlargeScreen(newWidth < xlargeScreen);
-            }
-        }, 50);
-        window.addEventListener('resize', handleResize);
-        return () => window.removeEventListener('resize', handleResize);
-    }, []);
-
     const image = logo ? (
         <img
             className="page-title__image"
@@ -51,7 +34,7 @@ const PageTitle = ({
                 <h2 className="page-title__name">
                     {name}&nbsp;&nbsp;
                     {name && covidDefCodes.length > 0 &&
-                    <TooltipWrapper tooltipPosition={isXlargeScreen ? "bottom" : "right"} offsetAdjustments={{ top: 0 }} className="page-title__tooltip" tooltipComponent={<CovidTooltip fy={fy} codes={covidDefCodes} />}>
+                    <TooltipWrapper tooltipPosition="bottom" offsetAdjustments={{ top: 0 }} className="page-title__tooltip" tooltipComponent={<CovidTooltip fy={fy} codes={covidDefCodes} />}>
                         <span className="covid-spending-flag">
                             Includes COVID-19 Spending
                         </span>
