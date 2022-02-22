14c772914492ac1b625fb8ff220975ab40270705
Afna <afnaa97@gmail.com>
2022-02-22 15:08:31 -0500

    edge case bug fix flip text right/left based on data interval

--- src/js/components/award/shared/awardAmounts/HorizontalSingleStackedBarViz.jsx
+++ src/js/components/award/shared/awardAmounts/HorizontalSingleStackedBarViz.jsx
@@ -166,26 +166,26 @@ const HorizontalSingleStackedBarViz = ({
                 .style('font-size', '20px');
             // outlay label
             chartSvg.append("foreignObject")
-                .attr('width', x(propsArr[3]) === x(propsArr[0]) ? x(propsArr[0]) - 10 : x(propsArr[0]) - x(propsArr[3]) - 10)
+                .attr('width', x(propsArr[0]) - x(propsArr[3]) <= 100 ? x(propsArr[3]) - 10 : x(propsArr[0]) - x(propsArr[3]) - 10)
                 .attr('height', 70)
-                .attr('x', x(propsArr[3]) === x(propsArr[0]) ? 0 : x(propsArr[3]) + 10)
+                .attr('x', x(propsArr[0]) - x(propsArr[3]) <= 100 ? 0 : x(propsArr[3]) + 10)
                 .attr('y', 20)
                 .html(`<div className="award-amounts-viz-outlays__desc-text"><strong>${outlayedAmountValue}</strong><br />${outlayedAmountLabel}</div>`)
                 .select('div')
-                .style('float', x(propsArr[3]) === x(propsArr[0]) ? 'right' : 'left')
-                .style('text-align', x(propsArr[3]) === x(propsArr[0]) ? 'right' : 'left')
+                .style('float', x(propsArr[0]) - x(propsArr[3]) <= 100 ? 'right' : 'left')
+                .style('text-align', x(propsArr[0]) - x(propsArr[3]) <= 100 ? 'right' : 'left')
                 .select('strong')
                 .style('font-size', '20px');
             // obligated label
             chartSvg.append("foreignObject")
-                .attr('width', x(propsArr[2]) === x(propsArr[0]) ? x(propsArr[0]) - 10 : x(propsArr[0]) - x(propsArr[2]) - 10)
+                .attr('width', x(propsArr[0]) - x(propsArr[2]) <= 100 ? x(propsArr[2]) - 10 : x(propsArr[0]) - x(propsArr[2]) - 10)
                 .attr('height', 70)
-                .attr('x', x(propsArr[2]) === x(propsArr[0]) ? 0 : x(propsArr[2]) + 10)
+                .attr('x', x(propsArr[0]) - x(propsArr[2]) <= 100 ? 0 : x(propsArr[2]) + 10)
                 .attr('y', 90)
                 .html(`<div className="award-amounts-viz-outlays__desc-text"><strong>${obligatedAmountValue}</strong><br />${obligatedAmountLabel}</div>`)
                 .select('div')
-                .style('float', x(propsArr[2]) === x(propsArr[0]) ? 'right' : 'left')
-                .style('text-align', x(propsArr[2]) === x(propsArr[0]) ? 'right' : 'left')
+                .style('float', x(propsArr[0]) - x(propsArr[2]) <= 100 ? 'right' : 'left')
+                .style('text-align', x(propsArr[0]) - x(propsArr[2]) <= 100 ? 'right' : 'left')
                 .select('strong')
                 .style('font-size', '20px');
         };
