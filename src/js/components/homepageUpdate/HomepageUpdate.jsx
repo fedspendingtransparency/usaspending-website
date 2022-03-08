5ff875334dc8a92242010d735f4d169e698d2b88
anjenkin <blackwell_andrea@bah.com>
2022-03-08 11:54:42 -0500

    added page wrapper

--- src/js/components/homepageUpdate/HomepageUpdate.jsx
+++ src/js/components/homepageUpdate/HomepageUpdate.jsx
@@ -5,35 +5,30 @@
 
 import React from 'react';
 
-import * as MetaTagHelper from 'helpers/metaTagHelper';
-import Footer from 'containers/Footer';
-
-import Header from 'containers/shared/HeaderContainer';
-import GlobalModalContainer from 'containers/globalModal/GlobalModalContainer';
-import MetaTags from '../sharedComponents/metaTags/MetaTags';
-
 import Download from './download/Download';
 import Community from './community/Community';
 import Hero from './hero/Hero';
 import Covid from './covid/Covid';
 import GettingStarted from './gettingStarted/GettingStarted';
+import PageWrapper from "../sharedComponents/PageWrapper";
+import { homePageMetaTags } from "../../helpers/metaTagHelper";
 
 require('pages/homepageUpdate/homepageUpdate.scss');
 
 const HomepageUpdate = () => (
-    <div className="usa-da-home-page">
-        <MetaTags {...MetaTagHelper.homePageMetaTags} />
-        <Header />
-        <main id="main-content" className="homepage-content">
+    <PageWrapper
+        pageName="Homepage"
+        classNames="usa-da-home-page"
+        noHeader
+        metaTagProps={{ ...homePageMetaTags }}>
+        <main id="main-content" className="main-content homepage-content">
             <Hero />
             <Covid />
             <GettingStarted />
             <Download />
             <Community />
         </main>
-        <GlobalModalContainer />
-        <Footer pageName="Homepage" />
-    </div>
+    </PageWrapper>
 );
 
 export default HomepageUpdate;
