9ea5611723a1da59da2234452b7c1d9c51dbe206
anjenkin <blackwell_andrea@bah.com>
2022-03-28 20:04:29 -0400

    fixed to make svg responsive

--- src/js/components/homepageUpdate/hero/Hero.jsx
+++ src/js/components/homepageUpdate/hero/Hero.jsx
@@ -21,51 +21,53 @@ const Hero = () => {
         label: 'about'
     });
     return (
-        <section className="homepage-hero-content" aria-label="Hero sections">
+        <section className="homepage-hero-container" aria-label="Hero sections">
+            <div className="homepage-hero-content">
             {/* temporary, until the animated component is done */ }
-            <div className="hero__temp-h1">Download government spending by who
-                receives money
-            </div>
-            <div className="hero__lower-wrapper">
-                <div className="hero__left-image-wrapper">
-                    <picture>
-                        <img
-                            src="../../../../img/homepage-hero-person-left.svg"
-                            alt="left" />
-                    </picture>
+                <div className="hero__temp-h1">Download government spending by who
+                    receives money
                 </div>
-                <div className="hero__center-content-wrapper">
-                    <div className="hero__center-content">
-                        <div className="hero__button-container">
-                            <Link
-                                className="hero__button-search"
-                                to="/search"
-                                onClick={trackSearchLink}>
-                                Start Searching Awards
-                            </Link>
-                            <Link
-                                className="hero__button-learn"
-                                to="/about"
-                                onClick={trackAboutLink}>
-                                Learn About USAspending
-                            </Link>
-                        </div>
-                        <div className="hero__text-container">
-                            <p>
-                                USAspending is the official open data source of federal spending information.
-                            </p>
-                        </div>
-                        <div className="hero__pause-button-container">
-                            <p>pause button placeholder</p>
+                <div className="hero__lower-wrapper">
+                    <div className="hero__left-image-wrapper">
+                        <picture>
+                            <img
+                                src="../../../../img/homepage-hero-person-left.svg"
+                                alt="left" />
+                        </picture>
+                    </div>
+                    <div className="hero__center-content-wrapper">
+                        <div className="hero__center-content">
+                            <div className="hero__button-container">
+                                <Link
+                                    className="hero__button-search"
+                                    to="/search"
+                                    onClick={trackSearchLink}>
+                                    Start Searching Awards
+                                </Link>
+                                <Link
+                                    className="hero__button-learn"
+                                    to="/about"
+                                    onClick={trackAboutLink}>
+                                    Learn About USAspending
+                                </Link>
+                            </div>
+                            <div className="hero__text-container">
+                                <p>
+                                    USAspending is the official open data source of federal spending information.
+                                </p>
+                            </div>
+                            <div className="hero__pause-button-container">
+                                <p>pause button placeholder</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="hero__right-image-wrapper">
-                    <picture>
-                        <img
-                            src="../../../../img/homepage-hero-people-right.svg"
-                            alt="right" />
-                    </picture>
+                    <div className="hero__right-image-wrapper">
+                        <picture>
+                            <img
+                                src="../../../../img/homepage-hero-people-right.svg"
+                                alt="right" />
+                        </picture>
+                    </div>
                 </div>
             </div>
         </section>
