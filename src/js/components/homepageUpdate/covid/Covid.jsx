e5cc071ba45f37ff948723fcc214ba73e163a171
brianpetway <brian.petway@kc.frb.org>
2022-03-29 13:59:11 -0500

    8514 - wip; initial layout, mobile

--- src/js/components/homepageUpdate/covid/Covid.jsx
+++ src/js/components/homepageUpdate/covid/Covid.jsx
@@ -3,34 +3,53 @@
  * Created by Andrea Blackwell 03/07/22
  */
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { throttle } from 'lodash';
 import {FlexGridRow, FlexGridCol} from 'data-transparency-ui';
+import { mediumScreen } from 'dataMapping/shared/mobileBreakpoints';
 
 require('pages/homepageUpdate/_homepageCovidSection.scss');
 
-const Covid = () => (
-    <section
-        className="homepage-covid"
-        aria-label="Covid sections">
-        <FlexGridRow
-            className="grid-content">
-            <FlexGridCol width={6} className="homepage-covid__column-one">
-                <div className="homepage-covid__column-one-content-wrapper">
-                    <div className="homepage-covid__heading">
-                        The Federal Response to COVID-19
+const Covid = () => {
+    const [windowWidth, setWindowWidth] = useState(0);
+    const [isMobile, setIsMobile] = useState(window.innerWidth < mediumScreen);
+    useEffect(() => {
+        const handleResize = throttle(() => {
+            const newWidth = window.innerWidth;
+            if (windowWidth !== newWidth) {
+                setWindowWidth(newWidth);
+                setIsMobile(newWidth < mediumScreen);
+            }
+        }, 50);
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    return (
+        <section
+            className="homepage-covid"
+            aria-label="Covid sections">
+            <FlexGridRow
+                className="grid-content">
+                <div className="homepage-covid__background-flair" />
+                <FlexGridCol width={12} desktop={6} className="homepage-covid__column-one">
+                    <div className="homepage-covid__column-one-content-wrapper">
+                        <div className="homepage-covid__heading">
+                            The Federal Response to COVID-19
+                        </div>
+                        <div className="homepage-covid__content">
+                            The federal government has spent [$3.59 trillion] in response to COVID-19.
+                        </div>
                     </div>
-                    <div className="homepage-covid__content">
-                        The federal government has spent [$3.59 trillion] in response to COVID-19.
+                </FlexGridCol>
+                <FlexGridCol width={12} desktop={6} className="homepage-covid__column-two">
+                    <div className="homepage-covi__column-two-content-wrapper">
+                        Column Two Content
                     </div>
-                </div>
-            </FlexGridCol>
-            <FlexGridCol width={6} className="homepage-covid__column-two">
-                <div className="homepage-covi__column-two-content-wrapper">
-                    Column Two Content
-                </div>
-            </FlexGridCol>
-        </FlexGridRow>
-    </section>
-);
+                </FlexGridCol>
+            </FlexGridRow>
+        </section>
+    );
+};
 
 export default Covid;
