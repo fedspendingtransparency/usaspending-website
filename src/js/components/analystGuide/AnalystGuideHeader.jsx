19049946cd7a264356100fcb024d28686eef5bcd
anjenkin <blackwell_andrea@bah.com>
2022-03-31 09:55:19 -0400

    fixed h1 and h2

--- src/js/components/analystGuide/AnalystGuideHeader.jsx
+++ src/js/components/analystGuide/AnalystGuideHeader.jsx
@@ -5,8 +5,8 @@ const AnalystGuideHeader = ({ title, subtitle }) => (
 	<div className="hero-container">
 		<div className="hero__left-item">
 			<div className="hero__left-item__content">
-				<h2>{title}</h2>
-				<h3>{subtitle}</h3>
+				<h1>{title}</h1>
+				<h2>{subtitle}</h2>
 			</div>
 		</div>
 		<div className="hero__right-item">
