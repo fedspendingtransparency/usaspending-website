9786848a8b014ab9ca15fc496d78e8363efea6ac
Nick Torres <nicholas.torres@kc.frb.org>
2022-04-08 13:48:56 -0600

    hero image styling

--- src/js/components/analystGuide/AnalystGuideHeader.jsx
+++ src/js/components/analystGuide/AnalystGuideHeader.jsx
@@ -13,6 +13,7 @@ const AnalystGuideHeader = ({ title, subtitle }) => (
             <div className="hero__right-image-wrapper">
                 <picture>
                     <img
+                        className="hero__right-image"
                         role="presentation"
                         src="../../../../img/analyst-guide-hero-graphic-2x.png"
                         alt="Treasury Department building in the background overlaid with a floating bar chart with a trend line." />
