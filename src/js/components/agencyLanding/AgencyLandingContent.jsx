999938b509e56a3f6a326d300fe8f452f6d87b65
anjenkin <blackwell_andrea@bah.com>
2022-04-09 15:32:35 -0400

    added link to cj csv

--- src/js/components/agencyLanding/AgencyLandingContent.jsx
+++ src/js/components/agencyLanding/AgencyLandingContent.jsx
@@ -28,7 +28,8 @@ export default class AgencyLandingContent extends React.Component {
                         Find an Agency Profile.
                     </h2>
                     <div className="landing-page__description">
-                        Featuring information on each agency&rsquo;s total obligations, along with obligation breakdowns by Object Class and Federal Accounts, our Agency Profiles help you understand how each agency spends its funding.
+                        <p>Featuring information on each agency&rsquo;s total obligations, along with obligation breakdowns by Object Class and Federal Accounts, our Agency Profiles help you understand how each agency spends its funding.</p>
+                        <p>P.L. 117-103 requires the posting of a list of agencies that have submitted Congressional Justifications and the dates that those were submitted to Congress. <a href="https://usaspending.gov/data/cj_list.csv" target="_blank" rel="noopener noreferrer">Click here</a> to download a machine-readable version of this list. Note that this list contains agencies that do not currently submit data to USAspending.gov and therefore do not appear elsewhere on the website.</p>
                     </div>
                 </div>
                 <LandingSearchBar
