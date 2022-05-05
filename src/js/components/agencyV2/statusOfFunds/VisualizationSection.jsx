e3d96644bb76b3373e9edefee174c7dc46cd6258
Nick Torres <nicholas.torres@kc.frb.org>
2022-05-05 14:18:21 -0600

    toggle styling and component

--- src/js/components/agencyV2/statusOfFunds/VisualizationSection.jsx
+++ src/js/components/agencyV2/statusOfFunds/VisualizationSection.jsx
@@ -8,6 +8,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { levels } from './StatusOfFunds';
 import StatusOfFundsChart from '../visualizations/StatusOfFundsChart';
+import RoundedToggle from "../../sharedComponents/RoundedToggle";
 
 const propTypes = {
     level: PropTypes.number.isRequired,
@@ -43,6 +44,9 @@ const VisualizationSection = ({
 }) => (
     <div className="status-of-funds__visualization">
         <h6>{level === 1 ? selectedSubcomponent?.name : agencyName} by <strong>{levels[level]}</strong> for FY {fy}</h6>
+        <div className="status-of-funds__controls">
+            <RoundedToggle label="View Outlays" />
+        </div>
         <div className="status-of-funds__visualization-chart">
             <StatusOfFundsChart fetchFederalAccounts={fetchFederalAccounts} totalItems={totalItems} setTotalItems={setTotalItems} loading={loading} setLoading={setLoading} fy={fy} results={results} level={level} setLevel={setLevel} />
         </div>
---
+++ src/js/components/sharedComponents/RoundedToggle.jsx
@@ -0,0 +1,26 @@
+/**
+ * RoundedToggle.jsx
+ * created by Nick Torres 5/5/2022
+ */
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../../../_scss/components/_roundedtoggle.scss';
+
+const propTypes = {
+    label: PropTypes.string,
+}
+
+const RoundedToggle = ({
+    label
+}) => (
+    <div className="rounded-toggle__wrapper">
+        <p className="rounded-toggle__label">{label}</p>
+        <label className="rounded-toggle__switch">
+            <input type="checkbox" />
+            <span className="rounded-toggle__slider rounded-toggle__round"></span>
+        </label>
+    </div>
+);
+
+RoundedToggle.propTypes = propTypes;
+export default RoundedToggle;
