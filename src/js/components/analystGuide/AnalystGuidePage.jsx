61a627899d8591bd4fd23cd85332f4e9f2578cdb
Nick Torres <nicholas.torres@kc.frb.org>
2022-05-05 09:35:52 -0600

    css styling

--- src/js/components/analystGuide/AnalystGuidePage.jsx
+++ src/js/components/analystGuide/AnalystGuidePage.jsx
@@ -5,12 +5,13 @@
 
 import React, { useState, useEffect } from 'react';
 import { Redirect } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { FlexGridRow, FlexGridCol, ShareIcon } from "data-transparency-ui";
+import { faFileDownload } from "@fortawesome/free-solid-svg-icons";
 import 'pages/analystGuide/analystGuide.scss';
 import { getBaseUrl, handleShareOptionClick } from 'helpers/socialShare';
 import { useDispatch } from "react-redux";
 import { showModal } from 'redux/actions/modal/modalActions';
-
 import AnalystGuideHeader from './AnalystGuideHeader';
 import PageWrapper from "../sharedComponents/PageWrapper";
 import { analystGuideMetaTags } from "../../helpers/metaTagHelper";
@@ -58,12 +59,29 @@ const AnalystGuidePage = () => {
                 </section>
                 <FlexGridRow style={{ justifyContent: 'center' }}>
                     <FlexGridCol desktop={6} tablet={12} className="analyst-guide-body">
-                        <div className="analyst-guide__share-wrapper">
-                            <ShareIcon
-                                url={getBaseUrl(slug)}
-                                onShareOptionClick={onShareClick}
-                                colors={{ backgroundColor: "white", color: "#0071bc" }}
-                                classNames="" />
+                        <div className="analyst-guide__share-dl-wrapper">
+                            <div className="analyst-guide__share-wrapper">
+                                <ShareIcon
+                                    url={getBaseUrl(slug)}
+                                    onShareOptionClick={onShareClick}
+                                    colors={{ backgroundColor: "white", color: "#0071bc" }}
+                                    classNames="" />
+                            </div>
+                            <div className="analyst-guide__download-wrapper">
+                                <button
+                                    value="/data/analyst-guide-download.pdf"
+                                    className="analyst-guide__download-button">
+                                    <a
+                                        href="/data/analyst-guide-download.pdf"
+                                        className="usa-button-link__icon"
+                                        download>
+                                        <FontAwesomeIcon data-href="/data/analyst-guide-download.pdf" icon={faFileDownload} className="analyst-guide__download-icon" />
+                                    </a>
+                                </button>
+                                <div>
+                                    <span>Download</span>
+                                </div>
+                            </div>
                         </div>
                         <AnalystGuideIntro onExternalLinkClick={onExternalLinkClick} />
                         <AnalystGuideQuestions onExternalLinkClick={onExternalLinkClick} />
