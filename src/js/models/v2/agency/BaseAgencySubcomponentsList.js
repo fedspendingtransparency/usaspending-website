7be22856dba30cac2bc0407e79e27fa7de4579cd
Nick Torres <nicholas.torres@kc.frb.org>
2022-05-12 14:32:00 -0600

    wip

--- src/js/models/v2/agency/BaseAgencySubcomponentsList.js
+++ src/js/models/v2/agency/BaseAgencySubcomponentsList.js
@@ -6,17 +6,22 @@ import { formatMoneyWithUnitsShortLabel } from 'helpers/moneyFormatter';
 
 const BaseAgencySubcomponentsList = {
     populate(data) {
+        console.debug(data);
         this.id = data?.id || '';
         this.name = data?.name || '';
         /* eslint-disable camelcase */
         this._budgetaryResources = data?.total_budgetary_resources || 0;
         this._obligations = data?.total_obligations || 0;
+        this._outlays = data?.total_outlays || 0;
     },
     get budgetaryResources() {
         return formatMoneyWithUnitsShortLabel(this._budgetaryResources, 2);
     },
     get obligations() {
         return formatMoneyWithUnitsShortLabel(this._obligations, 2);
+    },
+    get outlays() {
+        return formatMoneyWithUnitsShortLabel(this._outlays, 2);
     }
 };
 
