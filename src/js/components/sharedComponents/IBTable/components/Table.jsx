8e90708a25d3c4a23c323b275962d175ed80e3eb
brianpetway <brian.petway@kc.frb.org>
2022-06-06 17:31:48 -0500

    8729 - fixed x and y scroll; removed unused functions, wrappers

--- src/js/components/sharedComponents/IBTable/components/Table.jsx
+++ src/js/components/sharedComponents/IBTable/components/Table.jsx
@@ -40,17 +40,12 @@ export default class Table extends React.Component {
         super(props);
 
         this._restorePointerTimer = null;
-
-        this._headerComponent = null;
         this._bodyComponent = null;
-
         this._tableWrapper = null;
         this._internalDiv = null;
         this._tableId = `${uniqueId()}`;
 
         this._scrolledTable = this._scrolledTable.bind(this);
-        this._scrolledTableWithTopScroller = this._scrolledTableWithTopScroller.bind(this);
-        this._scrolledHeader = this._scrolledHeader.bind(this);
         this._scrolledTableTop = this._scrolledTableTop.bind(this);
         this._scrolledTableBottom = this._scrolledTableBottom.bind(this);
     }
@@ -69,105 +64,18 @@ export default class Table extends React.Component {
         }, 300);
     }
 
-    scrollTo(x, y) {
-        const scrollOperation = {
-            operation: () => {
-                if (!this._tableWrapper || !this._headerWrapper) {
-                    return;
-                }
-
-                this._tableWrapper.scrollLeft = x;
-                this._tableWrapper.scrollTop = y;
-                this._headerWrapper.scrollLeft = 0;
-            },
-            type: 'overrideScroll',
-            isSingle: true,
-            args: []
-        };
-
-        RenderQueue.addWrite(scrollOperation);
-    }
-
-    _scrolledHeader() {
-    // The header can only scroll through the use of accessibility hooks.
-    // Because there are two scrolling elements (the transform offset + the scroll offset), we
-    // need to combine them into a single scroll offset.
-        const scrollOperation = {
-            operation: () => {
-                const realX = this._headerWrapper.scrollLeft + this._headerComponent.currentX;
-                const x = realX;
-                const y = this._tableWrapper.scrollTop;
-                ScrollManager.update({
-                    x,
-                    y
-                });
-
-                // Scroll events in the table body will be applied as transforms while native
-                // header scrolls via accessibility hooks, so we need to reset the header scrollLeft
-                // to 0 and apply the full horizontal offset via transforms.
-                // By calling the scrollTo function, we will apply the full horizontal offset to the
-                // table body while simultaneously resetting the header scrollLeft to 0. The table
-                // body's scrollLeft change will trigger the same scroll event that occurs from
-                // native mouse scrolls in the table body (_scrolledTable). _scrolledTable will
-                // synchronize the horizontal offset in the header entirely via transforms.
-                this.scrollTo(x, y);
-            },
-            type: 'scroll',
-            isSingle: true,
-            args: []
-        };
-
-        RenderQueue.addRead(scrollOperation);
-    }
-
     _scrolledTableTop() {
         const topBar = document.getElementById("topBar");
         const bottomBar = document.getElementById("bottomBar");
         bottomBar.scrollLeft = topBar.scrollLeft;
-        this._scrolledTableWithTopScroller(topBar.scrollLeft);
+        this._scrolledTable();
     }
 
     _scrolledTableBottom() {
         const topBar = document.getElementById("topBar");
         const bottomBar = document.getElementById("bottomBar");
         topBar.scrollLeft = bottomBar.scrollLeft;
-        this._scrolledTableWithTopScroller(bottomBar.scrollLeft);
-    }
-
-    _scrolledTableWithTopScroller(xScroll) {
-        const scrollOperation = {
-            operation: () => {
-                // this._tableWrapper = calledFromTop ? topBar.scrollLeft : bottomBar.scrollLeft;
-                const x = xScroll;
-                const y = this._tableWrapper.scrollTop;
-                ScrollManager.update({
-                    x,
-                    y
-                });
-            },
-            type: 'scroll',
-            isSingle: true,
-            args: []
-        };
-
-        const pointerOperation = {
-            operation: () => {
-                if (this._restorePointerTimer) {
-                    window.clearTimeout(this.restorePointer);
-                    this._restorePointerTimer = null;
-                }
-                this._internalDiv.style.pointerEvents = 'none';
-                this._restorePointerTimer = window.setTimeout(() => {
-                    this._internalDiv.style.pointerEvents = 'auto';
-                }, 150);
-            },
-            type: 'pointer',
-            isSingle: true,
-            args: []
-        };
-
-        RenderQueue.addRead(scrollOperation);
-        RenderQueue.addWrite(pointerOperation);
+        this._scrolledTable();
     }
 
     _scrolledTable() {
@@ -263,20 +171,13 @@ export default class Table extends React.Component {
                     <div
                         className="ibt-table-header-container"
                         role="presentation"
-                        style={headerStyle}
-                        onScroll={this._scrolledHeader}
-                        ref={(div) => {
-                            this._headerWrapper = div;
-                        }}>
+                        style={headerStyle}>
                         <HeaderRow
                             tableId={this._tableId}
                             contentWidth={this.props.contentWidth}
                             headerHeight={this.props.headerHeight}
                             columns={this.props.columns}
-                            headerCellRender={this.props.headerCellRender}
-                            ref={(component) => {
-                                this._headerComponent = component;
-                            }} />
+                            headerCellRender={this.props.headerCellRender} />
                     </div>
                     <div
                         className="ibt-table-body-section"
@@ -329,6 +230,17 @@ export default class Table extends React.Component {
                         <div className="ibt-table__scroller" style={contentStyle} />
                     </div>
                     <div
+                        className="ibt-table-header-container"
+                        role="presentation"
+                        style={headerStyle}>
+                        <HeaderRow
+                            tableId={this._tableId}
+                            contentWidth={this.props.contentWidth}
+                            headerHeight={this.props.headerHeight}
+                            columns={this.props.columns}
+                            headerCellRender={this.props.headerCellRender} />
+                    </div>
+                    <div
                         className="ibt-table-body-section"
                         role="presentation"
                         style={bodyStyle}
@@ -337,24 +249,6 @@ export default class Table extends React.Component {
                         ref={(div) => {
                             this._tableWrapper = div;
                         }}>
-                        <div
-                            className="ibt-table-header-container"
-                            role="presentation"
-                            style={headerStyle}
-                            onScroll={this._scrolledHeader}
-                            ref={(div) => {
-                                this._headerWrapper = div;
-                            }}>
-                            <HeaderRow
-                                tableId={this._tableId}
-                                contentWidth={this.props.contentWidth}
-                                headerHeight={this.props.headerHeight}
-                                columns={this.props.columns}
-                                headerCellRender={this.props.headerCellRender}
-                                ref={(component) => {
-                                    this._headerComponent = component;
-                                }} />
-                        </div>
                         <div
                             className="ibt-table-content"
                             role="presentation"
