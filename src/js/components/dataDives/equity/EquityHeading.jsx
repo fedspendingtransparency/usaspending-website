11d7e59da996914599ac3539378b0e29b07f014d
brianpetway <brian.petway@kc.frb.org>
2022-06-17 16:44:36 -0500

    8868 - added css and the rest of the copy

--- src/js/components/dataDives/equity/EquityHeading.jsx
+++ src/js/components/dataDives/equity/EquityHeading.jsx
@@ -4,21 +4,27 @@
  */
 
 import React from 'react';
-import { FlexGridRow } from 'data-transparency-ui';
+import { FlexGridRow, FlexGridCol } from 'data-transparency-ui';
 
 
-const EquityHeading = ({ heading, intro, note }) => {
-    const blah = 'blah';
-
-    return (
-        <section
-            className="equity-heading"
-            aria-label="EquityHeading sections">
-            <FlexGridRow className="grid-content">
-                <div className="equity-heading_heading">{heading}</div>
-            </FlexGridRow>
-        </section>
-    );
-};
+const EquityHeading = ({ heading, intro, note }) => (
+    <section
+        className="equity-heading"
+        aria-label="EquityHeading sections">
+        <FlexGridRow className="grid-content">
+            <FlexGridCol>
+                <div className="equity-heading__heading">{heading}</div>
+                <div className="equity-heading__stats-row">
+                    <div className="equity-heading__pill">Partner Collaboration</div>
+                    <div className="equity-heading__date">Jan 18, 2022</div>
+                    <div className="equity-heading__dot">&#8226;</div>
+                    <div>4 posts</div>
+                </div>
+                <div className="equity-heading__intro">{intro}</div>
+                <div className="equity-heading__note">{note}</div>
+            </FlexGridCol>
+        </FlexGridRow>
+    </section>
+);
 
 export default EquityHeading;
