9cb101db2c1801491af7a85b8c267c1710dbbcf4
Nick Torres <nicholas.torres@kc.frb.org>
2022-06-21 11:18:09 -0600

    wip

--- src/js/components/dataDives/sections/MainCards.jsx
+++ src/js/components/dataDives/sections/MainCards.jsx
@@ -5,7 +5,10 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useDispatch } from "react-redux";
 import { FlexGridRow, FlexGridCol } from 'data-transparency-ui';
+import { showModal } from 'redux/actions/modal/modalActions';
 // import ReadMoreUpdated from '../../sharedComponents/ReadMoreUpdated';
 import TOPCard from '../../sharedComponents/TOPCard';
 
@@ -14,6 +17,14 @@ const propTypes = {
 };
 
 const MainCards = ({ contentObject }) => {
+    const dispatch = useDispatch();
+    const onExternalLinkClick = (e) => {
+        e.persist();
+        if (e?.target) {
+            dispatch(showModal(e.target.parentNode.getAttribute('data-href') || e.target.getAttribute('data-href') || e.target.value));
+        }
+    };
+
     const bowieImg = <img src="../../../../img/top-bowie-state-combined-image.svg" alt="" />;
     const kansasImg = <img src="../../../../img/top-university-kansas-combined-image.svg" alt="" />;
     const morehouseImg = <img src="../../../../img/top-morehouse-combined-image.svg" alt="" />;
@@ -24,6 +35,56 @@ const MainCards = ({ contentObject }) => {
     const kansasHdg = <h2>Morehouse College</h2>;
     const morehouseHdg = <h2>University of Kansas Center for Public Partnerships and Research</h2>;
     const momHdg = <h2>The Mom Project</h2>;
+
+    const bowieBtn = (
+        <button
+            value={contentObject.bowieLink}
+            role="link"
+            onClick={onExternalLinkClick}>
+    See Project{' '}
+            <span
+                data-href={contentObject.bowieLink}
+                className="usa-button-link__icon">
+                <FontAwesomeIcon data-href={contentObject.bowieLink} icon="external-link-alt" />
+            </span>
+        </button>);
+    const kansasBtn = (
+        <button
+            value={contentObject.kansasLink}
+            role="link"
+            onClick={onExternalLinkClick}>
+    See Project{' '}
+            <span
+                data-href={contentObject.kansasLink}
+                className="usa-button-link__icon">
+                <FontAwesomeIcon data-href={contentObject.kansasLink} icon="external-link-alt" />
+            </span>
+        </button>);
+    const morehouseBtn = (
+        <button
+            value={contentObject.morehouseLink}
+            role="link"
+            onClick={onExternalLinkClick}>
+    See Project{' '}
+            <span
+                data-href={contentObject.morehouseLink}
+                className="usa-button-link__icon">
+                <FontAwesomeIcon data-href={contentObject.morehouseLink} icon="external-link-alt" />
+            </span>
+        </button>);
+    const momBtn = (
+        <button
+            value={contentObject.momLink}
+            role="link"
+            onClick={onExternalLinkClick}>
+    See Project{' '}
+            <span
+                data-href={contentObject.momLink}
+                className="usa-button-link__icon">
+                <FontAwesomeIcon data-href={contentObject.momLink} icon="external-link-alt" />
+            </span>
+        </button>);
+
     const {
         bowieText, kansasText, momText, morehouseText
     } = contentObject;
@@ -32,18 +93,18 @@ const MainCards = ({ contentObject }) => {
         <section className="main-cards__wrapper">
             <FlexGridRow>
                 <FlexGridCol width={6} desktop={6} tablet={12} mobile={12}>
-                    <TOPCard image={bowieImg} imageColor="#ffbe60" heading={bowieHdg} text={bowieText} />
+                    <TOPCard image={bowieImg} imageColor="#ffbe60" heading={bowieHdg} text={bowieText} button={bowieBtn} />
                 </FlexGridCol>
                 <FlexGridCol width={6} desktop={6} tablet={12} mobile={12}>
-                    <TOPCard image={morehouseImg} imageColor="#339189" heading={morehouseHdg} text={morehouseText} />
+                    <TOPCard image={morehouseImg} imageColor="#339189" heading={morehouseHdg} text={morehouseText} button={morehouseBtn} />
                 </FlexGridCol>
             </FlexGridRow>
             <FlexGridRow>
                 <FlexGridCol width={6} desktop={6} tablet={12} mobile={12}>
-                    <TOPCard image={kansasImg} imageColor="#fa9441" heading={kansasHdg} text={kansasText} />
+                    <TOPCard image={kansasImg} imageColor="#fa9441" heading={kansasHdg} text={kansasText} button={kansasBtn} />
                 </FlexGridCol>
                 <FlexGridCol width={6} desktop={6} tablet={12} mobile={12}>
-                    <TOPCard image={momImg} imageColor="#29abe2" heading={momHdg} text={momText} />
+                    <TOPCard image={momImg} imageColor="#29abe2" heading={momHdg} text={momText} button={momBtn} />
                 </FlexGridCol>
             </FlexGridRow>
         </section>
