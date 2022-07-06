9e4a0506176751ed482f5efa4e9382ce3fb5f23d
brianpetway <brian.petway@kc.frb.org>
2022-07-06 15:46:33 -0500

    8908 - added content obj for heading

--- src/js/components/dataDives/WOSBPage.jsx
+++ src/js/components/dataDives/WOSBPage.jsx
@@ -9,6 +9,8 @@ import { ShareIcon } from 'data-transparency-ui';
 import PageWrapper from "../sharedComponents/PageWrapper";
 import { wosbPageMetaTags } from "../../helpers/metaTagHelper";
 
+require('pages/wosbPage/wosbPage.scss');
+
 const WOSBPage = () => {
     const slug = 'data-dives/women-owned-small-businesses-federal-contracting';
     const emailArgs = {
@@ -18,6 +20,17 @@ const WOSBPage = () => {
 
     const handleShare = (optionName) => {
         handleShareOptionClick(optionName, slug, emailArgs);
+    };
+
+    const HeadingContentObject = {
+        heading: 'Women-Owned Small Businesses (WOSB)',
+        tag: 'Partner Collaboration',
+        date: 'May 11, 2022',
+        numberOfPosts: '3',
+        intro: 'Learn about the federal government\'s progress toward helping Women-Owned Small Businesses win federal contracts.',
+        note: (
+            <p>The data represented in these charts includes only <b>prime award spending</b>, or agreements between the federal government and a non-federal recipient. The charts <b>do not include</b> sub-award spending, which are agreements between the recipient of a prime award and another entity to perform some of the work for the prime award.</p>
+        )
     };
 
     return (
@@ -31,7 +44,7 @@ const WOSBPage = () => {
                 <ShareIcon url={getBaseUrl(slug)} onShareOptionClick={handleShare} />
             ]}>
             <main id="main-content" className="main-content wosb-content">
-                <div>HEADING PLACEHOLDER</div>
+                <div content={HeadingContentObject}>HEADING PLACEHOLDER</div>
                 <div>CONTENT PLACEHOLDER</div>
             </main>
         </PageWrapper>
