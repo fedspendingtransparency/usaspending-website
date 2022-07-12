c7f1914374e5f81d817f7ad96a0b3aafd1e6c6de
Nick Torres <nicholas.torres@kc.frb.org>
2022-07-12 12:13:53 -0600

    unit test

---
+++ tests/components/sharedComponents/InfoTooltipContent-test.jsx
@@ -0,0 +1,18 @@
+/**
+ * InfoTooltipContent-test.jsx
+ * Created by Nick Torres 7/12/22
+ */
+
+import React from 'react';
+import { UnlinkedTooltip } from 'components/award/shared/InfoTooltipContent';
+import { render, screen } from '@test-utils';
+
+const mockText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.';
+
+describe('Tooltip tests', () => {
+    it('text for Unlinked tooltip should be rendered', () => {
+        render(<UnlinkedTooltip />);
+        expect(screen.queryByText('This award has not been linked to any federal account')).toBeTruthy();
+        expect(screen.queryByText(mockText)).toBeFalsy();
+    });
+});
