5eb82d52fd0bf8ec2970daab43fd4d63d9d82cf6
Nick Torres <nicholas.torres@kc.frb.org>
2022-07-14 07:21:41 -0600

    wip

--- tests/setup.js
+++ tests/setup.js
@@ -1,5 +1,4 @@
-
-import { configure } from "enzyme";
+import Enzyme from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import registerIcons from "../src/js/registerIcons";
 
@@ -7,4 +6,4 @@ window.URL.createObjectURL = function () {};
 registerIcons();
 
 // In Enzyme 3, shallow now by default triggers life cycle methods :thumbs-up: but this ruins all our tests :thumbs-down:
-configure({ adapter: new Adapter(), disableLifecycleMethods: true });
+Enzyme.configure({ adapter: new Adapter() });
