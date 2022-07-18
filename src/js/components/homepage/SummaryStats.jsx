3bf2ad242a7d51e576d1f4891bd4e93527c7ea44
anjenkin <blackwell_andrea@bah.com>
2022-07-18 13:55:44 -0400

    fix homepage max width

---
+++ src/js/components/homepage/SummaryStats.jsx
@@ -0,0 +1,13 @@
+/**
+ * SummaryStats.jsx
+ * Created by Andrea Blackwell 07/18/22
+ */
+
+import React from 'react';
+
+const SummaryStats = () => (
+    <div style={{ height: "98px", "background-color": "#00687D", "margin-top": "-16px" }}>
+        <p>Summary Stats</p>
+    </div>);
+
+export default SummaryStats;
