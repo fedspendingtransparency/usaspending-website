9ba28fdb6757d010bfa0dae097d4e1b1608ea99d
anjenkin <blackwell_andrea@bah.com>
2022-07-19 13:09:27 -0400

    initial mobile layout

--- src/js/components/homepage/SummaryStats.jsx
+++ src/js/components/homepage/SummaryStats.jsx
@@ -3,43 +3,102 @@
  * Created by Andrea Blackwell 07/18/22
  */
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FlexGridRow, FlexGridCol } from "data-transparency-ui";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const SummaryStats = () => (
-    <section className="summary-stats" style={{ height: "98px", "background-color": "#00687D" }}>
-        <FlexGridRow className="grid-content-max">
-            <FlexGridCol desktop="3" style={{ color: "white", margin: "24px 0 24px 8.33%" }}>
-                <span>So far this year, the federal government</span><br />
-                <span>plans to spend $4.30 Trillion including…</span>
-            </FlexGridCol>
-            <FlexGridCol desktop="6" style={{ color: "white", display: "flex"}}>
-                <div style={{ margin: "24px 104px 24px 56px" }}>
-                    <span>$710.11 Billion</span><br />
-                    <span>on Medicare</span>
-                </div>
-                <div style={{ margin: "24px 104px 24px 0" }}>
-                    <span>$617.04 Billion</span><br />
-                    <span>on National Defense</span>
-                </div>
-                <div style={{ margin: "24px 56px 24px 0" }}>
-                    <span>$6.89 Billion</span><br />
-                    <span>on Energy</span>
-                </div>
-            </FlexGridCol>
-            <FlexGridCol desktop="2" style={{ color: "white", margin: "24px 0" }}>
-                <FlexGridRow>
-                    <div style={{ "margin-right": "8px" }}>
-                        <span>See more breakdowns</span><br />
-                        <span>of federal spending</span>
-                    </div>
-                    <div style={{ margin: "auto 0" }}>
-                        <FontAwesomeIcon size="lg" fontSize="24" icon="arrow-alt-circle-right" />
-                    </div>
-                </FlexGridRow>
-            </FlexGridCol>
-        </FlexGridRow>
-    </section>);
+const SummaryStats = () => {
+    const [windowWidth, setWindowWidth] = useState(0);
+    const [isWide, setIsWide] = useState(0);
+
+    const handleWindowResize = () => {
+        console.log(window.innerWidth);
+        const wWidth = window.innerWidth;
+        if (windowWidth !== wWidth) {
+            setWindowWidth(wWidth);
+            setIsWide(wWidth >= 992);
+        }
+    };
+
+    useEffect(() => {
+        handleWindowResize();
+        window.addEventListener('resize', handleWindowResize);
+        return () => {
+            window.removeEventListener('resize', handleWindowResize);
+        };
+    }, [handleWindowResize]);
+
+    return (
+        <section className="summary-stats">
+            {isWide ?
+                <>
+                    <FlexGridRow className="grid-content">
+                        <FlexGridCol width={4} style={{ color: "white", margin: "24px 0", "min-width": "344px" }}>
+                            <span>So far this year, the federal government</span><br />
+                            <span>plans to spend $4.30 Trillion including…</span>
+                        </FlexGridCol>
+                        <FlexGridCol width={6} style={{ color: "white", display: "flex" }}>
+                            <div style={{ margin: "24px 7% 24px 3.5%" }}>
+                                <span>$710.11 Billion</span><br />
+                                <span>on Medicare</span>
+                            </div>
+                            <div style={{ margin: "24px 7% 24px 0" }}>
+                                <span>$617.04 Billion</span><br />
+                                <span>on National Defense</span>
+                            </div>
+                            <div style={{ margin: "24px 7% 24px 0" }}>
+                                <span>$6.89 Billion</span><br />
+                                <span>on Energy</span>
+                            </div>
+                        </FlexGridCol>
+                        <FlexGridCol width={2} style={{ color: "white", margin: "24px 0" }}>
+                            <FlexGridRow>
+                                <div style={{ "margin-right": "8px" }}>
+                                    <span>See more breakdowns</span><br />
+                                    <span>of federal spending</span>
+                                </div>
+                                <div style={{ margin: "auto 0" }}>
+                                    <FontAwesomeIcon size="lg" fontSize="24" icon="arrow-alt-circle-right" />
+                                </div>
+                            </FlexGridRow>
+                        </FlexGridCol>
+                    </FlexGridRow>
+                </>
+                :
+                <>
+                    <FlexGridRow className="grid-content">
+                        <FlexGridCol width={12} style={{ color: "white", margin: "32x 0 24px", "min-width": "344px", padding: "0" }}>
+                            <span>So far this year, the federal government</span><br />
+                            <span>plans to spend $4.30 Trillion including…</span>
+                        </FlexGridCol>
+                        <FlexGridCol width={12} style={{ color: "white", display: "flex", "min-width": "470px", padding: "0", "margin-bottom": "24px", flexDirection: windowWidth <= 576 ? 'column' : 'row' }}>
+                            <div style={{ margin: "24px 7% 24px 3.5%" }}>
+                                <span>$710.11 Billion</span><br />
+                                <span>on Medicare</span>
+                            </div>
+                            <div style={{ margin: "24px 7% 24px 0" }}>
+                                <span>$617.04 Billion</span><br />
+                                <span>on National Defense</span>
+                            </div>
+                            <div style={{ margin: "24px 7% 24px 0" }}>
+                                <span>$6.89 Billion</span><br />
+                                <span>on Energy</span>
+                            </div>
+                        </FlexGridCol>
+                        <FlexGridCol width={12} style={{ color: "white", margin: "0 0 32px", "min-width": "344px" }}>
+                            <FlexGridRow style={{ "justify-content": "center" }}>
+                                <div style={{ "margin-right": "8px" }}>
+                                    <span>See more breakdowns</span><br />
+                                    <span>of federal spending</span>
+                                </div>
+                                <div style={{ margin: "auto 0" }}>
+                                    <FontAwesomeIcon size="lg" fontSize="24" icon="arrow-alt-circle-right" />
+                                </div>
+                            </FlexGridRow>
+                        </FlexGridCol>
+                    </FlexGridRow>
+                </>}
+        </section>);
+};
 
 export default SummaryStats;
