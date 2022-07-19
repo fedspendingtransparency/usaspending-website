f05f71a0da13a768446333e3f1a84a61e4bf7abf
anjenkin <blackwell_andrea@bah.com>
2022-07-18 20:11:19 -0400

    initial layout

--- src/js/components/homepage/SummaryStats.jsx
+++ src/js/components/homepage/SummaryStats.jsx
@@ -4,10 +4,34 @@
  */
 
 import React from 'react';
+import { FlexGridRow, FlexGridCol } from "data-transparency-ui";
 
 const SummaryStats = () => (
-    <div style={{ height: "98px", "background-color": "#00687D", "margin-top": "-16px" }}>
-        <p>Summary Stats</p>
-    </div>);
+    <section className="summary-stats" style={{ height: "98px", "background-color": "#00687D" }}>
+        <FlexGridRow className="grid-content-max">
+            <FlexGridCol desktop="4" style={{ color: "white", margin: "24px 0" }}>
+                <span>So far this year, the federal government</span><br />
+                <span>plans to spend $4.30 Trillion including…</span>
+            </FlexGridCol>
+            <FlexGridCol desktop="6" style={{ color: "white", display: "flex" }}>
+                <div style={{ margin: "24px 104px 24px 56px" }}>
+                    <span>$710.11 Billion</span><br />
+                    <span>on Medicare</span>
+                </div>
+                <div style={{ margin: "24px 104px 24px 0" }}>
+                    <span>$617.04 Billion</span><br />
+                    <span>on National Defense</span>
+                </div>
+                <div style={{ margin: "24px 56px 24px 0" }}>
+                    <span>$6.89 Billion</span><br />
+                    <span>on Energy</span>
+                </div>
+            </FlexGridCol>
+            <FlexGridCol desktop="2" style={{ color: "white", margin: "24px 0" }}>
+                <span>See more breakdowns</span><br />
+                <span>of federal spending</span>
+            </FlexGridCol>
+        </FlexGridRow>
+    </section>);
 
 export default SummaryStats;
