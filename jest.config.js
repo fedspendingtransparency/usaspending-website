8be15e9200cfbd09fd216ab68584558f2fa736c2
Nick Torres <nicholas.torres@kc.frb.org>
2022-07-20 10:49:13 -0600

    removed tz

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,5 @@
 // TODO: [DEV-5897] USAS Front End Test Configuration Update: Functional Components & Redux
 // Update test configuration to make testing react functional components easier.
-process.env.TZ = 'America/New_York';
-
 module.exports = {
     rootDir: ".",
     testRegex: "(tests|scripts)/.*-(test)\\.jsx?$",
