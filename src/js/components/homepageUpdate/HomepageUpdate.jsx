dc6a70ab0113d492676a3a6eb357245fd4b4e580
anjenkin <blackwell_andrea@bah.com>
2022-09-06 19:42:20 -0400

    fixed linting errors

--- src/js/components/homepageUpdate/HomepageUpdate.jsx
+++ src/js/components/homepageUpdate/HomepageUpdate.jsx
@@ -3,8 +3,10 @@
  * Created by Brian Petway 08/22/22
  */
 
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { Redirect } from "react-router-dom";
 
+import GlobalConstants from "GlobalConstants";
 import PageWrapper from "../sharedComponents/PageWrapper";
 import { homePageMetaTags } from "../../helpers/metaTagHelper";
 import Hero from '../homepage/hero/Hero';
@@ -16,32 +18,32 @@ import HomepageResources from "./HomepageResources/HomepageResources";
 import ReadyToGetStarted from "./ReadyToGetStarted/ReadyToGetStarted";
 import StayInTouch from "./StayInTouch/StayInTouch";
 import HomepageFirstRow from "./HomepageFirstRow/HomepageFirstRow";
-import GlobalConstants from "GlobalConstants";
 
 require('pages/homepage/homepageUpdate.scss');
 
 const HomepageUpdate = () => {
     const isQAT = GlobalConstants.QAT;
-    return(isQAT ?
-    <PageWrapper
-        pageName="Homepage"
-        classNames="usa-da-home-page"
-        noHeader
-        metaTagProps={{ ...homePageMetaTags }}>
-        <main id="main-content" className="main-content homepage-update-content">
-            <Hero />
-            <SummaryStats />
-            <HomepageFirstRow />
-            <AwardSearch />
-            <HomepageExploreToggle />
-            <DownloadExplorePlaceholder />
-            <HomepageResources />
-            <ReadyToGetStarted />
-            <StayInTouch />
-        </main>
-    </PageWrapper>
+    return (isQAT ?
+        <PageWrapper
+            pageName="Homepage"
+            classNames="usa-da-home-page"
+            noHeader
+            metaTagProps={{ ...homePageMetaTags }}>
+            <main id="main-content" className="main-content homepage-update-content">
+                <Hero />
+                <SummaryStats />
+                <HomepageFirstRow />
+                <AwardSearch />
+                <HomepageExploreToggle />
+                <DownloadExplorePlaceholder />
+                <HomepageResources />
+                <ReadyToGetStarted />
+                <StayInTouch />
+            </main>
+        </PageWrapper>
         :
-    <Redirect to="/404" />
-)};
+        <Redirect to="/404" />
+    );
+};
 
 export default HomepageUpdate;
