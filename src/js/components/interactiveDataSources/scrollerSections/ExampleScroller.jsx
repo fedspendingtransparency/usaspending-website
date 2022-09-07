a4b3fb51bf56995bc6f39434cf05c6b8ff78bfa6
Andy Nguyen <91632450+Nguyen-Andy2@users.noreply.github.com>
2022-09-07 11:12:54 -0400

    Add ScrollerOverlayCard and styles - Add a new card component to be used within scroller overlays. - Update the example scroller to include the new card. - Adapt styles from existing Card component.

--- src/js/components/interactiveDataSources/scrollerSections/ExampleScroller.jsx
+++ src/js/components/interactiveDataSources/scrollerSections/ExampleScroller.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Scroller from "../scroller/Scroller";
 import ScrollerOverlay from "../scroller/scrollerOverlay/ScrollerOverlay";
+import ScrollerOverlayCard from '../scroller/scrollerOverlay/ScrollerOverlayCard';
+
 
 function ExampleScroller() {
+    // Content for first overlay card
+    const cardIcon1 = (
+        <span
+            className="fa-layers fa-fw">
+            <FontAwesomeIcon icon="chart-bar" inverse size="xl" style={{ height: '20px', width: '20px' }} />
+        </span>);
+    const cardHeading1 = <p>Card 1</p>;
+    const cardContent1 = (
+        <div>
+            <p>This is a Scroller component. It consists of an overlay card and a backdrop.</p>
+            <p>Scrolling changes the content!</p>
+        </div>);
+
+    // Content for second overlay card
+    const cardHeading2 = <p>Card 2</p>;
+    const cardContent2 = (
+        <div>
+            <p>This is the second content overlay and backdrop.</p>
+            <p>The background changed when this card scrolled into view.</p>
+        </div>);
     return (
         <div>
             <Scroller>
@@ -11,37 +34,32 @@ function ExampleScroller() {
                     name="background-color-1"
                     style={{
                         background: `black`
-                    }}>
-                    <div className="example-scroller-content">
-                        <p>First backdrop</p>
-                    </div>
-                </div>
+                    }} />
                 <div
                     name="background-color-2"
                     style={{
                         background: `teal`
-                    }}>
-                    <div className="example-scroller-content">
-                        <p>Second backdrop</p>
-                    </div>
-                </div>
+                    }} />
 
                 {/* SCROLLER OVERLAYS */}
                 <ScrollerOverlay
                     content="background-color-1"
                     position="left">
-                    <p className="scroller-card">
-                        This is a Scroller component. It consists of an overlay card and
-                        a backdrop. Scrolling changes the content!
-                    </p>
+                    <div className="scroller-overlay-card-container">
+                        <ScrollerOverlayCard
+                            icon={cardIcon1}
+                            heading={cardHeading1}
+                            content={cardContent1} />
+                    </div>
                 </ScrollerOverlay>
                 <ScrollerOverlay
                     content="background-color-2"
                     position="right">
-                    <p className="scroller-card">
-                        This is the second content overlay and backdrop.
-                        The background changed when this card scrolled into view.
-                    </p>
+                    <div className="scroller-overlay-card-container">
+                        <ScrollerOverlayCard
+                            heading={cardHeading2}
+                            content={cardContent2} />
+                    </div>
                 </ScrollerOverlay>
             </Scroller>
         </div>
