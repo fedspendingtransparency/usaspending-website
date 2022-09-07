4533548a94f685de67570d8fee87468b25ec16ff
brianpetway <brian.petway@kc.frb.org>
2022-09-07 13:59:12 -0500

    9082 - added copy; css for < 768

--- src/js/components/homepageUpdate/StayInTouch/StayInTouch.jsx
+++ src/js/components/homepageUpdate/StayInTouch/StayInTouch.jsx
@@ -4,12 +4,42 @@
  */
 
 import React from 'react';
+import { FlexGridRow } from 'data-transparency-ui';
 
 const StayInTouch = () => (
     <section className="stay-in-touch__section">
-        <div>
-            STAY IN TOUCH COMPONENT
-        </div>
+        <FlexGridRow className="stay-in-touch__top-row">
+            <div className="stay-in-touch__icon-container">
+                {/* icon goes here */}
+            </div>
+            <div className="stay-in-touch__title">
+                Stay in touch
+            </div>
+        </FlexGridRow>
+        <FlexGridRow className="stay-in-touch__second-row">
+            <div className="stay-in-touch__second-row-container top">
+                <div className="stay-in-touch__second-row-title">
+                    Get Release Notes delivered to your inbox
+                </div>
+                <div className="stay-in-touch__second-row-text">
+                    Sign up to receive our release notes to keep up with what’s new on USAspending.gov.
+                </div>
+                <div className="stay-in-touch__second-row-link">
+                    Sign up
+                </div>
+            </div>
+            <div className="stay-in-touch__second-row-container">
+                <div className="stay-in-touch__second-row-title">
+                    Request a USAspending.gov training session
+                </div>
+                <div className="stay-in-touch__second-row-text">
+                    Receive customized training for your organization on how to use our tools and data.
+                </div>
+                <div className="stay-in-touch__second-row-link">
+                    Learn more
+                </div>
+            </div>
+        </FlexGridRow>
     </section>
 );
 
