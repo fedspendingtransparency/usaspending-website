6908032f2dd8a1d59add1c44b1c42d54699687b9
brianpetway <brian.petway@kc.frb.org>
2022-09-13 12:08:02 -0500

    9081 - wip; content for cards

--- src/js/components/homepageUpdate/ReadyToGetStarted/ReadyToGetStarted.jsx
+++ src/js/components/homepageUpdate/ReadyToGetStarted/ReadyToGetStarted.jsx
@@ -5,6 +5,45 @@
 
 import React from 'react';
 import { FlexGridRow, FlexGridCol } from 'data-transparency-ui';
+import CardContainer from "../../sharedComponents/commonCards/CardContainer";
+import CardHero from "../../sharedComponents/commonCards/CardHero";
+import CardBody from "../../sharedComponents/commonCards/CardBody";
+import CardButton from "../../sharedComponents/commonCards/CardButton";
+
+const cardObjects = [
+    {
+        fillColor: '#00bde3',
+        image: 'img/homepage-ready-to-get-started/homepage-get-started-award-search.webp',
+        overline: 'AWARD SEARCH',
+        headline: 'Find details on federal awards',
+        buttonText: 'Go to Award Search',
+        buttonLink: ''
+    },
+    {
+        fillColor: '#34a37e',
+        image: 'img/homepage-ready-to-get-started/homepage-get-started-spending-explorer.webp',
+        overline: 'SPENDING EXPLORER',
+        headline: 'Break down the federal budget',
+        buttonText: 'Dive into Spending Explorer',
+        buttonLink: ''
+    },
+    {
+        fillColor: '#fa9441',
+        image: 'img/homepage-ready-to-get-started/homepage-get-started-recipient-profiles.webp',
+        overline: 'SPENDING PROFILES',
+        headline: 'Find recipients of federal funds',
+        buttonText: 'Browse Recipient Profiles',
+        buttonLink: ''
+    },
+    {
+        fillColor: '#9355dc',
+        image: 'img/homepage-ready-to-get-started/homepage-get-started-learn-more.webp',
+        overline: 'RESOURCES',
+        headline: 'Learn more about USAspending.gov',
+        buttonText: 'Find out what we do',
+        buttonLink: ''
+    }
+];
 
 const ReadyToGetStarted = () => (
     <section className="ready-to-get-started__section">
@@ -13,18 +52,31 @@ const ReadyToGetStarted = () => (
                 Ready to get started?
             </FlexGridCol>
             <FlexGridRow hasGutter gutterSize="lg">
-                <FlexGridCol mobile={12} tablet={6} desktop={3}>
-                    CARD
-                </FlexGridCol>
-                <FlexGridCol mobile={12} tablet={6} desktop={3}>
-                    CARD
-                </FlexGridCol>
-                <FlexGridCol mobile={12} tablet={6} desktop={3}>
-                    CARD
-                </FlexGridCol>
-                <FlexGridCol mobile={12} tablet={6} desktop={3}>
-                    CARD
-                </FlexGridCol>
+                {cardObjects.map((card) => (
+                    <FlexGridCol
+                        className="ready-to-get-started__card"
+                        mobile={12}
+                        tablet={6}
+                        desktop={3}>
+                        <CardContainer
+                            variant="outline"
+                            size="md">
+                            <CardHero
+                                size="md"
+                                variant="inset"
+                                fill={card.fillColor}
+                                img={card.image} />
+                            <CardBody
+                                overline={card.overline}
+                                headline={card.headline}>
+                                <CardButton
+                                    text={card.buttonText}
+                                    variant="text"
+                                    link={card.buttonLink} />
+                            </CardBody>
+                        </CardContainer>
+                    </FlexGridCol>
+                ))}
             </FlexGridRow>
         </FlexGridRow>
     </section>
