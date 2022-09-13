ee1cefed8954ce07a60f7c9181c0acc991aafb8a
Valeria Green <green_valeria@bah.com>
2022-09-13 18:35:36 -0400

    Brush up Styling and Responsiveness * Converted two class components into functional components due to not needing any state to function * Got rid of some styles from the aboutSection that were harming the responsiveness like height and padding-bottom from the bottom part of the about section * Added some media queries to the container and animation in the intro section for more margins and auto for height for the overall container * Added bullet points styling and padding for some content in the accordions * Added some more margin for the body container when screen is small enough

--- src/js/components/interactiveDataSources/sections/AboutSection.jsx
+++ src/js/components/interactiveDataSources/sections/AboutSection.jsx
@@ -5,34 +5,34 @@ import React from 'react';
 import Accordion from "../../sharedComponents/accordion/Accordion";
 import ScrollerOverlayCard from '../scroller/scrollerOverlay/ScrollerOverlayCard';
 
-export default class AboutSection extends React.Component {
-    render() {
-        const aboutDetails = [{
-            title: "What",
-            details: (<>
-                <p>
-                The Digital Accountability and Transparency Act (DATA Act) was signed into law in 2014, establishing governmentwide standards for financial data and a new reporting process for agencies to improve data quality, transparency, and accountability.
-                </p>
-                <p>
-                The result is an ongoing collaboration between the federal community and external stakeholders to create a standard data model and a publicly accessible and searchable website: USAspending.gov.
-                </p>
-                <p>
-                Today, more than 100 federal agencies submit financial data on a monthly basis, and thousands of public users visit USAspending every day to access the data via visualizations, downloads, and open APIs.
-                </p>
-            </>)
-        },
-        {
-            title: "Why",
-            details: (<>
-                <p>
-                Before the DATA Act, many programs in the federal government had the same types of spending data about agency expenses and federal awards such as contracts, grants, and loans. However, they weren't all defining their data elements in the same way. These differences made it hard to share or compare data across agencies and programs.
-                </p>
-            </>)
-        },
-        {
-            title: "How",
-            details: (<>
-                <ul>
+const AboutSection = () => {
+    const aboutDetails = [{
+        title: "What",
+        details: (<>
+            <p>
+            The Digital Accountability and Transparency Act (DATA Act) was signed into law in 2014, establishing governmentwide standards for financial data and a new reporting process for agencies to improve data quality, transparency, and accountability.
+            </p>
+            <p>
+            The result is an ongoing collaboration between the federal community and external stakeholders to create a standard data model and a publicly accessible and searchable website: USAspending.gov.
+            </p>
+            <p>
+            Today, more than 100 federal agencies submit financial data on a monthly basis, and thousands of public users visit USAspending every day to access the data via visualizations, downloads, and open APIs.
+            </p>
+        </>)
+    },
+    {
+        title: "Why",
+        details: (<>
+            <p>
+            Before the DATA Act, many programs in the federal government had the same types of spending data about agency expenses and federal awards such as contracts, grants, and loans. However, they weren't all defining their data elements in the same way. These differences made it hard to share or compare data across agencies and programs.
+            </p>
+        </>)
+    },
+    {
+        title: "How",
+        details: (
+            <>
+                <ul className="interactives-guide_bullet-points">
                     <li>
                     The USAspending data model standardizes data elements and definitions, and the USAspending Broker System is used to validate the data submitted and extracted from agency systems for quality, consistency, and accuracy.
                     </li>
@@ -40,66 +40,69 @@ export default class AboutSection extends React.Component {
                     USAspending is built on the principles of user-centered design and Agile software development, leveraging input from the public and federal stakeholders to iteratively release updates and enhancements many times per year.
                     </li>
                 </ul>
-            </>)
-        }];
-        const dataModelCardContent = {
-            heading: (
-                <>
-                    <p>USAspending Data Model</p>
-                    <div className="interactives-guide__cardLine" />
-                </>
-            ),
-            content: (
-                <>
-                    <p className="interactives-guide-cardText">
-                        To read the technical documentation for the data elements and source systems that flow into USAspending, <a href="https://fiscal.treasury.gov/data-transparency/DAIMS-current.html" target="_blank">please visit this resources page from the Treasury Department’s Bureau of the Fiscal Service.</a>
-                    </p>
-                    <div
-                        className="usa-button usa-button-outline read-more-button"
-                        aria-label="Read More Button"
-                        title="Read More Button">
-                            Read More
-                    </div>
-                </>
-            )
-        };
-
-        return (
-            <div className="body__content interactive-data-sources-intro-section">
-                <div className="body-padded__content">
-                    <h3 className="interactiveDataHeader__topTitle">Data Act and Creation of DAIMS/USAspending</h3>
-                    <h4 className="interactives-guide__questionSections">
-                        History
-                    </h4>
-                    <div className="interactives-guide__questionLine" />
-                    {aboutDetails.map((item, i) => (
-                        <Accordion
-                            key={`item_${i}`}
-                            title={item.title}>
-                            {item.details}
-                        </Accordion>
-                    ))}
+            </>
+        )
+    }];
+    const dataModelCardContent = {
+        heading: (
+            <>
+                <p>USAspending Data Model</p>
+                <div className="interactives-guide__cardLine" />
+            </>
+        ),
+        content: (
+            <>
+                <p className="interactives-guide-cardText">
+                    To read the technical documentation for the data elements and source systems that flow into USAspending, <a href="https://fiscal.treasury.gov/data-transparency/DAIMS-current.html" target="_blank">please visit this resources page from the Treasury Department’s Bureau of the Fiscal Service.</a>
+                </p>
+                <div
+                    className="usa-button usa-button-outline read-more-button"
+                    aria-label="Read More Button"
+                    title="Read More Button">
+                        Read More
                 </div>
-                <div className="interactives-guide__background-green">
-                    <div className="body-padded__content">
-                        <ScrollerOverlayCard
-                            heading={dataModelCardContent.heading}
-                            content={dataModelCardContent.content} />
-                    </div>
-                    <br />
-                    <div className="interactives-guide_begin-scroller">
-                        <h2>You can follow the money from the source systems through the submission process to see what is displayed on USAspending.gov.</h2>
-                        <h2>Scroll down to get started.</h2>
+            </>
+        )
+    };
 
-                        <div className="interactives-guide_scroll-indicator">
-                            <h5>Scroll down to get started.</h5>
-                            <div className="interactives-arrow-holder">
-                                <FontAwesomeIcon icon={faArrowDown} className="interactives-down-icon" />
-                            </div>
+    return (
+        <div className="body__content interactive-data-sources-intro-section">
+            <div className="body-padded__content">
+                <h3 className="interactiveDataHeader__topTitle">Data Act and Creation of DAIMS/USAspending</h3>
+                <h4 className="interactives-guide__questionSections">
+                    History
+                </h4>
+                <div className="interactives-guide__questionLine" />
+                {aboutDetails.map((item, i) => (
+                    <Accordion
+                        key={`item_${i}`}
+                        title={item.title}>
+                        {item.details}
+                    </Accordion>
+                ))}
+            </div>
+            <div className="interactives-guide__background-green">
+                <div className="body-padded__content">
+                    <ScrollerOverlayCard
+                        heading={dataModelCardContent.heading}
+                        content={dataModelCardContent.content} />
+                </div>
+                <br />
+                <div className="interactives-guide_begin-scroller">
+                    <h2>You can follow the money from the source systems through the submission process to see what is displayed on USAspending.gov.</h2>
+                    <h2>Scroll down to get started.</h2>
+
+                    <div className="interactives-guide_scroll-indicator">
+                        <h5>Scroll down to get started.</h5>
+                        <div className="interactives-arrow-holder">
+                            <FontAwesomeIcon icon={faArrowDown} className="interactives-down-icon" />
                         </div>
                     </div>
                 </div>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
+
+
+export default AboutSection;
--- src/js/components/interactiveDataSources/sections/IntroSection.jsx
+++ src/js/components/interactiveDataSources/sections/IntroSection.jsx
@@ -2,34 +2,32 @@
 import React from 'react';
 import LottieAnimation from '../lottieAnimation/LottieAnimation';
 
-export default class IntroSection extends React.Component {
-    render() {
-        return (
-            <div className="body__content intro-padded__content interactive-data-sources-intro-section">
-                <div className="hero-container">
-                    <div className="hero__left-item">
-                        <div className="hero__left-item__content">
-                            <h1>
-                    USAspending Data Sources
-                            </h1>
-                            <div>
-                                <h2>
-                                    A journey though government spending data
-                                </h2>
-                                <p>
-                                    USAspending links data from a variety of government systems, including agency financial systems and award systems. Scroll below to learn more about these systems, as well as the context for this historic initiative to provide federal spending transparency.
-                                </p>
-                            </div>
-                        </div>
+const IntroSection = () => (
+    <div className="body__content intro-padded__content interactive-data-sources-intro-section">
+        <div className="hero-container">
+            <div className="hero__left-item">
+                <div className="hero__left-item__content">
+                    <h1>
+                        USAspending Data Sources
+                    </h1>
+                    <div>
+                        <h2>
+                                A journey though government spending data
+                        </h2>
+                        <p>
+                            USAspending links data from a variety of government systems, including agency financial systems and award systems. Scroll below to learn more about these systems, as well as the context for this historic initiative to provide federal spending transparency.
+                        </p>
                     </div>
                 </div>
-                <div className="interactive-data-sources-intro-animation">
-                    <LottieAnimation
-                        autoplay
-                        loop
-                        src="/img/interactive-data-sources/intro-animation.json" />
-                </div>
             </div>
-        );
-    }
-}
+        </div>
+        <div className="interactive-data-sources-intro-animation">
+            <LottieAnimation
+                autoplay
+                loop
+                src="/img/interactive-data-sources/intro-animation.json" />
+        </div>
+    </div>
+);
+
+export default IntroSection;
