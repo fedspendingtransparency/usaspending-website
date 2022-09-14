961c659281ecaa40c292aab6fd9e3102f5869bea
brianpetway <brian.petway@kc.frb.org>
2022-09-14 16:09:06 -0500

    9080 - styles for top half of section

--- src/js/components/homepageUpdate/HomepageResources/HomepageResources.jsx
+++ src/js/components/homepageUpdate/HomepageResources/HomepageResources.jsx
@@ -5,13 +5,22 @@
 
 import React from 'react';
 import { FlexGridRow, FlexGridCol } from 'data-transparency-ui';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const HomepageResources = () => (
     <section className="homepage-resources__section">
         <FlexGridRow className="grid-content">
             <FlexGridCol width={12}>
-                <FlexGridRow>RESOURCES</FlexGridRow>
-                <FlexGridRow>Find answers to your data questions</FlexGridRow>
+                <FlexGridRow className="homepage-resources__top-label-container">
+                    <div className="homepage-resources__top-label-icon-container">
+                        <FontAwesomeIcon
+                            className="homepage-resources__book-icon"
+                            icon="book-open"
+                            size="xs" />
+                    </div>
+                    <div className="homepage-resources__top-label-text">RESOURCES</div>
+                </FlexGridRow>
+                <FlexGridRow className="homepage-resources__headline">Find answers to your data questions</FlexGridRow>
             </FlexGridCol>
             <FlexGridRow hasGutter gutterSize="lg">
                 <FlexGridCol>CARD</FlexGridCol>
