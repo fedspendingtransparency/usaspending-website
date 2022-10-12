e704d8ac0aef016271dc4eee3a3dc41e7266ba94
anjenkin <blackwell_andrea@bah.com>
2022-10-11 22:43:15 -0400

    fixed overflow and resize image by media query

--- src/js/components/homepage/hero/Hero.jsx
+++ src/js/components/homepage/hero/Hero.jsx
@@ -32,101 +32,85 @@ const Hero = () => {
     };
 
     return (
-        <div style={{ backgroundColor: '#d2e3e5' }}>
-            <div style={{ margin: '0 -8.33%' }}>
+        <div className="homepage-hero-background">
+            <div className="homepage-hero-margin">
                 <section className="homepage-hero-container" aria-label="Hero sections">
                     <div className="hero__graphic-layer-background">
                         <img
                             role="presentation"
-                            src="../../../../img/homepage-hero/homepage-hero-layer-background@2x.png"
+                            src="../../../../img/homepage-hero/hero-graphic-background-mountains.png"
                             alt="Hills" />
                     </div>
                     <div className="hero__graphic-layer-bridge">
                         <img
                             role="presentation"
-                            src="../../../../img/homepage-hero/homepage-hero-bridge@2x.png"
+                            src="../../../../img/homepage-hero/hero-graphic-background-bridge.png"
                             alt="Bridge" />
                     </div>
                     <div className="hero__graphic-layer-buildings">
                         <img
                             role="presentation"
-                            src="../../../../img/homepage-hero/homepage-hero-buildings@2x.png"
+                            src="../../../../img/homepage-hero/hero-graphic-background-left-hill.png"
                             alt="Buildings" />
                     </div>
                     <div className="hero__graphic-layer-windmills">
                         <img
                             role="presentation"
-                            src="../../../../img/homepage-hero/homepage-hero-trees-windmills@2x.png"
+                            src="../../../../img/homepage-hero/hero-graphic-background-right-hill.png"
                             alt="Windmills" />
                     </div>
                     <div className="hero__graphic-layer-foreground">
                         <img
                             role="presentation"
-                            src="../../../../img/homepage-hero/homepage-hero-layer-foreground@2x.png"
+                            src="../../../../img/homepage-hero/hero-graphic-foreground.png"
                             alt="Road" />
                     </div>
-                    <div className="homepage-hero-content">
-                        <AnimatedHeading paused={isPaused} />
-                        <div className="hero__lower-wrapper">
-                            {/* <div className="hero__left-image-wrapper">
-                                <picture>
-                                    <img
-                                        role="presentation"
-                                        src="../../../../img/homepage-hero-graphic-left.svg"
-                                        alt="" />
-                                </picture>
-                            </div> */}
-                            <div className="hero__center-content-wrapper">
-                                <div className="hero__center-content">
-                                    <div className="hero__button-container">
-                                        <Link
-                                            className="hero__button hero__button--action"
-                                            to="/search"
-                                            onClick={trackSearchLink}>
-                                            Start Searching Awards
-                                        </Link>
-                                        <Link
-                                            className="hero__button"
-                                            to="/about"
-                                            onClick={trackAboutLink}>
-                                            Learn about USAspending
-                                        </Link>
-                                    </div>
-                                    <div className="hero__text-container">
-                                        <p>
-                                            USAspending is the official open data source of federal spending information.
-                                        </p>
-                                    </div>
-                                    <div className="hero__pause-button-container">
-                                        <a
-                                            className="hero__pause-button"
-                                            role="button"
-                                            tabIndex="0"
-                                            onClick={() => {
-                                                setIsPaused((previousIsPaused) => !previousIsPaused);
-                                            }}
-                                            onKeyPress={(e) => {
-                                                keyPressHandler(e);
-                                            }}>
-                                            { isPaused ?
-                                                <><FontAwesomeIcon icon="play" width={10} />&nbsp;&nbsp;Play text animation</>
-                                                :
-                                                <><FontAwesomeIcon icon="pause" width={10} />&nbsp;&nbsp;Pause text animation</>
-                                            }
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                            {/* <div className="hero__right-image-wrapper">
-                                <picture>
-                                    <img
-                                        role="presentation"
-                                        src="../../../../img/homepage-hero-graphic-right.svg"
-                                        alt="" />
-                                </picture>
-                            </div> */}
-                        </div>
-                    </div>
+                    {/*<div className="homepage-hero-content">*/}
+                    {/*    <AnimatedHeading paused={isPaused} />*/}
+                    {/*    <div className="hero__lower-wrapper">*/}
+                    {/*        <div className="hero__center-content-wrapper">*/}
+                    {/*            <div className="hero__center-content">*/}
+                    {/*                <div className="hero__button-container">*/}
+                    {/*                    <Link*/}
+                    {/*                        className="hero__button hero__button--action"*/}
+                    {/*                        to="/search"*/}
+                    {/*                        onClick={trackSearchLink}>*/}
+                    {/*                        Start Searching Awards*/}
+                    {/*                    </Link>*/}
+                    {/*                    <Link*/}
+                    {/*                        className="hero__button"*/}
+                    {/*                        to="/about"*/}
+                    {/*                        onClick={trackAboutLink}>*/}
+                    {/*                        Learn about USAspending*/}
+                    {/*                    </Link>*/}
+                    {/*                </div>*/}
+                    {/*                <div className="hero__text-container">*/}
+                    {/*                    <p>*/}
+                    {/*                        USAspending is the official open data source of federal spending information.*/}
+                    {/*                    </p>*/}
+                    {/*                </div>*/}
+                    {/*                <div className="hero__pause-button-container">*/}
+                    {/*                    <a*/}
+                    {/*                        className="hero__pause-button"*/}
+                    {/*                        role="button"*/}
+                    {/*                        tabIndex="0"*/}
+                    {/*                        onClick={() => {*/}
+                    {/*                            setIsPaused((previousIsPaused) => !previousIsPaused);*/}
+                    {/*                        }}*/}
+                    {/*                        onKeyPress={(e) => {*/}
+                    {/*                            keyPressHandler(e);*/}
+                    {/*                        }}>*/}
+                    {/*                        { isPaused ?*/}
+                    {/*                            <><FontAwesomeIcon icon="play" width={10} />&nbsp;&nbsp;Play text animation</>*/}
+                    {/*                            :*/}
+                    {/*                            <><FontAwesomeIcon icon="pause" width={10} />&nbsp;&nbsp;Pause text animation</>*/}
+                    {/*                        }*/}
+                    {/*                    </a>*/}
+                    {/*                </div>*/}
+                    {/*            </div>*/}
+                    {/*        </div>*/}
+                    {/*    </div>*/}
+                    {/*</div>*/}
                 </section>
             </div>
         </div>
