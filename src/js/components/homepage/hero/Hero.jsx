72164be5c154514a19b2f400f43db9f5e83fb9b2
anjenkin <blackwell_andrea@bah.com>
2022-10-11 23:18:35 -0400

    fixed the text alignment

--- src/js/components/homepage/hero/Hero.jsx
+++ src/js/components/homepage/hero/Hero.jsx
@@ -33,6 +33,53 @@ const Hero = () => {
 
     return (
         <div className="homepage-hero-background">
+            <div className="homepage-hero-content">
+                <AnimatedHeading paused={isPaused} />
+                <div className="hero__lower-wrapper">
+                    <div className="hero__center-content-wrapper">
+                        <div className="hero__center-content">
+                            <div className="hero__button-container">
+                                <Link
+                                    className="hero__button hero__button--action"
+                                    to="/search"
+                                    onClick={trackSearchLink}>
+                                    Start Searching Awards
+                                </Link>
+                                <Link
+                                    className="hero__button"
+                                    to="/about"
+                                    onClick={trackAboutLink}>
+                                    Learn about USAspending
+                                </Link>
+                            </div>
+                            <div className="hero__text-container">
+                                <p>
+                                    USAspending is the official open data source of federal spending information.
+                                </p>
+                            </div>
+                            <div className="hero__pause-button-container">
+                                <a
+                                    className="hero__pause-button"
+                                    role="button"
+                                    tabIndex="0"
+                                    onClick={() => {
+                                        setIsPaused((previousIsPaused) => !previousIsPaused);
+                                    }}
+                                    onKeyPress={(e) => {
+                                        keyPressHandler(e);
+                                    }}>
+                                    { isPaused ?
+                                        <><FontAwesomeIcon icon="play" width={10} />&nbsp;&nbsp;Play text animation</>
+                                        :
+                                        <><FontAwesomeIcon icon="pause" width={10} />&nbsp;&nbsp;Pause text animation</>
+                                    }
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
             <div className="homepage-hero-margin">
                 <section className="homepage-hero-container" aria-label="Hero sections">
                     <div className="hero__graphic-layer-background">
@@ -65,52 +112,6 @@ const Hero = () => {
                             src="../../../../img/homepage-hero/hero-graphic-foreground.png"
                             alt="Road" />
                     </div>
-                    {/*<div className="homepage-hero-content">*/}
-                    {/*    <AnimatedHeading paused={isPaused} />*/}
-                    {/*    <div className="hero__lower-wrapper">*/}
-                    {/*        <div className="hero__center-content-wrapper">*/}
-                    {/*            <div className="hero__center-content">*/}
-                    {/*                <div className="hero__button-container">*/}
-                    {/*                    <Link*/}
-                    {/*                        className="hero__button hero__button--action"*/}
-                    {/*                        to="/search"*/}
-                    {/*                        onClick={trackSearchLink}>*/}
-                    {/*                        Start Searching Awards*/}
-                    {/*                    </Link>*/}
-                    {/*                    <Link*/}
-                    {/*                        className="hero__button"*/}
-                    {/*                        to="/about"*/}
-                    {/*                        onClick={trackAboutLink}>*/}
-                    {/*                        Learn about USAspending*/}
-                    {/*                    </Link>*/}
-                    {/*                </div>*/}
-                    {/*                <div className="hero__text-container">*/}
-                    {/*                    <p>*/}
-                    {/*                        USAspending is the official open data source of federal spending information.*/}
-                    {/*                    </p>*/}
-                    {/*                </div>*/}
-                    {/*                <div className="hero__pause-button-container">*/}
-                    {/*                    <a*/}
-                    {/*                        className="hero__pause-button"*/}
-                    {/*                        role="button"*/}
-                    {/*                        tabIndex="0"*/}
-                    {/*                        onClick={() => {*/}
-                    {/*                            setIsPaused((previousIsPaused) => !previousIsPaused);*/}
-                    {/*                        }}*/}
-                    {/*                        onKeyPress={(e) => {*/}
-                    {/*                            keyPressHandler(e);*/}
-                    {/*                        }}>*/}
-                    {/*                        { isPaused ?*/}
-                    {/*                            <><FontAwesomeIcon icon="play" width={10} />&nbsp;&nbsp;Play text animation</>*/}
-                    {/*                            :*/}
-                    {/*                            <><FontAwesomeIcon icon="pause" width={10} />&nbsp;&nbsp;Pause text animation</>*/}
-                    {/*                        }*/}
-                    {/*                    </a>*/}
-                    {/*                </div>*/}
-                    {/*            </div>*/}
-                    {/*        </div>*/}
-                    {/*    </div>*/}
-                    {/*</div>*/}
                 </section>
             </div>
         </div>
