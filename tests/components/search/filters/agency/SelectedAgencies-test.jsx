274874fdd565165e28ddc288bec7ac58ce454806
Nick Torres <nicholas.torres@kc.frb.org>
2022-10-14 13:13:43 -0600

    SelectedAgencies enzyme to rtl

--- tests/components/search/filters/agency/SelectedAgencies-test.jsx
+++ tests/components/search/filters/agency/SelectedAgencies-test.jsx
@@ -1,7 +1,5 @@
-
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { mount } from 'enzyme';
 import { OrderedMap } from 'immutable';
 import SelectedAgencies from '../../../../../src/js/components/search/filters/agency/SelectedAgencies';
 
