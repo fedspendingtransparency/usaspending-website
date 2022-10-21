4682f50d77c757df82de83d046e7006eaf6c11e9
chasls2 <110061741+chasls2@users.noreply.github.com>
2022-10-21 09:02:44 -0500

    fixed more linting issues
    fixed linting issue

--- src/js/components/interactiveDataSources/InteractiveDataSourcesPage.jsx
+++ src/js/components/interactiveDataSources/InteractiveDataSourcesPage.jsx
@@ -37,98 +37,98 @@ const InteractiveDataSourcesPage = () => {
             display: 'Introduction',
             showSectionTitle: false,
             scroller: false,
-            component: <IntroSection/>
+            component: <IntroSection />
         },
         {
             name: 'history-section',
             display: 'History of the DATA Act',
             showSectionTitle: false,
             scroller: false,
-            component: <AboutSection/>
+            component: <AboutSection />
         },
         {
             name: 'federal-spending-overview',
             display: 'Federal Spending Overview',
             showSectionTitle: false,
             scroller: true,
-            component: <FederalSpendingOverview/>
+            component: <FederalSpendingOverview />
         },
         {
             name: 'data-available',
             display: 'Data Available on USAspending',
             showSectionTitle: false,
             scroller: true,
-            component: <DataAvailable/>
+            component: <DataAvailable />
         },
         {
             name: 'data-types',
             display: 'USAspending Data Types',
             showSectionTitle: false,
             scroller: true,
-            component: <DataTypes/>
+            component: <DataTypes />
         },
         {
             name: 'data-source-systems',
             display: 'USAspending Source Systems',
             showSectionTitle: false,
             scroller: true,
-            component: <DataSourceSystems/>
+            component: <DataSourceSystems />
         },
         {
             name: 'account-data',
             display: 'USAspending Account Data',
             showSectionTitle: false,
             scroller: true,
-            component: <AccountData/>
+            component: <AccountData />
         },
         {
             name: 'award-data',
             display: 'USAspending Award Data',
             showSectionTitle: false,
             scroller: true,
-            component: <AwardData/>
+            component: <AwardData />
         },
         {
             name: 'additional-data',
             display: 'USAspending Additional Data',
             showSectionTitle: false,
             scroller: true,
-            component: <AdditionalData/>
+            component: <AdditionalData />
         },
         {
             name: 'data-submission-extraction',
             display: 'Data Submission and Extraction',
             showSectionTitle: false,
             scroller: true,
-            component: <DataSubmissionExtraction/>
+            component: <DataSubmissionExtraction />
         },
         {
             name: 'frequency',
             display: 'Frequency of Data Updates',
             showSectionTitle: false,
             scroller: true,
-            component: <Frequency/>
+            component: <Frequency />
         },
         {
             name: 'data-validation',
             display: 'Data Validation',
             showSectionTitle: false,
             scroller: true,
-            component: <DataValidation/>
+            component: <DataValidation />
         },
         {
             name: 'data-access',
             display: 'Features on USAspending',
             showSectionTitle: false,
             scroller: true,
-            component: <DataFeatures/>
+            component: <DataFeatures />
         },
         {
             name: 'data-use-cases',
             display: 'USAspending Use Cases',
             showSectionTitle: false,
             scroller: true,
-            component: <DataUseCases/>
+            component: <DataUseCases />
         }
     ];
     const jumpToSection = (section = '') => {
@@ -189,7 +189,7 @@ const InteractiveDataSourcesPage = () => {
                             sections={sections.map((section) => ({
                                 section: section.name,
                                 label: section.display
-                            }))}/>
+                            }))} />
                     </div>
                 </div>
                 <div className="body usda__flex-col">
@@ -198,7 +198,7 @@ const InteractiveDataSourcesPage = () => {
                             key={section.name}
                             section={section}
                             icon={section.icon}>
-                            {section.component || <ComingSoon/>}
+                            {section.component || <ComingSoon />}
                         </InteractiveDataSourcesSection>
                     ))}
                 </div>
