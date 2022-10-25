5c60829f90debb4fe5972451e35033168d15f22f
chasls2 <110061741+chasls2@users.noreply.github.com>
2022-10-25 14:31:27 -0500

    Updated Word of the Day Error card
    adjusting the a tag to look like a button

--- src/js/components/homepageUpdate/WordOfTheDay/ErrorWordOfTheDay.jsx
+++ src/js/components/homepageUpdate/WordOfTheDay/ErrorWordOfTheDay.jsx
@@ -7,13 +7,12 @@ import React from 'react';
 
 const ErrorWordOfTheDay = () => (
     <div className="word-of-the-day__error-message">
-        <picture>
-            <img
-                role="presentation"
-                src="../../../../img/homepage-word-of-the-day/Error-Placeholder.svg"
-                alt="" />
-        </picture>
-        Unable to load Word of the Day
+        <p className="word-of-the-day-paragraph-one">Something went wrong</p>
+        <p className="word-of-the-day-paragraph-two">Sorry, we're unable to load this content</p>
+        <div className="word-of-the-day-button">
+            <a href="mailto:usaspending.help@fiscal.treasury.gov?subject=Word%20of%20the%20Day$20Error">Report this error</a>
+        </div>
+
     </div>
 );
 
--- src/js/components/homepageUpdate/WordOfTheDay/WordOfTheDay.jsx
+++ src/js/components/homepageUpdate/WordOfTheDay/WordOfTheDay.jsx
@@ -172,7 +172,7 @@ const WordOfTheDay = () => {
                 selectWordOfTheDay();
                 setGlossary(res.data.results);
                 setLoading(false);
-                setError(false);
+                setError(true);
             })
             .catch((err) => {
                 if (!isCancel(err)) {
@@ -207,7 +207,7 @@ const WordOfTheDay = () => {
                         </CardBody>
                     </FlexGridCol>
                     :
-                    <CardBody customClassName="word-of-the-day__body">
+                    <CardBody customClassName="card__body_error">
                         {loading ? <LoadingWrapper isLoading={loading} /> : <ErrorWordOfTheDay />}
                     </CardBody>
                 }
