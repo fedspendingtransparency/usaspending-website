0d8cebe165fa2d87e2be2233bb06bdc2d87a3da9
chasls2 <110061741+chasls2@users.noreply.github.com>
2022-10-25 14:51:31 -0500

    Updated Button to send mailTO

--- src/js/components/homepageUpdate/WordOfTheDay/ErrorWordOfTheDay.jsx
+++ src/js/components/homepageUpdate/WordOfTheDay/ErrorWordOfTheDay.jsx
@@ -9,10 +9,7 @@ const ErrorWordOfTheDay = () => (
     <div className="word-of-the-day__error-message">
         <p className="word-of-the-day-paragraph-one">Something went wrong</p>
         <p className="word-of-the-day-paragraph-two">Sorry, we're unable to load this content</p>
-        <div className="word-of-the-day-button">
-            <a href="mailto:usaspending.help@fiscal.treasury.gov?subject=Word%20of%20the%20Day$20Error">Report this error</a>
-        </div>
-
+        <button className="word-of-the-day-button" onClick={() => window.location = 'mailto:usaspending.help@fiscal.treasury.gov?subject=Word%20of%20the%20Day$20Error'}>Report this error</button>
     </div>
 );
 
