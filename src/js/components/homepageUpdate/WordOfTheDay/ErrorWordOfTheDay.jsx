46683ede5eab43f7accc5081e5f802f7325fe71b
chasls2 <110061741+chasls2@users.noreply.github.com>
2022-10-25 15:20:34 -0500

    Updated the Card to no longer show error

--- src/js/components/homepageUpdate/WordOfTheDay/ErrorWordOfTheDay.jsx
+++ src/js/components/homepageUpdate/WordOfTheDay/ErrorWordOfTheDay.jsx
@@ -9,6 +9,7 @@ const ErrorWordOfTheDay = () => (
     <div className="word-of-the-day__error-message">
         <p className="word-of-the-day-paragraph-one">Something went wrong</p>
         <p className="word-of-the-day-paragraph-two">Sorry, we're unable to load this content</p>
+        {/* eslint-disable-next-line no-return-assign */}
         <button className="word-of-the-day-button" onClick={() => window.location = 'mailto:usaspending.help@fiscal.treasury.gov?subject=Word%20of%20the%20Day$20Error'}>Report this error</button>
     </div>
 );
