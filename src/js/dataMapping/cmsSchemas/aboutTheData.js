069512b9024571b6e42c61d172a5cd78662afa1d
anjenkin <blackwell_andrea@bah.com>
2022-10-27 14:38:20 -0400

    wip

---
+++ src/js/dataMapping/cmsSchemas/aboutTheData.js
@@ -0,0 +1,37 @@
+export const schema = {
+    collections: [
+        {
+            label: "Sitewide Data Sources",
+            path: 'content/data-sources/sitewide',
+            fields: [
+                {
+                    title: "File A (Account Balances)",
+                    slug: "file-a",
+                    isGlossaryTerm: false
+                }
+            ]
+        },
+        {
+            label: "Sitewide Data Disclosures",
+            path: 'content/data-sources/disclosures',
+            fields: [
+                {
+                    title: "File A (Account Balances)",
+                    slug: "file-a",
+                    isGlossaryTerm: false
+                }
+            ]
+        },
+        {
+            label: "Data Sources on this Page",
+            path: 'content/data-sources/page',
+            fields: [
+                {
+                    title: "File A (Account Balances)",
+                    slug: "file-a",
+                    isGlossaryTerm: false
+                }
+            ]
+        }
+    ]
+};
