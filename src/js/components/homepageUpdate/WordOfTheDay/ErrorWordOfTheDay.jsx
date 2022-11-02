04c03846decb30b055f52f5add2f33cf10e4545c
brianpetway <brian.petway@kc.frb.org>
2022-11-02 10:24:39 -0500

    9200 - added period to error msg for word of the day

--- src/js/components/homepageUpdate/WordOfTheDay/ErrorWordOfTheDay.jsx
+++ src/js/components/homepageUpdate/WordOfTheDay/ErrorWordOfTheDay.jsx
@@ -8,7 +8,7 @@ import React from 'react';
 const ErrorWordOfTheDay = () => (
     <div className="word-of-the-day__error-message">
         <p className="word-of-the-day-paragraph-one">Something went wrong</p>
-        <p className="word-of-the-day-paragraph-two">Sorry, we're unable to load this content</p>
+        <p className="word-of-the-day-paragraph-two">Sorry, we're unable to load this content.</p>
         {/* eslint-disable-next-line no-return-assign */}
         <button className="word-of-the-day-button" onClick={() => window.location = 'mailto:usaspending.help@fiscal.treasury.gov?subject=Word%20of%20the%20Day%20Error'}>Report this error</button>
     </div>
