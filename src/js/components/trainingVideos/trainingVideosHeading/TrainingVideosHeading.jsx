380829c9aa7bcc732837afc8a063e1d85d1de8f5
chasls2 <110061741+chasls2@users.noreply.github.com>
2022-12-06 12:05:52 -0600

    Final Update on DEV-9308

--- src/js/components/trainingVideos/trainingVideosHeading/TrainingVideosHeading.jsx
+++ src/js/components/trainingVideos/trainingVideosHeading/TrainingVideosHeading.jsx
@@ -4,12 +4,27 @@
  */
 
 import React from 'react';
+import { ShareIcon } from "data-transparency-ui";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShare, faShareAlt } from '@fortawesome/free-solid-svg-icons';
 
 const TrainingVideosHeading = () => (
-    <section className="training-videos-heading__section">
-        <div className="grid-content">
-            TRAINING VIDEOS HEADING
+    <section className="youtube-main-container">
+        <div className="youtube-content-container">
+            <div className="youtube-img-container">
+                <img className="youtube-img" src="img/youtube-page/youtube-landing-page-hero-image@2x.webp" alt="placeholder" />
+            </div>
         </div>
+        <div className="youtube-info-container" >
+            <div className="youtube-info-wrap">
+                <p className="youtube-resources-title">Resources</p>
+                <p className="youtube-header">Training Videos</p>
+                <p className="youtube-body">Learn how to use USAspending.gov and understand the data. Subscribe to our YouTube channel for the latest videos!</p>
+                <FontAwesomeIcon className="youtube-share-icon" icon={faShareAlt} />
+            </div>
+        </div>
+
+
     </section>
 );
 
