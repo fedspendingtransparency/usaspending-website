36a92c2c9f419f18ed0ffa75d2c840390f51cbab
chasls2 <110061741+chasls2@users.noreply.github.com>
2022-12-08 10:07:42 -0600

    Add grids to training video header

--- src/js/components/trainingVideos/trainingVideosHeading/TrainingVideosHeading.jsx
+++ src/js/components/trainingVideos/trainingVideosHeading/TrainingVideosHeading.jsx
@@ -6,25 +6,37 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShareAlt } from '@fortawesome/free-solid-svg-icons';
-
+import { FlexGridRow, FlexGridCol } from 'data-transparency-ui';
 
 const TrainingVideosHeading = () => (
     <section className="youtube-main-container">
-        <div className="youtube-content-container">
-            <div className="youtube-img-container">
-                <img className="youtube-img" src="img/youtube-page/youtube-landing-page-hero-image@2x.webp" alt="placeholder" />
-            </div>
-        </div>
-        <div className="youtube-info-container" >
-            <div className="youtube-info-wrap">
-                <p className="youtube-resources-title">Resources</p>
-                <p className="youtube-header">Training Videos</p>
-                <p className="youtube-body-tablet-desktop">Learn how to use USAspending.gov and understand the data. Subscribe to our YouTube channel for the latest videos!</p>
-                <p className="youtube-body-mobile">Watch our videos to learn how to use the website and understand the data.</p>
-                <FontAwesomeIcon className="youtube-share-icon" icon={faShareAlt} />
-            </div>
-        </div>
-
+        <FlexGridRow className="grid-content" hasGutter gutterSize="lg">
+            <FlexGridCol
+                width={6}
+                desktop={6}
+                tablet={0}
+                mobile={0}>
+                <div className="youtube-content-container">
+                    <div className="youtube-img-container">
+                        <img className="youtube-img" src="img/youtube-page/youtube-landing-page-hero-image@2x.webp" alt="placeholder" />
+                    </div>
+                </div>
+            </FlexGridCol>
+            <FlexGridCol
+                width={4}
+                desktop={4}
+                tablet={4}
+                mobile={2}>
+                <div className="youtube-info-container" >
+                    <div className="youtube-info-wrap">
+                        <p className="youtube-resources-title">RESOURCES</p>
+                        <p className="youtube-header">Training Videos</p>
+                        <p className="youtube-body">Learn how to use USAspending.gov and understand the data. Subscribe to our YouTube channel for the latest videos!</p>
+                        <FontAwesomeIcon className="youtube-share-icon" icon={faShareAlt} />
+                    </div>
+                </div>
+            </FlexGridCol>
+        </FlexGridRow>
 
     </section>
 );
