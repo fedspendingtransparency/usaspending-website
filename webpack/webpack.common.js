5b4bbcf7b1548850839f191242eb0e50ac0f2ffc
brianpetway <brian.petway@kc.frb.org>
2022-12-12 11:23:57 -0600

    9344 - moved jsx file back to components dir; added a line in webpack config to use babel as well mdx/loader for mdx files

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -70,6 +70,9 @@ module.exports = {
             {
                 test: /\.mdx?$/,
                 use: [
+                    // Note that Webpack runs right-to-left: `@mdx-js/loader` is used first, then
+                    // `babel-loader`.
+                    { loader: 'babel-loader', options: {} },
                     {
                         loader: '@mdx-js/loader',
                         /** @type {import('@mdx-js/loader').Options} */
