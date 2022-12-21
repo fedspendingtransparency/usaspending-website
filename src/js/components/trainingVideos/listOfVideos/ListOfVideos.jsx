ef0174a255a6d187d9c3982ebd8f37b99ca41bf9
anjenkin <blackwell_andrea@bah.com>
2022-12-21 16:39:01 -0500

    fixed linting error

--- src/js/components/trainingVideos/listOfVideos/ListOfVideos.jsx
+++ src/js/components/trainingVideos/listOfVideos/ListOfVideos.jsx
@@ -8,28 +8,26 @@ import { FlexGridRow, FlexGridCol } from "data-transparency-ui";
 import VideoCard from '../videoCard/VideoCard';
 
 
-const ListOfVideos = ({ videos }) => {
-    return (
-        <section className="list-of-videos__section">
-            <div className="grid-content">
-                <FlexGridRow hasGutter gutterSize="lg">
-                    {videos.map((video, index) => (
-                        <FlexGridCol
-                            width={3}
-                            desktop={3}
-                            className="list-of-videos__video">
-                            <VideoCard
-                                key={`video-${index}`}
-                                thumbnailUrl={video.thumbnails.standard.url}
-                                title={video.title}
-                                duration={video.duration}
-                                publishedAt={video.publishedAt}
-                                description={video.description} />
-                        </FlexGridCol>))
-                    }
-                </FlexGridRow>
-            </div>
-        </section>);
-};
+const ListOfVideos = ({ videos }) => (
+    <section className="list-of-videos__section">
+        <div className="grid-content">
+            <FlexGridRow hasGutter gutterSize="lg">
+                {videos.map((video, index) => (
+                    <FlexGridCol
+                        width={3}
+                        desktop={3}
+                        className="list-of-videos__video">
+                        <VideoCard
+                            key={`video-${index}`}
+                            thumbnailUrl={video.thumbnails.standard.url}
+                            title={video.title}
+                            duration={video.duration}
+                            publishedAt={video.publishedAt}
+                            description={video.description} />
+                    </FlexGridCol>))
+                }
+            </FlexGridRow>
+        </div>
+    </section>);
 
 export default ListOfVideos;
