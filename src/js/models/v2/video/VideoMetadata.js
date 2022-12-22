8c6b732a7af815caed080f75c110b57e45fc4005
anjenkin <blackwell_andrea@bah.com>
2022-12-21 20:22:50 -0500

    formatted publishedat date

--- src/js/models/v2/video/VideoMetadata.js
+++ src/js/models/v2/video/VideoMetadata.js
@@ -8,13 +8,22 @@ const VideoMetadata = {
         this.id = data.id || '';
         this.title = data.snippet.title || '';
         this.description = data.snippet.description || '';
-        this.publishedAt = data.snippet.publishedAt || '';
+        this._publishedAt = data.snippet.publishedAt || '';
         this.duration = data.contentDetails.duration || '';
         this.thumbnails = data.snippet.thumbnails || '';
         this._url = data.url || '';
     },
     get url() {
         return `https://www.youtube.com/watch?v=${this.id}`;
+    },
+
+    get publishedAt() {
+        const options = {
+            weekday: 'long', year: 'numeric', month: 'short', day: 'numeric'
+        };
+        const date = new Date(this._publishedAt);
+        const formattedDate = date.toLocaleDateString('en-us', options).replace(/^\w+,\s*/g, '');
+        return formattedDate;
     }
 };
 
