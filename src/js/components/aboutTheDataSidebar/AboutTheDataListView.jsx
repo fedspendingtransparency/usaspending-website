d3b18bdcbe43c973a0c0776934aa2f27422f8b2b
chasls2 <110061741+chasls2@users.noreply.github.com>
2022-12-27 15:42:20 -0600

    Google analytics added to results item

--- src/js/components/aboutTheDataSidebar/AboutTheDataListView.jsx
+++ src/js/components/aboutTheDataSidebar/AboutTheDataListView.jsx
@@ -17,6 +17,12 @@ const AboutTheDataListView = ({ section, selectItem }) => {
     const clickHandler = (e, index, section) => {
         e.preventDefault();
         selectItem(index, section);
+        Analytics.event({
+            category: 'About the Data',
+            action: 'Link',
+            label: 'About the Data'
+        });
+        console.log('herehere');
     };
 
     // eslint-disable-next-line no-shadow
@@ -26,16 +32,16 @@ const AboutTheDataListView = ({ section, selectItem }) => {
         }
     };
 
-    const handleAnalyticsAboutTheDataLink = () => Analytics.event({
-        category: 'About The Data',
-        acation: 'Link',
-        lable: 'About The Data'
+    const aboutTheDataAnalyticsHandler = () => Analytics.event({
+        category: 'About the Data',
+        action: 'Link',
+        label: 'About the Data'
     });
 
     return (<>
         <div className="atd__heading">{section.heading}</div>
         <hr />
-        {section.fields.map((list, index) => <p key={`atd-list-item-${index}`}><a className="atd__link" role="link" tabIndex={0} onKeyUp={(e) => keyHandler(e, index, section)} onClick={(e) => [clickHandler(e, index, section), handleAnalyticsAboutTheDataLink]}>{list.name}</a></p>)}
+        {section.fields.map((list, index) => <p key={`atd-list-item-${index}`}><a className="atd__link" role="link" tabIndex={0} onKeyUp={(e) => keyHandler(e, index, section)} onClick={(e) => [clickHandler(e, index, section), aboutTheDataAnalyticsHandler()]}>{list.name}</a></p>)}
     </>);
 };
 
