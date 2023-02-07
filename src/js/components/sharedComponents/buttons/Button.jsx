d9a5db19fa0c927b804cf32a2b173a23e2ed0621
Nick Torres <nicholas.torres@kc.frb.org>
2023-02-07 09:51:30 -0700

    stacked icon

--- src/js/components/sharedComponents/buttons/Button.jsx
+++ src/js/components/sharedComponents/buttons/Button.jsx
@@ -142,6 +142,11 @@ const Button = (props) => {
             <button className={classNameList} tabIndex="0" onClick={props.onClick}>{props.copy}{props.image}</button>
         );
     }
+    else if (classNameList.includes('stacked-icon')) {
+        return (
+            <button className={classNameList} tabIndex="0" onClick={props.onClick}><div className="stacked-button__only-image">{props.image}</div><div className="stacked-button__only-text">{props.copy}</div></button>
+        );
+    }
     return (
         <button className={classNameList} tabIndex="0" onClick={props.onClick}>{props.copy}</button>
     );
--- src/js/components/tempPage.jsx
+++ src/js/components/tempPage.jsx
@@ -135,7 +135,7 @@ const tempPage = () => {
                     </FlexGridRow>
                     <h1>New Button Variants</h1>
                     <FlexGridRow width={3} desktop={3} hasGutter gutterSize={32}>
-                        <Button copy="TEST" buttonSize="lg" buttonType="text" backgroundColor="dark" imageAlignment="right" image={<FontAwesomeIcon icon="share-alt" />} />
+                        <Button copy="TEST" buttonSize="sm" buttonType="stacked" backgroundColor="dark" image={<FontAwesomeIcon icon="share-alt" />} />
                     </FlexGridRow>
                 </main>
             </PageWrapper>
