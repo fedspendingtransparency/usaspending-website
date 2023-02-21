6be601424fa09aff724d43860ffb10d341d94970
Nick Torres <nicholas.torres@kc.frb.org>
2023-02-21 13:48:25 -0700

    changed flexgridcol to fragment to fix tabindex

--- src/js/components/homepageUpdate/WordOfTheDay/WordOfTheDay.jsx
+++ src/js/components/homepageUpdate/WordOfTheDay/WordOfTheDay.jsx
@@ -195,7 +195,7 @@ const WordOfTheDay = () => {
             <CardContainer variant="outline" fill="#1a4480">
                 {/* eslint-disable-next-line no-nested-ternary */}
                 {!loading && !error ?
-                    <FlexGridCol>
+                    <>
                         <div className="word-of-the-day__headline">{changedTerm === "" ? term : changedTerm}</div>
                         <div className="word-of-the-day__divider" />
                         <CardBody customClassName="word-of-the-day__body">
@@ -206,7 +206,7 @@ const WordOfTheDay = () => {
                                 </CardButton>
                             </>
                         </CardBody>
-                    </FlexGridCol>
+                    </>
                     :
                     <CardBody customClassName="card__body_error">
                         {loading ? <LoadingWrapper isLoading={loading} /> : <ErrorWordOfTheDay />}
