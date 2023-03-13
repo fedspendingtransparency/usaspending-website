229dfe23c54b246b7dd0f22aeefd8b2ed652640c
brianpetway <brian.petway@kc.frb.org>
2023-03-13 11:24:05 -0500

    9573 - made homepage resources glossary link open glossary without reloading the page; made its hover, active, and visited states match the others in that section; added cursor:pointer to the atd link in that section to match the others

--- src/js/components/homepageUpdate/HomepageResources/HomepageResources.jsx
+++ src/js/components/homepageUpdate/HomepageResources/HomepageResources.jsx
@@ -5,6 +5,7 @@
 
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 import * as slideoutActions from 'redux/actions/slideouts/slideoutActions';
 import * as aboutTheDataActions from 'redux/actions/aboutTheDataSidebar/aboutTheDataActions';
 import { FlexGridRow, FlexGridCol, CardContainer, CardBody, CardButton } from 'data-transparency-ui';
@@ -66,10 +67,10 @@ const HomepageResources = () => {
             headline: 'About the Data',
             text: 'Read important data disclosures',
             buttonText: (
-                <>
+                <div className="homepage-resources__atd-link-container">
                     View the disclosures&nbsp;&nbsp;
                     <FontAwesomeIcon icon="arrow-right" />
-                </>
+                </div>
             ),
             action: () => {
                 Analytics.event({
@@ -92,12 +93,15 @@ const HomepageResources = () => {
             headline: 'Glossary',
             text: 'Learn about spending terms',
             buttonText: (
-                <>
-                    <div>View the glossary&nbsp;&nbsp;</div>
-                    <FontAwesomeIcon icon="arrow-right" />
-                </>
+                <div className="homepage-resources__glossary-link-container">
+                    <Link
+                        to="/?glossary">
+                        <div>View the glossary&nbsp;&nbsp;
+                            <FontAwesomeIcon icon="arrow-right" />
+                        </div>
+                    </Link>
+                </div>
             ),
-            buttonLink: '/?glossary&',
             action: () => Analytics.event({
                 category: 'Homepage',
                 action: 'Link',
