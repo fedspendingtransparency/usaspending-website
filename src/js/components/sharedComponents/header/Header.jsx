cfdde2aa568bb318060773558365552b058cbfc0
anjenkin <blackwell_andrea@bah.com>
2023-03-28 13:12:47 -0400

    added info banner, colors, and positioning

--- src/js/components/sharedComponents/header/Header.jsx
+++ src/js/components/sharedComponents/header/Header.jsx
@@ -6,8 +6,10 @@ import GlossaryContainer from 'containers/glossary/GlossaryContainer';
 import GlobalModalContainer from 'containers/globalModal/GlobalModalContainer';
 import Analytics from 'helpers/analytics/Analytics';
 import AboutTheDataContainer from "containers/aboutTheDataSidebar/AboutTheDataContainer";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import ExternalLink from 'components/sharedComponents/ExternalLink';
 import NavBar from './NavBar';
+import InfoBanner from "./InfoBanner";
 
 const clickedHeaderLink = (route) => {
     Analytics.event({
@@ -90,6 +92,13 @@ export default class Header extends React.Component {
                         </div>
                     </div>
                     <NavBar />
+                    <InfoBanner
+                        icon={<FontAwesomeIcon style={{ marginTop: "-3px", width: "27", height: "27" }} icon="info-circle" color="#97d4ea" />}
+                        border="#97d4ea"
+                        backgroundColor="#e1f3f8"
+                        title="Love using USAspending.gov? Tell us more!"
+                        content={<>USAspending.gov is looking to share stories of how federal spending data has improved your life or increased your trust in government.
+                            Do you have a story to share?&nbsp;<ExternalLink isCard url="https://forms.office.com/g/neemMd2J4a">Fill out this survey and you may be featured in an upcoming USAspending YouTube video!</ExternalLink></>} />
                 </header>
                 <AboutTheDataContainer />
                 <GlossaryContainer />
