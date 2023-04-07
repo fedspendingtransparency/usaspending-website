8c833a83e2f170a840272975f487d5fc248047a0
brianpetway <brian.petway@kc.frb.org>
2023-04-07 14:34:49 -0500

    9672 - layout changes for large screens

--- src/js/containers/Footer2.jsx
+++ src/js/containers/Footer2.jsx
@@ -66,143 +66,149 @@ const Footer2 = ({
                         <img src="img/footer_logo.png" alt="USAspending.gov" />
                     </Link>
                 </div>
-                <div className="footer2-heading">
+                <div className="footer2-large-screen-row-one">
+                    <div className="footer2-heading-container">
+                        <div className="footer2-heading">
                     Building a more transparent government.
-                </div>
-                <div className="footer2-subHeading">
+                        </div>
+                        <div className="footer2-subHeading">
                     Providing publicly accessible and searchable data on what the federal government spends each year.
-                </div>
-                <div className="footer2-main-links-section">
-                    <div className="footer2-link-section">
-                        <div className="footer2-link-section-title">ABOUT</div>
-                        <ul>
-                            <li>
-                                <Link
-                                    to="/about"
-                                    onClick={clickedFooterLink.bind(null, '/about')}>
+                        </div>
+                    </div>
+                    <div className="footer2-main-links-section">
+                        <div className="footer2-link-section">
+                            <div className="footer2-link-section-title">ABOUT</div>
+                            <ul>
+                                <li>
+                                    <Link
+                                        to="/about"
+                                        onClick={clickedFooterLink.bind(null, '/about')}>
                             Mission
-                                </Link>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link
+                                        to="/about?section=careers"
+                                        onClick={clickedFooterLink.bind(null, '/about')}>
+                            Careers
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="footer2-link-section">
+                            <div className="footer2-link-section-title">HELP</div>
+                            <ul>
+                                <li>
+                                    <FooterExternalLink
+                                        link="https://fiscalservice.force.com/usaspending/s/recordlist/Knowledge__kav/00B3d000000V4WDEA0"
+                                        title="FAQs" />
+                                </li>
+                                <li>
+                                    <FooterExternalLink
+                                        link="https://fiscalservice.force.com/usaspending/s/"
+                                        title="Community" />
+                                </li>
+                                <li>
+                                    <a
+                                        href="mailto:usaspending.help@fiscal.treasury.gov?subject=Contact%20Us"
+                                        onClick={clickedFooterLink.bind(
+                                            null,
+                                            'mailto:usaspending.help@fiscal.treasury.gov?subject=Contact%20Us'
+                                        )}>
+                                Email Us
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="footer2-link-section">
+                            <div className="footer2-link-section-title">RELATED SITES</div>
+                            <ul>
+                                <li>
+                                    <a target="_blank" rel="noopener noreferrer" href="https://fiscaldata.treasury.gov/">Fiscal Data</a>
+                                </li>
+                                <li>
+                                    <a target="_blank" rel="noopener noreferrer" href="https://fiscal.treasury.gov/">Bureau of the Fiscal Service</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className="footer2-large-screen-row-two">
+                    <div className="footer2-social-media-section">
+                        <ul>
+                            <li>
+                                <button onClick={generateOnClick("https://www.youtube.com/channel/UCyDn83O-0XC98H3TCV-VCGQ")} title="Youtube">
+                                    <FontAwesomeIcon icon={faYoutube} size="sm" color="#dfe1e2" />
+                                </button>
                             </li>
+                            <li>
+                                <button onClick={generateOnClick("https://twitter.com/usaspending/")} title="Twitter">
+                                    <FontAwesomeIcon icon={faTwitter} size="sm" color="#dfe1e2" />
+                                </button>
+                            </li>
+                            <li>
+                                <button onClick={generateOnClick("https://www.facebook.com/fiscalservice/")} title="Facebook">
+                                    <FontAwesomeIcon icon={faFacebookSquare} size="sm" color="#dfe1e2" />
+                                </button>
+                            </li>
+                            <li>
+                                <button onClick={generateOnClick("https://github.com/fedspendingtransparency/usaspending-website")} title="Github">
+                                    <FontAwesomeIcon icon={faGithub} size="sm" color="#dfe1e2" />
+                                </button>
+                            </li>
+                            <li>
+                                <button onClick={generateOnClick("https://www.linkedin.com/company/bureau-of-the-fiscal-service/")} title="LinkedIn">
+                                    <FontAwesomeIcon icon={faLinkedin} size="sm" color="#dfe1e2" />
+                                </button>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="footer2-legal-links-section">
+                        <ul>
                             <li>
                                 <Link
-                                    to="/about?section=careers"
-                                    onClick={clickedFooterLink.bind(null, '/about')}>
-                            Careers
+                                    to="/about/accessibility"
+                                    onClick={clickedFooterLink.bind(null, '/about/accessibility')}>
+                                Accessibility
+                                </Link>
+                            </li>
+                            {!isMobile && (
+                                <li>|</li>
+                            )}
+                            <li>
+                                <Link
+                                    to="/about/privacy"
+                                    onClick={clickedFooterLink.bind(null, '/about/privacy')}>
+                                Privacy Policy
+                                </Link>
+                            </li>
+                            {!isMobile && (
+                                <li>|</li>
+                            )}
+                            <li>
+                                <Link
+                                    to="/about/foia"
+                                    onClick={clickedFooterLink.bind(null, '/about/foia')}>
+                                Freedom of Information Act
+                                </Link>
+                            </li>
+                            {!isMobile && (
+                                <li>|</li>
+                            )}
+                            <li>
+                                <Link
+                                    to="/db_info"
+                                    title="Limitation on Permissible Use of Dun & Bradstreet, Inc. (D&B) Data"
+                                    aria-label="Limitation on Permissible Use of Dun & Bradstreet, Inc. (D&B) Data"
+                                    onClick={clickedFooterLink.bind(null, '/db_info')}>
+                                D&B Information
                                 </Link>
                             </li>
                         </ul>
                     </div>
-                    <div className="footer2-link-section">
-                        <div className="footer2-link-section-title">HELP</div>
-                        <ul>
-                            <li>
-                                <FooterExternalLink
-                                    link="https://fiscalservice.force.com/usaspending/s/recordlist/Knowledge__kav/00B3d000000V4WDEA0"
-                                    title="FAQs" />
-                            </li>
-                            <li>
-                                <FooterExternalLink
-                                    link="https://fiscalservice.force.com/usaspending/s/"
-                                    title="Community" />
-                            </li>
-                            <li>
-                                <a
-                                    href="mailto:usaspending.help@fiscal.treasury.gov?subject=Contact%20Us"
-                                    onClick={clickedFooterLink.bind(
-                                        null,
-                                        'mailto:usaspending.help@fiscal.treasury.gov?subject=Contact%20Us'
-                                    )}>
-                                Email Us
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="footer2-link-section">
-                        <div className="footer2-link-section-title">RELATED SITES</div>
-                        <ul>
-                            <li>
-                                <a target="_blank" rel="noopener noreferrer" href="https://fiscaldata.treasury.gov/">Fiscal Data</a>
-                            </li>
-                            <li>
-                                <a target="_blank" rel="noopener noreferrer" href="https://fiscal.treasury.gov/">Bureau of the Fiscal Service</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <div className="footer2-social-media-section">
-                    <ul>
-                        <li>
-                            <button onClick={generateOnClick("https://www.youtube.com/channel/UCyDn83O-0XC98H3TCV-VCGQ")} title="Youtube">
-                                <FontAwesomeIcon icon={faYoutube} size="sm" color="#dfe1e2" />
-                            </button>
-                        </li>
-                        <li>
-                            <button onClick={generateOnClick("https://twitter.com/usaspending/")} title="Twitter">
-                                <FontAwesomeIcon icon={faTwitter} size="sm" color="#dfe1e2" />
-                            </button>
-                        </li>
-                        <li>
-                            <button onClick={generateOnClick("https://www.facebook.com/fiscalservice/")} title="Facebook">
-                                <FontAwesomeIcon icon={faFacebookSquare} size="sm" color="#dfe1e2" />
-                            </button>
-                        </li>
-                        <li>
-                            <button onClick={generateOnClick("https://github.com/fedspendingtransparency/usaspending-website")} title="Github">
-                                <FontAwesomeIcon icon={faGithub} size="sm" color="#dfe1e2" />
-                            </button>
-                        </li>
-                        <li>
-                            <button onClick={generateOnClick("https://www.linkedin.com/company/bureau-of-the-fiscal-service/")} title="LinkedIn">
-                                <FontAwesomeIcon icon={faLinkedin} size="sm" color="#dfe1e2" />
-                            </button>
-                        </li>
-                    </ul>
-                </div>
-                <div className="footer2-legal-links-section">
-                    <ul>
-                        <li>
-                            <Link
-                                to="/about/accessibility"
-                                onClick={clickedFooterLink.bind(null, '/about/accessibility')}>
-                                Accessibility
-                            </Link>
-                        </li>
-                        {!isMobile && (
-                            <li>|</li>
-                        )}
-                        <li>
-                            <Link
-                                to="/about/privacy"
-                                onClick={clickedFooterLink.bind(null, '/about/privacy')}>
-                                Privacy Policy
-                            </Link>
-                        </li>
-                        {!isMobile && (
-                            <li>|</li>
-                        )}
-                        <li>
-                            <Link
-                                to="/about/foia"
-                                onClick={clickedFooterLink.bind(null, '/about/foia')}>
-                                Freedom of Information Act
-                            </Link>
-                        </li>
-                        {!isMobile && (
-                            <li>|</li>
-                        )}
-                        <li>
-                            <Link
-                                to="/db_info"
-                                title="Limitation on Permissible Use of Dun & Bradstreet, Inc. (D&B) Data"
-                                aria-label="Limitation on Permissible Use of Dun & Bradstreet, Inc. (D&B) Data"
-                                onClick={clickedFooterLink.bind(null, '/db_info')}>
-                                D&B Information
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
-                <div className="footer2-copyright-section">
+                    <div className="footer2-copyright-section">
                         &copy; {year} USAspending.gov
+                    </div>
                 </div>
             </div>
         </>
