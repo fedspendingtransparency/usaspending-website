7f4ad08c36ee5da1b2a777ea8b9aa0acc977be1a
anjenkin <blackwell_andrea@bah.com>
2023-05-06 16:48:35 -0400

    wip

--- src/js/components/megaMenu/NavbarItem.jsx
+++ src/js/components/megaMenu/NavbarItem.jsx
@@ -1,38 +1,4 @@
-import styled from "styled-components";
 import React, { Component } from "react";
-
-const NavbarItemTitle = styled.button`
-  background-color: transparent;
-  font-family: inherit;
-  font-weight: bold;
-  border: none;
-  font-size: 18px;
-  padding: 1.25rem;
-  color: white;
-  display: flex;
-  justify-content: center;
-  transition: opacity 250ms;
-  cursor: pointer;
-  position: relative;
-  z-index: 2;
-  &:hover,
-  &:focus {
-    opacity: 0.7;
-    outline: none;
-  }
-`;
-
-const NavbarItemEl = styled.li`
-  position: relative;
-  cursor: pointer;
-`;
-
-const DropdownSlot = styled.div`
-  position: absolute;
-  left: 50%;
-  transform: translateX(-50%);
-  perspective: 1500px;
-`;
 
 export default class NavbarItem extends Component {
     onMouseEnter = () => {
@@ -42,15 +8,14 @@ export default class NavbarItem extends Component {
     render() {
         const { title, children } = this.props;
         return (
-            <NavbarItemEl>
-                <NavbarItemTitle
+            <li className="navbar-item-el">
+                <button className="navbar-item-title"
                     onMouseEnter={this.onMouseEnter}
-                    onFocus={this.onMouseEnter}
-                >
+                    onFocus={this.onMouseEnter}>
                     {title}
-                </NavbarItemTitle>
-                <DropdownSlot>{children}</DropdownSlot>
-            </NavbarItemEl>
+                </button>
+                <div className="dropdown-slot">{children}</div>
+            </li>
         );
     }
 }
