4afb9d8c8c8abb6f28f05265b66aeda7373ad638
Nick Torres <nicholas.torres@kc.frb.org>
2023-05-10 08:54:50 -0600

    fix errors

--- src/js/components/sharedComponents/header/megaMenu/ItemContent.jsx
+++ src/js/components/sharedComponents/header/megaMenu/ItemContent.jsx
@@ -19,13 +19,13 @@ const ItemContent = ({
     <div className="dropdown-section__wrapper">
         <div className="dropdown-section" data-first-dropdown-section>
             <div>
-                {section1Icon ? <FontAwesomeIcon className="" icon={section1Icon} /> : ''}<p>{section1Title}</p>
+                {section1Icon && section1Icon !== null && section1Icon !== '' ? <FontAwesomeIcon className="" icon={section1Icon} /> : ''}<p>{section1Title}</p>
                 {section1Sub !== null && section1Sub !== undefined && section1Sub !== '' ?
                     <span className="dropdown-section__section-subtitle">{section1Sub}</span> : ''}
                 <ul>
                     {section1Items.map((item, index) => (
                         <li key={`link-${index}`}>
-                            <FontAwesomeIcon className="" icon={item.icon} /><Link to={item.url}>{item.label}</Link>
+                            {item.icon && item.icon !== '' && item.icon !== null ? <FontAwesomeIcon className="" icon={item.icon} /> : ''}<Link to={item.url}>{item.label}</Link>
                         </li>
                     ))}
                 </ul>
@@ -33,13 +33,13 @@ const ItemContent = ({
         </div>
         <div className="dropdown-section">
             <div>
-                {section2Icon ? <FontAwesomeIcon className="" icon={section2Icon} /> : ''}<p>{section2Title}</p>
+                {section2Icon && section2Icon !== null && section2Icon !== '' ? <FontAwesomeIcon className="" icon={section2Icon} /> : ''}<p>{section2Title}</p>
                 {section2Sub !== null && section2Sub !== undefined && section2Sub !== '' ?
                     <span className="dropdown-section__section-subtitle">{section2Sub}</span> : ''}
                 <ul>
                     {section2Items.map((item, index) => (
                         <li key={`link-${index}`}>
-                            <FontAwesomeIcon className="" icon={item.icon} /><Link to={item.url}>{item.label}</Link>
+                            {item.icon && item.icon !== '' && item.icon !== null ? <FontAwesomeIcon className="" icon={item.icon} /> : ''}<Link to={item.url}>{item.label}</Link>
                         </li>
                     ))}
                 </ul>
@@ -48,7 +48,7 @@ const ItemContent = ({
         {section3Title !== null && section3Title !== undefined && section3Title !== '' ?
             <div className="dropdown-section">
                 <div>
-                    {section3Icon ? <FontAwesomeIcon className="" icon={section3Icon} /> : ''}<p>{section3Title}</p>
+                    {section3Icon && section3Icon !== null && section3Icon !== '' ? <FontAwesomeIcon className="" icon={section3Icon} /> : ''}<p>{section3Title}</p>
                     {section3Sub !== null && section3Sub !== undefined && section3Sub !== '' ?
                         <span className="dropdown-section__section-subtitle">{section3Sub}</span> : ''}
                     <ul>
