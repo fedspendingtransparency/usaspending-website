ae4ad168ac64519fcd9051d3d4c380f2afa3f106
brianpetway <brian.petway@kc.frb.org>
2023-05-11 10:25:00 -0500

    9748 - added toTitleCase fn for all names because Program Activity names are all caps

--- src/js/models/v2/agency/BaseAgencySubcomponentsList.js
+++ src/js/models/v2/agency/BaseAgencySubcomponentsList.js
@@ -2,12 +2,14 @@
  * BaseAgencySubcomponentsList.js
  * Created by Afna Saifudeen 12/14/21
  */
+
 import { formatMoneyWithUnitsShortLabel } from 'helpers/moneyFormatter';
+import { toTitleCase } from 'helpers/agency/StatusOfFundsVizHelper';
 
 const BaseAgencySubcomponentsList = {
     populate(data, id) {
         this.id = id || data?.id || data?.code || '';
-        this.name = data?.name || '';
+        this.name = data?.name ? toTitleCase(data?.name) : '';
         /* eslint-disable camelcase */
         this._budgetaryResources = data?.total_budgetary_resources || data?.budgetary_resources_amount || 0;
         this._obligations = data?.total_obligations || data?.obligated_amount || 0;
