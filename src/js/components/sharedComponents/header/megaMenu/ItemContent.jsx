28f49f79f840202ee3baf19f2fa8209f75ab3f0a
Nick Torres <nicholas.torres@kc.frb.org>
2023-05-11 15:32:58 -0600

    atd action

--- src/js/components/sharedComponents/header/megaMenu/ItemContent.jsx
+++ src/js/components/sharedComponents/header/megaMenu/ItemContent.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { useDispatch } from 'react-redux';
 import { FlexGridRow } from 'data-transparency-ui';
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { uniqueId } from 'lodash';
+import * as aboutTheDataActions from 'redux/actions/aboutTheDataSidebar/aboutTheDataActions';
+import * as slideoutActions from 'redux/actions/slideouts/slideoutActions';
 
 const ItemContent = ({
     section1Title,
@@ -16,42 +19,26 @@ const ItemContent = ({
     section1Icon,
     section2Icon,
     section3Icon
-}) => (
-    <div className="dropdown-section__wrapper">
-        <div className="dropdown-section__top-columns">
-            <div key={uniqueId()} className="dropdown-section first-dropdown-section" data-first-dropdown-section>
-                {section1Icon && section1Icon !== null && section1Icon !== '' ? <FontAwesomeIcon size="lg" className="" icon={section1Icon} /> : ''}
-                <div>
-                    <p className="dropdown-section__section-title">{section1Title}</p>
-                    {section1Sub !== null && section1Sub !== undefined && section1Sub !== '' ?
-                        <span className="dropdown-section__section-subtitle">{section1Sub}</span> : ''}
-                    <ul className="dropdown-section__section-list">
-                        {section1Items.map((item, index) => (
-                            <>
-                                <li key={`link-${uniqueId(index)}`}>
-                                    {item.icon && item.icon !== '' && item.icon !== null ? <FontAwesomeIcon size="lg" className="" icon={item.icon} /> : ''}
-                                    <div className="dropdown-item__link-desc">
-                                        <Link className="dropdown-item__link-label" to={item.url}>{item.label}
-                                            <span className="dropdown-item__description">{item.description}</span>
-                                        </Link>
-                                    </div>
-                                </li>
-                            </>
-                        ))}
-                    </ul>
-                </div>
-            </div>
-            <div key={uniqueId()} className="dropdown-section">
-                <div className="dropdown-section__alternate-top">
-                    {section2Icon && section2Icon !== null && section2Icon !== '' ? <FontAwesomeIcon size="lg" className="" icon={section2Icon} /> : ''}
+}) => {
+    const dispatch = useDispatch();
+
+    const openATD = () => {
+        dispatch(aboutTheDataActions.showAboutTheData());
+        dispatch(slideoutActions.setLastOpenedSlideout('atd'));
+    };
+    return (
+        <div className="dropdown-section__wrapper">
+            <div className="dropdown-section__top-columns">
+                <div key={uniqueId()} className="dropdown-section first-dropdown-section" data-first-dropdown-section>
+                    {section1Icon && section1Icon !== null && section1Icon !== '' ? <FontAwesomeIcon size="lg" className="" icon={section1Icon} /> : ''}
                     <div>
-                        <p className="dropdown-section__section-title">{section2Title}</p>
-                        {section2Sub !== null && section2Sub !== undefined && section2Sub !== '' ?
-                            <span className="dropdown-section__section-subtitle">{section2Sub}</span> : ''}
+                        <p className="dropdown-section__section-title">{section1Title}</p>
+                        {section1Sub !== null && section1Sub !== undefined && section1Sub !== '' ?
+                            <span className="dropdown-section__section-subtitle">{section1Sub}</span> : ''}
                         <ul className="dropdown-section__section-list">
-                            {section2Items.map((item, index) => (
+                            {section1Items.map((item, index) => (
                                 <>
-                                    <li key={`second-section-link-${uniqueId(index)}`}>
+                                    <li key={`link-${uniqueId(index)}`}>
                                         {item.icon && item.icon !== '' && item.icon !== null ? <FontAwesomeIcon size="lg" className="" icon={item.icon} /> : ''}
                                         <div className="dropdown-item__link-desc">
                                             <Link className="dropdown-item__link-label" to={item.url}>{item.label}
@@ -64,34 +51,58 @@ const ItemContent = ({
                         </ul>
                     </div>
                 </div>
-            </div>
-        </div>
-        {section3Title !== null && section3Title !== undefined && section3Title !== '' ?
-            <div key={uniqueId()} className="dropdown-section dropdown-section__bottom-portion">
-                <div className="dropdown-section__alternate-top">
-                    {section3Icon && section3Icon !== null && section3Icon !== '' ? <FontAwesomeIcon size="lg" className="" icon={section3Icon} /> : ''}
-                    <div>
-                        <p className="dropdown-section__section-title">{section3Title}</p>
-                        <div className="dropdown-section__section-list">
-                            {section3Items.map((item, index) => (
-                                <div key={`third-section-link-${uniqueId(index)}`}>
-                                    <FlexGridRow desktop={6} width={6} >
-                                        <div className="dropdown-section__section-list-item">
+                <div key={uniqueId()} className="dropdown-section">
+                    <div className="dropdown-section__alternate-top">
+                        {section2Icon && section2Icon !== null && section2Icon !== '' ? <FontAwesomeIcon size="lg" className="" icon={section2Icon} /> : ''}
+                        <div>
+                            <p className="dropdown-section__section-title">{section2Title}</p>
+                            {section2Sub !== null && section2Sub !== undefined && section2Sub !== '' ?
+                                <span className="dropdown-section__section-subtitle">{section2Sub}</span> : ''}
+                            <ul className="dropdown-section__section-list">
+                                {section2Items.map((item, index) => (
+                                    <>
+                                        <li key={`second-section-link-${uniqueId(index)}`}>
+                                            {item.icon && item.icon !== '' && item.icon !== null ? <FontAwesomeIcon size="lg" className="" icon={item.icon} /> : ''}
                                             <div className="dropdown-item__link-desc">
-                                                <Link className="dropdown-item__link-label" to={item.url}>{item.label}
-                                                    <div className="dropdown-item__description">{item.description}</div>
+                                                <Link className="dropdown-item__link-label" to={item.url !== "?about-the-data" ? item.url : ''} onMouseUp={item.url !== '?about-the-data' ? '' : openATD}>{item.label}
+                                                    <span className="dropdown-item__description">{item.description}</span>
                                                 </Link>
                                             </div>
-                                        </div>
-                                    </FlexGridRow>
-                                </div>
-                            ))}
+                                        </li>
+                                    </>
+                                ))}
+                            </ul>
                         </div>
                     </div>
                 </div>
-            </div> : ''}
-    </div>
+            </div>
+            {section3Title !== null && section3Title !== undefined && section3Title !== '' ?
+                <div key={uniqueId()} className="dropdown-section dropdown-section__bottom-portion">
+                    <div className="dropdown-section__alternate-top">
+                        {section3Icon && section3Icon !== null && section3Icon !== '' ? <FontAwesomeIcon size="lg" className="" icon={section3Icon} /> : ''}
+                        <div>
+                            <p className="dropdown-section__section-title">{section3Title}</p>
+                            <div className="dropdown-section__section-list">
+                                {section3Items.map((item, index) => (
+                                    <div key={`third-section-link-${uniqueId(index)}`}>
+                                        <FlexGridRow desktop={6} width={6} >
+                                            <div className="dropdown-section__section-list-item">
+                                                <div className="dropdown-item__link-desc">
+                                                    <Link className="dropdown-item__link-label" to={item.url}>{item.label}
+                                                        <div className="dropdown-item__description">{item.description}</div>
+                                                    </Link>
+                                                </div>
+                                            </div>
+                                        </FlexGridRow>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                </div> : ''}
+        </div>
 
-);
+    );
+};
 
 export default ItemContent;
