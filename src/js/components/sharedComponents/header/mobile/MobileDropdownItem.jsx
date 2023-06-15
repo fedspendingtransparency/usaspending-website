019c4b6433e66dc51f58a21cfb3e8c70519968fc
chasls2 <110061741+chasls2@users.noreply.github.com>
2023-06-15 08:04:07 -0500

    removed Link

--- src/js/components/sharedComponents/header/mobile/MobileDropdownItem.jsx
+++ src/js/components/sharedComponents/header/mobile/MobileDropdownItem.jsx
@@ -6,7 +6,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 
 
 const propTypes = {
