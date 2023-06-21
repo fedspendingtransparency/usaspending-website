04e5c25f6b8ea3cf9d6b22da711a1cff1386a63f
anjenkin <blackwell_andrea@bah.com>
2023-06-21 13:34:24 -0400

    Revert "9934 - merged qat in; added link to survey in new stay in touch section; changed order fo the sections, as specified in ticket"
    This reverts commit a28d863257307c3f20144d190b2165d42dbadf8f.

--- src/js/components/sharedComponents/StayInTouch.jsx
+++ src/js/components/sharedComponents/StayInTouch.jsx
@@ -4,8 +4,6 @@
  */
 
 import React from 'react';
-import { useDispatch } from 'react-redux';
-import { showModal } from 'redux/actions/modal/modalActions';
 import { FlexGridRow, FlexGridCol } from 'data-transparency-ui';
 import PropTypes from "prop-types";
 import Analytics from 'helpers/analytics/Analytics';
@@ -18,8 +16,6 @@ const propTypes = {
 };
 
 const StayInTouch = (pageName) => {
-    const dispatch = useDispatch();
-
     const trackLinkSignUp = () => Analytics.event({
         category: pageName,
         action: 'Link',
@@ -46,7 +42,7 @@ const StayInTouch = (pageName) => {
     };
     const handleSurveyClick = () => {
         trackLinkSurvey();
-        dispatch(showModal("https://forms.office.com/g/neemMd2J4a"));
+        console.log('open survey here');
     };
 
     return (
@@ -91,6 +87,30 @@ const StayInTouch = (pageName) => {
                     </div>
                 </FlexGridCol>
                 <FlexGridCol
+                    className="stay-in-touch__second-row-container top"
+                    mobile={12}
+                    tablet={12}
+                    desktop={3}>
+                    <div className="stay-in-touch__second-row-title">
+                                Get release notes delivered to your inbox
+                    </div>
+                    <div className="stay-in-touch__second-row-text">
+                                Sign up to receive our release notes to keep up with what’s new on USAspending.gov.
+                    </div>
+                    <div className="stay-in-touch__second-row-link">
+                        <Button
+                            copy="Sign Up"
+                            buttonTitle="Sign Up"
+                            buttonSize="md"
+                            onClick={handleSignUp}
+                            buttonType="text"
+                            backgroundColor="light"
+                            imageAlignment="right"
+                            textAlignment="left"
+                            image={<FontAwesomeIcon icon={faArrowRight} style={{ height: '16px', width: '14px' }} />} />
+                    </div>
+                </FlexGridCol>
+                <FlexGridCol
                     className="stay-in-touch__second-row-container"
                     mobile={12}
                     tablet={12}
@@ -111,30 +131,6 @@ const StayInTouch = (pageName) => {
                             backgroundColor="light"
                             textAlignment="left"
                             imageAlignment="right"
-                            image={<FontAwesomeIcon icon={faArrowRight} style={{ height: '16px', width: '14px' }} />} />
-                    </div>
-                </FlexGridCol>
-                <FlexGridCol
-                    className="stay-in-touch__second-row-container top"
-                    mobile={12}
-                    tablet={12}
-                    desktop={3}>
-                    <div className="stay-in-touch__second-row-title">
-                        Get release notes delivered to your inbox
-                    </div>
-                    <div className="stay-in-touch__second-row-text">
-                        Sign up to receive our release notes to keep up with what’s new on USAspending.gov.
-                    </div>
-                    <div className="stay-in-touch__second-row-link">
-                        <Button
-                            copy="Sign Up"
-                            buttonTitle="Sign Up"
-                            buttonSize="md"
-                            onClick={handleSignUp}
-                            buttonType="text"
-                            backgroundColor="light"
-                            imageAlignment="right"
-                            textAlignment="left"
                             image={<FontAwesomeIcon icon={faArrowRight} style={{ height: '16px', width: '14px' }} />} />
                     </div>
                 </FlexGridCol>
