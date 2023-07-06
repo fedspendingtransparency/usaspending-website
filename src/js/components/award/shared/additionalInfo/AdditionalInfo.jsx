5f8510003a380bbef6b8da9365c0e7c9ae1f2985
Nick Torres <nicholas.torres@kc.frb.org>
2023-07-06 10:42:51 -0600

    fix for recipient details

--- src/js/components/award/shared/additionalInfo/AdditionalInfo.jsx
+++ src/js/components/award/shared/additionalInfo/AdditionalInfo.jsx
@@ -56,7 +56,7 @@ export default class AdditionalInfo extends React.Component {
             data.placeOfPerformance["Congressional District"] = splitCDPOP[1];
         }
 
-        const cdRD = data.recipientDetails["Recipient Address"].data.pop().trim();
+        const cdRD = data.recipientDetails["Congressional District"].data.pop().trim();
         const splitCDRD = cdRD.split(": ");
         if (splitCDRD.length === 2) {
             data.recipientDetails["Congressional District"] = splitCDPOP[1];
@@ -160,11 +160,11 @@ export default class AdditionalInfo extends React.Component {
             data.placeOfPerformance["Congressional District"] = splitCDPOP[1];
         }
 
-        // const cdRD = data.recipientDetails["Recipient Address"].data.pop().trim();
-        // const splitCDRD = cdRD.split(": ");
-        // if (splitCDRD.length === 2) {
-        //     data.recipientDetails["Congressional District"] = splitCDPOP[1];
-        // }
+        const cdRD = data.recipientDetails["Congressional District"].data.pop().trim();
+        const splitCDRD = cdRD.split(": ");
+        if (splitCDRD.length === 2) {
+            data.recipientDetails["Congressional District"] = splitCDPOP[1];
+        }
         const columnOne = [
             (<Accordion
                 key="UniqueAwardKey"
