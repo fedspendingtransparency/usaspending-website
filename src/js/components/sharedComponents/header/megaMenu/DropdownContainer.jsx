6eb9b479652cb6d6c538e38152cc74a99f5ca52b
anjenkin <blackwell_andrea@bah.com>
2023-08-01 11:24:56 -0400

    wip

--- src/js/components/sharedComponents/header/megaMenu/DropdownContainer.jsx
+++ src/js/components/sharedComponents/header/megaMenu/DropdownContainer.jsx
@@ -66,7 +66,7 @@ export default class DropdownContainer extends Component {
                     <div className="dropdown-background">
                         <Flipped inverseFlipId="dropdown" scale>
                             <div>
-                                {currentDropdown}
+                                {children}
                             </div>
                         </Flipped>
                     </div>
