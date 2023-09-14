b0b9c81bbcf9c565b690e01988de3b2b5072aa34
anjenkin <blackwell_andrea@bah.com>
2023-09-14 11:00:26 -0400

    updated explorerdetailpagecontainer and explorerdetailpage components

--- src/js/components/explorer/detail/ExplorerDetailPage.jsx
+++ src/js/components/explorer/detail/ExplorerDetailPage.jsx
@@ -3,79 +3,56 @@
  * Created by Kevin Li 8/16/17
  */
 
-import React from 'react';
-import PropTypes from 'prop-types';
-
+import React, { useState } from 'react';
 import DetailContentContainer from 'containers/explorer/detail/DetailContentContainer';
-
 import ExplorerWrapperPage from '../ExplorerWrapperPage';
 import ExplorerTooltip from './visualization/ExplorerTooltip';
 import ExplorerAwardTooltip from './visualization/ExplorerAwardTooltip';
 
-const propTypes = {
-    explorer: PropTypes.object,
-    setExplorerYear: PropTypes.func
+const ExplorerDetailPage = () => {
+    const [showTooltip, setShowTooltip] = useState(false);
+    const [tooltip, setTooltip] = useState({
+        x: 0,
+        y: 0,
+        name: '',
+        code: '',
+        amount: 0,
+        percent: 0,
+        total: 0,
+        isAward: false
+    });
+
+    const showTooltipFn = (position, data) => {
+        setShowTooltip(true);
+        setTooltip(Object.assign({}, position, data));
+    };
+
+    const hideTooltipFn = () => {
+        setShowTooltip(false);
+    };
+
+    let tooltipUi = null;
+
+    if (showTooltip) {
+        tooltipUi = (<ExplorerTooltip
+            {...tooltip} />);
+
+        if (tooltip.isAward) {
+            tooltipUi = (<ExplorerAwardTooltip
+                {...tooltip} />);
+        }
+    }
+
+    return (
+        <ExplorerWrapperPage>
+            <div className="explorer-detail-wrap">
+                <DetailContentContainer
+                    showTooltip={showTooltipFn}
+                    hideTooltip={hideTooltipFn} />
+                {tooltipUi}
+            </div>
+        </ExplorerWrapperPage>
+    );
 };
 
-export default class ExplorerDetailPage extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            showTooltip: false,
-            tooltip: {
-                x: 0,
-                y: 0,
-                name: '',
-                code: '',
-                amount: 0,
-                percent: 0,
-                total: 0,
-                isAward: false
-            }
-        };
-
-        this.showTooltip = this.showTooltip.bind(this);
-        this.hideTooltip = this.hideTooltip.bind(this);
-    }
-
-    showTooltip(position, data) {
-        this.setState({
-            showTooltip: true,
-            tooltip: Object.assign({}, position, data)
-        });
-    }
-
-    hideTooltip() {
-        this.setState({
-            showTooltip: false
-        });
-    }
-
-    render() {
-        let tooltip = null;
-
-        if (this.state.showTooltip) {
-            tooltip = (<ExplorerTooltip
-                {...this.state.tooltip} />);
-
-            if (this.state.tooltip.isAward) {
-                tooltip = (<ExplorerAwardTooltip
-                    {...this.state.tooltip} />);
-            }
-        }
-
-        return (
-            <ExplorerWrapperPage>
-                <div className="explorer-detail-wrap">
-                    <DetailContentContainer
-                        showTooltip={this.showTooltip}
-                        hideTooltip={this.hideTooltip} />
-                    {tooltip}
-                </div>
-            </ExplorerWrapperPage>
-        );
-    }
-}
-
-ExplorerDetailPage.propTypes = propTypes;
+export default ExplorerDetailPage;
