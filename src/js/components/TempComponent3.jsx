958edb64eb4bb541e47e9c97be03cca643410fa6
brianpetway <brian.petway@kc.frb.org>
2023-09-20 08:56:05 -0500

    10164 - removed jumpToSection; trying to get api call to depend on IntersectionObserver

--- src/js/components/TempComponent3.jsx
+++ src/js/components/TempComponent3.jsx
@@ -1,18 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import fetchData from './fetchData';
 
-const TempComponent3 = () => (
-    <section
-        id="temp-component-three"
-        style={{
-            border: '2px solid blue',
-            height: '400px',
-            margin: '40px 40px',
-            textAlign: 'center',
-            fontSize: '24px',
-            paddingTop: '180px'
-        }}>
-        COMPONENT 3
-    </section>
-);
+const resource = fetchData();
+
+const TempComponent3 = (inViewport) => {
+    const [content, setContent] = useState(null);
+    const [term, setTerm] = useState(null);
+    const [definition, setDefinition] = useState(null);
+
+    useEffect(() => {
+        if (inViewport) {
+            // content = resource.read();
+            setContent(resource.read());
+            // term = content.results[0].term;
+            setTerm(content?.results[0].term);
+            // definition = content.results[0].official;
+            setDefinition(content?.results[0].official);
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [inViewport]);
+
+    return (
+        <section
+            id="temp-component-three"
+            style={{
+                border: '2px solid blue',
+                height: '400px',
+                margin: '40px 40px',
+                textAlign: 'center',
+                fontSize: '24px',
+                paddingTop: '80px'
+            }}>
+            COMPONENT 3
+            <div>
+                {term}
+                {definition}
+            </div>
+        </section>
+    );
+};
 
 export default TempComponent3;
--- src/js/components/TempSearchPage.jsx
+++ src/js/components/TempSearchPage.jsx
@@ -1,6 +1,5 @@
 import React, { Suspense, useEffect, useRef, useState } from "react";
 import { useHistory } from "react-router-dom";
-import { useQueryParams } from 'helpers/queryParams';
 import PageWrapper from "./sharedComponents/PageWrapper";
 import PageFeatureFlag from "./sharedComponents/PageFeatureFlag";
 import TempComponent1 from "./TempComponent1";
@@ -8,77 +7,37 @@ import TempComponent2 from "./TempComponent2";
 import TempComponent3 from "./TempComponent3";
 import TempComponent4 from "./TempComponent4";
 import TempLoadingComponent from "./TempLoadingComponent";
-import { kebabCase, snakeCase } from "lodash";
-import { getVerticalOffset } from "../helpers/covid19Helper";
-import { scrollToY } from 'helpers/scrollToHelper';
-
 
 require("pages/search/searchPage.scss");
 
 const TempSearchPage = () => {
     const containerRef = useRef(null);
-    const query = useQueryParams();
-    const history = useHistory();
-    const [activeSection, setActiveSection] = useState(query.section || 'one');
+    // const history = useHistory();
     const [isVisible, setIsVisible] = useState(false);
     const [isVisibleText, setIsVisibleText] = useState([]);
+    const [inViewport, setInViewport] = useState(false);
 
     const sections = [
         {
-            name: 'one'
+            section: 'one',
+            label: 'ONE'
         },
         {
-            name: 'two'
+            section: 'two',
+            label: 'TWO'
         },
         {
-            name: 'three'
+            section: 'three',
+            label: 'THREE'
         },
         {
-            name: 'four'
+            section: 'four',
+            label: 'FOUR'
         }
     ];
 
     const options = {
         threshold: 0.1
-    };
-
-    const jumpToSection = (
-        section = ''
-    ) => {
-        // we've been provided a section to jump to
-        // check if it's a valid section
-        let matchedSection;
-        sections.forEach((s) => {
-            if (section === s.name) {
-                matchedSection = s;
-            }
-        });
-
-        if (!matchedSection) {
-            // no matching section
-            return;
-        }
-
-        // find the section in dom
-        const selector = matchedSection.name;
-        const sectionDom = document.querySelector(selector);
-
-        if (!sectionDom) {
-            return;
-        }
-
-        // add offsets
-        scrollToY(sectionDom.offsetTop, 700);
-    };
-
-    const handleJumpToSection = (section) => {
-        jumpToSection(section);
-
-        // add section to url
-        history.replace(`${history.location.pathname}?section=${section.name}`);
-
-        // update the state
-        setActiveSection(section);
     };
 
     const callback = (entries) => {
@@ -88,7 +47,9 @@ const TempSearchPage = () => {
             // setIsVisibleText(entry.target.id);
             if (entry.isIntersecting) {
                 // add section to url
-                history.replace(`${history.location.pathname}?section=${entry.target.id}`);
+                // history.replace(`${history.location.pathname}?section=${entry.target.id}`);
+                // set state var
+                setInViewport(true);
             }
         });
     };
@@ -97,29 +58,22 @@ const TempSearchPage = () => {
         // eslint-disable-next-line no-undef
         const observer = new IntersectionObserver(callback, options);
 
-        sections.forEach((section) => {
-            const target = document.getElementById(`temp-component-${section.name}`);
-            if (target) {
-                observer.observe(target);
-            }
-        });
+        // this is causing the callback fn to fire constantly
+        // sections.forEach((section) => {
+        //     const target = document.getElementById(`temp-component-${section.section}`);
+        //     if (target) {
+        //         observer.observe(target);
+        //     }
+        // });
+        //
+        // return () => observer.disconnect();
 
-        // if (containerRef.current) observer.observe(containerRef.current);
+        if (containerRef.current) observer.observe(containerRef.current);
 
-        // return () => {
-        //     if (containerRef.current) observer.unobserve(containerRef.current);
-        // };
-
-        return () => observer.disconnect();
+        return () => {
+            if (containerRef.current) observer.unobserve(containerRef.current);
+        };
     }, [callback, containerRef, options, sections]);
-
-    useEffect(() => {
-        if (query.section) {
-            handleJumpToSection(query.section);
-        }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [query.section]);
-
 
     return (
         <PageFeatureFlag>
@@ -132,10 +86,13 @@ const TempSearchPage = () => {
                     <Suspense fallback={<TempLoadingComponent />}>
                         <TempComponent1 />
                     </Suspense>
-                    {/* <div>{isVisible ? "IN VIEWPORT" : "NOT IN VIEWPORT"}</div> */}
+                    <div>{isVisible ? "IN VIEWPORT" : "NOT IN VIEWPORT"}</div>
                     <TempComponent2 />
-                    {/* <div ref={containerRef}>Thing being observed</div> */}
-                    <TempComponent3 />
+                    <Suspense fallback={<TempLoadingComponent />}>
+                        <div ref={containerRef}>
+                            <TempComponent3 inViewport={inViewport} />
+                        </div>
+                    </Suspense>
                     <TempComponent4 />
                 </main>
             </PageWrapper>
