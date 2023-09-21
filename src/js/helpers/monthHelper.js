34e56dfbf4b7300d21d0f66a8b572ee204d1b9dd
Nick Torres <nicholas.torres@kc.frb.org>
2023-09-21 10:57:03 -0600

    replace

--- src/js/helpers/monthHelper.js
+++ src/js/helpers/monthHelper.js
@@ -3,15 +3,16 @@
  * Created by michaelbray on 8/23/17.
  */
 
-import moment from "moment";
 import * as FiscalYearHelper from './fiscalYearHelper';
+
+const dayjs = require('dayjs');
 
 // Months are 1-indexed strings, starting with the first month in the fiscal year, which is October
 // We parse the month, using 1 by default if the month is not a number
 // To account for the Fiscal Year beginning in October, we subtract three months
-// As moment months are 0-indexed, we need to actually subtract four months
+// As dayjs months are 0-indexed, we need to actually subtract four months
 const monthConversion = (m) =>
-    moment().month(isNaN(parseInt(m, 10)) ? 1 : parseInt(m, 10)).subtract(4, "months");
+    dayjs().month(isNaN(parseInt(m, 10)) ? 1 : parseInt(m, 10)).subtract(4, "months");
 
 export const convertNumToMonth = (m) => monthConversion(m).format("MMMM");
 
