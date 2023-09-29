dc22b22909b095e2d5b92751c3b39b69b5140470
anjenkin <blackwell_andrea@bah.com>
2023-09-29 18:28:56 -0400

    fixed atd a nd glossary initial animation but broke some other stuff

--- src/js/components/aboutTheDataSidebar/AnimatedAboutTheDataWrapper.jsx
+++ src/js/components/aboutTheDataSidebar/AnimatedAboutTheDataWrapper.jsx
@@ -3,7 +3,7 @@
  * Created by Nick Torres 11/2/22
  */
 
-import React from 'react';
+import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import AboutTheData from "./AboutTheData";
@@ -13,22 +13,24 @@ const propTypes = {
     schema: PropTypes.object
 };
 
-const AnimatedAboutTheDataWrapper = (props) => (
-    <div className="usa-atd-animations">
-        <React.StrictMode>
+const AnimatedAboutTheDataWrapper = (props) => {
+    const ref1 = useRef(null);
+    return (
+        <div className="usa-atd-animations">
             <TransitionGroup>
                 {props?.aboutTheDataSidebar?.display && (
                     <CSSTransition
+                        nodeRef={ref1}
                         classNames="atd-slide"
                         timeout={{ enter: 500, exit: 500 }}
                         exit>
-                        <AboutTheData {...props} />
+                        <div ref={ref1}><AboutTheData {...props} /></div>
                     </CSSTransition>
                 )}
             </TransitionGroup>
-        </React.StrictMode>
-    </div>
-);
+        </div>
+    );
+};
 
 AnimatedAboutTheDataWrapper.propTypes = propTypes;
 export default AnimatedAboutTheDataWrapper;
--- src/js/components/glossary/AnimatedGlossaryWrapper.jsx
+++ src/js/components/glossary/AnimatedGlossaryWrapper.jsx
@@ -3,7 +3,7 @@
  * Created by Kevin Li 4/28/17
  */
 
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import { useSelector } from "react-redux";
 import PropTypes from 'prop-types';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
@@ -17,6 +17,7 @@ const AnimatedGlossaryWrapper = (props) => {
     const [zIndexClass, setZIndexClass] = useState(null);
 
     const { lastOpenedSlideout } = useSelector((state) => state.slideouts);
+    const ref1 = useRef(null);
 
     useEffect(() => {
         setZIndexClass(lastOpenedSlideout === 'glossary' ? 'z-index-plus-one' : 'z-index');
@@ -24,18 +25,17 @@ const AnimatedGlossaryWrapper = (props) => {
 
     return (
         <div className="usa-da-glossary-animations">
-            <React.StrictMode>
-                <TransitionGroup>
-                    {props.glossary.display && (
-                        <CSSTransition
-                            classNames="glossary-slide"
-                            timeout={{ enter: 500, exit: 500 }}
-                            exit>
-                            <Glossary {...props} zIndexClass={zIndexClass} />
-                        </CSSTransition>
-                    )}
-                </TransitionGroup>
-            </React.StrictMode>
+            <TransitionGroup>
+                {props?.glossary?.display && (
+                    <CSSTransition
+                        nodeRef={ref1}
+                        classNames="glossary-slide"
+                        timeout={{ enter: 500, exit: 500 }}
+                        exit>
+                        <div ref={ref1}><Glossary {...props} zIndexClass={zIndexClass} /></div>
+                    </CSSTransition>
+                )}
+            </TransitionGroup>
         </div>
     );
 };
