9b789c945cdeab55fabb859084b5fe71281be506
Nick Torres <nicholas.torres@kc.frb.org>
2023-10-13 14:53:41 -0600

    removed errors in test

---
+++ tests/containers/aboutTheDataSidebar/AboutTheDataSidebarContainer-test.jsx
@@ -0,0 +1,41 @@
+/**
+ * @jest-environment jsdom
+ *
+ * AboutTheDataSidebarContainer-test.jsx
+ * Created by Nick Torres 10/13/2023
+ */
+
+import React from 'react';
+// import { expect } from '@jest/globals';
+// import * as redux from 'react-redux';
+import { mockData } from '../../components/aboutTheDataSidebar/mockAboutTheDataSidebarFn';
+import { render, screen, act } from '../../testResources/test-utils';
+import { AboutTheDataContainer } from "../../../src/js/containers/aboutTheDataSidebar/AboutTheDataContainer";
+
+
+export const mockATDContainerProps = {
+    aboutTheDataSidebar: mockData,
+    showAboutTheData: jest.fn(),
+    setAboutTheDataTerm: jest.fn(),
+    setAboutTheDataTermFromUrl: jest.fn(),
+    clearAboutTheDataTerm: jest.fn()
+};
+describe('About The Data Sidebar Container Test', () => {
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it(`Render ATD Container with appropriate props`, () => {
+        act(() => {
+            render(<AboutTheDataContainer {...mockATDContainerProps} />);
+        });
+        screen.debug();
+        // expect(spy).toHaveBeenCalledWith({
+        //     filter: {
+        //         award_type: 'assistance',
+        //         def_codes: mockDefcParams
+        //     }
+        // });
+    });
+});
+
