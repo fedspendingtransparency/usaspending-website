0a7b8f79a12258082bab37e888d8de262fef50ae
Nick Torres <nicholas.torres@kc.frb.org>
2023-10-16 15:14:39 -0600

    wip

--- tests/containers/aboutTheDataSidebar/AboutTheDataSidebarContainer-test.jsx
+++ tests/containers/aboutTheDataSidebar/AboutTheDataSidebarContainer-test.jsx
@@ -9,28 +9,47 @@ import React from 'react';
 import { mockData } from '../../components/aboutTheDataSidebar/mockAboutTheDataSidebarFn';
 import { render, screen, act } from '../../testResources/test-utils';
 import { AboutTheDataContainer } from "../../../src/js/containers/aboutTheDataSidebar/AboutTheDataContainer";
+import { showAboutTheData, setAboutTheDataTerm } from "../../../src/js/redux/actions/aboutTheDataSidebar/aboutTheDataActions";
+import { setLastOpenedSlideout } from "../../../src/js/redux/actions/slideouts/slideoutActions";
 
-export const mockATDContainerProps = {
-    aboutTheDataSidebar: mockData,
-    showAboutTheData: jest.fn(),
-    setAboutTheDataTerm: jest.fn(),
-    setAboutTheDataTermFromUrl: jest.fn(),
-    clearAboutTheDataTerm: jest.fn()
-};
-
-jest.mock("../../../src/js/components/aboutTheDataSidebar/AboutTheDataDrilldown", () => {
-    const ComponentToMock = () => <div id="atd-drilldown" />;
-    return ComponentToMock;
+beforeEach(() => {
+    jest.clearAllMocks();
 });
 
 describe('About The Data Sidebar Container Test', () => {
     it(`Render ATD Container with appropriate props`, () => {
+        const mockATDContainerProps = {
+            aboutTheDataSidebar: mockData,
+            showAboutTheData: jest.fn(),
+            setAboutTheDataTerm: jest.fn(),
+            setAboutTheDataTermFromUrl: jest.fn(),
+            clearAboutTheDataTerm: jest.fn()
+        };
+        jest.mock("../../../src/js/components/aboutTheDataSidebar/AboutTheDataDrilldown", () => {
+            const ComponentToMock = () => <div id="atd-drilldown" />;
+            return ComponentToMock;
+        });
         act(() => {
             render(<AboutTheDataContainer {...mockATDContainerProps} />);
         });
         const header = screen.queryByText('About the Data');
         expect(header).toBeTruthy();
-        screen.debug();
     });
+
+    // it('set last opened slideout', () => {
+    //     jest.mock("../../../src/js/components/aboutTheDataSidebar/AboutTheDataDrilldown", () => {
+    //         const ComponentToMock = () => <div id="atd-drilldown" />;
+    //         return ComponentToMock;
+    //     });
+    //     act(() => {
+    //         setLastOpenedSlideout('atd');
+    //     });
+    //     screen.debug();
+    // });
+    // it('show atd', () => {
+    //     showAboutTheData();
+    //     screen.debug();
+    // });
+    // it('set atd term from url', () => { });
 });
 
