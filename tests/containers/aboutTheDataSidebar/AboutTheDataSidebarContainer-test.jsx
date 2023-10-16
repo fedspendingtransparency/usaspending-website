0feff96a877c212e6cd1abeacecb61c3aa804e1f
Nick Torres <nicholas.torres@kc.frb.org>
2023-10-16 09:42:56 -0600

    not sure if i should be testing drilldown..

--- tests/containers/aboutTheDataSidebar/AboutTheDataSidebarContainer-test.jsx
+++ tests/containers/aboutTheDataSidebar/AboutTheDataSidebarContainer-test.jsx
@@ -6,12 +6,9 @@
  */
 
 import React from 'react';
-// import { expect } from '@jest/globals';
-// import * as redux from 'react-redux';
 import { mockData } from '../../components/aboutTheDataSidebar/mockAboutTheDataSidebarFn';
 import { render, screen, act } from '../../testResources/test-utils';
 import { AboutTheDataContainer } from "../../../src/js/containers/aboutTheDataSidebar/AboutTheDataContainer";
-
 
 export const mockATDContainerProps = {
     aboutTheDataSidebar: mockData,
@@ -21,19 +18,18 @@ export const mockATDContainerProps = {
     clearAboutTheDataTerm: jest.fn()
 };
 
-jest.mock('schema', () => ({
-    ...jest.requireActual('schema'),
-    ...require('../../../src/js/dataMapping/aboutTheDataSchema')
-}));
-describe('About The Data Sidebar Container Test', () => {
-    afterEach(() => {
-        jest.clearAllMocks();
-    });
+jest.mock("../../../src/js/components/aboutTheDataSidebar/AboutTheDataDrilldown", () => {
+    const ComponentToMock = () => <div id="atd-drilldown" />;
+    return ComponentToMock;
+});
 
+describe('About The Data Sidebar Container Test', () => {
     it(`Render ATD Container with appropriate props`, () => {
         act(() => {
             render(<AboutTheDataContainer {...mockATDContainerProps} />);
         });
+        const header = screen.queryByText('About the Data');
+        expect(header).toBeTruthy();
         screen.debug();
     });
 });
