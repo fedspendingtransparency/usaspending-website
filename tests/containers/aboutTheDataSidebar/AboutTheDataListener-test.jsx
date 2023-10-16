b077645c635adb7be1a6a4bdaa545ab54aa2dd87
Nick Torres <nicholas.torres@kc.frb.org>
2023-10-16 11:13:45 -0600

    wip

---
+++ tests/containers/aboutTheDataSidebar/AboutTheDataListener-test.jsx
@@ -0,0 +1,40 @@
+/**
+ * @jest-environment jsdom
+ *
+ * AboutTheDataSidebarListener-test.jsx
+ * Created by Nick Torres 10/16/2023
+ */
+import React from 'react';
+import { mockData } from '../../components/aboutTheDataSidebar/mockAboutTheDataSidebarFn';
+import { render, screen, act } from '../../testResources/test-utils';
+import { AboutTheDataListenerContainer } from "../../../src/js/containers/aboutTheDataSidebar/AboutTheDataListener";
+
+const mockPush = jest.fn();
+const mockReplace = jest.fn();
+export const mockATDListenerProps = {
+    history: {
+        push: mockPush,
+        replace: mockReplace,
+        location: {
+            search: '?about-the-data=gtas'
+        }
+    },
+    match: {
+        search: "?about-the-data=gtas"
+    },
+    location: {
+        search: '?about-the-data=gtas'
+    },
+    showAboutTheData: jest.fn(),
+    setAboutTheDataTermFromUrl: jest.fn(),
+    setLastOpenedSlideout: jest.fn()
+};
+
+describe('About The Data Sidebar Listener Test', () => {
+    it(`Render ATD Listener with appropriate props`, () => {
+        act(() => {
+            render(<AboutTheDataListenerContainer {...mockATDListenerProps} />);
+        });
+        screen.debug();
+    });
+});
