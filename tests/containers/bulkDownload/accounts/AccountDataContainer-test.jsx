316778f71831975c2b721fb9404105c3a4c16fc4
Josue Aguilar <josue.aguilar@kc.frb.org>
2023-10-23 15:51:35 -0500

    10309 - tests for calls and for a snapshot of child component

--- tests/containers/bulkDownload/accounts/AccountDataContainer-test.jsx
+++ tests/containers/bulkDownload/accounts/AccountDataContainer-test.jsx
@@ -7,13 +7,17 @@
 
 import React from 'react';
 import { mockProps } from "../mockData";
-import { render, screen, act } from '../../../testResources/test-utils';
+import { render, waitFor } from '../../../testResources/test-utils';
+import * as BulkDownloadHelper from '../../../../src/js/helpers/bulkDownloadHelper';
 import { AccountDataContainer } from "../../../../src/js/containers/bulkDownload/accounts/AccountDataContainer";
 import AccountDataContent from "../../../../src/js/components/bulkDownload/accounts/AccountDataContent";
+import { mockApiCall } from "../../../testResources/mockApiHelper";
 
 const mockBulkDownload = mockProps.bulkDownload;
+mockApiCall(BulkDownloadHelper, 'requestAgenciesList', { response: "42" });
 
-jest.mock('../../../../src/js/components/bulkDownload/accounts/AccountDataContent');
+jest.mock('../../../../src/js/components/bulkDownload/accounts/AccountDataContent', () => jest.fn(() => null));
+jest.mock('../../../../src/js/helpers/bulkDownloadHelper');
 
 beforeEach(() => {
     jest.clearAllMocks();
@@ -28,20 +32,52 @@ const mockADCContainerProps = {
 };
 
 describe('AccountDataContainer tests', () => {
-    it('renders ADC with appropriate props', () => {
-        act(() => {
-            render(<AccountDataContainer {...mockADCContainerProps} />);
-        });
-        expect(AccountDataContent).toBeCalled();
-    });
+    it('populates the agency list', () => {
+        const mockAgencyResponse = {
+            promise: new Promise((resolve) => {
+                process.nextTick(() => (
+                    resolve({
+                        data: {
+                            agencies: {
+                                cfo_agencies: 'stuff',
+                                other_agencies: 'other stuff'
+                            }
+                        }
+                    })
+                ));
+            })
+        };
 
-    // it('populates the agency list', () => {
-    //     act(() => {
-    //         render(<AccountDataContainer {...mockADCContainerProps} />);
-    //     });
-    //
-    //     let state =
-    //
-    //     expect(dropdown).toBeTruthy();
-    // });
+        const mockBudgetResponse = {
+            promise: new Promise((resolve) => {
+                process.nextTick(() => {
+                    resolve({
+                        data: {
+                            results: [
+                                {
+                                    budget_function_code: '300',
+                                    budget_function_title: 'Mock Budget Function'
+                                }
+                            ]
+                        }
+                    });
+                });
+            })
+        };
+
+        const spyAgency = jest.spyOn(BulkDownloadHelper, 'requestAgenciesList')
+            .mockReturnValueOnce(mockAgencyResponse);
+        const spyBudget = jest.spyOn(BulkDownloadHelper, 'requestBudgetFunctionList').mockReturnValueOnce(mockBudgetResponse);
+
+        render(<AccountDataContainer {...mockADCContainerProps} />);
+
+
+        return waitFor(() => {
+            expect(spyAgency).toHaveBeenCalledTimes(1);
+            expect(spyBudget).toHaveBeenCalledTimes(1);
+            expect(AccountDataContent).toBeCalledTimes(3);
+            // this api call happens three times, with the successful call only being on the third and final call
+            expect(AccountDataContent).toMatchSnapshot();
+        });
+    });
 });
