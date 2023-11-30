dc38de1f528c87da0aec2d9e96ca3122e61a691f
anjenkin <blackwell_andrea@bah.com>
2023-11-30 07:42:04 -0500

    added the inpage nav to the temp page

--- src/js/components/sharedComponents/PageWrapper.jsx
+++ src/js/components/sharedComponents/PageWrapper.jsx
@@ -10,7 +10,6 @@ import { getStickyBreakPointForSidebar } from 'helpers/stickyHeaderHelper';
 import MetaTags from 'components/sharedComponents/metaTags/MetaTags';
 import Header from 'containers/shared/HeaderContainer';
 import Footer from 'containers/Footer';
-import InPageNav from "../inPageNav/InPageNav";
 
 const PageWrapper = ({
     pageName,
@@ -25,7 +24,8 @@ const PageWrapper = ({
     filters = {},
     sections,
     activeSection,
-    jumpToSection
+    jumpToSection,
+    inPageNav = false
 }) => (
     <div className={classNames} ref={ref}>
         <MetaTags {...metaTagProps} />
@@ -34,14 +34,13 @@ const PageWrapper = ({
             title={title}
             stickyBreakPoint={getStickyBreakPointForSidebar()}
             overLine={overLine}
-            toolBar={toolBarComponents} />
-        {sections && <InPageNav
+            toolBar={toolBarComponents}
+            inPageNav={inPageNav}
             detectActiveSection
             pageName={pageName}
             sections={sections}
             activeSection={activeSection}
-            jumpToSection={jumpToSection} />}
-        </>}
+            jumpToSection={jumpToSection} /></>}
         {React.cloneElement(children, {
             className: `usda-page__container${children.props.className ? ` ${children.props.className}` : ''}`
         })}
