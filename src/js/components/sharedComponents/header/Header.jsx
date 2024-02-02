5b7494c6eaed09112851369e532902dcdfb686f5
brianpetway <brian.petway@kc.frb.org>
2024-02-02 11:09:34 -0600

    10578 - removed infoBanner from Header.jsx

--- src/js/components/sharedComponents/header/Header.jsx
+++ src/js/components/sharedComponents/header/Header.jsx
@@ -44,13 +44,6 @@ export default class Header extends React.Component {
                 <header
                     className="site-header__wrapper"
                     aria-label="Site header">
-                    <InfoBanner
-                        icon={<FontAwesomeIcon style={{ width: "20", height: "20" }} size="lg" icon="info-circle" color="#97d4ea" />}
-                        borderTopColor="#97d4ea"
-                        borderBottomColor="#c3ebfa"
-                        backgroundColor="#e1f3f8"
-                        title={<>New congressional district data available</>}
-                        content={<>USAspending.gov now has new congressional district data as a result of the 2020 census. Districts are identified sitewide as “current” or “submitted” (i.e., original). <AboutTheDataLink slug="congressional-district-data">Learn more about redistricting and the changes you’ll find on the site.</AboutTheDataLink></>} />
                     <GovBanner />
                     <NavbarWrapper />
                 </header>
