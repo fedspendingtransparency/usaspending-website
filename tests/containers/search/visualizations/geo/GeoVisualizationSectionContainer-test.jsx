e8144001f4236439fc5c7c6c2e790e181689a9fc
Josue Aguilar <josue.aguilar@kc.frb.org>
2024-02-05 16:50:21 -0600

    10460 - wip

--- tests/containers/search/visualizations/geo/GeoVisualizationSectionContainer-test.jsx
+++ tests/containers/search/visualizations/geo/GeoVisualizationSectionContainer-test.jsx
@@ -17,6 +17,7 @@ import GeoVisualizationSectionContainer
 import mapBroadcaster from "../../../../../src/js/helpers/mapBroadcaster";
 import * as search from "../../../../../src/js/apis/search";
 import appliedFiltersReducer from "../../../../../src/js/redux/reducers/search/appliedFiltersReducer";
+import SearchAwardsOperation from "../../../../../src/js/models/v1/search/SearchAwardsOperation";
 
 // mocking the child component to stringify the props being passed down.
 // This allows us to search the string for proper props
@@ -120,52 +121,142 @@ describe('GeoVisualizationSectionContainer tests', () => {
         expect(useRefSpy).toHaveBeenCalled();
         expect(consoleSpy).toHaveBeenCalled();
     });
-
-    it('runs compareEntities()', async () => {
-        const mockRe = {
-            appliedFilters: {
-                filters: {
-                    ...mockReduxFilters.filters,
-                    selectedLocations: Set([{
-                        filter: {
-                            state: true
-                        }
-                    }]),
-                    selectedRecipientLocations: Set([])
-                }
-            },
-            searchView: {
-                subAward: false
-            }
-        };
-        const mockStore = createStore(appliedFiltersReducer, mockRe);
-
-        const useRefSpy = jest.spyOn(React, 'useRef').mockReturnValue({ current: { visibleEntities: false } });
-
-        jest.spyOn(search, 'performSpendingByGeographySearch')
-            .mockReturnValue({ promise: Promise.resolve(mockApiRequestResponse[0]) });
-
-        jest.mock('../../../../../src/js/helpers/mapBroadcaster', () => {
-            const EventEmitter = require('events');
-            const emitter = new EventEmitter();
-            emitter.send = jest.fn();
-            emitter.setConfig = jest.fn();
-            return emitter;
-        });
-
-        render((
-            <Provider store={mockStore}><GeoVisualizationSectionContainer {...mockProps} /></Provider>
-        ));
-
-        act(() => {
-            mapBroadcaster.emit('mapMeasureDone', ["WI", "WA", "OR"], false);
-        });
-
-        await waitFor(() => {
-            const testFirst = screen.getByText('"locations":["WA","OR","WI"]', { exact: false });
-            expect(testFirst).toBeTruthy();
-        });
-
-        expect(useRefSpy).toHaveBeenCalled();
-    });
+    //
+    // it('runs fetchData() and mapScopeLogic()', async () => {
+    //     const mockRe = {
+    //         appliedFilters: {
+    //             filters: {
+    //                 ...mockReduxFilters.filters,
+    //                 selectedAwardingAgencies: Set([{ name: 'hellow' }]),
+    //                 selectedFundingAgencies: Set([{ name: 'hellow' }]),
+    //                 selectedLocations: Set([{
+    //                     filter: {
+    //                         state: true
+    //                     }
+    //                 }]),
+    //                 selectedRecipientLocations: Set([])
+    //             }
+    //         },
+    //         searchView: {
+    //             subAward: false
+    //         }
+    //     };
+    //     const mockStore = createStore(appliedFiltersReducer, mockRe);
+    //
+    //     const useRefSpy = jest.spyOn(React, 'useRef').mockReturnValue({ current: { visibleEntities: false } });
+    //
+    //     jest.spyOn(search, 'performSpendingByGeographySearch')
+    //         .mockReturnValue({ promise: Promise.resolve(mockApiRequestResponse[0]) });
+    //
+    //     // jest.spyOn(SearchAwardsOperation, 'toParams').mockReturnValue({
+    //     //     time_period: [
+    //     //         {
+    //     //             start_date: "2023-10-01",
+    //     //             end_date: "2024-09-30"
+    //     //         }
+    //     //     ],
+    //     //     place_of_performance_locations: [
+    //     //         {
+    //     //             state: "WI",
+    //     //             country: "USA"
+    //     //         }
+    //     //     ]
+    //     // });
+    //
+    //     const mockFromState = jest.fn(() => ({
+    //         keyword: [],
+    //         timePeriodType: "fy",
+    //         timePeriodFY: [
+    //             "2024"
+    //         ],
+    //         timePeriodRange: [],
+    //         dateType: false,
+    //         awardType: [],
+    //         awardingAgencies: [],
+    //         fundingAgencies: [],
+    //         tasCheckbox: {
+    //             require: [],
+    //             exclude: []
+    //         },
+    //         tasSources: [],
+    //         selectedRecipients: [],
+    //         recipientDomesticForeign: "all",
+    //         selectedRecipientLocations: [],
+    //         recipientType: [],
+    //         selectedLocations: [
+    //             {
+    //                 filter: {
+    //                     state: "WI",
+    //                     country: "USA"
+    //                 },
+    //                 display: {
+    //                     title: "Wisconsin",
+    //                     entity: "State",
+    //                     standalone: "Wisconsin"
+    //                 },
+    //                 identifier: "USA_WI"
+    //             }
+    //         ],
+    //         locationDomesticForeign: "all",
+    //         awardAmounts: [],
+    //         selectedAwardIDs: [],
+    //         selectedCFDA: [],
+    //         naicsCodes: {
+    //             require: [],
+    //             exclude: []
+    //         },
+    //         pscCheckbox: {
+    //             require: [],
+    //             exclude: []
+    //         },
+    //         defCodes: {
+    //             require: [],
+    //             exclude: []
+    //         },
+    //         pricingType: [],
+    //         setAside: [],
+    //         extentCompeted: []
+    //     }));
+    //     const mockToParams = jest.fn(() => ({
+    //         time_period: [
+    //             {
+    //                 start_date: "2023-10-01",
+    //                 end_date: "2024-09-30"
+    //             }
+    //         ],
+    //         place_of_performance_locations: [
+    //             {
+    //                 state: "WI",
+    //                 country: "USA"
+    //             }
+    //         ]
+    //     }));
+    //     jest.mock('../../../../../src/js/models/v1/search/SearchAwardsOperation', () => jest.fn().mockImplementation(() => ({
+    //         fromState: mockFromState,
+    //         toParams: mockToParams
+    //     })));
+    //
+    //     jest.mock('../../../../../src/js/helpers/mapBroadcaster', () => {
+    //         const EventEmitter = require('events');
+    //         const emitter = new EventEmitter();
+    //         emitter.send = jest.fn();
+    //         emitter.setConfig = jest.fn();
+    //         return emitter;
+    //     });
+    //
+    //     render((
+    //         <Provider store={mockStore}><GeoVisualizationSectionContainer {...mockProps} /></Provider>
+    //     ));
+    //
+    //     act(() => {
+    //         mapBroadcaster.emit('mapMeasureDone', ["WI", "WA", "OR"], false);
+    //     });
+    //
+    //     await waitFor(() => {
+    //         const testFirst = screen.getByText('"locations":["WA","OR","WI"]', { exact: false });
+    //         expect(testFirst).toBeTruthy();
+    //     });
+    //
+    //     expect(useRefSpy).toHaveBeenCalled();
+    // });
 });
