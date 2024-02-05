7d04d3c7cf344c398161359d7525a6b7ab37f5dc
Nick Torres <nicholas.torres@kc.frb.org>
2024-02-05 11:30:46 -0700

    wip

--- src/js/components/sharedComponents/dropdowns/TitleBarFilter.jsx
+++ src/js/components/sharedComponents/dropdowns/TitleBarFilter.jsx
@@ -2,8 +2,9 @@
  * TitleBarFilter.jsx
  * Created by Nick Torres 1/31/2024
  */
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
+import { uniqueId } from "lodash";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const propTypes = {
@@ -11,14 +12,37 @@ const propTypes = {
     label: PropTypes.string,
     buttonText: PropTypes.string,
     leftIcon: PropTypes.element,
-    enabled: PropTypes.bool
+    enabled: PropTypes.bool,
+    sortFn: PropTypes.func,
+    selectedOption: PropTypes.oneOfType([PropTypes.node, PropTypes.string]),
+    className: PropTypes.string,
+    id: PropTypes.string,
+    options: PropTypes.arrayOf(PropTypes.shape({
+        name: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+        value: PropTypes.any,
+        onClick: PropTypes.func,
+        classNames: PropTypes.string
+    }))
+};
+
+const defaultSort = (a, b, selectedOption) => {
+    // if no sort fn is provided, sort active element to lowest index
+    if (a.name === selectedOption) return -1;
+    if (b.name === selectedOption) return 1;
+    // then, sort alphabetically
+    if (a.name < b.name) return -1;
+    if (a.name > b.name) return 1;
+    return 0;
 };
 
 export const TitleBarFilter = ({
-    size, label, buttonText, leftIcon, enabled
+    size, label, buttonText, leftIcon, enabled, sortFn = defaultSort, selectedOption, className, id, options
 }) => {
+    const pickerRef = useRef(null);
+    const buttonRef = useRef(null);
     const [expanded, setExpanded] = useState(false);
     const [isEnabled, setIsEnabled] = useState(enabled || false);
+    const fontAwesomeIconId = "usa-dt-picker__button-icon--svg";
 
     let variation = '';
     if (size === 'sm' || size === 'small') {
@@ -30,10 +54,55 @@ export const TitleBarFilter = ({
     else if (size === 'lg' || size === 'large') {
         variation = '-lg';
     }
+
+    const toggleMenu = (e) => {
+        e.preventDefault();
+        if (!expanded) {
+            setExpanded(!expanded);
+        }
+    };
+
+    const enableButton = (e) => {
+        e.preventDefault();
+        if (!isEnabled) {
+            setIsEnabled(!isEnabled);
+        }
+    };
+
+    const handleSort = (a, b) => sortFn(a, b, selectedOption);
+    useEffect(() => {
+        const closeMenu = (e) => {
+            if ((
+                expanded &&
+                pickerRef.current &&
+                !pickerRef.current.contains(e.target) &&
+                e.target.id !== `${id}-${fontAwesomeIconId}` &&
+                e.target.parentNode.id !== `${id}-${fontAwesomeIconId}`
+            )) {
+                setExpanded(false);
+            }
+        };
+
+        document.addEventListener('click', closeMenu);
+
+        return () => {
+            document.removeEventListener('click', closeMenu);
+        };
+    }, [expanded, id]);
+
+    const createOnClickFn = (cb) => (param) => {
+        cb(param);
+        setExpanded(false);
+    };
+
     return (
         <div className="filter__dropdown-container">
             {label !== '' && <span className={`filter__dropdown-label${variation} ${isEnabled ? 'enabled' : 'not-enabled'}`}>{label}</span>}
-            <button className={`filter__dropdown-button${variation} ${isEnabled ? 'enabled' : 'not-enabled'}`}>
+            <button
+                className={`filter__dropdown-button${variation} ${isEnabled ? 'enabled' : 'not-enabled'}`}
+                ref={buttonRef}
+                aria-label="Filter Dropdown Button"
+                onClick={toggleMenu}>
                 <span className="filter__dropdown-left-icon">
                     <FontAwesomeIcon icon={leftIcon} alt="page title bar button icon" />
                 </span>
@@ -47,6 +116,33 @@ export const TitleBarFilter = ({
                     )}
                 </span>
             </button>
+            <ul className={`filter__dropdown-list ${expanded ? '' : 'hide'}`}>
+                {options
+                    .sort(handleSort)
+                    .map((option) => ({
+                        ...option,
+                        onClick: createOnClickFn(option.onClick)
+                    }))
+                    .map((option) => (
+                        <li key={uniqueId()} className={`usa-dt-picker__list-item ${option?.classNames ? option.classNames : ''}`}>
+                            <button
+                                className={`usa-dt-picker__item ${option.name === selectedOption ? 'active' : ''}`}
+                                value={`${option.value || option.name}`}
+                                onClick={(e) => {
+                                    e.preventDefault();
+                                    option.onClick(option.value);
+                                }}
+                                onKeyDown={(e) => {
+                                    if (option.name === 'reddit' && e.key === 'Tab') {
+                                        setExpanded(!expanded);
+                                    }
+                                }}>
+                                {option.component ? option.component : option.name}
+                            </button>
+                        </li>
+                    ))
+                }
+            </ul>
         </div>
     );
 };
