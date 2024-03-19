1b2fbe7e461abfd4db03d8aa47acd5115aa9e7fc
Josue Aguilar <josue.aguilar@kc.frb.org>
2024-03-19 11:16:07 -0500

    10741 - added initial dropdown

--- src/js/components/tempSearchPage/TempSearchSectionWrapper.jsx
+++ src/js/components/tempSearchPage/TempSearchSectionWrapper.jsx
@@ -1,19 +1,45 @@
-import React from 'react';
+/**
+ * TempSearchSectionWrapper.jsx
+ * Created by Josue Aguilar 3/19/2024
+ */
+
+import React, { useState } from 'react';
 import PropTypes from "prop-types";
+import NewPicker from "../sharedComponents/dropdowns/NewPicker";
 
 const propTypes = {
     sectionTitle: PropTypes.string,
     dropdownOptions: PropTypes.array,
-    selectedDropdownOption: PropTypes.string,
+    selectedDropdownOption: PropTypes.number,
     isVisualization: PropTypes.bool,
     children: PropTypes.string || PropTypes.element,
     dsmContent: PropTypes.string
 };
 
+const onClick = () => {
+    console.log('dropdown');
+};
+
 const defaultProps = {
     sectionTitle: 'Section Title',
-    dropdownOptions: ['Option 0', 'Option 1', 'Option 2'],
-    selectedDropdownOption: 'Option 1',
+    dropdownOptions: [
+        {
+            name: 'Option 0',
+            value: 0,
+            onClick
+        },
+        {
+            name: 'Option 1',
+            value: 1,
+            onClick
+        },
+        {
+            name: 'Option 2',
+            value: 2,
+            onClick
+        }
+    ],
+    selectedDropdownOption: 0,
     isVisualization: true,
     children: 'children',
     dsmContent: 'content'
@@ -27,14 +53,21 @@ const TempSearchSectionWrapper = ({
     children,
     dsmContent
 }) => {
-    const tempText = 'hellow';
+    const [selectedDropdown, setSelectedDropdown] = useState(selectedDropdownOption);
+
+    const sortFn = () => dropdownOptions;
 
     return (
         <div>
-            <div>{tempText}</div>
             <div>{sectionTitle}</div>
-            <div>{dropdownOptions}</div>
-            <div>{selectedDropdownOption}</div>
+            <NewPicker
+                leftIcon=""
+                size="sm"
+                options={dropdownOptions}
+                selectedOption={dropdownOptions.length
+                    ? dropdownOptions.find((obj) => obj.value === selectedDropdown).name
+                    : `${selectedDropdown}`}
+                sortFn={sortFn} />
             <div>{isVisualization}</div>
             <div>{children}</div>
             <div>{dsmContent}</div>
