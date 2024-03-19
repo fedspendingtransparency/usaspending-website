c683376f422e4fd6e55cc6abf532f44fb84e31e4
Josue Aguilar <josue.aguilar@kc.frb.org>
2024-03-19 10:29:58 -0500

    10741 - added props

--- src/js/components/tempSearchPage/TempSearchSectionWrapper.jsx
+++ src/js/components/tempSearchPage/TempSearchSectionWrapper.jsx
@@ -1,11 +1,48 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
-const TempSearchSectionWrapper = () => {
+const propTypes = {
+    sectionTitle: PropTypes.string,
+    dropdownOptions: PropTypes.array,
+    selectedDropdownOption: PropTypes.string,
+    isVisualization: PropTypes.bool,
+    children: PropTypes.string || PropTypes.element,
+    dsmContent: PropTypes.string
+};
+
+const defaultProps = {
+    sectionTitle: 'Section Title',
+    dropdownOptions: ['Option 0', 'Option 1', 'Option 2'],
+    selectedDropdownOption: 'Option 1',
+    isVisualization: true,
+    children: 'children',
+    dsmContent: 'content'
+};
+
+const TempSearchSectionWrapper = ({
+    sectionTitle,
+    dropdownOptions,
+    selectedDropdownOption,
+    isVisualization,
+    children,
+    dsmContent
+}) => {
     const tempText = 'hellow';
 
     return (
-        <div>{tempText}</div>
+        <div>
+            <div>{tempText}</div>
+            <div>{sectionTitle}</div>
+            <div>{dropdownOptions}</div>
+            <div>{selectedDropdownOption}</div>
+            <div>{isVisualization}</div>
+            <div>{children}</div>
+            <div>{dsmContent}</div>
+        </div>
     );
 };
+
+TempSearchSectionWrapper.propTypes = propTypes;
+TempSearchSectionWrapper.defaultProps = defaultProps;
 
 export default TempSearchSectionWrapper;
