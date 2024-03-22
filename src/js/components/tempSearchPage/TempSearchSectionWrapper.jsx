b5817f3b95d0c5688ff7bf0f7474cf17003a2781
Josue Aguilar <josue.aguilar@kc.frb.org>
2024-03-22 13:46:02 -0500

    10741 - test push

--- src/js/components/tempSearchPage/TempSearchSectionWrapper.jsx
+++ src/js/components/tempSearchPage/TempSearchSectionWrapper.jsx
@@ -38,6 +38,8 @@ const TempSearchSectionWrapper = ({
         setViewType(label);
     };
 
+    // this is a test push
+
     return (
         <div className="search-results-wrapper temp-search__section-wrapper">
             <div className="temp-search__section-wrapper-header">
