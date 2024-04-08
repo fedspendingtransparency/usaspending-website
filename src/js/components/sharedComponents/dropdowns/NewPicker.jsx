fe3c91b3f5816d02b444cbf1a79cc2f76092f714
anjenkin <blackwell_andrea@bah.com>
2024-04-08 09:19:48 -0400

    fixing the accessibility error

--- src/js/components/sharedComponents/dropdowns/NewPicker.jsx
+++ src/js/components/sharedComponents/dropdowns/NewPicker.jsx
@@ -154,25 +154,26 @@ const NewPicker = ({
                             onClick: createOnClickFn(option.onClick)
                         }))
                         .map((option) => (
-                            // eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions
                             <li
                                 key={uniqueId()}
-                                className={`filter__dropdown-list-item ${option?.classNames ? option.classNames : ''} ${option.name.trim() === selectedOption.trim() ? 'active' : ''}`}
-                                onClick={(e) => {
-                                    e.preventDefault();
-                                    option.onClick(option.value);
-                                }}
-                                onKeyUp={(e) => {
-                                    e.preventDefault();
-                                    if (e.key === "Enter") {
+                                className={`filter__dropdown-list-item ${option?.classNames ? option.classNames : ''} ${option.name.trim() === selectedOption.trim() ? 'active' : ''}`}>
+                                <a
+                                    style={{ display: "block", width: "100%" }}
+                                    tabIndex={0}
+                                    role="link"
+                                    onClick={(e) => {
+                                        e.preventDefault();
                                         option.onClick(option.value);
-                                    }
-                                }}>
-                                <button
-                                    className="filter__dropdown-item"
-                                    value={`${option.value || option.name}`} >
+                                    }}
+                                    onKeyUp={(e) => {
+                                        e.preventDefault();
+                                        if (e.key === "Enter") {
+                                            option.onClick(option.value);
+                                        }
+                                    }}
+                                    className="filter__dropdown-item">
                                     {option.component ? option.component : option.name}
-                                </button>
+                                </a>
                             </li>
                         ))
                     }
