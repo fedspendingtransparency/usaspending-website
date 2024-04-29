40d6f2141c5461383e0b63d4c78de71ff4819a42
Josue Aguilar <josue.aguilar@kc.frb.org>
2024-04-29 12:11:30 -0500

    10904 - set the filters to last year

--- src/js/components/search/newResultsView/NewSearchScreen.jsx
+++ src/js/components/search/newResultsView/NewSearchScreen.jsx
@@ -4,22 +4,37 @@
  */
 
 import React from 'react';
+import { useDispatch } from "react-redux";
 import Button from "../../sharedComponents/buttons/Button";
+import { updateTimePeriod } from "../../../redux/actions/search/searchFilterActions";
 
-const NewSearchScreen = () => (
-    <div className="new-search-container">
-        <img
-            className="new-search-icon"
-            src="graphics/award-search-default-empty-state.svg"
-            alt="alt-text" />
-        <p>Start your search by adding filters</p>
-        <Button
-            buttonSize="md"
-            copy="See spending from last year"
-            buttonTitle="See spending from last year"
-            buttonType="primary"
-            backgroundColor="light" />
-    </div>
-);
+const NewSearchScreen = () => {
+    const dispatch = useDispatch();
+    const lastYear = new Date().getFullYear() - 1;
+    const timePeriodFilter = {
+        dateType: "fy",
+        fy: new Set([lastYear]),
+        start: null,
+        end: null
+    };
+    const handleOnClick = () => dispatch(updateTimePeriod(timePeriodFilter));
+
+    return (
+        <div className="new-search-container">
+            <img
+                className="new-search-icon"
+                src="graphics/award-search-default-empty-state.svg"
+                alt="alt-text" />
+            <p>Start your search by adding filters</p>
+            <Button
+                buttonSize="md"
+                copy="See spending from last year"
+                buttonTitle="See spending from last year"
+                buttonType="primary"
+                backgroundColor="light"
+                onClick={handleOnClick} />
+        </div>
+    );
+};
 
 export default NewSearchScreen;
