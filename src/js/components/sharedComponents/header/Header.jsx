a39d6cc6db0ce4c7dfedbb57fc57bde129912e99
anjenkin <blackwell_andrea@bah.com>
2024-05-01 14:01:09 -0400

    added banner to covid page only

--- src/js/components/sharedComponents/header/Header.jsx
+++ src/js/components/sharedComponents/header/Header.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import { useLocation } from 'react-router-dom';
+import GlobalConstants from 'GlobalConstants';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import GlossaryContainer from 'containers/glossary/GlossaryContainer';
 import GlobalModalContainer from 'containers/globalModal/GlobalModalContainer';
 import AboutTheDataContainer from "containers/aboutTheDataSidebar/AboutTheDataContainer";
 import NavbarWrapper from './NavbarWrapper';
 import GovBanner from "./GovBanner";
+import InfoBanner from './InfoBanner';
 
-export default class Header extends React.Component {
-    constructor(props) {
-        super(props);
+const Header = () => {
+    const location = useLocation();
 
-        // bind functions
-        this.skippedNav = this.skippedNav.bind(this);
-    }
-
-    skippedNav(e) {
+    console.log(location.pathname);
+    const skippedNav = (e) => {
     // don't update the URL due to potential React Router conflicts
         e.preventDefault();
         // scroll to the main-content id
@@ -27,31 +26,50 @@ export default class Header extends React.Component {
         if (mainFocus) {
             mainFocus.focus();
         }
-    }
+    };
 
-    render() {
-        return (
-            <div className="site-header">
-                <a
-                    href="#main-content"
-                    className="skip-nav"
-                    onClick={this.skippedNav}>
-                        Skip to main content
-                </a>
-                <header
-                    className="site-header__wrapper"
-                    aria-label="Site header">
-                    <GovBanner />
-                    <NavbarWrapper />
-                </header>
-                <AboutTheDataContainer />
-                <GlossaryContainer />
-                <GlobalModalContainer />
-            </div>
-        );
-    }
-}
+    const isBannerActive = () => {
+        if (GlobalConstants.BANNER.isActive) {
+            console.log("HERE", location);
+            if (GlobalConstants.BANNER.page && GlobalConstants.BANNER.page !== "") {
+                return location.pathname === GlobalConstants.BANNER.page;
+            }
+            return true;
+        }
+
+        return false;
+    };
+
+    return (
+        <div className="site-header">
+            <a
+                href="#main-content"
+                className="skip-nav"
+                onClick={skippedNav}>
+                    Skip to main content
+            </a>
+            <header
+                className="site-header__wrapper"
+                aria-label="Site header">
+                <GovBanner />
+                {isBannerActive() &&
+                    <InfoBanner
+                        icon={<FontAwesomeIcon size="lg" icon="exclamation-triangle" color="#FDB81E" />}
+                        borderTopColor="#f9c642"
+                        backgroundColor="#fff1d2"
+                        title={GlobalConstants.BANNER.isActive ? GlobalConstants.BANNER.title : ""}
+                        content={GlobalConstants.BANNER.isActive ? GlobalConstants.BANNER.content : ""} />}
+                <NavbarWrapper />
+            </header>
+            <AboutTheDataContainer />
+            <GlossaryContainer />
+            <GlobalModalContainer />
+        </div>
+    );
+};
 
 Header.propTypes = {
     showModal: PropTypes.func
 };
+
+export default Header;
