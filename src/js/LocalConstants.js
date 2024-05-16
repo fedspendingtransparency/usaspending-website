e23b635a09d19eba52e4ecbab30378fe4aa6c3bd
anjenkin <blackwell_andrea@bah.com>
2024-05-16 14:06:50 -0400

    add new properties

--- src/js/LocalConstants.js
+++ src/js/LocalConstants.js
@@ -5,5 +5,5 @@
 
 export const localConstants = {
     API: null,
-    MAPBOX_TOKEN: null
+    MAPBOX_TOKEN: 'pk.eyJ1IjoidXNhc3BlbmRpbmciLCJhIjoiY2l6ZnZjcmh0MDBtbDMybWt6NDR4cjR6ZSJ9.zsCqjJgrMDOA-i1RcCvGvg',
 };
