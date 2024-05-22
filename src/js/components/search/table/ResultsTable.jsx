6caeb9d2cbf0c3402e7f570d53d1f6001d311af3
Nick Torres <nicholas.torres@kc.frb.org>
2024-05-22 08:24:25 -0600

    diff class name for table

--- src/js/components/search/table/ResultsTable.jsx
+++ src/js/components/search/table/ResultsTable.jsx
@@ -282,7 +282,7 @@ export default class ResultsTable extends React.Component {
             <>
                 <div className="advanced-search__table-wrapper">
                     <Table
-                        classNames="advanced-search__table"
+                        classNames="table-for-new-search-page"
                         stickyFirstColumn
                         columns={cols}
                         rows={limitedRows}
