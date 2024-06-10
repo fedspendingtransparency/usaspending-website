4c394eb0bfec9e35653703b0efd87dacaa0f581a
Nick Torres <nicholas.torres@kc.frb.org>
2024-06-10 12:14:31 -0600

    design review feedback

--- src/js/components/homepageUpdate/FeaturedContent/FeaturedContent.jsx
+++ src/js/components/homepageUpdate/FeaturedContent/FeaturedContent.jsx
@@ -31,14 +31,14 @@ const FeaturedContent = () => {
             </div>
             <div className="featured-content__section--flex-row">
                 <FlexGridCol width={12} desktop={6} tablet={6} mobile={12}>
-                    <a className="featured-content__section--link" href="https://www.youtube.com/watch?v=c-bqfpWSSrI" onClick={trackFeaturedYourDataLink}>
+                    <a className="featured-content__section--link" href="https://www.youtube.com/watch?v=c-bqfpWSSrI" target="_blank" rel="noopener noreferrer" onClick={trackFeaturedYourDataLink}>
                         <CardContainer variant="outline" size="md">
                             <CardHero fill="#59B9DE" variant="expanded" img="img/homepage-featured-content/homepage-feature-Your-Data-Your-Story@2x.webp" />
                             <CardBody
                                 overline="YOUR DATA, YOUR STORY"
                                 headline={
                                     <div>
-                                        Learn how Americans use USAspending in the new "Your Data, Your Story" video series
+                                        Learn how Americans use USAspending in the “Your Data, Your Story” video series
                                     </div>
                                 }>
                             </CardBody>
@@ -53,7 +53,7 @@ const FeaturedContent = () => {
                                 overline="PARTNER SITES"
                                 headline={
                                     <div>
-                                        Explore U.S. Treasury Savings Bonds on our partner site: Fiscal Data
+                                        Explore U.S. Treasury Savings Bonds on Fiscal Data
                                     </div>
                                 }>
                             </CardBody>
