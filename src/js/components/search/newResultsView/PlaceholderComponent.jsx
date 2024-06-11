165fb1b4a3c6093079a825e37b3c31441e843204
brianpetway <brian.petway@kc.frb.org>
2024-06-11 13:14:16 -0500

    11027 - added comment to PlaceholderComponent.jsx

--- src/js/components/search/newResultsView/PlaceholderComponent.jsx
+++ src/js/components/search/newResultsView/PlaceholderComponent.jsx
@@ -3,6 +3,13 @@
  * Created by Brian Petway 06/10/2024
  **/
 
+/**
+    This component is needed in order to make the progressive loading on the
+    new search page function correctly. The text is the same color as the background
+    so it is not visible. The css rules for this component are in resultsView.scss,
+    and the 800px height is arbitrary.
+**/
+
 import React from "react";
 
 const PlaceholderComponent = () => (
