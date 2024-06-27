374cbf20f322102258390806c3112f79c7972737
anjenkin <blackwell_andrea@bah.com>
2024-06-27 15:43:42 -0400

    fix the results by categories table

--- src/js/components/search/newResultsView/categories/CategoriesTable.jsx
+++ src/js/components/search/newResultsView/categories/CategoriesTable.jsx
@@ -3,9 +3,11 @@
  * Created by Andrea Blackwell 05/03/2024
  **/
 
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
 import { Table } from "data-transparency-ui";
+import { mediumScreen } from 'dataMapping/shared/mobileBreakpoints';
+import {throttle} from "lodash";
 import CategoriesPagination from "./CategoriesPagination";
 
 const propTypes = {
@@ -19,22 +21,42 @@ const propTypes = {
     recipientError: PropTypes.bool
 };
 
-const CategoriesTable = (props) => (
-    <>
-        <Table
-            classNames="table-for-new-search-page"
-            columns={props.columns}
-            rows={props.rows} />
-        <CategoriesPagination
-            nextPage={props.nextPage}
-            previousPage={props.previousPage}
-            loading={props.loading}
-            error={props.error}
-            hasNextPage={props.hasNextPage}
-            hasPreviousPage={props.hasPreviousPage}
-            recipientError={props.recipientError} />
-    </>
-);
+const CategoriesTable = (props) => {
+    const [windowWidth, setWindowWidth] = useState(0);
+    const [isMobile, setIsMobile] = useState(window.innerWidth < mediumScreen);
+
+    useEffect(() => {
+        const handleResize = throttle(() => {
+            const newWidth = window.innerWidth;
+            if (windowWidth !== newWidth) {
+                setWindowWidth(newWidth);
+                setIsMobile(newWidth < mediumScreen);
+                console.log(newWidth < mediumScreen);
+            }
+        }, 50);
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    return (
+        <>
+            <Table
+                classNames="table-for-new-search-page"
+                columns={props.columns}
+                rows={props.rows}
+                isStacked
+                isMobile={isMobile} />
+            <CategoriesPagination
+                nextPage={props.nextPage}
+                previousPage={props.previousPage}
+                loading={props.loading}
+                error={props.error}
+                hasNextPage={props.hasNextPage}
+                hasPreviousPage={props.hasPreviousPage}
+                recipientError={props.recipientError}/>
+        </>
+    );
+};
 
 CategoriesTable.propTypes = propTypes;
 export default CategoriesTable;
