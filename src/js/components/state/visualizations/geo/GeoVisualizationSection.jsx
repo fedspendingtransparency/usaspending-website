c188cfa58bd83f4a07a7fc75d87d6514e87eb78c
Nick Torres <nicholas.torres@kc.frb.org>
2024-07-02 13:34:37 -0600

    convert to functional

--- src/js/components/state/visualizations/geo/GeoVisualizationSection.jsx
+++ src/js/components/state/visualizations/geo/GeoVisualizationSection.jsx
@@ -3,7 +3,7 @@
  * Created by Lizzie Salita 5/14/18
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import MapboxGL from 'mapbox-gl/dist/mapbox-gl';
 
@@ -33,123 +33,109 @@ const propTypes = {
 
 const availableLayers = ['county', 'congressionalDistrict'];
 
-export default class GeoVisualizationSection extends React.Component {
-    constructor(props) {
-        super(props);
+const GeoVisualizationSection = (props) => {
+    const [showHover, setShowHover] = useState(false);
+    const [selectedItem, setSelectedItem] = useState({});
 
-        this.state = {
-            showHover: false,
-            selectedItem: {}
-        };
-
-        this.showTooltip = this.showTooltip.bind(this);
-        this.hideTooltip = this.hideTooltip.bind(this);
-    }
-
-    showTooltip(geoId, position) {
+    const showTooltip = (geoId, position) => {
     // convert state code to full string name
-        const label = this.props.data.labels[geoId];
-        this.setState({
-            showHover: true,
-            selectedItem: {
-                label: label.label,
-                total: this.props.total,
-                value: label.value,
-                x: position.x,
-                y: position.y
-            }
-        });
-    }
+        const label = props.data.labels[geoId];
+        setShowHover(true);
+        setSelectedItem(Object.assign(selectedItem, {
+            label: label.label,
+            total: props.total,
+            value: label.value,
+            x: position.x,
+            y: position.y
+        }));
+    };
 
-    hideTooltip() {
-        this.setState({
-            showHover: false,
-            selectedItem: {}
-        });
-    }
+    const hideTooltip = () => {
+        setShowHover(false);
+        setSelectedItem(Object.assign(selectedItem, {}));
+    };
 
-    render() {
-        let message = null;
+    let message = null;
 
-        if (!MapboxGL.supported()) {
-            return (
-                <div className="results-table-message-container">
-                    <ResultsTableErrorMessage title="WebGL Required for this map." description="Please enable WebGL in your browser settings to view this map visualization." />
-                </div>
-            );
-        }
-        else if (this.props.loading) {
-            message = (
-                <MapMessage>
-                    <div className="map-loading">
-                        <LoadingSpinner />
-                        <div className="loading-message">
-                            Gathering your data...
-                        </div>
-                    </div>
-                </MapMessage>
-            );
-        }
-        else if (this.props.error || this.props.stateCenter.length === 0) {
-            message = (
-                <MapMessage>
-                    <div className="map-no-results">
-                        <div className="error-icon">
-                            <ExclamationTriangle alt="An error occurred" />
-                        </div>
-                        <div className="title">
-                            An error occurred.
-                        </div>
-                        <div className="description">
-                            Something went wrong while gathering your data.
-                        </div>
-                    </div>
-                </MapMessage>
-            );
-        }
-        else if (this.props.noResults) {
-            message = (
-                <MapMessage>
-                    <div className="map-no-results">
-                        <div className="no-results-icon" />
-                        <div className="title">
-                            No results found in the current map area.
-                        </div>
-                    </div>
-                </MapMessage>
-            );
-        }
-
-        let center = this.props.stateCenter;
-        if (this.props.stateCenter.length === 0) {
-            // If there was an error getting the center of the state, use the center
-            // of the United States so that we don't generate a MapBox error
-            center = [-95.569430, 38.852892];
-        }
-
+    if (!MapboxGL.supported()) {
         return (
-            <div className="geo__map-section">
-                <MapWrapper
-                    className={this.props.className}
-                    data={this.props.data}
-                    renderHash={this.props.renderHash}
-                    scope={this.props.mapLayer}
-                    changeMapLayer={this.props.changeMapLayer}
-                    showHover={this.state.showHover}
-                    selectedItem={this.state.selectedItem}
-                    showTooltip={this.showTooltip}
-                    hideTooltip={this.hideTooltip}
-                    tooltip={GeoVisualizationTooltip}
-                    availableLayers={availableLayers}
-                    showLayerToggle
-                    center={center}
-                    stateInfo={this.props.stateInfo}
-                    stateProfile>
-                    {message}
-                </MapWrapper>
+            <div className="results-table-message-container">
+                <ResultsTableErrorMessage title="WebGL Required for this map." description="Please enable WebGL in your browser settings to view this map visualization." />
             </div>
         );
     }
-}
+    else if (props.loading) {
+        message = (
+            <MapMessage>
+                <div className="map-loading">
+                    <LoadingSpinner />
+                    <div className="loading-message">
+                            Gathering your data...
+                    </div>
+                </div>
+            </MapMessage>
+        );
+    }
+    else if (props.error || props.stateCenter.length === 0) {
+        message = (
+            <MapMessage>
+                <div className="map-no-results">
+                    <div className="error-icon">
+                        <ExclamationTriangle alt="An error occurred" />
+                    </div>
+                    <div className="title">
+                            An error occurred.
+                    </div>
+                    <div className="description">
+                            Something went wrong while gathering your data.
+                    </div>
+                </div>
+            </MapMessage>
+        );
+    }
+    else if (props.noResults) {
+        message = (
+            <MapMessage>
+                <div className="map-no-results">
+                    <div className="no-results-icon" />
+                    <div className="title">
+                            No results found in the current map area.
+                    </div>
+                </div>
+            </MapMessage>
+        );
+    }
+
+    let center = props.stateCenter;
+    if (props.stateCenter.length === 0) {
+        // If there was an error getting the center of the state, use the center
+        // of the United States so that we don't generate a MapBox error
+        center = [-95.569430, 38.852892];
+    }
+
+    return (
+        <div className="geo__map-section">
+            <MapWrapper
+                className={props.className}
+                data={props.data}
+                renderHash={props.renderHash}
+                scope={props.mapLayer}
+                changeMapLayer={props.changeMapLayer}
+                showHover={showHover}
+                selectedItem={selectedItem}
+                showTooltip={showTooltip}
+                hideTooltip={hideTooltip}
+                tooltip={GeoVisualizationTooltip}
+                availableLayers={availableLayers}
+                showLayerToggle
+                center={center}
+                stateInfo={props.stateInfo}
+                stateProfile>
+                {message}
+            </MapWrapper>
+        </div>
+    );
+};
 
 GeoVisualizationSection.propTypes = propTypes;
+export default GeoVisualizationSection;
