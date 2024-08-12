49a027e4d7c81ef9f1f078341bf2b8ef5f58480b
brianpetway <brian.petway@kc.frb.org>
2024-08-12 13:37:45 -0500

    11308 - wip; added file for tempData

---
+++ src/js/components/tempNAICSData.js
@@ -0,0 +1,122 @@
+export const tempData = [
+    {
+        naics: "11",
+        naics_description: "Agriculture, Forestry, Fishing and Hunting",
+        count: 64
+    },
+    {
+        naics: "21",
+        naics_description: "Mining, Quarrying, and Oil and Gas Extraction",
+        count: 32
+    },
+    {
+        naics: "22",
+        naics_description: "Utilities",
+        count: 15
+    },
+    {
+        naics: "23",
+        naics_description: "Construction",
+        count: 31
+    },
+    {
+        naics: "31",
+        naics_description: "Manufacturing",
+        count: 133
+    },
+    {
+        naics: "32",
+        naics_description: "Manufacturing",
+        count: 136
+    },
+    {
+        naics: "33",
+        naics_description: "Manufacturing",
+        count: 269
+    },
+    {
+        naics: "42",
+        naics_description: "Wholesale Trade",
+        count: 71
+    },
+    {
+        naics: "44",
+        naics_description: "Retail Trade",
+        count: 50
+    },
+    {
+        naics: "45",
+        naics_description: "Retail Trade",
+        count: 33
+    },
+    {
+        naics: "48",
+        naics_description: "Transportation and Warehousing",
+        count: 50
+    },
+    {
+        naics: "49",
+        naics_description: "Transportation and Warehousing",
+        count: 7
+    },
+    {
+        naics: "51",
+        naics_description: "Information",
+        count: 43
+    },
+    {
+        naics: "52",
+        naics_description: "Finance and Insurance",
+        count: 42
+    },
+    {
+        naics: "53",
+        naics_description: "Real Estate and Rental and Leasing",
+        count: 29
+    },
+    {
+        naics: "54",
+        naics_description: "Professional, Scientific, and Technical Services",
+        count: 52
+    },
+    {
+        naics: "55",
+        naics_description: "Management of Companies and Enterprises",
+        count: 3
+    },
+    {
+        naics: "56",
+        naics_description: "Administrative and Support and Waste Management and Remediation Services",
+        count: 45
+    },
+    {
+        naics: "61",
+        naics_description: "Educational Services",
+        count: 17
+    },
+    {
+        naics: "62",
+        naics_description: "Health Care and Social Assistance",
+        count: 39
+    },
+    {
+        naics: "71",
+        naics_description: "Arts, Entertainment, and Recreation",
+        count: 25
+    },
+    {
+        naics: "72",
+        naics_description: "Accommodation and Food Services",
+        count: 19
+    },
+    {
+        naics: "81",
+        naics_description: "Other Services (except Public Administration)",
+        count: 49
+    },
+    {
+        naics: "92",
+        naics_description: "Public Administration",
+        count: 29
+    }
+];
