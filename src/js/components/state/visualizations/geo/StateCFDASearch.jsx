6a49fc47ed6d5b64d3f98e64656652170406d9b3
Nick Torres <nicholas.torres@kc.frb.org>
2024-08-13 13:47:21 -0600

    some styling

--- src/js/components/state/visualizations/geo/StateCFDASearch.jsx
+++ src/js/components/state/visualizations/geo/StateCFDASearch.jsx
@@ -5,10 +5,7 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-
 import StateCFDAListContainer from "./StateCFDAListContainer";
-import SubmitHint from '../../../sharedComponents/filterSidebar/SubmitHint';
-import SelectedCFDA from "../../../search/filters/cfda/SelectedCFDA";
 
 const propTypes = {
     selectCFDA: PropTypes.func,
@@ -27,22 +24,10 @@ export default class StateCFDASearch extends React.Component {
     }
 
     render() {
-        let selectedCFDA = null;
-        if (this.props.selectedCFDA.size > 0) {
-            selectedCFDA = (<SelectedCFDA
-                selectedCFDA={this.props.selectedCFDA}
-                removeCFDA={this.props.removeCFDA} />);
-        }
-
         return (
             <div className="cfda-filter">
                 <div className="filter-item-wrap">
                     <StateCFDAListContainer {...this.props} selectCFDA={this.props.selectCFDA} />
-                    {selectedCFDA}
-                    <SubmitHint
-                        ref={(component) => {
-                            this.hint = component;
-                        }} />
                 </div>
             </div>
         );
