00054cebf4482b6a7dcb2eef1bad2ea182badfe3
Josue Aguilar <josue.aguilar@kc.frb.org>
2024-08-15 14:17:25 -0500

    11306 - initial data parse

--- src/js/containers/search/filters/location/NewLocationSectionContainer.jsx
+++ src/js/containers/search/filters/location/NewLocationSectionContainer.jsx
@@ -1,25 +1,39 @@
 /**
  * NewLocationSectionContainer.jsx
- * Created by Kevin Li 8/15/2024
+ * Created by Josue Aguilar 8/15/2024
  */
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { fetchLocations } from 'helpers/searchHelper';
 
 const NewLocationSectionContainer = () => {
-    const data = {
-        name: 'Two Door Cinema Club'
+    const [cityList, setCityList] = useState(['KANSAS CITY']);
+    const [countyList, setCountyList] = useState(['JACKSON COUNTY']);
+
+    const parseLocations = (data) => {
+        const citiesList = [];
+        const countiesList = [];
+        data.cities.map((item, index) => {
+            citiesList.push(`${index + 1}. ${item.city_name}, ${item.state_name} `);
+            setCityList(citiesList);
+        });
+
+        data.counties.map((item, index) => {
+            countiesList.push(`${index + 1}. ${item.county_name}, ${item.state_name} `);
+            setCountyList(countiesList);
+        });
     };
 
     const queryAutocompleteLocations = (input = 'den') => {
         const locationSearchParams = {
             search_text: input,
-            limit: 10
+            limit: 2
         };
 
         fetchLocations(locationSearchParams).promise
             .then((res) => {
-                console.log('res: ', res);
+                console.log(res.data.results);
+                parseLocations(res.data.results);
             })
             .catch((err) => {
                 console.log('error: ', err);
@@ -28,12 +42,21 @@ const NewLocationSectionContainer = () => {
 
     useEffect(() => {
         queryAutocompleteLocations();
-    }, [queryAutocompleteLocations]);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
     return (
-        <div>
-            {data.name}
-        </div>
+        <>
+            <h5>County:</h5>
+            <div>
+                {countyList}
+            </div>
+            <h5>City:</h5>
+            <div>
+                {cityList}
+            </div>
+        </>
+
     );
 };
 
