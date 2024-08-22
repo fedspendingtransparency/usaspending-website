acb5673e07b826233b7a207add255ee1ef47a2e7
Nick Torres <nicholas.torres@kc.frb.org>
2024-08-22 16:14:57 -0600

    remove consoles

--- src/js/components/state/visualizations/geo/StateAgencyListContainer.jsx
+++ src/js/components/state/visualizations/geo/StateAgencyListContainer.jsx
@@ -253,7 +253,6 @@ export default class StateAgencyListContainer extends React.Component {
             this.apiRequest = SearchHelper.performSpendingByGeographySearch(newSearch);
             this.apiRequest.promise
                 .then((res) => {
-                    console.debug("Res: ", res);
                     this.parseData(res.data);
                     this.apiRequest = null;
                 })
@@ -268,8 +267,6 @@ export default class StateAgencyListContainer extends React.Component {
                         });
                     }
                 });
-
-            console.debug("after query: ", this.state, this.props);
         });
     }
     parseData(data) {
