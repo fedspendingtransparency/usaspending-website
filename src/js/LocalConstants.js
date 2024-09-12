f90de8e91e2be2b028d4118f093abc017790bd71
anjenkin <blackwell_andrea@bah.com>
2024-09-12 10:04:17 -0400

    Changed the top five view in award search to title casing and fixed console error

--- src/js/LocalConstants.js
+++ src/js/LocalConstants.js
@@ -2,7 +2,8 @@
  * LocalConstants.js
  * Created by Andrea Blackwell 01/11/2024
  */
+
 export const localConstants = {
     API: null,
-    MAPBOX_TOKEN: null
+    MAPBOX_TOKEN: 'pk.eyJ1IjoidXNhc3BlbmRpbmciLCJhIjoiY2l6ZnZjcmh0MDBtbDMybWt6NDR4cjR6ZSJ9.zsCqjJgrMDOA-i1RcCvGvg'
 };
