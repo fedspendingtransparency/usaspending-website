5103a29a34d098fc4c2fbafaa951791b338d89f3
brianpetway <brian.petway@kc.frb.org>
2024-10-09 13:32:36 -0500

    11271 - removed some commented out lines; uncommented others, that need to stay

--- src/js/components/search/table/ResultsTable.jsx
+++ src/js/components/search/table/ResultsTable.jsx
@@ -55,7 +55,7 @@ export default class ResultsTable extends React.Component {
         this.prepareDTUIRows = this.prepareDTUIRows.bind(this);
         this.prepareTable = this.prepareTable.bind(this);
         this.measureHeight = this.measureHeight.bind(this);
-        // this.checkToAddRightFade = this.checkToAddRightFade.bind(this);
+        this.checkToAddRightFade = this.checkToAddRightFade.bind(this);
         this.clickHandler = this.clickHandler.bind(this);
     }
 
@@ -464,18 +464,18 @@ export default class ResultsTable extends React.Component {
         return values;
     }
 
-    // checkToAddRightFade(isScrolledLeft, isScrolledRight) {
-    //     if (!isScrolledLeft && !this.props.isMobile) {
-    //         this.setState({
-    //             activateRightFade: true
-    //         });
-    //     }
-    //     if (isScrolledRight || this.props.isMobile) {
-    //         this.setState({
-    //             activateRightFade: false
-    //         });
-    //     }
-    // }
+    checkToAddRightFade(isScrolledLeft, isScrolledRight) {
+        if (!isScrolledLeft && !this.props.isMobile) {
+            this.setState({
+                activateRightFade: true
+            });
+        }
+        if (isScrolledRight || this.props.isMobile) {
+            this.setState({
+                activateRightFade: false
+            });
+        }
+    }
 
     render() {
         const cols = this.prepareDTUIColumns();
@@ -490,13 +490,11 @@ export default class ResultsTable extends React.Component {
                 <div
                     className={`advanced-search__table-wrapper ${this.state.activateRightFade ? 'activate-right-fade' : ''} `}
                     id="advanced-search__table-wrapper"
-                    // style={this.state.tableHeight > this.state.windowHeight ? { height: this.state.windowHeight - stickyHeaderHeight - 16 - 40 - 57 } : null}>
-
                     style={{ height: this.state.windowHeight - stickyHeaderHeight - 16 - 40 - 57 }}>
                     <Table
                         classNames="table-for-new-search-page award-results-table-dtui"
                         stickyFirstColumn={!this.props.isMobile}
-                        // checkToAddRightFade={this.checkToAddRightFade}
+                        checkToAddRightFade={this.checkToAddRightFade}
                         columns={cols}
                         rows={limitedRows}
                         rowHeight={this.props.isMobile ? null : 58}
