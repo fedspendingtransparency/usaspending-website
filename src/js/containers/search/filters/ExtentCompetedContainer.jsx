b9201bf45f42900c04700eed81d94f5d8daec57a
Josue Aguilar <josue.aguilar@kc.frb.org>
2024-10-11 13:23:31 -0500

    11546 - removed feature flag

--- src/js/containers/search/filters/ExtentCompetedContainer.jsx
+++ src/js/containers/search/filters/ExtentCompetedContainer.jsx
@@ -7,36 +7,21 @@ import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { is } from 'immutable';
-import { QAT } from 'GlobalConstants';
 
 import * as searchFilterActions from 'redux/actions/search/searchFilterActions';
-import ContractFilter from 'components/search/filters/contractFilters/ContractFilter';
 import ListCheckbox from 'components/sharedComponents/checkbox/ListCheckbox';
 import { extentCompetedDefinitions, extentCompetedTypeMapping } from 'dataMapping/search/contractFields';
 import { EntityDropdownAutocomplete } from "../../../components/search/filters/location/EntityDropdownAutocomplete";
 
 const propTypes = {
     updateExtentCompeted: PropTypes.func,
-    extentCompeted: PropTypes.object,
-    appliedEC: PropTypes.object
+    extentCompeted: PropTypes.object
 };
 
-const ExtentCompetedContainer = ({ updateExtentCompeted, extentCompeted, appliedEC }) => {
+const ExtentCompetedContainer = ({ updateExtentCompeted, extentCompeted }) => {
     const [searchString, setSearchString] = useState('');
     const [filterCategoryMapping, setFilterCategoryMapping] = useState(extentCompetedTypeMapping);
     const [noResults, setNoResults] = useState(false);
-
-    const selectExtentCompeted = (value) => {
-        updateExtentCompeted(value);
-    };
-
-    const dirtyFilters = () => {
-        if (is(extentCompeted, appliedEC)) {
-            return null;
-        }
-        return Symbol('dirty extent competed');
-    };
 
     const handleTextInputChange = (e) => {
         setSearchString(e.target.value);
@@ -86,22 +71,15 @@ const ExtentCompetedContainer = ({ updateExtentCompeted, extentCompeted, applied
                 loading={false}
                 isClearable
                 onClear={onClear} />
-            {QAT ?
+            {noResults ?
+                <div className="no-results">No Results</div>
+                :
                 <ListCheckbox
                     filterCategoryMapping={filterCategoryMapping}
                     filters={extentCompetedDefinitions}
                     selectedFilters={extentCompeted}
                     singleFilterChange={updateExtentCompeted} />
-                :
-                <ContractFilter
-                    extentCompeted={extentCompeted}
-                    dirtyFilters={dirtyFilters()}
-                    contractFilterType="extent_competed"
-                    contractFilterOptions="extentCompetedDefinitions"
-                    contractFilterState="extentCompeted"
-                    toggleFilter={selectExtentCompeted} />
             }
-            {noResults && <div className="no-results">No Results</div>}
         </div>
     );
 };
