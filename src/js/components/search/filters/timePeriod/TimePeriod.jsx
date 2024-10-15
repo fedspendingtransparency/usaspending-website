f91c3a468b43964038748ba726278a5908c967b9
Nick Torres <nicholas.torres@kc.frb.org>
2024-10-15 13:16:04 -0600

    change default

--- src/js/components/search/filters/timePeriod/TimePeriod.jsx
+++ src/js/components/search/filters/timePeriod/TimePeriod.jsx
@@ -64,7 +64,7 @@ export default class TimePeriod extends React.Component {
             selectedFY: new Set(),
             allFY: false,
             clearHint: false,
-            activeTab: 'fy',
+            activeTab: 'dr',
             dateRangeChipRemoved: false
         };
 
