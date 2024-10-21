4ea05a491d5f656f0b8a34113dfa7e699042a411
Josue Aguilar <josue.aguilar@kc.frb.org>
2024-10-21 10:34:25 -0500

    11538 - new table mapping

---
+++ src/js/dataMapping/award/transactionHistoryTable/tableMapping.js
@@ -0,0 +1,150 @@
+const tableMapping = {
+    idv: [
+        {
+            columnWidth: 300,
+            displayName: 'Modification Number',
+            right: false,
+            title: 'modification_number'
+        },
+        {
+            columnWidth: 300,
+            displayName: 'Action Date',
+            right: false,
+            title: 'action_date'
+        },
+        {
+            columnWidth: 300,
+            displayName: 'Amount',
+            right: false,
+            title: 'federal_action_obligation'
+        },
+        {
+            columnWidth: 300,
+            displayName: 'Action Type',
+            right: false,
+            title: 'action_type'
+        },
+        {
+            columnWidth: 300,
+            displayName: 'Transaction Description',
+            right: false,
+            title: 'description'
+        }
+    ],
+    loan: [
+        {
+            columnWidth: 300,
+            displayName: 'Modification Number',
+            right: false,
+            title: 'modification_number'
+        },
+        {
+            columnWidth: 300,
+            displayName: 'CFDA Number',
+            right: false,
+            title: 'cfda_number'
+        },
+        {
+            columnWidth: 300,
+            displayName: 'Action Date',
+            right: false,
+            title: 'action_date'
+        },
+        {
+            columnWidth: 300,
+            displayName: 'Loan Face Value',
+            right: false,
+            title: 'face_value_loan_guarantee'
+        },
+        {
+            columnWidth: 300,
+            displayName: 'Loan Subsidy Cost (Total Obligations To Date)',
+            right: false,
+            title: 'original_loan_subsidy_cost'
+        },
+        {
+            columnWidth: 300,
+            displayName: 'Action Type',
+            right: false,
+            title: 'action_type'
+        },
+        {
+            columnWidth: 300,
+            displayName: 'Transaction Description',
+            right: false,
+            title: 'description'
+        }
+    ],
+    contract: [
+        {
+            columnWidth: 300,
+            displayName: 'Modification Number',
+            right: false,
+            title: 'modification_number'
+        },
+        {
+            columnWidth: 300,
+            displayName: 'Action Date',
+            right: false,
+            title: 'action_date'
+        },
+        {
+            columnWidth: 300,
+            displayName: 'Amount',
+            right: false,
+            title: 'federal_action_obligation'
+        },
+        {
+            columnWidth: 300,
+            displayName: 'Action Type',
+            right: false,
+            title: 'action_type'
+        },
+        {
+            columnWidth: 300,
+            displayName: 'Transaction Description',
+            right: false,
+            title: 'description'
+        }
+    ],
+    assistance: [
+        {
+            columnWidth: 300,
+            displayName: 'Modification Number',
+            right: false,
+            title: 'modification_number'
+        },
+        {
+            columnWidth: 300,
+            displayName: 'Assistance Listing',
+            right: false,
+            title: 'cfda_number'
+        },
+        {
+            columnWidth: 300,
+            displayName: 'Action Date',
+            right: false,
+            title: 'action_date'
+        },
+        {
+            columnWidth: 300,
+            displayName: 'Amount',
+            right: false,
+            title: 'federal_action_obligation'
+        },
+        {
+            columnWidth: 300,
+            displayName: 'Action Type',
+            right: false,
+            title: 'action_type'
+        },
+        {
+            columnWidth: 300,
+            displayName: 'Transaction Description',
+            right: false,
+            title: 'description'
+        }
+    ]
+};
+
+export default tableMapping;
