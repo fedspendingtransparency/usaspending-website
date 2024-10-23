192af96cab5297347b4c66cd7b06d9ca24be5beb
anjenkin <blackwell_andrea@bah.com>
2024-10-23 08:44:11 -0400

    fixed linting error

--- src/js/components/search/filters/location/LocationAutocomplete.jsx
+++ src/js/components/search/filters/location/LocationAutocomplete.jsx
@@ -14,7 +14,7 @@ const propTypes = {
     readyToStage: PropTypes.bool,
     addLocation: PropTypes.func,
     selectedLocations: PropTypes.object,
-    selectedRecipientLocations:  PropTypes.object,
+    selectedRecipientLocations: PropTypes.object,
     removeLocation: PropTypes.func
 };
 
