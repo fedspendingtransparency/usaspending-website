15f978cdf2fae84db6ed6d2efcc4ef6c7cf45417
Josue Aguilar <josue.aguilar@kc.frb.org>
2024-10-24 09:59:49 -0500

    11344 - converted to functional

--- src/js/containers/recipient/topFive/TopFiveContainer.jsx
+++ src/js/containers/recipient/topFive/TopFiveContainer.jsx
@@ -3,7 +3,7 @@
  * Created by Kwadwo Opoku-Debrah 07/10/18
  */
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { isCancel } from 'axios';
@@ -18,51 +18,34 @@ import * as SearchHelper from 'helpers/searchHelper';
 import BaseStateCategoryResult from 'models/v2/state/BaseStateCategoryResult';
 import TopFive from 'components/sharedComponents/TopFive';
 
-export class TopFiveContainer extends React.Component {
-    static propTypes = {
-        total: PropTypes.number,
-        category: PropTypes.string,
-        fy: PropTypes.string,
-        recipientHash: PropTypes.string,
-        recipientName: PropTypes.string
-    };
+const propTypes = {
+    total: PropTypes.number,
+    category: PropTypes.string,
+    fy: PropTypes.string,
+    recipientHash: PropTypes.string,
+    recipientName: PropTypes.string
+};
 
-    constructor(props) {
-        super(props);
+const TopFiveContainer = ({
+    total, category, fy, recipientHash, recipientName
+}) => {
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+    const [results, setResults] = useState([]);
+    const [noResults, setNoResults] = useState(false);
 
-        this.state = {
-            loading: true,
-            error: false,
-            results: [],
-            noResultState: false
-        };
+    let request = null;
 
-        this.request = null;
-    }
-
-    componentDidMount() {
-        this.loadCategory();
-    }
-
-    componentDidUpdate(prevProps) {
-        if (prevProps.recipientHash !== this.props.recipientHash) {
-            this.loadCategory();
-        }
-        if (prevProps.fy !== this.props.fy) {
-            this.loadCategory();
-        }
-    }
-
-    dataParams() {
+    const dataParams = () => {
         let timePeriod = null;
-        if (this.props.fy === 'latest') {
+        if (fy === 'latest') {
             const trailing = getTrailingTwelveMonths();
             timePeriod = {
                 start_date: trailing[0],
                 end_date: trailing[1]
             };
         }
-        else if (this.props.fy === 'all') {
+        else if (fy === 'all') {
             const currentFY = currentFiscalYear();
             const earliestRange = convertFYToDateRange(parseInt(earliestFiscalYear, 10));
             const latestRange = convertFYToDateRange(parseInt(currentFY, 10));
@@ -71,8 +54,8 @@ export class TopFiveContainer extends React.Component {
                 end_date: latestRange[1]
             };
         }
-        else if (this.props.fy !== 'all' && this.props.fy) {
-            const range = convertFYToDateRange(parseInt(this.props.fy, 10));
+        else if (fy !== 'all' && fy) {
+            const range = convertFYToDateRange(parseInt(fy, 10));
             timePeriod = {
                 start_date: range[0],
                 end_date: range[1]
@@ -80,8 +63,8 @@ export class TopFiveContainer extends React.Component {
         }
 
         const filters = {
-            recipient_id: this.props.recipientHash,
-            recipient_name: this.props.recipientName
+            recipient_id: recipientHash,
+            recipient_name: recipientName
         };
 
         if (timePeriod) {
@@ -90,44 +73,17 @@ export class TopFiveContainer extends React.Component {
 
         return {
             filters,
-            category: this.props.category,
+            category,
             limit: 5,
             page: 1
         };
-    }
+    };
 
-    loadCategory() {
-        if (this.request) {
-            this.request.cancel();
-        }
-
-        this.setState({
-            loading: true,
-            error: false
-        });
-
-        this.request = SearchHelper.performSpendingByCategorySearch(this.dataParams());
-        this.request.promise
-            .then((res) => {
-                this.parseResults(res.data.results, res.data.category);
-            })
-            .catch((err) => {
-                if (!isCancel(err)) {
-                    console.error(err);
-                    this.setState({
-                        loading: false,
-                        error: true
-                    });
-                }
-            });
-    }
-
-    parseResults(data, type) {
+    const parseResults = (data, type) => {
         if (data.length < 1) {
-            this.setState({
-                noResultState: true
-            });
-        } else {
+            setNoResults(true);
+        }
+        else {
             const parsed = data.map((item, index) => {
                 const result = Object.create(BaseStateCategoryResult);
                 result.populate(item, index + 1);
@@ -145,28 +101,61 @@ export class TopFiveContainer extends React.Component {
                 }
                 return result;
             });
-            this.setState({
-                loading: false,
-                error: false,
-                results: parsed
-            });
-        }
-    }
 
-    render() {
-        return (
-            <>
-                {!this.state.noResultState &&
-                    <TopFive
-                        category={this.props.category}
-                        total={this.props.total}
-                        dataParams={this.dataParams()}
-                        {...this.state} />
+            setLoading(false);
+            setError(false);
+            setResults(parsed);
+        }
+    };
+
+    const loadCategory = () => {
+        if (request) {
+            request.cancel();
+        }
+
+        setLoading(true);
+        setError(false);
+
+        request = SearchHelper.performSpendingByCategorySearch(dataParams());
+        request.promise
+            .then((res) => {
+                parseResults(res.data.results, res.data.category);
+            })
+            .catch((err) => {
+                if (!isCancel(err)) {
+                    console.error(err);
+                    setLoading(false);
+                    setError(true);
                 }
-            </>
-        );
-    }
-}
+            });
+    };
+
+    useEffect(() => {
+        loadCategory();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    useEffect(() => {
+        loadCategory();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [recipientHash, fy]);
+
+    return (
+        <>
+            {!noResults &&
+                <TopFive
+                    category={category}
+                    total={total}
+                    dataParams={dataParams()}
+                    loading={loading}
+                    error={error}
+                    results={results} />
+            }
+        </>
+    );
+};
+
+TopFiveContainer.propTypes = propTypes;
 
 
 export default connect(
