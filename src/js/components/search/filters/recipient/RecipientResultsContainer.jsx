72752797ccb57f9d8801a2819dd0f137861efa8a
brianpetway <brian.petway@kc.frb.org>
2024-11-07 16:26:30 -0600

    11619 - wip; rearranged some things to put the input box back in the container; and put the .map back there too; renamed the common component again; made a params object for the autocomplete api call, it's working correctly now

--- src/js/components/search/filters/recipient/RecipientResultsContainer.jsx
+++ src/js/components/search/filters/recipient/RecipientResultsContainer.jsx
@@ -7,7 +7,8 @@ import React, { useEffect, useState } from 'react';
 import PropTypes from "prop-types";
 import * as SearchHelper from 'helpers/searchHelper';
 import StackedCheckbox from "../../../sharedComponents/checkbox/StackedCheckbox";
-import { fetchRecipientsAutocomplete } from "../../../../helpers/searchHelper";
+import PrimaryStackedCheckbox from "../../../sharedComponents/checkbox/PrimaryStackedCheckbox";
+import { EntityDropdownAutocomplete } from "../location/EntityDropdownAutocomplete";
 
 const propTypes = {
     selectedRecipients: PropTypes.object,
@@ -16,6 +17,8 @@ const propTypes = {
 
 const RecipientResultsContainer = ({ toggleRecipient }) => {
     const [recipients, setRecipients] = useState([]);
+    const [searchString, setSearchString] = useState('');
+
     let recipientRequest;
 
     const levelMapping = {
@@ -37,12 +40,17 @@ const RecipientResultsContainer = ({ toggleRecipient }) => {
             });
     };
 
-    const getRecipientsFromSearchString = (searchString) => {
+    const getRecipientsFromSearchString = (term) => {
         if (recipientRequest) {
             recipientRequest.cancel();
         }
 
-        recipientRequest = SearchHelper.fetchRecipientsAutocomplete(searchString);
+        const paramObj = {
+            search_text: term,
+            limit: 50
+        };
+
+        recipientRequest = SearchHelper.fetchRecipientsAutocomplete(paramObj);
 
         recipientRequest.promise
             .then((res) => {
@@ -51,28 +59,57 @@ const RecipientResultsContainer = ({ toggleRecipient }) => {
             });
     };
 
+    const handleTextInputChange = (e) => {
+        if (e.target.value.length >= 3) {
+            setSearchString(e.target.value);
+        }
+    };
+
+    useEffect(() => {
+        getRecipientsFromSearchString(searchString);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [searchString]);
+
     useEffect(() => {
         getAllRecipients();
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
+    // if you end up using two common components here
+    // you'll need to send the recipients array to the SecondaryCheckbox cmpnt
+    // and the toggle fn prop
+    // and the labels for the UEi and DUNS fields
+    // and the itemLabelAfterName, which will require a loop through the data, ugh
+    // or we ask the backend to change the field for us, to the string we'll use
+
+    console.log('recipients', recipients);
+
     return (
-        <div className="recipient-results__container">
-            <div className="checkbox-type-filter">
-                { recipients.toSorted((a, b) => (a.name?.toUpperCase() < b.name?.toUpperCase() ? -1 : 1)).map((recipient, index) => (
-                    <StackedCheckbox
-                        index={index}
-                        checkboxLabel="UEI: "
-                        checkboxLabelContent={recipient.uei ? recipient.uei : 'Not provided'}
-                        subheadingLabel="Legacy DUNS: "
-                        subheadingLabelContent={recipient.duns ? recipient.duns : 'Not provided'}
-                        itemName={recipient.name}
-                        itemLabelAfterName={levelMapping[recipient.recipient_level]}
-                        toggleRecipient={toggleRecipient}
-                        getRecipientsFromSearchString={getRecipientsFromSearchString} />
-                ))}
+        <>
+            <EntityDropdownAutocomplete
+                placeholder="Search filters..."
+                enabled
+                handleTextInputChange={handleTextInputChange}
+                context={{}}
+                loading={false}
+                searchIcon />
+            <div className="recipient-results__container">
+                <div className="checkbox-type-filter">
+                    { recipients.toSorted((a, b) => (a.name?.toUpperCase() < b.name?.toUpperCase() ? -1 : 1)).map((recipient, index) => (
+                        <PrimaryStackedCheckbox
+                            index={index}
+                            checkboxLabel="UEI: "
+                            checkboxLabelContent={recipient.uei ? recipient.uei : 'Not provided'}
+                            subheadingLabel="Legacy DUNS: "
+                            subheadingLabelContent={recipient.duns ? recipient.duns : 'Not provided'}
+                            itemName={recipient.name || recipient.recipient_name}
+                            itemLabelAfterName={levelMapping[recipient.recipient_level]}
+                            toggleRecipient={toggleRecipient}
+                            getRecipientsFromSearchString={getRecipientsFromSearchString} />
+                    ))}
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 
--- src/js/components/sharedComponents/checkbox/StackedCheckbox.jsx
+++ src/js/components/sharedComponents/checkbox/StackedCheckbox.jsx
@@ -6,32 +6,15 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from "prop-types";
 
-import PrimaryCheckboxType from 'components/sharedComponents/checkbox/PrimaryCheckboxType';
 import { EntityDropdownAutocomplete } from "../../search/filters/location/EntityDropdownAutocomplete";
+// import PrimaryStackedCheckbox from "./PrimaryStackedCheckbox";
 
 const propTypes = {
-    checkboxLabel: PropTypes.string,
-    checkboxLabelContent: PropTypes.string,
-    subheadingLabel: PropTypes.string,
-    subheadingLabelContent: PropTypes.string,
-    itemName: PropTypes.string,
-    itemLabelAfterName: PropTypes.string,
-    index: PropTypes.number,
-    toggleRecipient: PropTypes.func,
+    dataArray: PropTypes.array,
     getRecipientsFromSearchString: PropTypes.func
 };
 
-const StackedCheckbox = ({
-    checkboxLabel,
-    checkboxLabelContent,
-    subheadingLabel,
-    subheadingLabelContent,
-    itemName,
-    itemLabelAfterName,
-    index,
-    toggleRecipient,
-    getRecipientsFromSearchString
-}) => {
+const StackedCheckbox = ({ dataArray, getRecipientsFromSearchString }) => {
     const [searchString, setSearchString] = useState('');
 
     const handleTextInputChange = (e) => {
@@ -45,6 +28,19 @@ const StackedCheckbox = ({
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [searchString]);
 
+    // const itemList = dataArray.toSorted((a, b) => (a.name?.toUpperCase() < b.name?.toUpperCase() ? -1 : 1)).map((item, index) => (
+    //     <PrimaryStackedCheckbox
+    //         index={index}
+    //         checkboxLabel="UEI: "
+    //         checkboxLabelContent={item.uei ? item.uei : 'Not provided'}
+    //         subheadingLabel="Legacy DUNS: "
+    //         subheadingLabelContent={item.duns ? item.duns : 'Not provided'}
+    //         itemName={item.name}
+    //         itemLabelAfterName={levelMapping[recipient.recipient_level]}
+    //         toggleRecipient={toggleRecipient}
+    //         getRecipientsFromSearchString={getRecipientsFromSearchString} />
+    // ));
+
     return (
         <div className="stacked-checkbox__container">
             <EntityDropdownAutocomplete
@@ -54,23 +50,9 @@ const StackedCheckbox = ({
                 context={{}}
                 loading={false}
                 searchIcon />
-            <PrimaryCheckboxType
-                name={(
-                    <div className="stacked-checkbox__checkbox-label">
-                        <span>{checkboxLabel}</span>{checkboxLabelContent}
-                    </div>)}
-                value={`primary-checkbox-${index}`}
-                key={checkboxLabelContent}
-                toggleCheckboxType={toggleRecipient} />
-            <div className="stacked-checkbox__lower-container">
-                <div
-                    className="stacked-checkbox__subheading-label">{subheadingLabel} {subheadingLabelContent}
-                </div>
-                <div className="stacked-checkbox__name-container">
-                    <span className="stacked-checkbox__item-name">{itemName}</span>
-                    <span className="stacked-checkbox__item-label-after-name">{itemLabelAfterName}</span>
-                </div>
-            </div>
+
+            {/* {itemList}*/}
+
         </div>
     );
 };
