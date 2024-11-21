80178f227aea906e78c4c9ea86eabb73c316afb2
Josue Aguilar <josue.aguilar@kc.frb.org>
2024-11-21 14:21:44 -0600

    11726 - removed unnecessary code

--- src/js/containers/search/filters/recipient/RecipientSearchContainer.jsx
+++ src/js/containers/search/filters/recipient/RecipientSearchContainer.jsx
@@ -3,38 +3,44 @@
  * Created by michaelbray on 2/16/17.
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { is } from 'immutable';
 import * as searchFilterActions from 'redux/actions/search/searchFilterActions';
-import RecipientSearch from 'components/search/filters/recipient/RecipientSearch';
+import RecipientResultsContainer from "../../../../components/search/filters/recipient/RecipientResultsContainer";
+import SubmitHint from "../../../../components/sharedComponents/filterSidebar/SubmitHint";
 
 const propTypes = {
     updateSelectedRecipients: PropTypes.func,
-    selectedRecipients: PropTypes.object,
-    appliedRecipients: PropTypes.object
+    selectedRecipients: PropTypes.object
 };
 
-const RecipientSearchContainer = (props) => {
-    const toggleRecipient = (recipient) => {
-        props.updateSelectedRecipients(recipient);
-    };
-
-    const dirtyFilters = () => {
-        if (is(props.selectedRecipients, props.appliedRecipients)) {
-            return null;
-        }
-        return Symbol('dirty recipients');
-    };
+const RecipientSearchContainer = ({ updateSelectedRecipients, selectedRecipients }) => {
+    const [newSearch, setNewSearch] = useState(true);
 
     return (
-        <RecipientSearch
-            {...props}
-            dirtyFilters={dirtyFilters}
-            toggleRecipient={toggleRecipient}
-            updateSelectedRecipients={props.updateSelectedRecipients} />
+        <div className="recipient-filter">
+            <div className="filter-item-wrap">
+                <RecipientResultsContainer
+                    selectedRecipients={selectedRecipients}
+                    updateSelectedRecipients={updateSelectedRecipients}
+                    newSearch={newSearch}
+                    setNewSearch={setNewSearch} />
+                <SubmitHint selectedFilters={selectedRecipients} />
+                {newSearch &&
+                    <>
+                        <div className="find-recipients-text label">
+                            Use the search bar to find recipients
+                        </div>
+                        <div className="find-recipients-text content">
+                            The first 100 recipients are displayed by default. Please use the search bar to find
+                            additional recipients.
+                        </div>
+                    </>
+                }
+            </div>
+        </div>
     );
 };
 
@@ -42,8 +48,7 @@ RecipientSearchContainer.propTypes = propTypes;
 
 export default connect(
     (state) => ({
-        selectedRecipients: state.filters.selectedRecipients,
-        appliedRecipients: state.appliedFilters.filters.selectedRecipients
+        selectedRecipients: state.filters.selectedRecipients
     }),
     (dispatch) => bindActionCreators(searchFilterActions, dispatch)
 )(RecipientSearchContainer);
