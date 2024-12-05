809c8b3e90a3fd81042e2dee92515ada656a6778
Josue Aguilar <josue.aguilar@kc.frb.org>
2024-12-05 10:59:37 -0600

    11754 - refactored DatePicker.jsx to not have defaultProps. Removed unnecessary props.

--- src/js/components/bulkDownload/awards/filters/dateRange/DownloadDateRange.jsx
+++ src/js/components/bulkDownload/awards/filters/dateRange/DownloadDateRange.jsx
@@ -7,8 +7,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import DatePicker from 'components/sharedComponents/DatePicker';
 
-const dayjs = require('dayjs');
-
 const defaultProps = {
     startDate: '01/01/2016',
     endDate: '12/31/2016'
@@ -44,44 +42,7 @@ export default class DownloadDateRange extends React.Component {
         }
     }
 
-    generateStartDateDisabledDays() {
-        const disabledDays = [];
-
-        if (this.props.endDate) {
-            // Cutoff date represents the latest possible date
-            // We only want users to be able to download 1 year's worth of data at a time,
-            // So we set the start date a year before the end date
-            // This requires adding a day after subtracting a year
-            disabledDays.push({
-                after: this.props.endDate.toDate(),
-                before: dayjs(this.props.endDate).subtract(1, 'y').add(1, 'd').toDate()
-            });
-        }
-
-        return disabledDays;
-    }
-
-    generateEndDateDisabledDays() {
-        const disabledDays = [];
-
-        if (this.props.startDate) {
-            // Cutoff date represents the earliest possible date, based on the start date
-            // We only want users to be able to download 1 year's worth of data at a time,
-            // So we set the end date a year after the start date
-            // This requires subtracting a day after adding a year
-            disabledDays.push({
-                before: this.props.startDate.toDate(),
-                after: dayjs(this.props.startDate).add(1, 'y').subtract(1, 'd').toDate()
-            });
-        }
-
-        return disabledDays;
-    }
-
     render() {
-        const startDateDisabledDays = this.generateStartDateDisabledDays();
-        const endDateDisabledDays = this.generateEndDateDisabledDays();
-
         return (
             <div className="date-pickers">
                 <DatePicker
@@ -93,11 +54,9 @@ export default class DownloadDateRange extends React.Component {
                     opposite={this.props.endDate}
                     showError={this.props.showError}
                     hideError={this.props.hideError}
-                    disabledDays={startDateDisabledDays}
                     ref={(component) => {
                         this.startPicker = component;
-                    }}
-                    allowClearing />
+                    }} />
                 <DatePicker
                     type="endDateBulk"
                     title="End Date"
@@ -107,11 +66,9 @@ export default class DownloadDateRange extends React.Component {
                     opposite={this.props.startDate}
                     showError={this.props.showError}
                     hideError={this.props.hideError}
-                    disabledDays={endDateDisabledDays}
                     ref={(component) => {
                         this.endPicker = component;
-                    }}
-                    allowClearing />
+                    }} />
             </div>
         );
     }
