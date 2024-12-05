d77d732930f99cdd4a3ca8b5f80f568f500027ac
Josue Aguilar <josue.aguilar@kc.frb.org>
2024-12-05 14:04:44 -0600

    11754 - refactored DatePicker.jsx to functional and to not have defaultProps

--- src/js/components/bulkDownload/awards/filters/dateRange/DownloadDateRange.jsx
+++ src/js/components/bulkDownload/awards/filters/dateRange/DownloadDateRange.jsx
@@ -7,11 +7,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import DatePicker from 'components/sharedComponents/DatePicker';
 
-const defaultProps = {
-    startDate: '01/01/2016',
-    endDate: '12/31/2016'
-};
-
 const propTypes = {
     onDateChange: PropTypes.func,
     startDate: PropTypes.object,
@@ -21,58 +16,36 @@ const propTypes = {
     startingTab: PropTypes.number
 };
 
-export default class DownloadDateRange extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            showError: false,
-            header: '',
-            errorMessage: ''
-        };
-    }
+const DownloadDateRange = ({
+    onDateChange,
+    startDate = '01/01/2016',
+    endDate = '12/31/2016',
+    showError,
+    hideError,
+    startingTab
+}) => (
+    <div className="date-pickers">
+        <DatePicker
+            type="startDateBulk"
+            title="Start Date"
+            tabIndex={startingTab}
+            onDateChange={onDateChange}
+            value={startDate}
+            opposite={endDate}
+            showError={showError}
+            hideError={hideError} />
+        <DatePicker
+            type="endDateBulk"
+            title="End Date"
+            tabIndex={startingTab + 4}
+            onDateChange={onDateChange}
+            value={endDate}
+            opposite={startDate}
+            showError={showError}
+            hideError={hideError} />
+    </div>
+);
 
-    componentDidUpdate(prevProps) {
-        if (prevProps.startDate !== this.props.startDate && !this.props.startDate) {
-            // the start date was reset to null, clear the picker
-            this.startPicker.clearValue();
-        }
-        if (prevProps.endDate !== this.props.endDate && !this.props.endDate) {
-            // the end date was reset to null, clear the picker
-            this.endPicker.clearValue();
-        }
-    }
-
-    render() {
-        return (
-            <div className="date-pickers">
-                <DatePicker
-                    type="startDateBulk"
-                    title="Start Date"
-                    tabIndex={this.props.startingTab}
-                    onDateChange={this.props.onDateChange}
-                    value={this.props.startDate}
-                    opposite={this.props.endDate}
-                    showError={this.props.showError}
-                    hideError={this.props.hideError}
-                    ref={(component) => {
-                        this.startPicker = component;
-                    }} />
-                <DatePicker
-                    type="endDateBulk"
-                    title="End Date"
-                    tabIndex={this.props.startingTab + 4}
-                    onDateChange={this.props.onDateChange}
-                    value={this.props.endDate}
-                    opposite={this.props.startDate}
-                    showError={this.props.showError}
-                    hideError={this.props.hideError}
-                    ref={(component) => {
-                        this.endPicker = component;
-                    }} />
-            </div>
-        );
-    }
-}
-
-DownloadDateRange.defaultProps = defaultProps;
 DownloadDateRange.propTypes = propTypes;
+
+export default DownloadDateRange;
