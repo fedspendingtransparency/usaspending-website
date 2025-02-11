54355fc0f215dffa44aba31c3226489f07d79147
Josue Aguilar <josue.aguilar@kc.frb.org>
2025-02-11 16:00:11 -0600

    11870 - got it to not propagate again

--- src/js/components/search/filters/otherFilters/ShownValue.jsx
+++ src/js/components/search/filters/otherFilters/ShownValue.jsx
@@ -13,36 +13,38 @@ const propTypes = {
     label: PropTypes.string
 };
 
-const ShownValue = ({ removeValue, label }) => (
-    <>
-        {/* eslint-disable jsx-a11y/no-noninteractive-tabindex */}
-        <div
-            className="shown-filter-button"
-            value={label}
-            aria-label={`Applied filter: ${label}`}
-            tabIndex={0} >
-            {label}
+const ShownValue = ({ removeValue, label }) => {
+    const keyDownHandler = (e) => {
+        e.stopPropagation();
+    };
+
+    return (
+        <>
+            {/* eslint-disable jsx-a11y/no-noninteractive-tabindex */}
+            {/* eslint-disable-next-line jsx-a11y/no-static-element-interactions */}
             <div
-                title="Click to remove filter">
-                <Button
-                    onClick={removeValue}
-                    onKeyUp={() => {
-                        console.log('here');
-                        removeValue();
-                    }}
-                    buttonSize="sm"
-                    buttonType="icon"
-                    backgroundColor="light"
-                    buttonTitle="close"
-                    copy="Click to remove filter."
-                    additionalClassnames="shown-filter-button__shown-filter-button-icon"
-                    tabIndex={0}
-                    image={<FontAwesomeIcon icon="times" style={{ cursor: "pointer" }} tabIndex={-1} />} />
+                className="shown-filter-button"
+                value={label}
+                aria-label={`Applied filter: ${label}`}
+                tabIndex={0}
+                onKeyDown={keyDownHandler} >
+                {label}
+                <div title="Click to remove filter">
+                    <Button
+                        onClick={removeValue}
+                        buttonSize="sm"
+                        buttonType="icon"
+                        backgroundColor="light"
+                        buttonTitle="close"
+                        copy="Click to remove filter."
+                        additionalClassnames="shown-filter-button__shown-filter-button-icon"
+                        image={<FontAwesomeIcon icon="times" style={{ cursor: "pointer" }} />} />
+                </div>
             </div>
-        </div>
-        {/* eslint-enable jsx-a11y/no-noninteractive-tabindex */}
-    </>
-);
+            {/* eslint-enable jsx-a11y/no-noninteractive-tabindex */}
+        </>
+    );
+};
 
 ShownValue.propTypes = propTypes;
 export default ShownValue;
