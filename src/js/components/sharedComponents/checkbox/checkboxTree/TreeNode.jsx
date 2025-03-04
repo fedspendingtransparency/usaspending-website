8c9716da0eb994462b0f772f560f78b1be369487
Andrea Blackwell <andrea.blackwell@kc.frb.org>
2025-03-04 14:23:12 -0500

    wip

--- src/js/components/sharedComponents/checkbox/checkboxTree/TreeNode.jsx
+++ src/js/components/sharedComponents/checkbox/checkboxTree/TreeNode.jsx
@@ -7,20 +7,30 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 const TreeNode = (props) => {
-    const { label, children, disabled, onChecked, onExpand, nodes } = props;
+    const {
+        label, children, disabled, onChecked, onExpand, nodes
+    } = props;
 
     const [isExpanded, setIsExpanded] = useState(false);
     const [loading, setLoading] = useState(false);
     const [childNodes, setChildNodes] = useState([]);
 
+    useEffect(() => {
+        console.log("loaded");
+    }, [nodes]);
+
     const handleToggle = async () => {
         const selectedNode = nodes.find((node) => node.description === label);
+
+        setChildNodes((prevState) => prevState.push(selectedNode.value));
+
+        const tempChildNodes = [...childNodes].push(selectedNode.id);
 
         if (!isExpanded) {
             setLoading(true);
             setIsExpanded(true);
             if (onExpand) {
-                onExpand([selectedNode.value], selectedNode);
+                onExpand(tempChildNodes, selectedNode);
             }
         }
     };
@@ -43,13 +53,16 @@ const TreeNode = (props) => {
             </div>
             {isExpanded && (
                 <div>
-                    {childNodes.length > 0 ? childNodes.map((childLabel) => (
+                    {props?.childNodes?.length > 0 ? props?.childNodes?.map((childLabel) => (
                         <TreeNode
                             key={childLabel}
                             label={childLabel}
-                            disabled={disabled}
                             onExpand={onExpand}
-                            onChecked={onChecked} />))
+                            onChecked={onChecked}
+                            disabled={disabled}
+                            checked={props.checked}
+                            expanded={props.expanded}
+                            icons={props.icons} />))
                         : !loading && <div>No child nodes found.</div>}
                 </div>
             )}
