a86b0b5f5fc5f83561bd31f949d38a41ac4b56b7
Andrea Blackwell <andrea.blackwell@kc.frb.org>
2025-04-09 16:38:18 -0400

    add banner

--- src/js/GlobalConstants.js
+++ src/js/GlobalConstants.js
@@ -4,6 +4,8 @@
 */
 
 const local = require('./LocalConstants');
+const ExternalLink = require("./components/sharedComponents/ExternalLink");
+const React = require("react");
 
 const filesServerUrlByEnv = {
     sandbox: 'https://files-nonprod.usaspending.gov',
@@ -23,37 +25,7 @@ const globalConstants = {
     FILES_SERVER_BASE_URL: filesServerUrlByEnv[process.env.ENV],
     ARP_RELEASED: process.env.ENV !== 'prod',
     DUNS_LABEL: 'Legacy ',
-    REQUEST_VERSION: '2020-06-01',
-    BANNER: [
-        {
-            isActive: true,
-            title: 'General',
-            content: 'This is a general notice.',
-            page: '/temp-page', // use 'site wide' to display a banner across the entire site
-            type: 'warning' // three options "general", "warning", "warning-resolved"
-        },
-        {
-            isActive: true,
-            title: 'Warning',
-            content: 'This is a warning notice.',
-            page: '/temp-page', // use 'site wide' to display a banner across the entire site
-            type: 'general' // three options "general", "warning", "warning-resolved"
-        },
-        {
-            isActive: true,
-            title: 'Resolved',
-            content: 'This is a warning resolved notice',
-            page: '/temp-page', // use 'site wide' to display a banner across the entire site
-            type: 'warning-resolved' // three options "general", "warning", "warning-resolved"
-        },
-        {
-            isActive: false,
-            title: 'Site Wide Notice',
-            content: 'This is a notice across the entire site',
-            page: 'site wide', // use 'site wide' to display a banner across the entire site
-            type: 'warning-resolved' // three options "general", "warning", "warning-resolved"
-        }
-    ]
+    REQUEST_VERSION: '2020-06-01'
 };
 
 module.exports = globalConstants;
--- src/js/components/sharedComponents/header/Header.jsx
+++ src/js/components/sharedComponents/header/Header.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
-import GlobalConstants from 'GlobalConstants';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import GlossaryContainer from 'containers/glossary/GlossaryContainer';
 import GlobalModalContainer from 'containers/globalModal/GlobalModalContainer';
 import AboutTheDataContainer from "containers/aboutTheDataSidebar/AboutTheDataContainer";
+import { bannerContent } from 'ActiveBanners';
+
 import NavbarWrapper from './NavbarWrapper';
 import GovBanner from "./GovBanner";
 import InfoBanner from './InfoBanner';
@@ -12,10 +13,12 @@ import InfoBanner from './InfoBanner';
 const Header = () => {
     const location = useLocation();
 
-    const siteBannersArray = GlobalConstants?.BANNER?.filter(
+    console.log(bannerContent);
+
+    const siteBannersArray = bannerContent?.filter(
         (banner) => banner.isActive && banner.page === "site wide"
     );
-    const pageBannersArray = GlobalConstants?.BANNER?.filter(
+    const pageBannersArray = bannerContent?.filter(
         (banner) => banner.isActive && location.pathname.includes(banner.page)
     );
 
