286244b16ae8952b79c72bd45a16c57fd4293db0
House <J1JDH03@rb.win.frb.org>
2025-04-24 14:52:53 -0500

    add FeatureFlag

--- src/js/components/sharedComponents/buttons/AwardTypeToggle.jsx
+++ src/js/components/sharedComponents/buttons/AwardTypeToggle.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import PropTypes from "prop-types";
 import { Button } from 'data-transparency-ui';
 import { Check } from 'components/sharedComponents/icons/Icons';
+import FeatureFlag from '../FeatureFlag';
 
 
 const propTypes = {
@@ -26,28 +27,30 @@ const AwardTypeToggle = ({
 
 
     return (
-        <div className="award-type-toggle" >
-            <Button
-                onClick={() => onToggleClick(nonGroup)}
-                buttonSize="sm"
-                buttonType={selected === nonGroup ? "primaryIcon" : "secondary"}
-                backgroundColor="light"
-                imageAlignment="left"
-                buttonTitle={selected !== "transactions" ? "Subawards Only" : "Transactions Only"}
-                copy={selected !== "transactions" ? "Subawards Only" : "Transactions Only"}
-                additionalClassnames={selected === "awards" ? "borderless" : ""}
-                image={<Check alt="check" />} />
-            <Button
-                onClick={() => onToggleClick('awards')}
-                buttonSize="sm"
-                buttonType={selected === 'awards' ? "primaryIcon" : 'secondary'}
-                backgroundColor="light"
-                imageAlignment="left"
-                buttonTitle="Grouped by Prime Awards"
-                copy="Grouped by Prime Awards"
-                additionalClassnames={selected === "awards" ? "" : "borderless"}
-                image={<Check alt="check" />} />
-        </div>
+        <FeatureFlag>
+            <div className="award-type-toggle" >
+                <Button
+                    onClick={() => onToggleClick(nonGroup)}
+                    buttonSize="sm"
+                    buttonType={selected === nonGroup ? "primaryIcon" : "secondary"}
+                    backgroundColor="light"
+                    imageAlignment="left"
+                    buttonTitle={selected !== "transactions" ? "Subawards Only" : "Transactions Only"}
+                    copy={selected !== "transactions" ? "Subawards Only" : "Transactions Only"}
+                    additionalClassnames={selected === "awards" ? "borderless" : ""}
+                    image={<Check alt="check" />} />
+                <Button
+                    onClick={() => onToggleClick('awards')}
+                    buttonSize="sm"
+                    buttonType={selected === 'awards' ? "primaryIcon" : 'secondary'}
+                    backgroundColor="light"
+                    imageAlignment="left"
+                    buttonTitle="Grouped by Prime Awards"
+                    copy="Grouped by Prime Awards"
+                    additionalClassnames={selected === "awards" ? "" : "borderless"}
+                    image={<Check alt="check" />} />
+            </div>
+        </FeatureFlag>
     );
 };
 
