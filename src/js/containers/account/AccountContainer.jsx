1d83e384943f9b4794a09fa48c76a5d95d7e8452
Nick Torres <nicholas.torres@kc.frb.org>
2025-04-30 08:31:56 -0600

    go back

--- src/js/containers/account/AccountContainer.jsx
+++ src/js/containers/account/AccountContainer.jsx
@@ -98,7 +98,7 @@ const AccountContainer = (props) => {
         const account = new FederalAccount(data);
         props.setSelectedAccount(account);
         if (props.latestPeriod.year) {
-            loadFiscalYearSnapshot(props.account.id);
+            loadFiscalYearSnapshot(account.id);
         }
     };
 
