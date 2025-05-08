713cfe9f590d63b08b312ea1e0e8809abb81a018
Nick Torres <nicholas.torres@kc.frb.org>
2025-05-08 09:11:59 -0600

    change button

--- src/js/components/homepageUpdate/heroUpdate/HeroUpdate.jsx
+++ src/js/components/homepageUpdate/heroUpdate/HeroUpdate.jsx
@@ -4,6 +4,7 @@
  */
 
 import React, { useState, useEffect } from 'react';
+import GlobalConstants from "GlobalConstants";
 import { useHistory } from "react-router-dom";
 import { throttle } from 'lodash';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -11,6 +12,7 @@ import { Button } from 'data-transparency-ui';
 import { mediumScreen } from 'dataMapping/shared/mobileBreakpoints';
 import Analytics from 'helpers/analytics/Analytics';
 import AnimatedHeading from './AnimatedHeading';
+import { Global } from 'recharts';
 
 const HeroUpdate = () => {
     const [isPaused, setIsPaused] = useState(false);
@@ -32,8 +34,7 @@ const HeroUpdate = () => {
     const history = useHistory();
     const handleSearch = () => {
         trackSearchLink();
-        const path = `/search`;
-        history.push(path);
+        history.push(GlobalConstants.SEARCH_V2_PATH);
     };
     const handleDataSources = () => {
         trackAboutLink();
