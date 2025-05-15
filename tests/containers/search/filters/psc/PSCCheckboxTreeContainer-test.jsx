1a9bb910524bbe2085295a615ced700e9f64cbac
Josue Aguilar <josue.aguilar@kc.frb.org>
2025-05-15 15:42:05 -0500

    12409 - removed unused code

--- tests/containers/search/filters/psc/PSCCheckboxTreeContainer-test.jsx
+++ tests/containers/search/filters/psc/PSCCheckboxTreeContainer-test.jsx
@@ -6,45 +6,12 @@
  */
 
 import React from 'react';
-import storeSingleton from "redux/storeSingleton";
-import { combineReducers, createStore } from "redux";
-import { List } from "immutable";
 import { expect } from "@jest/globals";
 import { getDefaultNormalizer, waitFor } from "@testing-library/react";
 
 import PSCCheckboxTreeContainer from "containers/search/filters/psc/PSCCheckboxTreeContainer";
 import { render, screen } from '../../../../testResources/test-utils';
-import * as pscReducer from "../../../../../src/js/redux/reducers/search/pscReducer";
-import appliedFiltersReducer from "../../../../../src/js/redux/reducers/search/appliedFiltersReducer";
-import searchFiltersReducer from "../../../../../src/js/redux/reducers/search/searchFiltersReducer";
 import * as searchHelper from "../../../../../src/js/helpers/searchHelper";
-
-const mockPSCReducers = combineReducers({
-    psc: pscReducer.default,
-    appliedFilters: appliedFiltersReducer,
-    filters: searchFiltersReducer
-});
-
-const mockData = {
-    appliedFilters: {
-        filters: {
-            pscCodes: { require: new Set(), exclude: new Set(), counts: new Set() }
-        },
-        _empty: true,
-        _complete: true
-    },
-    psc: {
-        psc: new List(),
-        expanded: new List(),
-        searchExpanded: new List(),
-        checked: new List(),
-        unchecked: new List(),
-        counts: new List()
-    }
-};
-
-const store = createStore(mockPSCReducers, mockData);
-storeSingleton.setStore(store);
 
 const mockResponse = {
     data: {
