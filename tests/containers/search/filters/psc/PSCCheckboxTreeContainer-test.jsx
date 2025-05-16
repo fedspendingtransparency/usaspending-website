d274bec96d67e8e6a567c61473896d25527f7c2b
Josue Aguilar <josue.aguilar@kc.frb.org>
2025-05-16 09:45:35 -0500

    12409 - added test for search

--- tests/containers/search/filters/psc/PSCCheckboxTreeContainer-test.jsx
+++ tests/containers/search/filters/psc/PSCCheckboxTreeContainer-test.jsx
@@ -10,10 +10,10 @@ import { expect } from "@jest/globals";
 import { getDefaultNormalizer, waitFor } from "@testing-library/react";
 
 import PSCCheckboxTreeContainer from "containers/search/filters/psc/PSCCheckboxTreeContainer";
-import { render, screen } from '../../../../testResources/test-utils';
+import { render, screen, act, fireEvent } from '../../../../testResources/test-utils';
 import * as searchHelper from "../../../../../src/js/helpers/searchHelper";
 
-const mockResponse = {
+const initialMockResponse = {
     data: {
         results: [
             {
@@ -41,35 +41,96 @@ const mockResponse = {
     }
 };
 
+const agriMockResponse = {
+    data: {
+        results: [
+            {
+                id: "Research and Development",
+                ancestors: [],
+                description: "",
+                count: 868,
+                children: [
+                    {
+                        id: "AA",
+                        ancestors: [
+                            "Research and Development"
+                        ],
+                        description: "AGRICULTURE R&D SERVICES",
+                        count: 39,
+                        children: null
+                    }
+                ]
+            },
+            {
+                id: "Service",
+                ancestors: [],
+                description: "",
+                count: 2048,
+                children: null
+            },
+            {
+                id: "Product",
+                ancestors: [],
+                description: "",
+                count: 695,
+                children: null
+            }
+        ]
+    }
+};
+
 describe('PSCCheckboxTreeContainer', () => {
-    it('should load', () => {
-        render(<PSCCheckboxTreeContainer />);
-
-        const test = screen.getByText('{"isError":false,"errorMessage":"","isLoading":false,"data":[],"checked":[],"searchText":"","noResults":false,"expanded":[]}', { exact: false });
-
-        expect(test).toBeTruthy();
-    });
-
     it('should populate the checkbox tree with all psc groups', async () => {
-        jest.spyOn(searchHelper, 'fetchPsc').mockReturnValue({ promise: Promise.resolve(mockResponse) });
+        jest.spyOn(searchHelper, 'fetchPsc').mockReturnValue({ promise: Promise.resolve(initialMockResponse) });
 
         render(<PSCCheckboxTreeContainer />);
 
         await waitFor(() => {
-            const testRandD = screen.getByText('Research and Development', {
+            const rAndDTest = screen.getByText('Research and Development', {
                 exact: false,
                 normalizer: (str) => getDefaultNormalizer()(str).replace(/<span class="highlight"><\/span>/, '')
             });
-            expect(testRandD).toBeTruthy();
+            expect(rAndDTest).toBeTruthy();
         });
 
-        const testService = screen.getByText('2048 codes', { exact: false });
-        const testProduct = screen.getByText('Product', {
+        const serviceTest = screen.getByText('2048 codes', { exact: false });
+        const productTest = screen.getByText('Product', {
             exact: false,
             normalizer: (str) => getDefaultNormalizer()(str).replace(/<span class="highlight"><\/span>/, '')
         });
 
-        expect(testService).toBeTruthy();
-        expect(testProduct).toBeTruthy();
+        expect(serviceTest).toBeTruthy();
+        expect(productTest).toBeTruthy();
+    });
+
+    it('should populate the checkbox tree search results', async () => {
+        jest.spyOn(searchHelper, 'fetchPsc').mockReturnValueOnce({ promise: Promise.resolve(initialMockResponse) });
+
+        render(<PSCCheckboxTreeContainer />);
+
+        await waitFor(() => {
+            const rAndDTest = screen.getByText('Research and Development', {
+                exact: false,
+                normalizer: (str) => getDefaultNormalizer()(str).replace(/<span class="highlight"><\/span>/, '')
+            });
+
+            expect(rAndDTest).toBeTruthy();
+        });
+
+        jest.spyOn(searchHelper, 'fetchPsc').mockReturnValueOnce({ promise: Promise.resolve(agriMockResponse) });
+
+        act(() => {
+            const searchBar = screen.getByPlaceholderText('Type to filter results');
+
+            fireEvent.change(searchBar, { target: { value: 'agri' } });
+            expect(searchBar).toBeTruthy();
+            expect(searchBar).toHaveValue('agri');
+        });
+
+        await waitFor(() => {
+            const agriTest = screen.getByText('CULTURE R&D SERVICES', { exact: false });
+
+            expect(agriTest).toBeTruthy();
+        });
     });
 });
