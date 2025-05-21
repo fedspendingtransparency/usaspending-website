47f006ec2b1429998d0cd985532df807c4975df0
House <J1JDH03@rb.win.frb.org>
2025-05-21 15:04:29 -0500

    well the tests pass now

--- tests/containers/search/filters/naics/NAICSCheckboxTree-test.jsx
+++ tests/containers/search/filters/naics/NAICSCheckboxTree-test.jsx
@@ -12,13 +12,17 @@ import { initialMockResponse, naicsSearchMockResponse, accordionOpenMockResponse
 import * as searchHelper from "../../../../../src/js/helpers/searchHelper";
 
 describe('NAICSCheckboxTreeContainer', () => {
+    afterEach(() => {
+        jest.resetAllMocks();
+    });
+
     it('should populate the checkbox tree with all naics', async () => {
         jest.spyOn(searchHelper, "naicsRequest").mockReturnValueOnce({ promise: Promise.resolve(initialMockResponse) });
 
         render(<NAICSCheckboxTree />);
 
         await waitFor(() => {
-            expect(screen.queryByText('Agriculture, Forestry, Fishing and Hunting')).toBeTruthy();
+            expect(screen.getByText('Agriculture, Forestry, Fishing and Hunting')).toBeInTheDocument();
         });
     });
 
@@ -28,8 +32,11 @@ describe('NAICSCheckboxTreeContainer', () => {
         render(<NAICSCheckboxTree />);
 
         await waitFor(() => {
-            expect(screen.queryByText('Agriculture, Forestry, Fishing and Hunting')).toBeTruthy();
-            expect(screen.queryByText('Mining, Quarrying, and Oil and Gas Extraction')).toBeTruthy();
+            const firstNode = screen.getByText('Agriculture, Forestry, Fishing and Hunting');
+
+            expect(firstNode).toBeInTheDocument();
+            expect(screen.getByText('Mining, Quarrying, and Oil and Gas Extraction')).toBeInTheDocument();
+            expect(screen.getByText('Mining, Quarrying, and Oil and Gas Extraction').parentElement.parentElement.parentElement.parentElement.parentElement).not.toHaveClass('hide');
         });
 
         jest.spyOn(searchHelper, 'naicsRequest').mockReturnValueOnce({ promise: Promise.resolve(naicsSearchMockResponse) });
@@ -41,9 +48,11 @@ describe('NAICSCheckboxTreeContainer', () => {
         });
 
         await waitFor(() => {
-            expect(screen.queryAllByText('fish', { exact: false }).length).toEqual(12); // kind of a bad test.  counts all the time "fish" return not actaul number of nodes.
-            expect(screen.getByText('Agriculture, Forestry,', { exact: false })).toBeInTheDocument(); // not only should this be returned but "Fish" should be highlighted
-            expect(screen.getByText('Mining, Quarrying, and Oil and Gas Extraction')).not.toBeInTheDocument();
+            expect(screen.getAllByText('fish', { exact: false }).length).toEqual(12); // kind of a bad test.  counts all the time "fish" return not actaul number of nodes.
+            expect(screen.queryByText('Agriculture, Forestry, Fishing and Hunting')).toBeNull(); // "Fish" should be wrapped in a span to highlight now so this should not pass
+            expect(screen.getByText('Agriculture, Forestry, ', { exact: false })).toBeInTheDocument(); // check to make sure this element is still there.
+            // kind of a bad test due to deep nesting, however "hide" className removes element from tree
+            expect(screen.queryByText('Mining, Quarrying, and Oil and Gas Extraction').parentElement.parentElement.parentElement.parentElement.parentElement).toHaveClass('hide');
         });
 
         act(() => {
@@ -52,92 +61,93 @@ describe('NAICSCheckboxTreeContainer', () => {
         });
 
         await waitFor(() => {
-            expect(screen.queryByText('Agriculture, Forestry, Fishing and Hunting')).toBeTruthy();
-            expect(screen.queryByText('Mining, Quarrying, and Oil and Gas Extraction')).toBeTruthy();
+            expect(screen.getByText('Agriculture, Forestry, Fishing and Hunting')).toBeInTheDocument();
+            expect(screen.getByText('Mining, Quarrying, and Oil and Gas Extraction')).toBeInTheDocument();
+            // deep nested element should no logger have a parent with "hide" className.
+            expect(screen.getByText('Mining, Quarrying, and Oil and Gas Extraction').parentElement.parentElement.parentElement.parentElement.parentElement).not.toHaveClass('hide');
         });
     });
 
-    // it('should open/close accordion', async () => {
-    //     jest.spyOn(searchHelper, 'naicsRequest').mockReturnValueOnce({ promise: Promise.resolve(initialMockResponse) });
+    it('should open/close accordion', async () => {
+        jest.spyOn(searchHelper, 'naicsRequest').mockReturnValueOnce({ promise: Promise.resolve(initialMockResponse) });
 
-    //     render(<NAICSCheckboxTree />);
+        render(<NAICSCheckboxTree />);
 
-    //     await waitFor(() => {
-    //         expect(screen.queryByText('Agriculture, Forestry, Fishing and Hunting')).toBeTruthy();
-    //     });
+        await waitFor(() => {
+            expect(screen.getByText('Agriculture, Forestry, Fishing and Hunting')).toBeInTheDocument();
+        });
 
-    //     jest.spyOn(searchHelper, 'naicsRequest').mockReturnValueOnce({ promise: Promise.resolve(accordionOpenMockResponse) });
+        jest.spyOn(searchHelper, 'naicsRequest').mockReturnValueOnce({ promise: Promise.resolve(accordionOpenMockResponse) });
 
-    //     act(() => {
-    //         const accordionChevron = screen.getAllByRole('button', 'Toggle');
-    //         fireEvent.click(accordionChevron[1]);
-    //     });
+        act(() => {
+            const accordionChevron = screen.getAllByRole('button', 'Toggle');
+            fireEvent.click(accordionChevron[1]);
+        });
 
-    //     await waitFor(() => {
-    //         const test = screen.getByText('Oilseed and Grain Farmin');
-    //         expect(test).toBeInTheDocument();
-    //     });
+        await waitFor(() => {
+            const test = screen.getByText('Oilseed and Grain Farming');
+            expect(test).toBeInTheDocument();
+        });
 
-    //     act(() => {
-    //         const accordionChevron = screen.getAllByRole('button', 'Toggle');
-    //         fireEvent.click(accordionChevron[1]);
-    //     });
+        act(() => {
+            const accordionChevron = screen.getAllByRole('button', 'Toggle');
+            fireEvent.click(accordionChevron[1]);
+        });
 
-    //     await waitFor(() => {
-    //         const test = screen.queryByText('Oilseed and Grain Farmin');
-    //         expect(test).not.toBeInTheDocument();
-    //     });
-    // });
+        await waitFor(() => {
+            const test = screen.queryByText('Oilseed and Grain Farming');
+            expect(test).toBeNull();
+        });
+    });
 
-    // it('check/uncheck based on parent child relationship', async () => {
-    //     jest.spyOn(searchHelper, 'naicsRequest').mockReturnValueOnce({ promise: Promise.resolve(initialMockResponse) });
+    it('check/uncheck based on parent child relationship', async () => {
+        jest.spyOn(searchHelper, 'naicsRequest').mockReturnValueOnce({ promise: Promise.resolve(initialMockResponse) });
 
-    //     render(<NAICSCheckboxTree />);
+        render(<NAICSCheckboxTree />);
 
-    //     await waitFor(() => {
-    //         expect(screen.queryByText('Agriculture, Forestry, Fishing and Hunting')).toBeTruthy();
-    //     });
+        await waitFor(() => {
+            expect(screen.getByText('Agriculture, Forestry, Fishing and Hunting')).toBeInTheDocument();
+        });
 
-    //     jest.spyOn(searchHelper, 'naicsRequest').mockReturnValueOnce({ promise: Promise.resolve(accordionOpenMockResponse) });
+        jest.spyOn(searchHelper, 'naicsRequest').mockReturnValueOnce({ promise: Promise.resolve(accordionOpenMockResponse) });
 
-    //     act(() => {
-    //         const accordionChevron = screen.getAllByRole('button', 'Toggle');
-    //         fireEvent.click(accordionChevron[1]);
-    //     });
+        act(() => {
+            const accordionChevron = screen.getAllByRole('button', 'Toggle');
+            fireEvent.click(accordionChevron[1]);
+        });
 
-    //     await waitFor(() => {
-    //         const test = screen.getByText('Agriculture, Forestry, Fishing and Hunting');
-    //         expect(test).toBeInTheDocument();
-    //     });
+        await waitFor(() => {
+            const test = screen.getByText('Agriculture, Forestry, Fishing and Hunting');
+            expect(test).toBeInTheDocument();
+        });
 
-    //     const checkboxes = document.getElementsByClassName('rct-checkbox');
+        const checkboxes = document.getElementsByClassName('rct-checkbox');
 
-    //     console.log(checkboxes);
-    // parent checked
-    // act(() => {
-    //     fireEvent.click(checkboxes[1]);
-    // });
+        // parent checked
+        act(() => {
+            fireEvent.click(checkboxes[1]);
+        });
 
-    // expect(checkboxes[1].children[0]).toHaveAttribute('data-icon', 'check-square');
-    // expect(checkboxes[2].children[0]).toHaveAttribute('data-icon', 'check-square');
-    // expect(checkboxes[3].children[0]).toHaveAttribute('data-icon', 'check-square');
+        expect(checkboxes[1].children[0]).toHaveAttribute('data-icon', 'check-square');
+        expect(checkboxes[2].children[0]).toHaveAttribute('data-icon', 'check-square');
+        expect(checkboxes[3].children[0]).toHaveAttribute('data-icon', 'check-square');
 
-    // // one child unchecked
-    // act(() => {
-    //     fireEvent.click(checkboxes[2]);
-    // });
+        // one child unchecked
+        act(() => {
+            fireEvent.click(checkboxes[2]);
+        });
 
-    // expect(checkboxes[1].children[0]).toHaveAttribute('data-icon', 'minus-square');
-    // expect(checkboxes[2].children[0]).toHaveAttribute('data-icon', 'square');
-    // expect(checkboxes[3].children[0]).toHaveAttribute('data-icon', 'check-square');
+        expect(checkboxes[1].children[0]).toHaveAttribute('data-icon', 'minus-square');
+        expect(checkboxes[2].children[0]).toHaveAttribute('data-icon', 'square');
+        expect(checkboxes[3].children[0]).toHaveAttribute('data-icon', 'check-square');
 
-    // // both children unchecked
-    // act(() => {
-    //     fireEvent.click(checkboxes[3]);
-    // });
+        // both children unchecked
+        act(() => {
+            fireEvent.click(checkboxes[3]);
+        });
 
-    // expect(checkboxes[1].children[0]).toHaveAttribute('data-icon', 'square');
-    // expect(checkboxes[2].children[0]).toHaveAttribute('data-icon', 'square');
-    // expect(checkboxes[3].children[0]).toHaveAttribute('data-icon', 'square');
-    // });
+        expect(checkboxes[1].children[0]).toHaveAttribute('data-icon', 'square');
+        expect(checkboxes[2].children[0]).toHaveAttribute('data-icon', 'square');
+        expect(checkboxes[3].children[0]).toHaveAttribute('data-icon', 'square');
+    });
 });
