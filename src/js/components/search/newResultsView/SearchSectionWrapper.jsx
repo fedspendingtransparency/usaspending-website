4a6e05f7a56b0adc8b5559c2d375c42b7902ddd3
House <J1JDH03@rb.win.frb.org>
2025-05-27 12:56:12 -0500

    update for mobile updates

--- src/js/components/search/newResultsView/SearchSectionWrapper.jsx
+++ src/js/components/search/newResultsView/SearchSectionWrapper.jsx
@@ -246,10 +246,17 @@ const SearchSectionWrapper = ({
                     <ChartTableToggle activeType={viewType} changeView={changeView} classname="search__chart-table-toggle" />
                 </div>
                 :
-                <div className="search__section-wrapper-header">
-                    <span className="filter__dropdown-label">{sectionTitle}</span>
-                    {showToggle && <AwardTypeToggle spendingLevel={spendingLevel} onToggle={onToggle} />}
-                </div>
+                <>
+                    <div className="search__section-wrapper-header">
+                        <span className="filter__dropdown-label">{sectionTitle}</span>
+                        {showToggle && <AwardTypeToggle spendingLevel={spendingLevel} onToggle={onToggle} />}
+                    </div>
+                    {showToggle &&
+                        <div className="award-type-toggle__mobile">
+                            <AwardTypeToggle spendingLevel={spendingLevel} onToggle={onToggle} />
+                        </div>
+                    }
+                </>
             }
             {!openAccordion &&
                 <div className={`search__section-wrapper-content new-results-view search__${sectionName}`}>
