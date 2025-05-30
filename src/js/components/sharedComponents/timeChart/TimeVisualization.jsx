5d0e677847570a0ed2f61fe7b763105b9a781c30
Josue Aguilar <josue.aguilar@kc.frb.org>
2025-05-30 13:25:13 -0500

    12687 - refactored TimeVisualization.jsx to function and removed defaultProps

--- src/js/components/sharedComponents/timeChart/TimeVisualization.jsx
+++ src/js/components/sharedComponents/timeChart/TimeVisualization.jsx
@@ -3,9 +3,8 @@
  * Created by Kevin Li 12/16/16
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-
 import { CSSTransition, TransitionGroup } from 'react-transitioning';
 
 import BarChart from './chart/BarChart';
@@ -13,21 +12,6 @@ import Tooltip from './TimeVisualizationTooltip';
 import ChartLoadingMessage from '../ChartLoadingMessage';
 import ChartNoResults from '../ChartNoResults';
 import ChartError from '../ChartError';
-
-const defaultProps = {
-    groups: [],
-    xSeries: [],
-    ySeries: [],
-    width: 0,
-    height: 280,
-    legend: [
-        {
-            color: '#708893',
-            label: 'Amount Obligated',
-            offset: 0
-        }
-    ]
-};
 
 /**
  * groups - an array of X-axis labels. Each group can have multiple bars/data points
@@ -38,8 +22,7 @@ const defaultProps = {
  * ySeries - an array of values that describe the Y-axis values for each data point in the group
  *
  */
-/* eslint-disable react/no-unused-prop-types */
-// allow unused prop types. they are passed to child components, but documented here
+
 const propTypes = {
     width: PropTypes.number,
     height: PropTypes.number,
@@ -51,67 +34,76 @@ const propTypes = {
     visualizationPeriod: PropTypes.string,
     error: PropTypes.bool
 };
-/* eslint-enable react/no-unused-prop-types */
 
-export default class TimeVisualization extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            tooltipData: null,
-            tooltipX: 0,
-            tooltipY: 0,
-            barWidth: 0
-        };
-
-        this.showTooltip = this.showTooltip.bind(this);
-    }
-
-    showTooltip(data, x, y, width) {
-        this.setState({
-            tooltipData: data,
-            tooltipX: x,
-            tooltipY: y,
-            barWidth: width
-        });
-    }
-
-    render() {
-        let tooltip = null;
-        if (this.state.tooltipData && window.innerWidth > 720) {
-            tooltip = (<Tooltip
-                chartWidth={this.props.width}
-                data={this.state.tooltipData}
-                x={this.state.tooltipX}
-                y={this.state.tooltipY}
-                barWidth={this.state.barWidth} />);
+const TimeVisualization = ({
+    width = 0,
+    height = 280,
+    groups = [],
+    xSeries = [],
+    ySeries = [],
+    loading,
+    legend = [
+        {
+            color: '#708893',
+            label: 'Amount Obligated',
+            offset: 0
         }
+    ],
+    visualizationPeriod,
+    error
+}) => {
+    const [tooltipData, setTooltipData] = useState(null);
+    const [tooltipX, setTooltipX] = useState(0);
+    const [tooltipY, setTooltipY] = useState(0);
+    const [barWidth, setBarWidth] = useState(0);
 
-        return (
-            <div className="results-visualization-time-container">
-                <TransitionGroup>
-                    <CSSTransition
-                        classNames="visualization-content-fade"
-                        timeout={{ exit: 225, enter: 195 }}
-                        exit>
-                        <>
-                            {!this.props.loading && !this.props.error && this.props.groups.length === 0 && <ChartNoResults />}
-                            {this.props.loading && <ChartLoadingMessage />}
-                            {this.props.error && <ChartError />}
-                            {!this.props.loading && !this.props.error && this.props.groups.length > 0 && (
-                                <BarChart
-                                    {...this.props}
-                                    showTooltip={this.showTooltip}
-                                    activeLabel={this.state.tooltipData} />
-                            )}
-                        </>
-                    </CSSTransition>
-                </TransitionGroup>
-                {tooltip}
-            </div>
-        );
+    let tooltip = null;
+
+    if (tooltipData && window.innerWidth > 720) {
+        tooltip = (<Tooltip
+            chartWidth={width}
+            data={tooltipData}
+            x={tooltipX}
+            y={tooltipY}
+            barWidth={barWidth} />);
     }
-}
+
+    const showTooltip = (data, x, y, newWidth) => {
+        setTooltipData(data);
+        setTooltipX(x);
+        setTooltipY(y);
+        setBarWidth(newWidth);
+    };
+
+    return (
+        <div className="results-visualization-time-container">
+            <TransitionGroup>
+                <CSSTransition
+                    classNames="visualization-content-fade"
+                    timeout={{ exit: 225, enter: 195 }}
+                    exit>
+                    <>
+                        {!loading && !error && groups.length === 0 && <ChartNoResults />}
+                        {loading && <ChartLoadingMessage />}
+                        {error && <ChartError />}
+                        {!loading && !error && groups.length > 0 && (
+                            <BarChart
+                                width={width}
+                                height={height}
+                                xSeries={xSeries}
+                                ySeries={ySeries}
+                                legend={legend}
+                                visualizationPeriod={visualizationPeriod}
+                                showTooltip={showTooltip}
+                                activeLabel={tooltipData} />
+                        )}
+                    </>
+                </CSSTransition>
+            </TransitionGroup>
+            {tooltip}
+        </div>
+    );
+};
 
 TimeVisualization.propTypes = propTypes;
-TimeVisualization.defaultProps = defaultProps;
+export default TimeVisualization;
