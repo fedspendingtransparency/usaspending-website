95a2e7d7125076aab995df3b96763934bb70eed2
Josue Aguilar <josue.aguilar@kc.frb.org>
2025-06-02 15:56:36 -0500

    12439 - collapsiblesidebarHelper-test.js wip

--- tests/helpers/collapsiblesidebarHelper-test.js
+++ tests/helpers/collapsiblesidebarHelper-test.js
@@ -4,17 +4,32 @@
  */
 
 
-import * as collapsiblesidebarHelper from '../../src/js/helpers/search/collapsiblesidebarHelper';
+import {
+    truncateItemCount,
+    checkInView
+} from '../../src/js/helpers/search/collapsiblesidebarHelper';
 
 describe('collapsiblesidebarHelper', () => {
     describe('truncateItemCount', () => {
         it('should return itemCount as is', () => {
-            const itemCount = collapsiblesidebarHelper.truncateItemCount(11);
+            const itemCount = truncateItemCount(11);
             expect(itemCount).toBe(11);
         });
         it('should return itemCount as condensed to 999+', () => {
-            const itemCount = collapsiblesidebarHelper.truncateItemCount(1000);
+            const itemCount = truncateItemCount(1000);
             expect(itemCount).toBe('999+');
+        });
+    });
+
+    describe('checkInView', () => {
+        it('should return element in view', () => {
+            const mockElement = document.createElement('div');
+
+            console.log(mockElement.getBoundingClientRect());
+            console.log(mockElement.getBoundingClientRect().top);
+            console.log(window.innerHeight);
+
+            expect(checkInView(mockElement)).toBe(0);
         });
     });
 });
