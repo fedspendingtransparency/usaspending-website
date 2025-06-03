205bbf79c2648d4b0bd85861d077843c34743ca0
Josue Aguilar <josue.aguilar@kc.frb.org>
2025-06-03 11:38:34 -0500

    12439 - deleted awardingAgencyResponseParse

--- src/js/helpers/search/filterCheckboxHelper.js
+++ src/js/helpers/search/filterCheckboxHelper.js
@@ -1,28 +1,5 @@
-import { Set } from 'immutable';
 import { recipientTypeGroups } from 'dataMapping/search/recipientType';
 import { awardTypeGroups } from "../../dataMapping/search/awardType";
-
-export const awardingAgencyResponseParse = ({ results }) => {
-    // filter down to only top tier codes
-    const onlyTopTierCodes = results.map((res) => ({
-        id: res.toptier_agency.toptier_code,
-        name: res.toptier_agency.name,
-        filters: []
-    }));
-    const setTiers = new Set(onlyTopTierCodes.map(JSON.stringify));
-    const uniqueTopTiers = Array.from(setTiers).map(JSON.parse);
-
-    // organize each sub tier into their top tier
-    results.forEach((res) => {
-        const category = uniqueTopTiers.find(
-            (topTier) => topTier.id === res.toptier_agency.toptier_code
-        );
-
-        category.filters.push(res.id);
-    });
-
-    return uniqueTopTiers;
-};
 
 export const awardingAgencyCodes = (({ results }) => {
     const codesObj = {};
