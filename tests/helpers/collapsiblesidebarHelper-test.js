511478a3ec87a3f187de551ad76c6e8a194e43c0
Josue Aguilar <josue.aguilar@kc.frb.org>
2025-06-03 11:13:26 -0500

    12439 - sortAlphaNumbersLast test

--- tests/helpers/collapsiblesidebarHelper-test.js
+++ tests/helpers/collapsiblesidebarHelper-test.js
@@ -6,21 +6,10 @@
 
 import {
     truncateItemCount,
-    checkInView
+    checkInView, sortAlphaNumbersLast
 } from '../../src/js/helpers/search/collapsiblesidebarHelper';
 
 describe('collapsiblesidebarHelper', () => {
-    describe('truncateItemCount', () => {
-        it('should return itemCount as is', () => {
-            const itemCount = truncateItemCount(11);
-            expect(itemCount).toBe(11);
-        });
-        it('should return itemCount as condensed to 999+', () => {
-            const itemCount = truncateItemCount(1000);
-            expect(itemCount).toBe('999+');
-        });
-    });
-
     describe('checkInView', () => {
         it('should return element in view', () => {
             const mockElement = document.createElement('div');
@@ -28,7 +17,7 @@ describe('collapsiblesidebarHelper', () => {
             expect(checkInView(mockElement)).toBe(0);
         });
 
-        it('should return element not in view', async () => {
+        it('should return element not in view', () => {
             const mockElement = document.createElement('div');
 
             jest.spyOn(mockElement, "getBoundingClientRect").mockImplementation(() => ({
@@ -41,6 +30,26 @@ describe('collapsiblesidebarHelper', () => {
             }));
 
             expect(checkInView(mockElement)).toBe(-1);
+        });
+    });
+
+    describe('sortAlphaNumbersLast', () => {
+        it('should sort list', () => {
+            const mockArray = ['a', 'b', 1, 2, 3, 'c', 4, 'd'];
+            const expected = ["a", "b", "c", "d", 1, 2, 3, 4];
+
+            expect(sortAlphaNumbersLast(mockArray)).toStrictEqual(expected);
+        });
+    });
+
+    describe('truncateItemCount', () => {
+        it('should return itemCount as is', () => {
+            const itemCount = truncateItemCount(11);
+            expect(itemCount).toBe(11);
+        });
+        it('should return itemCount as condensed to 999+', () => {
+            const itemCount = truncateItemCount(1000);
+            expect(itemCount).toBe('999+');
         });
     });
 });
