3e792e2401c5b1514f1dc57d02c2d38cf7e903ba
Nick Torres <nicholas.torres@kc.frb.org>
2025-07-31 13:39:23 -0600

    wip

--- src/js/components/sharedComponents/RedirectModal.jsx
+++ src/js/components/sharedComponents/RedirectModal.jsx
@@ -37,30 +37,32 @@ export default class RedirectModal extends React.Component {
                         </button>
                     </div>
                     <div className="usa-dt-modal__body">
-                        <div className="usa-dt-modal__title">
-                            <div className="usa-dt-modal__title-icon">
-                                <i className="usa-da-icon">
-                                    <ExclamationTriangle />
-                                </i>
-                            </div>
-                            <div className="usa-dt-modal__title-text">
+                        <div className="usa-dt-modal__title-icon">
+                            <i className="usa-da-icon">
+                                <ExclamationTriangle />
+                            </i>
+                        </div>
+                        <div className="usa-dt-modal__text-wrapper">
+                            <div className="usa-dt-modal__title">
+                                <div className="usa-dt-modal__title-text">
                                 You&apos;re leaving a Federal Government website.
+                                </div>
                             </div>
-                        </div>
-                        <div className="usa-dt-modal__explanation">
+                            <div className="usa-dt-modal__directions">
+                            You have selected to visit:
+                            </div>
+                            <div className="usa-dt-modal__link">
+                                <a
+                                    href={this.props.url}
+                                    target="_blank"
+                                    rel="noopener noreferrer">
+                                    {this.props.url}
+                                </a>
+                            </div>
+                            <div className="usa-dt-modal__explanation">
                             You&apos;re going to a website that is not managed or controlled by the Federal Government.
-                            <br /> Its privacy policies may differ from ours.
-                        </div>
-                        <div className="usa-dt-modal__directions">
-                            Click this link to go to the website you have selected.
-                        </div>
-                        <div className="usa-dt-modal__link">
-                            <a
-                                href={this.props.url}
-                                target="_blank"
-                                rel="noopener noreferrer">
-                                {this.props.url}
-                            </a>
+                                <br /> Its privacy policies may differ from ours.
+                            </div>
                         </div>
                     </div>
                 </div>
