7940f04190037bc8e26c4f376ed5f97ca13673c5
J.D. House <jd.house@kc.frb.org>
2025-08-21 10:35:01 -0500

    wip

--- src/js/components/search/table/groupedTable/NestedAwardTable.jsx
+++ src/js/components/search/table/groupedTable/NestedAwardTable.jsx
@@ -5,14 +5,11 @@
 
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import SearchAwardsOperation from 'models/v1/search/SearchAwardsOperation';
 import { isCancel } from 'axios';
-import * as SearchHelper from 'helpers/searchHelper';
-import { performKeywordSearch } from 'helpers/keywordHelper';
-import { subawardTypeGroups, transactionTypeGroups } from 'dataMapping/search/awardType';
 import { uniqueId, throttle } from 'lodash';
 import { ErrorMessage, LoadingMessage, NoResultsMessage } from "data-transparency-ui";
 import ResultsTable from '../ResultsTable';
+import { getNestedTableData } from '../../../../helpers/search/table/tableUtilsHelper';
 
 const propTypes = {
     columnType: PropTypes.string,
@@ -43,89 +40,17 @@ const NestedAwardTable = (props) => {
             searchRequest.cancel();
         }
 
-        // get searchParams from state
-        const searchParamsTemp = new SearchAwardsOperation();
-        searchParamsTemp.fromState(props.filters);
-
         // indicate the request is about to start
         setIsLoading(true);
         setError(false);
 
-        const requestFields = [
-            "Sub-Award ID",
-            "Sub-Awardee Name",
-            "Sub-Award Amount",
-            "Sub-Award Date",
-            "Sub-Award Description",
-            "Sub-Recipient UEI",
-            "Sub-Recipient Location",
-            "Sub-Award Primary Place of Performance",
-            "Sub-Award Type",
-            "Prime Award ID",
-            "Prime Recipient Name",
-            "Prime Award Recipient UEI",
-            "Awarding Agency",
-            "Awarding Sub Agency",
-            "NAICS",
-            "PSC",
-            "recipient_id",
-            "prime_award_recipient_id"
-        ];
-
-        // needs to be dynamic but for now we will go with defaults
-        searchParamsTemp.awardType = props.columnType === "subawards" ?
-            subawardTypeGroups.subcontracts :
-            transactionTypeGroups.transaction_contracts;
-
-        const newFilters = searchParamsTemp;
-        if (!Object.prototype.hasOwnProperty.call(newFilters, "selectedAwardIDs")) {
-            newFilters.selectedAwardIDs = [];
-        }
-        newFilters.selectedAwardIDs.push(props.awardId);
-
-        const params = {
-            filters: newFilters.toParams(),
-            fields: requestFields,
-            page: subPage,
-            limit: subResultsLimit,
-            sort: subSort.field,
-            order: subSort.direction,
-            subawards: true,
-            auditTrail: 'Results Table - Spending by award search'
+        const paramsOptions = {
+            subSort,
+            subPage,
+            subResultsLimit
         };
 
-        // Set the params needed for download API call
-        if (!params.filters.award_type_codes) {
-            return null;
-        }
-
-        if (props.columnType === "transactions") {
-            params.fields = [
-                "Award ID",
-                "Mod",
-                "Recipient Name",
-                "Transaction Amount",
-                "Action Date",
-                "Transaction Description",
-                "Action Type",
-                "Award Type",
-                "Recipient UEI",
-                "Recipient Location",
-                "Primary Place of Performance",
-                "Awarding Agency",
-                "awarding_agency_id",
-                "recipient_id",
-                "Awarding Sub Agency",
-                "NAICS",
-                "PSC",
-                "Assistance Listing"
-            ];
-
-            searchRequest = performKeywordSearch(params);
-        }
-        else {
-            searchRequest = SearchHelper.performSpendingByAwardSearch(params);
-        }
+        searchRequest = getNestedTableData(props.columnType, props.awardId, props.filters, paramsOptions);
 
         return searchRequest.promise
             .then((res) => {
@@ -209,10 +134,13 @@ const NestedAwardTable = (props) => {
 
     return (
         <>
-            <span className="table-title">{props.awardId}</span>
-            <br />
-            <span className="table-subTitle">{`${props.columnType === "subawards" ? 'Subawards' : 'Transacitions'} that match search criteria`}</span>
-
+            {props.title && <span className="table-title">{props.title}</span>}
+            {props.subTitle && (
+                <>
+                    <br />
+                    <span className="table-subTitle">{props.subTitle}</span>
+                </>
+            )}
             <ResultsTable
                 {...props}
                 results={subData}
