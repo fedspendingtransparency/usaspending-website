14b335b09b283fbbf0aedfc141b350c13a2de86a
Nick Torres <nicholas.torres@kc.frb.org>
2025-08-28 10:15:20 -0600

    upgraded sb

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,20 +3,14 @@ module.exports = {
 
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-mdx-gfm",
     "@storybook/addon-webpack5-compiler-babel",
-    "@chromatic-com/storybook"
+    "@chromatic-com/storybook",
+    "@storybook/addon-docs"
   ],
 
   "framework": {
     name: "@storybook/react-webpack5",
     options: {}
-  },
-
-  docs: {
-    autodocs: true
   },
 
   typescript: {
