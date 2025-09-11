482dd10abf97f016e4cb012fea8d14b69c121e7a
Andrea Blackwell <andrea.blackwell@kc.frb.org>
2025-09-11 15:37:12 -0400

    initial setup

---
+++ src/js/components/featuredContent/FeaturedContentPage.jsx
@@ -0,0 +1,11 @@
+/**
+ * FeaturedContentPage.jsx
+ * Created by Andrea Blackwell 9/11/2025
+ **/
+
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+const FeaturedContentPage = () => (<>"hello world"</>);
+
+export default FeaturedContentPage;
