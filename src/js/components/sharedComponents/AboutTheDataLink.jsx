a06e4777debe654685d22b7ca971b65867660e63
Josue Aguilar <josue.aguilar@kc.frb.org>
2025-09-12 10:44:44 -0500

    13324 - AboutTheDataLink.jsx now changes the url without reloading page

--- src/js/components/sharedComponents/AboutTheDataLink.jsx
+++ src/js/components/sharedComponents/AboutTheDataLink.jsx
@@ -7,7 +7,7 @@ import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 import schema from "dataMapping/aboutTheDataSchema";
-import { Link, useLocation } from "react-router";
+import { useLocation } from "react-router";
 import { getDrilldownEntrySectionAndId } from "helpers/aboutTheDataSidebarHelper";
 import { showSlideout } from '../../helpers/slideoutHelper';
 import { getNewUrlForGlossary } from "../../helpers/glossaryHelper";
@@ -19,42 +19,42 @@ const propTypes = {
 
 const AboutTheDataLink = ({ slug, children }) => {
     const [urlSearchParam, setUrlSearchParam] = useState(null);
-    const [newUrl, setNewUrl] = useState(null);
     const { pathname, search } = useLocation();
 
+    let newUrl;
     const item = getDrilldownEntrySectionAndId(schema, slug);
     const entry = item.section.fields[item?.entryId];
 
     useEffect(() => {
         setUrlSearchParam(search.includes('about-the-data') ? '' : search);
-
-        // there is already a search query
-        if (search && !search.includes('about-the-data')) {
-            // url with original search &about-the-data={term}
-            setNewUrl(`${pathname}${search}&about-the-data=${entry.slug}`);
-        }
-        else {
-            // url with search term as query
-            setNewUrl(
-                getNewUrlForGlossary(pathname, `?about-the-data=${entry.slug}`, urlSearchParam)
-            );
-        }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [search]);
 
+    // there is already a search query
+    if (search && !search.includes('about-the-data')) {
+        // url with original search &about-the-data={term}
+        newUrl = `${pathname}${search}&about-the-data=${entry.slug}`;
+    }
+    else {
+        // url with search term as query
+        newUrl = getNewUrlForGlossary(
+            pathname, `?about-the-data=${entry.slug}`, urlSearchParam
+        );
+    }
+
     const openAboutTheDataSidebar = (e) => {
+        window.history.replaceState(null, '', newUrl);
         showSlideout('atd', { term: entry });
         e.stopPropagation();
     };
 
     return (
-        <Link
-            to={newUrl}
+        <span
             className="usda-atd-link"
             aria-label="Open the About the Data"
-            onClick={(e) => openAboutTheDataSidebar(e)}>
+            onClick={openAboutTheDataSidebar}>
             {children}
-        </Link>
+        </span>
     );
 };
 
