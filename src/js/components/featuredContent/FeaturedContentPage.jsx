84e49def3e41568017eb02029e92e25a2177a030
J.D. House <jd.house@kc.frb.org>
2025-09-16 09:44:23 -0500

    add new content header to featured content page

--- src/js/components/featuredContent/FeaturedContentPage.jsx
+++ src/js/components/featuredContent/FeaturedContentPage.jsx
@@ -5,7 +5,33 @@
 
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
+import { ComingSoon } from "data-transparency-ui";
+import { homePageMetaTags } from "../../helpers/metaTagHelper";
+import PageWrapper from "../sharedComponents/PageWrapper";
+import ContentPageHeader from '../sharedComponents/header/ContentPageHeader';
 
-const FeaturedContentPage = () => (<>"hello world"</>);
+
+require('pages/contentPage/contentPage.scss');
+
+const FeaturedContentPage = () => (
+    <PageWrapper
+        pageName="Featured Content"
+        classNames="content-page"
+        noHeader
+        metaTagProps={{ ...homePageMetaTags }}>
+        <main
+            id="main-content"
+            className="main-content content-page">
+            <ContentPageHeader
+                className="content-page-header"
+                kicker="RESOURCES"
+                title="Featured Content"
+                body="Read the latest featured content to learn more about government spending."
+                image="img/contentPage/featured-content.png"
+                slug="featured-content" />
+
+            <ComingSoon />
+        </main>
+    </PageWrapper>);
 
 export default FeaturedContentPage;
