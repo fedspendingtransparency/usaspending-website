9f9b103d91cb8bf7043c3afa989105082a729346
Josue Aguilar <josue.aguilar@kc.frb.org>
2025-09-22 09:53:43 -0500

    13189 - converted StateTimeVisualizationSectionContainer.jsx to function component

--- src/js/containers/state/StateTimeVisualizationSectionContainer.jsx
+++ src/js/containers/state/StateTimeVisualizationSectionContainer.jsx
@@ -3,90 +3,109 @@
  * Created by David Trinh 5/15/18
  */
 
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { isCancel } from 'axios';
 
-
-import StateTimeVisualizationSection from
-    'components/state/spendingovertime/StateTimeVisualizationSection';
-
+import StateTimeVisualizationSection from 'components/state/spendingovertime/StateTimeVisualizationSection';
 import * as stateActions from 'redux/actions/state/stateActions';
-
 import * as FiscalYearHelper from 'helpers/fiscalYearHelper';
 import * as MonthHelper from 'helpers/monthHelper';
 import * as SearchHelper from 'helpers/searchHelper';
-
 
 const propTypes = {
     stateProfile: PropTypes.object
 };
 
-export class StateTimeVisualizationSectionContainer extends React.Component {
-    constructor(props) {
-        super(props);
+const StateTimeVisualizationSectionContainer = (props) => {
+    const [visualizationPeriod, setVisualizationPeriod] = useState('fiscal_year');
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+    const [groups, setGroups] = useState([]);
+    const [xSeries, setXSeries] = useState([]);
+    const [ySeries, setYSeries] = useState([]);
+    const [combined, setCombined] = useState([]);
+    const [combinedOutlay, setCombinedOutlay] = useState();
+    const [ySeriesOutlay, setYSeriesOutlay] = useState([]);
+    const [outlayToggle, setOutlayToggle] = useState(false);
 
-        this.state = {
-            visualizationPeriod: 'fiscal_year',
-            loading: true,
-            error: false,
-            groups: [],
-            xSeries: [],
-            ySeries: [],
-            combined: [],
-            ySeriesOutlay: [],
-            outlayToggle: false
-        };
+    const apiRequest = useRef(null);
 
-        this.apiRequest = null;
-        this.updateVisualizationPeriod = this.updateVisualizationPeriod.bind(this);
-        this.onOutlaysToggle = this.onOutlaysToggle.bind(this);
-        this.onKeyOutlaysToggle = this.onKeyOutlaysToggle.bind(this);
-    }
+    const onOutlaysToggle = () => setOutlayToggle(!outlayToggle);
 
-    componentDidMount() {
-        this.fetchData();
-    }
-
-    componentDidUpdate(prevProps) {
-        if (prevProps.stateProfile.overview.code !== this.props.stateProfile.overview.code) {
-            this.fetchData();
+    const onKeyOutlaysToggle = (e) => {
+        if (e.key === 'Enter') {
+            setOutlayToggle(!outlayToggle);
         }
-    }
-
-    onOutlaysToggle = () => {
-        this.setState({
-            outlayToggle: !this.state.outlayToggle
-        });
     };
 
-    onKeyOutlaysToggle = (event) => {
-        if (event.key === 'Enter') {
-            this.setState({
-                outlayToggle: !this.state.outlayToggle
+    const updateVisualizationPeriod = (newVizPeriod) => {
+        setVisualizationPeriod(newVizPeriod);
+    };
+
+    const generateTime = (group, timePeriod, type) => {
+        const month = MonthHelper.convertNumToShortMonth(timePeriod.month);
+        const year = MonthHelper.convertMonthToFY(timePeriod.month, timePeriod.fiscal_year);
+
+        if (group === 'fiscal_year') {
+            return type === 'label' ?
+                `${timePeriod.fiscal_year}` :
+                { period: null, year: timePeriod.fiscal_year };
+        }
+        else if (group === 'quarter') {
+            return type === 'label' ?
+                `Q${timePeriod.quarter} ${timePeriod.fiscal_year}` :
+                { period: `Q${timePeriod.quarter}`, year: `${timePeriod.fiscal_year}` };
+        }
+        return type === 'label' ? `${month} ${year}` : { period: `${month}`, year: `${year}` };
+    };
+
+    const parseData = (data, group) => {
+        const groupsLocal = [];
+        const xSeriesLocal = [];
+        const ySeriesLocal = [];
+        const combinedLocal = [];
+        const combinedOutlayLocal = [];
+        const ySeriesOutlayLocal = [];
+
+        // iterate through each response object and break it up into groups, x series, and y series
+        data.results.forEach((item) => {
+            groupsLocal.push(generateTime(group, item.time_period, "label"));
+            xSeriesLocal.push([generateTime(group, item.time_period, "label")]);
+            ySeriesLocal.push([parseFloat(item.aggregated_amount)]);
+            combinedLocal.push(
+                {
+                    x: generateTime(group, item.time_period, "label"),
+                    y: parseFloat(item.aggregated_amount)
+                }
+            );
+            ySeriesOutlayLocal.push([parseFloat(item.total_outlays)]);
+            combinedOutlayLocal.push({
+                x: generateTime(group, item.time_period, "label"),
+                y: parseFloat(item.total_outlays)
             });
-        }
+        });
+
+        setGroups(groupsLocal);
+        setXSeries(xSeriesLocal);
+        setYSeries(ySeriesLocal);
+        setCombined(combinedLocal);
+        setCombinedOutlay(combinedOutlayLocal);
+        setYSeries(ySeriesLocal);
+        setYSeriesOutlay(ySeriesOutlayLocal);
+        setLoading(false);
+        setError(false);
     };
 
-    updateVisualizationPeriod(visualizationPeriod) {
-        this.setState({
-            visualizationPeriod
-        }, () => {
-            this.fetchData();
-        });
-    }
-
-    fetchData() {
-        this.setState({
-            loading: true,
-            error: false
-        });
+    const fetchData = () => {
+        setLoading(true);
+        setError(false);
 
         // Cancel API request if it exists
-        if (this.apiRequest) {
-            this.apiRequest.cancel();
+        if (apiRequest.current) {
+            apiRequest.current.cancel();
         }
 
         // Fetch data from the Awards v2 endpoint
@@ -103,7 +122,7 @@ export class StateTimeVisualizationSectionContainer extends React.Component {
             place_of_performance_locations: [
                 {
                     country: 'USA',
-                    state: this.props.stateProfile.overview.code
+                    state: props.stateProfile.overview.code
                 }
             ]
         };
@@ -112,101 +131,59 @@ export class StateTimeVisualizationSectionContainer extends React.Component {
 
         // Generate the API parameters
         const apiParams = {
-            group: this.state.visualizationPeriod,
+            group: visualizationPeriod,
             filters: searchParams,
             spending_level: "awards"
         };
 
         apiParams.auditTrail = 'Spending Over Time Visualization';
 
-        this.apiRequest = SearchHelper.performSpendingOverTimeSearch(apiParams);
+        apiRequest.current = SearchHelper.performSpendingOverTimeSearch(apiParams);
 
-        this.apiRequest.promise
+        apiRequest.current.promise
             .then((res) => {
-                this.parseData(res.data, this.state.visualizationPeriod);
-                this.apiRequest = null;
+                parseData(res.data, visualizationPeriod);
+                apiRequest.current = null;
             })
             .catch((err) => {
                 if (isCancel(err)) {
                     return;
                 }
 
-                this.apiRequest = null;
+                apiRequest.current = null;
+
                 console.log(err);
-                this.setState({
-                    loading: false,
-                    error: true
-                });
+
+                setLoading(false);
+                setError(true);
             });
-    }
+    };
 
-    generateTime(group, timePeriod, type) {
-        const month = MonthHelper.convertNumToShortMonth(timePeriod.month);
-        const year = MonthHelper.convertMonthToFY(timePeriod.month, timePeriod.fiscal_year);
+    useEffect(() => {
+        fetchData();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [props.stateProfile.overview.code, visualizationPeriod]);
 
-        if (group === 'fiscal_year') {
-            return type === 'label' ? `${timePeriod.fiscal_year}` : { period: null, year: timePeriod.fiscal_year };
-        }
-        else if (group === 'quarter') {
-            return type === 'label' ? `Q${timePeriod.quarter} ${timePeriod.fiscal_year}` : { period: `Q${timePeriod.quarter}`, year: `${timePeriod.fiscal_year}` };
-        }
-        return type === 'label' ? `${month} ${year}` : { period: `${month}`, year: `${year}` };
-    }
-
-    parseData(data, group) {
-        const groups = [];
-        const xSeries = [];
-        const ySeries = [];
-        const combined = [];
-        const combinedOutlay = [];
-        const ySeriesOutlay = [];
-        const rawLabels = [];
-
-        // iterate through each response object and break it up into groups, x series, and y series
-        data.results.forEach((item) => {
-            groups.push(this.generateTime(group, item.time_period, "label"));
-            rawLabels.push(this.generateTime(group, item.time_period, "raw"));
-            xSeries.push([this.generateTime(group, item.time_period, "label")]);
-            ySeries.push([parseFloat(item.aggregated_amount)]);
-            combined.push(
-                {
-                    x: this.generateTime(group, item.time_period, "label"),
-                    y: parseFloat(item.aggregated_amount)
-                }
-            );
-            ySeriesOutlay.push([parseFloat(item.total_outlays)]);
-            combinedOutlay.push({
-                x: this.generateTime(group, item.time_period, "label"),
-                y: parseFloat(item.total_outlays)
-            });
-        });
-
-        this.setState({
-            groups,
-            xSeries,
-            ySeries,
-            combined,
-            combinedOutlay,
-            ySeriesOutlay,
-            rawLabels,
-            loading: false,
-            error: false
-        });
-    }
-
-    render() {
-        return (
-            <StateTimeVisualizationSection
-                data={this.state}
-                loading={this.state.loading}
-                updateVisualizationPeriod={this.updateVisualizationPeriod}
-                visualizationPeriod={this.state.visualizationPeriod}
-                outlayToggle={this.state.outlayToggle}
-                onKeyOutlaysToggle={this.onKeyOutlaysToggle}
-                onOutlaysToggle={this.onOutlaysToggle} />
-        );
-    }
-}
+    return (
+        <StateTimeVisualizationSection
+            data={{
+                loading,
+                error,
+                groups,
+                xSeries,
+                ySeries,
+                combined,
+                combinedOutlay,
+                ySeriesOutlay
+            }}
+            loading={loading}
+            updateVisualizationPeriod={updateVisualizationPeriod}
+            visualizationPeriod={visualizationPeriod}
+            outlayToggle={outlayToggle}
+            onKeyOutlaysToggle={onKeyOutlaysToggle}
+            onOutlaysToggle={onOutlaysToggle} />
+    );
+};
 
 StateTimeVisualizationSectionContainer.propTypes = propTypes;
 
