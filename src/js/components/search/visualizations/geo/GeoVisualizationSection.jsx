7ed1a0adbf53ee6bf75c9ec7e868b24a35418f4b
Andrea Blackwell <andrea.blackwell@kc.frb.org>
2025-10-09 16:25:42 -0400

    remove additional dead code

--- src/js/components/search/visualizations/geo/GeoVisualizationSection.jsx
+++ src/js/components/search/visualizations/geo/GeoVisualizationSection.jsx
@@ -3,7 +3,7 @@
  * Created by Kevin Li 2/13/17
  */
 
-import React, { useEffect, useState, useRef, useCallback } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import PropTypes from 'prop-types';
 import MapboxGL from 'mapbox-gl/dist/mapbox-gl';
 
@@ -12,7 +12,6 @@ import LoadingSpinner from 'components/sharedComponents/LoadingSpinner';
 import { ExclamationTriangle } from 'components/sharedComponents/icons/Icons';
 import Note from 'components/sharedComponents/Note';
 import { noteMessage } from 'dataMapping/search/geoVisualizationSection';
-import { getAtdDefcText } from "helpers/aboutTheDataSidebarHelper";
 import {
     advancedSearchFilters,
     filtersOnClickHandler
@@ -21,8 +20,6 @@ import { awardTypeTabs } from 'dataMapping/covid19/covid19';
 import MapWrapper from './MapWrapper';
 import GeoVisualizationTooltip from './GeoVisualizationTooltip';
 import MapMessage from './MapMessage';
-import GlossaryLink from '../../../sharedComponents/GlossaryLink';
-import { usePrevious } from "../../../../helpers";
 
 const propTypes = {
     scope: PropTypes.string,
@@ -50,10 +47,6 @@ const availableLayers = ['country', 'state', 'county', 'congressionalDistrict'];
 const GeoVisualizationSection = (props) => {
     const [showHover, setShowHover] = useState(false);
     const [selectedItem, setSelectedItem] = useState({});
-    const [tableBody, setTableBody] = useState("");
-    const [tableTitle, setTableTitle] = useState("");
-    const [tablePreview, setTablePreview] = useState("");
-    const [expanded, setExpanded] = useState(null);
     const [activeFilters, setActiveFilters] = useState({
         territory: props.mapLayer,
         spendingType: 'obligation',
@@ -61,8 +54,6 @@ const GeoVisualizationSection = (props) => {
         recipientType: 'all',
         awardType: 'all'
     });
-    const sectionHr = useRef(null);
-    const prevProps = usePrevious(props);
     const dataRef = useRef(props.data);
 
     const updateAmountTypeFilter = (value) => {
@@ -101,74 +92,9 @@ const GeoVisualizationSection = (props) => {
         setSelectedItem({});
     };
 
-    const handleUpdateTitle = () => {
-        const primeAwardTitle = "Spending by Geography";
-        const subAwardTitle = "Sub-Award Spending by Geography";
-        const dropdownValue = document.querySelector(".filter__dropdown-button-text");
-        if (dropdownValue === "Subawards") {
-            setTableTitle(subAwardTitle);
-        }
-        else {
-            setTableTitle(primeAwardTitle);
-        }
-    };
-
-    const handleUpdateBody = useCallback(() => {
-        const dropdownValue = document.querySelector(".filter__dropdown-button-text");
-
-        const primeAwardPreview = "Use the map below to break down spending by state, county, or congressional district.";
-        const primeAwardBody = <>
-            {getAtdDefcText(props.isDefCodeInFilter?.length > 0)}
-            <p className="award-search__body-text">
-                The data in the map represent <span className="award-search__glossary-term">federal action</span>{' '}<GlossaryLink term="federal-action-obligation" /> {<span className="award-search__glossary-term"> obligation</span>}{' '}{<GlossaryLink term="obligation" />} amounts for non-loan prime award {<span className="award-search__glossary-term"> transactions</span>}{' '}{<GlossaryLink term="transaction" />} within the selected filters. Loan awards use the {<span className="award-search__glossary-term">subsidy cost</span>}{' '}{<GlossaryLink term="loan-subsidy-cost" />} rather than the obligated amount to sum up the value of the loan. Prime award transactions with the same unique award ID are grouped under a single prime award summary. Prime award summaries can be viewed in the Table tab.
-            </p>
-        </>;
-
-        const subAwardPreview = "Use the map below to break down spending by state, county, or congressional district.";
-        const subAwardBody = (
-            <>
-                {getAtdDefcText(props.isDefCodeInFilter?.length > 0)}
-                <p className="award-search__body-text">
-                    The data below represent{<span className="award-search__glossary-term"> sub-awards</span>}{' '}{<GlossaryLink term="sub-award" />}{' '}that meet the selected filter criteria. The results do not reflect sub-awards whose
-                    {<span className="award-search__glossary-term"> prime awards</span>}{' '}{<GlossaryLink term="prime-award" />}
-                    {' '}meet the selected filter criteria. For example, if you filter by Place of Performance in your county, you will see only sub-awards with Place of Performance in your county, but you will not see all sub-awards whose prime award lists Place of Performance in your county.
-                </p>
-                <p className="award-search__body-text">
-                    Sub-award amounts are funded by prime award obligations and outlays. In theory, the total value of all sub-award amounts for any given prime award is a subset of the Current Award Amount for that prime award; sub-award amounts generally should not exceed the Current Award Amount for their associated prime award. To avoid double-counting the overall value of a prime award, do not sum up sub-award amounts and prime award obligations or outlays.
-                </p>
-            </>);
-        if (dropdownValue === "Subawards") {
-            setTableBody(subAwardBody);
-            setTablePreview(subAwardPreview);
-        }
-        else {
-            setTableBody(primeAwardBody);
-            setTablePreview(primeAwardPreview);
-        }
-    });
-
-    useEffect(() => {
-        handleUpdateTitle();
-        handleUpdateBody();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-
     useEffect(() => {
         dataRef.current = props.data;
     }, [props.data]);
-
-    useEffect(() => {
-        if (!expanded || expanded === null) {
-            const elem = document.querySelector(".read-more__preview-lines");
-            elem?.classList.add("line-clamp");
-        }
-
-        if (props.spendingLevel !== prevProps?.spendingLevel) {
-            handleUpdateTitle();
-            handleUpdateBody();
-        }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [expanded, prevProps?.spendingLevel, props.spendingLevel]);
 
     useEffect(() => {
         updateTerritoryFilter(props.mapLayer);
