ec02b7f6d288ca7fed1429dc2e378c94fda80339
Nick Torres <nicholas.torres@kc.frb.org>
2025-10-14 12:09:58 -0600

    explore more

--- src/js/components/featuredContent/ExploreMore.jsx
+++ src/js/components/featuredContent/ExploreMore.jsx
@@ -15,25 +15,23 @@ const propTypes = {
     citations: PropTypes.array
 };
 
-const ExploreMore = (props) => {
-    return (
-        <>
-            <span className="featured-content__citation-heading">
-                {props.header}
-            </span>
-            <FlexGridRow className="featured-content__citation-wrapper">
-                {props.citations?.map((citation, index) => (
+const ExploreMore = (props) => (
+    <>
+        <span className="featured-content__citation-heading">
+            {props.header}
+        </span>
+        <FlexGridRow className="featured-content__citation-wrapper">
+            {props.citations?.map((citation, index) => (
                 // eslint-disable-next-line react/no-array-index-key
-                    <FlexGridCol mobile={12} tablet={6} desktop={12} className="featured-content__citation" key={`featured-content__citation-${index}`}>
-                        {citation.type === "external" ?
-                            <><ExternalLink isCard url={citation.slug}><FontAwesomeIcon icon="external-link-alt" />{citation.label}</ExternalLink></> :
-                            <><Link to={citation.slug}><FontAwesomeIcon icon="link" />{citation.label}</Link></>}
-                    </FlexGridCol>
-                ))}
-            </FlexGridRow>
-        </>
-    );
-};
+                <FlexGridCol mobile={12} tablet={6} desktop={12} className="featured-content__citation" key={`featured-content__citation-${index}`}>
+                    {citation.type === "external" ?
+                        <><ExternalLink isCard url={citation.slug}><FontAwesomeIcon icon="external-link-alt" />{citation.label}</ExternalLink></> :
+                        <><Link to={citation.slug}><FontAwesomeIcon icon="link" />{citation.label}</Link></>}
+                </FlexGridCol>
+            ))}
+        </FlexGridRow>
+    </>
+);
 
 ExploreMore.propTypes = propTypes;
 export default ExploreMore;
