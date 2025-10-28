5c99b54a8cfa56c843eae732e231200457c8f08e
Josue Aguilar <josue.aguilar@kc.frb.org>
2025-10-28 10:45:21 -0500

    13708 - moved sidebar content to it's own component

---
+++ src/js/components/featuredContent/FeaturedContentSidebar.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { FlexGridCol } from "data-transparency-ui";
+import PropTypes from "prop-types";
+
+import ExploreMore from "./ExploreMore";
+import RelatedTerms from "./RelatedTerms";
+
+const propTypes = { chosenArticle: PropTypes.object.isRequired };
+
+const FeaturedContentSidebar = ({ chosenArticle }) => (
+    <FlexGridCol
+        tablet={12}
+        mobile={12}
+        desktop={4}
+        className="featured-content__column-two">
+        <div>share</div>
+        {chosenArticle?.explore_more.length > 0 &&
+            <ExploreMore
+                header="Explore More"
+                citations={chosenArticle?.explore_more} />
+        }
+        {chosenArticle?.related_terms.length > 0 &&
+            <RelatedTerms
+                header="Related Terms"
+                citations={chosenArticle?.related_terms} />
+        }
+    </FlexGridCol>
+);
+
+FeaturedContentSidebar.propTypes = propTypes;
+export default FeaturedContentSidebar;
