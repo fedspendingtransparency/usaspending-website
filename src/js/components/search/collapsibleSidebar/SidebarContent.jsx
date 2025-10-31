8d40b4575fb0e2debbfc0adfcf544aa8a9f3d37f
Josue Aguilar <josue.aguilar@kc.frb.org>
2025-10-31 12:07:40 -0500

    13788 - separated out the sidebar filters into their own components

--- src/js/components/search/collapsibleSidebar/SidebarContent.jsx
+++ src/js/components/search/collapsibleSidebar/SidebarContent.jsx
@@ -5,17 +5,13 @@
 
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
-import { useSelector } from "react-redux";
 
-import { searchFilterCategoryTree } from "dataMapping/search/searchFilterCategories";
 import SearchSidebarSubmitContainer from "../../../containers/search/SearchSidebarSubmitContainer";
-import Accordion from "../../sharedComponents/accordion/Accordion";
 // removed in DEV-13712
 // import DsmSlider from "./DsmSlider";
-import { excludeIDVB, generateCount } from "../../../helpers/search/filterCheckboxHelper";
-import KeywordContainer from "../../../containers/search/filters/KeywordContainer";
 import * as Icons from '../../../components/sharedComponents/icons/Icons';
 import { mediumScreen } from '../../../dataMapping/shared/mobileBreakpoints';
+import SidebarContentFilters from "./SidebarContentFilters";
 
 const propTypes = {
     sidebarContentHeight: PropTypes.number,
@@ -33,54 +29,8 @@ const SidebarContent = ({
     // setIsDsmOpened,
     isMobile
 }) => {
-    const [open, setOpen] = useState({
-        Location: false,
-        "Time Period": true,
-        "Award Description": false,
-        "Award ID": false,
-        "Spending Amount": false,
-        "Contract Award Type": false,
-        "North American Industry Classification System (NAICS)": false,
-        "Product and Service Code (PSC)": false,
-        "Type of Contract Pricing": false,
-        "Type of Set Aside": false,
-        "Extent Competed": false,
-        "Financial Assistance Award Type": false,
-        "Assistance Listing": false,
-        Recipient: false,
-        "Recipient Type": false,
-        Agency: false,
-        "Treasury Account Symbol (TAS)": false,
-        "COVID-19 Spending": false,
-        "Infrastructure Spending": false
-    });
     const [isSmall, setIsSmall] = useState(window.innerWidth < mediumScreen);
     const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-
-    const filters = useSelector((state) => state.filters);
-
-    const filterCount = {
-        Location: filters.selectedLocations.size + filters.selectedRecipientLocations.size,
-        'Time Period':
-            filters.timePeriodType === 'dr' ? filters.time_period.size : filters.timePeriodFY.size,
-        'Award Description': filters.awardDescription ? 1 : 0,
-        'Award ID': filters.selectedAwardIDs.size,
-        'Spending Amount': filters.awardAmounts.size,
-        'Contract Award Type': excludeIDVB(filters.contractAwardType),
-        'North American Industry Classification System (NAICS)': generateCount(filters.naicsCodes),
-        'Product and Service Code (PSC)': generateCount(filters.pscCodes),
-        'Type of Contract Pricing': filters.pricingType.size,
-        'Type of Set Aside': filters.setAside.size,
-        'Extent Competed': filters.extentCompeted.size,
-        'Financial Assistance Award Type': filters.financialAssistanceAwardType.size,
-        'Assistance Listing': filters.selectedCFDA.size,
-        Recipient: filters.selectedRecipients.size,
-        'Recipient Type': filters.recipientType.size,
-        Agency: filters.selectedAwardingAgencies.size + filters.selectedFundingAgencies.size,
-        'Treasury Account Symbol (TAS)': generateCount(filters.tasCodes),
-        'COVID-19 Spending': filters.covidDefCode.size,
-        'Infrastructure Spending': filters.infraDefCode.size
-    };
 
     useEffect(() => {
     // determine if the width changed
@@ -91,24 +41,6 @@ const SidebarContent = ({
             setIsSmall(window.innerWidth < mediumScreen);
         }
     }, [windowWidth]);
-
-    // removed in DEV-13712
-    // const dsmElHeight = sidebarContentHeight;
-
-    const filtersArray = searchFilterCategoryTree.map((filter) => (
-        <div className="search-filters-list">
-            <Accordion
-                title={filter.title}
-                setOpen={() => setOpen({ ...open, [filter.title]: !open[filter.title] })}
-                openObject={open[filter.title]}
-                closedIcon="chevron-down"
-                openIcon="chevron-up"
-                contentClassName={open[filter.title] ? '' : 'hidden'}
-                selectedChipCount={filterCount[filter.title]}>
-                {!isMobile && open[filter.title] && filter.component}
-            </Accordion>
-        </div>
-    ));
 
     return (
         <>
@@ -126,14 +58,10 @@ const SidebarContent = ({
                         <Icons.Close alt="Close About The Data" />
                     </button>
                 </div>}
-                {!isDsmOpened && (
-                    <div
-                        className="collapsible-sidebar--search-filters-list"
-                        style={{ height: sidebarContentHeight }}>
-                        <KeywordContainer searchV2 />
-                        {filtersArray}
-                    </div>
-                )}
+                <SidebarContentFilters
+                    isDsmOpened={isDsmOpened}
+                    sidebarContentHeight={sidebarContentHeight}
+                    isMobile={isMobile} />
                 {/* removed in DEV-13712 */}
                 {/* <DsmSlider */}
                 {/*     isDsmOpened={isDsmOpened} */}
@@ -141,7 +69,7 @@ const SidebarContent = ({
                 {/*     dsmFile="learn-filters-panel.mdx" */}
                 {/*     currentLevel={1} */}
                 {/*     selectedCategoryTitle="" */}
-                {/*     height={dsmElHeight} */}
+                {/*     height={sidebarContentHeight} */}
                 {/*     hasChildren={false} /> */}
             </div>
             <div className="sidebar-bottom-submit">
---
+++ src/js/components/search/collapsibleSidebar/SidebarContentFilters.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import { useSelector } from "react-redux";
+import PropTypes from "prop-types";
+
+import KeywordContainer from "../../../containers/search/filters/KeywordContainer";
+import { searchFilterCategoryTree } from "../../../dataMapping/search/searchFilterCategories";
+import { getFilterCount } from "../../../helpers/search/filterCheckboxHelper";
+import SidebarContentFilterAccordion from "./SidebarContentFilterAccordion";
+
+const propTypes = {
+    isDsmOpened: PropTypes.bool,
+    sidebarContentHeight: PropTypes.number,
+    isMobile: PropTypes.bool
+};
+
+const SidebarContentFilters = ({ isDsmOpened, sidebarContentHeight, isMobile }) => {
+    const [open, setOpen] = useState({
+        Location: false,
+        "Time Period": true,
+        "Award Description": false,
+        "Award ID": false,
+        "Spending Amount": false,
+        "Contract Award Type": false,
+        "North American Industry Classification System (NAICS)": false,
+        "Product and Service Code (PSC)": false,
+        "Type of Contract Pricing": false,
+        "Type of Set Aside": false,
+        "Extent Competed": false,
+        "Financial Assistance Award Type": false,
+        "Assistance Listing": false,
+        Recipient: false,
+        "Recipient Type": false,
+        Agency: false,
+        "Treasury Account Symbol (TAS)": false,
+        "COVID-19 Spending": false,
+        "Infrastructure Spending": false
+    });
+    const filters = useSelector((state) => state.filters);
+    const filterCount = getFilterCount(filters);
+
+    return (
+        <>
+            {!isDsmOpened && (
+                <div
+                    className="collapsible-sidebar--search-filters-list"
+                    style={{ height: sidebarContentHeight }}>
+                    <KeywordContainer searchV2 />
+                    {searchFilterCategoryTree.map(({ title, component }) => (
+                        <SidebarContentFilterAccordion
+                            title={title}
+                            component={component}
+                            open={open}
+                            setOpen={setOpen}
+                            count={filterCount[title]}
+                            isMobile={isMobile} />
+                    ))}
+                </div>
+            )}
+        </>
+    );
+};
+
+SidebarContentFilters.propTypes = propTypes;
+export default SidebarContentFilters;
