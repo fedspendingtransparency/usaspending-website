7fb7d8999fb692d32ebcb1e43341f30eb97b2b11
Josue Aguilar <josue.aguilar@kc.frb.org>
2025-11-13 10:45:03 -0600

    12049 - const renaming

--- src/js/components/search/resultsView/SearchSectionWrapper/SearchSectionWrapperContent.jsx
+++ src/js/components/search/resultsView/SearchSectionWrapper/SearchSectionWrapperContent.jsx
@@ -59,7 +59,7 @@ const SearchSectionWrapperContent = ({
     downloadComponent,
     children
 }) => {
-    const [isMobile] = useWindowWidth();
+    const [isTablet] = useWindowWidth();
 
     const Message = () => {
         if (isLoading) {
@@ -107,7 +107,7 @@ const SearchSectionWrapperContent = ({
                     <Message />
                     :
                     <>
-                        {((viewType === "table" || sectionName === "table") && isMobile) ?
+                        {((viewType === "table" || sectionName === "table") && isTablet) ?
                             <MobileSort
                                 columns={columns}
                                 options={mobileDropdownOptions}
