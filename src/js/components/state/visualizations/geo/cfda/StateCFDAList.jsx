b43b299a150fcb4f44d65a28b0301c1af8e4e8be
J.D. House <jd.house@kc.frb.org>
2025-12-11 11:40:07 -0600

    revert state cfda changes

--- src/js/components/state/visualizations/geo/cfda/StateCFDAList.jsx
+++ src/js/components/state/visualizations/geo/cfda/StateCFDAList.jsx
@@ -9,9 +9,7 @@ import PropTypes from 'prop-types';
 
 import * as SearchHelper from 'helpers/searchHelper';
 
-// import Autocomplete from 'components/sharedComponents/autocomplete/Autocomplete';
-import AutocompleteWithCheckboxList from '../../../../sharedComponents/autocomplete/AutocompleteWithCheckboxList';
-import replaceString from '../../../../../helpers/replaceString';
+import Autocomplete from 'components/sharedComponents/autocomplete/Autocomplete';
 
 const propTypes = {
     selectCFDA: PropTypes.func,
@@ -20,16 +18,12 @@ const propTypes = {
 
 const StateCFDAList = (props) => {
     const [cfdaTitleString, setCfdaTitleString] = useState('');
-    const [cfdaSearchString, setCfdaSearchString] = useState('');
     const [autocompleteCFDA, setAutocompleteCFDA] = useState([]);
     const [noResults, setNoResults] = useState(false);
     const [searchData, setSearchData] = useState({});
-    const [isLoading, setIsLoading] = useState(false);
-    // const selectedCFDA = useSelector((state) => state.filters.selectedCFDA);
-
-    const highlightText = (text) => replaceString(text, cfdaSearchString, 'bold-highlight');
 
     let apiRequest = null;
+    let timeout = null;
 
     const selectCFDA = (cfda) => {
         setCfdaTitleString(`${cfda.program_number} - ${cfda.program_title}`);
@@ -50,11 +44,9 @@ const StateCFDAList = (props) => {
                 const subtitle = '';
 
                 values.push({
-                    title: highlightText(title),
+                    title,
                     subtitle,
-                    data: item,
-                    value: item.program_number,
-                    key: item.program_number
+                    data: item
                 });
             });
         }
@@ -62,82 +54,60 @@ const StateCFDAList = (props) => {
         setAutocompleteCFDA(values);
     };
 
-    const queryAutocompleteCFDA = () => {
+    const queryAutocompleteCFDA = (input) => {
         setNoResults(false);
 
-        if (apiRequest) {
+        // Only search if input is 3 or more characters
+        if (input.length >= 3) {
+            if (apiRequest) {
+                // A request is currently in-flight, cancel it
+                apiRequest.cancel();
+            }
+
+            const cfdaSearchParams = {
+                search_text: input,
+                limit: 1000
+            };
+
+            apiRequest = SearchHelper.fetchCFDA(cfdaSearchParams);
+
+            apiRequest.promise
+                .then((res) => {
+                    const autocompleteData = res.data.results;
+                    setNoResults(autocompleteData.length === 0);
+
+                    // Add search results to Redux
+                    parseAutocompleteCFDA(autocompleteData);
+                })
+                .catch((err) => {
+                    if (!isCancel(err)) {
+                        setNoResults(true);
+                    }
+                });
+        }
+        else if (apiRequest) {
             // A request is currently in-flight, cancel it
             apiRequest.cancel();
         }
-
-        const cfdaSearchParams = {
-            search_text: cfdaSearchString,
-            limit: 1000
-        };
-
-        setIsLoading(true);
-        apiRequest = SearchHelper.fetchCFDA(cfdaSearchParams);
-
-        apiRequest.promise
-            .then((res) => {
-                const autocompleteData = res.data.results;
-                setNoResults(autocompleteData.length === 0);
-
-                // Add search results to Redux
-                parseAutocompleteCFDA(autocompleteData);
-                setIsLoading(false);
-            })
-            .catch((err) => {
-                if (!isCancel(err)) {
-                    setNoResults(true);
-                    setIsLoading(false);
-                }
-            });
     };
 
-    const handleTextInputChange = (e) => {
-        setCfdaSearchString(e.target.value);
-    };
-
-    const handleClearCFDAs = () => {
-        // selectedCFDA.forEach((cfda) => {
-        //     dispatch(updateSelectedCFDA({ cfda }));
-        // });
-
-        setCfdaSearchString(''); // clean up if previously set
+    const clearAutocompleteSuggestions = () => {
         setAutocompleteCFDA([]);
     };
 
-    const toggleAll = (selectAll) => {
-        // const selectedArray = props.selectedCFDA.toArray();
+    const handleTextInput = (cfdaInput) => {
+    // Clear existing cfdas to ensure user can't select an old or existing one
+        setAutocompleteCFDA([]);
 
-        if (selectAll) {
-            // let filteredList = autocompleteCFDA;
-            // if (selectedArray?.length) {
-            //     filteredList = autocompleteCFDA.filter((cfda) => !selectedArray.some((c) => c.program_number === cfda.data.program_number));
-            // }
+        // Grab input, clear any exiting timeout
+        const input = cfdaInput.target?.value;
+        window.clearTimeout(timeout);
 
-            // filteredList.forEach((fcfda) => {
-            //     dispatch(updateSelectedCFDA({ cfda: fcfda.data }));
-            // });
-        }
-        else {
-            setCfdaSearchString(''); // clean up if previously set
-            setAutocompleteCFDA([]);
-        }
+        // Perform search if user doesn't type again for 300ms
+        timeout = window.setTimeout(() => {
+            queryAutocompleteCFDA(input);
+        }, 300);
     };
-
-
-    useEffect(() => {
-        if (cfdaSearchString?.length >= 3) {
-            queryAutocompleteCFDA();
-        }
-
-        if (cfdaSearchString === '') {
-            handleClearCFDAs();
-        }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [cfdaSearchString]);
 
     useEffect(() => {
         if (Object.keys(searchData).length > 0) {
@@ -146,65 +116,49 @@ const StateCFDAList = (props) => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [searchData]);
 
-    // useEffect(() => {
-    //     const el = document.getElementById("state__cfda-id");
-    //     el.addEventListener("focus", (e) => {
-    //         if (e.target.value !== "") {
-    //             el.select();
-    //         }
-    //     });
-    //     el.addEventListener("blur", (e) => {
-    //         if (e.target.value === "") {
-    //             handleClearCFDAs();
-    //             props.clearSearchFilters("program_number");
-    //             setCfdaTitleString('');
-    //         }
-    //     });
-    //     return () => {
-    //         el.removeEventListener("focus", (e) => {
-    //             if (e.target.value !== "") {
-    //                 el.select();
-    //             }
-    //         });
-    //         el.removeEventListener("blur", (e) => {
-    //             if (e.target.value === "") {
-    //                 handleClearCFDAs();
-    //                 props.clearSearchFilters("agency");
-    //                 setCfdaTitleString('');
-    //             }
-    //         });
-    //     };
-    //     // eslint-disable-next-line react-hooks/exhaustive-deps
-    // }, []);
+    useEffect(() => {
+        const el = document.getElementById("state__cfda-id");
+        el.addEventListener("focus", (e) => {
+            if (e.target.value !== "") {
+                el.select();
+            }
+        });
+        el.addEventListener("blur", (e) => {
+            if (e.target.value === "") {
+                clearAutocompleteSuggestions();
+                props.clearSearchFilters("program_number");
+                setCfdaTitleString('');
+            }
+        });
+        return () => {
+            el.removeEventListener("focus", (e) => {
+                if (e.target.value !== "") {
+                    el.select();
+                }
+            });
+            el.removeEventListener("blur", (e) => {
+                if (e.target.value === "") {
+                    clearAutocompleteSuggestions();
+                    props.clearSearchFilters("agency");
+                    setCfdaTitleString('');
+                }
+            });
+        };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
     return (
-        <div className="cfda-filter">
-            <AutocompleteWithCheckboxList
-                id="state__cfda-id"
-                placeholder="e.g., 93.778 - Medical Assistance Program"
-                filterType="CFDA"
-                handleTextInputChange={handleTextInputChange}
-                onSearchClear={handleClearCFDAs}
-                searchString={cfdaSearchString}
-                filters={autocompleteCFDA}
-                selectedFilters={props.selectedCFDA}
-                toggleSingleFilter={selectCFDA}
-                toggleAll={toggleAll}
-                noResults={noResults}
-                isLoading={isLoading}
-                limit={1000} />
-        </div>
-        // <Autocomplete
-        //     {...props}
-        //     id="state__cfda-id"
-        //     label="Assistance Listing"
-        //     values={autocompleteCFDA}
-        //     handleTextInput={handleTextInput}
-        //     onSelect={selectCFDA}
-        //     type="program_number"
-        //     clearAutocompleteSuggestions={clearAutocompleteSuggestions}
-        //     noResults={noResults}
-        //     retainValue />
+        <Autocomplete
+            {...props}
+            id="state__cfda-id"
+            label="Assistance Listing"
+            values={autocompleteCFDA}
+            handleTextInput={handleTextInput}
+            onSelect={selectCFDA}
+            type="program_number"
+            clearAutocompleteSuggestions={clearAutocompleteSuggestions}
+            noResults={noResults}
+            retainValue />
     );
 };
 
