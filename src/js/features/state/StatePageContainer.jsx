80bd4db5b288688d8b98a846ca155366a6ef1d75
Josue Aguilar <josue.aguilar@kc.frb.org>
2025-12-12 14:25:37 -0600

    created StatePageNavigation.jsx

--- src/js/features/state/StatePageContainer.jsx
+++ src/js/features/state/StatePageContainer.jsx
@@ -1,29 +1,26 @@
 /**
- * StateContainer.jsx
+ * StatePageContainer.jsx
  * Created by Lizzie Salita 5/1/18
  */
 
 import React, { useState, useEffect, useCallback, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { isCancel } from 'axios';
-import { useNavigate, useMatch } from 'react-router';
 
 import BaseStateProfile from 'models/v2/state/BaseStateProfile';
-import * as StateHelper from 'helpers/stateHelper';
 import {
     setStateOverview,
-    resetState,
     setStateFiscalYear,
     setStateCenter
 } from 'redux/actions/state/stateActions';
 import { stateCenterFromFips } from 'helpers/mapHelper';
 import StatePage from 'components/state/StatePage';
-import { fetchStateOverview, parseStateDataFromUrl } from 'helpers/stateHelper';
+import { fetchStateOverview } from 'helpers/stateHelper';
 
 require('pages/state/statePage.scss');
 
 
-const Navigate = ({
+const StatePageContainer = ({
     onClickFy, stateId, state, fy
 }) => {
     const stateProfile = useSelector((s) => s.stateProfile);
@@ -89,46 +86,4 @@ const Navigate = ({
     );
 };
 
-const StateContainer = () => {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const match = useMatch(`/state/:state/:fyParam?`);
-    const { state, fyParam } = match.params;
-
-    const [wasInputStateName, stateName, stateId] = parseStateDataFromUrl(state);
-    const fy = fyParam;
-
-    const onClickFy = (newFy) => {
-        navigate(`/state/${stateName}/${newFy}`);
-        dispatch(setStateFiscalYear(newFy));
-    };
-
-    useEffect(() => {
-        if (!fy) {
-            // this may be an issue on the first day of 2026 fiscal year
-            // history(`/state/${stateName}/latest`, { replace: true });
-            navigate(`/state/${stateName}/2026`, { replace: true });
-        }
-        else if (!wasInputStateName) {
-            navigate(`/state/${stateName}/${fy}`, { replace: true });
-        }
-        else {
-            dispatch(setStateFiscalYear(fy));
-        }
-
-        return () => {
-            dispatch(resetState());
-        };
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-
-    return (
-        <Navigate
-            onClickFy={onClickFy}
-            stateId={stateId}
-            state={state}
-            fy={fy} />
-    );
-};
-
-export default StateContainer;
+export default StatePageContainer;
---
+++ src/js/features/state/StatePageNavigation.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useMatch, useNavigate } from "react-router";
+
+import { parseStateDataFromUrl } from "helpers/stateHelper";
+import { resetState, setStateFiscalYear } from "redux/actions/state/stateActions";
+import StatePageContainer from "./StatePageContainer";
+
+const StatePageNavigation = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const match = useMatch(`/state/:state/:fyParam?`);
+    const { state, fyParam } = match.params;
+
+    const [wasInputStateName, stateName, stateId] = parseStateDataFromUrl(state);
+    const fy = fyParam;
+
+    const onClickFy = (newFy) => {
+        navigate(`/state/${stateName}/${newFy}`);
+        dispatch(setStateFiscalYear(newFy));
+    };
+
+    useEffect(() => {
+        if (!fy) {
+            // this may be an issue on the first day of 2026 fiscal year
+            // history(`/state/${stateName}/latest`, { replace: true });
+            navigate(`/state/${stateName}/2026`, { replace: true });
+        }
+        else if (!wasInputStateName) {
+            navigate(`/state/${stateName}/${fy}`, { replace: true });
+        }
+        else {
+            dispatch(setStateFiscalYear(fy));
+        }
+
+        return () => {
+            dispatch(resetState());
+        };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    return (
+        <StatePageContainer
+            onClickFy={onClickFy}
+            stateId={stateId}
+            state={state}
+            fy={fy} />
+    );
+};
+
+export default StatePageNavigation;
