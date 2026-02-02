b75933b340a9ca2e4ff3a7245acd43a8f6302741
Josue Aguilar <josue.aguilar@kc.frb.org>
2026-02-02 09:14:09 -0600

    refactored defc fetching

--- src/js/components/state/visualizations/geo/filters/StateMapFiltersDropdown.jsx
+++ src/js/components/state/visualizations/geo/filters/StateMapFiltersDropdown.jsx
@@ -1,20 +1,15 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React from "react";
 import { uniqueId } from "lodash-es";
 import { NewPicker } from "data-transparency-ui";
 import PropTypes from "prop-types";
 
 import { handleSort } from "helpers/covid19Helper";
 import {
-    awardTypeTabs,
     filtersOnClickHandler,
     mapFilterSortOrderByValue,
     stateFilters
 } from "dataMapping/state/stateMap";
 import { useDefCodes } from "containers/covid19/WithDefCodes";
-
-const awardTypeFilters = awardTypeTabs.map((filter) => filter.internal)
-    .filter((filter) => filter !== 'all')
-    .filter((filter) => filter !== 'loans');
 
 const propTypes = {
     activeFilters: PropTypes.object,
@@ -26,6 +21,8 @@ const propTypes = {
 const StateMapFiltersDropdown = ({
     activeFilters, changeScope, clearSearchFilters, changeMapLayer
 }) => {
+    const [, , defCodes] = useDefCodes();
+
     const updateDefcFilter = (value) => {
         if (value === "all") {
             clearSearchFilters("def_code");
@@ -40,7 +37,7 @@ const StateMapFiltersDropdown = ({
     };
 
     // this will need to be updated as more filters are added
-    const addOnClickToFilters = () => Object.keys(stateFilters).reduce((acc, filter) => {
+    const mapFilters = Object.keys(stateFilters).reduce((acc, filter) => {
         acc[filter] = {
             ...stateFilters[filter],
             onClick: filtersOnClickHandler[filter] === 'updateTerritoryFilter' ?
@@ -50,60 +47,29 @@ const StateMapFiltersDropdown = ({
         return acc;
     }, {});
 
-    const [mapFilters, setMapFilters] = useState(addOnClickToFilters());
-    const [, , defCodes] = useDefCodes();
-
-    const parseDefCodes = useCallback((codes, filters) => {
-        const defCodeOptionsList = codes.map((code) => ({
+    if (mapFilters.def_codes.options.length === 1) {
+        const defCodeOptionsList = defCodes.map((code) => ({
             label: `${code.code} - ${code.title}`,
             value: code.code
         }));
 
-        // Move DEFC 1 to the end of the list
-        const firstElement = defCodeOptionsList.shift();
-        defCodeOptionsList.push(firstElement);
-
-        const tempMapFilters = filters;
-
-        if (tempMapFilters.def_codes.options.length === 1) {
-            tempMapFilters.def_codes.options.push(...defCodeOptionsList);
-            setMapFilters(tempMapFilters);
-        }
-    }, []);
-
-    useEffect(() => {
-        if (defCodes.length > 0) {
-            parseDefCodes(defCodes, mapFilters);
-        }
-    }, [defCodes, parseDefCodes, mapFilters]);
-
-    if (!activeFilters) return null;
-
-    let tempMapFilters = mapFilters;
-
-    if (awardTypeFilters?.includes(activeFilters.awardType)) {
-        tempMapFilters.spendingType.options.pop();
+        mapFilters.def_codes.options.push(...defCodeOptionsList);
     }
 
-    tempMapFilters = Object.assign({},
-        {
-            territory: tempMapFilters.territory,
-            def_codes: tempMapFilters.def_codes
-        }
-    );
+    if (!activeFilters) return null;
 
     return (
         <>
             {
-                Object.keys(tempMapFilters)
+                Object.keys(mapFilters)
                     .map((filter) => {
                         let filterType = null;
 
-                        if (tempMapFilters[filter].label.includes('DEFC')) {
+                        if (mapFilters[filter].label.includes('DEFC')) {
                             filterType = 'defc';
                         }
 
-                        if (tempMapFilters[filter].label.includes('Award Type')) {
+                        if (mapFilters[filter].label.includes('Award Type')) {
                             filterType = 'awardType';
                         }
 
@@ -111,26 +77,26 @@ const StateMapFiltersDropdown = ({
                             <div key={uniqueId()} className="map__filters-filter__container">
                                 <div className="map__filters-wrapper">
                                     <span className="map__filters-label">
-                                        {tempMapFilters[filter].label}
+                                        {mapFilters[filter].label}
                                     </span>
                                     <NewPicker
-                                        enabled={tempMapFilters[filter].enabled}
+                                        enabled={mapFilters[filter].enabled}
                                         size="sm"
                                         classname={`map__filters-button ${filterType}`}
                                         dropdownClassname="map__filters-dropdown"
                                         sortFn={handleSort}
                                         selectedOption={
-                                            tempMapFilters[filter]
+                                            mapFilters[filter]
                                                 .options?.find(
                                                     (option) =>
                                                         option.value === activeFilters[filter]
                                                 )?.label
                                         }
                                         options={
-                                            tempMapFilters[filter].options?.map((option) => ({
+                                            mapFilters[filter].options?.map((option) => ({
                                                 name: option.label,
                                                 value: option.value,
-                                                onClick: tempMapFilters[filter].onClick,
+                                                onClick: mapFilters[filter].onClick,
                                                 sortOrder: mapFilterSortOrderByValue[option.value]
                                             }))
                                         } />
