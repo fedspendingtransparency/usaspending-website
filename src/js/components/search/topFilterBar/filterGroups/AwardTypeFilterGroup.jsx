b93db3ac6e8ee37e7cea8181c2130d4d347900e8
Josue Aguilar <josue.aguilar@kc.frb.org>
2026-02-10 09:39:11 -0600

    refactored for active filter chip staging (groups)

--- src/js/components/search/topFilterBar/filterGroups/AwardTypeFilterGroup.jsx
+++ src/js/components/search/topFilterBar/filterGroups/AwardTypeFilterGroup.jsx
@@ -6,17 +6,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { indexOf, difference, concat } from 'lodash-es';
+import { useDispatch, useSelector } from "react-redux";
 
-import { Set } from 'immutable';
-
-import * as AwardType from 'dataMapping/search/awardType';
-
+import { awardTypeCodes, awardTypeGroups } from "dataMapping/search/awardType";
+import { updateGenericFilter } from "redux/actions/search/searchFilterActions";
 import BaseTopFilterGroup from '../BaseTopFilterGroup';
 
 const propTypes = {
-    filter: PropTypes.object,
-    redux: PropTypes.object,
-    compressed: PropTypes.bool
+    filter: PropTypes.object
 };
 
 const groupKeys = ['contracts', 'idvs', 'grants', 'direct_payments', 'loans', 'other'];
@@ -29,105 +26,89 @@ const groupLabels = {
     other: 'Other'
 };
 
-export default class AwardTypeFilterGroup extends React.Component {
-    constructor(props) {
-        super(props);
+const AwardTypeFilterGroup = ({ filter }) => {
+    const awardType = useSelector((state) => state.filters.awardType);
+    const dispatch = useDispatch();
 
-        this.removeFilter = this.removeFilter.bind(this);
-        this.removeGroup = this.removeGroup.bind(this);
-        this.clearGroup = this.clearGroup.bind(this);
-    }
-
-    removeFilter(value) {
-    // remove a single filter item
-        const newValue = this.props.redux.reduxFilters.awardType.delete(value);
-        this.props.redux.updateGenericFilter({
+    const removeFilter = (value) => {
+        // remove a single filter item
+        const newValue = awardType.delete(value);
+        dispatch(updateGenericFilter({
             type: 'awardType',
             value: newValue
-        });
-    }
+        }));
+    };
 
-    removeGroup(value) {
-    // remove a group of filter items
-    // let's actually fake the removal by just overwriting the the filter value with everything
-    // except for the values in the specified group
-        let updatedValues = new Set(this.props.filter.values);
+    const removeGroup = (value, staged) => {
+        const awardValues = awardTypeGroups[value];
 
-        // remove the current group's values
-        const awardValues = AwardType.awardTypeGroups[value];
-        updatedValues = updatedValues.filterNot((x) => indexOf(awardValues, x) > -1);
+        let updatedValues = awardType;
 
-        this.props.redux.updateGenericFilter({
+        if (staged) updatedValues = updatedValues.filter((x) => !(indexOf(awardValues, x) > -1));
+        else awardValues.forEach((x) => updatedValues = updatedValues.add(x));
+
+        dispatch(updateGenericFilter({
             type: 'awardType',
             value: updatedValues
-        });
-    }
+        }));
+    };
 
-    clearGroup() {
-        this.props.redux.clearFilterType('awardType');
-    }
+    const tags = [];
 
-    generateTags() {
-        const tags = [];
+    // check to see if any type groups are fully selected
+    const selectedValues = filter.values;
+    const fullGroups = [];
+    const unstagedGroups = [];
 
-        // check to see if any type groups are fully selected
-        const selectedValues = this.props.filter.values;
-        const fullGroups = [];
-        groupKeys.forEach((key) => {
-            const fullMembership = AwardType.awardTypeGroups[key];
+    groupKeys.forEach((key) => {
+        const fullMembership = awardTypeGroups[key];
 
-            // quick way of checking for full group membership is to return an array of missing
-            // values; it'll be empty if all the values are selected
-            const missingValues = difference(fullMembership, selectedValues);
+        // quick way of checking for full group membership is to return an array of missing
+        // values; it'll be empty if all the values are selected
+        const missingValues = difference(fullMembership, selectedValues);
+        const unstaged = difference(fullMembership, awardType.toArray());
 
-            if (missingValues.length === 0) {
-                // this group is complete
-                fullGroups.push(key);
-            }
-        });
+        if (missingValues.length === 0) {
+            // this group is complete
+            fullGroups.push(key);
+        }
 
-        // add full groups to the beginning of the tag list
-        let excludedValues = [];
-        fullGroups.forEach((group) => {
-            const tag = {
-                value: group,
-                title: `All ${groupLabels[group]}`,
-                removeFilter: this.removeGroup
-            };
+        if (unstaged.length > 0) unstagedGroups.push(key);
+    });
 
+    // add full groups to the beginning of the tag list
+    let excludedValues = [];
+    fullGroups.forEach((group) => {
+        const unstaged = unstagedGroups.includes(group);
+        const tag = {
+            value: group,
+            title: `All ${groupLabels[group]}`,
+            unstageFilter: () => removeGroup(group, !unstaged),
+            unstaged
+        };
+
+        tags.push(tag);
+
+        // exclude these values from the remaining tags
+        excludedValues = concat(excludedValues, awardTypeGroups[group]);
+    });
+
+    selectedValues.forEach((value) => {
+        const tag = {
+            title: awardTypeCodes[value],
+            unstageFilter: removeFilter
+        };
+
+        if (indexOf(excludedValues, value) < 0) {
+            // only insert individual tags that aren't part of a fully-selected group
+            // excluded values is an array of values that are already included in a full group,
+            // so if this value isn't in that array, it can be shown individually
             tags.push(tag);
+        }
+    });
 
-            // exclude these values from the remaining tags
-            excludedValues = concat(excludedValues, AwardType.awardTypeGroups[group]);
-        });
-
-        selectedValues.forEach((value) => {
-            const tag = {
-                value,
-                title: AwardType.awardTypeCodes[value],
-                removeFilter: this.removeFilter
-            };
-
-            if (indexOf(excludedValues, value) < 0) {
-                // only insert individual tags that aren't part of a fully-selected group
-                // excluded values is an array of values that are already included in a full group,
-                // so if this value isn't in that array, it can be shown individually
-                tags.push(tag);
-            }
-        });
-
-        return tags;
-    }
-
-    render() {
-        const tags = this.generateTags();
-
-        return (<BaseTopFilterGroup
-            tags={tags}
-            filter={this.props.filter}
-            clearFilterGroup={this.clearGroup}
-            compressed={this.props.compressed} />);
-    }
-}
+    return (<BaseTopFilterGroup tags={tags} filter={filter} />);
+};
 
 AwardTypeFilterGroup.propTypes = propTypes;
+export default AwardTypeFilterGroup;
