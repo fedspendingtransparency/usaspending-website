6914f591b33ca376562d171474f4e8148594150b
Josue Aguilar <josue.aguilar@kc.frb.org>
2026-02-24 14:48:42 -0600

    refactored ExplorerVisualization.jsx to function

--- src/js/components/explorer/detail/visualization/ExplorerVisualization.jsx
+++ src/js/components/explorer/detail/visualization/ExplorerVisualization.jsx
@@ -3,12 +3,10 @@
  * Created by Kevin Li 8/17/17
  */
 
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 
 import Analytics from 'helpers/analytics/Analytics';
-import * as redirectHelper from 'helpers/redirectHelper';
-
 import UnreportedErrorScreen from 'components/explorer/detail/UnreportedErrorScreen';
 import ExplorerTable from 'containers/explorer/detail/table/ExplorerTable';
 import Note, { dodNote } from 'components/sharedComponents/Note';
@@ -30,154 +28,134 @@ const propTypes = {
     goToUnreported: PropTypes.func
 };
 
-export default class ExplorerVisualization extends React.Component {
-    constructor(props) {
-        super(props);
+const ExplorerVisualization = ({
+    isRoot,
+    isLoading,
+    root,
+    active,
+    trail,
+    data,
+    total,
+    goDeeper,
+    changeSubdivisionType,
+    showTooltip,
+    hideTooltip,
+    goToUnreported
+}) => {
+    const [width, setWidth] = useState(0);
+    const [viewType, setViewType] = useState('treemap');
+    const widthRef = useRef(null);
 
-        this.state = {
-            width: 0,
-            viewType: 'treemap'
-        };
+    const measureWidth = () => setWidth(widthRef.current.offsetWidth);
 
-        this.measureWidth = this.measureWidth.bind(this);
-        this.changeView = this.changeView.bind(this);
-    }
-
-    componentDidMount() {
-        this.measureWidth();
-        window.addEventListener('resize', this.measureWidth);
+    const changeView = (type) => {
+        setViewType(type);
 
         Analytics.event({
             event: 'Spending Explorer - Visualization Type',
             category: 'Spending Explorer - Visualization Type',
-            action: this.state.viewType
+            action: type
         });
+    };
+
+    const loadingTreemapClass = isLoading ? '' : 'explorer-vis__treemap-transition__loading';
+    const loadingTableClass = isLoading ? '' : 'explorer-vis__table-transition__loading';
+
+    let visualization = (
+        <div className={`treemap-loading-transition ${loadingTreemapClass}`}>
+            <ExplorerTreemap
+                activeSubdivision={active.subdivision}
+                isLoading={isLoading}
+                width={width}
+                data={data}
+                total={total}
+                goDeeper={goDeeper}
+                showTooltip={showTooltip}
+                hideTooltip={hideTooltip}
+                goToUnreported={goToUnreported}
+                changeView={changeView} />
+        </div>
+    );
+    if (viewType === 'table') {
+        visualization = (
+            <div className={`explorer-vis__table-transition ${loadingTableClass}`}>
+                <ExplorerTable
+                    isLoading={isLoading}
+                    results={data}
+                    total={total}
+                    goDeeper={goDeeper}
+                    goToUnreported={goToUnreported} />
+            </div>
+        );
     }
 
-    componentWillUnmount() {
-        window.removeEventListener('resize', this.measureWidth);
+    let dropDown;
+    let disclaimer;
+
+    if (data.get(0).name === 'Unreported Data' && data.count() === 1) {
+        visualization = (<div><UnreportedErrorScreen /></div>);
+    }
+    else {
+        dropDown = (
+            <div className="explorer-vis__toolbar">
+                <BreakdownDropdown
+                    root={root}
+                    active={active}
+                    trail={trail}
+                    isRoot={isRoot}
+                    changeSubdivisionType={changeSubdivisionType}
+                    viewType={viewType}
+                    changeView={changeView} />
+            </div>);
+
+        disclaimer = (
+            <div className="explorer-vis__disclaimer">
+                <p>All dollar amounts shown here represent agency reported obligated amounts</p>
+                <Note
+                    title="Unreported Data*:"
+                    message={(
+                        <>
+                            Unreported amounts are calculated using the difference in the total obligated amount from the&nbsp;
+                            <a
+                                className="usa-bold-link"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                href="https://portal.max.gov/portal/document/SF133/Budget/FACTS%20II%20-%20SF%20133%20Report%20on%20Budget%20Execution%20and%20Budgetary%20Resources.html">
+                                Report on Budget Execution and Budgetary Resources
+                            </a>
+                            &nbsp;(excluding financing accounts) and the total obligated amount reported by agencies to USAspending.gov in 'Account Breakdown by Program Activity &amp; Object Class' data (also called 'File B' data).
+                        </>
+                    )} />
+                <Note message={dodNote} />
+            </div>
+        );
     }
 
-    measureWidth() {
-        const width = this.widthRef.offsetWidth;
-
-        this.setState({
-            width
-        });
-    }
-
-    redirect() {
-        redirectHelper.showRedirectModal("https://max.omb.gov/maxportal/document/SF133/Budget/FACTS%20II%20-%20SF%20133%20Report%20on%20Budget%20Execution%20and%20Budgetary%20Resources.html");
-    }
-
-    changeView(viewType) {
-        this.setState({
-            viewType
-        });
+    useEffect(() => {
+        measureWidth();
+        window.addEventListener('resize', measureWidth);
 
         Analytics.event({
             event: 'Spending Explorer - Visualization Type',
             category: 'Spending Explorer - Visualization Type',
             action: viewType
         });
-    }
 
-    render() {
-        let loadingTreemapClass = '';
-        let loadingTableClass = '';
-        if (this.props.isLoading) {
-            loadingTreemapClass = 'explorer-vis__treemap-transition__loading';
-            loadingTableClass = 'explorer-vis__table-transition__loading';
-        }
+        return () => window.removeEventListener('resize', measureWidth);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
-        let visualization = (
-            <div className={`treemap-loading-transition ${loadingTreemapClass}`}>
-                <ExplorerTreemap
-                    activeSubdivision={this.props.active.subdivision}
-                    isLoading={this.props.isLoading}
-                    width={this.state.width}
-                    data={this.props.data}
-                    total={this.props.total}
-                    goDeeper={this.props.goDeeper}
-                    showTooltip={this.props.showTooltip}
-                    hideTooltip={this.props.hideTooltip}
-                    goToUnreported={this.props.goToUnreported}
-                    changeView={this.changeView} />
-            </div>
-        );
-        if (this.state.viewType === 'table') {
-            visualization = (
-                <div className={`explorer-vis__table-transition ${loadingTableClass}`}>
-                    <ExplorerTable
-                        isLoading={this.props.isLoading}
-                        results={this.props.data}
-                        total={this.props.total}
-                        goDeeper={this.props.goDeeper}
-                        goToUnreported={this.props.goToUnreported} />
-                </div>
-            );
-        }
-        let dropDown;
-        let disclaimer;
-        if (this.props.data.get(0).name === 'Unreported Data' && this.props.data.count() === 1) {
-            visualization = (
-                <div>
-                    <UnreportedErrorScreen />
-                </div>
-            );
-        }
-        else {
-            dropDown = (
-                <div className="explorer-vis__toolbar">
-                    <BreakdownDropdown
-                        root={this.props.root}
-                        active={this.props.active}
-                        trail={this.props.trail}
-                        isRoot={this.props.isRoot}
-                        changeSubdivisionType={this.props.changeSubdivisionType}
-                        viewType={this.state.viewType}
-                        changeView={this.changeView} />
-                </div>);
-
-            disclaimer = (
-                <div className="explorer-vis__disclaimer">
-                    <p>All dollar amounts shown here represent agency reported obligated amounts</p>
-                    <Note
-                        title="Unreported Data*:"
-                        message={(
-                            <>
-                                Unreported amounts are calculated using the difference in the total obligated amount from the&nbsp;
-                                <a
-                                    className="usa-bold-link"
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                    href="https://portal.max.gov/portal/document/SF133/Budget/FACTS%20II%20-%20SF%20133%20Report%20on%20Budget%20Execution%20and%20Budgetary%20Resources.html">
-                                    Report on Budget Execution and Budgetary Resources
-                                </a>
-                                &nbsp;(excluding financing accounts) and the total obligated amount reported by agencies to USAspending.gov in 'Account Breakdown by Program Activity &amp; Object Class' data (also called 'File B' data).
-                            </>
-                        )} />
-                    <Note message={dodNote} />
-                </div>
-            );
-        }
-
-        return (
-            <div className="explorer-vis">
-                {dropDown}
-
-                <div
-                    className="explorer-vis__width-reference"
-                    ref={(div) => {
-                        this.widthRef = div;
-                    }} />
-
-                {visualization}
-
-                {disclaimer}
-            </div>
-        );
-    }
-}
+    return (
+        <div className="explorer-vis">
+            {dropDown}
+            <div
+                className="explorer-vis__width-reference"
+                ref={widthRef} />
+            {visualization}
+            {disclaimer}
+        </div>
+    );
+};
 
 ExplorerVisualization.propTypes = propTypes;
+export default ExplorerVisualization;
