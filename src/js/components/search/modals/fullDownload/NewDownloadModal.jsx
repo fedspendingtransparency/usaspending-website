6c8f1155a39fdf067a954982cfccc07ddf823980
Nick Torres <nicholas.torres@kc.frb.org>
2026-03-02 08:38:28 -0700

    wip

---
+++ src/js/components/search/modals/fullDownload/NewDownloadModal.jsx
@@ -0,0 +1,11 @@
+/**
+ * NewDownloadModal.jsx
+ * Created by Nick Torres 2/27/26
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import Modal from 'react-aria-modal';
+
+import { Close } from 'components/sharedComponents/icons/Icons';
+
