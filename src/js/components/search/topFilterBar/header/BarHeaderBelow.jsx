386190a5a4fdf64af1d0f0b9a35d72669c32e7fc
Josue Aguilar <josue.aguilar@kc.frb.org>
2026-03-03 15:18:59 -0600

    styling below the line done

--- src/js/components/search/topFilterBar/header/BarHeaderBelow.jsx
+++ src/js/components/search/topFilterBar/header/BarHeaderBelow.jsx
@@ -1,12 +1,17 @@
 import React, { useMemo } from "react";
 import { Button } from "data-transparency-ui";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import PropTypes from "prop-types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const propTypes = { filterCount: PropTypes.number };
 
 const BarHeaderBelow = ({ filterCount }) => {
-    const image = useMemo(() => (<FontAwesomeIcon icon="window-restore" />), []);
+    /* TODO: change these icons to font awesome 7 */
+    const closeIcon = useMemo(() => (<FontAwesomeIcon icon="times" />), []);
+    const chevronIcon = useMemo(() => (<FontAwesomeIcon icon="chevron-up" />), []);
+
+    const removeOnClick = () => console.log("Remove Selected Filters");
+    const collapseOnClick = () => console.log("Remove Selected Filters");
 
     return (
         <div className="below-line">
@@ -15,26 +20,28 @@ const BarHeaderBelow = ({ filterCount }) => {
                 id="top-filter-bar-title">
                 {`${filterCount} Active Filter${filterCount !== 1 ? 's' : ''}:`}
             </h2>
-            <Button
-                onClick={() => console.log("Remove Selected Filters")}
-                onKeyUp={() => console.log("Remove Selected Filters")}
-                copy="Remove Selected Filters"
-                buttonTitle="filter modal"
-                buttonSize="sm"
-                buttonType="text"
-                backgroundColor="light"
-                imageAlignment="right"
-                image={image} />
-            <Button
-                onClick={() => console.log('Collapse Active Filters')}
-                onKeyUp={() => console.log('Collapse Active Filters')}
-                copy="Collapse Active Filters"
-                buttonTitle="filter modal"
-                buttonSize="sm"
-                buttonType="text"
-                backgroundColor="light"
-                imageAlignment="right"
-                image={image} />
+            <div className="filter-buttons">
+                <Button
+                    onClick={removeOnClick}
+                    onKeyUp={removeOnClick}
+                    copy="Remove Selected Filters"
+                    buttonTitle="filter modal"
+                    buttonSize="sm"
+                    buttonType="text"
+                    backgroundColor="light"
+                    imageAlignment="right"
+                    image={closeIcon} />
+                <Button
+                    onClick={collapseOnClick}
+                    onKeyUp={collapseOnClick}
+                    copy="Collapse Active Filters"
+                    buttonTitle="filter modal"
+                    buttonSize="sm"
+                    buttonType="text"
+                    backgroundColor="light"
+                    imageAlignment="right"
+                    image={chevronIcon} />
+            </div>
         </div>
     );
 };
