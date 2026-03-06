5910ce5fbdc9c0190375ab56357b560bcf006f7b
Josue Aguilar <josue.aguilar@kc.frb.org>
2026-03-06 10:52:31 -0600

    fixed chips, moved logic to SelectedAwardAmountBound.jsx

--- src/js/components/search/filters/awardAmount/AwardAmountSearch.jsx
+++ src/js/components/search/filters/awardAmount/AwardAmountSearch.jsx
@@ -4,28 +4,19 @@
  */
 
 import React from 'react';
-import { reduce, each } from 'lodash-es';
+import { reduce } from 'lodash-es';
 import { useDispatch, useSelector } from "react-redux";
 
 import { awardRanges } from 'dataMapping/search/awardAmount';
 import { formatAwardAmountRange } from 'helpers/awardAmountHelper';
-import SelectedAwardAmountBound from
-    'components/search/filters/awardAmount/SelectedAwardAmountBound';
 import PrimaryCheckboxType from 'components/sharedComponents/checkbox/PrimaryCheckboxType';
 import { updateAwardAmounts, updateGenericFilter } from "redux/actions/search/searchFilterActions";
 import SpecificAwardAmountItem from './SpecificAwardAmountItem';
+import SelectedAwardAmountBound from "./SelectedAwardAmountBound";
 
 const AwardAmountSearch = () => {
     const awardAmounts = useSelector((state) => state.filters.awardAmounts);
     const dispatch = useDispatch();
-
-    const removeFilter = (key) => {
-        const newValue = awardAmounts.delete(key);
-        dispatch(updateGenericFilter({
-            type: 'awardAmounts',
-            value: newValue
-        }));
-    };
 
     const toggleSelection = ({ value: key }) => {
         const newValue = awardAmounts.has(key) ?
@@ -41,12 +32,13 @@ const AwardAmountSearch = () => {
     const searchSpecificRange = (selections) => {
         const min = selections[0];
         const max = selections[1];
+
         dispatch(updateAwardAmounts({ value: [min, max] }));
     };
 
     const awardAmountCheckboxes = () => reduce(awardRanges, (result, value, key) => {
-        const name = formatAwardAmountRange(
-            value, 0);
+        const name = formatAwardAmountRange(value, 0);
+
         result.push(
             <PrimaryCheckboxType
                 id={`award-${key}`}
@@ -58,28 +50,10 @@ const AwardAmountSearch = () => {
                 toggleCheckboxType={toggleSelection}
                 key={key} />
         );
+
         return result;
     }, []);
 
-    const stagedFilters = () => {
-        const filterObject = awardAmounts.toObject();
-        let stagedFilter;
-        let name;
-        each(filterObject, (val, key) => {
-            stagedFilter = val;
-            name = key;
-        });
-        if (!stagedFilter) return null;
-        const label = formatAwardAmountRange(stagedFilter);
-        return (
-            <SelectedAwardAmountBound
-                removeFilter={removeFilter}
-                name={name}
-                label={label} />
-        );
-    };
-
-    const stagedFiltersResult = stagedFilters();
     const awardAmountRangeItems = awardAmountCheckboxes();
 
     return (
@@ -93,7 +67,7 @@ const AwardAmountSearch = () => {
                     <div
                         className="selected-filters"
                         role="status">
-                        {stagedFiltersResult}
+                        <SelectedAwardAmountBound awardAmounts={awardAmounts} />
                     </div>
                 </div>
             </div>
