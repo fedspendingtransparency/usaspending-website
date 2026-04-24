41ed69842c2f0444d6f3896c7b4d8f8c9b87d743
Nick Torres <nicholas.torres@kc.frb.org>
2026-04-24 09:14:43 -0600

    test

--- src/js/components/search/topFilterBar/TopFilterBar.jsx
+++ src/js/components/search/topFilterBar/TopFilterBar.jsx
@@ -9,15 +9,17 @@
   **/
 
 import React, { memo, useEffect, useRef, useState } from 'react';
+import PropTypes from 'prop-types';
 import TopFilterGroupGenerator from './TopFilterGroupGenerator';
 import BarHeader from "./header/BarHeader";
-import PropTypes from 'prop-types';
 
 const propTypes = {
     filters: PropTypes.array,
     filterCount: PropTypes.number,
     resultsView: PropTypes.bool
 };
+
+
 
 // eslint-disable-next-line prefer-arrow-callback
 const TopFilterBar = memo(function TopFilterBar({ filters, filterCount, resultsView }) {
