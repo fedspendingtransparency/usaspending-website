6c85850a785463948aa2ff9e80191af5ffe2b4bf
Josue Aguilar <josue.aguilar@kc.frb.org>
2026-05-01 10:00:34 -0500

    font awesome prop

--- src/js/components/sharedComponents/header/BannerPageHeader.jsx
+++ src/js/components/sharedComponents/header/BannerPageHeader.jsx
@@ -7,6 +7,7 @@ const propTypes = {
     kicker: PropTypes.string,
     title: PropTypes.string,
     body: PropTypes.string,
+    faIcon: PropTypes.string,
     primaryColor: PropTypes.string,
     overrideBackgroundColor: PropTypes.string,
     overrideIconColor: PropTypes.string,
@@ -18,6 +19,7 @@ const BannerPageHeader = ({
     kicker = 'PROFILES',
     title = 'Federal Response to COVID-19',
     body = 'Need short description here',
+    faIcon = "virus-covid",
     primaryColor = '#39215E',
     overrideBackgroundColor,
     overrideIconColor,
@@ -36,7 +38,7 @@ const BannerPageHeader = ({
             <FlexGridRow className="banner-page-header__row">
                 <FlexGridCol width="auto" className="icon-column">
                     <div className="icon-container">
-                        <FontAwesomeIcon icon="chevron-left" color={iconColor} />
+                        <FontAwesomeIcon icon={faIcon} color={iconColor} />
                     </div>
                 </FlexGridCol>
                 <FlexGridCol width="fill" className="text-column">
