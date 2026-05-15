dc4be0642fd70898973610299eb4044cfd4ec4a3
Josue Aguilar <josue.aguilar@kc.frb.org>
2026-05-15 14:33:40 -0500

    added jsxA11y eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,27 @@
 import js from "@eslint/js";
 import globals from "globals";
-import { defineConfig } from "eslint/config";
-
+import { defineConfig, globalIgnores } from "eslint/config";
 
 import pluginReact from "eslint-plugin-react";
+import jsxA11y from 'eslint-plugin-jsx-a11y';
 
 export default defineConfig([
-  {
-    files: ["**/*.{js,mjs,cjs,jsx}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-    languageOptions: { globals: globals.browser }
-  },
-  pluginReact.configs.flat.recommended,
+    globalIgnores([
+        "**/webpack/",
+        "**/storybook-static/",
+        "**/docs/",
+        "**/__mocks__",
+        "**/.storybook/",
+        "**/coverage/",
+        "**/scripts/",
+        "**/tests/" // TODO: don't forget to remove this line
+    ]),
+    {
+        files: ["**/*.{js,mjs,cjs,jsx}"],
+        plugins: { js },
+        extends: ["js/recommended"],
+        languageOptions: { globals: globals.browser }
+    },
+    pluginReact.configs.flat.recommended,
+    jsxA11y.flatConfigs.recommended
 ]);
