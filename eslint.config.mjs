f6f343ae14115a17bb14f8533ab8224ec750d4d3
Josue Aguilar <josue.aguilar@kc.frb.org>
2026-05-18 13:00:30 -0500

    added rules, organized eslint js rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -22,10 +22,90 @@ export default defineConfig([
         files: ["**/*.{js,mjs,cjs,jsx}"],
         plugins: { js },
         extends: ["js/recommended"],
-        languageOptions: { globals: globals.browser }
+        languageOptions: { globals: globals.browser },
+        rules: {
+            // disabling class method "this" requirement to avoid React conflicts
+            "class-methods-use-this": [0],
+            // allow await in for-await-in loops
+            "no-await-in-loop": [0],
+            // allow continue statements
+            "no-continue": [0],
+            // allow ++ and --
+            "no-plusplus": [0],
+            // allow some globals
+            "no-restricted-globals": [0],
+            // allow for loops
+            "no-restricted-syntax": [2, "LabeledStatement", "WithStatement"],
+            "no-underscore-dangle": [0, { "allowAfterThis": true }],
+            "prefer-arrow-callback": ["error"],
+            // for now, don't do destructuring
+            "prefer-destructuring": [0],
+
+            // TODO: The following rules have been deprecated and should be replaced,
+            //  all have been addressed in @stylistic/eslint-plugin
+            "indent": [2, 4, { "SwitchCase": 1 }],
+            "max-len": [1, 100, { "tabWidth": 4 }],
+            "comma-dangle": [2, "never"],
+            "no-extra-semi": [1],
+            "arrow-parens": [2, "always"],
+            "quotes": [0],
+            "brace-style": [1, "stroustrup"],
+            "spaced-comment": [2, "always", { "exceptions": ["*"] }],
+            "function-paren-newline": [0] // TODO: deprecated
+        }
     },
-    react.configs.flat.recommended,
-    jsxA11y.flatConfigs.recommended,
-    reactHooks.configs.flat.recommended,
-    pluginImport.flatConfigs.recommended
+    {
+        ...react.configs.flat.recommended,
+        rules: {
+            // require 4 spaces in JSX as well
+            "react/jsx-indent": [0],
+            "react/jsx-indent-props": [0],
+            // closing brackets should be aligned with the final prop (props.. />)
+            "react/jsx-closing-bracket-location": [2, {"location": "after-props"}],
+            "react/prop-types":[1],
+            // downgrading to warning when using props purely within componentWillReceiveProps
+            "react/no-unused-prop-types": [1],
+            // disallow stateless functions in place of fully declared React components
+            "react/prefer-stateless-function": [0],
+            // allow binding in React props because we don't have autobind in ES6
+            "react/jsx-no-bind": [0],
+            // default prop types not required
+            "react/require-default-props": [0],
+            "react/default-props-match-prop-types": [0],
+            // allow object prop-type
+            "react/forbid-prop-types": [1, { "forbid": ["any"] }],
+            // allow unused state because linter does not always know
+            // when a state is passed to child components
+            "react/no-unused-state": [0],
+            // downgrade array index as React key to warning (though it should be higher, this would
+            // be an expensive refactor)
+            "react/no-array-index-key": [1]
+        }
+    },
+    {
+        ...jsxA11y.flatConfigs.recommended,
+        rules: {
+            "jsx-a11y/anchor-is-valid": "warn",
+            // downgrade label has for to a warning due to some design considerations
+            "jsx-a11y/label-has-for": [1]
+        }
+    },
+    {
+        ...reactHooks.configs.flat.recommended,
+        rules: {
+            "react-hooks/rules-of-hooks": "error",
+            "react-hooks/exhaustive-deps": [1]
+        }
+    },
+    {
+        ...pluginImport.flatConfigs.recommended,
+        rules: {
+            // allow named exports in files with default exports in order to expose containers
+            // for testing
+            "import/no-named-as-default": [0],
+            // downgrading export default preference to warning,
+            // since we may add additional exports to files in the future
+            "import/prefer-default-export": [1]
+        }
+    }
 ]);
