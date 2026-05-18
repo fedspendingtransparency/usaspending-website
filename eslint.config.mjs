00c24a0947fad909efeab4bfb753d4a17a128d7f
Josue Aguilar <josue.aguilar@kc.frb.org>
2026-05-18 11:13:11 -0500

    eslint-plugin-import, removed jasmine and babel eslint plugins

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,7 @@ import js from "@eslint/js";
 import react from "eslint-plugin-react";
 import reactHooks from 'eslint-plugin-react-hooks';
 import jsxA11y from 'eslint-plugin-jsx-a11y';
+import pluginImport from 'eslint-plugin-import';
 
 export default defineConfig([
     globalIgnores([
@@ -25,5 +26,6 @@ export default defineConfig([
     },
     react.configs.flat.recommended,
     jsxA11y.flatConfigs.recommended,
-    reactHooks.configs.flat.recommended
+    reactHooks.configs.flat.recommended,
+    pluginImport.flatConfigs.recommended
 ]);
