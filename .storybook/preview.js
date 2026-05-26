0d10480927578f2e0a40db0d2f79958b20e1f6da
Nick Torres <nicholas.torres@kc.frb.org>
2026-05-26 16:01:09 -0600

    storybook deletion

--- .storybook/preview.js
+++
@@ -1,9 +0,0 @@
-import '../src/_scss/_global.scss';
-import registerIcons from '../src/js/registerIcons';
-
-registerIcons();
-
-export const parameters = {
-  actions: {}
-}
-export const tags = ["autodocs"];
