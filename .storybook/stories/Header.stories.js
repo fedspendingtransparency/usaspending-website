023c77ad7c07d4993201111052d3983a0c7604f1
Nick Torres <nicholas.torres@kc.frb.org>
2026-05-28 14:29:40 -0600

    switch the heading

--- .storybook/stories/Header.stories.js
+++ .storybook/stories/Header.stories.js
@@ -3,7 +3,7 @@ import { fn } from 'storybook/test';
 import { Header } from './Header';
 
 export default {
-  title: 'Example/Header',
+  title: 'Examples/Header',
   component: Header,
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
--- .storybook/stories/Page.stories.js
+++ .storybook/stories/Page.stories.js
@@ -3,7 +3,7 @@ import { expect, userEvent, within } from 'storybook/test';
 import { Page } from './Page';
 
 export default {
-  title: 'Example/Page',
+  title: 'Examples/Page',
   component: Page,
   parameters: {
     // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
