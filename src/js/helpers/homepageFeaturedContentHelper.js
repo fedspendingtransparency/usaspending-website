7e29ba8cd56eb2b9a179f9437aa645cfc771f56f
Nick Torres <nicholas.torres@kc.frb.org>
2026-06-12 08:37:12 -0600

    change title

--- src/js/helpers/homepageFeaturedContentHelper.js
+++ src/js/helpers/homepageFeaturedContentHelper.js
@@ -9,7 +9,7 @@ const partition = (array, isValid) => array.reduce(
 );
 
 const fallbackMarketingArticle = {
-    title: "Exploring America's Finances Has Never Been Easier",
+    title: "Exploring America's Finances",
     fill: '#1b2b85',
     thumbnail_path: "../../img/featuredContent/thumbnails/exploring-americas-finances-thumbnail.webp",
     taxonomy: "Exploring America's Finances",
