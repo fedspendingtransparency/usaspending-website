24936fb724d016bd65fcc203375a9da1ef59db76
Josue Aguilar <josue.aguilar@kc.frb.org>
2026-06-17 13:43:22 -0500

    functionality started

--- src/js/components/search/collapsibleSidebar/SidebarWrapper.jsx
+++ src/js/components/search/collapsibleSidebar/SidebarWrapper.jsx
@@ -3,7 +3,7 @@
  * Created by Andrea Blackwell 11/05/2024
  **/
 
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import PropTypes from "prop-types";
 
@@ -20,6 +20,7 @@ const SidebarWrapper = React.memo(function SidebarWrapper({
     showMobileFilters, setShowMobileFilters, sidebarIsOpen, setSidebarIsOpen
 }) {
     const { isMedium } = useIsMobile();
+    const [sidebarContent, setSidebarContent] = useState('filters');
 
     const toggleOpened = (e) => {
         e.preventDefault();
@@ -32,38 +33,62 @@ const SidebarWrapper = React.memo(function SidebarWrapper({
         }
     };
 
+    console.log({ sidebarContent });
+
     return (
         <>
+            <div className="sidebar-natural-language-button" style={{ backgroundColor: "teal"}}>
+                <button onClick={() => setSidebarContent('filters')}>filters</button>
+                <button onClick={() => setSidebarContent('natural language')}>NL</button>
+            </div>
             {/* Eventually remove search-sidebar css */}
-            <div className={`search-collapsible-sidebar-container search-sidebar sticky ${sidebarIsOpen ? "opened" : ""} ${showMobileFilters ? "mobile" : ""}`}>
+            { sidebarContent === 'filters' ?
                 <div
-                    className="collapsible-sidebar--toggle"
-                    onClick={(e) => {
-                        toggleOpened(e);
-                    }}
-                    onKeyDown={(e) => {
-                        keyHandler(e, toggleOpened);
-                    }}
-                    role="button"
-                    aria-label={sidebarIsOpen ? "Close" : "Open"}
-                    focusable="true"
-                    tabIndex={0}>
-                    {sidebarIsOpen ?
-                        <FontAwesomeIcon className="chevron" icon="chevron-left" />
-                        :
-                        <FontAwesomeIcon className="chevron" icon="chevron-right" />
+                    className={`search-collapsible-sidebar-container search-sidebar sticky ${
+                        sidebarIsOpen ? "opened" : ""
+                    } ${
+                        showMobileFilters ? "mobile" : ""}`
+                    }>
+                    <div
+                        className="collapsible-sidebar--toggle"
+                        onClick={(e) => {
+                            toggleOpened(e);
+                        }}
+                        onKeyDown={(e) => {
+                            keyHandler(e, toggleOpened);
+                        }}
+                        role="button"
+                        aria-label={sidebarIsOpen ? "Close" : "Open"}
+                        focusable="true"
+                        tabIndex={0}>
+                        {sidebarIsOpen ?
+                            <FontAwesomeIcon className="chevron" icon="chevron-left" />
+                            :
+                            <FontAwesomeIcon className="chevron" icon="chevron-right" />
+                        }
+                    </div>
+                    { sidebarIsOpen && !isMedium &&
+                        <SidebarContent />
+                    }
+                    { sidebarIsOpen && showMobileFilters &&
+                        <MobileSidebarContent setShowMobileFilters={setShowMobileFilters} />
+                    }
+                    { !sidebarIsOpen && !isMedium &&
+                        <div style={{ margin: "18px 16px" }}>
+                            <FontAwesomeIcon title="Filters" icon="filter" />
+                        </div>
                     }
                 </div>
-                { sidebarIsOpen && !isMedium &&
-                    <SidebarContent />
-                }
-                { sidebarIsOpen && showMobileFilters &&
-                    <MobileSidebarContent setShowMobileFilters={setShowMobileFilters} />
-                }
-                { !sidebarIsOpen && !isMedium &&
-                    <div style={{ margin: "18px 16px" }}><FontAwesomeIcon title="Filters" icon="filter" /></div>
-                }
-            </div>
+                :
+                <div
+                    className={`search-collapsible-sidebar-container search-sidebar sticky ${
+                        sidebarIsOpen ? "opened" : ""
+                    } ${
+                        showMobileFilters ? "mobile" : ""}`
+                    }>
+                    natural language sidebar goes here
+                </div>
+            }
         </>
     );
 });
