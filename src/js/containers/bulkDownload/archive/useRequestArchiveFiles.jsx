f63384810a74716e7496179afcb1ef420c416e7d
Josue Aguilar <josue.aguilar@kc.frb.org>
2026-07-07 13:18:16 -0500

    using Tanstack Query for file fetching

---
+++ src/js/containers/bulkDownload/archive/useRequestArchiveFiles.jsx
@@ -0,0 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
+import { requestArchiveFiles } from "helpers/bulkDownloadHelper";
+
+
+const useRequestArchiveFiles = (id, fy, type) => {
+
+    const { data } = useQuery({
+        queryKey: ['requestArchiveFiles', id, fy, type],
+        queryFn: () => requestArchiveFiles({
+            agency: id,
+            fiscal_year: fy,
+            type
+        }).promise,
+        staleTime: Infinity,
+        refetchOnWindowFocus: false
+    })
+
+    return data?.data?.monthly_files || [];
+}
+
+export default useRequestArchiveFiles;
