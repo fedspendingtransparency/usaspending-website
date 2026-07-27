db72f9ce66d26d593a42041acd6a94d6ec5be109
Nick Torres <nicholas.torres@kc.frb.org>
2026-07-27 13:12:01 -0600

    react router update causes tests not to run

--- jest.config.js
+++ jest.config.js
@@ -46,8 +46,7 @@ module.exports = {
     },
     transformIgnorePatterns: [
         "node_modules/(?!(data-transparency-ui))",
-        "node_modules/(?!(react-markdown))/",
-        "/node_modules/(?!react-router|@react-router|react-router-dom|@remix-run)/"],
+        "node_modules/(?!(react-markdown))/"],
     modulePaths: ["<rootDir>/tests/testResources", "<rootDir>/src/js"],
     moduleFileExtensions: ['js', 'jsx', 'json', 'md', 'mdx']
 };
