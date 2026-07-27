2b60415bdfe38b5661cc9e023193c11eba69d0ca
Nick Torres <nicholas.torres@kc.frb.org>
2026-07-27 11:08:31 -0600

    change config

--- jest.config.js
+++ jest.config.js
@@ -40,11 +40,14 @@ module.exports = {
         "<rootDir>/tests/setup.js",
         "jest-canvas-mock"
     ],
+    testEnvironment: 'jsdom',
     transform: {
         "^.+\\.jsx$|js$": "babel-jest"
     },
     transformIgnorePatterns: [
-        "node_modules/(?!(data-transparency-ui))", "node_modules/(?!(react-markdown))/"],
+        "node_modules/(?!(data-transparency-ui))",
+        "node_modules/(?!(react-markdown))/",
+        "/node_modules/(?!react-router|@react-router)/"],
     modulePaths: ["<rootDir>/tests/testResources", "<rootDir>/src/js"],
     moduleFileExtensions: ['js', 'jsx', 'json', 'md', 'mdx']
 };
