55dfdb0ced04b9a5a42019e8e6c4e5deb96a0e7c
Trey Morgan <trey.morgan@kc.frb.org>
2026-08-05 20:03:17 -0500

    More updates to quarter picker and filter selection title

--- src/js/components/bulkDownload/FilterSelectionTitle.jsx
+++ src/js/components/bulkDownload/FilterSelectionTitle.jsx
@@ -95,7 +95,8 @@ const titleData = {
         showRequired: false,
         background: '#E5FAFF',
         fill: '#0E4F5C',
-        addClassName: "no-right-margin"
+        addClassName: "no-right-margin",
+        optClassName: "no-padding"
     },
     fileType: {
         icon: "folder",
@@ -105,7 +106,8 @@ const titleData = {
         showRequired: true,
         background: '#FEF2E4',
         fill: '#E66F0E',
-        addClassName: "no-right-margin"
+        addClassName: "no-right-margin",
+        optClassName: "no-padding"
     },
     defc: {
         icon: "hand-holding-medical",
@@ -130,7 +132,8 @@ const titleData = {
         showRequired: false,
         background: '#E8F5FF',
         fill: '#0B4778',
-        addClassName: ""
+        addClassName: "",
+        optClassName: "no-padding"
     },
     agencyFy: {
         icon: "building-columns",
@@ -166,18 +169,21 @@ const FilterSectionTitle = ({
         showRequired,
         addClassName,
         background,
-        fill
+        fill,
+        optClassName = ""
     } = titleData[type];
+
+    console.log({addClassName});
 
     return (
         <div className="download-filter__title-wrapper">
             <h4 className="download-filter__title">
                 <div
-                    className="title-icon"
+                    className={`title-icon ${optClassName}`}
                     style={{backgroundColor: background}} >
                     <FontAwesomeIcon icon={icon} color={fill} />
                 </div>
-                <span className="title-content">
+                <span>
                     {preSpan}
                     <span
                         className={`download-filter__title_em ${addClassName}`}
