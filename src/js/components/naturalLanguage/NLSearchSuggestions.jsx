656df65bd704c1c5037253739e7f3d3589b639ab
J.D. House <jd.house@kc.frb.org>
2026-08-21 13:27:41 -0500

    wip

--- src/js/components/naturalLanguage/NLSearchSuggestions.jsx
+++ src/js/components/naturalLanguage/NLSearchSuggestions.jsx
@@ -3,15 +3,23 @@
  * Created by Trey Morgan 7/2/2026
  */
 
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import useIsMobile from "hooks/useIsMobile";
 import { FlexGridRow, FlexGridCol, CardContainer, CardBody, Button } from 'data-transparency-ui';
 import Analytics from "../../helpers/analytics/Analytics";
 import { searchCardData } from "./NLData";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Keyboard, A11y, Pagination } from 'swiper/modules';
+import "swiper/css/bundle";
+import "swiper/css";
+
 const NLSearchSuggestions = () => {
     const navigate = useNavigate();
+    const { isDesktopSm } = useIsMobile();
+    const [activeCardIndex, setActiveCardIndex] = useState(0);
     
     const handleWatchVideosClick = () => {
         Analytics.event({
@@ -22,6 +30,85 @@ const NLSearchSuggestions = () => {
         });
         navigate("/training-videos");
     }
+
+     const onSlideChange = (d) => {
+        const currentIndex = d.realIndex;
+
+        d.slides.forEach((slide, i) => {
+            if (i === currentIndex) {
+                // eslint-disable-next-line no-param-reassign
+                slide.ariaHidden = false;
+                setActiveCardIndex(currentIndex);
+            }
+            else {
+                // eslint-disable-next-line no-param-reassign
+                slide.ariaHidden = true;
+            }
+        });
+    };
+
+    const getCardContent = () => {
+        console.log("checking isMobile context ===== ", isDesktopSm);
+        
+        if (!isDesktopSm) {
+            return (
+                <FlexGridRow className="search-suggestions__card-row">
+                    {searchCardData.map((card) => (
+                        <FlexGridCol 
+                            className="search-suggestions__card" 
+                            key={`search-suggestions-card-${card.id}`}
+                            mobile={12}
+                            tablet={12}
+                            desktop={4}>
+                            <CardContainer variant="outline" size="md">
+                                <CardBody
+                                    customClassName="search-suggestions__card-body"
+                                    overline={card.overline} 
+                                    headline={card.headline}
+                                    text={card.filterByHeader}>
+                                    {card.icons.map((icon) => icon)}
+                                </CardBody>
+                            </CardContainer>
+                        </FlexGridCol>
+                        ))   
+                    }
+                </FlexGridRow>
+            ); 
+        }
+        
+        return (
+            <Swiper
+                direction={"horizontal"}
+                slidesPerView={'auto'}
+                spaceBetween={16}
+                pagination={{
+                    clickable: true,
+                }}
+                keyboard
+                a11y
+                modules={[Keyboard, A11y, Pagination]}
+                onSlideChange={onSlideChange}
+                className="search-suggestions__swiper">
+                {searchCardData.map((card, i) => (
+                    <SwiperSlide
+                        tabIndex={activeCardIndex === i ? 0 : -1}
+                        className="search-suggestions__slide" 
+                        key={`search-suggestions-card-${card.id}`}>
+                            <CardContainer variant="outline" size="md">
+                                <CardBody
+                                    customClassName="search-suggestions__card-body"
+                                    overline={card.overline} 
+                                    headline={card.headline}
+                                    text={card.filterByHeader}>
+                                    {card.icons.map((icon) => icon)}
+                                </CardBody>
+                            </CardContainer>
+                        </SwiperSlide>
+                    ))   
+                }
+            </Swiper>
+        );
+    };
 
     return (
         <section className="search-suggestions__section"> 
@@ -48,27 +135,7 @@ const NLSearchSuggestions = () => {
                                 </div>}/>
                     </FlexGridCol>
                 </FlexGridRow>
-                <FlexGridRow className="search-suggestions__card-row">
-                    {searchCardData.map((card) => (
-                        <FlexGridCol 
-                            className="search-suggestions__card" 
-                            key={`search-suggestions-card-${card.id}`}
-                            mobile={12}
-                            tablet={12}
-                            desktop={4}>
-                            <CardContainer variant="outline" size="md">
-                                <CardBody
-                                    customClassName="search-suggestions__card-body"
-                                    overline={card.overline} 
-                                    headline={card.headline}
-                                    text={card.filterByHeader}>
-                                    {card.icons.map((icon) => icon)}
-                                </CardBody>
-                            </CardContainer>
-                        </FlexGridCol>
-                    ))   
-                    }
-                </FlexGridRow>
+                {getCardContent()}
             </FlexGridRow>
         </section> 
     );
