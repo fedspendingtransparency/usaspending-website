c9a697b0724b92a32ff52feb86e8872c3a75a4f8
J.D. House <jd.house@kc.frb.org>
2026-09-08 16:16:39 -0500

    wip -- no real api data

--- src/js/components/search/collapsibleSidebar/NLSearchButton.jsx
+++ src/js/components/search/collapsibleSidebar/NLSearchButton.jsx
@@ -10,20 +10,22 @@ const propTypes = {
     loadingState: PropTypes.string,
     classname: PropTypes.string,
     icon: PropTypes.string,
-    text: PropTypes.string
-    /* ,onclick: PropTypes.func */
+    text: PropTypes.string,
+    onClick: PropTypes.func
 };
 
-const NLSearchButton = ({loadingState, classname="default-search", icon=DEFAULT_ICON_PATH, text = "Search"}) => {
-
-    return (
-        <button className={`natural-language-submit ${classname}`} /* onClick={()=> console.debug("clicked")} */ >
-            {!loadingState && <img src={icon} alt="Icon for Search Button"/>}
-            {loadingState && <FontAwesomeIcon icon={['far', 'wand-magic-sparkles']} />}
-            {text}
-        </button>
-    );
-};
+const NLSearchButton = ({
+    loadingState,
+    classname="default-search",
+    icon=DEFAULT_ICON_PATH,
+    text = "Search", onClick
+}) => (
+    <button className={`natural-language-submit ${classname}`} onClick={onClick} >
+        {!loadingState && <img src={icon} alt="Icon for Search Button"/>}
+        {loadingState && <FontAwesomeIcon icon={['far', 'wand-magic-sparkles']} />}
+        {text}
+    </button>
+);
 
 NLSearchButton.propTypes = propTypes;
 export default NLSearchButton;
--- src/js/components/search/collapsibleSidebar/NLSidebarContent.jsx
+++ src/js/components/search/collapsibleSidebar/NLSidebarContent.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+/**
+ * NLSidebarContent.jsx
+ * Created by Nick Torres 8/28/2026
+ */
+
+/* eslint-disable max-len */
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import NLDefaultHint from "./NLDefaultHint";
 import NLSearchButton from "./NLSearchButton";
+import NLSearch from "./NLSearch";
+
+// will need updated when real api data is available
+const RESPONSE_TYPE = {
+    SEARCH_START: 'search_start',
+    SEARCH_COMPLETE: 'search_complete',
+    SEARCH_ERROR: 'search_error',
+    TOOL_START: 'tool_start',
+    TOOL_COMPLETE: 'tool_complete',
+    TOOL_ERROR: 'tool_error' 
+};
+
 const propTypes = {
     hintOnClick: PropTypes.func,
     text: PropTypes.string,
@@ -9,21 +27,52 @@ const propTypes = {
 };
 
 const NLSidebarContent = ({ hintOnClick, text, setText }) => {
+    // start api dummy states
+    const [loading, setLoading] = useState(false);
+    const queryText = "What schools in Anne Arundel, MD receive the most money in federal funding?";
+    const searchResponse = [
+        {
+            type: [RESPONSE_TYPE.TOOL_COMPLETE],
+            message: "Thinking..."
+        },
+        {   
+            type: [RESPONSE_TYPE.TOOL_COMPLETE],
+            message: 'Selecting Anne Arundel, MD'
+        },
+        {
+            type: [RESPONSE_TYPE.TOOL_COMPLETE],
+            message: 'Selecting higher education and public schools'
+        },
+        {
+            type: [RESPONSE_TYPE.TOOL_START],
+            message: 'Selecting funding over $500,000'
+        }
+    ];
+    // end api dummy states
+
     const MAX_CHARS = 500;
     const reset = () => setText("");
     let searchClass = 'default-search';
-    // eslint-disable-next-line no-useless-assignment
-    let icon = '';
-    if (text.length === 0) {
+    let btnText = "Search";
+    let icon = '../../../../img/magnifying-glass-white.svg';
+
+    if (loading) {
+        searchClass += " loading";
+        btnText = "Working...";
+    }
+    else if (text.length === 0) {
         searchClass += " disabled";
         icon = '../../../../img/magnifying-glass-disabled.svg';
     }
-    else {
-        icon = '../../../../img/magnifying-glass-white.svg';
+
+    const handleSubmit = () => {
+        setLoading(true);
     }
-    return (
+
+    let content = (
         <>
-            <p className="sidebar-text">Start a USAspending search in your own words, or use one of the prompts below to help you get started.</p><div className="sidebar-body-row">
+            <p className="sidebar-text">Start a USAspending search in your own words, or use one of the prompts below to help you get started.</p>
+            <div className="sidebar-body-row">
                 <span className="sidebar-example">Example Prompts: </span>
                 <NLDefaultHint onClick={hintOnClick} hint={<p>What schools in <span tabIndex={-1} className="hint-user-replace">[county, state]</span> receive the most money in federal funding?</p>} />
                 <NLDefaultHint onClick={hintOnClick} hint={<p>What programs received funding for veterans in <span tabIndex={-1} className="hint-user-replace">[state]</span> during <span tabIndex={-1} className="hint-user-replace">[time period]</span>?</p>} />
@@ -44,13 +93,34 @@ const NLSidebarContent = ({ hintOnClick, text, setText }) => {
                     <span className="textarea-char-count">{text.length} / {MAX_CHARS}</span>
                 </div>
             </div>
+        </>
+    )
+
+    if (loading) {
+        content = (
+            <>
+                <p className="sidebar-text query-text">{queryText}</p>
+                <div className="sidebar-body-row response">
+                    {searchResponse.map((response) => (
+                        <div key={`querysearch-feedback-${response.type}`}>
+                            <NLSearch {...response} />
+                        </div>  
+                    ))}
+                </div>
+            </>
+        )
+    }
+
+    return (
+        <>
+            {content}
             <div className="sidebar-body-row">
-                { /* We will have to make a couple adjustments to this when we have the api hooked up and are getting loading states back
-                on submit we have to sanitize the html*/}
                 <NLSearchButton
-                    text="Search"
+                    text={btnText}
                     icon={icon}
-                    classname={searchClass} />
+                    classname={searchClass}
+                    onClick={handleSubmit} 
+                    loadingState={loading} />
             </div>
             <div className="sidebar-body-row">
                 <span className="sidebar-ai-blurb">This is a new AI feature on USAspending.gov. AI can make mistakes, so be sure to check the results.</span>
