081e7099bbd7aa65226095dd8b638e7b9926873f
Su <J1SZS02@rb.win.frb.org>
2026-09-09 13:59:25 -0400

    [DEV-16252] Fix Obligated Amount sorting.

--- src/js/containers/explorer/detail/helpers/parseResults.js
+++ src/js/containers/explorer/detail/helpers/parseResults.js
@@ -8,7 +8,7 @@ const parseResults = (data, total, sort, goDeeper, goToUnreported) => {
         // Format obligated amount
         const obligatedAmount =
             formatMoneyWithPrecision(item.amount, 0);
-
+        const obligatedAmountNumeric = parseInt(obligatedAmount.replace(/[^\d]/g, ""), 10);
         // Convert from decimal value to percentage and round to 2 decimal places
         const formattedPercentage = ((item.amount / total) * 100).toFixed(2);
 
@@ -25,12 +25,19 @@ const parseResults = (data, total, sort, goDeeper, goToUnreported) => {
         const result = {
             Name: name,
             "Obligated Amount": obligatedAmount,
+            "Obligated Amount unformatted": obligatedAmountNumeric,
             "Percent of Total": percent,
             link
         };
         resultsArray.push(result);
     });
-
+    if (sort.field == 'Obligated Amount' ){
+        return orderBy(
+            resultsArray,
+            ['Obligated Amount unformatted'],
+            [sort.direction]
+        )
+    }  
     return orderBy(
         resultsArray,
         [sort.field],
