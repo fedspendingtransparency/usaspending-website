2c53e73963d649479a2dde4831c47c406d891063
Andrea Blackwell <andrea.blackwell@kc.frb.org>
2026-09-14 14:21:35 -0400

    updated constants for endpoint

--- src/js/components/search/collapsibleSidebar/useRequestNLSearch.jsx
+++ src/js/components/search/collapsibleSidebar/useRequestNLSearch.jsx
@@ -1,23 +1,35 @@
 import { useQuery, experimental_streamedQuery as streamedQuery } from "@tanstack/react-query";
 import GlobalConstants from 'GlobalConstants';
 
-const LLM_VALUE = GlobalConstants?.LLM?.HEADER_VALUE;
+const headers = {
+    'Content-Type': 'application/json',
+    'X-LLM-API-Key': GlobalConstants?.LLM?.HEADER_VALUE
+};
+
+const LLM_API = GlobalConstants?.LLM?.API ? GlobalConstants?.LLM?.API + 'v2/llm/filter-search/' : '/v2/llm/filter-search/';
+
 // const sample = "Show me all contracts greater than $3M in California for IT services in 2023";
 
 const useRequestNLSearch = (prompt) => {
+    const requestHeader = GlobalConstants?.LLM?.API ?
+        {
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify({'query': prompt})
+        }
+    :
+        {
+            method: 'POST',
+            body: JSON.stringify({'query': prompt})
+        }
+    ;
+
     const { data, refetch, status } = useQuery({
         queryKey: ['nl-search-stream'],
         enabled: false,
         queryFn: streamedQuery({
             streamFn: async function* () {
-                const request = await fetch(GlobalConstants.LLM.API, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'X-LLM-API-Key': LLM_VALUE
-                    },
-                    body: JSON.stringify({'query': prompt})
-                });
+                const request = fetch(LLM_API, requestHeader);
 
                 const reader = request.body.getReader();
                 const decoder = new TextDecoder();
