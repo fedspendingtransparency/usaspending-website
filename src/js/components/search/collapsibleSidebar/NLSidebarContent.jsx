95f1561997d349ef782be772d9a46756c0770043
Trey Morgan <trey.morgan@kc.frb.org>
2026-09-14 09:51:23 -0500

    Moved buildResponseState into its own helper file and updated all references

--- src/js/components/search/collapsibleSidebar/NLSidebarContent.jsx
+++ src/js/components/search/collapsibleSidebar/NLSidebarContent.jsx
@@ -4,8 +4,8 @@ import PropTypes from "prop-types";
 import NLDefaultHint from "./NLDefaultHint";
 import NLSearchButton from "./NLSearchButton";
 import NLSearchSuggestionsIcon from "../../naturalLanguage/NLSearchSuggestionsIcon";
-import { RESPONSE_TYPE, responseLookup, OPERATION } from "./NLConstants";
 import { setIsSearchActive } from "../../../redux/actions/sidebar/sidebarActions";
+import { buildResponseState } from "../../../helpers/search/naturalLanguage/searchResponseHelper";
 
 const propTypes = {
     hintOnClick: PropTypes.func,
@@ -14,74 +14,6 @@ const propTypes = {
     startNLSearch: PropTypes.func,
     data: PropTypes.array
 };
-
-const buildResponseState = (data = []) => {
-    const state = {
-        items: []
-    };
-    
-    data.forEach((event) => {
-        const {
-            search_id, 
-            tool_use_id, 
-            type, 
-            message, 
-            result
-        } = event ?? {};
-           
-        const response = responseLookup[type];
-    
-        if (!response) {
-            return;
-        }
-    
-        const item = {
-            searchId: search_id,
-            ...(tool_use_id && {
-                toolId: tool_use_id
-            }),
-            ...response,
-            ...(message && {
-                label: message
-            }),
-            ...(result && {
-                result
-            })
-        };
-    
-        // Search messages are always new display items
-        if (response.operation === OPERATION.SEARCH) {
-            state.items.push(item);
-            return; 
-        }
-    
-        const toolId = tool_use_id;
-    
-        if (!toolId) {
-            return;
-        }
-    
-        // Tool start creates a new display item
-        if (type === RESPONSE_TYPE.TOOL_START) {
-            state.items.push(item);
-            return;
-        }
-    
-        // Tool complete/error updates the existing item
-        const itemIndex = state.items.findIndex(
-            (existingItem) => existingItem.toolId === toolId
-        );
-    
-        if (itemIndex !== -1) {
-            state.items[itemIndex] = {
-                ...state.items[itemIndex],
-                ...item
-            };
-        }
-    });
-    
-    return state;
-}
 
 const NLSidebarContent = ({ hintOnClick, text, setText, startNLSearch, data }) => {
     const isSearchActive = useSelector((state) => state.sidebar.isSearchActive);
---
+++ src/js/helpers/search/naturalLanguage/searchResponseHelper.js
@@ -0,0 +1,70 @@
+import {RESPONSE_TYPE, OPERATION, responseLookup } from "../../../components/search/collapsibleSidebar/NLConstants";
+
+// eslint-disable-next-line import/prefer-default-export
+export const buildResponseState = (data = []) => {
+    const state = {
+        items: []
+    };
+    
+    data.forEach((event) => {
+        const {
+            search_id, 
+            tool_use_id, 
+            type, 
+            message, 
+            result
+        } = event ?? {};
+           
+        const response = responseLookup[type];
+    
+        if (!response) {
+            return;
+        }
+    
+        const item = {
+            searchId: search_id,
+            ...(tool_use_id && {
+                toolId: tool_use_id
+            }),
+            ...response,
+            ...(message && {
+                label: message
+            }),
+            ...(result && {
+                result
+            })
+        };
+    
+        // Search messages are always new display items
+        if (response.operation === OPERATION.SEARCH) {
+            state.items.push(item);
+            return; 
+        }
+    
+        const toolId = tool_use_id;
+    
+        if (!toolId) {
+            return;
+        }
+    
+        // Tool start creates a new display item
+        if (type === RESPONSE_TYPE.TOOL_START) {
+            state.items.push(item);
+            return;
+        }
+    
+        // Tool complete/error updates the existing item
+        const itemIndex = state.items.findIndex(
+            (existingItem) => existingItem.toolId === toolId
+        );
+    
+        if (itemIndex !== -1) {
+            state.items[itemIndex] = {
+                ...state.items[itemIndex],
+                ...item
+            };
+        }
+    });
+    
+    return state;
+}
