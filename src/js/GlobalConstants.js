3f5d915c9ecd040f192cfcf4f2596be971317ed1
Andrea Jenkins Blackwell <121010+anjenkin@users.noreply.github.com>
2026-09-21 10:39:42 -0400

    fixing issue with slash (#5566)

--- src/js/GlobalConstants.js
+++ src/js/GlobalConstants.js
@@ -25,7 +25,7 @@ const globalConstants = {
     REQUEST_VERSION: '2020-06-01',
     IS_NEW_DOWNLOAD: (process.env.ENV === 'qat' || process.env.ENV === 'sandbox'),
     FEATURED_CONTENT_ROTATION: "week",   // "week" or "sprint"
-    LLM_API: local?.localConstants?.LLM_API ||  local?.localConstants?.API || process.env.USASPENDING_API || 'https://api.usaspending.gov/api/',
+    LLM_API: local?.localConstants?.LLM_API ||  local?.localConstants?.API || process.env.USASPENDING_API,
     LLM_HEADER_VALUE: local?.localConstants?.LLM_HEADER_VALUE || process.env.X_LLM_API_KEY
 };
 
