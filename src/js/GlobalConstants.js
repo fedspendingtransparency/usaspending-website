a4b9edac61e8dc545a9e2be696126e914ab98129
Andrea Blackwell <andrea.blackwell@kc.frb.org>
2026-09-25 12:52:13 -0400

    fixing staging specific issue

--- src/js/GlobalConstants.js
+++ src/js/GlobalConstants.js
@@ -24,7 +24,7 @@ const globalConstants = {
     DUNS_LABEL: 'Legacy ',
     REQUEST_VERSION: '2020-06-01',
     FEATURED_CONTENT_ROTATION: "week",   // "week" or "sprint"
-    LLM_API: local?.localConstants?.LLM_API ||  local?.localConstants?.API || process.env.USASPENDING_API,
+    LLM_API: local?.localConstants?.LLM_API || process.env.USASPENDING_API,
     LLM_HEADER_VALUE: local?.localConstants?.LLM_HEADER_VALUE || process.env.X_LLM_API_KEY
 };
 
